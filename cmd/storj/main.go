29ebb3a6073c2749b0eed4aaf7ac96b4e68cd684
Kaloyan Raev <kaloyan@storj.io>
2018-04-12 11:39:19 +0300

    Remove LGPL license files and adjust copyright headers

--- cmd/storj/main.go
+++ cmd/storj/main.go
@@ -1,19 +1,5 @@
 // Copyright (C) 2018 Storj Labs, Inc.
-//
-// This file is part of Storj CLI.
-//
-// Storj CLI is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// Storj CLI is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Storj CLI.  If not, see <http://www.gnu.org/licenses/>.
+// See LICENSE for copying information.
 
 package main
 
