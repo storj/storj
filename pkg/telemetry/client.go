5be1da982f15d6b12c763c347fb3d7886eb5cbb5
JT Olds <hello@jtolio.com>
2018-05-07 12:03:40 -0600

    pkg/telemetry: telemetry package using zeebo/admission (#26)
    * pkg/telemetry: use zeebo/admission
    
    * pkg/telemetry: updated to use latest zeebo/admission changes

---
+++ pkg/telemetry/client.go
@@ -0,0 +1,127 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package telemetry
+
+import (
+	"context"
+	"net"
+	"os"
+	"time"
+
+	"github.com/zeebo/admission/admmonkit"
+	"github.com/zeebo/admission/admproto"
+	"go.uber.org/zap"
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+)
+
+const (
+	DefaultInterval = time.Hour
+
+	// MTUs are often 1500, though a good argument could be made for 512
+	DefaultPacketSize = 1000
+)
+
+// ClientOpts allows you to set Client Options
+type ClientOpts struct {
+	// Interval is how frequently stats from the provided Registry will be
+	// sent up. Note that this interval is "jittered", so the actual interval
+	// is taken from a normal distribution with a mean of Interval and a
+	// variance of Interval/4. Defaults to DefaultInterval
+	Interval time.Duration
+
+	// Application is the application name, usually prepended to metric names.
+	// By default it will be os.Args[0]
+	Application string
+
+	// Instance is a string that identifies this particular server. Could be a
+	// node id, but defaults to the first non-nil MAC address
+	Instance string
+
+	// PacketSize controls how we fragment the data as it goes out in UDP
+	// packets. Defaults to DefaultPacketSize
+	PacketSize int
+
+	// Registry is where to get stats from. Defaults to monkit.Default
+	Registry *monkit.Registry
+
+	// FloatEncoding is how floats should be encoded on the wire.
+	// Default is float16.
+	FloatEncoding admproto.FloatEncoding
+}
+
+// Client is a telemetry client for sending UDP packets at a regular interval
+// from a monkit.Registry
+type Client struct {
+	interval time.Duration
+	opts     admmonkit.Options
+}
+
+// NewClient constructs a telemetry client that sends packets to remote_addr
+// over UDP.
+func NewClient(remote_addr string, opts ClientOpts) (rv *Client, err error) {
+	if opts.Interval == 0 {
+		opts.Interval = DefaultInterval
+	}
+	if opts.Application == "" {
+		if len(os.Args) > 0 {
+			opts.Application = os.Args[0]
+		} else {
+			// what the actual heck
+			opts.Application = "unknown"
+		}
+	}
+	if opts.Instance == "" {
+		// instance by default is the first non-nil mac address
+		ifaces, err := net.Interfaces()
+		if err != nil {
+			return nil, err
+		}
+		for _, iface := range ifaces {
+			if iface.HardwareAddr != nil {
+				opts.Instance = iface.HardwareAddr.String()
+				break
+			}
+		}
+		if opts.Instance == "" {
+			opts.Instance = "unknown"
+		}
+	}
+	if opts.Registry == nil {
+		opts.Registry = monkit.Default
+	}
+	if opts.PacketSize == 0 {
+		opts.PacketSize = DefaultPacketSize
+	}
+
+	return &Client{
+		interval: opts.Interval,
+		opts: admmonkit.Options{
+			Application: opts.Application,
+			InstanceId:  []byte(opts.Instance),
+			Address:     remote_addr,
+			PacketSize:  opts.PacketSize,
+			Registry:    opts.Registry,
+			ProtoOpts:   admproto.Options{FloatEncoding: opts.FloatEncoding},
+		},
+	}, nil
+}
+
+// Run calls Report roughly every Interval
+func (c *Client) Run(ctx context.Context) {
+	for {
+		time.Sleep(jitter(c.interval))
+		if ctx.Err() != nil {
+			return
+		}
+		err := c.Report(ctx)
+		if err != nil {
+			zap.S().Errorf("failed sending telemetry report: %v", err)
+		}
+	}
+}
+
+// Report bundles up all the current stats and writes them out as UDP packets
+func (c *Client) Report(ctx context.Context) error {
+	return admmonkit.Send(ctx, c.opts)
+}
---
+++ pkg/telemetry/server.go
@@ -0,0 +1,114 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package telemetry
+
+import (
+	"context"
+	"net"
+	"syscall"
+
+	"github.com/zeebo/admission"
+	"github.com/zeebo/admission/admproto"
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+)
+
+var (
+	mon = monkit.Package()
+)
+
+// Handler is called every time a new metric comes in
+type Handler interface {
+	Metric(application, instance string, key []byte, val float64)
+}
+
+type HandlerFunc func(application, instance string, key []byte, val float64)
+
+func (f HandlerFunc) Metric(a, i string, k []byte, v float64) { f(a, i, k, v) }
+
+// Server listens for incoming metrics
+type Server struct {
+	conn net.PacketConn
+}
+
+// Addr returns the address the server is serving on
+func (s *Server) Addr() string {
+	return s.conn.LocalAddr().String()
+}
+
+// Listen will start listening on addr for metrics
+func Listen(addr string) (*Server, error) {
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return &Server{conn: conn}, nil
+}
+
+// Close will stop listening
+func (s *Server) Close() error {
+	return s.conn.Close()
+}
+
+// Serve will wait for metrics and call Handler h as they come in
+func (s *Server) Serve(ctx context.Context, h Handler) error {
+	scconn, ok := s.conn.(syscall.Conn)
+	if !ok {
+		return Error.New("invalid conn: %T", s.conn)
+	}
+	rc, err := scconn.SyscallConn()
+	if err != nil {
+		return err
+	}
+	return admission.Dispatcher{
+		Handler: handlerWrapper{h: h},
+		Conn:    rc,
+	}.Run(ctx)
+}
+
+// ListenAndServe combines Listen and Serve
+func ListenAndServe(ctx context.Context, addr string, h Handler) error {
+	s, err := Listen(addr)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+	return s.Serve(ctx, h)
+}
+
+type handlerWrapper struct {
+	h Handler
+}
+
+var (
+	handleTask = mon.Task()
+)
+
+func (h handlerWrapper) Handle(ctx context.Context, m *admission.Message) {
+	finish := handleTask(nil)
+
+	data, err := admproto.CheckChecksum(m.Data)
+	if err != nil {
+		finish(&err)
+		return
+	}
+	r := admproto.NewReaderWith(m.Scratch[:])
+	data, application_b, instance_b, err := r.Begin(data)
+	if err != nil {
+		finish(&err)
+		return
+	}
+	application, instance := string(application_b), string(instance_b)
+	var key []byte
+	var value float64
+	for len(data) > 0 {
+		data, key, value, err = r.Next(data)
+		if err != nil {
+			finish(&err)
+			return
+		}
+		h.h.Metric(application, instance, key, value)
+	}
+
+	finish(nil)
+}
