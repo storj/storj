3f61e9319258c74f9761b0c243a664d8e9dabf01
nfarah86 <nfarah86@gmail.com>
2018-05-08 10:27:18 -0700

    updated main.go with setting env variable

--- examples/auth/main.go
+++ examples/auth/main.go
@@ -3,20 +3,28 @@
 
 package main
 
-import  (
+import (
 	"fmt"
-	
+	"os"
+
 	"storj.io/storj/netstate/auth"
-	
-	"github.com/spf13/viper"
+
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	// example of how the auth package is working. 
+	// see readme in auth/ for how to run
+	
 	pflag.String("key", "", "this is your API KEY")
 	viper.BindPFlag("key", pflag.Lookup("key"))
 	pflag.Parse()
-	
+
+	viper.SetEnvPrefix("API")
+	os.Setenv("API_KEY", "12345")
+	viper.AutomaticEnv()
+
 	httpRequestHeaders := auth.InitializeHeaders()
 	xApiKey := httpRequestHeaders.Get("X-Api-Key")
 
