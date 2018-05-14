90ca28f7bc5a7537f6bdf22340612e958e2ce565
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2018-05-14 23:30:57 +0300

    eestream avoid waiting for slow pieces (#30)
    * WIP eestream avoid waiting for slow pieces
    
    * Use non-blocking select to harvest all available inbufs
    
    * Better way to check for elapsed time in tests
    
    * Improve comment
    
    * Determine EOF based on expected size
    
    * Remove unused readerError type
    
    * Configurable readers channel size
    
    * Close properly decodedReader in tests
    
    * Use context for properly closing the decodedReader
    
    * Handle infectious errors using the new descendent classes
    
    * Refactor decodedReader.Read() into helper functions
    
    * Reenable TestRSErrors
    
    * Test with Rangers
    
    * Remove obsolete comment

--- pkg/eestream/rs_test.go
+++ pkg/eestream/rs_test.go
@@ -5,17 +5,21 @@ package eestream
 
 import (
 	"bytes"
+	"context"
+	"crypto/sha256"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/vivint/infectious"
 
 	"storj.io/storj/internal/pkg/readcloser"
+	"storj.io/storj/pkg/ranger"
 )
 
 func TestRS(t *testing.T) {
@@ -30,7 +34,9 @@ func TestRS(t *testing.T) {
 	for i, reader := range readers {
 		readerMap[i] = ioutil.NopCloser(reader)
 	}
-	data2, err := ioutil.ReadAll(DecodeReaders(readerMap, rs))
+	decoder := DecodeReaders(context.Background(), readerMap, rs, 32*1024, 0)
+	defer decoder.Close()
+	data2, err := ioutil.ReadAll(decoder)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -53,16 +59,64 @@ func TestRSUnexectedEOF(t *testing.T) {
 	for i, reader := range readers {
 		readerMap[i] = ioutil.NopCloser(reader)
 	}
+	decoder := DecodeReaders(context.Background(), readerMap, rs, 32*1024, 0)
+	defer decoder.Close()
 	// Try ReadFull more data from DecodeReaders than available
 	data2 := make([]byte, len(data)+1024)
-	_, err = io.ReadFull(DecodeReaders(readerMap, rs), data2)
+	_, err = io.ReadFull(decoder, data2)
 	assert.EqualError(t, err, io.ErrUnexpectedEOF.Error())
+}
+
+func TestRSRanger(t *testing.T) {
+	data := randData(32 * 1024)
+	fc, err := infectious.NewFEC(2, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rs := NewRSScheme(fc, 8*1024)
+	encKey := sha256.Sum256([]byte("the secret key"))
+	var firstNonce [24]byte
+	encrypter, err := NewSecretboxEncrypter(
+		&encKey, &firstNonce, rs.DecodedBlockSize())
+	if err != nil {
+		t.Fatal(err)
+	}
+	readers := EncodeReader(TransformReader(PadReader(ioutil.NopCloser(
+		bytes.NewReader(data)), encrypter.InBlockSize()), encrypter, 0), rs)
+	pieces, err := readAll(readers)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rrs := map[int]ranger.Ranger{}
+	for i, piece := range pieces {
+		rrs[i] = ranger.ByteRanger(piece)
+	}
+	decrypter, err := NewSecretboxDecrypter(
+		&encKey, &firstNonce, rs.DecodedBlockSize())
+	rr, err := Decode(context.Background(), rrs, rs, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr, err = Transform(rr, decrypter)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr, err = UnpadSlow(rr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data2, err := ioutil.ReadAll(rr.Range(0, rr.Size()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(data, data2) {
+		t.Fatalf("rs encode/decode failed")
+	}
 }
 
 // Some pieces will read error.
 // Test will pass if at least required number of pieces are still good.
-// TODO: renable this test!
-func DisabledTestRSErrors(t *testing.T) {
+func TestRSErrors(t *testing.T) {
 	for i, tt := range []testCase{
 		{4 * 1024, 1024, 1, 1, 0, false},
 		{4 * 1024, 1024, 1, 1, 1, true},
@@ -97,18 +151,18 @@ func TestRSEOF(t *testing.T) {
 		{4 * 1024, 1024, 1, 1, 0, false},
 		{4 * 1024, 1024, 1, 1, 1, true},
 		{4 * 1024, 1024, 1, 2, 0, false},
-		{4 * 1024, 1024, 1, 2, 1, true},
+		{4 * 1024, 1024, 1, 2, 1, false},
 		{4 * 1024, 1024, 1, 2, 2, true},
 		{4 * 1024, 1024, 2, 4, 0, false},
 		{4 * 1024, 1024, 2, 4, 1, false},
-		{4 * 1024, 1024, 2, 4, 2, true},
+		{4 * 1024, 1024, 2, 4, 2, false},
 		{4 * 1024, 1024, 2, 4, 3, true},
 		{4 * 1024, 1024, 2, 4, 4, true},
 		{6 * 1024, 1024, 3, 7, 0, false},
 		{6 * 1024, 1024, 3, 7, 1, false},
 		{6 * 1024, 1024, 3, 7, 2, false},
-		{6 * 1024, 1024, 3, 7, 3, true},
-		{6 * 1024, 1024, 3, 7, 4, true},
+		{6 * 1024, 1024, 3, 7, 3, false},
+		{6 * 1024, 1024, 3, 7, 4, false},
 		{6 * 1024, 1024, 3, 7, 5, true},
 		{6 * 1024, 1024, 3, 7, 6, true},
 		{6 * 1024, 1024, 3, 7, 7, true},
@@ -123,37 +177,6 @@ func TestRSEOF(t *testing.T) {
 // Some pieces will read EOF earlier than expected
 // Test will pass if those pieces are less than required.
 func TestRSEarlyEOF(t *testing.T) {
-	for i, tt := range []testCase{
-		{4 * 1024, 1024, 1, 1, 0, false},
-		{4 * 1024, 1024, 1, 1, 1, true},
-		{4 * 1024, 1024, 1, 2, 0, false},
-		{4 * 1024, 1024, 1, 2, 1, true},
-		{4 * 1024, 1024, 1, 2, 2, true},
-		{4 * 1024, 1024, 2, 4, 0, false},
-		{4 * 1024, 1024, 2, 4, 1, false},
-		{4 * 1024, 1024, 2, 4, 2, true},
-		{4 * 1024, 1024, 2, 4, 3, true},
-		{4 * 1024, 1024, 2, 4, 4, true},
-		{6 * 1024, 1024, 3, 7, 0, false},
-		{6 * 1024, 1024, 3, 7, 1, false},
-		{6 * 1024, 1024, 3, 7, 2, false},
-		{6 * 1024, 1024, 3, 7, 3, true},
-		{6 * 1024, 1024, 3, 7, 4, true},
-		{6 * 1024, 1024, 3, 7, 5, true},
-		{6 * 1024, 1024, 3, 7, 6, true},
-		{6 * 1024, 1024, 3, 7, 7, true},
-	} {
-		testRSProblematic(t, tt, i, func(in []byte) io.ReadCloser {
-			// Read EOF after 500 bytes
-			return readcloser.LimitReadCloser(
-				ioutil.NopCloser(bytes.NewReader(in)), 500)
-		})
-	}
-}
-
-// Some pieces will read EOF later than expected.
-// Test will pass if at least required number of pieces are still good.
-func TestRSLateEOF(t *testing.T) {
 	for i, tt := range []testCase{
 		{4 * 1024, 1024, 1, 1, 0, false},
 		{4 * 1024, 1024, 1, 1, 1, true},
@@ -173,6 +196,37 @@ func TestRSLateEOF(t *testing.T) {
 		{6 * 1024, 1024, 3, 7, 5, true},
 		{6 * 1024, 1024, 3, 7, 6, true},
 		{6 * 1024, 1024, 3, 7, 7, true},
+	} {
+		testRSProblematic(t, tt, i, func(in []byte) io.ReadCloser {
+			// Read EOF after 500 bytes
+			return readcloser.LimitReadCloser(
+				ioutil.NopCloser(bytes.NewReader(in)), 500)
+		})
+	}
+}
+
+// Some pieces will read EOF later than expected.
+// Test will pass if at least required number of pieces are still good.
+func TestRSLateEOF(t *testing.T) {
+	for i, tt := range []testCase{
+		{4 * 1024, 1024, 1, 1, 0, false},
+		{4 * 1024, 1024, 1, 1, 1, false},
+		{4 * 1024, 1024, 1, 2, 0, false},
+		{4 * 1024, 1024, 1, 2, 1, false},
+		{4 * 1024, 1024, 1, 2, 2, false},
+		{4 * 1024, 1024, 2, 4, 0, false},
+		{4 * 1024, 1024, 2, 4, 1, false},
+		{4 * 1024, 1024, 2, 4, 2, false},
+		{4 * 1024, 1024, 2, 4, 3, false},
+		{4 * 1024, 1024, 2, 4, 4, false},
+		{6 * 1024, 1024, 3, 7, 0, false},
+		{6 * 1024, 1024, 3, 7, 1, false},
+		{6 * 1024, 1024, 3, 7, 2, false},
+		{6 * 1024, 1024, 3, 7, 3, false},
+		{6 * 1024, 1024, 3, 7, 4, false},
+		{6 * 1024, 1024, 3, 7, 5, false},
+		{6 * 1024, 1024, 3, 7, 6, false},
+		{6 * 1024, 1024, 3, 7, 7, false},
 	} {
 		testRSProblematic(t, tt, i, func(in []byte) io.ReadCloser {
 			// extend the input with random number of random bytes
@@ -213,6 +267,29 @@ func TestRSRandomData(t *testing.T) {
 	}
 }
 
+// Some pieces will read slowly
+func TestRSSlow(t *testing.T) {
+	for i, tt := range []testCase{
+		{4 * 1024, 1024, 1, 1, 0, false},
+		{4 * 1024, 1024, 1, 2, 0, false},
+		{4 * 1024, 1024, 2, 4, 0, false},
+		{4 * 1024, 1024, 2, 4, 1, false},
+		{6 * 1024, 1024, 3, 7, 0, false},
+		{6 * 1024, 1024, 3, 7, 1, false},
+		{6 * 1024, 1024, 3, 7, 2, false},
+		{6 * 1024, 1024, 3, 7, 3, false},
+	} {
+		start := time.Now()
+		testRSProblematic(t, tt, i, func(in []byte) io.ReadCloser {
+			// sleep 1 second before every read
+			return ioutil.NopCloser(SlowReader(bytes.NewReader(in), 1*time.Second))
+		})
+		if time.Since(start) > 1*time.Second {
+			t.Fatalf("waited for slow reader")
+		}
+	}
+}
+
 type testCase struct {
 	dataSize    int
 	blockSize   int
@@ -248,7 +325,10 @@ func testRSProblematic(t *testing.T, tt testCase, i int, fn problematicReadClose
 	for i := tt.problematic; i < tt.total; i++ {
 		readerMap[i] = ioutil.NopCloser(bytes.NewReader(pieces[i]))
 	}
-	data2, err := ioutil.ReadAll(DecodeReaders(readerMap, rs))
+	decoder := DecodeReaders(context.Background(),
+		readerMap, rs, int64(tt.dataSize), 3*1024)
+	defer decoder.Close()
+	data2, err := ioutil.ReadAll(decoder)
 	if tt.fail {
 		if err == nil && bytes.Compare(data, data2) == 0 {
 			assert.Fail(t, "expected to fail, but didn't", errTag)
@@ -275,4 +355,18 @@ func readAll(readers []io.Reader) ([][]byte, error) {
 		}
 	}
 	return pieces, nil
+}
+
+func SlowReader(r io.Reader, delay time.Duration) io.Reader {
+	return &slowReader{Reader: r, Delay: delay}
+}
+
+type slowReader struct {
+	Reader io.Reader
+	Delay  time.Duration
+}
+
+func (s *slowReader) Read(p []byte) (n int, err error) {
+	time.Sleep(s.Delay)
+	return s.Reader.Read(p)
 }
