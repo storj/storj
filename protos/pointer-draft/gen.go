a9d0ed4f4c9bbfd92f45a3d8d019935faee2b896
Natalie Ventura Villasana <nat@storj.io>
2018-05-15 16:39:22 -0400

    removes unnecessary gen.go file

--- protos/pointer-draft/gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-// Package pointer will be package netstate eventually
-package pointer
-
-//go:generate protoc --go_out=plugins=grpc:. pointer.proto
