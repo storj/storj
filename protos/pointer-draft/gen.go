16a68449391c02e8fd38d1489a67949ce35e6e1b
Natalie Ventura Villasana <nat@storj.io>
2018-05-15 16:38:08 -0400

    draft for Pointer data structure defined in pb format

---
+++ protos/pointer-draft/gen.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+// Package pointer will be package netstate eventually
+package pointer
+
+//go:generate protoc --go_out=plugins=grpc:. pointer.proto
