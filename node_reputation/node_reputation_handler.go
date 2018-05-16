912ebe358a8f76cc01a6fb9a44edb0da9d95135e
James <jhagans@storj.io>
2018-05-16 14:46:19 -0400

    post split of node rep go

--- node_reputation/node_reputation_handler.go
+++ node_reputation/node_reputation_handler.go
@@ -4,6 +4,9 @@
 package nodereputation
 
 import (
+	"database/sql"
+	"strconv"
+
 	"golang.org/x/net/context"
 )
 
@@ -52,4 +55,177 @@ func (s *Server) FilterNodeReputation(ctx context.Context, in *NodeFilter) (*Nod
 	}
 
 	return &nodes, nil
+}
+
+// byNodeName function used in handler by update reputation
+func byNodeName(db *sql.DB, nodeName string) (NodeReputationRecord, error) {
+	var recordForError NodeReputationRecord
+	selectNodeStmt := genWhereStatement(selectAllStmt, nodeNameColumn, equal, nodeName)
+	row, err := endianReputation(db, selectNodeStmt)
+	if err != nil {
+		return recordForError, err
+	}
+
+	return row.serde(), nil
+}
+
+// toWhereOpt is a method to convert a proto operand to a where operation
+func (opt NodeFilter_Operand) toWhereOpt() whereOpt {
+	res := notEqual
+	switch opt {
+	case NodeFilter_EQUAL_TO:
+		res = equal
+	case NodeFilter_GREATER_THAN:
+		res = greater
+	case NodeFilter_GREATER_THAN_EQUAL_TO:
+		res = greaterEqual
+	case NodeFilter_LESS_THAN:
+		res = less
+	case NodeFilter_LESS_THAN_EQUAL_TO:
+		res = lessEqual
+	case NodeFilter_NOT_EQUAL_TO:
+		res = notEqual
+	}
+	return res
+}
+
+// toColum method converts a proto column type to a sum column type
+func (col ColumnName) toColumn() column {
+	res := sourceColumn
+	switch col {
+	case ColumnName_source:
+		res = sourceColumn
+	case ColumnName_node_name:
+		res = nodeNameColumn
+	case ColumnName_timestamp:
+		res = timestampColumn
+	case ColumnName_uptime:
+		res = uptimeColumn
+	case ColumnName_audit_success:
+		res = auditSuccessColumn
+	case ColumnName_audit_fail:
+		res = auditFailColumn
+	case ColumnName_latency:
+		res = latencyColumn
+	case ColumnName_amount_of_data_stored:
+		res = amountOfDataStoredColumn
+	case ColumnName_false_claims:
+		res = falseClaimsColumn
+	case ColumnName_shards_modified:
+		res = shardsModifiedColumn
+	}
+
+	return res
+}
+
+// selectNodeWhere is a function that queries the reputation db and finds nodes that satisfies the where clause
+func selectNodeWhere(db *sql.DB, col ColumnName, operand NodeFilter_Operand, value string) (NodeReputationRecords, error) {
+	var records []*NodeReputationRecord
+	recordsForError := NodeReputationRecords{Records: records}
+
+	selectNodeStmt := genWhereStatement(selectAllStmt, col.toColumn(), operand.toWhereOpt(), value)
+	nodes, err := getNodeReputationRecords(db, selectNodeStmt)
+	if err != nil {
+		return recordsForError, err
+	}
+
+	for _, node := range nodes {
+		n := node.serde()
+		records = append(records, &n)
+	}
+
+	return NodeReputationRecords{
+		Records: records,
+	}, nil
+}
+
+// insertNodeUpdate used in handler by update node reputation
+func insertNodeUpdate(db *sql.DB, in *NodeUpdate) UpdateReply_ReplyType {
+	res := UpdateReply_UPDATE_FAILED
+	newRecord := newReputationRow(in.Source, in.NodeName)
+
+	switch in.ColumnName.toColumn() {
+	case uptimeColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(uptimeColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case auditSuccessColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(auditSuccessColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case auditFailColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(auditFailColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case latencyColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(latencyColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case amountOfDataStoredColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(amountOfDataStoredColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case falseClaimsColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(falseClaimsColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	case shardsModifiedColumn:
+		val, err := strconv.ParseInt(in.ColumnValue, 10, 64)
+		if err != nil {
+			res = UpdateReply_UPDATE_FAILED
+		} else {
+			res = UpdateReply_UPDATE_SUCCESS
+			newRecord = newRecord.morphism(shardsModifiedColumn, overWrite, val)
+		}
+		morph := []nodeReputationRecord{newRecord}
+		insertRows(db, morph, insertStmt)
+
+	default:
+		res = UpdateReply_UPDATE_FAILED
+
+	}
+
+	return res
 }
---
+++ node_reputation/node_reputation_sqlite3.go
@@ -0,0 +1,317 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package nodereputation
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/zeebo/errs"
+	// import of sqlite3 for side effects
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// StartDBError is an error class for errors related to the reputation package
+var StartDBError = errs.Class("reputation start sqlite3 error")
+
+// CreateTableError is an error class for errors related to the reputation package
+var CreateTableError = errs.Class("reputation table creation error")
+
+// InsertError is an error class for errors related to the reputation package
+var InsertError = errs.Class("reputation insertion error")
+
+// SelectError is an error class for errors related to the reputation package
+var SelectError = errs.Class("reputation selection error")
+
+// IterError is an error class for errors related to the reputation package
+var IterError = errs.Class("reputation iteration error")
+
+// DeleteError is an error class for errors related to the reputation package
+var DeleteError = errs.Class("reputation deletion error")
+
+// nodeReputationRecord is the Data type for Rows in Reputation table
+type nodeReputationRecord struct {
+	source             string
+	nodeName           string
+	timestamp          string
+	uptime             int64
+	auditSuccess       int64
+	auditFail          int64
+	latency            int64
+	amountOfDataStored int64
+	falseClaims        int64
+	shardsModified     int64
+}
+
+// column base type for nodeReputationRecord struct
+type column string
+
+// coproduct/sum type for the column type
+const (
+	sourceColumn             column = "source"
+	nodeNameColumn           column = "nodeName"
+	timestampColumn          column = "timestamp"
+	uptimeColumn             column = "uptime"
+	auditSuccessColumn       column = "auditSuccess"
+	auditFailColumn          column = "auditFail"
+	latencyColumn            column = "latency"
+	amountOfDataStoredColumn column = "amountOfDataStored"
+	falseClaimsColumn        column = "falseClaims"
+	shardsModifiedColumn     column = "shardsModified"
+)
+
+// startDB starts a sqlite3 database from the file path parameter
+func startDB(filePath string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", filePath)
+	if err != nil {
+		return nil, StartDBError.Wrap(err)
+	}
+
+	return db, nil
+}
+
+// createTable creates a table in sqlite3 based on the create table string parameter
+func createTable(createStmt string, db *sql.DB) error {
+	_, err := db.Exec(createStmt)
+	if err != nil {
+		return CreateTableError.Wrap(err)
+	}
+	return nil
+}
+
+// SetServerDB public function for a server
+func SetServerDB(filepath string) (*sql.DB, error) {
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		db, err := startDB(filepath)
+		if err != nil {
+			return nil, err
+		}
+		return nil, createTable(createStmt, db)
+	}
+	db, err := startDB(filepath)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// insertRows inserts the slice of reputation row structs based on the insert string
+func insertRows(db *sql.DB, rows []nodeReputationRecord, insertString string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return InsertError.Wrap(err)
+	}
+	defer tx.Rollback()
+
+	insertStmt, err := tx.Prepare(insertString)
+	if err != nil {
+		return InsertError.Wrap(err)
+	}
+	defer insertStmt.Close()
+
+	for _, row := range rows {
+		_, err = insertStmt.Exec(
+			row.source,
+			row.nodeName,
+			row.uptime,
+			row.auditSuccess,
+			row.auditFail,
+			row.latency,
+			row.amountOfDataStored,
+			row.falseClaims,
+			row.shardsModified,
+		)
+		if err != nil {
+			return InsertError.Wrap(err)
+		}
+	}
+	return tx.Commit()
+}
+
+// selectFromDB side effect function that prints the rows from the query string
+func selectFromDB(db *sql.DB, selectString string) error {
+	rows, err := db.Query(selectString)
+	if err != nil {
+		return SelectError.Wrap(err)
+	}
+	defer rows.Close()
+
+	transformedRows, err := iterOnDBRows(rows)
+	if err != nil {
+		return SelectError.Wrap(err)
+	}
+
+	for _, row := range transformedRows {
+		// side effect
+		fmt.Println(row)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return SelectError.Wrap(err)
+	}
+
+	return nil
+}
+
+// base type for the filter operation for a sql where clause
+type whereOpt string
+
+// coproduct/sum type for the generation of the sql string statement
+const (
+	equal        whereOpt = "="
+	greater      whereOpt = ">"
+	greaterEqual whereOpt = ">="
+	less         whereOpt = "<"
+	lessEqual    whereOpt = "<="
+	notEqual     whereOpt = "!="
+)
+
+// toString is a method to convert the sum type to a string for the sql string
+func (opt whereOpt) toString() string {
+	res := ""
+	switch opt {
+	case equal:
+		res = "="
+	case greater:
+		res = ">"
+	case greaterEqual:
+		res = ">="
+	case less:
+		res = "<"
+	case lessEqual:
+		res = "<="
+	case notEqual:
+		res = "!="
+	}
+
+	return res
+}
+
+// genWhereStatement is a function that makes a sql string with a single where clause
+func genWhereStatement(selectAll string, col column, opt whereOpt, value string) string {
+	where := " WHERE"
+	operand := opt.toString()
+
+	switch col {
+	case sourceColumn:
+		where = where + " source" + operand + " '" + value + "'"
+	case nodeNameColumn:
+		where = where + " node_name" + operand + " '" + value + "'"
+	case timestampColumn:
+		where = where + " timestamp" + operand + " STRFTIME('%Y-%m-%d %H:%M:%f'," + value + ")"
+	case uptimeColumn:
+		where = where + " uptime" + operand + " " + value
+	case auditSuccessColumn:
+		where = where + " audit_succes" + operand + " " + value
+	case auditFailColumn:
+		where = where + " audit_fail" + operand + " " + value
+	case latencyColumn:
+		where = where + " latency" + operand + " " + value
+	case amountOfDataStoredColumn:
+		where = where + " amount_of_data_stored" + operand + " " + value
+	case falseClaimsColumn:
+		where = where + " false_claims" + operand + " " + value
+	case shardsModifiedColumn:
+		where = where + " shards_modified" + operand + " " + value
+
+	default:
+		where = ""
+	}
+
+	return selectAll + where
+}
+
+// iterOnDBRows iterate on rows in the database to transform into slice of nodeReputationRecord
+func iterOnDBRows(rows *sql.Rows) ([]nodeReputationRecord, error) {
+	var res []nodeReputationRecord
+
+	for rows.Next() {
+		var row nodeReputationRecord
+
+		err := rows.Scan(
+			&row.source,
+			&row.nodeName,
+			&row.timestamp,
+			&row.uptime,
+			&row.auditSuccess,
+			&row.auditFail,
+			&row.latency,
+			&row.amountOfDataStored,
+			&row.falseClaims,
+			&row.shardsModified,
+		)
+		if err != nil {
+			return nil, IterError.Wrap(err)
+		}
+
+		res = append(res, row)
+	}
+
+	return res, nil
+}
+
+// getNodeReputationRecords function that returns a slice of reputation rows based on the query string
+func getNodeReputationRecords(db *sql.DB, selectString string) ([]nodeReputationRecord, error) {
+	rows, err := db.Query(selectString)
+	if err != nil {
+		return nil, SelectError.Wrap(err)
+	}
+	defer rows.Close()
+
+	res, err := iterOnDBRows(rows)
+	if err != nil {
+		return nil, SelectError.Wrap(err)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, SelectError.Wrap(err)
+	}
+
+	return res, nil
+}
+
+/*
+  pruneNodeReputationRecords is very destructive!
+  this function is used to make a snapshot of the current node
+  it removes the data that is older than the node passed in
+*/
+func pruneNodeReputationRecords(db *sql.DB, recordToKeep nodeReputationRecord, deleteString string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return DeleteError.Wrap(err)
+	}
+	defer tx.Rollback()
+
+	deleteStmt, err := tx.Prepare(deleteString)
+	if err != nil {
+		return DeleteError.Wrap(err)
+	}
+	defer deleteStmt.Close()
+
+	_, err = deleteStmt.Exec(
+		recordToKeep.nodeName,
+		recordToKeep.source,
+		recordToKeep.nodeName,
+		recordToKeep.timestamp,
+		recordToKeep.uptime,
+		recordToKeep.auditSuccess,
+		recordToKeep.auditFail,
+		recordToKeep.latency,
+		recordToKeep.amountOfDataStored,
+		recordToKeep.falseClaims,
+		recordToKeep.shardsModified,
+	)
+	if err != nil {
+		return DeleteError.Wrap(err)
+	}
+	return tx.Commit()
+}
+
+// cleanUpDB close sqlite3
+func cleanUpDB(db *sql.DB) error {
+	return db.Close()
+}
