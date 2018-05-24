6ac69c77ea94eabe03d27712d2aef9789e2cbf08
kishore <kishore@storj.io>
2018-05-24 13:46:37 -0400

    integrated eestream's serve-pieces functionality

--- pkg/miniogw/gateway-storj.go
+++ pkg/miniogw/gateway-storj.go
@@ -5,18 +5,33 @@ package storj
 
 import (
 	"context"
+	"crypto/sha256"
+	"flag"
+	"fmt"
 	"io"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/minio/cli"
+	"github.com/vivint/infectious"
+	"storj.io/storj/pkg/eestream"
 
-	"github.com/minio/minio/cmd"
+	minio "github.com/minio/minio/cmd"
 	"github.com/minio/minio/pkg/auth"
 	"github.com/minio/minio/pkg/hash"
 )
 
+var (
+	pieceBlockSize = flag.Int("piece_block_size", 4*1024, "block size of pieces")
+	key            = flag.String("key", "a key", "the secret key")
+	rsk            = flag.Int("required", 20, "rs required")
+	rsn            = flag.Int("total", 40, "rs total")
+)
+
 func init() {
-	cmd.RegisterGatewayCommand(cli.Command{
+	minio.RegisterGatewayCommand(cli.Command{
 		Name:            "storj",
 		Usage:           "Storj",
 		Action:          storjGatewayMain,
@@ -25,7 +40,7 @@ func init() {
 }
 
 func storjGatewayMain(ctx *cli.Context) {
-	cmd.StartGateway(ctx, &Storj{})
+	minio.StartGateway(ctx, &Storj{})
 }
 
 // Storj is the implementation of a minio cmd.Gateway
@@ -38,7 +53,7 @@ func (s *Storj) Name() string {
 
 // NewGatewayLayer implements cmd.Gateway
 func (s *Storj) NewGatewayLayer(creds auth.Credentials) (
-	cmd.ObjectLayer, error) {
+	minio.ObjectLayer, error) {
 	return &storjObjects{}, nil
 }
 
@@ -48,7 +63,8 @@ func (s *Storj) Production() bool {
 }
 
 type storjObjects struct {
-	cmd.GatewayUnsupported
+	minio.GatewayUnsupported
+	TempDir string // Temporary storage location for file transfers.
 }
 
 func (s *storjObjects) DeleteBucket(ctx context.Context, bucket string) error {
@@ -61,7 +77,7 @@ func (s *storjObjects) DeleteObject(ctx context.Context, bucket,
 }
 
 func (s *storjObjects) GetBucketInfo(ctx context.Context, bucket string) (
-	bucketInfo cmd.BucketInfo, err error) {
+	bucketInfo minio.BucketInfo, err error) {
 	panic("TODO")
 }
 
@@ -72,23 +88,23 @@ func (s *storjObjects) GetObject(ctx context.Context, bucket, object string,
 }
 
 func (s *storjObjects) GetObjectInfo(ctx context.Context, bucket,
-	object string) (objInfo cmd.ObjectInfo, err error) {
+	object string) (objInfo minio.ObjectInfo, err error) {
 	panic("TODO")
 }
 
 func (s *storjObjects) ListBuckets(ctx context.Context) (
-	buckets []cmd.BucketInfo, err error) {
-	return []cmd.BucketInfo{{
+	buckets []minio.BucketInfo, err error) {
+	return []minio.BucketInfo{{
 		Name:    "test-bucket",
 		Created: time.Now(),
 	}}, nil
 }
 
 func (s *storjObjects) ListObjects(ctx context.Context, bucket, prefix, marker,
-	delimiter string, maxKeys int) (result cmd.ListObjectsInfo, err error) {
-	return cmd.ListObjectsInfo{
+	delimiter string, maxKeys int) (result minio.ListObjectsInfo, err error) {
+	return minio.ListObjectsInfo{
 		IsTruncated: false,
-		Objects: []cmd.ObjectInfo{{
+		Objects: []minio.ObjectInfo{{
 			Bucket:      "test-bucket",
 			Name:        "test-file",
 			ModTime:     time.Now(),
@@ -104,16 +120,84 @@ func (s *storjObjects) MakeBucketWithLocation(ctx context.Context,
 	panic("TODO")
 }
 
+// Main is the exported CLI executable function
+func Main(data io.ReadCloser, blockSize uint) error {
+	dir := "gateway"
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+	fc, err := infectious.NewFEC(*rsk, *rsn)
+	if err != nil {
+		return err
+	}
+	es := eestream.NewRSScheme(fc, *pieceBlockSize)
+	encKey := sha256.Sum256([]byte(*key))
+	var firstNonce [12]byte
+	encrypter, err := eestream.NewAESGCMEncrypter(
+		&encKey, &firstNonce, es.DecodedBlockSize())
+	if err != nil {
+		return err
+	}
+	readers := eestream.EncodeReader(eestream.TransformReader(
+		eestream.PadReader(data, encrypter.InBlockSize()), encrypter, 0), es)
+	errs := make(chan error, len(readers))
+	for i := range readers {
+		go func(i int) {
+			fh, err := os.Create(
+				filepath.Join(dir, fmt.Sprintf("%d.piece", i)))
+			if err != nil {
+				errs <- err
+				return
+			}
+			defer fh.Close()
+			_, err = io.Copy(fh, readers[i])
+			errs <- err
+		}(i)
+	}
+	for range readers {
+		err := <-errs
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (s *storjObjects) PutObject(ctx context.Context, bucket, object string,
-	data *hash.Reader, metadata map[string]string) (objInfo cmd.ObjectInfo,
+	data *hash.Reader, metadata map[string]string) (objInfo minio.ObjectInfo,
 	err error) {
-	panic("TODO")
+	srcFile := path.Join(s.TempDir, minio.MustGetUUID())
+	writer, err := os.Create(srcFile)
+	if err != nil {
+		return objInfo, err
+	}
+
+	wsize, err := io.CopyN(writer, data, data.Size())
+	if err != nil {
+		os.Remove(srcFile)
+		return objInfo, err
+	}
+
+	fmt.Printf("hello hello hello bucket = %s; object = %s wsize = %d\n ", bucket, object, wsize)
+	fmt.Println(" data =", data)
+	/* @TODO Attention: Need to handle the file size limit */
+	Main(writer, uint(wsize))
+	fmt.Println("Bucket = ", bucket)
+
+	return minio.ObjectInfo{
+		Name:    object,
+		Bucket:  bucket,
+		ModTime: time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC),
+		Size:    wsize,
+		ETag:    minio.GenETag(),
+	}, nil
 }
 
 func (s *storjObjects) Shutdown(context.Context) error {
 	panic("TODO")
 }
 
-func (s *storjObjects) StorageInfo(context.Context) cmd.StorageInfo {
-	panic("TODO")
+func (s *storjObjects) StorageInfo(context.Context) minio.StorageInfo {
+	return minio.StorageInfo{}
 }
