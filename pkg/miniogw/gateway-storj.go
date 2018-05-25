be19cb7a5e4aa6241ab8920f01bb2b9b5cb552d7
kishore <kishore@storj.io>
2018-05-25 11:31:48 -0400

    added ref to http request function

--- pkg/miniogw/gateway-storj.go
+++ pkg/miniogw/gateway-storj.go
@@ -4,11 +4,15 @@
 package storj
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -162,6 +166,55 @@ func Main(data io.ReadCloser, blockSize uint) error {
 		}
 	}
 	return nil
+}
+
+func postFile(filename string, targetURL string) error {
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	// this step is very important
+	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return err
+	}
+
+	// open file handle
+	fh, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file")
+		return err
+	}
+	defer fh.Close()
+
+	//iocopy
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		return err
+	}
+
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	resp, err := http.Post(targetURL, contentType, bodyBuf)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Status)
+	fmt.Println(string(respBody))
+	return nil
+}
+
+// sample usage
+func postMain() {
+	targetURL := "http://localhost:9090/upload"
+	filename := "./astaxie.pdf"
+	postFile(filename, targetURL)
 }
 
 func (s *storjObjects) PutObject(ctx context.Context, bucket, object string,
