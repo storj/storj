d2465f5476db78a1350b3efa633b10993dc943d9
JT Olio <hello@jtolio.com>
2018-06-08 16:49:57 +0000

    treesame commit of 10cc294dd4968ca22582ca96aadf01cc8e0c454e
    Treesame-Commit-Id: 10cc294dd4968ca22582ca96aadf01cc8e0c454e

---
+++ examples/udp/server/server2.go
@@ -0,0 +1,36 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:7777")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+	}
+}
--- pkg/miniogw/gateway-storj.go
+++ pkg/miniogw/gateway-storj.go
@@ -6,32 +6,86 @@ package storj
 import (
 	"context"
 	"crypto/sha256"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
-	"os"
+	"io/ioutil"
 	"path"
-	"path/filepath"
 	"strconv"
 	"time"
 
+	"storj.io/storj/pkg/piecestore"
+
+	"github.com/golang/protobuf/proto"
 	"github.com/minio/cli"
 	minio "github.com/minio/minio/cmd"
 	"github.com/minio/minio/pkg/auth"
 	"github.com/minio/minio/pkg/hash"
 	"github.com/vivint/infectious"
+	"github.com/zeebo/errs"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
 
 	"storj.io/storj/pkg/eestream"
+	"storj.io/storj/pkg/overlay"
+	"storj.io/storj/pkg/paths"
+	"storj.io/storj/pkg/piecestore/rpc/client"
+	"storj.io/storj/pkg/ranger"
+	pb "storj.io/storj/protos/netstate"
+	opb "storj.io/storj/protos/overlay"
 )
 
 var (
 	pieceBlockSize = flag.Int("piece_block_size", 4*1024, "block size of pieces")
 	key            = flag.String("key", "a key", "the secret key")
-	rsk            = flag.Int("required", 20, "rs required")
-	rsn            = flag.Int("total", 40, "rs total")
+	rsk            = flag.Int("required", 5, "rs required")
+	rsn            = flag.Int("total", 20, "rs total")
+	rsm            = flag.Int("minimum", 0, "rs minimum safe")
+	rso            = flag.Int("optimal", 0, "rs optimal safe")
+
+	mon   = monkit.Package()
+	Error = errs.Class("error")
 )
 
+var nodes = []string{
+	"35.232.47.152:4242",
+	"35.226.188.0:4242",
+	"35.202.182.77:4242",
+	"35.232.88.171:4242",
+	"35.225.50.137:4242",
+	"130.211.168.182:4242",
+	"104.197.5.134:4242",
+	"35.192.126.41:4242",
+	"35.193.196.52:4242",
+	"130.211.203.52:4242",
+	"35.224.122.76:4242",
+	"104.198.233.25:4242",
+	"35.226.21.152:4242",
+	"130.211.221.239:4242",
+	"35.202.144.175:4242",
+	"130.211.190.250:4242",
+	"35.194.56.141:4242",
+	"35.192.108.107:4242",
+	"35.202.91.191:4242",
+	"35.192.7.33:4242",
+}
+
+// nodeIds maps address to piece id (shards v2 name)
+var nodeIds = map[string]string{}
+
+// nodeAddrs maps piece id(shards) to address
+var nodeAddrs = map[string]string{}
+
 func init() {
+	for i, address := range nodes {
+		nodeAddrs[fmt.Sprint(i)] = address
+		nodeIds[address] = fmt.Sprint(i)
+	}
+
 	minio.RegisterGatewayCommand(cli.Command{
 		Name:            "storj",
 		Usage:           "Storj",
@@ -67,7 +121,7 @@ func (s *storjObjects) uploadFile(bucket, object string, filesize int64, metadat
 					ModTime:     time.Now(),
 					Size:        filesize,
 					IsDir:       false,
-					ContentType: "application/octet-stream",
+					ContentType: "video/mp4",
 				},
 			)
 			/* populate the filelist */
@@ -119,6 +173,56 @@ func (s *storjObjects) getFiles(bucket string) (result minio.ListObjectsInfo, er
 		Objects:     fl,
 	}
 	return result, nil
+}
+
+// deleteFile returns the files list for a bucket
+func (s *storjObjects) deleteFile(bucket, object string) (err error) {
+	for i, v := range s.storj.bucketlist {
+		k := 0
+		if v.bucket.Name == bucket {
+			for j := 0; j < len(s.storj.bucketlist[i].filelist.file.Objects); j++ {
+				fi := s.storj.bucketlist[i].filelist.file.Objects[j].Name
+				fmt.Println("fi=", fi)
+				if fi != object {
+					s.storj.bucketlist[i].filelist.file.Objects[k].Name = fi
+					k++
+				}
+			}
+			s.storj.bucketlist[i].filelist.file.Objects = s.storj.bucketlist[i].filelist.file.Objects[:k]
+		}
+	}
+	return nil
+}
+
+// removeBucket returns the files list for a bucket
+func (s *storjObjects) removeBucket(bucket string) (err error) {
+	k := 0
+	for i, v := range s.storj.bucketlist {
+		bi := s.storj.bucketlist[i].bucket.Name
+		fmt.Println("bi=", bi)
+		if v.bucket.Name != bucket {
+			s.storj.bucketlist[k] = v
+			k++
+		}
+	}
+	s.storj.bucketlist = s.storj.bucketlist[:k]
+	return nil
+}
+
+// addBucket returns the files list for a bucket
+func (s *storjObjects) addBucket(bucket string) (err error) {
+	s.storj.bucketlist = append(s.storj.bucketlist,
+		S3Bucket{
+			minio.BucketInfo{
+				Name:    bucket,
+				Created: time.Now(),
+			},
+			S3FileList{
+				minio.ListObjectsInfo{},
+			},
+		},
+	)
+	return nil
 }
 
 func storjGatewayMain(ctx *cli.Context) {
@@ -184,12 +288,12 @@ type storjObjects struct {
 }
 
 func (s *storjObjects) DeleteBucket(ctx context.Context, bucket string) error {
-	panic("TODO")
+	return s.removeBucket(bucket)
 }
 
 func (s *storjObjects) DeleteObject(ctx context.Context, bucket,
 	object string) error {
-	panic("TODO")
+	return s.deleteFile(bucket, object)
 }
 
 func (s *storjObjects) GetBucketInfo(ctx context.Context, bucket string) (
@@ -197,15 +301,189 @@ func (s *storjObjects) GetBucketInfo(ctx context.Context, bucket string) (
 	panic("TODO")
 }
 
-func (s *storjObjects) GetObject(ctx context.Context, bucket, object string,
-	startOffset int64, length int64, writer io.Writer, etag string) (err error) {
+//nodes []*proto.RemotePiece,
+func netstateGet(ctx context.Context, path string) (rv *pb.Pointer, err error) {
+	defer mon.Task()(&ctx)(&err)
 
-	panic("TODO")
+	conn, err := grpc.Dial(*netstateAddress, grpc.WithInsecure())
+	if err != nil {
+		zap.L().Error("Failed to dial: ", zap.Error(err))
+		return nil, err
+	}
+
+	client := pb.NewNetStateClient(conn)
+
+	//zap.L().Debug(fmt.Sprintf("client dialed port %s", netstateAddress))
+
+	// Example pointer paths to put
+	//pr1 passes with api creds
+	gr := pb.GetRequest{
+		Path:   []byte(path), //key
+		APIKey: []byte("abc123"),
+	}
+
+	// Example Puts
+	// puts passes api creds
+	getRes, err := client.Get(ctx, &gr)
+	if err != nil || status.Code(err) == codes.Internal {
+		zap.L().Error("failed to get", zap.Error(err))
+		return nil, err
+	}
+	var p pb.Pointer
+	err = proto.Unmarshal(getRes.Pointer, &p)
+	if err != nil {
+		zap.L().Error("failed to marshal", zap.Error(err))
+		return nil, err
+	}
+	prettyprint(&p)
+	return &p, nil
 }
 
-func (s *storjObjects) GetObjectInfo(ctx context.Context, bucket,
-	object string) (objInfo minio.ObjectInfo, err error) {
-	panic("TODO")
+func (s *storjObjects) GetObject(ctx context.Context, bucket, object string,
+	startOffset int64, length int64, writer io.Writer, etag string) (err error) {
+	defer func() {
+		if err != nil {
+			fmt.Printf("%+v", Error.Wrap(err))
+		}
+	}()
+
+	encKey := sha256.Sum256([]byte(*key))
+
+	/* making request to network state to get the piece IDs */
+	/* create a unique fileID */
+	netStateKey := []byte(*key)
+	encryptedPath, err := paths.Encrypt([]string{bucket, object}, netStateKey)
+
+	pointer, err := netstateGet(ctx, path.Join(encryptedPath...))
+	if err != nil {
+		return err
+	}
+
+	if pointer.GetType() != pb.Pointer_REMOTE {
+		return fmt.Errorf("TODO: only remote supported!")
+	}
+	remote := pointer.GetRemote()
+	redundancy := remote.GetRedundancy()
+	if redundancy.GetType() != pb.RedundancyScheme_RS {
+		return fmt.Errorf("TODO: only rs supported!")
+	}
+
+	fc, err := infectious.NewFEC(int(redundancy.GetMinReq()), int(redundancy.GetTotal()))
+	if err != nil {
+		return err
+	}
+	es := eestream.NewRSScheme(fc, *pieceBlockSize)
+	var firstNonce [12]byte
+	decrypter, err := eestream.NewAESGCMDecrypter(
+		&encKey, &firstNonce, es.DecodedBlockSize())
+	if err != nil {
+		return err
+	}
+
+	addr := "bootstrap.storj.io:7070"
+	c, err := overlay.NewClient(&addr, grpc.WithInsecure())
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+	errs := make(chan error, redundancy.GetTotal())
+	conns := make([]*grpc.ClientConn, redundancy.GetTotal())
+	for i := 0; i < len(remote.GetRemotePieces()); i++ {
+		go func(i int) {
+			resp, err := c.Lookup(ctx, &opb.LookupRequest{NodeID: remote.RemotePieces[i].NodeId})
+			if err != nil {
+				errs <- err
+				return
+			}
+
+			conns[i], err = grpc.Dial(resp.NodeAddress.Address, grpc.WithInsecure())
+			errs <- err
+		}(i)
+	}
+	for range conns {
+		err := <-errs
+		if err != nil {
+			// TODO don't fail just because of failure with only one node
+			return err
+		}
+	}
+	defer func() {
+		for i := 0; i < len(remote.GetRemotePieces()); i++ {
+			defer conns[i].Close()
+		}
+	}()
+	rrs := map[int]ranger.Ranger{}
+	type rangerInfo struct {
+		i   int64
+		rr  ranger.Ranger
+		err error
+	}
+	rrch := make(chan rangerInfo, redundancy.GetTotal())
+	for i := 0; i < len(remote.GetRemotePieces()); i++ {
+		go func(i int) {
+			cl := client.New(ctx, conns[i])
+			rr, err := ranger.GRPCRanger(cl, remote.GetPieceId())
+			rrch <- rangerInfo{remote.GetRemotePieces()[i].GetPieceNum(), rr, err}
+		}(i)
+	}
+	for range conns {
+		info := <-rrch
+		if info.err != nil {
+			// TODO don't fail just because of failure with only one node
+			return info.err
+		}
+		rrs[int(info.i)] = info.rr
+	}
+	rr, err := eestream.Decode(context.Background(), rrs, es, 4*1024*1024)
+	if err != nil {
+		return err
+	}
+	rr, err = eestream.Transform(rr, decrypter)
+	if err != nil {
+		return err
+	}
+	rr, err = eestream.UnpadSlow(rr)
+	if err != nil {
+		return err
+	}
+	if length == -1 {
+		length = rr.Size()
+	}
+	r := rr.Range(startOffset, length)
+	_, err = io.Copy(writer, r)
+	return err
+}
+
+func (s *storjObjects) GetObjectInfo(ctx context.Context, bucket, object string) (objInfo minio.ObjectInfo, err error) {
+	defer func() {
+		if err != nil {
+			fmt.Printf("%+v", Error.Wrap(err))
+		}
+	}()
+
+	/* making request to network state to get the piece IDs */
+	/* create a unique fileID */
+	netStateKey := []byte(*key)
+	encryptedPath, err := paths.Encrypt([]string{bucket, object}, netStateKey)
+
+	pointer, err := netstateGet(ctx, path.Join(encryptedPath...))
+	if err != nil {
+		return minio.ObjectInfo{}, err
+	}
+
+	size, err := strconv.ParseInt(string(pointer.EncryptedUnencryptedSize), 10, 64)
+	if err != nil {
+		return minio.ObjectInfo{}, err
+	}
+
+	return minio.ObjectInfo{
+		Bucket:      bucket,
+		Name:        object,
+		ModTime:     time.Date(2018, 6, 5, 17, 20, 32, 0, time.Local),
+		Size:        size,
+		IsDir:       false,
+		ContentType: "video/mp4",
+	}, nil
 }
 
 func (s *storjObjects) ListBuckets(ctx context.Context) (
@@ -220,21 +498,83 @@ func (s *storjObjects) ListObjects(ctx context.Context, bucket, prefix, marker,
 
 func (s *storjObjects) MakeBucketWithLocation(ctx context.Context,
 	bucket string, location string) error {
-	panic("TODO")
+	return s.addBucket(bucket)
+}
+
+/* Integration stuff with Network state */
+var (
+	netstateAddress = flag.String("netstate_addr", "35.202.58.176:4242", "address")
+)
+
+func netstatePut(ctx context.Context, path string, k, m, o, n int, size int64, pieceID string, nodes []*pb.RemotePiece) (err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	conn, err := grpc.Dial(*netstateAddress, grpc.WithInsecure())
+	if err != nil {
+		zap.L().Error("Failed to dial: ", zap.Error(err))
+		return Error.Wrap(err)
+	}
+
+	client := pb.NewNetStateClient(conn)
+
+	//zap.L().Debug(fmt.Sprintf("client dialed port %s", netstateAddress))
+
+	// Example pointer paths to put
+	//pr1 passes with api creds
+	pr1 := pb.PutRequest{
+		Path: []byte(path), //key
+		Pointer: &pb.Pointer{
+			Type: pb.Pointer_REMOTE,
+			Remote: &pb.RemoteSegment{
+				Redundancy: &pb.RedundancyScheme{
+					Type:             pb.RedundancyScheme_RS,
+					MinReq:           int64(k),
+					Total:            int64(n),
+					RepairThreshold:  int64(m),
+					SuccessThreshold: int64(o),
+				},
+				PieceId:      pieceID, //piece id (with frame #)
+				RemotePieces: nodes,   //list of node ids
+			},
+			EncryptedUnencryptedSize: []byte(fmt.Sprint(size)),
+		},
+		APIKey: []byte("abc123"),
+	}
+
+	// Example Puts
+	// puts passes api creds
+	_, err = client.Put(ctx, &pr1)
+	if err != nil || status.Code(err) == codes.Internal {
+		zap.L().Error("failed to put", zap.Error(err))
+		return Error.Wrap(err)
+	}
+	return nil
+}
+
+func prettyprint(val interface{}) {
+	bytes, err := json.MarshalIndent(val, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
 }
 
 //encryptFile encrypts the uploaded files
-func encryptFile(data io.ReadCloser, blockSize uint, bucket, object string) error {
-	dir := os.TempDir()
-	dir = filepath.Join(dir, "gateway", bucket, object)
-	err := os.MkdirAll(dir, 0755)
-	if err != nil {
-		return err
-	}
+func encryptFile(ctx context.Context, data *hash.Reader, bucket, object string) (err error) {
+	defer mon.Task()(&ctx)(&err)
+
 	fc, err := infectious.NewFEC(*rsk, *rsn)
 	if err != nil {
 		return err
 	}
+
+	/* create a unique fileID */
+	netStateKey := []byte(*key)
+	encryptedPath, err := paths.Encrypt([]string{bucket, object}, netStateKey)
+	fmt.Println("encrypted path ", encryptedPath)
+	decryptedPath, err := paths.Decrypt(encryptedPath, netStateKey)
+	fmt.Println("decrypted path ", decryptedPath)
+
 	es := eestream.NewRSScheme(fc, *pieceBlockSize)
 	encKey := sha256.Sum256([]byte(*key))
 	var firstNonce [12]byte
@@ -244,58 +584,97 @@ func encryptFile(data io.ReadCloser, blockSize uint, bucket, object string) erro
 		return err
 	}
 	readers, err := eestream.EncodeReader(context.Background(), eestream.TransformReader(
-		eestream.PadReader(data, encrypter.InBlockSize()), encrypter, 0),
-		es, 0, 0, 4*1024*1024)
+		eestream.PadReader(ioutil.NopCloser(data), encrypter.InBlockSize()), encrypter, 0),
+		es, *rsm, *rso, 4*1024*1024)
 	if err != nil {
-		return err
+		return Error.Wrap(err)
 	}
+
+	/* integrating with DHT for uploading to get the farmer's IP address */
+	addr := "bootstrap.storj.io:7070"
+	c, err := overlay.NewClient(&addr, grpc.WithInsecure())
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+	r, err := c.FindStorageNodes(context.Background(), &opb.FindStorageNodesRequest{})
+	if err != nil {
+		return Error.Wrap(err)
+	}
+	fmt.Printf("r %#v\n", r)
+	pieceId := pstore.DetermineID()
+	// r is your nodes
+	var remotePieces []*pb.RemotePiece
 	errs := make(chan error, len(readers))
 	for i := range readers {
+		remotePieces = append(remotePieces, &pb.RemotePiece{
+			PieceNum: int64(i),
+			NodeId:   r.Node[i].Id,
+		})
 		go func(i int) {
-			fh, err := os.Create(
-				filepath.Join(dir, fmt.Sprintf("%d.piece", i)))
+			conn, err := grpc.Dial(r.Node[i].Address.Address, grpc.WithInsecure())
 			if err != nil {
-				errs <- err
+				zap.S().Errorf("error dialing: %v\n", err)
+				errs <- Error.Wrap(err)
 				return
 			}
-			defer fh.Close()
-			_, err = io.Copy(fh, readers[i])
-			errs <- err
+			defer conn.Close()
+			cl := client.New(ctx, conn)
+
+			w, err := cl.StorePieceRequest(pieceId, 0)
+			if err != nil {
+				zap.S().Errorf("error req: %v\n", err)
+				errs <- Error.Wrap(err)
+				return
+			}
+			_, err = io.Copy(w, readers[i])
+			// Make sure writer is closed to avoid losing last bytes
+			if err != nil {
+				w.Close()
+				zap.S().Errorf("error copy: %v\n", err)
+				errs <- Error.Wrap(err)
+				return
+			}
+			err = w.Close()
+			if err != io.EOF {
+				zap.S().Errorf("error close: %v\n", err)
+				errs <- Error.Wrap(err)
+				return
+			}
+			errs <- nil
 		}(i)
 	}
+	var errbucket []error
 	for range readers {
 		err := <-errs
 		if err != nil {
-			return err
+			errbucket = append(errbucket, err)
 		}
 	}
-	return nil
+	if len(readers)-len(errbucket) < *rsm {
+		for _, err := range errbucket {
+			fmt.Printf("%+v\n", err)
+		}
+		return errbucket[0]
+	}
+	/* integrate with Network State */
+	// initialize the client
+	return Error.Wrap(netstatePut(ctx, path.Join(encryptedPath...), es.RequiredCount(), *rsm, *rso, es.TotalCount(), data.Size(), pieceId, remotePieces))
 }
 
 func (s *storjObjects) PutObject(ctx context.Context, bucket, object string,
 	data *hash.Reader, metadata map[string]string) (objInfo minio.ObjectInfo,
 	err error) {
-	srcFile := path.Join(s.TempDir, minio.MustGetUUID())
-	writer, err := os.Create(srcFile)
-	if err != nil {
-		return objInfo, err
-	}
-
-	wsize, err := io.CopyN(writer, data, data.Size())
-	if err != nil {
-		os.Remove(srcFile)
-		return objInfo, err
-	}
-
-	err = encryptFile(writer, uint(wsize), bucket, object)
+	defer mon.Task()(&ctx)(&err)
+	err = encryptFile(ctx, data, bucket, object)
 	if err == nil {
-		s.uploadFile(bucket, object, wsize, metadata)
+		s.uploadFile(bucket, object, data.Size(), metadata)
 	}
 	return minio.ObjectInfo{
 		Name:    object,
 		Bucket:  bucket,
 		ModTime: time.Now(),
-		Size:    wsize,
+		Size:    data.Size(),
 		ETag:    minio.GenETag(),
 	}, err
 }
