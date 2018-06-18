aeff7d29b6206497fd32d2d08bed7384dce060d0
Bryan White <bryanchriswhite@gmail.com>
2018-06-18 13:29:16 +0200

    forgot to add

---
+++ internal/test/port.go
@@ -0,0 +1,59 @@
+package test
+
+import (
+	"net"
+	"strconv"
+)
+
+// New gets an available port
+func NewPort() (port int, err error) {
+
+	// Create a new server without specifying a port
+	// which will result in an open port being chosen
+	server, err := net.Listen("tcp", ":0")
+
+	// If there's an error it likely means no ports
+	// are available or something else prevented finding
+	// an open port
+	if err != nil {
+		return 0, err
+	}
+
+	// Defer the closing of the server so it closes
+	defer server.Close()
+
+	// Get the host string in the format "127.0.0.1:4444"
+	hostString := server.Addr().String()
+
+	// Split the host from the port
+	_, portString, err := net.SplitHostPort(hostString)
+	if err != nil {
+		return 0, err
+	}
+
+	// Return the port as an int
+	return strconv.Atoi(portString)
+}
+
+// Check if a port is available
+func CheckPort(port int) (status bool, err error) {
+
+	// Concatenate a colon and the port
+	host := ":" + strconv.Itoa(port)
+
+	// Try to create a server with the port
+	server, err := net.Listen("tcp", host)
+
+	// if it fails then the port is likely taken
+	if err != nil {
+		return false, err
+	}
+
+	// close the server
+	server.Close()
+
+	// we successfully used and closed the port
+	// so it's now available to be used again
+	return true, nil
+
+}
