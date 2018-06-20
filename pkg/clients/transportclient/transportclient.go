850f0e24fe09f0efccb5979d0ebaf1554f585301
kishore <kishore@storj.io>
2018-06-20 12:52:53 -0400

    added the review changes

--- pkg/clients/transportclient/transportclient.go
+++ pkg/clients/transportclient/transportclient.go
@@ -12,26 +12,35 @@ import (
 	proto "storj.io/storj/protos/overlay"
 )
 
-// NetworkClient defines the interface to an network client.
-type NetworkClient interface {
+// TransportClient defines the interface to an network client.
+type TransportClient interface {
 	DialUnauthenticated(ctx context.Context, node *proto.Node) (*grpc.ClientConn, error)
 	DialNode(ctx context.Context, node *proto.Node) (*grpc.ClientConn, error)
 }
 
-// TransportClient is the concrete implementation of the networkclient interface
-type TransportClient struct {
-	Conn *grpc.ClientConn
-	Cc   *overlay.Overlay
+// transportClient is the concrete implementation of the networkclient interface
+type transportClient struct {
+	overlayClient *overlay.Overlay
+}
+
+//NewTransportClient returns TransportClient interface
+func NewTransportClient(ctx context.Context, addr string) (TransportClient, error) {
+	c, err := overlay.NewOverlayClient(addr)
+	if err != nil {
+		return nil, err
+	}
+	return &transportClient{
+		overlayClient: c,
+	}, err
 }
 
 // Dial using the authenticated mode
-func (o *TransportClient) DialNode(ctx context.Context, node *proto.Node) (conn *grpc.ClientConn, err error) {
-	o.Conn = nil
+func (o *transportClient) DialNode(ctx context.Context, node *proto.Node) (conn *grpc.ClientConn, err error) {
 	/* check to see if address is empty? */
 	if node.Address.Address == "" {
 		/* check to see nodeID is present to look up for the corresponding address */
 		if node.Id != "" {
-			lookupNode, err := o.Cc.Lookup(ctx, overlay.NodeID(node.Id))
+			lookupNode, err := o.overlayClient.Lookup(ctx, overlay.NodeID(node.Id))
 			conn, err = grpc.Dial(lookupNode.Address.Address, grpc.WithInsecure())
 			if err != nil {
 				return nil, err
@@ -41,7 +50,6 @@ func (o *TransportClient) DialNode(ctx context.Context, node *proto.Node) (conn 
 		}
 	} else {
 		conn, err = grpc.Dial(node.Address.Address, grpc.WithInsecure())
-		o.Conn = conn
 		if err != nil {
 			return nil, err
 		}
@@ -51,13 +59,12 @@ func (o *TransportClient) DialNode(ctx context.Context, node *proto.Node) (conn 
 }
 
 // Dial using unauthenticated mode
-func (o *TransportClient) DialUnauthenticated(ctx context.Context, node *proto.Node) (conn *grpc.ClientConn, err error) {
-	o.Conn = nil
+func (o *transportClient) DialUnauthenticated(ctx context.Context, node *proto.Node) (conn *grpc.ClientConn, err error) {
 	/* check to see if address is empty? */
 	if node.Address.Address == "" {
 		/* check to see nodeID is present to look up for the corresponding address */
 		if node.Id != "" {
-			lookupNode, err := o.Cc.Lookup(ctx, overlay.NodeID(node.Id))
+			lookupNode, err := o.overlayClient.Lookup(ctx, overlay.NodeID(node.Id))
 			conn, err = grpc.Dial(lookupNode.Address.Address, grpc.WithInsecure())
 			if err != nil {
 				return nil, err
@@ -72,6 +79,5 @@ func (o *TransportClient) DialUnauthenticated(ctx context.Context, node *proto.N
 		}
 	}
 
-	o.Conn = conn
 	return conn, err
 }
