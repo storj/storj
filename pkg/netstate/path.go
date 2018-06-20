53c60197d41f3b330988133b13eb63176ef51c88
nfarah86 <nfarah86@gmail.com>
2018-06-20 13:09:37 -0700

    working on put request for nsclient

---
+++ pkg/netstate/path.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package netstate
+
+// Path is the unique identifer of file paths
+type Path []string
+
+// This function transforms the string path of type []string to a []byte type
+// given []string{"path/one/is/here"}
+func (path *Path) Bytes() []byte {
+	stringPath := strings.Join(*path, " ")
+	bytesPath := []byte(stringPath)
+	return &bytesPath
+}
+
+
