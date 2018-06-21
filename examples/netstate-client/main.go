6fda93e72dbeec1802a2eeaa10214f1f59e206fc
nfarah86 <nfarah86@gmail.com>
2018-06-20 23:35:55 -0700

    wip - testing client  and working through some errors

--- examples/netstate-client/main.go
+++ examples/netstate-client/main.go
@@ -35,6 +35,7 @@ func main() {
 
 	nsclient, err := client.NewNetStateClient(port)
 	if err != nil {
+		fmt.Println("error page !!!!!")
 		logger.Error("Failed to dial: ", zap.Error(err))
 	}
 
@@ -105,6 +106,11 @@ func main() {
 		logger.Error("failed to put", zap.Error(err))
 	}
 	
+
+
+
+
+
 	
 	// _, err = client.Put(ctx, &pr2)
 	// if err != nil || status.Code(err) == codes.Internal {
