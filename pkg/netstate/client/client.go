0a4f9983e3aabf130ce6f964c02f96e38c342ebd
nfarah86 <nfarah86@gmail.com>
2018-06-22 19:46:22 -0700

    working through delete error

--- pkg/netstate/client/client.go
+++ pkg/netstate/client/client.go
@@ -25,15 +25,16 @@ type NetState struct {
 type NSClient interface {
 	Put(ctx context.Context, path []byte, pointer *pb.Pointer, APIKey []byte) error
 	Get(ctx context.Context, path []byte, APIKey []byte) (*pb.Pointer, error)
-	Delete(ctx context.Context, path []byte, APIKey []byte) error
 	List(ctx context.Context, startingPathKey []byte, limit int64, APIKey []byte) (
 		paths []byte, truncated bool, err error)
+	Delete(ctx context.Context, path []byte, APIKey []byte) error
 }
 
 // NewNetstateClient initializes a new netstate client
 func NewNetstateClient(address string) (*NetState, error) {
     c, err := NewClient(&address, grpc.WithInsecure())
-    if err != nil {
+   
+	if err != nil {
         return nil, err
     }
     return &NetState{
@@ -43,12 +44,12 @@ func NewNetstateClient(address string) (*NetState, error) {
 
 // NewClient makes a server connection
 func NewClient(serverAddr *string, opts ...grpc.DialOption) (pb.NetStateClient, error) {
-	conn, err := grpc.Dial(*serverAddr, opts...)
-
+    conn, err := grpc.Dial(*serverAddr, opts...)
+   
 	if err != nil {
-		return nil, err
-	}
-	return pb.NewNetStateClient(conn), nil
+        return nil, err
+    }
+    return pb.NewNetStateClient(conn), nil
 }
 
 // Put is the interface to make a PUT request, needs Pointer and APIKey
@@ -69,7 +70,7 @@ func (ns *NetState) Get(ctx context.Context, path []byte, APIKey []byte) (*pb.Po
 	}
 
 	pointer := &pb.Pointer{}
-	err = proto.Unmarshal(res.GetPointer(), pointer)
+	err = proto.Unmarshal(res.GetPointer(),pointer)
 
 	if err != nil {
 		return nil, err
@@ -87,10 +88,9 @@ func (ns *NetState) List(ctx context.Context, startingPathKey []byte, limit int6
 	return res.Paths, true, nil
 }
 
-// Delete is the interface to make a Delete request, needs Path and APIKey
 func (ns *NetState) Delete(ctx context.Context, path []byte, APIKey []byte) error {
-	_, err := ns.grpcClient.Delete(ctx, &pb.DeleteRequest{Path: path, APIKey: APIKey})
-
+	err := ns.grpcClient.Delete(ctx, &pb.DeleteRequest{Path: path, APIKey: APIKey})
+	
 	if err != nil {
 		return err
 	}
