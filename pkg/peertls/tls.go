1fad29e5e681f27ac13e9e2815a10f0eae4a28df
Bryan White <bryanchriswhite@gmail.com>
2018-06-25 22:44:07 +0200

    cleanup

--- pkg/peertls/tls.go
+++ pkg/peertls/tls.go
@@ -199,3 +199,19 @@ func (t *TLSFileOptions) ServerOption() (_ grpc.ServerOption) {
 
 	return grpc.Creds(creds)
 }
+
+// * Copyright 2017 gRPC authors.
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * (see https://github.com/grpc/grpc-go/blob/v1.13.0/credentials/credentials_util_go18.go)
+// cloneTLSConfig returns a shallow clone of the exported
+// fields of cfg, ignoring the unexported sync.Once, which
+// contains a mutex and must not be copied.
+//
+// If cfg is nil, a new zero tls.Config is returned.
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {
+	if cfg == nil {
+		return &tls.Config{}
+	}
+
+	return cfg.Clone()
+}
