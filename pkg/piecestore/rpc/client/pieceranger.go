2989765ce739a2f4eea6b219f5cbf88f8e23d0c0
Alexander Leitner <leitnersalex@gmail.com>
2018-06-27 11:59:54 -0400

    Actually we don't want to use the interface in Piece Ranger

--- pkg/piecestore/rpc/client/pieceranger.go
+++ pkg/piecestore/rpc/client/pieceranger.go
@@ -18,13 +18,13 @@ import (
 var Error = errs.Class("pieceRanger error")
 
 type pieceRanger struct {
-	c    PSClient
+	c    *Client
 	id   PieceID
 	size int64
 }
 
 // PieceRanger PieceRanger returns a RangeCloser from a PieceID.
-func PieceRanger(ctx context.Context, c PSClient, id PieceID) (ranger.RangeCloser, error) {
+func PieceRanger(ctx context.Context, c *Client, id PieceID) (ranger.RangeCloser, error) {
 	piece, err := c.Meta(ctx, PieceID(id))
 	if err != nil {
 		return nil, err
@@ -35,7 +35,7 @@ func PieceRanger(ctx context.Context, c PSClient, id PieceID) (ranger.RangeClose
 // PieceRangerSize creates a PieceRanger with known size.
 // Use it if you know the piece size. This will safe the extra request for
 // retrieving the piece size from the piece storage.
-func PieceRangerSize(c PSClient, id PieceID, size int64) ranger.RangeCloser {
+func PieceRangerSize(c *Client, id PieceID, size int64) ranger.RangeCloser {
 	return &pieceRanger{c: c, id: id, size: size}
 }
 
