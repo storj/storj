a2242552ccb3bde5909a5ce03584fcd62bd53db6
Dylan Lott <lott.dylan@gmail.com>
2018-06-28 15:34:53 -0600

    Scaffold test setup

--- pkg/process/exec_test.go
+++ pkg/process/exec_test.go
@@ -1,9 +1,43 @@
 package process
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestMain(t *testing.T) {
+type mockService struct {
+	Called int
+}
 
+var (
+	getCases = []struct {
+		testID              string
+		expectedTimesCalled int
+		expectedResponse    error
+	}{
+		// test cases
+		{
+			testID:              "valid process",
+			expectedTimesCalled: 1,
+			expectedResponse:    nil,
+		},
+		{
+			testID:              "multiple processes",
+			expectedTimesCalled: 1,
+			expectedResponse:    nil,
+		},
+		{
+			testID:              "error process",
+			expectedTimesCalled: 1,
+			expectedResponse:    nil,
+		},
+	}
+)
+
+func TestMain(t *testing.T) {
+	for _, c := range getCases {
+		t.Run(c.testID, func(t *testing.T) {
+			fmt.Printf("running test %s", c.testID)
+		})
+	}
 }
