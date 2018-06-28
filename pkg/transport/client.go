4a004651cb031298d6d679e695826040785931b0
kishore <kishore@storj.io>
2018-06-28 14:14:36 -0400

    transport client incorporates review comments

--- pkg/transport/client.go
+++ pkg/transport/client.go
@@ -1,11 +1,10 @@
 // Copyright (C) 2018 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-package client
+package transport
 
 import (
 	"context"
-	"errors"
 
 	"github.com/zeebo/errs"
 	"google.golang.org/grpc"
@@ -20,32 +19,7 @@ var (
 )
 
 // TransportClient defines the interface to an network client.
-type Client interface {
+type client interface {
 	DialUnauthenticated(ctx context.Context, node proto.Node) (*grpc.ClientConn, error)
 	DialNode(ctx context.Context, node proto.Node) (*grpc.ClientConn, error)
-}
-
-// transportClient is the concrete implementation of the networkclient interface
-type client struct {
-}
-
-// Dial using the authenticated mode
-func (o *client) DialNode(ctx context.Context, node proto.Node) (conn *grpc.ClientConn, err error) {
-
-	/* TODO@ASK security feature under development */
-	return o.DialUnauthenticated(ctx, node)
-}
-
-// Dial using unauthenticated mode
-func (o *client) DialUnauthenticated(ctx context.Context, node proto.Node) (conn *grpc.ClientConn, err error) {
-	if node.Address.Address == "" {
-		return nil, errors.New("No Address")
-	}
-
-	conn, err = grpc.Dial(node.Address.Address, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, err
 }
---
+++ pkg/transport/transport.go
@@ -0,0 +1,33 @@
+package transport
+
+import (
+	"context"
+	"errors"
+
+	"google.golang.org/grpc"
+
+	proto "storj.io/storj/protos/overlay"
+)
+
+// transportClient is the concrete implementation of the networkclient interface
+type Transport struct {
+}
+
+// Dial using the authenticated mode
+func (o *Transport) DialNode(ctx context.Context, node proto.Node) (conn *grpc.ClientConn, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	/* TODO@ASK security feature under development */
+	return o.DialUnauthenticated(ctx, node)
+}
+
+// Dial using unauthenticated mode
+func (o *Transport) DialUnauthenticated(ctx context.Context, node proto.Node) (conn *grpc.ClientConn, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	if node.Address == nil || node.Address.Address == "" {
+		return nil, errors.New("No Address")
+	}
+
+	return grpc.Dial(node.Address.Address, grpc.WithInsecure())
+}
