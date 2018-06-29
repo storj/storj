f049d834fa9597b1fb0f5b774a3bc2eeea977c98
Dylan Lott <lott.dylan@gmail.com>
2018-06-29 16:21:52 -0600

    Add error classes for testing, test main logger error

--- pkg/process/exec_test.go
+++ pkg/process/exec_test.go
@@ -61,6 +61,17 @@ func TestMainMultipleProcess(t *testing.T) {
 	mockService2.AssertExpectations(t)
 }
 
+func TestMainLoggerError(t *testing.T) {
+	mockService := MockedService{}
+
+	err := process.ErrLogger.New("Logger Error")
+	mockService.On("SetLogger", mock.Anything).Return(err)
+	mockService.On("SetMetricHandler", mock.Anything).Return(nil)
+	mockService.On("Process", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	assert.Equal(t, process.Main(&mockService), err)
+	mockService.AssertExpectations(t)
+}
+
 func TestMust(t *testing.T) {
 	t.Skip()
 }
