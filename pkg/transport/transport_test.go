62edf9067c0ab784133414210e578c58230ab29f
kishore <kishore@storj.io>
2018-06-29 11:10:04 -0400

    added test cases

--- pkg/transport/transport_test.go
+++ pkg/transport/transport_test.go
@@ -12,6 +12,8 @@ import (
 
 func TestDialNode(t *testing.T) {
 	oc := Transport{}
+
+	// node.Address.Address == "" condition test
 	node := proto.Node{
 		Id: "DUMMYID1",
 		Address: &proto.NodeAddress{
@@ -22,4 +24,25 @@ func TestDialNode(t *testing.T) {
 	conn, err := oc.DialNode(context.Background(), node)
 	assert.Error(t, err)
 	assert.Nil(t, conn)
+
+	// node.Address == nil condition test
+	node = proto.Node{
+		Id:      "DUMMYID2",
+		Address: nil,
+	}
+	conn, err = oc.DialNode(context.Background(), node)
+	assert.Error(t, err)
+	assert.Nil(t, conn)
+
+	// node is valid argument condition test
+	node = proto.Node{
+		Id: "DUMMYID3",
+		Address: &proto.NodeAddress{
+			Transport: proto.NodeTransport_TCP,
+			Address:   "127.0.0.0:9000",
+		},
+	}
+	conn, err = oc.DialNode(context.Background(), node)
+	assert.NoError(t, err)
+	assert.NotNil(t, conn)
 }
