ee8cc71c6cf25e6e03aeaf0faa1b892bc79fa3aa
nfarah86 <nfarah86@gmail.com>
2018-07-02 11:52:16 -0700

    resolving merge conflicts

--- examples/netstate-client/main.go
+++ examples/netstate-client/main.go
@@ -7,15 +7,15 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"strings"
+	//"strings"
 
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
+	//"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	proto "storj.io/storj/protos/netstate"
-	nsclient "storj.io/storj/pkg/netstate"
+	client "storj.io/storj/pkg/netstate/client"
 )
 
 var (
@@ -34,16 +34,29 @@ func main() {
 	logger, _ := zap.NewDevelopment()
 	defer logger.Sync()
 
-	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	nsclient, err := client.NewNetStateClient(port)
 	if err != nil {
 		logger.Error("Failed to dial: ", zap.Error(err))
 	}
 
-	client := proto.NewNetStateClient(conn)
-
 	logger.Debug(fmt.Sprintf("client dialed port %s", port))
 
+
+	// conn, err := grpc.Dial(port, grpc.WithInsecure())
+	// if err != nil {
+	// 	logger.Error("Failed to dial: ", zap.Error(err))
+	// }
+
+	// client := proto.NewNetStateClient(conn)
+
+	// logger.Debug(fmt.Sprintf("client dialed port %s", port))
+
 	ctx := context.Background()
+
+
+
+
+
 
 	// Example pointer paths to put
 	pr1 := proto.PutRequest{
@@ -97,6 +110,7 @@ func main() {
 	}
 
 	// Example Puts
+<<<<<<< HEAD
 	_, err = client.Put(ctx, &pr1)
 	if err != nil || status.Code(err) == codes.Internal {
 		logger.Error("failed to put", zap.Error(err))
@@ -153,4 +167,70 @@ func main() {
 	if err != nil || status.Code(err) == codes.Internal {
 		logger.Error("failed to delete: " + string(delReq.Path))
 	}
+=======
+	// puts passes api creds
+	_, err = nsclient.Put(ctx, &pr1)
+	if err != nil || status.Code(err) == codes.Internal {
+		logger.Error("failed to put", zap.Error(err))
+	}
+	
+	
+	// _, err = client.Put(ctx, &pr2)
+	// if err != nil || status.Code(err) == codes.Internal {
+	// 	logger.Error("failed to put", zap.Error(err))
+	// }
+	// _, err = client.Put(ctx, &pr3)
+	// if err != nil || status.Code(err) == codes.Internal {
+	// 	logger.Error("failed to put", zap.Error(err))
+	// }
+
+
+
+
+	// Example Get
+	// get passes api creds
+	// getReq := proto.GetRequest{
+	// 	Path:   []byte("so/many/pointers"),
+	// 	APIKey: []byte("abc123"),
+	// }
+	// getRes, err := client.Get(ctx, &getReq)
+	// if err != nil || status.Code(err) == codes.Internal {
+	// 	logger.Error("failed to get", zap.Error(err))
+	// } else {
+	// 	pointer := string(getRes.Pointer)
+	// 	logger.Debug("get response: " + pointer)
+	// }
+
+	// // Example List
+	// // list passes api creds
+	// listReq := proto.ListRequest{
+	// 	// This pagination functionality doesn't work yet.
+	// 	// The given arguments are placeholders.
+	// 	StartingPathKey: []byte("test/pointer/path"),
+	// 	Limit:           5,
+	// 	APIKey:          []byte("abc123"),
+	// }
+
+	// listRes, err := client.List(ctx, &listReq)
+	// if err != nil || status.Code(err) == codes.Internal {
+	// 	logger.Error("failed to list file paths")
+	// } else {
+	// 	var stringList []string
+	// 	for _, pathByte := range listRes.Paths {
+	// 		stringList = append(stringList, string(pathByte))
+	// 	}
+	// 	logger.Debug("listed paths: " + strings.Join(stringList, ", "))
+	// }
+
+	// // Example Delete
+	// // delete passes api creds
+	// delReq := proto.DeleteRequest{
+	// 	Path:   []byte("welcome/to/my/pointer/journey"),
+	// 	APIKey: []byte("abc123"),
+	// }
+	// _, err = client.Delete(ctx, &delReq)
+	// if err != nil || status.Code(err) == codes.Internal {
+	// 	logger.Error("failed to delete: " + string(delReq.Path))
+	// }
+>>>>>>> wip testing client
 }
