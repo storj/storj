74ba007388181a5f3d81ae41fa94d025c467fa58
Moby von Briesen <mobyvb@gmail.com>
2018-07-05 17:44:30 -0400

    rename netstate to pointerdb

--- pkg/pointerdb/common.go
+++ pkg/pointerdb/common.go
@@ -1,11 +1,11 @@
 // Copyright (C) 2018 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-package netstate
+package pointerdb
 
 import (
 	"github.com/zeebo/errs"
 )
 
 // Error is the default boltdb errs class
-var Error = errs.Class("netstate error")
+var Error = errs.Class("pointerdb error")
