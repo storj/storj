f1e7793862a1bf2b6a44c9e4f6a9e67c0aadd4de
Dylan Lott <lott.dylan@gmail.com>
2018-07-05 13:53:58 -0600

    DRY up cmd/netstate package

--- cmd/netstate/main.go
+++ cmd/netstate/main.go
@@ -4,62 +4,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"go.uber.org/zap"
-	"google.golang.org/grpc"
-	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+	"log"
 
 	"storj.io/storj/pkg/netstate"
-	"storj.io/storj/storage/boltdb"
+	"storj.io/storj/pkg/process"
 )
 
-func (s *serv) Process(ctx context.Context, _ *cobra.Command, _ []string) error {
-	bdb, err := boltdb.NewClient(s.logger, *dbPath, boltdb.PointerBucket)
-
+func main() {
+	err := process.Main(process.ConfigEnvironment, &netstate.Service{})
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer bdb.Close()
-
-	// start grpc server
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		return err
-	}
-
-	grpcServer := grpc.NewServer()
-
-	proto.RegisterNetStateServer(grpcServer, netstate.NewServer(bdb, s.logger))
-	s.logger.Debug(fmt.Sprintf("server listening on port %d", *port))
-
-	defer grpcServer.GracefulStop()
-	return grpcServer.Serve(lis)
 }
-
-type serv struct {
-	logger  *zap.Logger
-	metrics *monkit.Registry
-}
-
-func (s *serv) SetLogger(l *zap.Logger) error {
-	s.logger = l
-	return nil
-}
-
-func setEnv() error {
-	viper.SetEnvPrefix("API")
-	viper.AutomaticEnv()
-	return nil
-}
-
-func (s *serv) SetMetricHandler(m *monkit.Registry) error {
-	s.metrics = m
-	return nil
-}
-
-func (s *serv) InstanceID() string { return "" }
