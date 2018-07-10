7be57c470991ce28d394c4fecca5947d19ab8bda
Natalie Ventura Villasana <nat@storj.io>
2018-07-10 13:26:55 -0400

    adds overlay calls to put

--- pkg/segment/segment.go
+++ pkg/segment/segment.go
@@ -8,11 +8,12 @@ import (
 	"io"
 	"time"
 
-	"storj.io/storj/pkg/dtypes"
+	"storj.io/storj/pkg/paths"
 	"storj.io/storj/pkg/overlay"
 	"storj.io/storj/pkg/ranger"
 	"storj.io/storj/pkg/transport"
-	"storj.io/storj/protos/netstate"
+	"storj.io/storj/protos/pointerdb"
+	opb "storj.io/storj/protos/overlay"
 )
 
 // Meta describes associated Nodes and if data is Inline
@@ -20,40 +21,54 @@ type Meta struct {
 	dtypes.Meta
 
 	Inline bool
-	Nodes  []dtypes.NodeID
+	Nodes  []overlay.NodeID
 }
 
 // Store allows Put, Get, Delete, and List methods on paths
 type Store interface {
-	Put(ctx context.Context, path dtypes.Path, data io.Reader, metadata []byte,
+	Put(ctx context.Context, path paths.Path, data io.Reader, metadata []byte,
 		expiration time.Time) error
-	Get(ctx context.Context, path dtypes.Path) (ranger.Ranger, Meta, error)
-	Delete(ctx context.Context, path dtypes.Path) error
-	List(ctx context.Context, startingPath, endingPath dtypes.Path) (
-		paths []dtypes.Path, truncated bool, err error)
+	Get(ctx context.Context, path paths.Path) (ranger.Ranger, Meta, error)
+	Delete(ctx context.Context, path paths.Path) error
+	List(ctx context.Context, startingPath, endingPath paths.Path) (
+		paths []paths.Path, truncated bool, err error)
 }
 
 // SegmentStore defines the SegmentStore
 type segmentStore struct {
-	nc *netstate.NetStateClient
-	oc *overlay.Overlay
-	tc *transport.Transport
+	pdb *pointerdb.PointerDBClient
+	oc  *overlay.Overlay
+	tc  *transport.Transport
 }
 
 // Put uploads a file to an erasure code client
-func (s *SegmentStore) Put(ctx context.Context, path dtypes.Path, data io.Reader,
-	metadata []byte, expiration time.Time) error {
+func (s *SegmentStore) Put(ctx context.Context, path paths.Path, data io.Reader,
+metadata []byte, expiration time.Time) err error {
+	defer mon.Task()(&tcx)(&err)
+
+	addr := "bootstrap.storj.io:7070"
+	c, err := overlay.NewClient(&addr, grpc.WithInsecure())
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+	r, err := c.FindStorageNodes(ctx, &opb.FindStorageNodesRequest{})
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+
 }
 
 // Get retrieves a file from the erasure code client with help from overlay and pointerdb
-func (s *SegmentStore) Get(ctx context.Context, path dtypes.Path) (ranger.Ranger, Meta, error) {
+func (s *SegmentStore) Get(ctx context.Context, path paths.Path) (ranger.Ranger, Meta, error) {
 }
 
 // Delete issues deletes of a file to all piece stores and deletes from pointerdb
-func (s *SegmentStore) Delete(ctx context.Context, path dtypes.Path) error {
+func (s *SegmentStore) Delete(ctx context.Context, path paths.Path) error {
 }
 
 // List lists paths stored in the pointerdb
-func (s *SegmentStore) List(ctx context.Context, startingPath, endingPath dtypes.Path) (
-	paths []dtypes.Path, truncated bool, err error) {
+func (s *SegmentStore) List(ctx context.Context, startingPath, endingPath paths.Path) (
+paths []paths.Path, truncated bool, err error) {
 }
