80a5ad361bcaa3fd33d591397e02cfe12d381bb2
Kaloyan Raev <kaloyan@storj.io>
2018-07-11 15:32:02 +0300

    Remove methods for extended attributes

--- pkg/storage/objects/store.go
+++ pkg/storage/objects/store.go
@@ -20,22 +20,14 @@ var mon = monkit.Package()
 
 // Store for objects
 type Store interface {
-	GetObjectMeta(ctx context.Context, path paths.Path) (meta storage.Meta,
-		err error)
-	GetObject(ctx context.Context, path paths.Path) (rr ranger.RangeCloser,
+	Meta(ctx context.Context, path paths.Path) (meta storage.Meta, err error)
+	Get(ctx context.Context, path paths.Path) (rr ranger.RangeCloser,
 		meta storage.Meta, err error)
-	PutObject(ctx context.Context, path paths.Path, data io.Reader,
-		metadata []byte, expiration time.Time) (meta storage.Meta, err error)
-	DeleteObject(ctx context.Context, path paths.Path) (err error)
-	ListObjects(ctx context.Context, root, startAfter, endBefore paths.Path,
+	Put(ctx context.Context, path paths.Path, data io.Reader, metadata []byte,
+		expiration time.Time) (meta storage.Meta, err error)
+	Delete(ctx context.Context, path paths.Path) (err error)
+	List(ctx context.Context, root, startAfter, endBefore paths.Path,
 		recursive bool, limit int) (result []paths.Path, more bool, err error)
-	GetXAttr(ctx context.Context, path paths.Path, xattr string) (
-		rr ranger.RangeCloser, meta storage.Meta, err error)
-	SetXAttr(ctx context.Context, path paths.Path, xattr string,
-		data io.Reader, metadata []byte) (meta storage.Meta, err error)
-	DeleteXAttr(ctx context.Context, path paths.Path, xattr string) (err error)
-	ListXAttrs(ctx context.Context, path paths.Path, startAfter,
-		endBefore string, limit int) (xattrs []string, more bool, err error)
 }
 
 type objStore struct {
@@ -47,93 +39,41 @@ func NewStore(store streams.Store) Store {
 	return &objStore{s: store}
 }
 
-func (o *objStore) PutObject(ctx context.Context, path paths.Path,
-	data io.Reader, metadata []byte, expiration time.Time) (
-	meta storage.Meta, err error) {
+func (o *objStore) Put(ctx context.Context, path paths.Path, data io.Reader,
+	metadata []byte, expiration time.Time) (meta storage.Meta, err error) {
 	defer mon.Task()(&ctx)(&err)
 	objPath := getDefautStreamPath(path)
 	return o.s.Put(ctx, objPath, data, metadata, expiration)
 }
 
-func (o *objStore) GetObject(ctx context.Context, path paths.Path) (
+func (o *objStore) Get(ctx context.Context, path paths.Path) (
 	rr ranger.RangeCloser, meta storage.Meta, err error) {
 	defer mon.Task()(&ctx)(&err)
 	objPath := getDefautStreamPath(path)
 	return o.s.Get(ctx, objPath)
 }
 
-func (o *objStore) GetObjectMeta(ctx context.Context, path paths.Path) (
+func (o *objStore) Meta(ctx context.Context, path paths.Path) (
 	meta storage.Meta, err error) {
 	defer mon.Task()(&ctx)(&err)
 	objPath := getDefautStreamPath(path)
 	return o.s.Meta(ctx, objPath)
 }
 
-func (o *objStore) DeleteObject(ctx context.Context, path paths.Path) (
-	err error) {
+func (o *objStore) Delete(ctx context.Context, path paths.Path) (err error) {
 	defer mon.Task()(&ctx)(&err)
 	objPath := getDefautStreamPath(path)
 	return o.s.Delete(ctx, objPath)
 }
 
-func (o *objStore) ListObjects(ctx context.Context, root, startAfter,
-	endBefore paths.Path, recursive bool, limit int) (
-	result []paths.Path, more bool, err error) {
+func (o *objStore) List(ctx context.Context, root, startAfter,
+	endBefore paths.Path, recursive bool, limit int) (result []paths.Path,
+	more bool, err error) {
 	defer mon.Task()(&ctx)(&err)
 	rootObjPath := getDefautStreamPath(root)
 	return o.s.List(ctx, rootObjPath, startAfter, endBefore, recursive, limit)
 }
 
-func (o *objStore) SetXAttr(ctx context.Context, path paths.Path, xattr string,
-	data io.Reader, metadata []byte) (meta storage.Meta, err error) {
-	defer mon.Task()(&ctx)(&err)
-	objPath := getDefautStreamPath(path)
-	objMeta, err := o.s.Meta(ctx, objPath)
-	if err != nil {
-		return storage.Meta{}, err
-	}
-	xattrPath := getNamedStreamPath(path, xattr)
-	return o.s.Put(ctx, xattrPath, data, metadata, objMeta.Expiration)
-}
-
-func (o *objStore) GetXAttr(ctx context.Context, path paths.Path,
-	xattr string) (rr ranger.RangeCloser, meta storage.Meta, err error) {
-	defer mon.Task()(&ctx)(&err)
-	xattrPath := getNamedStreamPath(path, xattr)
-	return o.s.Get(ctx, xattrPath)
-}
-
-func (o *objStore) DeleteXAttr(ctx context.Context, path paths.Path,
-	xattr string) (err error) {
-	defer mon.Task()(&ctx)(&err)
-	xattrPath := getNamedStreamPath(path, xattr)
-	return o.s.Delete(ctx, xattrPath)
-}
-
-func (o *objStore) ListXAttrs(ctx context.Context, path paths.Path, startAfter,
-	endBefore string, limit int) (xattrs []string, more bool, err error) {
-	defer mon.Task()(&ctx)(&err)
-	root := getNamedStreamsRoot(path)
-	paths, more, err := o.s.List(ctx, root, paths.New(startAfter),
-		paths.New(endBefore), false, limit)
-	if err != nil {
-		return []string{}, false, err
-	}
-	xattrs = make([]string, len(paths))
-	for i, p := range paths {
-		xattrs[i] = p[len(p)-1]
-	}
-	return xattrs, more, nil
-}
-
 func getDefautStreamPath(path paths.Path) paths.Path {
 	return path.Prepend("object")
-}
-
-func getNamedStreamsRoot(path paths.Path) paths.Path {
-	return path.Prepend("xattr")
-}
-
-func getNamedStreamPath(path paths.Path, xattr string) paths.Path {
-	return getNamedStreamsRoot(path).Append(xattr)
 }
