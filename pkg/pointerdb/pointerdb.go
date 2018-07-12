56b41cac9aa6e9d4d5da1b22e63c56aca87c0876
nfarah86 <nfarah86@gmail.com>
2018-07-12 15:52:48 -0700

    resolved merge conflicts

--- pkg/pointerdb/pointerdb.go
+++ pkg/pointerdb/pointerdb.go
@@ -86,7 +86,7 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 func (s *Server) List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
 	s.logger.Debug("entering pointerdb list")
 
-	if req.Limit <= 0 {
+	if req.Limit <= 0 {		
 		return nil, Error.New("err Limit is less than or equal to 0")
 	}
 
