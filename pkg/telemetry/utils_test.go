005f1f99a641500ce9d1f4c976430ea9de110e28
crawter <crawter@hotmail.com>
2018-07-17 14:11:59 +0300

    Unit tests added for Utils package

---
+++ pkg/telemetry/utils_test.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package telemetry
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJitter_NegativeDuration(t *testing.T) {
+	duration := time.Duration(-1)
+	expected := time.Duration(1)
+
+	actual := jitter(duration)
+
+	assert.Equal(t, expected, actual)
+}
