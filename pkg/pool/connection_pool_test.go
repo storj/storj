ea077e4dcb04461e5dd11741811dc41b16f9c6a6
Dennis Coyle <coyle@users.noreply.github.com>
2018-07-19 10:48:08 -0400

    node-node communication (#145)
    * node-node communication
    
    * PR reviews comments from @bryanchriswhite addressed

---
+++ pkg/pool/connection_pool_test.go
@@ -0,0 +1,89 @@
+package pool
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestFoo struct {
+	called string
+}
+
+func TestGet(t *testing.T) {
+	cases := []struct {
+		pool          ConnectionPool
+		key           string
+		expected      TestFoo
+		expectedError error
+	}{
+		{
+			pool:          ConnectionPool{cache: map[string]interface{}{"foo": TestFoo{called: "hoot"}}},
+			key:           "foo",
+			expected:      TestFoo{called: "hoot"},
+			expectedError: nil,
+		},
+	}
+
+	for _, v := range cases {
+		test, err := v.pool.Get(context.Background(), v.key)
+		assert.Equal(t, v.expectedError, err)
+		assert.Equal(t, v.expected, test)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	cases := []struct {
+		pool          ConnectionPool
+		key           string
+		value         TestFoo
+		expected      TestFoo
+		expectedError error
+	}{
+		{
+			pool:          ConnectionPool{cache: map[string]interface{}{}},
+			key:           "foo",
+			value:         TestFoo{called: "hoot"},
+			expected:      TestFoo{called: "hoot"},
+			expectedError: nil,
+		},
+	}
+
+	for _, v := range cases {
+		err := v.pool.Add(context.Background(), v.key, v.value)
+		assert.Equal(t, v.expectedError, err)
+
+		test, err := v.pool.Get(context.Background(), v.key)
+		assert.Equal(t, v.expectedError, err)
+
+		assert.Equal(t, v.expected, test)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	cases := []struct {
+		pool          ConnectionPool
+		key           string
+		value         TestFoo
+		expected      interface{}
+		expectedError error
+	}{
+		{
+			pool:          ConnectionPool{cache: map[string]interface{}{"foo": TestFoo{called: "hoot"}}},
+			key:           "foo",
+			expected:      nil,
+			expectedError: nil,
+		},
+	}
+
+	for _, v := range cases {
+		err := v.pool.Remove(context.Background(), v.key)
+		assert.Equal(t, v.expectedError, err)
+
+		test, err := v.pool.Get(context.Background(), v.key)
+		assert.Equal(t, v.expectedError, err)
+
+		assert.Equal(t, v.expected, test)
+	}
+}
