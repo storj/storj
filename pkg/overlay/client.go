d26bc8bda0a003ac7ed5031cced49e3eb845b42c
Kaloyan Raev <kaloyan@storj.io>
2018-07-23 11:42:05 +0300

    Adjust Segment Store to the updated interface

--- pkg/overlay/client.go
+++ pkg/overlay/client.go
@@ -9,7 +9,6 @@ import (
 	"google.golang.org/grpc"
 
 	"storj.io/storj/pkg/dht"
-	"storj.io/storj/pkg/kademlia"
 	proto "storj.io/storj/protos/overlay"
 )
 
@@ -21,8 +20,8 @@ import (
 //
 // Lookup finds a Node with the provided identifier.
 type Client interface {
-	Choose(ctx context.Context, limit, space int64) ([]*dht.NodeID, error)
-	Lookup(ctx context.Context, nodeID kademlia.NodeID) (*proto.Node, error)
+	Choose(ctx context.Context, limit int, space int64) ([]*proto.Node, error)
+	Lookup(ctx context.Context, nodeID dht.NodeID) (*proto.Node, error)
 }
 
 // Overlay is the overlay concrete implementation of the client interface
@@ -31,7 +30,7 @@ type Overlay struct {
 }
 
 // NewOverlayClient returns a new intialized Overlay Client
-func NewOverlayClient(address string) (*Overlay, error) {
+func NewOverlayClient(address string) (Client, error) {
 	c, err := NewClient(&address, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
@@ -42,7 +41,7 @@ func NewOverlayClient(address string) (*Overlay, error) {
 }
 
 // Choose implements the client.Choose interface
-func (o *Overlay) Choose(ctx context.Context, limit, space int64) ([]*proto.Node, error) {
+func (o *Overlay) Choose(ctx context.Context, limit int, space int64) ([]*proto.Node, error) {
 	// TODO(coyle): We will also need to communicate with the reputation service here
 	resp, err := o.client.FindStorageNodes(ctx, &proto.FindStorageNodesRequest{})
 	if err != nil {
