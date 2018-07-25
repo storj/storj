daba5fff90a4ea2fed1d3bf16ce678763f737ae6
JT Olio <hello@jtolio.com>
2018-07-25 12:16:38 -0600

    add setup to gw binary too

--- cmd/gw/main.go
+++ cmd/gw/main.go
@@ -4,12 +4,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/miniogw"
+	"storj.io/storj/pkg/peertls"
 	"storj.io/storj/pkg/process"
 )
 
@@ -30,12 +34,52 @@ func init() {
 		Short: "Run the gateway",
 		RunE:  cmdRun,
 	})
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "setup",
+		Short: "Create config files",
+		RunE:  cmdSetup,
+	})
 	cfgstruct.Bind(rootCmd.PersistentFlags(), &cfg,
 		cfgstruct.ConfDir(defaultConfDir))
 }
 
 func cmdRun(cmd *cobra.Command, args []string) (err error) {
 	return cfg.Run(process.Ctx(cmd))
+}
+
+func cmdSetup(cmd *cobra.Command, args []string) (err error) {
+	ctx := process.Ctx(cmd)
+
+	// TODO: clean this up somehow?
+	if !strings.HasSuffix(cfg.IdentityConfig.CertPath, ".leaf.cert") {
+		return fmt.Errorf("certificate path should end with .leaf.cert")
+	}
+	certpath := cfg.IdentityConfig.CertPath[:len(cfg.IdentityConfig.CertPath)-len(".leaf.cert")]
+	if !strings.HasSuffix(cfg.IdentityConfig.KeyPath, ".leaf.key") {
+		return fmt.Errorf("key path should end with .leaf.key")
+	}
+	keypath := cfg.IdentityConfig.KeyPath[:len(cfg.IdentityConfig.KeyPath)-len(".leaf.key")]
+
+	err = os.MkdirAll(filepath.Dir(certpath), 0700)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Dir(keypath), 0700)
+	if err != nil {
+		return err
+	}
+
+	_, err = peertls.NewTLSFileOptions(certpath, keypath, true, false)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Dir(process.CfgPath(ctx)), 0700)
+	if err != nil {
+		return err
+	}
+
+	return process.SaveConfigAs(cmd, process.CfgPath(ctx))
 }
 
 func main() {
