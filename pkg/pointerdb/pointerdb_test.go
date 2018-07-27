daf391e47368ac3733f36469a43806c8ff96e98d
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2018-07-27 09:02:59 +0300

    Refactor List in PointerDB (#163)
    * Refactor List in Pointer DB
    
    * Fix pointerdb-client example
    
    * Fix issue in Path type related to empty paths
    
    * Test for the PointerDB service with some fixes
    
    * Fixed debug message in example: trancated --> more
    
    * GoDoc comments for unexported methods
    
    * TODO comment to check if Put is overwriting
    
    * Log warning if protobuf timestamp cannot be converted
    
    * TODO comment to make ListPageLimit configurable
    
    * Rename 'segment' package to 'segments' to reflect folder name

--- pkg/pointerdb/pointerdb_test.go
+++ pkg/pointerdb/pointerdb_test.go
@@ -4,104 +4,53 @@
 package pointerdb
 
 import (
-	"context"
-	"fmt"
 	"errors"
+	"fmt"
 	"testing"
-	"log"
-	"strings"
 
 	"github.com/golang/protobuf/proto"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"storj.io/storj/pkg/paths"
+	meta "storj.io/storj/pkg/storage"
 	pb "storj.io/storj/protos/pointerdb"
-	p "storj.io/storj/pkg/paths"
+	"storj.io/storj/storage"
 )
 
-const (
-	unauthenticated = "failed API creds"
-	noPathGiven = "file path not given"
-	noLimitGiven = "limit not given"
-)
-
-var (
-	ctx = context.Background()
-	ErrUnauthenticated = errors.New(unauthenticated)
-	ErrNoFileGiven = errors.New(noPathGiven)
-	ErrNoLimitGiven = errors.New(noLimitGiven)
-)
-
-func TestNewPointerDBClient(t *testing.T) {
-	// mocked grpcClient so we don't have
-	// to call the network to test the code
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	gc:= NewMockPointerDBClient(ctrl)
-	pdb := PointerDB{grpcClient: gc}
-
-	assert.NotNil(t, pdb)
-	assert.NotNil(t, pdb.grpcClient)
-}
-
-func makePointer(path p.Path, auth []byte) pb.PutRequest {
-	// rps is an example slice of RemotePieces to add to this
-	// REMOTE pointer type.
-	var rps []*pb.RemotePiece
-	rps = append(rps, &pb.RemotePiece{
-		PieceNum: int64(1),
-		NodeId:   "testId",
-	})
-	pr := pb.PutRequest{
-		Path: path.Bytes(),
-		Pointer: &pb.Pointer{
-			Type: pb.Pointer_REMOTE,
-			Remote: &pb.RemoteSegment{
-				Redundancy: &pb.RedundancyScheme{
-					Type:             pb.RedundancyScheme_RS,
-					MinReq:           int64(1),
-					Total:            int64(3),
-					RepairThreshold:  int64(2),
-					SuccessThreshold: int64(3),
-				},
-				PieceId:      "testId",
-				RemotePieces: rps,
-			},
-			Size: int64(1),
-		},
-		APIKey: auth,
-	}
-	return pr
-}
-
-func TestPut(t *testing.T){
+func TestServicePut(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	for i, tt := range []struct {
-		APIKey []byte
-		path p.Path
-		err error 
+		apiKey    []byte
+		err       error
 		errString string
 	}{
-		{[]byte("abc123"), p.New("file1/file2"), nil, ""},
-		{[]byte("wrong key"), p.New("file1/file2"), ErrUnauthenticated,unauthenticated},
-		{[]byte("abc123"), p.New(""), ErrNoFileGiven, noPathGiven},
-		{[]byte("wrong key"), p.New(""), ErrUnauthenticated, unauthenticated},
-		{[]byte(""), p.New(""), ErrUnauthenticated, unauthenticated},
-	}{
-		putRequest:= makePointer(tt.path, tt.APIKey)
-
+		{nil, nil, ""},
+		{[]byte("wrong key"), nil, grpc.Errorf(codes.Unauthenticated, "Invalid API credential").Error()},
+		{nil, errors.New("put error"), status.Errorf(codes.Internal, "put error").Error()},
+	} {
 		errTag := fmt.Sprintf("Test case #%d", i)
-		gc:= NewMockPointerDBClient(ctrl)
-		pdb := PointerDB{grpcClient: gc}
 
-		// here we don't care what type of context we pass
-		gc.EXPECT().Put(gomock.Any(), &putRequest).Return(nil, tt.err)
+		db := NewMockKeyValueStore(ctrl)
+		s := Server{DB: db, logger: zap.NewNop()}
 
-		err := pdb.Put(ctx, tt.path, putRequest.Pointer, tt.APIKey)
-		
+		path := "a/b/c"
+		pr := pb.Pointer{}
+
+		if tt.err != nil || tt.errString == "" {
+			db.EXPECT().Put(storage.Key([]byte(path)), gomock.Any()).Return(tt.err)
+		}
+
+		req := pb.PutRequest{Path: path, Pointer: &pr, APIKey: tt.apiKey}
+		_, err := s.Put(ctx, &req)
+
 		if err != nil {
 			assert.EqualError(t, err, tt.errString, errTag)
 		} else {
@@ -110,157 +59,166 @@ func TestPut(t *testing.T){
 	}
 }
 
-func TestGet(t *testing.T){
+func TestServiceGet(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	for i, tt := range []struct {
-		APIKey []byte
-		path p.Path
-		err error 
+		apiKey    []byte
+		err       error
 		errString string
 	}{
-		{[]byte("wrong key"), p.New("file1/file2"), ErrUnauthenticated,unauthenticated},
-		{[]byte("abc123"), p.New(""), ErrNoFileGiven, noPathGiven},
-		{[]byte("wrong key"), p.New(""), ErrUnauthenticated, unauthenticated},
-		{[]byte(""), p.New(""), ErrUnauthenticated, unauthenticated},
-		{[]byte("abc123"), p.New("file1/file2"), nil, ""},
-	}{
-		getPointer := makePointer(tt.path, tt.APIKey)
-		getRequest:= pb.GetRequest{Path: tt.path.Bytes(), APIKey: tt.APIKey}
-		
-		data, err := proto.Marshal(getPointer.Pointer)
-		if err != nil {
-			log.Fatal("marshaling error: ", err)
+		{nil, nil, ""},
+		{[]byte("wrong key"), nil, grpc.Errorf(codes.Unauthenticated, "Invalid API credential").Error()},
+		{nil, errors.New("get error"), status.Errorf(codes.Internal, "get error").Error()},
+	} {
+		errTag := fmt.Sprintf("Test case #%d", i)
+
+		db := NewMockKeyValueStore(ctrl)
+		s := Server{DB: db, logger: zap.NewNop()}
+
+		path := "a/b/c"
+		pr := pb.Pointer{}
+		prBytes, err := proto.Marshal(&pr)
+		assert.NoError(t, err, errTag)
+
+		if tt.err != nil || tt.errString == "" {
+			db.EXPECT().Get(storage.Key([]byte(path))).Return(prBytes, tt.err)
 		}
 
-		byteData := []byte(data)
-
-		getResponse := pb.GetResponse{Pointer: byteData}
-
-		errTag := fmt.Sprintf("Test case #%d", i)
-		
-		gc:= NewMockPointerDBClient(ctrl)
-		pdb := PointerDB{grpcClient: gc}
-
-		gc.EXPECT().Get(gomock.Any(), &getRequest).Return(&getResponse, tt.err)
-
-		pointer, err := pdb.Get(ctx, tt.path, tt.APIKey)
+		req := pb.GetRequest{Path: path, APIKey: tt.apiKey}
+		resp, err := s.Get(ctx, &req)
 
 		if err != nil {
 			assert.EqualError(t, err, tt.errString, errTag)
-			assert.Nil(t, pointer)
 		} else {
-			assert.NotNil(t, pointer)
+			assert.NoError(t, err, errTag)
+			respPr := pb.Pointer{}
+			err := proto.Unmarshal(resp.GetPointer(), &respPr)
+			assert.NoError(t, err, errTag)
+			assert.Equal(t, pr, respPr, errTag)
+		}
+	}
+}
+
+func TestServiceDelete(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	for i, tt := range []struct {
+		apiKey    []byte
+		err       error
+		errString string
+	}{
+		{nil, nil, ""},
+		{[]byte("wrong key"), nil, grpc.Errorf(codes.Unauthenticated, "Invalid API credential").Error()},
+		{nil, errors.New("delete error"), status.Errorf(codes.Internal, "delete error").Error()},
+	} {
+		errTag := fmt.Sprintf("Test case #%d", i)
+
+		db := NewMockKeyValueStore(ctrl)
+		s := Server{DB: db, logger: zap.NewNop()}
+
+		path := "a/b/c"
+
+		if tt.err != nil || tt.errString == "" {
+			db.EXPECT().Delete(storage.Key([]byte(path))).Return(tt.err)
+		}
+
+		req := pb.DeleteRequest{Path: path, APIKey: tt.apiKey}
+		_, err := s.Delete(ctx, &req)
+
+		if err != nil {
+			assert.EqualError(t, err, tt.errString, errTag)
+		} else {
 			assert.NoError(t, err, errTag)
 		}
 	}
 }
 
-func TestList(t *testing.T){
+func TestServiceList(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	keys := storage.Keys{
+		storage.Key(paths.New("sample.jpg").Bytes()),
+		storage.Key(paths.New("music/song1.mp3").Bytes()),
+		storage.Key(paths.New("music/song2.mp3").Bytes()),
+		storage.Key(paths.New("music/album/song3.mp3").Bytes()),
+		storage.Key(paths.New("music/song4.mp3").Bytes()),
+		storage.Key(paths.New("videos/movie.mkv").Bytes()),
+	}
+
 	for i, tt := range []struct {
-		APIKey []byte
-		startingPath p.Path
-		limit int64 
-		truncated bool
-		paths []string
-		err error 
-		errString string
+		prefix       string
+		startAfter   string
+		endBefore    string
+		recursive    bool
+		limit        int
+		metaFlags    uint64
+		apiKey       []byte
+		returnedKeys storage.Keys
+		expectedKeys storage.Keys
+		expectedMore bool
+		err          error
+		errString    string
 	}{
-		{[]byte("wrong key"), p.New(""), 2, true, []string{""}, ErrUnauthenticated, unauthenticated},
-		{[]byte("abc123"), p.New("file1"), 2, true, []string{"test"},  nil, ""},
-		{[]byte("abc123"), p.New(""), 2, true, []string{"file1/file2", "file3/file4", "file1", "file1/file2/great3", "test"},  ErrNoFileGiven, noPathGiven},
-		{[]byte("abc123"), p.New("file1"), 2, false, []string{""},  nil, ""},
-		{[]byte("wrong key"), p.New("file1"), 2, true, []string{"file1/file2", "file3/file4", "file1", "file1/file2/great3", "test"}, ErrUnauthenticated,unauthenticated},
-		{[]byte("abc123"), p.New("file1"), 3, true, []string{"file1/file2", "file3/file4", "file1", "file1/file2/great3", "test"},  nil, ""},
-		{[]byte("abc123"), p.New("file1"), 0, true, []string{"file1/file2", "file3/file4", "file1", "file1/file2/great3", "test"},  ErrNoLimitGiven, noLimitGiven},
-	}{
-		listRequest := pb.ListRequest{
-			StartingPathKey: tt.startingPath.Bytes(),
-			Limit:           tt.limit,
-			APIKey:          tt.APIKey,
-		}
+		{"", "", "", true, 0, meta.MetaNone, nil, keys, keys, false, nil, ""},
+		{"", "", "", true, 0, meta.MetaAll, nil, keys, keys, false, nil, ""},
+		{"", "", "", true, 0, meta.MetaNone, []byte("wrong key"), keys, keys, false,
+			nil, grpc.Errorf(codes.Unauthenticated, "Invalid API credential").Error()},
+		{"", "", "", true, 0, meta.MetaNone, nil, keys, keys, false,
+			errors.New("list error"), status.Errorf(codes.Internal, "list error").Error()},
+		{"", "", "", true, 2, meta.MetaNone, nil, keys, keys[:2], true, nil, ""},
+		{"", "", "", false, 0, meta.MetaNone, nil, keys, keys[:1], false, nil, ""},
+		{"music", "", "", false, 0, meta.MetaNone, nil, keys[1:], storage.Keys{keys[1], keys[2], keys[4]}, false, nil, ""},
+		{"music", "", "", true, 0, meta.MetaNone, nil, keys[1:], keys[1:5], false, nil, ""},
+		{"music", "song1.mp3", "", true, 0, meta.MetaNone, nil, keys, keys[2:5], false, nil, ""},
+		{"music", "song1.mp3", "album/song3.mp3", true, 0, meta.MetaNone, nil, keys, keys[2:3], false, nil, ""},
+		{"music", "", "song4.mp3", true, 0, meta.MetaNone, nil, keys, keys[1:4], false, nil, ""},
+		{"music", "", "song4.mp3", true, 1, meta.MetaNone, nil, keys, keys[3:4], true, nil, ""},
+		{"music", "", "song4.mp3", false, 0, meta.MetaNone, nil, keys, keys[1:3], false, nil, ""},
+		{"music", "song2.mp3", "song4.mp3", true, 0, meta.MetaNone, nil, keys, keys[3:4], false, nil, ""},
+	} {
+		errTag := fmt.Sprintf("Test case #%d", i)
 
-		var truncatedPathsBytes [][]byte
+		db := NewMockKeyValueStore(ctrl)
+		s := Server{DB: db, logger: zap.NewNop()}
 
-		getCorrectPaths := func(fileName string) bool { return strings.HasPrefix(fileName, "file1")}
-		filterPaths := filterPathName(tt.paths, getCorrectPaths)
-		
-		if len(filterPaths) == 0 {
-			truncatedPathsBytes = [][]byte{}
-		} else{
-			truncatedPaths := filterPaths[0:tt.limit]
-			truncatedPathsBytes := make([][]byte, len(truncatedPaths))
-		
-			for i, pathName := range truncatedPaths {
-				bytePathName := []byte(pathName)
-				truncatedPathsBytes[i] = bytePathName
+		if tt.err != nil || tt.errString == "" {
+			prefix := storage.Key(paths.New(tt.prefix).Bytes())
+			db.EXPECT().List(prefix, storage.Limit(0)).Return(tt.returnedKeys, tt.err)
+
+			if tt.metaFlags != meta.MetaNone {
+				pr := pb.Pointer{}
+				b, err := proto.Marshal(&pr)
+				assert.NoError(t, err, errTag)
+				for _, key := range keys {
+					db.EXPECT().Get(key).Return(b, nil)
+				}
 			}
 		}
-			
-		listResponse := pb.ListResponse{Paths: truncatedPathsBytes, Truncated: tt.truncated }
 
-		errTag := fmt.Sprintf("Test case #%d", i)
-
-		gc:= NewMockPointerDBClient(ctrl)
-		pdb := PointerDB{grpcClient: gc}
-
-		gc.EXPECT().List(gomock.Any(), &listRequest).Return(&listResponse, tt.err)
-
-		paths, trunc, err := pdb.List(ctx, tt.startingPath, tt.limit, tt.APIKey)
-		
-		if err != nil {
-			assert.EqualError(t, err, tt.errString, errTag)
-			assert.NotNil(t, trunc)
-			assert.Nil(t, paths)
-		} else {
-			assert.NoError(t, err, errTag)
+		req := pb.ListRequest{
+			Prefix:     tt.prefix,
+			StartAfter: tt.startAfter,
+			EndBefore:  tt.endBefore,
+			Recursive:  tt.recursive,
+			Limit:      int32(tt.limit),
+			MetaFlags:  tt.metaFlags,
+			APIKey:     tt.apiKey,
 		}
-	}
-}
+		resp, err := s.List(ctx, &req)
 
-func filterPathName(pathString []string, test func(string) bool) (filteredPathNames []string) {
-	for _, name := range pathString{
-		if test(name) {
-			filteredPathNames = append(filteredPathNames, name)
-		}
-	}
-	return
-}
-
-func TestDelete(t *testing.T){
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	for i, tt := range []struct {
-		APIKey []byte
-		path p.Path
-		err error 
-		errString string
-	}{
-		{[]byte("wrong key"), p.New("file1/file2"), ErrUnauthenticated,unauthenticated},
-		{[]byte("abc123"), p.New(""), ErrNoFileGiven, noPathGiven},
-		{[]byte("wrong key"), p.New(""), ErrUnauthenticated, unauthenticated},
-		{[]byte(""), p.New(""), ErrUnauthenticated, unauthenticated},
-		{[]byte("abc123"), p.New("file1/file2"), nil, ""},
-	}{
-		deleteRequest:= pb.DeleteRequest{Path: tt.path.Bytes(), APIKey: tt.APIKey}
-
-		errTag := fmt.Sprintf("Test case #%d", i)
-		gc:= NewMockPointerDBClient(ctrl)
-		pdb := PointerDB{grpcClient: gc}
-
-		gc.EXPECT().Delete(gomock.Any(), &deleteRequest).Return(nil, tt.err)
-		
-		err := pdb.Delete(ctx, tt.path, tt.APIKey)
-		
 		if err != nil {
 			assert.EqualError(t, err, tt.errString, errTag)
 		} else {
 			assert.NoError(t, err, errTag)
+			assert.Equal(t, tt.expectedMore, resp.GetMore(), errTag)
+			assert.Equal(t, len(tt.expectedKeys), len(resp.GetItems()), errTag)
+			for i, item := range resp.GetItems() {
+				assert.Equal(t, tt.expectedKeys[i].String(), item.Path, errTag)
+			}
 		}
 	}
 }
