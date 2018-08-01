59c1e9121327b606f325aa8bb9cf7fcd7f48a7a8
Natalie Ventura Villasana <nat@storj.io>
2018-08-01 18:49:15 -0400

    starts adding segmentStore tests

--- pkg/storage/segments/store_test.go
+++ pkg/storage/segments/store_test.go
@@ -2,3 +2,25 @@
 // See LICENSE for copying information.
 
 package segments
+
+import (
+	"io"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"storj.io/storj/pkg/paths"
+)
+
+func TestSegmentStore(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	for _, tt := range []struct {
+		path       paths.Path
+		data       io.Reader
+		expiration time.Time
+	}{} {
+
+	}
+}
