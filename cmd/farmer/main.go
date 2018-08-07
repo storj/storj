f12bc29166116a847537828b4ca2adbde15e8238
JT Olio <hello@jtolio.com>
2018-08-07 17:06:45 -0600

    captplanet standalone farmer setup

--- cmd/farmer/main.go
+++ cmd/farmer/main.go
@@ -4,12 +4,14 @@
 package main
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
 
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/kademlia"
+	"storj.io/storj/pkg/peertls"
 	"storj.io/storj/pkg/piecestore/psservice"
 	"storj.io/storj/pkg/process"
 	"storj.io/storj/pkg/provider"
@@ -20,32 +22,72 @@ var (
 		Use:   "farmer",
 		Short: "Farmer",
 	}
+	runCmd = &cobra.Command{
+		Use:   "run",
+		Short: "Run the farmer",
+		RunE:  cmdRun,
+	}
+	setupCmd = &cobra.Command{
+		Use:   "setup",
+		Short: "Create config files",
+		RunE:  cmdSetup,
+	}
 
-	cfg struct {
+	runCfg struct {
 		Identity provider.IdentityConfig
 		Kademlia kademlia.Config
 		Storage  psservice.Config
+	}
+	setupCfg struct {
+		BasePath string `default:"$CONFDIR" help:"base path for setup"`
 	}
 
 	defaultConfDir = "$HOME/.storj/farmer"
 )
 
 func init() {
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "run",
-		Short: "Run the farmer",
-		RunE:  cmdRun,
-	})
-	cfgstruct.Bind(rootCmd.PersistentFlags(), &cfg,
-		cfgstruct.ConfDir(defaultConfDir),
-	)
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(setupCmd)
+	cfgstruct.Bind(runCmd.Flags(), &runCfg, cfgstruct.ConfDir(defaultConfDir))
+	cfgstruct.Bind(setupCmd.Flags(), &setupCfg, cfgstruct.ConfDir(defaultConfDir))
 }
 
 func cmdRun(cmd *cobra.Command, args []string) (err error) {
-	return cfg.Identity.Run(process.Ctx(cmd), cfg.Kademlia, cfg.Storage)
+	return runCfg.Identity.Run(process.Ctx(cmd), runCfg.Kademlia, runCfg.Storage)
+}
+
+func cmdSetup(cmd *cobra.Command, args []string) (err error) {
+	setupCfg.BasePath, err = filepath.Abs(setupCfg.BasePath)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(setupCfg.BasePath, 0700)
+	if err != nil {
+		return err
+	}
+
+	identityPath := filepath.Join(setupCfg.BasePath, "identity")
+	_, err = peertls.NewTLSFileOptions(identityPath, identityPath, true, false)
+	if err != nil {
+		return err
+	}
+
+	overrides := map[string]interface{}{
+		"identity.cert-path": filepath.Join(
+			setupCfg.BasePath, "identity.leaf.cert"),
+		"identity.key-path": filepath.Join(
+			setupCfg.BasePath, "identity.leaf.key"),
+		"storage.path": filepath.Join(
+			setupCfg.BasePath, "storage"),
+	}
+
+	return process.SaveConfig(runCmd.Flags(),
+		filepath.Join(setupCfg.BasePath, "config.yaml"), overrides)
 }
 
 func main() {
-	process.ExecuteWithConfig(rootCmd,
-		filepath.Join(defaultConfDir, "config.yaml"))
+	runCmd.Flags().String("config",
+		filepath.Join(defaultConfDir, "config.yaml"), "path to configuration")
+	process.Exec(rootCmd)
 }
