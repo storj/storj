393c51ff95bbff8ff0d13501c553b8056e83c948
Natalie Ventura Villasana <nat@storj.io>
2018-08-09 14:55:56 -0400

    wip - fixing test cases in object tests

--- pkg/miniogw/gateway-storj_test.go
+++ pkg/miniogw/gateway-storj_test.go
@@ -4,6 +4,7 @@
 package miniogw
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"math/rand"
@@ -11,6 +12,7 @@ import (
 	time "time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/minio/minio/pkg/hash"
 	"github.com/stretchr/testify/assert"
 
 	paths "storj.io/storj/pkg/paths"
@@ -77,7 +79,7 @@ import (
 const charset = "abcdefghijklmnopqrstuvwxyz" +
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "0123456789"
 
-var seededRand *rand.Rand = rand.New(
+var seededRand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
 func stringWithCharset(length int, charset string) string {
@@ -92,77 +94,83 @@ func checksumGen(length int) string {
 	return stringWithCharset(length, charset)
 }
 
-// func TestPutObject(t *testing.T) {
-// 	mockCtrl := gomock.NewController(t)
-// 	defer mockCtrl.Finish()
+func TestPutObject(t *testing.T) {
+	mockBucketCtrl := gomock.NewController(t)
+	defer mockBucketCtrl.Finish()
 
-// 	mockGetObject := NewMockStore(mockCtrl)
-// 	s := Storj{os: mockGetObject}
+	mockObjCtrl := gomock.NewController(t)
+	defer mockObjCtrl.Finish()
 
-// 	testUser := storjObjects{storj: &s}
+	mockBS := mock_buckets.NewMockStore(mockBucketCtrl)
+	b := Storj{bs: mockBS}
 
-// 	for i, example := range []struct {
-// 		bucket, object string
-// 		MetaKey        []string
-// 		MetaVal        []string
-// 		Modified       time.Time
-// 		Expiration     time.Time
-// 		Size           int64
-// 		Checksum       string
-// 		err            error // used by mock function
-// 		errString      string
-// 	}{
-// 		// happy scenario
-// 		{"mybucket", "myobject1", []string{"content-type", "userdef_key1", "userdef_key2"}, []string{"foo1", "userdef_val1", "user_val2"}, time.Now(), time.Time{}, int64(rand.Intn(1000)), checksumGen(25), nil, ""},
+	mockOS := NewMockStore(mockObjCtrl)
+	bs := buckets.BucketStore{O: mockOS}
 
-// 		// emulating objects.Put() returning err
-// 		{"mybucket", "myobject1", []string{"content-type1", "userdef_key1", "userdef_key2"}, []string{"foo1", "userdef_val1", "user_val2"}, time.Now(), time.Time{}, int64(rand.Intn(1000)), checksumGen(25), Error.New("some non nil error"), "Storj Gateway error: some non nil error"},
-// 	} {
-// 		errTag := fmt.Sprintf("Test case #%d", i)
+	storjObj := storjObjects{storj: &b}
 
-// 		var metadata = make(map[string]string)
-// 		for i := 0x00; i < len(example.MetaKey); i++ {
-// 			metadata[example.MetaKey[i]] = example.MetaVal[i]
-// 		}
+	for i, example := range []struct {
+		bucket, object string
+		MetaKey        []string
+		MetaVal        []string
+		Modified       time.Time
+		Expiration     time.Time
+		Size           int64
+		Checksum       string
+		err            error // used by mock function
+		errString      string
+	}{
+		// happy scenario
+		//{"mybucket", "myobject1", []string{"content-type", "userdef_key1", "userdef_key2"}, []string{"foo1", "userdef_val1", "user_val2"}, time.Now(), time.Time{}, int64(rand.Intn(1000)), checksumGen(25), nil, ""},
 
-// 		//metadata serialized
-// 		serMetaInfo := objects.SerializableMeta{
-// 			ContentType: metadata["content-type"],
-// 			UserDefined: metadata,
-// 		}
+		// emulating objects.Put() returning err
+		{"mybucket", "myobject1", []string{"content-type1", "userdef_key1", "userdef_key2"}, []string{"foo1", "userdef_val1", "user_val2"}, time.Now(), time.Time{}, int64(rand.Intn(1000)), checksumGen(25), Error.New("some non nil error"), "Storj Gateway error: some non nil error"},
+	} {
+		errTag := fmt.Sprintf("Test case #%d", i)
 
-// 		meta1 := objects.Meta{
-// 			serMetaInfo,
-// 			example.Modified,
-// 			example.Expiration,
-// 			example.Size,
-// 			example.Checksum,
-// 		}
+		var metadata = make(map[string]string)
+		for i := 0x00; i < len(example.MetaKey); i++ {
+			metadata[example.MetaKey[i]] = example.MetaVal[i]
+		}
 
-// 		if example.err != nil {
-// 			meta1 = objects.Meta{}
-// 		}
+		//metadata serialized
+		serMetaInfo := objects.SerializableMeta{
+			ContentType: metadata["content-type"],
+			UserDefined: metadata,
+		}
 
-// 		r, err := hash.NewReader(bytes.NewReader([]byte("abcdefgiiuweriiwyrwyiywrywhti")), int64(len("abcdefgiiuweriiwyrwyiywrywhti")), "e2fc714c4727ee9395f324cd2e7f331f", "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589")
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
+		meta1 := objects.Meta{
+			SerializableMeta: serMetaInfo,
+			Modified:         example.Modified,
+			Expiration:       example.Expiration,
+			Size:             example.Size,
+			Checksum:         example.Checksum,
+		}
 
-// 		/* for valid io.reader only */
-// 		mockGetObject.EXPECT().Put(gomock.Any(), paths.New(example.bucket, example.object), r, serMetaInfo, example.Expiration).Return(meta1, example.err).Times(1)
+		if example.err != nil {
+			meta1 = objects.Meta{}
+		}
 
-// 		objInfo, err := testUser.PutObject(context.Background(), example.bucket, example.object, r, metadata)
-// 		if err != nil {
-// 			assert.EqualError(t, err, example.errString)
-// 			if example.err != nil {
-// 				assert.Empty(t, objInfo, errTag)
-// 			}
-// 		} else {
-// 			assert.NoError(t, err, errTag)
-// 			assert.NotNil(t, objInfo, errTag)
-// 		}
-// 	}
-// }
+		r, err := hash.NewReader(bytes.NewReader([]byte("abcdefgiiuweriiwyrwyiywrywhti")), int64(len("abcdefgiiuweriiwyrwyiywrywhti")), "e2fc714c4727ee9395f324cd2e7f331f", "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		mockBS.EXPECT().GetObjectStore(gomock.Any(), example.bucket).Return(bs.O, example.err).Times(1)
+		/* for valid io.reader only */
+		mockOS.EXPECT().Put(gomock.Any(), paths.New(example.object), r, serMetaInfo, example.Expiration).Return(meta1, example.err).Times(1)
+		objInfo, err := storjObj.PutObject(context.Background(), example.bucket, example.object, r, metadata)
+		if err != nil {
+			assert.EqualError(t, err, example.errString)
+			if example.err != nil {
+				//assert.Empty(t, objInfo, errTag)
+			}
+		} else {
+			assert.NoError(t, err, errTag)
+			assert.NotNil(t, objInfo, errTag)
+		}
+	}
+}
 
 func TestGetObjectInfo(t *testing.T) {
 	mockBucketCtrl := gomock.NewController(t)
@@ -171,13 +179,13 @@ func TestGetObjectInfo(t *testing.T) {
 	mockObjCtrl := gomock.NewController(t)
 	defer mockObjCtrl.Finish()
 
-	mockBucketGetObject := mock_buckets.NewMockStore(mockBucketCtrl)
-	b := Storj{bs: mockBucketGetObject}
+	mockBS := mock_buckets.NewMockStore(mockBucketCtrl)
+	b := Storj{bs: mockBS}
 
-	mockObjGetObject := NewMockStore(mockObjCtrl)
-	bs := buckets.BucketStore{O: mockObjGetObject}
+	mockOS := NewMockStore(mockObjCtrl)
+	bs := buckets.BucketStore{O: mockOS}
 
-	BucketMock := storjObjects{storj: &b}
+	storjObj := storjObjects{storj: &b}
 
 	for i, example := range []struct {
 		bucket, object string
@@ -215,17 +223,17 @@ func TestGetObjectInfo(t *testing.T) {
 		}
 
 		meta1 := objects.Meta{
-			serMetaInfo,
-			example.Modified,
-			example.Expiration,
-			example.Size,
-			example.Checksum,
+			SerializableMeta: serMetaInfo,
+			Modified:         example.Modified,
+			Expiration:       example.Expiration,
+			Size:             example.Size,
+			Checksum:         example.Checksum,
 		}
 
-		mockBucketGetObject.EXPECT().GetObjectStore(gomock.Any(), example.bucket).Return(bs.O, nil).Times(1)
-		mockObjGetObject.EXPECT().Meta(gomock.Any(), paths.New(example.object)).Return(meta1, example.err).Times(1)
+		mockBS.EXPECT().GetObjectStore(gomock.Any(), example.bucket).Return(bs.O, nil).Times(1)
+		mockOS.EXPECT().Meta(gomock.Any(), paths.New(example.object)).Return(meta1, example.err).Times(1)
 
-		objInfo, err := BucketMock.GetObjectInfo(context.Background(), example.bucket, example.object)
+		objInfo, err := storjObj.GetObjectInfo(context.Background(), example.bucket, example.object)
 		if err != nil {
 			assert.EqualError(t, err, example.errString)
 			if example.err != nil {
