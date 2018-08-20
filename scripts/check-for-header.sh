5877983c59489366c19f4e2023eac5acaaddb8cf
Dennis Coyle <dennis@coyle.io>
2018-08-20 11:27:20 -0400

    cleanup

--- scripts/check-for-header.sh
+++ scripts/check-for-header.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=$(find $PWD -type f ! -path '*vendor/*' \( -iname '*.go' ! -iname "*.pb.go" \)) 
+FILES=$(find $PWD -type f ! -path '*vendor/*' \( -iname '*.go' ! -iname "*.pb.go" ! -iname "*mock.go" \)) 
 for i in $FILES
 do
   if ! grep -q 'Copyright' <<< "$(head -n 2 "$i")" 
