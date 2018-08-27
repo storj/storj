caa588d087edb5196b5bd4eb13bc43bd0bfdfd50
Egon Elbre <egonelbre@gmail.com>
2018-08-27 14:20:07 +0300

    try using GO111MODULE

--- scripts/travis-deps.sh
+++ scripts/travis-deps.sh
@@ -1,10 +1,5 @@
 set -x
 
-# checkout outside gopath
-git clone git@github.com:storj/storj.git $HOME/storj
-export TRAVIS_BUILD_DIR=$HOME/storj
-cd $TRAVIS_BUILD_DIR
-
 virtualenv my_py3 --python=/usr/bin/python3.4
 source my_py3/bin/activate
 pip install --upgrade awscli
