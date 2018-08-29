ebc99d1ceb2ed8945bbddbe3590020c818b0cb46
Cameron Ayer <camayer92@gmail.com>
2018-08-29 12:06:32 -0400

    extract bucketstore code from NewGateway into helper

--- pkg/miniogw/config.go
+++ pkg/miniogw/config.go
@@ -112,9 +112,9 @@ func (c Config) action(ctx context.Context, cliCtx *cli.Context,
 	return Error.New("unexpected minio exit")
 }
 
-// NewGateway creates a new minio Gateway
-func (c Config) NewGateway(ctx context.Context,
-	identity *provider.FullIdentity) (gw minio.Gateway, err error) {
+// GetBucketStore returns an implementation of buckets.Store
+func (c Config) GetBucketStore(ctx context.Context,
+	identity *provider.FullIdentity) (bs buckets.Store, err error) {
 	defer mon.Task()(&ctx)(&err)
 
 	t := transport.NewClient(identity)
@@ -151,5 +151,18 @@ func (c Config) NewGateway(ctx context.Context,
 	}
 	obj := objects.NewStore(stream)
 
-	return NewStorjGateway(buckets.NewStore(obj)), nil
+	return buckets.NewStore(obj), nil
+}
+
+// NewGateway creates a new minio Gateway
+func (c Config) NewGateway(ctx context.Context,
+	identity *provider.FullIdentity) (gw minio.Gateway, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	bs, err := c.GetBucketStore(ctx, identity)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewStorjGateway(bs), nil
 }
