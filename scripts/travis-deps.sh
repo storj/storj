ba89351c4f9c011132acbc9a2c254a9b24f60de5
Egon Elbre <egonelbre@gmail.com>
2018-08-29 14:09:01 +0300

    use correct options

--- scripts/travis-deps.sh
+++ scripts/travis-deps.sh
@@ -1,5 +1,10 @@
 set -x
 
+mkdir -p $GOPATH/src/storj.io
+mv $GOPATH/src/github.com/storj/storj $GOPATH/src/storj.io
+export TRAVIS_BUILD_DIR=$GOPATH/src/storj.io/storj
+cd $TRAVIS_BUILD_DIR
+
 pushd $HOME
 
 virtualenv my_py3 --python=/usr/bin/python3.4
