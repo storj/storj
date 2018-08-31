8e843d4b85bdee7667ac74caec0b805328b4113f
Egon Elbre <egonelbre@gmail.com>
2018-08-31 17:48:00 +0300

    generate mock

--- pkg/pointerdb/kvstore_mock_test.go
+++ pkg/pointerdb/kvstore_mock_test.go
@@ -107,6 +107,30 @@ func (mr *MockKeyValueStoreMockRecorder) IterateAll(arg0, arg1, arg2 interface{}
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IterateAll", reflect.TypeOf((*MockKeyValueStore)(nil).IterateAll), arg0, arg1, arg2)
 }
 
+// IterateReverse mocks base method
+func (m *MockKeyValueStore) IterateReverse(arg0, arg1 storage.Key, arg2 byte, arg3 func(storage.Iterator) error) error {
+	ret := m.ctrl.Call(m, "IterateReverse", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// IterateReverse indicates an expected call of IterateReverse
+func (mr *MockKeyValueStoreMockRecorder) IterateReverse(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IterateReverse", reflect.TypeOf((*MockKeyValueStore)(nil).IterateReverse), arg0, arg1, arg2, arg3)
+}
+
+// IterateReverseAll mocks base method
+func (m *MockKeyValueStore) IterateReverseAll(arg0, arg1 storage.Key, arg2 func(storage.Iterator) error) error {
+	ret := m.ctrl.Call(m, "IterateReverseAll", arg0, arg1, arg2)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// IterateReverseAll indicates an expected call of IterateReverseAll
+func (mr *MockKeyValueStoreMockRecorder) IterateReverseAll(arg0, arg1, arg2 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IterateReverseAll", reflect.TypeOf((*MockKeyValueStore)(nil).IterateReverseAll), arg0, arg1, arg2)
+}
+
 // List mocks base method
 func (m *MockKeyValueStore) List(arg0 storage.Key, arg1 storage.Limit) (storage.Keys, error) {
 	ret := m.ctrl.Call(m, "List", arg0, arg1)
