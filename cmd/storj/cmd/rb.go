d5b1000b8bfdad7c1809cb0797738bc28579e382
Cameron Ayer <camayer92@gmail.com>
2018-09-04 15:58:50 -0400

    rb command with bucket store functionality

--- cmd/storj/cmd/rb.go
+++ cmd/storj/cmd/rb.go
@@ -11,7 +11,9 @@ import (
 	"github.com/zeebo/errs"
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/process"
+	"storj.io/storj/pkg/storage/meta"
 	"storj.io/storj/pkg/utils"
+	"storj.io/storj/storage"
 )
 
 var (
@@ -32,13 +34,18 @@ func init() {
 func deleteBucket(cmd *cobra.Command, args []string) error {
 	ctx := process.Ctx(cmd)
 
-	if len(args) == 0 {
-		return errs.New("No bucket specified for deletion")
-	}
-
-	so, err := getStorjObjects(ctx, rbCfg)
+	identity, err := rbCfg.Load()
 	if err != nil {
 		return err
+	}
+
+	bs, err := rbCfg.GetBucketStore(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	if len(args) == 0 {
+		return errs.New("No bucket specified for deletion")
 	}
 
 	u, err := utils.ParseURL(args[0])
@@ -46,12 +53,34 @@ func deleteBucket(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	err = so.DeleteBucket(ctx, u.Host)
+	_, err = bs.Get(ctx, u.Host)
+	if err != nil {
+		if storage.ErrKeyNotFound.Has(err) {
+			return errs.New("Bucket: %s not found", u.Host)
+		}
+		return err
+	}
+
+	o, err := bs.GetObjectStore(ctx, u.Host)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Deleted %s", u.Host)
+	items, _, err := o.List(ctx, nil, nil, nil, true, 1, meta.None)
+	if err != nil {
+		return err
+	}
+
+	if len(items) > 0 {
+		return errs.New("Bucket: %s not empty", u.Host)
+	}
+
+	err = bs.Delete(ctx, u.Host)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Bucket: %s deleted", u.Host)
 
 	return nil
 }
