64d2ce9d68a4e52e611b9a9a762d4f6e8f835ca5
Cameron Ayer <camayer92@gmail.com>
2018-09-06 18:07:45 -0400

    split copy into helpers

--- cmd/storj/cmd/cp.go
+++ cmd/storj/cmd/cp.go
@@ -4,8 +4,10 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"path/filepath"
 	"time"
@@ -15,6 +17,7 @@ import (
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/paths"
 	"storj.io/storj/pkg/process"
+	"storj.io/storj/pkg/storage/buckets"
 	"storj.io/storj/pkg/storage/objects"
 	"storj.io/storj/pkg/utils"
 )
@@ -24,7 +27,7 @@ var (
 	cpCmd = &cobra.Command{
 		Use:   "cp",
 		Short: "Copies a local file or Storj object to another location locally or in Storj",
-		RunE:  copy,
+		RunE:  copyMain,
 	}
 )
 
@@ -34,7 +37,132 @@ func init() {
 	cpCmd.Flags().String("config", filepath.Join(defaultConfDir, "config.yaml"), "path to configuration")
 }
 
-func copy(cmd *cobra.Command, args []string) (err error) {
+// upload uploads args[0] from local machine to s3 compatible object args[1]
+func upload(ctx context.Context, bs buckets.Store, srcFile string, destObj *url.URL) error {
+	if destObj.Scheme == "" {
+		fmt.Println("Invalid destination")
+		return nil
+	}
+
+	// if object name not specified, default to filename
+	if destObj.Path == "" || destObj.Path == "/" {
+		destObj.Path = filepath.Base(srcFile)
+	}
+
+	f, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+
+	defer utils.LogClose(f)
+
+	o, err := bs.GetObjectStore(ctx, destObj.Host)
+	if err != nil {
+		return err
+	}
+
+	meta := objects.SerializableMeta{}
+	expTime := time.Time{}
+
+	_, err = o.Put(ctx, paths.New(destObj.Path), f, meta, expTime)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created: %s\n", destObj.Path)
+
+	return nil
+}
+
+// download downloads s3 compatible object args[0] to args[1] on local machine
+func download(ctx context.Context, bs buckets.Store, srcObj *url.URL, destFile string) error {
+	o, err := bs.GetObjectStore(ctx, srcObj.Host)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+
+	defer utils.LogClose(f)
+
+	rr, _, err := o.Get(ctx, paths.New(srcObj.Path))
+	if err != nil {
+		return err
+	}
+	defer utils.LogClose(rr)
+
+	r, err := rr.Range(ctx, 0, rr.Size())
+	if err != nil {
+		return err
+	}
+	defer utils.LogClose(r)
+
+	_, err = io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Downloaded %s to %s\n", srcObj.Path, destFile)
+
+	return nil
+}
+
+// copy copies s3 compatible object args[0] to s3 compatible object args[1]
+func copy(ctx context.Context, bs buckets.Store, srcObj *url.URL, destObj *url.URL) error {
+	o, err := bs.GetObjectStore(ctx, srcObj.Host)
+	if err != nil {
+		return err
+	}
+
+	rr, _, err := o.Get(ctx, paths.New(srcObj.Path))
+	if err != nil {
+		return err
+	}
+	defer utils.LogClose(rr)
+
+	r, err := rr.Range(ctx, 0, rr.Size())
+	if err != nil {
+		return err
+	}
+	defer utils.LogClose(r)
+
+	o, err = bs.GetObjectStore(ctx, destObj.Host)
+	if err != nil {
+		return err
+	}
+
+	meta := objects.SerializableMeta{}
+	expTime := time.Time{}
+
+	// if destination object name not specified, default to source object name
+	if destObj.Path == "" || destObj.Path == "/" {
+		destObj.Path = srcObj.Path
+	}
+
+	_, err = o.Put(ctx, paths.New(destObj.Path), r, meta, expTime)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s copied to %s\n", srcObj.Host+srcObj.Path, destObj.Host+destObj.Path)
+
+	return nil
+}
+
+// copyMain is the function executed when cpCmd is called
+func copyMain(cmd *cobra.Command, args []string) (err error) {
+	if len(args) == 0 {
+		fmt.Println("No object specified for copy")
+		return nil
+	}
+	if len(args) == 1 {
+		fmt.Println("No destination specified")
+		return nil
+	}
+
 	ctx := process.Ctx(cmd)
 
 	identity, err := cpCfg.Load()
@@ -45,16 +173,6 @@ func copy(cmd *cobra.Command, args []string) (err error) {
 	bs, err := cpCfg.GetBucketStore(ctx, identity)
 	if err != nil {
 		return err
-	}
-
-	if len(args) == 0 {
-		fmt.Println("No file specified for copy")
-		return nil
-	}
-
-	if len(args) == 1 {
-		fmt.Println("No destination specified")
-		return nil
 	}
 
 	u0, err := utils.ParseURL(args[0])
@@ -69,109 +187,14 @@ func copy(cmd *cobra.Command, args []string) (err error) {
 
 	// if uploading
 	if u0.Scheme == "" {
-		if u1.Scheme == "" {
-			fmt.Println("Invalid destination")
-			return nil
-		}
-
-		// if object name not specified, default to filename
-		if u1.Path == "" || u1.Path == "/" {
-			u1.Path = filepath.Base(args[0])
-		}
-
-		f, err := os.Open(args[0])
-		if err != nil {
-			return err
-		}
-
-		defer utils.LogClose(f)
-
-		o, err := bs.GetObjectStore(ctx, u1.Host)
-		if err != nil {
-			return err
-		}
-
-		meta := objects.SerializableMeta{}
-		expTime := time.Time{}
-
-		_, err = o.Put(ctx, paths.New(u1.Path), f, meta, expTime)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Created: %s\n", u1.Path)
-
-		return nil
-	}
-
-	o, err := bs.GetObjectStore(ctx, u0.Host)
-	if err != nil {
-		return err
+		return upload(ctx, bs, args[0], u1)
 	}
 
 	// if downloading
 	if u1.Scheme == "" {
-		f, err := os.Create(args[1])
-		if err != nil {
-			return err
-		}
-
-		defer utils.LogClose(f)
-
-		rr, _, err := o.Get(ctx, paths.New(u0.Path))
-		if err != nil {
-			return err
-		}
-		defer utils.LogClose(rr)
-
-		r, err := rr.Range(ctx, 0, rr.Size())
-		if err != nil {
-			return err
-		}
-		defer utils.LogClose(r)
-
-		_, err = io.Copy(f, r)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Downloaded %s to %s\n", u0.Path, args[1])
-
-		return nil
+		return download(ctx, bs, u0, args[1])
 	}
 
 	// if copying from one remote location to another
-	rr, _, err := o.Get(ctx, paths.New(u0.Path))
-	if err != nil {
-		return err
-	}
-	defer utils.LogClose(rr)
-
-	r, err := rr.Range(ctx, 0, rr.Size())
-	if err != nil {
-		return err
-	}
-	defer utils.LogClose(r)
-
-	o, err = bs.GetObjectStore(ctx, u1.Host)
-	if err != nil {
-		return err
-	}
-
-	meta := objects.SerializableMeta{}
-	expTime := time.Time{}
-
-	// if destination object name not specified, default to source object name
-	if u1.Path == "" || u1.Path == "/" {
-		u1.Path = u0.Path
-	}
-
-	_, err = o.Put(ctx, paths.New(u1.Path), r, meta, expTime)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("%s copied to %s\n", u0.Host+u0.Path, u1.Host+u1.Path)
-
-	return nil
+	return copy(ctx, bs, u0, u1)
 }
