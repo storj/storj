5b23b6871c3c788e16bcba26267ade15234ac891
James Hagans <jhagans3@gmail.com>
2018-09-17 22:28:47 -0400

    add meta test

---
+++ pkg/storage/streams/store_test.go
@@ -0,0 +1,101 @@
+package streams
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"storj.io/storj/pkg/paths"
+	ranger "storj.io/storj/pkg/ranger"
+	"storj.io/storj/pkg/storage/segments"
+)
+
+var ctx = context.Background()
+
+type metaClosure = func(context.Context, paths.Path) (segments.Meta, error)
+type getClosure = func(context.Context, paths.Path) (ranger.Ranger, segments.Meta, error)
+type putClosure = func(context.Context, paths.Path, io.Reader, []byte, time.Time) (segments.Meta, error)
+type deleteClosure = func(context.Context, paths.Path) error
+type listClosure = func(context.Context, paths.Path, paths.Path, paths.Path, bool, int, uint32) ([]ListItem, bool, error)
+
+type segmentStub struct {
+	mc metaClosure
+	gc getClosure
+	pc putClosure
+	dc deleteClosure
+	lc listClosure
+}
+
+func (s *segmentStub) Meta(ctx context.Context, path paths.Path) (meta segments.Meta, err error) {
+	return meta, err
+}
+
+func (s *segmentStub) Get(ctx context.Context, path paths.Path) (rr ranger.Ranger,
+	meta segments.Meta, err error) {
+	return rr, meta, err
+}
+
+func (s *segmentStub) Put(ctx context.Context, path paths.Path, data io.Reader, metadata []byte,
+	expiration time.Time) (meta segments.Meta, err error) {
+	return meta, err
+}
+func (s *segmentStub) Delete(ctx context.Context, path paths.Path) (err error) {
+	return err
+}
+func (s *segmentStub) List(ctx context.Context, prefix, startAfter, endBefore paths.Path,
+	recursive bool, limit int, metaFlags uint32) (items []segments.ListItem,
+	more bool, err error) {
+	return items, more, err
+}
+
+func TestMeta(t *testing.T) {
+
+	errorFn := func(ctx context.Context, path paths.Path) (segments.Meta, error) {
+		return segments.Meta{}, errors.New("an error")
+	}
+	emptyFn := func(ctx context.Context, path paths.Path) (segments.Meta, error) {
+		return segments.Meta{
+			Modified:   time.Time{},
+			Expiration: time.Time{},
+			Size:       0,
+			Data:       []byte(""),
+		}, nil
+	}
+	metaFn := func(ctx context.Context, path paths.Path) (segments.Meta, error) {
+		return segments.Meta{
+			Modified:   time.Now(),
+			Expiration: time.Now(),
+			Size:       100,
+			Data:       []byte("data"),
+		}, nil
+	}
+
+	metaSlice := []metaClosure{
+		errorFn,
+		emptyFn,
+		metaFn,
+	}
+
+	for i, closure := range metaSlice {
+		errTag := fmt.Sprintf("Test case #%d", i)
+
+		segment := &segmentStub{}
+		segment.mc = closure
+
+		stream, err := NewStreamStore(segment, int64(10))
+		if err != nil {
+			assert.Empty(t, stream, errTag)
+			t.Fatal(err)
+		}
+
+		meta, err := stream.Meta(ctx, paths.New("bucket"))
+		if err != nil {
+			assert.Empty(t, meta, errTag)
+			t.Fatal(err)
+		}
+	}
+}
