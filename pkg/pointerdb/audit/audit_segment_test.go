3b771cacf5b0133740f8a654cbaf1483c28fb47f
nadine farah <nfarah86@gmail.com>
2018-09-21 00:12:21 -0700

    working on nil  reference for testing

--- pkg/pointerdb/audit/audit_segment_test.go
+++ pkg/pointerdb/audit/audit_segment_test.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+
 	p "storj.io/storj/pkg/paths"
+	"storj.io/storj/pkg/pb"
 	pdbclient "storj.io/storj/pkg/pointerdb/pdbclient"
 	"storj.io/storj/pkg/provider"
 )
@@ -40,7 +42,7 @@ func TestMain(m *testing.M) {
 	}
 
 	client, err := pdbclient.NewClient(identity, pointerdbClientPort, APIKey)
-
+	fmt.Println("this is  the client: ", client)
 	if err != nil {
 		log.Fatal("Failed to dial: ", zap.Error(err))
 		os.Exit(1)
@@ -50,9 +52,40 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func makePointer(path p.Path, auth []byte) pb.PutRequest {
+	var rps []*pb.RemotePiece
+	rps = append(rps, &pb.RemotePiece{
+		PieceNum: 1,
+		NodeId:   "testId",
+	})
+	pr := pb.PutRequest{
+		Path: path.String(),
+		Pointer: &pb.Pointer{
+			Type: pb.Pointer_REMOTE,
+			Remote: &pb.RemoteSegment{
+				Redundancy: &pb.RedundancyScheme{
+					Type:             pb.RedundancyScheme_RS,
+					MinReq:           1,
+					Total:            3,
+					RepairThreshold:  2,
+					SuccessThreshold: 3,
+				},
+				PieceId:      "testId",
+				RemotePieces: rps,
+			},
+			Size: int64(1),
+		},
+		APIKey: auth,
+	}
+	return pr
+}
+
 func TestList(t *testing.T) {
+
 	tests := []struct {
 		bm         string
+		path       p.Path
+		APIKey     []byte
 		startAfter p.Path
 		limit      int
 		items      []pdbclient.ListItem
@@ -61,7 +94,9 @@ func TestList(t *testing.T) {
 	}{
 		{
 			bm:         "should fail with no limit given",
-			startAfter: p.New("file1/file2"),
+			path:       p.New("file1/file2"),
+			APIKey:     []byte("abc123"),
+			startAfter: p.New("file3/file4"),
 			limit:      0,
 			items:      nil,
 			more:       false,
@@ -69,20 +104,38 @@ func TestList(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
+	// add  values to db so we can list
+
+	for i, tt := range tests {
 		t.Run(tt.bm, func(t *testing.T) {
 			assert := assert.New(t)
+			errTag := fmt.Sprintf("Test case #%d", i)
 
-			a := NewAudit(client)
-			items, more, err := a.List(ctx, tt.startAfter, tt.limit)
+			//a := NewAudit(client)
+
+			// create a pointer and put in db
+			fmt.Println("this is  the client again: ", client)
+			putRequest := makePointer(tt.path, tt.APIKey)
+			fmt.Println(putRequest)
+
+			err := client.Put(ctx, tt.path, putRequest.Pointer)
+
+			if err != nil {
+				assert.NotNil(t, err, errTag)
+			} else {
+				assert.Nil(t, err, errTag)
+			}
+
+			// call LIST
+			//items, more, err := a.List(ctx, tt.startAfter, tt.limit)
 
 			// if err != nil {
-			assert.NotNil(err)
-			// 	assert.Equal(tt.err, err.Error())
+			//assert.NotNil(err)
+			//assert.Equal(tt.err, tt.err)
 			// 	t.Errorf("Error: %s", err.Error())
 			// }
 
-			fmt.Println(items, more, err)
+			//fmt.Println(err)
 			// write rest of  test
 		})
 	}
