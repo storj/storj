97cbacb299b5a23cbacb235321426b7d8b27f8ef
aligeti <34487396+aligeti@users.noreply.github.com>
2018-09-25 12:49:55 -0400

    storage node quick check and startup validation (#368)
    * storage node quick check and startup validation
    
    *  rearranged the startup validation and quick check logic
    
    * travis lint warning fixes
    
    * travis lint warning fixes
    
    * travis lint warning fixes
    
    * code changes per review comments
    
    * code clean dev debug info
    
    * travis lint wranings
    
    * code changes per code review comments
    
    * code changes per code review comments
    
    * code update per review
    
    * sqlite SUM is having issue when getting the SUM of an empty column; filepath was checking a directory that doesn't exist when starting server; Example updated to print allocated and used space
    
    * storage node quick check and startup validation
    
    *  rearranged the startup validation and quick check logic
    
    * travis lint warning fixes
    
    * travis lint warning fixes
    
    * travis lint warning fixes
    
    * code changes per review comments
    
    * code clean dev debug info
    
    * travis lint wranings
    
    * code changes per code review comments
    
    * code changes per code review comments
    
    * code update per review
    
    * no file or directory error
    
    * Updated mock PSClient

--- pkg/piecestore/rpc/server/server.go
+++ pkg/piecestore/rpc/server/server.go
@@ -6,13 +6,16 @@ package server
 import (
 	"crypto"
 	"crypto/ecdsa"
+	"errors"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 
 	"github.com/gtank/cryptopasta"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/zeebo/errs"
+	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	"gopkg.in/spacemonkeygo/monkit.v2"
 
@@ -32,7 +35,8 @@ var (
 
 // Config contains everything necessary for a server
 type Config struct {
-	Path string `help:"path to store data in" default:"$CONFDIR"`
+	Path               string `help:"path to store data in" default:"$CONFDIR"`
+	AllocatedDiskSpace int64  `help:"total allocated disk space, default(1GB)" default:"1073741824"`
 }
 
 // Run implements provider.Responsibility
@@ -53,11 +57,30 @@ func (c Config) Run(ctx context.Context, server *provider.Provider) (err error) 
 	return server.Run(ctx)
 }
 
+//DirSize returns the total size of the files in that directory
+func DirSize(path string) (int64, error) {
+	var size int64
+	_, err := os.Stat(path)
+	if err != nil {
+		return 0, errors.New("path doesn't exists")
+	}
+	adjSize := func(_ string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	}
+	err = filepath.Walk(path, adjSize)
+
+	return size, err
+}
+
 // Server -- GRPC server meta data used in route calls
 type Server struct {
-	DataDir string
-	DB      *psdb.DB
-	pkey    crypto.PrivateKey
+	DataDir        string
+	DB             *psdb.DB
+	pkey           crypto.PrivateKey
+	totalAllocated int64
 }
 
 // Initialize -- initializes a server struct
@@ -65,12 +88,55 @@ func Initialize(ctx context.Context, config Config, pkey crypto.PrivateKey) (*Se
 	dbPath := filepath.Join(config.Path, "piecestore.db")
 	dataDir := filepath.Join(config.Path, "piece-store-data")
 
+	// read the allocated disk space from the config file
+	allocatedDiskSpace := config.AllocatedDiskSpace
+
+	// get the disk space details
+	// The returned path ends in a slash only if it represents a root directory, such as "/" on Unix or `C:\` on Windows.
+	rootPath := filepath.Dir(filepath.Clean(config.Path))
+	diskSpace, err := disk.Usage(rootPath)
+	if err != nil {
+		return nil, ServerError.Wrap(err)
+	}
+	freeDiskSpace := int64(diskSpace.Free)
+
 	db, err := psdb.Open(ctx, dataDir, dbPath)
 	if err != nil {
-		return nil, err
+		return nil, ServerError.Wrap(err)
 	}
 
-	return &Server{DataDir: dataDir, DB: db, pkey: pkey}, nil
+	// get how much is currently used, if for the first time totalUsed = 0
+	totalUsed, err := db.SumTTLSizes()
+	if err != nil {
+		//first time setup
+		totalUsed = 0x00
+	}
+
+	// check your hard drive is big enough
+	// first time setup as a piece node server
+	if (totalUsed == 0x00) && (freeDiskSpace < allocatedDiskSpace) {
+		allocatedDiskSpace = freeDiskSpace
+		zap.S().Warnf("Disk space is less than requested allocated space, allocating = %d Bytes", allocatedDiskSpace)
+		return &Server{DataDir: dataDir, DB: db, pkey: pkey, totalAllocated: allocatedDiskSpace}, nil
+	}
+
+	// on restarting the Piece node server, assuming already been working as a node
+	// used above the alloacated space, user changed the allocation space setting
+	// before restarting
+	if totalUsed >= allocatedDiskSpace {
+		zap.S().Warnf("Used more space then allocated, allocating = %d Bytes", allocatedDiskSpace)
+		return &Server{DataDir: dataDir, DB: db, pkey: pkey, totalAllocated: allocatedDiskSpace}, nil
+	}
+
+	// the available diskspace is less than remaining allocated space,
+	// due to change of setting before restarting
+	if freeDiskSpace < (allocatedDiskSpace - totalUsed) {
+		allocatedDiskSpace = freeDiskSpace
+		zap.S().Warnf("Disk space is less than requested allocated space, allocating = %d Bytes", allocatedDiskSpace)
+		return &Server{DataDir: dataDir, DB: db, pkey: pkey, totalAllocated: allocatedDiskSpace}, nil
+	}
+
+	return &Server{DataDir: dataDir, DB: db, pkey: pkey, totalAllocated: allocatedDiskSpace}, nil
 }
 
 // Stop the piececstore node
@@ -120,7 +186,7 @@ func (s *Server) Stats(ctx context.Context, in *pb.StatsReq) (*pb.StatSummary, e
 		return nil, err
 	}
 
-	return &pb.StatSummary{UsedSpace: totalUsed, AvailableSpace: 0}, nil
+	return &pb.StatSummary{UsedSpace: totalUsed, AvailableSpace: (s.totalAllocated - totalUsed)}, nil
 }
 
 // Delete -- Delete data by Id from piecestore
