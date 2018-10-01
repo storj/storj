430ccb634fb81925a7545975d08649d233e68506
Moby von Briesen <mobyvb@gmail.com>
2018-10-01 12:18:05 -0400

    add statdb client class

--- pkg/statdb/config.go
+++ pkg/statdb/config.go
@@ -9,22 +9,26 @@ import (
 	"go.uber.org/zap"
 
 	"storj.io/storj/pkg/provider"
+	pb "storj.io/storj/pkg/statdb/proto"
 )
 
 // Config is a configuration struct that is everything you need to start a
 // StatDB responsibility
 type Config struct {
-	DatabaseURL    string `help:"the database connection string to use" default:"sqlite3://$CONFDIR/stats.db"`
+	DatabaseURL    string `help:"the database connection string to use" default:"$CONFDIR/stats.db"`
 	DatabaseDriver string `help:"the database driver to use" default:"sqlite3"`
 }
 
 // Run implements the provider.Responsibility interface
 func (c Config) Run(ctx context.Context, server *provider.Provider) error {
-	_, err := NewServer(c.DatabaseDriver, c.DatabaseURL, zap.L())
+	// TODO(moby) use struct for NewServer args
+	ns, err := NewServer("sqlite3", "stats.db", zap.L())
 	if err != nil {
 		return err
 	}
 
-	// TODO(moby) defer closing server?
+	pb.RegisterStatDBServer(server.GRPC(), ns)
+
+	// TODO(moby) defer closing statdb server?
 	return server.Run(ctx)
 }
