4a3010f5957d84da2a87813773f3a12c4c97702b
Cameron Ayer <camayer92@gmail.com>
2018-10-02 14:19:59 -0400

    remove 24 hour interval

--- pkg/datarepair/repair.go
+++ pkg/datarepair/repair.go
@@ -5,7 +5,6 @@ package datarepair
 
 import (
 	"context"
-	"time"
 
 	"gopkg.in/spacemonkeygo/monkit.v2"
 
@@ -34,15 +33,14 @@ func (c *Config) Run(ctx context.Context) (err error) {
 
 	// Initialize Repairer with queue
 	_, err = repairer.Initialize(ctx, queue)
-
-	ticker := time.NewTicker(24 * time.Hour)
-	for {
-		select {
-		case <-ticker.C:
-			// TODO: Run the Checker in goroutine
-			// TODO: Run the Repairer in goroutine
-		}
+	if err != nil {
+		return err
 	}
+
+	// TODO: Run the Checker in goroutine
+	// TODO: Run the Repairer in goroutine
+
 	// TODO: defer stop of checker and repairer
 
+	return err
 }
