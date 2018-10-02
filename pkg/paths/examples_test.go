425e8e8e3f801b03118f1d3ae37c7c28008bc7e8
Egon Elbre <egonelbre@gmail.com>
2018-10-02 10:39:01 +0300

    remove bip39 dependency

--- pkg/paths/examples_test.go
+++ pkg/paths/examples_test.go
@@ -7,23 +7,16 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/tyler-smith/go-bip39"
-
 	"storj.io/storj/pkg/paths"
 )
 
 func ExamplePath_Encrypt() {
-	const mnemonic = "style inspire blade just ignore expose midnight maze " +
-		"boring code burst host giraffe face parent basic ritual distance " +
-		"trophy join relief hidden fine yard"
-
 	var path = paths.New("fold1/fold2/fold3/file.txt")
 
-	// create new seed from mnemonic
-	fmt.Println("mnemonic:", mnemonic)
-	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, "")
-	if err != nil {
-		panic(err)
+	// seed
+	seed := make([]byte, 64)
+	for i := range seed {
+		seed[i] = byte(i)
 	}
 	fmt.Printf("root key (%d bytes): %s\n", len(seed), hex.EncodeToString(seed))
 
@@ -62,13 +55,12 @@ func ExamplePath_Encrypt() {
 	fmt.Println("path in Bytes is: ", pathBytes)
 
 	// Output:
-	// mnemonic: style inspire blade just ignore expose midnight maze boring code burst host giraffe face parent basic ritual distance trophy join relief hidden fine yard
-	// root key (64 bytes): 2cc5ebc6ea4c76be8e7347e7cefd0a36069ee14376fd5bc31350648e66999ae40765ea65e8239f9f63f90c7329d5a801b53592fd08016a426812adb625471ace
+	// root key (64 bytes): 000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f
 	// path to encrypt: fold1/fold2/fold3/file.txt
-	// encrypted path:  1IxvMgcKDrllZs0fTSsCHTyB5LAoa/1lUAp4w2L7DKTPaW7ROooO19Al6Ai/1-7AAlcbkD5Gc6nCDq_Hb2sUNJEuL/1VNDpswHFmSY_3SU5_l3Ai5APPJ9aWIxR
+	// encrypted path:  1ziIKg8Mw9_ywBqSOm78gQ9aQVbrQ/1FWdS4WUzuWXBrNML_FzGH8O2usos/1TKa8xYYCrUBEvEGj7YENdwViZOYh/17JWesXCBVU9Nx8IdnwuNUuwqgGFKL_jH
 	// decrypted path:  fold1/fold2/fold3/file.txt
-	// shared path:     1-7AAlcbkD5Gc6nCDq_Hb2sUNJEuL/1VNDpswHFmSY_3SU5_l3Ai5APPJ9aWIxR
-	// derived key (64 bytes): 367f581e49f0a501b64def5ec2a835d7193038c35a7562209c7015a1ee00e5c15e1aa41b5b7b5019ee906d5b76cee84b220fc6b900a55337a245071dbbc72c8f
+	// shared path:     1TKa8xYYCrUBEvEGj7YENdwViZOYh/17JWesXCBVU9Nx8IdnwuNUuwqgGFKL_jH
+	// derived key (64 bytes): 2592f0694bc72a2719d09b7192b9b8f9e2517fda71419314d93a7c96844f28763ed3b829f3c9a09e812b402a66b1b0a832ae3cdd7435b7b496ca95eec108c629
 	// decrypted path:  fold3/file.txt
 	// path in Bytes is:  [102 111 108 100 49 47 102 111 108 100 50 47 102 111 108 100 51 47 102 105 108 101 46 116 120 116]
 }
