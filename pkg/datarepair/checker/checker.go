f7afafe4e1a2827f26981e720dab9d675093e549
Dylan Lott <dylan@storj.io>
2018-10-04 15:40:34 -0600

    Repair cron (#420)
    * Creates cron-job for checker, adds it to captplanet and satellite
    
    * removes datarepair from satellite & captplanet run
    
    * Delete config.go
    
    * removes unused datarepair imports
    
    * adds comments to fix linter

--- pkg/datarepair/checker/checker.go
+++ pkg/datarepair/checker/checker.go
@@ -5,17 +5,48 @@ package checker
 
 import (
 	"context"
+	"time"
+
+	"github.com/zeebo/errs"
+	"go.uber.org/zap"
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+
+	"storj.io/storj/pkg/provider"
+)
+
+var (
+	mon = monkit.Package()
+	// Error is a standard error class for this package.
+	Error = errs.Class("checker error")
 )
 
 // Config contains configurable values for checker
 type Config struct {
-	// queueAddress string `help:"data repair queue address" default:"localhost:7777"`
+	Interval time.Duration `help:"how frequently checker should audit segments" default:"30s"`
 }
 
 // Run runs the checker with configured values
-func (c *Config) Run(ctx context.Context) (err error) {
+func (c Config) Run(ctx context.Context, server *provider.Provider) (err error) {
+	defer mon.Task()(&ctx)(&err)
 
-	// TODO: start checker server
+	zap.S().Info("Checker is starting up")
 
-	return err
+	ticker := time.NewTicker(c.Interval)
+	defer ticker.Stop()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				zap.S().Info("Starting segment checker service")
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return server.Run(ctx)
 }
