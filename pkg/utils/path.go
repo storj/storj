9ea647b8df5a3adcf66f43347497c64e18b1eea1
stefanbenten <mail@stefan-benten.de>
2018-10-04 16:07:00 +0200

    Hide struct properties to prevent manual interaction

--- pkg/utils/path.go
+++ pkg/utils/path.go
@@ -13,9 +13,9 @@ import (
 )
 
 type FPath struct {
-	Local  bool
-	Schema string
-	Path   string
+	local  bool
+	schema string
+	path   string
 }
 
 // Creates new Struct from handed over URL
@@ -26,11 +26,11 @@ func (p *FPath) New(url string) {
 
 	switch len(sepstring) {
 	case 2: // Has Schema
-		p.Schema = sepstring[0]
-		p.Path = sepstring[1]
+		p.schema = sepstring[0]
+		p.path = sepstring[1]
 	case 1: // No Schema
-		p.Local = true
-		p.Path = sepstring[0]
+		p.local = true
+		p.path = sepstring[0]
 	default: // Everything else is misformatted
 		log.Fatalf("misformatted URL: %v", url)
 	}
@@ -39,31 +39,31 @@ func (p *FPath) New(url string) {
 	cprefix, _ := regexp.Compile(`^\\\\\?\\(UNC\\)?`)
 
 	// when Prefix present, omit further changes to the path
-	if prefix := cprefix.FindString(p.Path); prefix != "" {
-		p.Schema = prefix
-		p.Path = strings.Replace(p.Path, prefix, "", -1) //Strip Prefix
+	if prefix := cprefix.FindString(p.path); prefix != "" {
+		p.schema = prefix
+		p.path = strings.Replace(p.path, prefix, "", -1) //Strip Prefix
 	} else {
 		// when file is local, ensure path absolute
-		if p.IsLocal() && !filepath.IsAbs(p.Path) {
-			p.Path, _ = filepath.Abs(p.Path)
+		if p.IsLocal() && !filepath.IsAbs(p.path) {
+			p.path, _ = filepath.Abs(p.path)
 		}
 	}
 }
 
 // Joins/appends segment to the path
 func (p FPath) Join(segment string) FPath {
-	p.Path = filepath.Join(p.Path, segment)
+	p.path = filepath.Join(p.path, segment)
 	return p
 }
 
 // Returns the last folder of Path
 func (p FPath) Folder() string {
-	return filepath.Dir(p.Path)
+	return filepath.Dir(p.path)
 }
 
 // Returns if Path is a folder
 func (p FPath) IsFolder() bool {
-	fileInfo, err := os.Stat(p.Path)
+	fileInfo, err := os.Stat(p.path)
 	if err != nil {
 		//fmt.Println(err)
 		return false
@@ -73,35 +73,45 @@ func (p FPath) IsFolder() bool {
 
 // Returns Base of Path
 func (p FPath) Base() string {
-	return filepath.Base(p.Path)
+	return filepath.Base(p.path)
 }
 
 // Returns whether URL refers to local or remote location
 func (p FPath) IsLocal() bool {
-	return p.Local
+	return p.local
 }
 
 // Returns if URL had a schema
 func (p FPath) HasSchema() bool {
-	return p.Schema != ""
+	return p.schema != ""
 }
 
-// Returns entire URL
+// Returns Schema if existing
+func (p FPath) Schema() string {
+	return p.schema
+}
+
+// Returns Path
+func (p FPath) Path() string {
+	return p.path
+}
+
+// Returns entire URL including Schema
 func (p FPath) String() string {
 	var cpl string
 
 	switch runtime.GOOS {
 	case "windows":
-		if p.Schema != "" {
-			cpl = p.Schema + "://" + p.Path
+		if p.schema != "" {
+			cpl = p.schema + "://" + p.path
 		} else {
-			cpl = p.Path
+			cpl = p.path
 		}
 	default:
-		if p.Schema != "" {
-			cpl = p.Schema + "://" + p.Path
+		if p.schema != "" {
+			cpl = p.schema + "://" + p.path
 		} else {
-			cpl = p.Path
+			cpl = p.path
 		}
 	}
 	return cpl
