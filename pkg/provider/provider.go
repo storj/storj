70caa86d89c94ac00b56cfad51d25724ce3e79da
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2018-10-09 10:29:00 +0300

    Reduce garbage errors in captplanet when streaming (#435)

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -5,6 +5,7 @@ package provider
 
 import (
 	"context"
+	"io"
 	"net"
 	"time"
 
@@ -119,9 +120,11 @@ func streamInterceptor(srv interface{}, ss grpc.ServerStream,
 	info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
 	err = handler(srv, ss)
 	if err != nil {
-		// modified for wrong file downloads
-		// to not zap errors
-		if storage.ErrKeyNotFound.Has(err) {
+		// no zap errors for canceled or wrong file downloads
+		if storage.ErrKeyNotFound.Has(err) ||
+			status.Code(err) == codes.Canceled ||
+			status.Code(err) == codes.Unavailable ||
+			err == io.EOF {
 			return err
 		}
 		zap.S().Errorf("%+v", err)
@@ -134,8 +137,7 @@ func unaryInterceptor(ctx context.Context, req interface{},
 	err error) {
 	resp, err = handler(ctx, req)
 	if err != nil {
-		// modified for wrong file downloads
-		// to not zap errors
+		// no zap errors for wrong file downloads
 		if status.Code(err) == codes.NotFound {
 			return resp, err
 		}
