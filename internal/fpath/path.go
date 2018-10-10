305b2ee97bd4ad402b3343a85469535689f936af
Kaloyan Raev <kaloyan@storj.io>
2018-10-10 16:06:35 +0300

    Improve comments and error messages

--- internal/fpath/path.go
+++ internal/fpath/path.go
@@ -4,7 +4,6 @@
 package fpath
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -13,7 +12,7 @@ import (
 	"strings"
 )
 
-// FPath creates an OS independently Path Handling Structure
+// FPath is an OS independently path handling structure
 type FPath struct {
 	local  bool   //set if file is local
 	scheme string //url scheme
@@ -21,59 +20,55 @@ type FPath struct {
 	path   string //filepath OR remote path (without bucket)
 }
 
-// New creates Struct from handed over URL
+// New creates new FPath from the given URL
 func New(url string) (p FPath, err error) {
-
-	// Check for Schema
+	// Check for schema
 	sepstring := strings.SplitN(url, "://", 2)
 
 	switch len(sepstring) {
-	case 2: // Has Schema
+	case 2: // Has scheme
 		p.scheme = sepstring[0]
 		if p.scheme != "sj" {
-			return FPath{}, errors.New("unsupported URL scheme")
+			return FPath{}, fmt.Errorf("unsupported URL scheme: %s", p.scheme)
 		}
-		//Trim initial / of the path and clean it, afterwards split on first /
+		// Trim initial slash of the path and clean it, afterwards split on first slash
 		split := strings.SplitN(path.Clean(strings.TrimLeft(sepstring[1], "/")), "/", 2)
 		p.bucket = split[0]
 		if len(split) == 2 {
 			p.path = filepath.ToSlash(split[1])
 		}
-	case 1: // No Scheme
+	case 1: // No scheme
 		p.local = true
 		p.path = sepstring[0]
 	default: // Everything else is malformed
 		return FPath{}, fmt.Errorf("malformed URL: %s", url)
 	}
 
-	// Check for Windows Special Handling Prefix
+	// Check for Universal Naming Convention path (Windows)
 	cprefix, err := regexp.Compile(`^\\\\\?\\(UNC\\)?`)
 	if err != nil {
 		return FPath{}, err
 	}
 
-	// when Prefix present, omit further changes to the path
+	// If UNC prefix is present, omit further changes to the path
 	if prefix := cprefix.FindString(p.path); prefix != "" {
-
 		p.scheme = prefix
-		p.path = strings.Replace(p.path, prefix, "", 1) //Strip Prefix
+		p.path = strings.Replace(p.path, prefix, "", 1) // strip prefix
 		return p, nil
 	}
 
-	// when file is local, ensure path absolute
+	// if file is local, ensure path is absolute
 	if p.IsLocal() && !filepath.IsAbs(p.path) {
-
 		fullpath, err := filepath.Abs(p.path)
 		if err != nil {
 			return FPath{}, fmt.Errorf("unable to create absolute path for: %s", p.path)
 		}
-
 		p.path = fullpath
 	}
 	return p, nil
 }
 
-// Join is merging segment to the path
+// Join is appends the given segment to the path
 func (p *FPath) Join(segment string) *FPath {
 	p.path = filepath.Join(p.path, segment)
 	if !p.local {
@@ -91,23 +86,22 @@ func (p FPath) Folder() string {
 func (p FPath) IsFolder() bool {
 	fileInfo, err := os.Stat(p.path)
 	if err != nil {
-		//fmt.Println(err)
 		return false
 	}
 	return fileInfo.IsDir()
 }
 
-// Base returns Base Segment of the path
+// Base returns the last segment of the path
 func (p FPath) Base() string {
 	return filepath.Base(p.path)
 }
 
-// Bucket returns first segment of path
+// Bucket returns the first segment of path
 func (p FPath) Bucket() string {
 	return p.bucket
 }
 
-// BucketPath returns Path including prefixed bucket
+// BucketPath returns path prepended with the bucket name
 func (p FPath) BucketPath() string {
 	if !p.local && p.bucket != "" {
 		return p.bucket + "/" + p.path
@@ -115,27 +109,27 @@ func (p FPath) BucketPath() string {
 	return ""
 }
 
-// Path returns the URL path without schema
+// Path returns the URL path without the scheme
 func (p FPath) Path() string {
 	return p.path
 }
 
-// IsLocal returns whether URL refers to local or remote location
+// IsLocal returns whether the path refers to local or remote location
 func (p FPath) IsLocal() bool {
 	return p.local
 }
 
-// HasScheme returns if URL had a schema
+// HasScheme returns if the URL had a scheme
 func (p FPath) HasScheme() bool {
 	return p.scheme != ""
 }
 
-// Scheme returns the scheme for URL-s
+// Scheme returns the scheme of the URL
 func (p FPath) Scheme() string {
 	return p.scheme
 }
 
-// String returns entire URL
+// String returns the entire URL
 func (p FPath) String() string {
 	if p.HasScheme() {
 		return p.scheme + "://" + p.BucketPath()
