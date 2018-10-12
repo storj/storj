0e7f6358fbddc56f6a1eb0e1b585bde8c4899ebc
Jennifer Li Johnson <jennifer@storj.io>
2018-10-12 13:49:49 -0400

    creates configs for data repair package (#463)
    * creates configs

--- pkg/datarepair/repairer/repairer.go
+++ pkg/datarepair/repairer/repairer.go
@@ -11,9 +11,7 @@ import (
 
 	q "storj.io/storj/pkg/datarepair/queue"
 	"storj.io/storj/pkg/pb"
-	"storj.io/storj/pkg/provider"
 	"storj.io/storj/pkg/utils"
-	"storj.io/storj/storage/redis"
 )
 
 // Repairer is the interface for the data repair queue
@@ -21,40 +19,6 @@ type Repairer interface {
 	Repair(seg *pb.InjuredSegment) error
 	Run() error
 	Stop() error
-}
-
-// Config contains configurable values for repairer
-type Config struct {
-	QueueAddress string        `help:"data repair queue address" default:"redis://localhost:6379?db=5&password=123"`
-	MaxRepair    int           `help:"maximum segments that can be repaired concurrently" default:"100"`
-	Interval     time.Duration `help:"how frequently checker should audit segments" default:"3600s"`
-}
-
-// Initialize a repairer struct
-func (c Config) initialize(ctx context.Context) (Repairer, error) {
-	var r repairer
-	r.ctx, r.cancel = context.WithCancel(ctx)
-
-	client, err := redis.NewClientFrom(c.QueueAddress)
-	if err != nil {
-		return nil, Error.Wrap(err)
-	}
-	r.queue = q.NewQueue(client)
-
-	r.cond.L = &r.mu
-	r.maxRepair = c.MaxRepair
-	r.interval = c.Interval
-	return &r, nil
-}
-
-// Run runs the repairer with configured values
-func (c Config) Run(ctx context.Context, server *provider.Provider) (err error) {
-	r, err := c.initialize(ctx)
-	if err != nil {
-		return err
-	}
-
-	return r.Run()
 }
 
 // repairer holds important values for data repair
