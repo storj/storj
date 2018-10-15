2042bab67ea1851d5f0e68ee2a52293dde4cfe83
Egon Elbre <egonelbre@gmail.com>
2018-10-15 20:14:29 +0300

    Delete dead code in overlay (#476)
    * Delete dead code
    
    * Delete some more dead code.
    
    * Format

--- pkg/overlay/service.go
+++ pkg/overlay/service.go
@@ -4,26 +4,10 @@
 package overlay
 
 import (
-	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"gopkg.in/spacemonkeygo/monkit.v2"
 
-	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/pb"
 )
-
-// NewServer creates a new Overlay Service Server
-func NewServer(k *kademlia.Kademlia, cache *Cache, l *zap.Logger, m *monkit.Registry) *grpc.Server {
-	grpcServer := grpc.NewServer()
-	pb.RegisterOverlayServer(grpcServer, &Server{
-		dht:     k,
-		cache:   cache,
-		logger:  l,
-		metrics: m,
-	})
-
-	return grpcServer
-}
 
 // NewClient connects to grpc server at the provided address with the provided options
 // returns a new instance of an overlay Client
