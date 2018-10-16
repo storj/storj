87cf0fb0247d99c3172f859fbc2f25587efa2c04
Egon Elbre <egonelbre@gmail.com>
2018-10-16 15:02:41 +0300

    setup travis cache

--- scripts/setup-gopath.sh
+++ scripts/setup-gopath.sh
@@ -13,10 +13,17 @@ mv $GOPATH/src/github.com/storj/storj $GOPATH/src/storj.io
 # TODO: setup gospace
 mv $GOPATH/src/storj.io/storj/scripts/gospace ~/bin
 chmod +x ~/bin/gospace
-# TODO: setup cache
-mv $GOPATH/src/storj.io/storj/scripts/storj-vendor.zip ~/cache/storj-vendor.zip
 
-gospace unzip-vendor ~/cache/storj-vendor.zip
+# find module dependency hash
+MODHASH=$(gospace hash)
+
+# download dependencies, if we don't have them in cache
+if [ ! -f $HOME/cache/$MODHASH.zip ]; then
+    gospace zip-vendor $HOME/cache/$MODHASH.zip
+fi
+
+# unpack the dependencies into gopath
+gospace unzip-vendor $HOME/cache/$MODHASH.zip
 gospace flatten-vendor
 
 export TRAVIS_BUILD_DIR=$GOPATH/src/storj.io/storj
