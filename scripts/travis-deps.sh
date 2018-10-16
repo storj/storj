6eed1b3ae3c2bbf1bdf6253604b8799502325136
Egon Elbre <egonelbre@gmail.com>
2018-10-16 13:13:36 +0300

    Fix paths

--- scripts/travis-deps.sh
+++ scripts/travis-deps.sh
@@ -1,5 +1,8 @@
 set -x
 
+mkdir -p ~/bin ~/cache
+export PATH=~/bin:$PATH
+
 export GOPATH=$HOME/storj
 
 export GOSPACE_ROOT=$GOPATH
@@ -10,9 +13,14 @@ mkdir -p $GOPATH/src/storj.io
 mv $HOME/gopath/src/github.com/storj/storj $GOPATH/src/storj.io
 rm -rf $HOME/gopath
 
-./$GOPATH/src/storj.io/storj/scripts/gospace unzip-vendor ./$GOPATH/src/storj.io/storj/scripts/storj-vendor.zip
-./$GOPATH/src/storj.io/storj/scripts/gospace flatten-vendor
-./$GOPATH/src/storj.io/storj/scripts/gospace update
+# TODO: setup gospace
+mv $GOPATH/src/storj.io/storj/scripts/gospace ~/bin
+# TODO: setup cache
+mv $GOPATH/src/storj.io/storj/scripts/storj-vendor.zip ~/cache/storj-vendor.zip
+
+gospace unzip-vendor ~/cache/storj-vendor.zip
+gospace flatten-vendor
+gospace update
 
 export TRAVIS_BUILD_DIR=$GOPATH/src/storj.io/storj
 cd $TRAVIS_BUILD_DIR
@@ -23,7 +31,6 @@ pushd $HOME/awscli
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
-export PATH=~/bin:$PATH
 
 popd
 
