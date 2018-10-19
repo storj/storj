b3d6073e09979cc020bd09e9cdc2a48987eea275
Dylan Lott <lott.dylan@gmail.com>
2018-10-19 10:06:27 -0600

    creates tmp dir and cleans it up after script runs

--- scripts/test-captplanet.sh
+++ scripts/test-captplanet.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -ueo pipefail
 go install -v storj.io/storj/cmd/captplanet
 
@@ -13,30 +13,35 @@ kill -9 $CAPT_PID
 captplanet run &
 CAPT_PID=$!
 
+#setup tmpdir for testfiles and cleanup
+TMP_DIR=/tmp/capt
+TMP_DIR_CMP=/tmp/capt/cmp
+mkdir -p $TMP_DIR
+
 aws configure set aws_access_key_id insecure-dev-access-key
 aws configure set aws_secret_access_key insecure-dev-secret-key
 aws configure set default.region us-east-1
 
-head -c 1024 </dev/urandom > ./small-upload-testfile # create 1mb file of random bytes (inline)
-head -c 5120 </dev/urandom > ./big-upload-testfile   # create 5mb file of random bytes (remote)
-head -c 5 </dev/urandom > ./multipart-upload-testfile     # create 5kb file of random bytes (remote)
+head -c 1024 </dev/urandom > $TMP_DIR/small-upload-testfile # create 1mb file of random bytes (inline)
+head -c 5120 </dev/urandom > $TMP_DIR/big-upload-testfile   # create 5mb file of random bytes (remote)
+head -c 5 </dev/urandom > $TMP_DIR/multipart-upload-testfile     # create 5kb file of random bytes (remote)
 
 aws s3 --endpoint=http://localhost:7777/ mb s3://bucket
 
 aws configure set default.s3.multipart_threshold 1TB
-aws s3 --endpoint=http://localhost:7777/ cp ./small-upload-testfile s3://bucket/small-testfile
-aws s3 --endpoint=http://localhost:7777/ cp ./big-upload-testfile s3://bucket/big-testfile
+aws s3 --endpoint=http://localhost:7777/ cp $TMP_DIR/small-upload-testfile s3://bucket/small-testfile
+aws s3 --endpoint=http://localhost:7777/ cp $TMP_DIR/big-upload-testfile s3://bucket/big-testfile
 
 aws configure set default.s3.multipart_threshold 4KB
-aws s3 --endpoint=http://localhost:7777/ cp ./multipart-upload-testfile s3://bucket/multipart-testfile
+aws s3 --endpoint=http://localhost:7777/ cp $TMP_DIR/multipart-upload-testfile s3://bucket/multipart-testfile
 
 aws s3 --endpoint=http://localhost:7777/ ls s3://bucket
-aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/small-testfile ./small-download-testfile
-aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/big-testfile ./big-download-testfile
-aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/multipart-testfile ./multipart-download-testfile
+aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/small-testfile $TMP_DIR/cmp/small-download-testfile
+aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/big-testfile $TMP_DIR/cmp/big-download-testfile
+aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/multipart-testfile $TMP_DIR/cmp/multipart-download-testfile
 aws s3 --endpoint=http://localhost:7777/ rb s3://bucket --force
 
-if cmp ./small-upload-testfile ./small-download-testfile
+if cmp $TMP_DIR/small-upload-testfile $TMP_DIR/cmp/small-download-testfile
 then
   echo "Downloaded file matches uploaded file";
 else
@@ -45,7 +50,7 @@ else
   exit 1;
 fi
 
-if cmp ./big-upload-testfile ./big-download-testfile
+if cmp $TMP_DIR/big-upload-testfile $TMP_DIR/cmp/big-download-testfile
 then
   echo "Downloaded file matches uploaded file";
 else
@@ -54,7 +59,7 @@ else
   exit 1;
 fi
 
-if cmp ./multipart-upload-testfile ./multipart-download-testfile
+if cmp $TMP_DIR/multipart-upload-testfile $TMP_DIR/cmp/multipart-download-testfile
 then
   echo "Downloaded file matches uploaded file";
 else
@@ -70,11 +75,11 @@ captplanet run &
 CAPT_PID=$!
 
 aws s3 --endpoint=http://localhost:7777/ mb s3://bucket
-aws s3 --endpoint=http://localhost:7777/ cp ./big-upload-testfile s3://bucket/big-testfile
-aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/big-testfile ./big-download-testfile-ipv6
+aws s3 --endpoint=http://localhost:7777/ cp $TMP_DIR/big-upload-testfile s3://bucket/big-testfile
+aws s3 --endpoint=http://localhost:7777/ cp s3://bucket/big-testfile $TMP_DIR/cmp/big-download-testfile-ipv6
 aws s3 --endpoint=http://localhost:7777/ rb s3://bucket --force
 
-if cmp ./big-upload-testfile ./big-download-testfile-ipv6
+if cmp $TMP_DIR/big-upload-testfile $TMP_DIR/cmp/big-download-testfile-ipv6
 then
   echo "Downloaded ipv6 file matches uploaded file";
 else
@@ -85,7 +90,4 @@ fi
 
 kill -9 $CAPT_PID
 
-rm small-upload-testfile
-rm big-upload-testfile
-rm multipart-upload-testfile
-rm big-download-testfile-ipv6
+rm -rf $TMP_DIR
