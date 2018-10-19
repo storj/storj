e6c6d50dd9e43113f78a0d7bfc4bd927ca59387d
Egon Elbre <egonelbre@gmail.com>
2018-10-19 17:04:19 +0300

    Add ipv6 to cert template

--- pkg/peertls/templates.go
+++ pkg/peertls/templates.go
@@ -36,7 +36,7 @@ func LeafTemplate() (*x509.Certificate, error) {
 		SerialNumber:          serialNumber,
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-		DNSNames:              []string{"127.0.0.1", "localhost"}, // TODO: fix
+		DNSNames:              []string{"127.0.0.1", "localhost", "0:0:0:0:0:0:0:1", "::1"}, // TODO: fix
 		BasicConstraintsValid: true,
 		IsCA:                  false,
 	}
