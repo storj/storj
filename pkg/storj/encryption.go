e43f5413d6be97afe3deea6117c08d4e419b1235
Kaloyan Raev <kaloyan@storj.io>
2018-10-19 15:29:22 +0300

    Adapt storj and encryption packages

--- pkg/storj/encryption.go
+++ pkg/storj/encryption.go
@@ -13,14 +13,33 @@ type EncryptionAlgorithm byte
 
 // List of supported encryption algorithms
 const (
-	InvalidEncryptionAlgorithm = EncryptionAlgorithm(iota)
-	Unencrypted
+	// InvalidEncryptionAlgorithm = EncryptionAlgorithm(iota)
+	Unencrypted = EncryptionAlgorithm(iota)
 	AESGCM
-	Secretbox
+	SecretBox
 )
 
+// Constant definitions for key and nonce sizes
+const (
+	KeySize   = 32
+	NonceSize = 24
+)
+
+// Key represents the largest key used by any encryption protocol
+type Key [KeySize]byte
+
+// Raw returns the key as a raw byte array pointer
+func (key *Key) Raw() *[KeySize]byte {
+	return (*[KeySize]byte)(key)
+}
+
 // Nonce represents the largest nonce used by any encryption protocol
-type Nonce [24]byte // TODO: unify with eestream.Nonce
+type Nonce [NonceSize]byte
+
+// Raw returns the nonce as a raw byte array pointer
+func (nonce *Nonce) Raw() *[NonceSize]byte {
+	return (*[NonceSize]byte)(nonce)
+}
 
 // EncryptedPrivateKey is a private key that has been encrypted
 type EncryptedPrivateKey []byte
