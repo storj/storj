d0cd4942fbf280c5541e47b6ae21242760ea2ede
Egon Elbre <egonelbre@gmail.com>
2018-10-23 13:17:20 +0300

    Fix repairing

---
+++ internal/sync2/limiter_test.go
@@ -0,0 +1,69 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information
+
+package sync2
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestLimiterLimiting(t *testing.T) {
+	const N, Limit = 1000, 10
+	ctx := context.Background()
+	limiter := NewLimiter(Limit)
+	counter := int32(0)
+	for i := 0; i < N; i++ {
+		limiter.Go(ctx, func() {
+			if atomic.AddInt32(&counter, 1) > Limit {
+				panic("limit exceeded")
+			}
+			time.Sleep(time.Millisecond)
+			atomic.AddInt32(&counter, -1)
+		})
+	}
+	limiter.Wait()
+}
+
+func TestLimiterCancelling(t *testing.T) {
+	const N, Limit = 1000, 10
+	limiter := NewLimiter(Limit)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	counter := int32(0)
+
+	waitForCancel := make(chan struct{})
+	block := make(chan struct{})
+
+	go func() {
+		for i := 0; i < N; i++ {
+			limiter.Go(ctx, func() {
+				if atomic.AddInt32(&counter, 1) > Limit {
+					panic("limit exceeded")
+				}
+
+				waitForCancel <- struct{}{}
+				select {
+				case <-block:
+					return
+				}
+			})
+		}
+	}()
+
+	for i := 0; i < Limit; i++ {
+		<-waitForCancel
+	}
+	cancel()
+	time.Sleep(time.Millisecond)
+	close(block)
+
+	limiter.Wait()
+
+	if counter > Limit {
+		t.Fatal("too many times run")
+	}
+}
