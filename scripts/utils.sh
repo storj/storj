1062b94c64591b090ebe6ccbc1c71810b3784eb2
Bryan White <bryanchriswhite@gmail.com>
2018-11-02 13:25:55 -0400

    add storj.sh and user.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -16,19 +16,49 @@ dots_off() {
 	kill "$dots_pid"
 }
 
-temp_build() {
+build() {
 	local tmp_dir=$1
 	shift
-	echo "building binaries:"
+	echo "building temp binaries:"
 	for cmd in $@; do
-		echo -n "  buliding $cmd..."
+		echo -n "	buliding $cmd..."
 		dots_on
 		local path=${tmp_dir}/${cmd}
 		declare -g ${cmd}=${path}
 		go build -o ${path} storj.io/storj/cmd/${cmd}
-		#				sleep .5
 		dots_off
 		echo "done"
 	done
-	echo "binaries built in $tmp_dir"
+	echo "	binaries built in $tmp_dir"
+}
+
+temp_build() {
+	tmp_dir=$(mktemp -d)
+	build ${tmp_dir} $@
+}
+
+check_help() {
+	if [ $1 == "--help" ] || [ $1 == "-h" ]; then
+		echo $2
+		exit 0
+	fi
+}
+
+ensure_dir() {
+	if [ ! -d $1 ]; then
+		mkdir $1
+	fi
+}
+
+no_overwrite() {
+	if [ -e $1 ]; then
+	echo "Error: $1 already exists; refusing to overwrite"
+		exit 10
+	fi
+}
+
+log_list() {
+	for f in $@; do
+		echo ${f}
+	done
 }
