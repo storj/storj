a760e48d4d1da28f0c393bddb2d84d9211344845
aligeti <34487396+aligeti@users.noreply.github.com>
2018-11-05 10:23:54 -0500

    Satellite bw usage v3 121 (#547)
    * protobuf for sending bandwidth agreements to satellite from storage nodes
    
    * Setup process for sending agreements
    
    * Add payer_id to db with bandwidth agreements for better sorting
    
    * Linter errors
    
    * Read agreements from PSDB
    
    * Try writing message to server
    
    * Cleanup
    
    * Basic functionality
    
    * Better error handelling
    
    * Fix test
    
    * setup config and server structure for receiving bandwidth agreements
    
    * Resolve linter issues
    
    * Optional commit for if we want to handle deletes all at once
    
    * add identity to Server, add logic for receiving bandwidth messsages
    
    * Bandwidth agreement DBX creation and integration with bw agreement endpoint
    
    Co-authored-by: Kishore <kishore@storj.io>
    Co-authored-by: Cam <cameron@storj.io>
    
    * protobuf for sending bandwidth agreements to satellite from storage nodes
    
    * Setup process for sending agreements
    
    * Add payer_id to db with bandwidth agreements for better sorting
    
    * Linter errors
    
    * Read agreements from PSDB
    
    * Try writing message to server
    
    * Cleanup
    
    * Basic functionality
    
    * Better error handelling
    
    * Fix test
    
    * setup config and server structure for receiving bandwidth agreements
    
    * Resolve linter issues
    
    * Optional commit for if we want to handle deletes all at once
    
    * add identity to Server, add logic for receiving bandwidth messsages
    
    * Bandwidth agreement DBX creation and integration with bw agreement endpoint
    
    Co-authored-by: Kishore <kishore@storj.io>
    Co-authored-by: Cam <cameron@storj.io>
    
    * added postgres create/read/delete test function
    
    Co-authored-by: kishore <kishore@storj.io
    Co-authored-by: cam <cameron@storj.io>
    
    * edit comment
    
    * removed sqlite3 driver from dbx
    
    * remove generated sqlite code, add dbx read limitoffset
    
    * remove getServerAndDB function, rename getDBPath to getPSQLInfo
    
    * WIP writing server endpoint test
    
    * code review changes

---
+++ pkg/bwagreement/common.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package bwagreement
+
+import (
+	"github.com/zeebo/errs"
+)
+
+// Error is the default bwagreement errs class
+var Error = errs.Class("bwagreement error")
