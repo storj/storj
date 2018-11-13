2bf507acc3a9684b886e6616f500153b9aa55135
Moby von Briesen <mobyvb@gmail.com>
2018-11-13 18:04:21 -0500

    add command for creating statdb nodes from csv

--- internal/sdbinspect/main.go
+++ internal/sdbinspect/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
@@ -53,10 +57,82 @@ func main() {
 		},
 	}
 
+	var cmdCreateCSVStats = &cobra.Command{
+		// create node stats from csv
+		Use:   "createcsv",
+		Short: "Print node stats",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			ca, err := provider.NewTestCA(ctx)
+			if err != nil {
+				fmt.Println("Error", err)
+				os.Exit(1)
+			}
+			identity, err := ca.NewIdentity()
+			if err != nil {
+				fmt.Println("Error", err)
+				os.Exit(1)
+			}
+			client, err := sdbclient.NewClient(identity, port, []byte(apiKey))
+			if err != nil {
+				fmt.Println("Error", err)
+				os.Exit(1)
+			}
+
+			// get csv
+			csvPath := args[0]
+			csvFile, _ := os.Open(csvPath)
+			reader := csv.NewReader(bufio.NewReader(csvFile))
+			for {
+				line, err := reader.Read()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+
+				idStr := line[0]
+				nodeID := node.IDFromString(idStr)
+
+				auditCount, err := strconv.ParseInt(line[1], 10, 64)
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+				auditSuccessCount, err := strconv.ParseInt(line[2], 10, 64)
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+				uptimeCount, err := strconv.ParseInt(line[3], 10, 64)
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+				uptimeSuccessCount, err := strconv.ParseInt(line[4], 10, 64)
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+
+				err = client.CreateWithStats(ctx, nodeID.Bytes(), auditCount, auditSuccessCount, uptimeCount, uptimeSuccessCount)
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+
+				fmt.Printf("Created statdb entry for ID %s\n", nodeID.String())
+			}
+		},
+	}
+
 	cmdGetStats.Flags().StringVarP(&port, "port", "p", ":7778", "statdb port")
 	cmdGetStats.Flags().StringVarP(&apiKey, "apikey", "a", "abc123", "statdb api key")
+	cmdCreateCSVStats.Flags().StringVarP(&port, "port", "p", ":7778", "statdb port")
+	cmdCreateCSVStats.Flags().StringVarP(&apiKey, "apikey", "a", "abc123", "statdb api key")
 
 	var rootCmd = &cobra.Command{Use: "sdbinspect"}
-	rootCmd.AddCommand(cmdGetStats)
+	rootCmd.AddCommand(cmdGetStats, cmdCreateCSVStats)
 	rootCmd.Execute()
 }
