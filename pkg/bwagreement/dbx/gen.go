168d349d3b0c5ab046e2c1f6bdf611ed0e32165a
Alexander Leitner <leitnersalex@gmail.com>
2018-11-14 11:00:07 -0500

    Simplify dbx generate

--- pkg/bwagreement/dbx/gen.go
+++ pkg/bwagreement/dbx/gen.go
@@ -3,7 +3,5 @@
 
 package bwagreement
 
-//go:generate dbx.v1 golang -d postgres bwagreement.dbx .
-//go:generate dbx.v1 schema -d postgres bwagreement.dbx .
-//go:generate dbx.v1 golang -d sqlite3 bwagreement.dbx .
-//go:generate dbx.v1 schema -d sqlite3 bwagreement.dbx .
+//go:generate dbx.v1 golang -d postgres -d sqlite3  bwagreement.dbx .
+//go:generate dbx.v1 schema -d postgres -d sqlite3  bwagreement.dbx .
