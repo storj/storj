5ff2585d2ad1f7991ede1e0971d20a8e641e86ca
Moby von Briesen <mobyvb@gmail.com>
2018-11-14 08:00:30 -0500

    add getCSVStats and create helper func for getting sdb client

--- internal/sdbinspect/main.go
+++ internal/sdbinspect/main.go
@@ -27,24 +27,13 @@ func main() {
 		Short: "Print node stats",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			client, err := getSdbClient(ctx, port, apiKey)
+			if err != nil {
+				fmt.Println("Error", err)
+				os.Exit(1)
+			}
+
 			nodeID := node.IDFromString(args[0])
-
-			ca, err := provider.NewTestCA(ctx)
-			if err != nil {
-				fmt.Println("Error", err)
-				os.Exit(1)
-			}
-			identity, err := ca.NewIdentity()
-			if err != nil {
-				fmt.Println("Error", err)
-				os.Exit(1)
-			}
-			client, err := sdbclient.NewClient(identity, port, []byte(apiKey))
-			if err != nil {
-				fmt.Println("Error", err)
-				os.Exit(1)
-			}
-
 			nodeStats, err := client.Get(ctx, nodeID.Bytes())
 			if err != nil {
 				fmt.Println("Error", err)
@@ -57,23 +46,54 @@ func main() {
 		},
 	}
 
+	var cmdGetCSVStats = &cobra.Command{
+		// get node stats from csv
+		Use:   "getcsv",
+		Short: "Print node stats from csv",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			client, err := getSdbClient(ctx, port, apiKey)
+			if err != nil {
+				fmt.Println("Error", err)
+				os.Exit(1)
+			}
+
+			// get csv
+			csvPath := args[0]
+			csvFile, _ := os.Open(csvPath)
+			reader := csv.NewReader(bufio.NewReader(csvFile))
+			for {
+				line, err := reader.Read()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+
+				idStr := line[0]
+				nodeID := node.IDFromString(idStr)
+
+				nodeStats, err := client.Get(ctx, nodeID.Bytes())
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+
+				fmt.Printf("Stats for ID %s:\n", nodeID.String())
+				fmt.Printf("AuditSuccessRatio: %f, UptimeRatio: %f, AuditCount: %d\n",
+					nodeStats.AuditSuccessRatio, nodeStats.UptimeRatio, nodeStats.AuditCount)
+			}
+		},
+	}
+
 	var cmdCreateCSVStats = &cobra.Command{
 		// create node stats from csv
 		Use:   "createcsv",
-		Short: "Print node stats",
+		Short: "Create node stats from csv",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			ca, err := provider.NewTestCA(ctx)
-			if err != nil {
-				fmt.Println("Error", err)
-				os.Exit(1)
-			}
-			identity, err := ca.NewIdentity()
-			if err != nil {
-				fmt.Println("Error", err)
-				os.Exit(1)
-			}
-			client, err := sdbclient.NewClient(identity, port, []byte(apiKey))
+			client, err := getSdbClient(ctx, port, apiKey)
 			if err != nil {
 				fmt.Println("Error", err)
 				os.Exit(1)
@@ -129,10 +149,29 @@ func main() {
 
 	cmdGetStats.Flags().StringVarP(&port, "port", "p", ":7778", "statdb port")
 	cmdGetStats.Flags().StringVarP(&apiKey, "apikey", "a", "abc123", "statdb api key")
+	cmdGetCSVStats.Flags().StringVarP(&port, "port", "p", ":7778", "statdb port")
+	cmdGetCSVStats.Flags().StringVarP(&apiKey, "apikey", "a", "abc123", "statdb api key")
 	cmdCreateCSVStats.Flags().StringVarP(&port, "port", "p", ":7778", "statdb port")
 	cmdCreateCSVStats.Flags().StringVarP(&apiKey, "apikey", "a", "abc123", "statdb api key")
 
 	var rootCmd = &cobra.Command{Use: "sdbinspect"}
-	rootCmd.AddCommand(cmdGetStats, cmdCreateCSVStats)
+	rootCmd.AddCommand(cmdGetStats, cmdGetCSVStats, cmdCreateCSVStats)
 	rootCmd.Execute()
+}
+
+func getSdbClient(ctx context.Context, port, apiKey string) (sdbclient.Client, error) {
+	ca, err := provider.NewTestCA(ctx)
+	if err != nil {
+		return nil, err
+	}
+	identity, err := ca.NewIdentity()
+	if err != nil {
+		return nil, err
+	}
+	client, err := sdbclient.NewClient(identity, port, []byte(apiKey))
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
