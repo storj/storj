fdbc493b90cd4bb16a4185105f910abb536caecf
Alexander Leitner <leitnersalex@gmail.com>
2018-11-15 12:14:22 -0500

    Are we using the right keys

--- pkg/bwagreement/server_test.go
+++ pkg/bwagreement/server_test.go
@@ -25,7 +25,6 @@ import (
 
 	"storj.io/storj/pkg/bwagreement/database-manager"
 	"storj.io/storj/pkg/pb"
-	"storj.io/storj/pkg/peertls"
 	"storj.io/storj/pkg/provider"
 )
 
@@ -77,7 +76,7 @@ func NewTestServer(t *testing.T) *TestServer {
 	co, err := fiC.DialOption("")
 	check(err)
 
-	s := newTestServerStruct(t)
+	s := newTestServerStruct(t, fiC.Key)
 	grpcs := grpc.NewServer(so)
 
 	k, ok := fiC.Key.(*ecdsa.PrivateKey)
@@ -99,7 +98,7 @@ var (
 	testPostgres = flag.String("postgres-test-db", os.Getenv("STORJ_POSTGRESKV_TEST"), "PostgreSQL test database connection string")
 )
 
-func newTestServerStruct(t *testing.T) *Server {
+func newTestServerStruct(t *testing.T, k crypto.PrivateKey) *Server {
 	if *testPostgres == "" {
 		t.Skipf("postgres flag missing, example:\n-postgres-test-db=%s", defaultPostgresConn)
 	}
@@ -108,9 +107,6 @@ func newTestServerStruct(t *testing.T) *Server {
 	if err != nil {
 		t.Fatalf("Failed to initialize dbmanager when creating test server: %+v", err)
 	}
-
-	k, err := peertls.NewKey()
-	assert.NoError(t, err)
 
 	p, _ := k.(*ecdsa.PrivateKey)
 	server, err := NewServer(dbm, zap.NewNop(), &p.PublicKey)
@@ -155,12 +151,12 @@ func generatePayerBandwidthAllocation(action pb.PayerBandwidthAllocation_Action,
 	// Generate PayerBandwidthAllocation_Data
 	data, _ := proto.Marshal(
 		&pb.PayerBandwidthAllocation_Data{
-			SatelliteId: []byte("SatelliteID"),
-			UplinkId:  []byte("UplinkID"),
+			SatelliteId:       []byte("SatelliteID"),
+			UplinkId:          []byte("UplinkID"),
 			ExpirationUnixSec: time.Now().Add(time.Hour * 24 * 10).Unix(),
-			SerialNumber: "SerialNumber",
-			Action: action,
-			CreatedUnixSec: time.Now().Unix(),
+			SerialNumber:      "SerialNumber",
+			Action:            action,
+			CreatedUnixSec:    time.Now().Unix(),
 		},
 	)
 
@@ -172,44 +168,44 @@ func generatePayerBandwidthAllocation(action pb.PayerBandwidthAllocation_Action,
 
 	// Combine Signature and Data for PayerBandwidthAllocation
 	return &pb.PayerBandwidthAllocation{
-		Data: data,
+		Data:      data,
 		Signature: s,
 	}, nil
 }
 
 func generateRenterBandwidthAllocation(pba *pb.PayerBandwidthAllocation, uplinkKey crypto.PrivateKey) (*pb.RenterBandwidthAllocation, error) {
-		// get "Uplink" Public Key
-		uplinkKeyEcdsa, ok := uplinkKey.(*ecdsa.PrivateKey)
-		if !ok {
-			return nil, errs.New("Uplink Private Key is not a valid *ecdsa.PrivateKey")
-		}
-	
-		pubbytes, err := x509.MarshalPKIXPublicKey(&uplinkKeyEcdsa.PublicKey)
-		if err != nil {
-			return nil, errs.New("Could not generate byte array from Uplink Public key: %+v", err)
-		}
-	
-		// Generate RenterBandwidthAllocation_Data
-		data, _ := proto.Marshal(
-			&pb.RenterBandwidthAllocation_Data{
-				PayerAllocation: pba,
-				PubKey: pubbytes,
-				StorageNodeId: []byte("StorageNodeID"),
-				Total: int64(666),
-			},
-		)
-	
-		// Sign the PayerBandwidthAllocation_Data with the "Uplink" Private Key
-		s, err := cryptopasta.Sign(data, uplinkKeyEcdsa)
-		if err != nil {
-			return nil, errs.New("Failed to sign RenterBandwidthAllocation_Data with uplink Private Key: %+v", err)
-		}
-	
-		// Combine Signature and Data for RenterBandwidthAllocation
-		return &pb.RenterBandwidthAllocation{
-			Signature: s,
-			Data:      data,
-		}, nil
+	// get "Uplink" Public Key
+	uplinkKeyEcdsa, ok := uplinkKey.(*ecdsa.PrivateKey)
+	if !ok {
+		return nil, errs.New("Uplink Private Key is not a valid *ecdsa.PrivateKey")
+	}
+
+	pubbytes, err := x509.MarshalPKIXPublicKey(&uplinkKeyEcdsa.PublicKey)
+	if err != nil {
+		return nil, errs.New("Could not generate byte array from Uplink Public key: %+v", err)
+	}
+
+	// Generate RenterBandwidthAllocation_Data
+	data, _ := proto.Marshal(
+		&pb.RenterBandwidthAllocation_Data{
+			PayerAllocation: pba,
+			PubKey:          pubbytes,
+			StorageNodeId:   []byte("StorageNodeID"),
+			Total:           int64(666),
+		},
+	)
+
+	// Sign the PayerBandwidthAllocation_Data with the "Uplink" Private Key
+	s, err := cryptopasta.Sign(data, uplinkKeyEcdsa)
+	if err != nil {
+		return nil, errs.New("Failed to sign RenterBandwidthAllocation_Data with uplink Private Key: %+v", err)
+	}
+
+	// Combine Signature and Data for RenterBandwidthAllocation
+	return &pb.RenterBandwidthAllocation{
+		Signature: s,
+		Data:      data,
+	}, nil
 }
 
 func (TS *TestServer) Stop() {
