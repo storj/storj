71cb763147451df4ea8fb21a41fbeff2b0bf934d
Bryan White <bryanchriswhite@gmail.com>
2018-11-20 12:02:02 -0500

    linter fixes

--- pkg/pb/gen.go
+++ pkg/pb/gen.go
@@ -5,6 +5,7 @@ package pb
 
 import "storj.io/storj/pkg/storj"
 
+// Path represents a object path
 type Path = storj.Path
 
 //go:generate protoc -I. --gogo_out=plugins=grpc:. meta.proto overlay.proto pointerdb.proto piecestore.proto bandwidth.proto kadcli.proto datarepair.proto
