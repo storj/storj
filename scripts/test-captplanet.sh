cd6561c7edd589edb5c37f04ad7a15a4604d0929
Dylan Lott <lott.dylan@gmail.com>
2018-11-20 14:55:39 -0700

    updates the refresh interval in captplanet test script

--- scripts/test-captplanet.sh
+++ scripts/test-captplanet.sh
@@ -3,7 +3,7 @@ set -ueo pipefail
 go install -v storj.io/storj/cmd/captplanet
 
 captplanet setup --overwrite
-sed -i~ 's/interval:.*/interval: 3s/g' $HOME/.storj/capt/config.yaml
+sed -i~ 's/interval:.*/interval: 1s/g' $HOME/.storj/capt/config.yaml
 
 # run captplanet for 5 seconds to reproduce kademlia problems. See V3-526
 captplanet run &
@@ -76,9 +76,11 @@ fi
 kill -9 $CAPT_PID
 
 captplanet setup --listen-host ::1 --overwrite
-sed -i~ 's/interval:.*/interval: 3s/g' $HOME/.storj/capt/config.yaml
+sed -i~ 's/interval:.*/interval: 1s/g' $HOME/.storj/capt/config.yaml
 captplanet run &
 CAPT_PID=$!
+
+sleep 25
 
 aws s3 --endpoint=http://localhost:7777/ mb s3://bucket
 aws s3 --endpoint=http://localhost:7777/ cp $TMP_DIR/big-upload-testfile s3://bucket/big-testfile
