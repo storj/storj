7be243c073a029f9c0eaee5d809023b7c04cebd6
Moby von Briesen <mobyvb@gmail.com>
2018-11-21 11:30:28 -0500

    set api key from statdb/config.go

--- pkg/statdb/config.go
+++ pkg/statdb/config.go
@@ -5,10 +5,10 @@ package statdb
 
 import (
 	"context"
+	"flag"
 
 	"go.uber.org/zap"
 
-	"storj.io/storj/pkg/auth"
 	"storj.io/storj/pkg/provider"
 	pb "storj.io/storj/pkg/statdb/proto"
 )
@@ -20,6 +20,10 @@ const (
 	ctxKeyStats CtxKey = iota
 )
 
+var (
+	apiKey = flag.String("stat-db.auth.api-key", "", "api key")
+)
+
 // Config is a configuration struct that is everything you need to start a
 // StatDB responsibility
 type Config struct {
@@ -29,12 +33,7 @@ type Config struct {
 
 // Run implements the provider.Responsibility interface
 func (c Config) Run(ctx context.Context, server *provider.Provider) error {
-	apiKey, ok := auth.GetAPIKey(ctx)
-	if !ok {
-		return Error.New("API key not set")
-	}
-
-	ns, err := NewServer(c.DatabaseDriver, c.DatabaseURL, string(apiKey), zap.L())
+	ns, err := NewServer(c.DatabaseDriver, c.DatabaseURL, *apiKey, zap.L())
 	if err != nil {
 		return err
 	}
