db4b0e502cf78fd6c24500e52351f836fec15eaf
Jennifer Johnson <jennifer@storj.io>
2018-11-21 23:19:51 -0500

    tallyAtRestStorage

--- pkg/accounting/tally/tally.go
+++ pkg/accounting/tally/tally.go
@@ -28,26 +28,40 @@ type Tally interface {
 }
 
 type tally struct {
-	pointerdb *pointerdb.Server
-	overlay   pb.OverlayServer
-	kademlia  *kademlia.Kademlia
-	limit     int
-	logger    *zap.Logger
-	ticker    *time.Ticker
-	db        *dbx.DB              // accounting db
-	dbm       *dbManager.DBManager // bwagreements database
+	pointerdb  *pointerdb.Server
+	overlay    pb.OverlayServer
+	kademlia   *kademlia.Kademlia
+	limit      int
+	logger     *zap.Logger
+	ticker     *time.Ticker
+	nodes      map[string]int64
+	nodeClient node.Client
+	//TODO:
+	//accountingDBServer
 }
 
-func newTally(logger *zap.Logger, db *dbx.DB, dbm *dbManager.DBManager, pointerdb *pointerdb.Server, overlay pb.OverlayServer, kademlia *kademlia.Kademlia, limit int, interval time.Duration) *tally {
+func newTally(ctx context.Context, pointerdb *pointerdb.Server, overlay pb.OverlayServer, kademlia *kademlia.Kademlia, limit int, logger *zap.Logger, interval time.Duration) *tally {
+	rt, err := kademlia.GetRoutingTable(ctx)
+	if err != nil {
+		// return Error.Wrap(err)
+	}
+	self := rt.Local()
+	identity := &provider.FullIdentity{} //do i need anything in here?
+	client, err := node.NewNodeClient(identity, self, kademlia)
+	if err != nil {
+		// return Error.Wrap(err)
+	}
 	return &tally{
-		pointerdb: pointerdb,
-		overlay:   overlay,
-		kademlia:  kademlia,
-		limit:     limit,
-		logger:    logger,
-		ticker:    time.NewTicker(interval),
-		dbm:       dbm,
-		db:        db,
+		pointerdb:  pointerdb,
+		overlay:    overlay,
+		kademlia:   kademlia,
+		limit:      limit,
+		logger:     logger,
+		ticker:     time.NewTicker(interval),
+		nodes:      make(map[string]int64),
+		nodeClient: client,
+		//TODO:
+		//accountingDBServer
 	}
 }
 
@@ -74,17 +88,7 @@ func (t *tally) Run(ctx context.Context) (err error) {
 func (t *tally) identifyActiveNodes(ctx context.Context) (err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	rt, err := t.kademlia.GetRoutingTable(ctx)
-	if err != nil {
-		return Error.Wrap(err)
-	}
-	self := rt.Local()
-	identity := &provider.FullIdentity{} //do i need anything in here?
-	client, err := node.NewNodeClient(identity, self, t.kademlia)
-	if err != nil {
-		return Error.Wrap(err)
-	}
-
+	t.logger.Debug("entering pointerdb iterate")
 	err = t.pointerdb.Iterate(ctx, &pb.IterateRequest{Recurse: true},
 		func(it storage.Iterator) error {
 			var item storage.ListItem
@@ -107,12 +111,15 @@ func (t *tally) identifyActiveNodes(ctx context.Context) (err error) {
 				if err != nil {
 					return Error.Wrap(err)
 				}
-				go t.tallyAtRestStorage(ctx, pointer, online, client)
+				err = t.tallyAtRestStorage(ctx, pointer, online)
+				if err != nil {
+					return Error.Wrap(err)
+				}
 			}
 			return nil
 		},
 	)
-	return err
+	return t.updateRawTable()
 }
 
 func (t *tally) onlineNodes(ctx context.Context, nodeIDs storj.NodeIDList) (online []*pb.Node, err error) {
@@ -129,42 +136,30 @@ func (t *tally) onlineNodes(ctx context.Context, nodeIDs storj.NodeIDList) (onli
 	return online, nil
 }
 
-func (t *tally) tallyAtRestStorage(ctx context.Context, pointer *pb.Pointer, nodes []*pb.Node, client node.Client) {
+func (t *tally) tallyAtRestStorage(ctx context.Context, pointer *pb.Pointer, nodes []*pb.Node) error {
 	segmentSize := pointer.GetSegmentSize()
 	minReq := pointer.Remote.Redundancy.GetMinReq()
 	if minReq <= 0 {
-		zap.L().Error("minReq must be an int greater than 0")
-		return
+		return Error.New("minReq must be an int greater than 0")
 	}
 	pieceSize := segmentSize / int64(minReq)
 	for _, n := range nodes {
-		nodeAvail := true
-		var err error
-		ok := t.needToContact(n.Id)
+		ok, err := t.nodeClient.Ping(ctx, *n)
 		if ok {
-			nodeAvail, err = client.Ping(ctx, *n)
-			if err != nil {
-				zap.L().Error("ping failed")
-				continue
-			}
+			t.nodes[n.Id] = pieceSize
 		}
-		if nodeAvail {
-			err := t.updateGranularTable(n.Id, pieceSize)
-			if err != nil {
-				zap.L().Error("update failed")
-			}
+		if !ok || err != nil {
+			zap.L().Error("ping failed for node: " + n.Id)
+			continue
 		}
 	}
+	return nil
 }
 
-func (t *tally) needToContact(id storj.NodeID) bool {
+func (t *tally) updateRawTable() error {
 	//TODO
-	//check db if node was updated within the last time period
-	return true
-}
-
-func (t *tally) updateGranularTable(id storj.NodeID, pieceSize int64) error {
-	//TODO
+	//takes in-memory map of ok nodes
+	//adds all and sets all other nodes to 0
 	return nil
 }
 
