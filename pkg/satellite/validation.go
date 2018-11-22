633bd057539fa6c5946e4888cb39cc23d3a09b25
crawter <crawter@hotmail.com>
2018-11-22 14:32:51 +0200

    tests added, email validation improved

--- pkg/satellite/validation.go
+++ pkg/satellite/validation.go
@@ -3,23 +3,57 @@
 
 package satellite
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 // IsValid is a method for validating UserInfo entity
 func (userInfo *UserInfo) IsValid() bool {
-	regexpEmail := "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-	re := regexp.MustCompile(regexpEmail)
-
 	if userInfo.FirstName == "" {
 		return false
 	}
 	if userInfo.LastName == "" {
 		return false
 	}
-	if userInfo.Email == "" || !re.MatchString(userInfo.Email) {
+	if !validateEmail(userInfo.Email) {
 		return false
 	}
 	if len(userInfo.Password) < 6 {
+		return false
+	}
+
+	return true
+}
+
+// validateEmail is a function for email validation
+func validateEmail(email string) bool {
+	localPartRegexp := regexp.MustCompile("^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+$")
+	noDotsRegexp := regexp.MustCompile("(^[.]{1})|([.]{1}$)|([.]{2,})")
+	hostRegexp := regexp.MustCompile("^[^\\s]+\\.[^\\s]+$")
+
+	length := len(email)
+
+	if length > 254 {
+		return false
+	}
+
+	delimiter := strings.LastIndex(email, "@")
+	if delimiter <= 0 || delimiter > len(email)-3 {
+		return false
+	}
+
+	localPart := email[:delimiter]
+	lenValid := len(localPart) <= 64
+	validString := localPartRegexp.MatchString(localPart)
+	noDots := !noDotsRegexp.MatchString(localPart)
+
+	if !(lenValid && validString && noDots) {
+		return false
+	}
+
+	hostPart := email[delimiter+1:]
+	if !hostRegexp.MatchString(hostPart) {
 		return false
 	}
 
---
+++ pkg/satellite/validation_test.go
@@ -0,0 +1,504 @@
+// Copyright (C) 2018 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package satellite
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestValidationUserInfo(t *testing.T) {
+	cases := []struct {
+		testName string
+		testFunc func()
+	}{
+		{
+			testName: "Valid test 1",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Bill",
+					Password:  "123456",
+					Email:     "abcdqe@yahoo.com",
+					LastName:  "Gate",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 2",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "1234561231231231231231231231231231231231231231231231231231231231231231231",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 3",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "12345612312312312312312",
+					Email:     "email@gmail.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 4",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "1234561231231231231231231231231",
+					Email:     "email.email@gmail.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 5",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "12345612312312312312312312312312312",
+					Email:     "email+extra@example.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 6",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "123456123123123123123123123123123123",
+					Email:     "EMAIL@aol.co.uk",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 7",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "123456123123123123123123123123123123123",
+					Email:     "EMAIL+EXTRA@aol.co.uk",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 8",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "123456123123123123123123123123123123123",
+					Email:     "example-indeed@strange-example.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Valid test 9",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "Arnold",
+					Password:  "123456123123123123123123123123123123123",
+					Email:     "other.email-with-hyphen@example.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, true, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 1",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "",
+					Password:  "123456123123123123123123123123123123123123",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 2",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qwe",
+					Password:  "123",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 3",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qwe",
+					Password:  "12334",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "Snowmilk",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 4",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qwe",
+					Password:  "1234567",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 5",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "",
+					Password:  "",
+					Email:     "Arnold.Snowmilk@yahoo.com",
+					LastName:  "",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 6",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 7",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "some_invalid_email@,,,",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 8",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 9",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 10",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@x",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 11",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 12",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email...@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 13",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email..test@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 14",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     ".email..test.@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 15",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@at@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 16",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "some whitespace@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 17",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@whitespace example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 18",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 19",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "email@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 20",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "just\"not\"right@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+		{
+			testName: "Invalid test 21",
+
+			testFunc: func() {
+				userInfo := UserInfo{
+					FirstName: "qweqwe",
+					Password:  "qweqw123123",
+					Email:     "john..doe@example.com",
+					LastName:  "qweqweqwe",
+				}
+
+				isValid := userInfo.IsValid()
+
+				assert.Equal(t, false, isValid)
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.testName, func(t *testing.T) {
+			c.testFunc()
+		})
+	}
+}
