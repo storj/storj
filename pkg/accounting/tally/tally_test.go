3daf739edb04ee14c0ce95de9a87f233b5b4a56b
Bill Thorp <billt@storj.io>
2018-11-30 10:18:12 -0500

    redo tally query

--- pkg/accounting/tally/tally_test.go
+++ pkg/accounting/tally/tally_test.go
@@ -12,9 +12,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
-
 	"storj.io/storj/internal/teststorj"
 	"storj.io/storj/pkg/accounting"
+	dbManager "storj.io/storj/pkg/bwagreement/database-manager"
 	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/overlay"
 	"storj.io/storj/pkg/overlay/mocks"
@@ -57,8 +57,13 @@ func TestOnlineNodes(t *testing.T) {
 	accountingDb, err := accounting.NewDb("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
 	defer func() { _ = accountingDb.Close() }()
-	tally, err := newTally(logger, accountingDb, pointerdb, overlayServer, kad, limit, interval)
+
+	bwDb, err := dbManager.NewDBManager("sqlite3", "file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
+	defer func() { _ = accountingDb.Close() }()
+
+	tally := newTally(logger, accountingDb, bwDb, pointerdb, overlayServer, kad, limit, interval)
+
 	online, err := tally.onlineNodes(ctx, nodeIDs)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedOnline, online)
@@ -73,5 +78,9 @@ func TestNeedToContact(t *testing.T) {
 }
 
 func TestUpdateGranularTable(t *testing.T) {
+
+}
+
+func TestQuery(t *testing.T) {
 
 }
