3137f3d0b109b0de188ceb67ada16881e2e6a86d
Cameron Ayer <camayer92@gmail.com>
2018-12-03 10:45:36 -0500

    move Config and Client into common.go

--- internal/s3api/minio.go
+++ internal/s3api/minio.go
@@ -15,29 +15,6 @@ import (
 // MinioError is class for minio errors
 var MinioError = errs.Class("minio error")
 
-// Config is the setup for a particular client
-type Config struct {
-	S3Gateway     string
-	Satellite     string
-	AccessKey     string
-	SecretKey     string
-	APIKey        string
-	EncryptionKey string
-	NoSSL         bool
-}
-
-// Client is the common interface for different implementations
-type Client interface {
-	MakeBucket(bucket, location string) error
-	RemoveBucket(bucket string) error
-	ListBuckets() ([]string, error)
-
-	Upload(bucket, objectName string, data []byte) error
-	Download(bucket, objectName string, buffer []byte) ([]byte, error)
-	Delete(bucket, objectName string) error
-	ListObjects(bucket, prefix string) ([]string, error)
-}
-
 // Minio implements basic S3 Client with minio
 type Minio struct {
 	api *minio.Client
