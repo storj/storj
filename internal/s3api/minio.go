0d1d100cd45101bcca04c86e0d72ca304762de36
Cameron Ayer <camayer92@gmail.com>
2018-12-03 10:34:23 -0500

    move clients to internal/s3api

--- internal/s3api/minio.go
+++ internal/s3api/minio.go
@@ -1,7 +1,7 @@
 // Copyright (C) 2018 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-package main
+package s3api
 
 import (
 	"bytes"
@@ -14,6 +14,29 @@ import (
 
 // MinioError is class for minio errors
 var MinioError = errs.Class("minio error")
+
+// Config is the setup for a particular client
+type Config struct {
+	S3Gateway     string
+	Satellite     string
+	AccessKey     string
+	SecretKey     string
+	APIKey        string
+	EncryptionKey string
+	NoSSL         bool
+}
+
+// Client is the common interface for different implementations
+type Client interface {
+	MakeBucket(bucket, location string) error
+	RemoveBucket(bucket string) error
+	ListBuckets() ([]string, error)
+
+	Upload(bucket, objectName string, data []byte) error
+	Download(bucket, objectName string, buffer []byte) ([]byte, error)
+	Delete(bucket, objectName string) error
+	ListObjects(bucket, prefix string) ([]string, error)
+}
 
 // Minio implements basic S3 Client with minio
 type Minio struct {
