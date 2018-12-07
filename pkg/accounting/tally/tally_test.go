034a34069aa9645cb2997976f3e927ad66fe06fc
Michal Niewrzal <michal@storj.io>
2018-12-07 10:59:31 +0100

    Merge bwagreement db into satellite master db (#770)
    * Merge bwagreement db into satellite master db
    
    * adjust to recent tally changes
    
    * linter problems
    
    * linter problems
    
    * returning db structs in more optimal way
    
    * added pointer for assignment
    
    * error message changed
    
    * better param message

--- pkg/accounting/tally/tally_test.go
+++ pkg/accounting/tally/tally_test.go
@@ -13,10 +13,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+
 	"storj.io/storj/internal/identity"
 	"storj.io/storj/internal/teststorj"
 	"storj.io/storj/pkg/accounting"
-	dbManager "storj.io/storj/pkg/bwagreement/database-manager"
+	"storj.io/storj/pkg/bwagreement"
 	"storj.io/storj/pkg/bwagreement/test"
 	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/overlay"
@@ -24,6 +25,7 @@ import (
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/pointerdb"
 	"storj.io/storj/pkg/storj"
+	"storj.io/storj/satellite/satellitedb"
 	"storj.io/storj/storage/teststore"
 )
 
@@ -61,11 +63,11 @@ func TestOnlineNodes(t *testing.T) {
 	assert.NoError(t, err)
 	defer func() { _ = accountingDb.Close() }()
 
-	bwDb, err := dbManager.NewDBManager("sqlite3", "file::memory:?mode=memory&cache=shared")
+	masterDB, err := satellitedb.NewDB("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
-	defer func() { _ = accountingDb.Close() }()
+	defer func() { _ = masterDB.Close() }()
 
-	tally := newTally(logger, accountingDb, bwDb, pointerdb, overlayServer, kad, limit, interval)
+	tally := newTally(logger, accountingDb, masterDB.BandwidthAgreement(), pointerdb, overlayServer, kad, limit, interval)
 
 	online, err := tally.onlineNodes(ctx, nodeIDs)
 	assert.NoError(t, err)
@@ -92,10 +94,12 @@ func TestQueryNoAgreements(t *testing.T) {
 	accountingDb, err := accounting.NewDb("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
 	defer func() { _ = accountingDb.Close() }()
-	bwDb, err := dbManager.NewDBManager("sqlite3", "file::memory:?mode=memory&cache=shared")
+
+	masterDB, err := satellitedb.NewDB("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
-	defer func() { _ = accountingDb.Close() }()
-	tally := newTally(zap.NewNop(), accountingDb, bwDb, pointerdb, overlayServer, kad, 0, time.Second)
+	defer func() { _ = masterDB.Close() }()
+
+	tally := newTally(zap.NewNop(), accountingDb, masterDB.BandwidthAgreement(), pointerdb, overlayServer, kad, 0, time.Second)
 
 	//check the db
 	err = tally.Query(ctx)
@@ -110,9 +114,14 @@ func TestQueryWithBw(t *testing.T) {
 	accountingDb, err := accounting.NewDb("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
 	defer func() { _ = accountingDb.Close() }()
-	bwDb, err := dbManager.NewDBManager("sqlite3", "file::memory:?mode=memory&cache=shared")
+
+	masterDB, err := satellitedb.NewDB("sqlite3://file::memory:?mode=memory&cache=shared")
 	assert.NoError(t, err)
-	defer func() { _ = accountingDb.Close() }()
+	err = masterDB.CreateTables()
+	assert.NoError(t, err)
+	defer func() { _ = masterDB.Close() }()
+
+	bwDb := masterDB.BandwidthAgreement()
 	tally := newTally(zap.NewNop(), accountingDb, bwDb, pointerdb, overlayServer, kad, 0, time.Second)
 
 	//get a private key
@@ -126,7 +135,7 @@ func TestQueryWithBw(t *testing.T) {
 	rba, err := test.GenerateRenterBandwidthAllocation(pba, k)
 	assert.NoError(t, err)
 	//save to db
-	_, err = bwDb.Create(ctx, rba)
+	err = bwDb.CreateAgreement(ctx, bwagreement.Agreement{Signature: rba.GetSignature(), Agreement: rba.GetData()})
 	assert.NoError(t, err)
 
 	//check the db
--- pkg/bwagreement/database-manager/dbx/gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-package bwagreement
-
-//go:generate dbx.v1 golang -d postgres -d sqlite3  bwagreement.dbx .
-//go:generate dbx.v1 schema -d postgres -d sqlite3  bwagreement.dbx .
