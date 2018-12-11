1ed0b0609d78744ceee3224523120a6638eaaa06
Egon Elbre <egonelbre@gmail.com>
2018-12-11 11:24:08 +0200

    Reduce number of storage nodes

--- pkg/miniogw/integration_test.go
+++ pkg/miniogw/integration_test.go
@@ -30,7 +30,7 @@ func TestUploadDownload(t *testing.T) {
 	ctx := testcontext.New(t)
 	defer ctx.Cleanup()
 
-	planet, err := testplanet.New(t, 1, 30, 0)
+	planet, err := testplanet.New(t, 1, 4, 0)
 	assert.NoError(t, err)
 
 	defer ctx.Check(planet.Shutdown)
