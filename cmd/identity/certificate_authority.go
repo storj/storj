e735957ec159cffec96f993fd3d176ca2996b956
Bryan White <bryanchriswhite@gmail.com>
2018-12-12 19:45:29 -0500

    wip

--- cmd/identity/certificate_authority.go
+++ cmd/identity/certificate_authority.go
@@ -5,8 +5,13 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
+	"storj.io/storj/pkg/peertls"
 
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/process"
@@ -29,6 +34,11 @@ var (
 		Short: "Get the id of a CA",
 		RunE:  cmdGetID,
 	}
+	revokeCACmd = &cobra.Command{
+		Use:   "revoke",
+		Short: "Revoke the identity's leaf certificate (creates backup)",
+		RunE:  cmdRevokeCA,
+	}
 
 	newCACfg struct {
 		CA provider.CASetupConfig
@@ -37,14 +47,22 @@ var (
 	getIDCfg struct {
 		CA provider.PeerCAConfig
 	}
+
+	revokeCACfg struct {
+		CA       provider.FullCAConfig
+		Identity provider.IdentityConfig
+		// TODO: add "broadcast" option to send revocation to network nodes
+	}
 )
 
 func init() {
 	rootCmd.AddCommand(caCmd)
 	caCmd.AddCommand(newCACmd)
-	caCmd.AddCommand(getIDCmd)
 	cfgstruct.Bind(newCACmd.Flags(), &newCACfg, cfgstruct.ConfDir(defaultConfDir))
+	caCmd.AddCommand(getIDCmd)
 	cfgstruct.Bind(getIDCmd.Flags(), &getIDCfg, cfgstruct.ConfDir(defaultConfDir))
+	idCmd.AddCommand(revokeCACmd)
+	cfgstruct.Bind(revokeCACmd.Flags(), &revokeCACfg, cfgstruct.ConfDir(defaultConfDir))
 }
 
 func cmdNewCA(cmd *cobra.Command, args []string) error {
@@ -59,5 +77,35 @@ func cmdGetID(cmd *cobra.Command, args []string) (err error) {
 	}
 
 	fmt.Println(p.ID.String())
+	return nil
+}
+
+func cmdRevokeCA(cmd *cobra.Command, args []string) (err error) {
+	ca, err := revokeCACfg.CA.Load()
+	if err != nil {
+		return err
+	}
+
+	// NB: backup original cert
+	var backupCfg provider.FullCAConfig
+	extRegex, err := regexp.Compile(filepath.Ext(revokeCACfg.CA.CertPath) + "$")
+	backupCfg.CertPath = extRegex.ReplaceAllString(
+		revokeCACfg.Identity.CertPath,
+		strconv.Itoa(int(time.Now().Unix()))+".bak$1",
+	)
+	if err := backupCfg.Save(ca); err != nil {
+		return err
+	}
+
+	if err := peertls.AddRevocationExt(ca.Key, ca.Cert, ca.Cert); err != nil {
+		return err
+	}
+
+	updateCfg := provider.FullCAConfig{
+		CertPath: revokeCACfg.Identity.CertPath,
+	}
+	if err := updateCfg.Save(ca); err != nil {
+		return err
+	}
 	return nil
 }
--- cmd/identity/identity.go
+++ cmd/identity/identity.go
@@ -4,7 +4,13 @@
 package main
 
 import (
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"time"
+
 	"github.com/spf13/cobra"
+	"storj.io/storj/pkg/peertls"
 
 	"storj.io/storj/pkg/cfgstruct"
 	"storj.io/storj/pkg/provider"
@@ -21,10 +27,21 @@ var (
 		Short: "Creates a new identity from an existing certificate authority",
 		RunE:  cmdNewID,
 	}
+	revokeLeafCmd = &cobra.Command{
+		Use:   "revoke",
+		Short: "Revoke the identity's leaf certificate (creates backup)",
+		RunE:  cmdRevokeLeaf,
+	}
 
 	newIDCfg struct {
 		CA       provider.FullCAConfig
 		Identity provider.IdentitySetupConfig
+	}
+
+	revokeLeafCfg struct {
+		CA       provider.FullCAConfig
+		Identity provider.IdentityConfig
+		// TODO: add "broadcast" option to send revocation to network nodes
 	}
 )
 
@@ -32,6 +49,8 @@ func init() {
 	rootCmd.AddCommand(idCmd)
 	idCmd.AddCommand(newIDCmd)
 	cfgstruct.Bind(newIDCmd.Flags(), &newIDCfg, cfgstruct.ConfDir(defaultConfDir))
+	idCmd.AddCommand(revokeLeafCmd)
+	cfgstruct.Bind(revokeLeafCmd.Flags(), &revokeLeafCfg, cfgstruct.ConfDir(defaultConfDir))
 }
 
 func cmdNewID(cmd *cobra.Command, args []string) (err error) {
@@ -46,4 +65,43 @@ func cmdNewID(cmd *cobra.Command, args []string) (err error) {
 		return err
 	}
 	return provider.ErrSetup.New("identity file(s) exist: %s", s)
+}
+
+func cmdRevokeLeaf(cmd *cobra.Command, args []string) (err error) {
+ca, err := revokeLeafCfg.CA.Load()
+	if err != nil {
+		return err
+	}
+	originalIdent, err := revokeLeafCfg.Identity.Load()
+	if err != nil {
+		return err
+	}
+
+	updatedIdent, err := ca.NewIdentity()
+	if err != nil {
+		return err
+	}
+
+	if err := peertls.AddRevocationExt(ca.Key, originalIdent.Leaf, updatedIdent.Leaf); err != nil {
+		return err
+	}
+
+	// NB: backup original cert
+	var backupCfg provider.IdentityConfig
+	extRegex, err := regexp.Compile(filepath.Ext(revokeLeafCfg.Identity.CertPath) + "$")
+	backupCfg.CertPath = extRegex.ReplaceAllString(
+		revokeLeafCfg.Identity.CertPath,
+		strconv.Itoa(int(time.Now().Unix()))+".bak$1",
+	)
+	if err := backupCfg.Save(updatedIdent); err != nil {
+		return err
+	}
+
+	updateCfg := provider.IdentityConfig{
+		CertPath: revokeLeafCfg.Identity.CertPath,
+	}
+	if err := updateCfg.Save(updatedIdent); err != nil {
+		return err
+	}
+	return nil
 }
