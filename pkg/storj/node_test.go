506c55401994d07e8b0a70fffc3697cfeb6bd7e1
Egon Elbre <egonelbre@gmail.com>
2018-12-14 12:33:49 +0200

    fix typo

--- pkg/storj/node_test.go
+++ pkg/storj/node_test.go
@@ -14,9 +14,9 @@ func TestNodeID_Difficulty(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, uint16(0), difficulty)
 
-	// node id with difficulty 12
-	node12 := storj.NodeID{253, 160, 157, 107, 237, 151, 13, 122, 56, 254, 115, 137, 205, 43, 27, 150, 32, 207, 14, 161, 252, 218, 36, 4, 211, 83, 195, 250, 17, 61, 224, 0}
-	difficulty, err = node12.Difficulty()
+	// node id with difficulty 13
+	node13 := storj.NodeID{253, 160, 157, 107, 237, 151, 13, 122, 56, 254, 115, 137, 205, 43, 27, 150, 32, 207, 14, 161, 252, 218, 36, 4, 211, 83, 195, 250, 17, 61, 224, 0}
+	difficulty, err = node13.Difficulty()
 	assert.NoError(t, err)
-	assert.Equal(t, uint16(4), difficulty)
+	assert.Equal(t, uint16(13), difficulty)
 }
