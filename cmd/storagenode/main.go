f368ea7f608cbb2aca25f5e5d253735ae22b808e
littleskunk <jens.heimbuerge@googlemail.com>
2018-12-19 19:43:37 -0500

    add overwrite flag

--- cmd/storagenode/main.go
+++ cmd/storagenode/main.go
@@ -57,6 +57,7 @@ var (
 	setupCfg struct {
 		CA       provider.CASetupConfig
 		Identity provider.IdentitySetupConfig
+		Overwrite bool `default:"false" help:"whether to overwrite pre-existing configuration files"`
 	}
 	diagCfg struct {
 	}
@@ -110,6 +111,18 @@ func cmdSetup(cmd *cobra.Command, args []string) (err error) {
 	setupDir, err := filepath.Abs(*confDir)
 	if err != nil {
 		return err
+	}
+
+	valid, err := fpath.IsValidSetupDir(setupDir)
+	if !setupCfg.Overwrite && !valid {
+		fmt.Printf("storagenode configuration already exists (%v). rerun with --overwrite\n", setupDir)
+		return nil
+	} else if setupCfg.Overwrite && err == nil {
+		fmt.Println("overwriting existing satellite config")
+		err = os.RemoveAll(setupDir)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = os.MkdirAll(setupDir, 0700)
