e9fdc0b7d8ec917b1f50cd339c89ecb15e95376d
Egon Elbre <egonelbre@gmail.com>
2019-01-04 16:13:08 +0200

    dev

--- scripts/test-storj-sdk.sh
+++ scripts/test-storj-sdk.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -ueo pipefail
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 go install -v storj.io/storj/cmd/{storj-sdk,satellite,storagenode,uplink,gateway}
 
-storj-sdk network setup
-storj-sdk network test ./test-storj-upload.sh
+#setup tmpdir for testfiles and cleanup
+TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
+
+storj-sdk --config-dir $TMP network setup
+storj-sdk --config-dir $TMP network test  bash $SCRIPTDIR/test-storj-sdk-aws.sh
+
+rm -rf $TMP
