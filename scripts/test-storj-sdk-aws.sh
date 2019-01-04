9d246c13159fc838072a0b534a6022edf7538111
Egon Elbre <egonelbre@gmail.com>
2019-01-04 18:55:28 +0200

    add scheme to argument

--- scripts/test-storj-sdk-aws.sh
+++ scripts/test-storj-sdk-aws.sh
@@ -16,23 +16,23 @@ head -c 1024 </dev/urandom > $SRC_DIR/small-upload-testfile      # create 1mb fi
 head -c 5120 </dev/urandom > $SRC_DIR/big-upload-testfile        # create 5mb file of random bytes (remote)
 head -c 5    </dev/urandom > $SRC_DIR/multipart-upload-testfile  # create 5kb file of random bytes (remote)
 
-aws s3 --endpoint=$GATEWAY_0_ADDR mb s3://bucket
+aws s3 --endpoint=http://$GATEWAY_0_ADDR mb s3://bucket
 
 aws configure set default.s3.multipart_threshold 1TB
-aws s3 --endpoint=$GATEWAY_0_ADDR cp $SRC_DIR/small-upload-testfile s3://bucket/small-testfile
-aws s3 --endpoint=$GATEWAY_0_ADDR cp $SRC_DIR/big-upload-testfile   s3://bucket/big-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp $SRC_DIR/small-upload-testfile s3://bucket/small-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp $SRC_DIR/big-upload-testfile   s3://bucket/big-testfile
 
 # Wait 5 seconds to trigger any error related to one of the different intervals
 sleep 5
 
 aws configure set default.s3.multipart_threshold 4KB
-aws s3 --endpoint=$GATEWAY_0_ADDR cp $SRC_DIR/multipart-upload-testfile s3://bucket/multipart-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp $SRC_DIR/multipart-upload-testfile s3://bucket/multipart-testfile
 
-aws s3 --endpoint=$GATEWAY_0_ADDR ls s3://bucket
-aws s3 --endpoint=$GATEWAY_0_ADDR cp s3://bucket/small-testfile     $DST_DIR/small-download-testfile
-aws s3 --endpoint=$GATEWAY_0_ADDR cp s3://bucket/big-testfile       $DST_DIR/big-download-testfile
-aws s3 --endpoint=$GATEWAY_0_ADDR cp s3://bucket/multipart-testfile $DST_DIR/multipart-download-testfile
-aws s3 --endpoint=$GATEWAY_0_ADDR rb s3://bucket --force
+aws s3 --endpoint=http://$GATEWAY_0_ADDR ls s3://bucket
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp s3://bucket/small-testfile     $DST_DIR/small-download-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp s3://bucket/big-testfile       $DST_DIR/big-download-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR cp s3://bucket/multipart-testfile $DST_DIR/multipart-download-testfile
+aws s3 --endpoint=http://$GATEWAY_0_ADDR rb s3://bucket --force
 
 if cmp $SRC_DIR/small-upload-testfile $DST_DIR/small-download-testfile
 then
