ff174e7e87cffec721d623b97ce83d5923b7c280
Egon Elbre <egonelbre@gmail.com>
2019-01-04 16:13:30 +0200

    dev

--- scripts/test-storj-sdk.sh
+++ scripts/test-storj-sdk.sh
@@ -5,7 +5,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 go install -v storj.io/storj/cmd/{storj-sdk,satellite,storagenode,uplink,gateway}
 
-#setup tmpdir for testfiles and cleanup
+# setup tmpdir for testfiles and cleanup
 TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
 
 storj-sdk --config-dir $TMP network setup
