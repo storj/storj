d146f95a68a558ff7fdd33b8b448cc884e066456
Egon Elbre <egonelbre@gmail.com>
2019-01-04 19:06:32 +0200

    use traps to cleanup

--- scripts/test-storj-sdk.sh
+++ scripts/test-storj-sdk.sh
@@ -7,8 +7,14 @@ go install -race -v storj.io/storj/cmd/{storj-sdk,satellite,storagenode,uplink,g
 
 # setup tmpdir for testfiles and cleanup
 TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
+cleanup(){
+	rm -rf "$TMP"
+}
+trap cleanup EXIT
 
+
+# setup the network
 storj-sdk -x --config-dir $TMP network setup
-storj-sdk -x --config-dir $TMP network test  bash $SCRIPTDIR/test-storj-sdk-aws.sh
 
-rm -rf $TMP
+# run test-storj-sdk-aws.sh case
+storj-sdk -x --config-dir $TMP network test bash $SCRIPTDIR/test-storj-sdk-aws.sh
