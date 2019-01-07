e1a5d2eaba613b47c6afafd6398b4e6c2a4fe478
Egon Elbre <egonelbre@gmail.com>
2019-01-07 15:44:45 +0200

    Add fence implementation

---
+++ internal/sync2/fence_test.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information
+
+package sync2_test
+
+import (
+	"errors"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/storj/internal/sync2"
+)
+
+func TestFence(t *testing.T) {
+	var group errgroup.Group
+	var fence sync2.Fence
+	var done int32
+
+	for i := 0; i < 10; i++ {
+		group.Go(func() error {
+			fence.Wait()
+			if atomic.LoadInt32(&done) == 0 {
+				return errors.New("fence not yet released")
+			}
+			return nil
+		})
+	}
+
+	time.Sleep(time.Second)
+
+	for i := 0; i < 3; i++ {
+		group.Go(func() error {
+			atomic.StoreInt32(&done, 1)
+			fence.Release()
+			return nil
+		})
+	}
+
+	if err := group.Wait(); err != nil {
+		t.Fatal(err)
+	}
+}
