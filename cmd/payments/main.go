e7d15a817fb39d8b0daff942dd2ddf81c16ad9f9
Jennifer Johnson <jennifer@storj.io>
2019-01-09 17:08:18 -0500

    writes to csv

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -5,15 +5,17 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/csv"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
+
 	"storj.io/storj/pkg/process"
 )
 
 var (
-	ctx = context.Background()
-
+	ctx     = context.Background()
 	rootCmd = &cobra.Command{Use: "payments"}
 
 	cmdGenerate = &cobra.Command{
@@ -33,7 +35,28 @@ func generateCSV(cmd *cobra.Command, args []string) error {
 	return query(args[0], args[1])
 }
 
-func query(a, b string) error {
-	fmt.Printf("a: %v, b: %v \n", a, b)
+func query(startTime, endTime string) error {
+	cols := [][]string{
+		{"nodeID", "nodeIDCreationDate", "nodeStatus", "walletAddress", "GBAtRest", "GBBWRepair", "GBBWAudit", "GBBWDownload", "start", "end", "satelliteID"},
+	}
+
+	file, err := os.Create("./out/" + startTime + "-" + endTime + ".csv")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := csv.NewWriter(file)
+
+	for _, record := range cols {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
+	w.Flush()
+
 	return nil
 }
