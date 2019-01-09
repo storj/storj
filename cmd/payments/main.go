f1c8fee0a0fc83e6e33c3db756928a94ea14bbce
Jennifer Johnson <jennifer@storj.io>
2019-01-09 18:01:40 -0500

    wip

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -6,16 +6,19 @@ package main
 import (
 	"context"
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	"storj.io/storj/pkg/process"
 )
 
 var (
-	ctx     = context.Background()
+	ctx = context.Background()
+
+	port   string
+	apiKey string
+
 	rootCmd = &cobra.Command{Use: "payments"}
 
 	cmdGenerate = &cobra.Command{
@@ -27,19 +30,23 @@ var (
 )
 
 func main() {
+	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", ":7778", "satellite port")
+	rootCmd.PersistentFlags().StringVarP(&apiKey, "apikey", "a", "abc123", "satellite api key")
 	rootCmd.AddCommand(cmdGenerate)
-	process.Exec(rootCmd)
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
 }
 
 func generateCSV(cmd *cobra.Command, args []string) error {
-	return query(args[0], args[1])
-}
+	//TODO check validity of args
 
-func query(startTime, endTime string) error {
-	cols := [][]string{
-		{"nodeID", "nodeIDCreationDate", "nodeStatus", "walletAddress", "GBAtRest", "GBBWRepair", "GBBWAudit", "GBBWDownload", "start", "end", "satelliteID"},
-	}
+	startTime := args[0]
+	endTime := args[1]
 
+	headers := []string{"nodeID", "nodeIDCreationDate", "nodeStatus", "walletAddress", "GBAtRest", "GBBWRepair", "GBBWAudit", "GBBWDownload", "start", "end", "satelliteID"}
 	file, err := os.Create("./out/" + startTime + "-" + endTime + ".csv")
 	if err != nil {
 		return err
@@ -47,16 +54,25 @@ func query(startTime, endTime string) error {
 	defer file.Close()
 
 	w := csv.NewWriter(file)
-
-	for _, record := range cols {
-		if err := w.Write(record); err != nil {
-			log.Fatalln("error writing record to csv:", err)
-		}
+	if err := w.Write(headers); err != nil {
+		log.Fatalln("error writing headers to csv:", err)
 	}
+
+	qErr := query(startTime, endTime)
+	if qErr != nil {
+		return err
+	}
+
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
 	}
 	w.Flush()
+
+	return query(args[0], args[1])
+}
+
+func query(startTime, endTime string) error {
+	//maybe return queried result
 
 	return nil
 }
