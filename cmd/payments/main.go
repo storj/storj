3369b8e79c592788b8bc517030a06bb7e3a9bc53
Jennifer Johnson <jennifer@storj.io>
2019-01-09 13:49:08 -0500

    wip

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -3,6 +3,28 @@
 
 package main
 
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	ctx = context.Background()
+
+	rootCmd = &cobra.Command{Use: "payments"}
+
+	cmdGenerate = &cobra.Command{
+		Use:   "generate",
+		Short: "generates payment csv",
+		Args:  cobra.MinimumNArgs(2),
+		Run:   generateCSV,
+	}
+)
+
 func main() {
-	
+}
+
+func generateCSV(cmd *cobra.Command, args []string) {
+
 }
