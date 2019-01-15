b2c09395aa570e6212261c88fe4ba51ba4a8e980
Jennifer Johnson <jennifer@storj.io>
2019-01-15 10:04:16 -0500

    wip

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -7,25 +7,40 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/zeebo/errs"
 
 	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/provider"
+	"storj.io/storj/pkg/transport"
 )
 
 var (
 	ctx = context.Background()
+	// ErrPaymentsDial throws when there are errors dialing the payments client
+	ErrPaymentsDial = errs.Class("error dialing payments client:")
 
-	port   string
-	apiKey string
+	// ErrRequest is for gRPC request errors after dialing
+	ErrRequest = errs.Class("error processing request:")
+
+	// ErrIdentity is for errors during identity creation for this CLI
+	ErrIdentity = errs.Class("error creating identity:")
+
+	// ErrArgs throws when there are errors with CLI args
+	ErrArgs = errs.Class("error with CLI args:")
+
+	port string
+	// apiKey string
 
 	rootCmd = &cobra.Command{Use: "payments"}
 
 	cmdGenerate = &cobra.Command{
-		Use:   "generateCSV",
-		Short: "generates payment csv",
+		Use:   "GenerateCSV",
+		Short: "Generates payment csv",
 		Args:  cobra.MinimumNArgs(2),
-		RunE:  generateCSV,
+		RunE:  GenerateCSV,
 	}
 )
 
@@ -36,7 +51,7 @@ type Payments struct {
 
 func main() {
 	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", ":7778", "satellite port")
-	rootCmd.PersistentFlags().StringVarP(&apiKey, "apikey", "a", "abc123", "satellite api key")
+	// rootCmd.PersistentFlags().StringVarP(&apiKey, "apikey", "a", "abc123", "satellite api key")
 	rootCmd.AddCommand(cmdGenerate)
 	err := rootCmd.Execute()
 	if err != nil {
@@ -45,38 +60,32 @@ func main() {
 	}
 }
 
-func NewPayments(address string) (*Payments, error) {
-	ctx := context.Background()
+// NewPayments creates a payments object
+func NewPayments() (*Payments, error) {
+	identity, err := provider.NewFullIdentity(ctx, 12, 4)
+	if err != nil {
+		return &Payments{}, ErrIdentity.Wrap(err)
+	}
 
+	tc := transport.NewClient(identity)
+	conn, err := tc.DialAddress(ctx, port)
+	if err != nil {
+		return &Payments{}, ErrPaymentsDial.Wrap(err)
+	}
+
+	c := pb.NewPaymentsClient(conn)
+	return &Payments{client: c}, nil
 }
 
-// func NewInspector(address string) (*Inspector, error) {
-// 	ctx := context.Background()
-// 	identity, err := provider.NewFullIdentity(ctx, 12, 4)
-// 	if err != nil {
-// 		return &Inspector{}, ErrIdentity.Wrap(err)
-// 	}
-
-// 	tc := transport.NewClient(identity)
-// 	conn, err := tc.DialAddress(ctx, address)
-// 	if err != nil {
-// 		return &Inspector{}, ErrInspectorDial.Wrap(err)
-// 	}
-
-// 	c := pb.NewInspectorClient(conn)
-
-// 	return &Inspector{
-// 		identity: identity,
-// 		client:   c,
-// 	}, nil
-// }
-
-
-func generateCSV(cmd *cobra.Command, args []string) error {
+// GenerateCSV makes a call to the payments client to query the db and generate a csv
+func GenerateCSV(cmd *cobra.Command, args []string) error {
 	//TODO check validity of args
-
+	p, err := NewPayments()
 	startTime := args[0]
 	endTime := args[1]
-	pc := pb.NewPaymentsClient()
+	start := time.Now()
+	end := time.Now()
+
+	p.client.GenerateCSV(ctx, start, end)
 	// return query(args[0], args[1])
 }
--- pkg/payments/server.go
+++ pkg/payments/server.go
@@ -5,9 +5,6 @@ package payments
 
 import (
 	"context"
-	"encoding/csv"
-	"log"
-	"os"
 
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
@@ -50,28 +47,28 @@ func (srv *Server) AdjustPrices(ctx context.Context, req *pb.AdjustPricesRequest
 
 // GenerateCSV creates a csv file for payment purposes
 func (srv *Server) GenerateCSV(ctx context.Context, req *pb.GenerateCSVRequest) (*pb.GenerateCSVResponse, error) {
-	rows, err := srv.accountingDB.QueryPaymentInfo(ctx, req.startTime, req.endTime)
+	// rows, err := srv.accountingDB.QueryPaymentInfo(ctx, req.startTime, req.endTime)
 
-	headers := []string{"nodeID", "nodeIDCreationDate", "nodeStatus", "walletAddress", "GBAtRest", "GBBWRepair", "GBBWAudit", "GBBWDownload", "start", "end", "satelliteID"}
-	file, err := os.Create(srv.filepath + req.startTime + "-" + req.endTime + ".csv")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	w := csv.NewWriter(file)
-	if err := w.Write(headers); err != nil {
-		log.Fatalln("error writing headers to csv:", err)
-	}
-
-	// qErr := query(startTime, endTime)
-	// if qErr != nil {
+	// headers := []string{"nodeID", "nodeIDCreationDate", "nodeStatus", "walletAddress", "GBAtRest", "GBBWRepair", "GBBWAudit", "GBBWDownload", "start", "end", "satelliteID"}
+	// file, err := os.Create(srv.filepath + req.startTime + "-" + req.endTime + ".csv")
+	// if err != nil {
 	// 	return err
 	// }
+	// defer file.Close()
 
-	if err := w.Error(); err != nil {
-		log.Fatal(err)
-	}
-	w.Flush()
+	// w := csv.NewWriter(file)
+	// if err := w.Write(headers); err != nil {
+	// 	log.Fatalln("error writing headers to csv:", err)
+	// }
+
+	// // qErr := query(startTime, endTime)
+	// // if qErr != nil {
+	// // 	return err
+	// // }
+
+	// if err := w.Error(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// w.Flush()
 	return &pb.GenerateCSVResponse{}, nil
 }
