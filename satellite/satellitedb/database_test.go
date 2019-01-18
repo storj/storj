abeeb51fe4d60b118468266c53f2b0b630affd77
Bill Thorp <billt@storj.io>
2019-01-18 11:35:00 -0500

    fixed import cycle

--- satellite/satellitedb/database_test.go
+++ satellite/satellitedb/database_test.go
@@ -1,13 +1,12 @@
 // Copyright (C) 2018 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-package satellitedb
+package satellitedb_test
 
 import (
 	"testing"
 
 	"storj.io/storj/satellite"
-	"storj.io/storj/satellite/satellitedb"
 	"storj.io/storj/satellite/satellitedb/satellitedbtest"
 )
 
