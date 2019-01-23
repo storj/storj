6848383cbb858b69c5865a79093670cb2cf3fa4c
Egon Elbre <egonelbre@gmail.com>
2019-01-23 13:31:52 +0200

    add startup message

--- satellite/peer.go
+++ satellite/peer.go
@@ -340,6 +340,8 @@ func (peer *Peer) Run(ctx context.Context) error {
 		return ignoreCancel(peer.Repair.Repairer.Run(ctx))
 	})
 	group.Go(func() error {
+		// TODO: move the message into Server instead
+		peer.Log.Sugar().Infof("Node %s started on %s", peer.Identity.ID, peer.Public.Server.Addr().String())
 		return ignoreCancel(peer.Public.Server.Run(ctx))
 	})
 
