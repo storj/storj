a45e0e83bd25bf5744146ec987b532b1859cc992
Egon Elbre <egonelbre@gmail.com>
2019-02-04 17:03:33 +0200

    fix typos in sed

--- scripts/test-sim.sh
+++ scripts/test-sim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ueo pipefail
+set +x
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -24,7 +25,7 @@ storj-sim -x network destroy
 # setup the network with ipv6
 storj-sim -x --host "::1" network setup
 # aws-cli doesn't support gateway with ipv6 address, so change it to use localhost
-find $STORJ_NETWORK_DIR/gateway -type f -exec sed -i 's/server.address: "[::1]/server.address: "127.0.0.1:/' \;
+find $STORJ_NETWORK_DIR/gateway -type f -name config.yaml -exec sed -i "s/server.address: \"\[::1\]/server.address: \"127.0.0.1/" {} +
 # run aws-cli tests using ipv6
 storj-sim -x --host "::1" network test bash $SCRIPTDIR/test-sim-aws.sh
 storj-sim -x network destroy
