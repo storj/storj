3331e3a2dae37b60a09b3e68c0734ee765fc626b
Cameron Ayer <camayer92@gmail.com>
2019-02-04 14:14:00 -0500

    add test tables, verify data from QueryPaymentInfo

--- pkg/accounting/rollup/rollup_test.go
+++ pkg/accounting/rollup/rollup_test.go
@@ -15,87 +15,145 @@ import (
 )
 
 func TestQueryOneDay(t *testing.T) {
-	ctx := testcontext.New(t)
-	defer ctx.Cleanup()
+	tests := []struct {
+		expected int
+		rest     float64
+		bw       []int64
+	}{
+		{
+			expected: 0,
+			rest:     float64(5000),
+			bw:       []int64{1000, 2000, 3000, 4000},
+		},
+	}
 
-	planet, err := testplanet.New(t, 1, 4, 0)
-	assert.NoError(t, err)
-	defer ctx.Check(planet.Shutdown)
+	for _, tt := range tests {
+		ctx := testcontext.New(t)
+		defer ctx.Cleanup()
 
-	planet.Start(ctx)
-	time.Sleep(2 * time.Second)
+		planet, err := testplanet.New(t, 1, 4, 0)
+		assert.NoError(t, err)
+		defer ctx.Check(planet.Shutdown)
 
-	nodeData, bwTotals := createData(planet)
+		planet.Start(ctx)
+		time.Sleep(2 * time.Second)
 
-	now := time.Now().UTC()
-	before := now.Add(time.Hour * -48)
+		nodeData, bwTotals := createData(planet, tt.rest, tt.bw)
 
-	err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, before, before, nodeData)
-	assert.NoError(t, err)
+		now := time.Now().UTC()
+		before := now.Add(time.Hour * -24)
 
-	err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, before, before, bwTotals)
-	assert.NoError(t, err)
+		err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, before, before, nodeData)
+		assert.NoError(t, err)
 
-	err = planet.Satellites[0].Accounting.Rollup.Query(ctx)
-	assert.NoError(t, err)
+		err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, before, before, bwTotals)
+		assert.NoError(t, err)
 
-	// rollup.Query cuts off the hr/min/sec before saving, we need to do the same when querying
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
-	before = time.Date(before.Year(), before.Month(), before.Day(), 0, 0, 0, 0, before.Location())
+		err = planet.Satellites[0].Accounting.Rollup.Query(ctx)
+		assert.NoError(t, err)
 
-	rows, err := planet.Satellites[0].DB.Accounting().QueryPaymentInfo(ctx, before, now)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(rows))
+		// rollup.Query cuts off the hr/min/sec before saving, we need to do the same when querying
+		now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+		before = time.Date(before.Year(), before.Month(), before.Day(), 0, 0, 0, 0, before.Location())
+
+		rows, err := planet.Satellites[0].DB.Accounting().QueryPaymentInfo(ctx, before, now)
+		assert.NoError(t, err)
+		assert.Equal(t, tt.expected, len(rows))
+	}
 }
 
 func TestQueryTwoDays(t *testing.T) {
-	ctx := testcontext.New(t)
-	defer ctx.Cleanup()
+	tests := []struct {
+		expected int
+		rest     float64
+		bw       []int64
+	}{
+		{
+			expected: 4,
+			rest:     float64(5000),
+			bw:       []int64{1000, 2000, 3000, 4000},
+		},
+	}
 
-	planet, err := testplanet.New(t, 1, 4, 0)
-	assert.NoError(t, err)
-	defer ctx.Check(planet.Shutdown)
+	for _, tt := range tests {
+		ctx := testcontext.New(t)
+		defer ctx.Cleanup()
 
-	planet.Start(ctx)
-	time.Sleep(2 * time.Second)
+		planet, err := testplanet.New(t, 1, 4, 0)
+		assert.NoError(t, err)
+		defer ctx.Check(planet.Shutdown)
 
-	nodeData, bwTotals := createData(planet)
+		planet.Start(ctx)
+		time.Sleep(2 * time.Second)
 
-	now := time.Now().UTC()
-	before := now.Add(time.Hour * -48)
+		nodeData, bwTotals := createData(planet, tt.rest, tt.bw)
 
-	err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, before, before, nodeData)
-	assert.NoError(t, err)
+		now := time.Now().UTC()
+		before := now.Add(time.Hour * -24)
 
-	err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, before, before, bwTotals)
-	assert.NoError(t, err)
+		// Save data for day 1
+		err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, before, before, nodeData)
+		assert.NoError(t, err)
 
-	err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, now, now, nodeData)
-	assert.NoError(t, err)
+		err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, before, before, bwTotals)
+		assert.NoError(t, err)
 
-	err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, now, now, bwTotals)
-	assert.NoError(t, err)
+		// Save data for day 2
+		err = planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, now, now, nodeData)
+		assert.NoError(t, err)
 
-	err = planet.Satellites[0].Accounting.Rollup.Query(ctx)
-	assert.NoError(t, err)
+		err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, now, now, bwTotals)
+		assert.NoError(t, err)
 
-	// rollup.Query cuts off the hr/min/sec before saving, we need to do the same when querying
-	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
-	before = time.Date(before.Year(), before.Month(), before.Day(), 0, 0, 0, 0, before.Location())
+		err = planet.Satellites[0].Accounting.Rollup.Query(ctx)
+		assert.NoError(t, err)
 
-	rows, err := planet.Satellites[0].DB.Accounting().QueryPaymentInfo(ctx, before, now)
-	assert.Equal(t, 4, len(rows))
-	assert.NoError(t, err)
+		// rollup.Query cuts off the hr/min/sec before saving, we need to do the same when querying
+		now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+		before = time.Date(before.Year(), before.Month(), before.Day(), 0, 0, 0, 0, before.Location())
+
+		rows, err := planet.Satellites[0].DB.Accounting().QueryPaymentInfo(ctx, before, now)
+		assert.Equal(t, 4, len(rows))
+		assert.NoError(t, err)
+
+		// verify data is correct
+		var nodeIDs []*storj.NodeID
+		for _, n := range planet.StorageNodes {
+			ptr := n.Identity.ID
+			nodeIDs = append(nodeIDs, &ptr)
+		}
+		for _, r := range rows {
+			assert.Equal(t, tt.bw[0], r.PutTotal)
+			assert.Equal(t, tt.bw[1], r.GetTotal)
+			assert.Equal(t, tt.bw[2], r.GetAuditTotal)
+			assert.Equal(t, tt.bw[3], r.GetRepairTotal)
+			assert.Equal(t, tt.rest, r.AtRestTotal)
+			assert.True(t, contains(r.NodeID, nodeIDs))
+		}
+	}
 }
 
-func createData(planet *testplanet.Planet) (nodeData map[storj.NodeID]float64, bwTotals map[storj.NodeID][]int64) {
+// contains
+func contains(entry storj.NodeID, list []*storj.NodeID) bool {
+	for i, n := range list {
+		if n == nil {
+			continue
+		}
+		if entry == *n {
+			list[i] = nil
+			return true
+		}
+	}
+	return false
+}
+
+func createData(planet *testplanet.Planet, atRest float64, bw []int64) (nodeData map[storj.NodeID]float64, bwTotals map[storj.NodeID][]int64) {
 	nodeData = make(map[storj.NodeID]float64)
 	bwTotals = make(map[storj.NodeID][]int64)
-	totals := []int64{1000, 2000, 3000, 4000, 5000}
 	for _, n := range planet.StorageNodes {
 		id := n.Identity.ID
-		nodeData[id] = float64(6000)
-		bwTotals[id] = totals
+		nodeData[id] = atRest
+		bwTotals[id] = bw
 	}
 	return nodeData, bwTotals
 }
