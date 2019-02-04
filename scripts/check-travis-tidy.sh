626b90ec238c9de8a4f6291982b0e8b04c495ff1
Michal Niewrzal <michal@storj.io>
2019-02-04 15:18:08 +0100

    fix check-travis-tidy

--- scripts/check-travis-tidy.sh
+++ scripts/check-travis-tidy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ueo pipefail
 
-CHANGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE -- go.mod go.sum)
+CHANGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE -- go.mod go.sum) || CHANGES="fail"
 
 if [ -z "$CHANGES" ]
 then
     echo "go modules not changed"
 else
-    echo "go module changes detected"
+    echo "go module changes detected: $CHANGES"
     gospace istidy
 fi
