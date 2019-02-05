07fe2e8e83e461c7935ae19945485d0e3b7b5b6a
Egon Elbre <egonelbre@gmail.com>
2019-02-05 20:36:22 +0200

    fix typo

--- scripts/setup-gopath.sh
+++ scripts/setup-gopath.sh
@@ -4,9 +4,9 @@ mkdir -p ~/bin ~/cache
 export PATH=~/bin:$PATH
 
 if [[ "${GO111MODULE}" == on ]]; then
-    export GOMODULES=auto
+    export GO111MODULE=auto
     mkdir -p $GOPATH/src/storj.io/storj
-    cp -r ${TRAVIS_BUILD_DIR} $GOPATH/src/storj.io/storj
+    cp -a -r ${TRAVIS_BUILD_DIR} $GOPATH/src/storj.io/storj
     export TRAVIS_BUILD_DIR=$GOPATH/src/storj.io/storj
     cd ${TRAVIS_BUILD_DIR}
 fi
