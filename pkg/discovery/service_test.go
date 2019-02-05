476ee21e4044cd88d17e5f47e4a47f83e6fb78bf
Egon Elbre <egonelbre@gmail.com>
2019-02-05 09:39:15 +0200

    Ensure discovery tests run against postgres

--- pkg/discovery/service_test.go
+++ pkg/discovery/service_test.go
@@ -14,23 +14,17 @@ import (
 )
 
 func TestCache_Refresh(t *testing.T) {
-	ctx := testcontext.New(t)
-	defer ctx.Cleanup()
+	testplanet.Run(t, testplanet.Config{
+		SatelliteCount: 1, StorageNodeCount: 10, UplinkCount: 0,
+	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		time.Sleep(5 * time.Second)
 
-	planet, err := testplanet.New(t, 1, 10, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer ctx.Check(planet.Shutdown)
-
-	planet.Start(ctx)
-	time.Sleep(5 * time.Second)
-
-	satellite := planet.Satellites[0]
-	for _, storageNode := range planet.StorageNodes {
-		node, err := satellite.Overlay.Service.Get(ctx, storageNode.ID())
-		if assert.NoError(t, err) {
-			assert.Equal(t, storageNode.Addr(), node.Address.Address)
+		satellite := planet.Satellites[0]
+		for _, storageNode := range planet.StorageNodes {
+			node, err := satellite.Overlay.Service.Get(ctx, storageNode.ID())
+			if assert.NoError(t, err) {
+				assert.Equal(t, storageNode.Addr(), node.Address.Address)
+			}
 		}
-	}
+	})
 }
