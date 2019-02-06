60b0275a440eadb6921bc6c5d3bf39fd945c5037
Egon Elbre <egonelbre@gmail.com>
2019-02-06 14:47:00 +0200

    Enable ipv6 tests (#1204)

--- scripts/test-sim.sh
+++ scripts/test-sim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ueo pipefail
+set +x
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -12,7 +13,7 @@ cleanup(){
 }
 trap cleanup EXIT
 
-export STORJ_LOCAL_NETWORK=$TMP
+export STORJ_NETWORK_DIR=$TMP
 
 # setup the network
 storj-sim -x network setup
@@ -21,9 +22,10 @@ storj-sim -x network setup
 storj-sim -x network test bash $SCRIPTDIR/test-sim-aws.sh
 storj-sim -x network destroy
 
-# ipv6 tests disabled because aws-cli doesn't seem to support connecting to ipv6 host
-# # setup the network with ipv6
-# storj-sim -x --host "::1" network setup
-# # run aws-cli tests using ipv6
-# storj-sim -x --host "::1" network test bash $SCRIPTDIR/test-storj-sim-aws.sh
-# storj-sim -x network destroy
+# setup the network with ipv6
+storj-sim -x --host "::1" network setup
+# aws-cli doesn't support gateway with ipv6 address, so change it to use localhost
+find $STORJ_NETWORK_DIR/gateway -type f -name config.yaml -exec sed -i "s/server.address: \"\[::1\]/server.address: \"127.0.0.1/" {} +
+# run aws-cli tests using ipv6
+storj-sim -x --host "::1" network test bash $SCRIPTDIR/test-sim-aws.sh
+storj-sim -x network destroy
