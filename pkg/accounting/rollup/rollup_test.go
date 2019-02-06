ba47f9e58414619e057e43cb011f5447a37f2fed
Cameron Ayer <camayer92@gmail.com>
2019-02-06 13:32:55 -0500

    merge test cases

--- pkg/accounting/rollup/rollup_test.go
+++ pkg/accounting/rollup/rollup_test.go
@@ -20,19 +20,59 @@ type testFlags struct {
 	bw     []int64
 }
 
-func TestOneDay(t *testing.T) {
+func TestQuery(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 10, UplinkCount: 0,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
-		flags := &testFlags{
-			days:   1,
-			atRest: float64(5000),
-			bw:     []int64{1000, 2000, 3000, 4000},
-		}
+		days := 5
+		atRest := float64(5000)
+		bw := []int64{1000, 2000, 3000, 4000}
 
 		time.Sleep(time.Second * 2)
 
-		saveAndQuery(t, ctx, flags, planet)
+		nodeData, bwTotals := createData(planet, atRest, bw)
+
+		// Set timestamp back by the number of days we want to save
+		timestamp := time.Now().UTC().AddDate(0, 0, -1*days)
+		start := timestamp
+
+		for i := 1; i <= days; i++ {
+			err := planet.Satellites[0].DB.Accounting().SaveAtRestRaw(ctx, timestamp, timestamp, nodeData)
+			assert.NoError(t, err)
+
+			err = planet.Satellites[0].DB.Accounting().SaveBWRaw(ctx, timestamp, timestamp, bwTotals)
+			assert.NoError(t, err)
+
+			err = planet.Satellites[0].Accounting.Rollup.Query(ctx)
+			assert.NoError(t, err)
+
+			// Advance time by 24 hours
+			timestamp = timestamp.Add(time.Hour * 24)
+			end := timestamp
+
+			// rollup.Query cuts off the hr/min/sec before saving, we need to do the same when querying
+			start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+			end = time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, end.Location())
+
+			rows, err := planet.Satellites[0].DB.Accounting().QueryPaymentInfo(ctx, start, end)
+			assert.NoError(t, err)
+			if i == 1 {
+				assert.Equal(t, 0, len(rows))
+				return
+			}
+			// TODO: once we sum data totals by node ID across rollups, number of rows should be number of nodes
+			assert.Equal(t, (i-1)*len(planet.StorageNodes), len(rows))
+
+			// verify data is correct
+			for _, r := range rows {
+				assert.Equal(t, bw[0], r.PutTotal)
+				assert.Equal(t, bw[1], r.GetTotal)
+				assert.Equal(t, bw[2], r.GetAuditTotal)
+				assert.Equal(t, bw[3], r.GetRepairTotal)
+				assert.Equal(t, atRest, r.AtRestTotal)
+				assert.NotNil(t, nodeData[r.NodeID])
+			}
+		}
 	})
 }
 
