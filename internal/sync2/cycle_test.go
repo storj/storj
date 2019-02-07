e9dcd728d9ad64a13241adc9de72ad42912d7c01
Egon Elbre <egonelbre@gmail.com>
2019-02-07 19:10:22 +0200

    test inplace and pointer usage

--- internal/sync2/cycle_test.go
+++ internal/sync2/cycle_test.go
@@ -17,57 +17,68 @@ import (
 
 func TestCycle_Basic(t *testing.T) {
 	ctx := context.Background()
-	cycle := sync2.NewCycle(time.Second)
-	count := int64(0)
 
-	var group errgroup.Group
+	var inplace sync2.Cycle
+	inplace.SetInterval(time.Second)
+	var pointer = sync2.NewCycle(time.Second)
 
-	start := time.Now()
+	for _, cycle := range []*sync2.Cycle{pointer, &inplace} {
+		cycle := cycle
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
 
-	cycle.Start(ctx, &group, func(ctx context.Context) error {
-		atomic.AddInt64(&count, 1)
-		return nil
-	})
+			count := int64(0)
 
-	group.Go(func() error {
-		defer cycle.Stop()
+			var group errgroup.Group
 
-		const expected = 10
-		cycle.Pause()
+			start := time.Now()
 
-		startingCount := atomic.LoadInt64(&count)
-		for i := 0; i < expected-1; i++ {
+			cycle.Start(ctx, &group, func(ctx context.Context) error {
+				atomic.AddInt64(&count, 1)
+				return nil
+			})
+
+			group.Go(func() error {
+				defer cycle.Stop()
+
+				const expected = 10
+				cycle.Pause()
+
+				startingCount := atomic.LoadInt64(&count)
+				for i := 0; i < expected-1; i++ {
+					cycle.Trigger()
+				}
+				cycle.TriggerWait()
+				countAfterTrigger := atomic.LoadInt64(&count)
+
+				change := countAfterTrigger - startingCount
+				if expected != change {
+					return fmt.Errorf("invalid triggers expected %d got %d", expected, change)
+				}
+
+				cycle.Restart()
+				time.Sleep(3 * time.Second)
+
+				countAfterRestart := atomic.LoadInt64(&count)
+				if countAfterRestart == countAfterTrigger {
+					return fmt.Errorf("cycle has not restarted")
+				}
+
+				return nil
+			})
+
+			err := group.Wait()
+			if err != nil {
+				t.Error(err)
+			}
+
+			testDuration := time.Since(start)
+			if testDuration > 7*time.Second {
+				t.Errorf("test took too long %v, expected approximately 3s", testDuration)
+			}
+
+			// shouldn't block
 			cycle.Trigger()
-		}
-		cycle.TriggerWait()
-		countAfterTrigger := atomic.LoadInt64(&count)
-
-		change := countAfterTrigger - startingCount
-		if expected != change {
-			return fmt.Errorf("invalid triggers expected %d got %d", expected, change)
-		}
-
-		cycle.Restart()
-		time.Sleep(3 * time.Second)
-
-		countAfterRestart := atomic.LoadInt64(&count)
-		if countAfterRestart == countAfterTrigger {
-			return fmt.Errorf("cycle has not restarted")
-		}
-
-		return nil
-	})
-
-	err := group.Wait()
-	if err != nil {
-		t.Error(err)
+		})
 	}
-
-	testDuration := time.Since(start)
-	if testDuration > 7*time.Second {
-		t.Errorf("test took too long %v, expected approximately 3s", testDuration)
-	}
-
-	// shouldn't block
-	cycle.Trigger()
 }
