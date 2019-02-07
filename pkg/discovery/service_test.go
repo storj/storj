dbb2e1b28829237ff953f0fe6c6806d09434798a
Dylan Lott <lott.dylan@gmail.com>
2019-02-07 15:53:14 -0700

    adds tests for cache graveyard

--- pkg/discovery/service_test.go
+++ pkg/discovery/service_test.go
@@ -28,3 +28,34 @@ func TestCache_Refresh(t *testing.T) {
 		}
 	})
 }
+
+func TestCache_Graveyard(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.New(t, 1, 8, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+	satellite := planet.Satellites[0]
+	offline := planet.StorageNodes[0].ID()
+
+	k := satellite.Kademlia.Service
+	k.WaitForBootstrap() // redundant, but leaving here to be clear
+
+	seen := k.Seen()
+	assert.NotNil(t, seen)
+
+	err = satellite.Overlay.Service.Delete(ctx, offline)
+	assert.NoError(t, err)
+
+	time.Sleep(5 * time.Second)
+
+	node, err := satellite.Overlay.Service.Get(ctx, offline)
+	assert.NoError(t, err)
+	assert.NotNil(t, node)
+	assert.Equal(t, node.Id, offline)
+}
