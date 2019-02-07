5c10789ebaf6e9b47659e7c6ef12ed81311043eb
Nikolai Yurchenko <nickolai.yurchenko@gmail.com>
2019-02-07 15:14:29 +0200

    web satellite popups changed

--- web/satellite/src/components/apiKeys/CopyApiKeyPopup.vue
+++ web/satellite/src/components/apiKeys/CopyApiKeyPopup.vue
@@ -3,46 +3,29 @@
 
 <template>   
     <div id="addApiKeyPopup">
-        <div class="save-api-popup" v-if="true">
-            <div class="save-api-popup__content">
-                <h1 class="save-api-popup__content__title">Save you API Key or download it in .txt file.</h1>
-                <p class="save-api-popup__content__name">You will need this to share access to the project with your team members.</p>
-                <div class="save-api-popup__content__copy-area">
-                    <p class="save-api-popup__content__copy-area__save-api">{{apiKey}}</p>
-                    <Button class="save-api-popup__content__copy-area__save-btn" v-clipboard="apiKey" label="Copy" width="140px" height="48px" :onPress="onCopyClick" />
-                    <!--<Button label="Download" width="140px" height="48px" isWhite/>-->
-                </div>
-                <div class="save-api-popup__content__info-area">
-                    <img src="../../../static/images/register/ErrorInfo.svg"/>
-                    <p class="save-api-popup__content__info-area__text" >Warning! You must save it anyway, otherwise you will lose a further access to this API Key for ever.</p>
-                </div>
-            </div>
-            <div class="add-api-key-popup__close-cross-container">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" v-on:click="onCloseClick">
-                    <path d="M15.7071 1.70711C16.0976 1.31658 16.0976 0.683417 15.7071 0.292893C15.3166 -0.0976311 14.6834 -0.0976311 14.2929 0.292893L15.7071 1.70711ZM0.292893 14.2929C-0.0976311 14.6834 -0.0976311 15.3166 0.292893 15.7071C0.683417 16.0976 1.31658 16.0976 1.70711 15.7071L0.292893 14.2929ZM1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L1.70711 0.292893ZM14.2929 15.7071C14.6834 16.0976 15.3166 16.0976 15.7071 15.7071C16.0976 15.3166 16.0976 14.6834 15.7071 14.2929L14.2929 15.7071ZM14.2929 0.292893L0.292893 14.2929L1.70711 15.7071L15.7071 1.70711L14.2929 0.292893ZM0.292893 1.70711L14.2929 15.7071L15.7071 14.2929L1.70711 0.292893L0.292893 1.70711Z" fill="#384B65"/>
+        <div class="save-api-popup">
+           <div class="save-api-popup__main">
+               <div class="save-api-popup__content">
+                   <h1 class="save-api-popup__content__title">Save your API Key</h1>
+                   <p class="save-api-popup__content__name">You will need this to share access to the project with your team members.</p>
+                   <div class="save-api-popup__content__copy-area">
+                       <p class="save-api-popup__content__copy-area__save-api">{{apiKey}}</p>
+                       <Button class="save-api-popup__content__copy-area__save-btn" v-clipboard="apiKey" label="Copy" width="140px" height="48px" :onPress="onCopyClick" />
+                   </div>
+               </div>
+               <div class="save-api-popup__close-cross-container">
+                   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" v-on:click="onCloseClick">
+                       <path d="M15.7071 1.70711C16.0976 1.31658 16.0976 0.683417 15.7071 0.292893C15.3166 -0.0976311 14.6834 -0.0976311 14.2929 0.292893L15.7071 1.70711ZM0.292893 14.2929C-0.0976311 14.6834 -0.0976311 15.3166 0.292893 15.7071C0.683417 16.0976 1.31658 16.0976 1.70711 15.7071L0.292893 14.2929ZM1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L1.70711 0.292893ZM14.2929 15.7071C14.6834 16.0976 15.3166 16.0976 15.7071 15.7071C16.0976 15.3166 16.0976 14.6834 15.7071 14.2929L14.2929 15.7071ZM14.2929 0.292893L0.292893 14.2929L1.70711 15.7071L15.7071 1.70711L14.2929 0.292893ZM0.292893 1.70711L14.2929 15.7071L15.7071 14.2929L1.70711 0.292893L0.292893 1.70711Z" fill="#384B65"/>
+                   </svg>
+               </div>
+           </div>
+            <div class="notification-wrap">
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="40" height="40" rx="10" fill="#2683FF"/>
+                    <path d="M18.1489 17.043H21.9149V28H18.1489V17.043ZM20 12C20.5816 12 21.0567 12.1823 21.4255 12.5468C21.8085 12.8979 22 13.357 22 13.9241C22 14.4776 21.8085 14.9367 21.4255 15.3013C21.0567 15.6658 20.5816 15.8481 20 15.8481C19.4184 15.8481 18.9362 15.6658 18.5532 15.3013C18.1844 14.9367 18 14.4776 18 13.9241C18 13.357 18.1844 12.8979 18.5532 12.5468C18.9362 12.1823 19.4184 12 20 12Z" fill="#F5F6FA"/>
                 </svg>
-            </div>
-        </div>
-        <div class="confirm-api-key-container" v-if="false">
-            <div class="confirm-api-key-container__content">
-                <div class="confirm-api-key-container__content__back-area">
-                    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5607 0.43934C11.1464 1.02513 11.1464 1.97487 10.5607 2.56066L5.12132 8H17.5C18.3284 8 19 8.67157 19 9.5C19 10.3284 18.3284 11 17.5 11H5.12132L10.5607 16.4393C11.1464 17.0251 11.1464 17.9749 10.5607 18.5607C9.97487 19.1464 9.02513 19.1464 8.43934 18.5607L0.43934 10.5607C-0.146447 9.97487 -0.146447 9.02513 0.43934 8.43934L8.43934 0.43934C9.02513 -0.146447 9.97487 -0.146447 10.5607 0.43934Z" fill="#384B65"/>
-                    </svg>
-                    <p>Back</p>
-                </div>
-                <h1 class="confirm-api-key-container__content__title">To ensure that you saved API Key please right down it below</h1>
-                <p class="confirm-api-key-container__content__label" >API Key</p>
-                <div class="confirm-api-key-container__content__input-area">
-                    <HeaderedInput
-                        placeholder="Enter API Key"
-                        class="full-input"
-                        width="98%"/>
-                    <Button label="Ok" width="120px" height="52px" />
-                </div>
-                <div class="confirm-api-key-container__content__info-area">
-                    <img src="../../../static/images/register/ErrorInfo.svg"/>
-                    <p class="confirm-api-key-container__content__info-area__text" >Warning! You must save it anyway, otherwise you will lose a further access to this API Key for ever.</p>
+                <div class="notification-wrap__text">
+                    <p>Warning! You must save it anyway, otherwise you will lose a further access to this API Key for ever.</a></p>
                 </div>
             </div>
         </div>
@@ -74,7 +57,6 @@ import { APP_STATE_ACTIONS, NOTIFICATION_ACTIONS } from "@/utils/constants/actio
                 this.$store.dispatch(APP_STATE_ACTIONS.TOGGLE_NEW_API_KEY);
             },
             onCopyClick: function (): void {
-                //TODO: save to clipboardt
                 this.$store.dispatch(NOTIFICATION_ACTIONS.SUCCESS, "Key saved to clipboard");
             }
         },
@@ -101,14 +83,24 @@ export default class AddApiKeyPopup extends Vue {
         width: 100%;
         max-width: 1041px;
         max-height: 500px;
-        background-color: #FFFFFF;
-        border-radius: 6px;
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: flex-start;
         position: relative;
         justify-content: space-between;
-        padding: 30px 30px 45px 50px;
+
+        &__main {
+            width: 100%;
+            border-top-left-radius: 6px;
+            border-top-right-radius: 6px;
+            display: flex;
+            flex-direction: row;
+            align-items: flex-start;
+            position: relative;
+            justify-content: center;
+            background-color: #FFFFFF;
+            padding: 30px 0 45px 0;
+        }
 
         &__content {
             max-width: 1041px;
@@ -147,117 +139,34 @@ export default class AddApiKeyPopup extends Vue {
                     margin-right: 10px;
                 }
             }
-
-            &__info-area {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: flex-start;
-                margin-top: 70px;
-
-                &__text {
-                    font-size: 12px;
-                    line-height: 18px;
-                    color: #AFB7C1;
-                    margin-left: 20px;
-                }
-            }
-        }
-    }
-    .confirm-api-key-container {
-        width: 100%;
-        max-width: 1041px;
-        max-height: 500px;
-        background-color: #FFFFFF;
-        border-radius: 6px;
-        display: flex;
-        flex-direction: row;
-        align-items: flex-start;
-        position: relative;
-        justify-content: space-between;
-        padding: 60px 40px 60px 40px;
-
-        &__content {
-            max-width: 1041px;
-            width: 100%;
-
-            &__back-area {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: flex-start;
-                width: 100%;
-
-                p {
-                    font-family: 'montserrat_medium';
-                    margin-left: 27px;
-                }
-            }
-
-            &__name {
-                font-family: 'montserrat_medium';
-                font-size: 16px;
-                color: #AFB7C1;
-                display: flex;
-
-                span {
-                    color: #354049;
-                    margin-bottom: 20px;
-                    margin-left: 10px;;
-                    display: block;
-                }
-            }
-
-            &__title {
-                font-family: 'montserrat_bold';
-                font-size: 32px;
-                width: 80%;
-            }
-
-            &__label {
-                margin-block-start: 0;
-                margin-block-end: 0;
-            }
-
-            &__input-area {
-                display: flex;
-                flex-direction: row;
-                justify-content: flex-end;
-                align-items: flex-end;
-                height: 60px;
-            }
-
-            &__copy-area {
-                background: #F5F6FA;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                padding: 10px 20px;
-                width: 100%;
-                margin-top: 30px;
-
-                &__save-btn {
-                    margin-right: 20px;
-                }
-            }
-
-            &__info-area {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: flex-start;
-                margin-top: 70px;
-
-                &__text {
-                    font-size: 12px;
-                    line-height: 18px;
-                    color: #AFB7C1;
-                    margin-left: 20px;
-                }
-            }
         }
     }
     .input-container.full-input {
         width: 100%;
+    }
+    .notification-wrap {
+        background-color: rgba(194, 214, 241, 1);
+        height: 98px;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 50px;
+        align-items: center;
+        border-bottom-left-radius: 6px;
+        border-bottom-right-radius: 6px;
+
+        &__text {
+            display: flex;
+            align-items: center;
+
+            p {
+                font-family: 'montserrat_medium';
+                font-size: 16px;
+                margin-left: 40px;
+
+                span {
+                    margin-right: 10px;
+                }
+            }
+        }
     }
 </style>
