f68a34192062f42b85eb702627ad38888e31562b
paul cannon <paul@thepaul.org>
2019-02-07 14:09:57 -0600

    chop up these nice brand new tests
    alexander made a bunch of lovely tests and then I had to go and mash
    them all up again to kep the ECDSA and cryptopasta dependencies out.

--- satellite/satellitedb/certdb.go
+++ satellite/satellitedb/certdb.go
@@ -6,11 +6,8 @@ package satellitedb
 import (
 	"context"
 	"crypto"
-	"crypto/ecdsa"
-	"crypto/x509"
 
-	"github.com/zeebo/errs"
-
+	"storj.io/storj/pkg/pkcrypto"
 	"storj.io/storj/pkg/storj"
 	"storj.io/storj/pkg/utils"
 	dbx "storj.io/storj/satellite/satellitedb/dbx"
@@ -29,12 +26,7 @@ func (b *certDB) SavePublicKey(ctx context.Context, nodeID storj.NodeID, publicK
 	_, err = tx.Get_CertRecord_By_Id(ctx, dbx.CertRecord_Id(nodeID.Bytes()))
 	if err != nil {
 		// no rows err, so create/insert an entry
-		publicKeyEcdsa, ok := publicKey.(*ecdsa.PublicKey)
-		if !ok {
-			return Error.Wrap(utils.CombineErrors(errs.New("Uplink Private Key is not a valid *ecdsa.PrivateKey"), tx.Rollback()))
-		}
-
-		pubbytes, err := x509.MarshalPKIXPublicKey(publicKeyEcdsa)
+		pubbytes, err := pkcrypto.PublicKeyToPKIX(publicKey)
 		if err != nil {
 			return Error.Wrap(utils.CombineErrors(err, tx.Rollback()))
 		}
@@ -57,22 +49,14 @@ func (b *certDB) SavePublicKey(ctx context.Context, nodeID storj.NodeID, publicK
 	return Error.Wrap(tx.Commit())
 }
 
-func (b *certDB) GetPublicKey(ctx context.Context, nodeID storj.NodeID) (*ecdsa.PublicKey, error) {
+func (b *certDB) GetPublicKey(ctx context.Context, nodeID storj.NodeID) (crypto.PublicKey, error) {
 	dbxInfo, err := b.db.Get_CertRecord_By_Id(ctx, dbx.CertRecord_Id(nodeID.Bytes()))
 	if err != nil {
 		return nil, err
 	}
-
-	pubkey, err := x509.ParsePKIXPublicKey(dbxInfo.Publickey)
+	pubkey, err := pkcrypto.PublicKeyFromPKIX(dbxInfo.Publickey)
 	if err != nil {
-		return nil, Error.Wrap(Error.New("Failed to extract Public Key from RenterBandwidthAllocation: %+v", err))
+		return nil, Error.New("Failed to extract Public Key from RenterBandwidthAllocation: %+v", err)
 	}
-
-	// Typecast public key
-	pkey, ok := pubkey.(*ecdsa.PublicKey)
-	if !ok {
-		return nil, Error.Wrap(Error.New("UnsupportedKey error: %+v", pubkey))
-	}
-
-	return pkey, nil
+	return pubkey, nil
 }
