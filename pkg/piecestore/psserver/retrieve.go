802679bd1c99cf924a5b53cfa847ed18f6774915
Egon Elbre <egonelbre@gmail.com>
2019-02-08 15:51:51 +0200

    remove jitter

--- pkg/piecestore/psserver/retrieve.go
+++ pkg/piecestore/psserver/retrieve.go
@@ -7,10 +7,8 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"math/rand"
 	"os"
 	"sync/atomic"
-	"time"
 
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
@@ -38,14 +36,10 @@ func (s *Server) Retrieve(stream pb.PieceStoreRoutes_RetrieveServer) (err error)
 		return RetrieveError.New("error receiving piece data")
 	}
 
-	jitter()
-
 	authorization := recv.GetAuthorization()
 	if err := s.verifier(authorization); err != nil {
 		return ServerError.Wrap(err)
 	}
-
-	jitter()
 
 	pd := recv.GetPieceData()
 	if pd == nil {
@@ -56,39 +50,32 @@ func (s *Server) Retrieve(stream pb.PieceStoreRoutes_RetrieveServer) (err error)
 	if err != nil {
 		return err
 	}
-	jitter()
 	s.log.Debug("Retrieving",
 		zap.String("Piece ID", id),
 		zap.Int64("Offset", pd.GetOffset()),
 		zap.Int64("Size", pd.GetPieceSize()),
 	)
-	jitter()
 	// Get path to data being retrieved
 	path, err := s.storage.PiecePath(id)
 	if err != nil {
 		return err
 	}
-	jitter()
 	// Verify that the path exists
 	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return RetrieveError.Wrap(err)
 	}
-	jitter()
 	// Read the size specified
 	totalToRead := pd.GetPieceSize()
 	fileSize := fileInfo.Size()
-	jitter()
 	// Read the entire file if specified -1 but make sure we do it from the correct offset
 	if pd.GetPieceSize() <= -1 || totalToRead+pd.GetOffset() > fileSize {
 		totalToRead = fileSize - pd.GetOffset()
 	}
-	jitter()
 	retrieved, allocated, err := s.retrieveData(ctx, stream, id, pd.GetOffset(), totalToRead)
 	if err != nil {
 		return err
 	}
-	jitter()
 	s.log.Info("Successfully retrieved",
 		zap.String("Piece ID", id),
 		zap.Int64("Allocated", allocated),
@@ -109,8 +96,6 @@ func (s *Server) retrieveData(ctx context.Context, stream pb.PieceStoreRoutes_Re
 		err = errs.Combine(err, RetrieveError.Wrap(storeFile.Close()))
 	}()
 
-	jitter()
-
 	writer := NewStreamWriter(s, stream)
 	allocationTracking := sync2.NewThrottle()
 	totalAllocated := int64(0)
@@ -125,31 +110,26 @@ func (s *Server) retrieveData(ctx context.Context, stream pb.PieceStoreRoutes_Re
 			if lastAllocation == nil {
 				return
 			}
-			jitter()
 			dbErr := s.DB.WriteBandwidthAllocToDB(lastAllocation)
 			err = errs.Combine(err, RetrieveError.Wrap(dbErr))
 		}()
 
 		for {
-			jitter()
 			recv, err := stream.Recv()
 			if err != nil {
 				allocationTracking.Fail(RetrieveError.Wrap(err))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			rba := recv.BandwidthAllocation
 			if err = s.verifySignature(stream.Context(), rba); err != nil {
 				allocationTracking.Fail(RetrieveError.Wrap(err))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			pba := rba.PayerAllocation
 			if err = s.verifyPayerAllocation(&pba, "GET"); err != nil {
 				allocationTracking.Fail(RetrieveError.Wrap(err))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			//todo: figure out why this fails tests
 			// if rba.Total > pba.MaxSize {
 			// 	allocationTracking.Fail(fmt.Errorf("attempt to send more data than allocation %v got %v", rba.Total, pba.MaxSize))
@@ -159,13 +139,10 @@ func (s *Server) retrieveData(ctx context.Context, stream pb.PieceStoreRoutes_Re
 				allocationTracking.Fail(fmt.Errorf("got lower allocation was %v got %v", lastTotal, rba.Total))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			atomic.StoreInt64(&totalAllocated, rba.Total)
-			jitter()
 			if err = allocationTracking.Produce(rba.Total - lastTotal); err != nil {
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			lastAllocation = rba
 			lastTotal = rba.Total
 		}
@@ -176,51 +153,38 @@ func (s *Server) retrieveData(ctx context.Context, stream pb.PieceStoreRoutes_Re
 	used := int64(0)
 	group.Go(func() error {
 		for used < length {
-			jitter()
 			nextMessageSize, err := allocationTracking.ConsumeOrWait(messageSize)
 			if err != nil {
 				allocationTracking.Fail(RetrieveError.Wrap(err))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 			toCopy := nextMessageSize
 			if length-used < nextMessageSize {
 				toCopy = length - used
 			}
-			jitter()
 			used += nextMessageSize
-			jitter()
 			n, err := io.CopyN(writer, storeFile, toCopy)
 			// correct errors when needed
-			jitter()
 			if n != nextMessageSize {
 				if err := allocationTracking.Produce(nextMessageSize - n); err != nil {
 					return RetrieveError.Wrap(err)
 				}
 				used -= nextMessageSize - n
 			}
-			jitter()
 			if used >= length && err == io.EOF {
 				return nil
 			}
-			jitter()
 			if err != nil {
 				// break on error
 				allocationTracking.Fail(RetrieveError.Wrap(err))
 				return RetrieveError.Wrap(err)
 			}
-			jitter()
 		}
 		return nil
 	})
 
 	errx := group.Wait()
-	jitter()
 	// write to bandwidth usage table
 	bwUsedErr := s.DB.AddBandwidthUsed(used)
 	return used, atomic.LoadInt64(&totalAllocated), errs.Combine(errx, allocationTracking.Err(), RetrieveError.Wrap(bwUsedErr))
-}
-
-func jitter() {
-	time.Sleep(time.Duration(rand.Intn(3)*100) * time.Microsecond * 0)
 }
