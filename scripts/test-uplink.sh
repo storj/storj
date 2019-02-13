7df9cadbd3ce793fd533b5879d22727cdbfb3ddb
Dylan Lott <lott.dylan@gmail.com>
2019-02-13 14:01:53 -0700

    get script working for testing uplink

--- scripts/test-uplink.sh
+++ scripts/test-uplink.sh
@@ -10,30 +10,51 @@ cleanup(){
 
 trap cleanup EXIT
 
-# setup uplink
 BUCKET=bucket-123
 SRC_DIR=$TMPDIR/source
 DST_DIR=$TMPDIR/dst
+
 mkdir -p $SRC_DIR $DST_DIR
 
 head -c 1024 </dev/urandom > $SRC_DIR/small-upload-testfile      # create 1mb file of random bytes (inline)
 head -c 5120 </dev/urandom > $SRC_DIR/big-upload-testfile        # create 5mb file of random bytes (remote)
 head -c 5    </dev/urandom > $SRC_DIR/multipart-upload-testfile  # create 5kb file of random bytes (remote)
 
-uplink --config-dir $GATEWAY_0_DIR ls 
+uplink --config-dir $GATEWAY_0_DIR mb sj://$BUCKET/ 
 
-# uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/small-upload-testfile sj://$BUCKET/ 
-# uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/big-upload-testfile sj://$BUCKET/ 
-# uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/multipart-upload-testfile sj://$BUCKET/ 
+uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/small-upload-testfile sj://$BUCKET/ 
+uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/big-upload-testfile sj://$BUCKET/ 
+uplink --config-dir $GATEWAY_0_DIR cp $SRC_DIR/multipart-upload-testfile sj://$BUCKET/ 
 
-# uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/small-upload-testfile $DST_DIR
-# uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/big-upload-testfile $DST_DIR
-# uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/multipart-upload-testfile $DST_DIR
+uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/small-upload-testfile $DST_DIR
+uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/big-upload-testfile $DST_DIR
+uplink --config-dir $GATEWAY_0_DIR cp sj://$BUCKET/multipart-upload-testfile $DST_DIR
 
-# uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/small-upload-testfile
-# uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/big-upload-testfile
-# uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/multipart-upload-testfile
+uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/small-upload-testfile
+uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/big-upload-testfile
+uplink --config-dir $GATEWAY_0_DIR rm sj://$BUCKET/multipart-upload-testfile
 
-# uplink --config-dir $GATEWAY_0_DIR ls sj://$BUCKET
+uplink --config-dir $GATEWAY_0_DIR ls sj://$BUCKET
 
-# uplink --config-dir $GATEWAY_0_DIR rb sj://$BUCKET
+uplink --config-dir $GATEWAY_0_DIR rb sj://$BUCKET
+
+if cmp $SRC_DIR/small-upload-testfile $DST_DIR/small-upload-testfile
+then
+    echo "small upload testfile matches uploaded file"
+else
+    echo "small upload testfile does not match uploaded file"
+fi
+
+if cmp $SRC_DIR/big-upload-testfile $DST_DIR/big-upload-testfile
+then
+    echo "big upload testfile matches uploaded file"
+else
+    echo "big upload testfile does not match uploaded file"
+fi
+
+if cmp $SRC_DIR/multipart-upload-testfile $DST_DIR/multipart-upload-testfile
+then
+    echo "multipart upload testfile matches uploaded file"
+else
+    echo "multipart upload testfile does not match uploaded file"
+fi
