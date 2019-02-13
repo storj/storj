e8f36842b7dbe21b1417353c747c5964f63d015b
Egon Elbre <egonelbre@gmail.com>
2019-02-13 22:31:00 +0200

    nicer test code for pgutil

---
+++ internal/dbutil/pgutil/db.go
@@ -0,0 +1,41 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package pgutil
+
+import (
+	"database/sql"
+
+	"github.com/zeebo/errs"
+)
+
+// DB is postgres database with schema
+type DB struct {
+	*sql.DB
+	Schema string
+}
+
+// Open opens a postgres database with a schema
+func Open(connstr string, schemaPrefix string) (*DB, error) {
+	schemaName := schemaPrefix + "-" + RandomString(8)
+
+	db, err := sql.Open("postgres", ConnstrWithSchema(connstr, schemaName))
+	if err != nil {
+		return nil, err
+	}
+
+	err = CreateSchema(db, schemaName)
+	if err != nil {
+		return nil, errs.Combine(err, db.Close())
+	}
+
+	return &DB{db, schemaName}, err
+}
+
+// Close closes the database and deletes the schema.
+func (db *DB) Close() error {
+	return errs.Combine(
+		DropSchema(db.DB, db.Schema),
+		db.DB.Close(),
+	)
+}
