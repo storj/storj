4b351c7fb9f8f4022c384eb608498ad589bb86c0
Yaroslav Vorobiov <yaroslav@storj.io>
2019-02-28 13:19:17 +0200

    remove message reciever

--- satellite/console/consoleweb/consoleql/mail.go
+++ satellite/console/consoleweb/consoleql/mail.go
@@ -14,10 +14,10 @@ type AccountActivationEmail struct {
 }
 
 // Template returns email template name
-func (tmpl *AccountActivationEmail) Template() string { return "Welcome" }
+func (*AccountActivationEmail) Template() string { return "Welcome" }
 
 // Subject gets email subject
-func (tmpl *AccountActivationEmail) Subject() string { return "Activate your email" }
+func (*AccountActivationEmail) Subject() string { return "Activate your email" }
 
 // ForgotPasswordEmail is mailservice template with reset password data
 type ForgotPasswordEmail struct {
@@ -26,10 +26,10 @@ type ForgotPasswordEmail struct {
 }
 
 // Template returns email template name
-func (tmpl *ForgotPasswordEmail) Template() string { return "Forgot" }
+func (*ForgotPasswordEmail) Template() string { return "Forgot" }
 
 // Subject gets email subject
-func (tmpl *ForgotPasswordEmail) Subject() string { return "" }
+func (*ForgotPasswordEmail) Subject() string { return "" }
 
 // ProjectInvitationEmail is mailservice template for project invitation email
 type ProjectInvitationEmail struct {
@@ -38,7 +38,7 @@ type ProjectInvitationEmail struct {
 }
 
 // Template returns email template name
-func (tmpl *ProjectInvitationEmail) Template() string { return "Invite" }
+func (*ProjectInvitationEmail) Template() string { return "Invite" }
 
 // Subject gets email subject
-func (tmpl *ProjectInvitationEmail) Subject() string { return "" }
+func (*ProjectInvitationEmail) Subject() string { return "" }
