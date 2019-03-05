d6885af07771c2fccaa77514996281de39f8df1d
crawter <crawter@hotmail.com>
2019-03-05 13:04:23 +0200

    V3-1284 Node bootstrap web frontend

---
+++ web/bootstrap/src/utils/constants.ts
@@ -0,0 +1,12 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+export const MUTATIONS = {
+    SET_NODE_STATUS: 'SET_NODE_STATUS',
+    SET_LOADING: 'SET_LOADING',
+};
+
+export const ACTIONS = {
+    CHECK_NODE_STATUS: 'CHECK_NODE_STATUS',
+    SET_LOADING: 'SET_LOADING',
+};
