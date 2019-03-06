c01e487e6618bc9da60d2470f3b9f19acb98dff0
Kaloyan Raev <kaloyan@storj.io>
2019-03-06 18:42:18 +0200

    Metainfo Client implementation

--- pkg/metainfo/client.go
+++ pkg/metainfo/client.go
@@ -5,7 +5,6 @@ package metainfo
 
 import (
 	"context"
-	"errors"
 
 	"google.golang.org/grpc"
 	monkit "gopkg.in/spacemonkeygo/monkit.v2"
@@ -25,12 +24,12 @@ type Metainfo struct {
 	client pb.MetainfoClient
 }
 
-// New Used as a public function
+// New used as a public function
 func New(gcclient pb.MetainfoClient) (metainfo *Metainfo) {
 	return &Metainfo{client: gcclient}
 }
 
-// a compiler trick to make sure *Overlay implements Client
+// a compiler trick to make sure *Metainfo implements Client
 var _ Client = (*Metainfo)(nil)
 
 // ListItem is a single item in a listing
@@ -42,11 +41,11 @@ type ListItem struct {
 
 // Client interface for the Metainfo service
 type Client interface {
-	CreateSegment(ctx context.Context, path storj.Path) (*[]pb.OrderLimit2, error)
-	CommitSegment(ctx context.Context, path storj.Path, pointer *pb.Pointer) error
-	ReadSegment(ctx context.Context, path storj.Path) (*pb.Pointer, *[]pb.OrderLimit2, error)
-	DeleteSegment(ctx context.Context, path storj.Path) (*[]pb.OrderLimit2, error)
-	ListSegments(ctx context.Context, prefix, startAfter, endBefore storj.Path, recursive bool, limit int, metaFlags uint32) (items []ListItem, more bool, err error)
+	CreateSegment(ctx context.Context, bucket string, path storj.Path, totalNodes int32, maxSegmentSize int64) ([]*pb.AddressedOrderLimit, error)
+	CommitSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64, pointer *pb.Pointer) error
+	ReadSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64) (*pb.Pointer, []*pb.AddressedOrderLimit, error)
+	DeleteSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64) ([]*pb.AddressedOrderLimit, error)
+	ListSegments(ctx context.Context, bucket string, prefix, startAfter, endBefore storj.Path, recursive bool, limit int32, metaFlags uint32) (items []ListItem, more bool, err error)
 }
 
 // NewClient initializes a new metainfo client
@@ -64,40 +63,83 @@ func NewClient(ctx context.Context, tc transport.Client, address string, APIKey 
 	return &Metainfo{client: pb.NewMetainfoClient(conn)}, nil
 }
 
-// a compiler trick to make sure *PointerDB implements Client
-var _ Client = (*Metainfo)(nil)
-
 // CreateSegment requests the order limits for creating a new segment
-func (metainfo *Metainfo) CreateSegment(ctx context.Context, path storj.Path) (orders *[]pb.OrderLimit2, err error) {
+func (metainfo *Metainfo) CreateSegment(ctx context.Context, bucket string, path storj.Path, totalNodes int32, maxSegmentSize int64) (limits []*pb.AddressedOrderLimit, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	return nil, errors.New("not implemented")
+	response, err := metainfo.client.CreateSegment(ctx, &pb.SegmentWriteRequest{
+		Bucket:         []byte(bucket),
+		Path:           []byte(path),
+		TotalNodes:     totalNodes,
+		MaxSegmentSize: maxSegmentSize,
+	})
+
+	return response.GetAddressedLimits(), err
 }
 
 // CommitSegment requests to store the pointer for the segment
-func (metainfo *Metainfo) CommitSegment(ctx context.Context, path storj.Path, pointer *pb.Pointer) (err error) {
+func (metainfo *Metainfo) CommitSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64, pointer *pb.Pointer) (err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	return errors.New("not implemented")
+	_, err = metainfo.client.CommitSegment(ctx, &pb.SegmentCommitRequest{
+		Bucket:  []byte(bucket),
+		Path:    []byte(path),
+		Segment: segmentIndex,
+		Pointer: pointer,
+	})
+
+	return err
 }
 
 // ReadSegment requests the order limits for reading a segment
-func (metainfo *Metainfo) ReadSegment(ctx context.Context, path storj.Path) (pointer *pb.Pointer, orders *[]pb.OrderLimit2, err error) {
+func (metainfo *Metainfo) ReadSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64) (pointer *pb.Pointer, limits []*pb.AddressedOrderLimit, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	return nil, nil, errors.New("not implemented")
+	response, err := metainfo.client.DownloadSegment(ctx, &pb.SegmentDownloadRequest{
+		Bucket:  []byte(bucket),
+		Path:    []byte(path),
+		Segment: segmentIndex,
+	})
+
+	return response.GetPointer(), response.GetAddressedLimits(), err
 }
 
 // DeleteSegment requests the order limits for deleting a segment
-func (metainfo *Metainfo) DeleteSegment(ctx context.Context, path storj.Path) (orders *[]pb.OrderLimit2, err error) {
+func (metainfo *Metainfo) DeleteSegment(ctx context.Context, bucket string, path storj.Path, segmentIndex int64) (limits []*pb.AddressedOrderLimit, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	return nil, errors.New("not implemented")
+	response, err := metainfo.client.DeleteSegment(ctx, &pb.SegmentDeleteRequest{
+		Bucket:  []byte(bucket),
+		Path:    []byte(path),
+		Segment: segmentIndex,
+	})
+
+	return response.GetAddressedLimits(), err
 }
 
 // ListSegments lists the available segments
-func (metainfo *Metainfo) ListSegments(ctx context.Context, prefix, startAfter, endBefore storj.Path, recursive bool, limit int, metaFlags uint32) (items []ListItem, more bool, err error) {
+func (metainfo *Metainfo) ListSegments(ctx context.Context, bucket string, prefix, startAfter, endBefore storj.Path, recursive bool, limit int32, metaFlags uint32) (items []ListItem, more bool, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	return nil, false, errors.New("not implemented")
+	response, err := metainfo.client.ListSegments(ctx, &pb.ListSegmentsRequest{
+		Bucket:     []byte(bucket),
+		Prefix:     []byte(prefix),
+		StartAfter: []byte(startAfter),
+		EndBefore:  []byte(endBefore),
+		Recursive:  recursive,
+		Limit:      limit,
+		MetaFlags:  metaFlags,
+	})
+
+	list := response.GetItems()
+	items = make([]ListItem, len(list))
+	for i, item := range list {
+		items[i] = ListItem{
+			Path:     storj.Path(item.GetPath()),
+			Pointer:  item.GetPointer(),
+			IsPrefix: item.IsPrefix,
+		}
+	}
+
+	return items, response.GetMore(), err
 }
