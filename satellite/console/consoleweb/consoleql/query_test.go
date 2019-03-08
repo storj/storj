3ab696164f693557df1f19a724daeab969cc7aa8
Yehor Butko <crawter@hotmail.com>
2019-03-08 16:01:11 +0200

    Satellite console new activation flow (#1421)
    Satellite console new activation flow

--- satellite/console/consoleweb/consoleql/query_test.go
+++ satellite/console/consoleweb/consoleql/query_test.go
@@ -86,7 +86,7 @@ func TestGraphqlQuery(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			_, err = service.ActivateAccount(ctx, activationToken)
+			err = service.ActivateAccount(ctx, activationToken)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -217,7 +217,7 @@ func TestGraphqlQuery(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			_, err = service.ActivateAccount(ctx, activationToken1)
+			err = service.ActivateAccount(ctx, activationToken1)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -247,7 +247,7 @@ func TestGraphqlQuery(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			_, err = service.ActivateAccount(ctx, activationToken2)
+			err = service.ActivateAccount(ctx, activationToken2)
 			if err != nil {
 				t.Fatal(err)
 			}
