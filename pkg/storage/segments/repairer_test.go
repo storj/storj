0ed0c1be7de139d31d4f9dc6ef134bf5fcc743be
Jennifer Johnson <jennifer@storj.io>
2019-03-08 10:13:25 -0500

    TestNewSegmentRepairer

--- pkg/storage/segments/repairer_test.go
+++ pkg/storage/segments/repairer_test.go
@@ -10,8 +10,7 @@ import (
 
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testplanet"
-	// "storj.io/storj/pkg/overlay"
-	// ecclient "storj.io/storj/pkg/storage/ec"
+	ecclient "storj.io/storj/pkg/storage/ec"
 	"storj.io/storj/pkg/storage/segments"
 )
 
@@ -19,13 +18,13 @@ func TestNewSegmentRepairer(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 1, UplinkCount: 1,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
-		// oc := overlay.NewClient()
-		// ec := ecclient.NewClient()
+		oc, err := planet.Uplinks[0].DialOverlay(planet.Satellites[0])
+		assert.NoError(t, err)
+		ec := ecclient.NewClient(planet.Uplinks[0].Transport, 0)
 		pdb := planet.Satellites[0].Metainfo.Endpoint
 		ss := segments.NewSegmentRepairer(oc, ec, pdb)
 		assert.NotNil(t, ss)
 	})
-
 }
 
 func TestSegmentStoreRepairRemote(t *testing.T) {
