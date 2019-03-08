3f40aa612a09d0f0c29e0670fe27f933648552ff
Egon Elbre <egonelbre@gmail.com>
2019-03-08 14:32:16 +0200

    actually clear the signature

--- pkg/auth/signing/encode.go
+++ pkg/auth/signing/encode.go
@@ -11,18 +11,21 @@ import (
 
 func EncodeOrderLimit(limit *pb.OrderLimit2) ([]byte, error) {
 	signature := limit.SatelliteSignature
+	limit.SatelliteSignature = nil
 	defer func() { limit.SatelliteSignature = signature }()
 	return proto.Marshal(limit)
 }
 
 func EncodeOrder(order *pb.Order2) ([]byte, error) {
 	signature := order.UplinkSignature
+	order.UplinkSignature = nil
 	defer func() { order.UplinkSignature = signature }()
 	return proto.Marshal(order)
 }
 
 func EncodePieceHash(hash *pb.PieceHash) ([]byte, error) {
 	signature := hash.Signature
+	hash.Signature = nil
 	defer func() { hash.Signature = signature }()
 	return proto.Marshal(hash)
 }
