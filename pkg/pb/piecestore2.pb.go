bfd0a449e4f980b1904c27a3758a360530b07322
Michal Niewrzal <michal@storj.io>
2019-03-13 13:34:31 +0100

    New orders protobuf (#1469)

--- pkg/pb/piecestore2.pb.go
+++ pkg/pb/piecestore2.pb.go
@@ -3,14 +3,14 @@
 
 package pb
 
+import proto "github.com/gogo/protobuf/proto"
+import fmt "fmt"
+import math "math"
+import _ "github.com/gogo/protobuf/gogoproto"
+
 import (
-	context "context"
-	fmt "fmt"
-	_ "github.com/gogo/protobuf/gogoproto"
-	proto "github.com/gogo/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/duration"
+	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -49,7 +49,7 @@ func (m *PieceUploadRequest) Reset()         { *m = PieceUploadRequest{} }
 func (m *PieceUploadRequest) String() string { return proto.CompactTextString(m) }
 func (*PieceUploadRequest) ProtoMessage()    {}
 func (*PieceUploadRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{0}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{0}
 }
 func (m *PieceUploadRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceUploadRequest.Unmarshal(m, b)
@@ -57,8 +57,8 @@ func (m *PieceUploadRequest) XXX_Unmarshal(b []byte) error {
 func (m *PieceUploadRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceUploadRequest.Marshal(b, m, deterministic)
 }
-func (m *PieceUploadRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceUploadRequest.Merge(m, src)
+func (dst *PieceUploadRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceUploadRequest.Merge(dst, src)
 }
 func (m *PieceUploadRequest) XXX_Size() int {
 	return xxx_messageInfo_PieceUploadRequest.Size(m)
@@ -110,7 +110,7 @@ func (m *PieceUploadRequest_Chunk) Reset()         { *m = PieceUploadRequest_Chu
 func (m *PieceUploadRequest_Chunk) String() string { return proto.CompactTextString(m) }
 func (*PieceUploadRequest_Chunk) ProtoMessage()    {}
 func (*PieceUploadRequest_Chunk) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{0, 0}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{0, 0}
 }
 func (m *PieceUploadRequest_Chunk) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceUploadRequest_Chunk.Unmarshal(m, b)
@@ -118,8 +118,8 @@ func (m *PieceUploadRequest_Chunk) XXX_Unmarshal(b []byte) error {
 func (m *PieceUploadRequest_Chunk) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceUploadRequest_Chunk.Marshal(b, m, deterministic)
 }
-func (m *PieceUploadRequest_Chunk) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceUploadRequest_Chunk.Merge(m, src)
+func (dst *PieceUploadRequest_Chunk) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceUploadRequest_Chunk.Merge(dst, src)
 }
 func (m *PieceUploadRequest_Chunk) XXX_Size() int {
 	return xxx_messageInfo_PieceUploadRequest_Chunk.Size(m)
@@ -155,7 +155,7 @@ func (m *PieceUploadResponse) Reset()         { *m = PieceUploadResponse{} }
 func (m *PieceUploadResponse) String() string { return proto.CompactTextString(m) }
 func (*PieceUploadResponse) ProtoMessage()    {}
 func (*PieceUploadResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{1}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{1}
 }
 func (m *PieceUploadResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceUploadResponse.Unmarshal(m, b)
@@ -163,8 +163,8 @@ func (m *PieceUploadResponse) XXX_Unmarshal(b []byte) error {
 func (m *PieceUploadResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceUploadResponse.Marshal(b, m, deterministic)
 }
-func (m *PieceUploadResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceUploadResponse.Merge(m, src)
+func (dst *PieceUploadResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceUploadResponse.Merge(dst, src)
 }
 func (m *PieceUploadResponse) XXX_Size() int {
 	return xxx_messageInfo_PieceUploadResponse.Size(m)
@@ -204,7 +204,7 @@ func (m *PieceDownloadRequest) Reset()         { *m = PieceDownloadRequest{} }
 func (m *PieceDownloadRequest) String() string { return proto.CompactTextString(m) }
 func (*PieceDownloadRequest) ProtoMessage()    {}
 func (*PieceDownloadRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{2}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{2}
 }
 func (m *PieceDownloadRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDownloadRequest.Unmarshal(m, b)
@@ -212,8 +212,8 @@ func (m *PieceDownloadRequest) XXX_Unmarshal(b []byte) error {
 func (m *PieceDownloadRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDownloadRequest.Marshal(b, m, deterministic)
 }
-func (m *PieceDownloadRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDownloadRequest.Merge(m, src)
+func (dst *PieceDownloadRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDownloadRequest.Merge(dst, src)
 }
 func (m *PieceDownloadRequest) XXX_Size() int {
 	return xxx_messageInfo_PieceDownloadRequest.Size(m)
@@ -258,7 +258,7 @@ func (m *PieceDownloadRequest_Chunk) Reset()         { *m = PieceDownloadRequest
 func (m *PieceDownloadRequest_Chunk) String() string { return proto.CompactTextString(m) }
 func (*PieceDownloadRequest_Chunk) ProtoMessage()    {}
 func (*PieceDownloadRequest_Chunk) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{2, 0}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{2, 0}
 }
 func (m *PieceDownloadRequest_Chunk) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDownloadRequest_Chunk.Unmarshal(m, b)
@@ -266,8 +266,8 @@ func (m *PieceDownloadRequest_Chunk) XXX_Unmarshal(b []byte) error {
 func (m *PieceDownloadRequest_Chunk) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDownloadRequest_Chunk.Marshal(b, m, deterministic)
 }
-func (m *PieceDownloadRequest_Chunk) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDownloadRequest_Chunk.Merge(m, src)
+func (dst *PieceDownloadRequest_Chunk) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDownloadRequest_Chunk.Merge(dst, src)
 }
 func (m *PieceDownloadRequest_Chunk) XXX_Size() int {
 	return xxx_messageInfo_PieceDownloadRequest_Chunk.Size(m)
@@ -303,7 +303,7 @@ func (m *PieceDownloadResponse) Reset()         { *m = PieceDownloadResponse{} }
 func (m *PieceDownloadResponse) String() string { return proto.CompactTextString(m) }
 func (*PieceDownloadResponse) ProtoMessage()    {}
 func (*PieceDownloadResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{3}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{3}
 }
 func (m *PieceDownloadResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDownloadResponse.Unmarshal(m, b)
@@ -311,8 +311,8 @@ func (m *PieceDownloadResponse) XXX_Unmarshal(b []byte) error {
 func (m *PieceDownloadResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDownloadResponse.Marshal(b, m, deterministic)
 }
-func (m *PieceDownloadResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDownloadResponse.Merge(m, src)
+func (dst *PieceDownloadResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDownloadResponse.Merge(dst, src)
 }
 func (m *PieceDownloadResponse) XXX_Size() int {
 	return xxx_messageInfo_PieceDownloadResponse.Size(m)
@@ -343,7 +343,7 @@ func (m *PieceDownloadResponse_Chunk) Reset()         { *m = PieceDownloadRespon
 func (m *PieceDownloadResponse_Chunk) String() string { return proto.CompactTextString(m) }
 func (*PieceDownloadResponse_Chunk) ProtoMessage()    {}
 func (*PieceDownloadResponse_Chunk) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{3, 0}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{3, 0}
 }
 func (m *PieceDownloadResponse_Chunk) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDownloadResponse_Chunk.Unmarshal(m, b)
@@ -351,8 +351,8 @@ func (m *PieceDownloadResponse_Chunk) XXX_Unmarshal(b []byte) error {
 func (m *PieceDownloadResponse_Chunk) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDownloadResponse_Chunk.Marshal(b, m, deterministic)
 }
-func (m *PieceDownloadResponse_Chunk) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDownloadResponse_Chunk.Merge(m, src)
+func (dst *PieceDownloadResponse_Chunk) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDownloadResponse_Chunk.Merge(dst, src)
 }
 func (m *PieceDownloadResponse_Chunk) XXX_Size() int {
 	return xxx_messageInfo_PieceDownloadResponse_Chunk.Size(m)
@@ -388,7 +388,7 @@ func (m *PieceDeleteRequest) Reset()         { *m = PieceDeleteRequest{} }
 func (m *PieceDeleteRequest) String() string { return proto.CompactTextString(m) }
 func (*PieceDeleteRequest) ProtoMessage()    {}
 func (*PieceDeleteRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{4}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{4}
 }
 func (m *PieceDeleteRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDeleteRequest.Unmarshal(m, b)
@@ -396,8 +396,8 @@ func (m *PieceDeleteRequest) XXX_Unmarshal(b []byte) error {
 func (m *PieceDeleteRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDeleteRequest.Marshal(b, m, deterministic)
 }
-func (m *PieceDeleteRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDeleteRequest.Merge(m, src)
+func (dst *PieceDeleteRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDeleteRequest.Merge(dst, src)
 }
 func (m *PieceDeleteRequest) XXX_Size() int {
 	return xxx_messageInfo_PieceDeleteRequest.Size(m)
@@ -425,7 +425,7 @@ func (m *PieceDeleteResponse) Reset()         { *m = PieceDeleteResponse{} }
 func (m *PieceDeleteResponse) String() string { return proto.CompactTextString(m) }
 func (*PieceDeleteResponse) ProtoMessage()    {}
 func (*PieceDeleteResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ff32dd550c2439, []int{5}
+	return fileDescriptor_piecestore2_6f4182b059c6fda3, []int{5}
 }
 func (m *PieceDeleteResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_PieceDeleteResponse.Unmarshal(m, b)
@@ -433,8 +433,8 @@ func (m *PieceDeleteResponse) XXX_Unmarshal(b []byte) error {
 func (m *PieceDeleteResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_PieceDeleteResponse.Marshal(b, m, deterministic)
 }
-func (m *PieceDeleteResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PieceDeleteResponse.Merge(m, src)
+func (dst *PieceDeleteResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PieceDeleteResponse.Merge(dst, src)
 }
 func (m *PieceDeleteResponse) XXX_Size() int {
 	return xxx_messageInfo_PieceDeleteResponse.Size(m)
@@ -455,39 +455,6 @@ func init() {
 	proto.RegisterType((*PieceDownloadResponse_Chunk)(nil), "piecestore.PieceDownloadResponse.Chunk")
 	proto.RegisterType((*PieceDeleteRequest)(nil), "piecestore.PieceDeleteRequest")
 	proto.RegisterType((*PieceDeleteResponse)(nil), "piecestore.PieceDeleteResponse")
-}
-
-func init() { proto.RegisterFile("piecestore2.proto", fileDescriptor_23ff32dd550c2439) }
-
-var fileDescriptor_23ff32dd550c2439 = []byte{
-	// 426 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x54, 0x41, 0xcb, 0xd3, 0x40,
-	0x10, 0xfd, 0xb6, 0x4d, 0x82, 0x8e, 0x22, 0xba, 0x9f, 0x95, 0x10, 0xb0, 0xd6, 0x20, 0x6d, 0x41,
-	0x48, 0x25, 0xbd, 0x89, 0x16, 0xd4, 0x1e, 0x04, 0x15, 0xcb, 0x4a, 0x2f, 0x5e, 0x24, 0x6d, 0x26,
-	0x69, 0x30, 0xcd, 0xc6, 0x64, 0x83, 0xd0, 0xab, 0x47, 0x7f, 0xa9, 0xbf, 0x42, 0xc9, 0x6e, 0xda,
-	0x92, 0xb6, 0x36, 0x50, 0xf8, 0x6e, 0xd9, 0x99, 0xf7, 0x66, 0xde, 0x7b, 0xbb, 0x04, 0x1e, 0xa4,
-	0x11, 0x2e, 0x31, 0x17, 0x3c, 0x43, 0xd7, 0x49, 0x33, 0x2e, 0x38, 0x85, 0x7d, 0xc9, 0x82, 0x90,
-	0x87, 0x5c, 0xd5, 0xad, 0x6e, 0xc8, 0x79, 0x18, 0xe3, 0x48, 0x9e, 0x16, 0x45, 0x30, 0xf2, 0x8b,
-	0xcc, 0x13, 0x11, 0x4f, 0xaa, 0xfe, 0xbd, 0x35, 0x0a, 0x2f, 0x4a, 0x82, 0x0a, 0x6f, 0xff, 0x6a,
-	0x01, 0x9d, 0x95, 0xa3, 0xe6, 0x69, 0xcc, 0x3d, 0x9f, 0xe1, 0x8f, 0x02, 0x73, 0x41, 0x9f, 0x83,
-	0x1e, 0x47, 0xeb, 0x48, 0x98, 0xa4, 0x47, 0x86, 0x77, 0xdc, 0x8e, 0xb3, 0xa3, 0x7d, 0xce, 0x7c,
-	0xcc, 0x3e, 0x96, 0x3d, 0x97, 0x29, 0x0c, 0xed, 0x83, 0xce, 0xcb, 0xaa, 0xd9, 0x92, 0xe0, 0xfb,
-	0x07, 0x60, 0x97, 0xa9, 0x36, 0x7d, 0x09, 0xfa, 0x72, 0x55, 0x24, 0xdf, 0xcd, 0xb6, 0xc4, 0x3d,
-	0x73, 0xf6, 0x1e, 0x9c, 0x63, 0x0d, 0xce, 0xbb, 0x12, 0xcb, 0x14, 0x85, 0x0e, 0x40, 0xf3, 0x79,
-	0x82, 0xa6, 0x26, 0xa9, 0xd7, 0xfb, 0x15, 0x92, 0xf8, 0xde, 0xcb, 0x57, 0x4c, 0x02, 0xac, 0x31,
-	0xe8, 0x92, 0x48, 0x1f, 0x81, 0xc1, 0x83, 0x20, 0x47, 0xe5, 0xa1, 0xcd, 0xaa, 0x13, 0xa5, 0xa0,
-	0xf9, 0x9e, 0xf0, 0xa4, 0xd8, 0xbb, 0x4c, 0x7e, 0xdb, 0x13, 0xb8, 0xae, 0x09, 0xc8, 0x53, 0x9e,
-	0xe4, 0xb8, 0x5b, 0x4a, 0x1a, 0x96, 0xda, 0x7f, 0x08, 0x3c, 0x94, 0xb5, 0x29, 0xff, 0x99, 0xdc,
-	0x78, 0x8e, 0xaf, 0xea, 0x39, 0xf6, 0x8f, 0x72, 0x3c, 0x50, 0x51, 0x4b, 0xd2, 0x9a, 0x34, 0x05,
-	0xf4, 0x18, 0x40, 0x22, 0xbf, 0xe5, 0xd1, 0x06, 0xa5, 0x96, 0x36, 0xbb, 0x2d, 0x2b, 0x5f, 0xa2,
-	0x0d, 0xda, 0xbf, 0x09, 0x74, 0x0e, 0xb6, 0x54, 0x71, 0xbd, 0xde, 0xea, 0x52, 0x66, 0x07, 0x67,
-	0x74, 0x29, 0x46, 0x5d, 0xd8, 0x45, 0x37, 0xf7, 0xa6, 0x7a, 0xbe, 0x53, 0x8c, 0x51, 0xe0, 0x25,
-	0xb1, 0xdb, 0x9d, 0xea, 0xf2, 0xb7, 0x23, 0x94, 0x36, 0xf7, 0x2f, 0x01, 0x98, 0xed, 0x0c, 0xd0,
-	0x4f, 0x60, 0xa8, 0xd7, 0x41, 0xbb, 0xe7, 0xdf, 0xad, 0xf5, 0xe4, 0xbf, 0x7d, 0x35, 0xd9, 0xbe,
-	0x1a, 0x12, 0x3a, 0x87, 0x5b, 0xdb, 0x34, 0x68, 0xaf, 0xe9, 0x02, 0xad, 0xa7, 0x8d, 0x51, 0x96,
-	0x43, 0x5f, 0x10, 0xfa, 0x01, 0x0c, 0x65, 0xe3, 0x84, 0xca, 0x5a, 0x44, 0x27, 0x54, 0xd6, 0xfd,
-	0xdb, 0x57, 0x6f, 0xb5, 0xaf, 0xad, 0x74, 0xb1, 0x30, 0xe4, 0x8f, 0x62, 0xfc, 0x2f, 0x00, 0x00,
-	0xff, 0xff, 0x83, 0x6c, 0xd9, 0xff, 0x85, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -693,4 +660,36 @@ var _Piecestore_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Metadata: "piecestore2.proto",
+}
+
+func init() { proto.RegisterFile("piecestore2.proto", fileDescriptor_piecestore2_6f4182b059c6fda3) }
+
+var fileDescriptor_piecestore2_6f4182b059c6fda3 = []byte{
+	// 404 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x94, 0x51, 0x4f, 0xea, 0x30,
+	0x14, 0xc7, 0x29, 0x6c, 0xcb, 0xbd, 0xe7, 0x92, 0x9b, 0x50, 0xc4, 0x2c, 0x4b, 0x54, 0x5c, 0x50,
+	0xf1, 0x65, 0x31, 0xe3, 0xcd, 0xa0, 0x26, 0xca, 0x83, 0x89, 0x1a, 0x49, 0x0d, 0x2f, 0xbe, 0x98,
+	0xc1, 0x0a, 0x2c, 0xe2, 0x3a, 0xd7, 0x11, 0x13, 0xbe, 0x82, 0x9f, 0xd3, 0x8f, 0x61, 0x34, 0x6b,
+	0x37, 0xc8, 0x00, 0x21, 0x9a, 0xf8, 0x04, 0x3d, 0xe7, 0x7f, 0xfa, 0xff, 0xf5, 0xdf, 0x66, 0x50,
+	0x0a, 0x3c, 0xda, 0xa3, 0x3c, 0x62, 0x21, 0xb5, 0xad, 0x20, 0x64, 0x11, 0xc3, 0x30, 0x2b, 0x19,
+	0x30, 0x60, 0x03, 0x26, 0xeb, 0x46, 0x91, 0x85, 0x2e, 0x0d, 0xb9, 0x5c, 0x99, 0xef, 0x08, 0x70,
+	0x3b, 0x16, 0x76, 0x82, 0x11, 0x73, 0x5c, 0x42, 0x9f, 0xc7, 0x94, 0x47, 0xf8, 0x10, 0xd4, 0x91,
+	0xf7, 0xe4, 0x45, 0x3a, 0xaa, 0xa2, 0xfa, 0x3f, 0xbb, 0x6c, 0x25, 0x43, 0xb7, 0xf1, 0xcf, 0x75,
+	0xdc, 0xb1, 0x89, 0x54, 0xe0, 0x1a, 0xa8, 0xa2, 0xa9, 0xe7, 0x85, 0xf4, 0x7f, 0x46, 0x6a, 0x13,
+	0xd9, 0xc4, 0xc7, 0xa0, 0xf6, 0x86, 0x63, 0xff, 0x51, 0x2f, 0x08, 0x55, 0xcd, 0x9a, 0xd1, 0x59,
+	0x8b, 0xfe, 0xd6, 0x45, 0xac, 0x25, 0x72, 0x04, 0xef, 0x81, 0xe2, 0x32, 0x9f, 0xea, 0x8a, 0x18,
+	0x2d, 0xa5, 0x06, 0x62, 0xec, 0xd2, 0xe1, 0x43, 0x22, 0xda, 0x46, 0x03, 0x54, 0x31, 0x86, 0x37,
+	0x41, 0x63, 0xfd, 0x3e, 0xa7, 0x92, 0xbe, 0x40, 0x92, 0x15, 0xc6, 0xa0, 0xb8, 0x4e, 0xe4, 0x08,
+	0xd0, 0x22, 0x11, 0xff, 0xcd, 0x26, 0x94, 0x33, 0xf6, 0x3c, 0x60, 0x3e, 0xa7, 0x53, 0x4b, 0xb4,
+	0xd2, 0xd2, 0x7c, 0x43, 0xb0, 0x21, 0x6a, 0x2d, 0xf6, 0xe2, 0xff, 0x6a, 0x7e, 0xcd, 0x6c, 0x7e,
+	0xfb, 0x0b, 0xf9, 0xcd, 0x11, 0x64, 0x12, 0x34, 0x4e, 0xd7, 0x45, 0xb3, 0x05, 0x20, 0x94, 0x0f,
+	0xdc, 0x9b, 0x50, 0x41, 0x52, 0x20, 0x7f, 0x45, 0xe5, 0xce, 0x9b, 0x50, 0xf3, 0x15, 0x41, 0x65,
+	0xce, 0x25, 0x09, 0xea, 0x24, 0xe5, 0x92, 0x07, 0x3d, 0x58, 0xc1, 0x25, 0x27, 0xb2, 0x60, 0x3f,
+	0xba, 0xb3, 0xb3, 0xe4, 0xc9, 0xb6, 0xe8, 0x88, 0x46, 0xf4, 0xfb, 0x91, 0x9b, 0x95, 0xe4, 0xd2,
+	0xd3, 0x0d, 0x24, 0x99, 0xfd, 0x81, 0x00, 0xda, 0x53, 0x7c, 0x7c, 0x03, 0x9a, 0x7c, 0x15, 0x78,
+	0x7b, 0xf5, 0x6b, 0x35, 0x76, 0xbe, 0xec, 0xcb, 0x9d, 0xcd, 0x5c, 0x1d, 0xe1, 0x0e, 0xfc, 0x49,
+	0xb3, 0xc0, 0xd5, 0x75, 0xd7, 0x67, 0xec, 0xae, 0x0d, 0x32, 0xde, 0xf4, 0x08, 0xe1, 0x2b, 0xd0,
+	0xe4, 0x31, 0x96, 0x50, 0x66, 0x02, 0x5a, 0x42, 0x99, 0x3d, 0xbf, 0x99, 0x3b, 0x57, 0xee, 0xf3,
+	0x41, 0xb7, 0xab, 0x89, 0x4f, 0x43, 0xe3, 0x33, 0x00, 0x00, 0xff, 0xff, 0xf5, 0xfa, 0xf7, 0xbe,
+	0x55, 0x04, 0x00, 0x00,
 }
