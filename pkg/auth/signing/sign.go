6030a446292abdd9732b6654bb4e2fafbe499c80
Egon Elbre <egonelbre@gmail.com>
2019-03-14 12:45:34 +0200

    comments for signing package

--- pkg/auth/signing/sign.go
+++ pkg/auth/signing/sign.go
@@ -10,16 +10,18 @@ import (
 	"storj.io/storj/pkg/storj"
 )
 
-var (
-	Error = errs.Class("signing")
-)
+// Error is the default error class for signing package.
+var Error = errs.Class("signing")
 
+// Signer is able to sign data and verify own signature belongs.
 type Signer interface {
 	ID() storj.NodeID
 	HashAndSign(data []byte) ([]byte, error)
 	HashAndVerifySignature(data, signature []byte) error
 }
 
+// SignOrderLimit signs the order limit using the specified signer.
+// Signer is a satellite.
 func SignOrderLimit(satellite Signer, unsigned *pb.OrderLimit2) (*pb.OrderLimit2, error) {
 	bytes, err := EncodeOrderLimit(unsigned)
 	if err != nil {
@@ -35,6 +37,8 @@ func SignOrderLimit(satellite Signer, unsigned *pb.OrderLimit2) (*pb.OrderLimit2
 	return &signed, nil
 }
 
+// SignOrder signs the order using the specified signer.
+// Signer is an uplink.
 func SignOrder(uplink Signer, unsigned *pb.Order2) (*pb.Order2, error) {
 	bytes, err := EncodeOrder(unsigned)
 	if err != nil {
@@ -50,6 +54,8 @@ func SignOrder(uplink Signer, unsigned *pb.Order2) (*pb.Order2, error) {
 	return &signed, nil
 }
 
+// SignOrder signs the piece hash using the specified signer.
+// Signer is either uplink or storage node.
 func SignPieceHash(signer Signer, unsigned *pb.PieceHash) (*pb.PieceHash, error) {
 	bytes, err := EncodePieceHash(unsigned)
 	if err != nil {
