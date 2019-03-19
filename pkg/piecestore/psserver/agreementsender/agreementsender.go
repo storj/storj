6de8f7426a6d9bfd30346b28cd68ff8d6cb3f2c6
Egon Elbre <egonelbre@gmail.com>
2019-03-19 15:22:35 +0200

    backport of agreement sender fixes (#1523)
    * agreement sender fixes
    * minimal fix for PeerIdentityFromPeer

--- pkg/piecestore/psserver/agreementsender/agreementsender.go
+++ pkg/piecestore/psserver/agreementsender/agreementsender.go
@@ -4,11 +4,13 @@
 package agreementsender
 
 import (
+	"io"
 	"time"
 
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 	"golang.org/x/net/context"
+	"golang.org/x/sync/errgroup"
 
 	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/pb"
@@ -44,20 +46,32 @@ func (as *AgreementSender) Run(ctx context.Context) error {
 	ticker := time.NewTicker(as.checkInterval)
 	defer ticker.Stop()
 	for {
-		as.log.Debug("AgreementSender is running", zap.Duration("duration", as.checkInterval))
+		as.log.Debug("is running", zap.Duration("duration", as.checkInterval))
 		agreementGroups, err := as.DB.GetBandwidthAllocations()
 		if err != nil {
-			as.log.Error("Agreementsender could not retrieve bandwidth allocations", zap.Error(err))
+			as.log.Error("could not retrieve bandwidth allocations", zap.Error(err))
 			continue
 		}
-		// send agreement payouts
-		for satellite, agreements := range agreementGroups {
-			as.SendAgreementsToSatellite(ctx, satellite, agreements)
+
+		if len(agreementGroups) > 0 {
+			var group errgroup.Group
+			// send agreement payouts
+			for satellite, agreements := range agreementGroups {
+				satellite, agreements := satellite, agreements
+				group.Go(func() error {
+					timedCtx, cancel := context.WithTimeout(ctx, time.Hour)
+					defer cancel()
+
+					as.SettleAgreements(timedCtx, satellite, agreements)
+					return nil
+				})
+			}
+			_ = group.Wait() // doesn't return errors
 		}
 
 		// Delete older payout irrespective of its status
 		if err = as.DB.DeleteBandwidthAllocationPayouts(); err != nil {
-			as.log.Error("Agreementsender failed to delete bandwidth allocation", zap.Error(err))
+			as.log.Error("failed to delete bandwidth allocation", zap.Error(err))
 		}
 		select {
 		case <-ticker.C:
@@ -67,55 +81,73 @@ func (as *AgreementSender) Run(ctx context.Context) error {
 	}
 }
 
-//SendAgreementsToSatellite uploads agreements to the satellite
-func (as *AgreementSender) SendAgreementsToSatellite(ctx context.Context, satID storj.NodeID, agreements []*psdb.Agreement) {
-	as.log.Info("Sending agreements to satellite", zap.Int("number of agreements", len(agreements)), zap.String("satellite id", satID.String()))
-	// todo: cache kad responses if this interval is very small
-	// Get satellite ip from kademlia
-	satellite, err := as.kad.FindNode(ctx, satID)
+// SettleAgreements uploads agreements to the satellite
+func (as *AgreementSender) SettleAgreements(ctx context.Context, satelliteID storj.NodeID, agreements []*psdb.Agreement) {
+	as.log.Info("sending agreements to satellite", zap.Int("number of agreements", len(agreements)), zap.String("satellite id", satelliteID.String()))
+
+	satellite, err := as.kad.FindNode(ctx, satelliteID)
 	if err != nil {
-		as.log.Warn("Agreementsender could not find satellite", zap.Error(err))
+		as.log.Warn("could not find satellite", zap.Error(err))
 		return
 	}
-	// Create client from satellite ip
+
 	conn, err := as.transport.DialNode(ctx, &satellite)
 	if err != nil {
-		as.log.Warn("Agreementsender could not dial satellite", zap.Error(err))
+		as.log.Warn("could not dial satellite", zap.Error(err))
 		return
 	}
-	client := pb.NewBandwidthClient(conn)
 	defer func() {
 		if err := conn.Close(); err != nil {
-			as.log.Warn("Agreementsender failed to close connection", zap.Error(err))
+			as.log.Warn("failed to close connection", zap.Error(err))
 		}
 	}()
 
-	//todo:  stop sending these one-by-one, send all at once
-	for _, agreement := range agreements {
-		rba := agreement.Agreement
-		// Send agreement to satellite
-		sat, err := client.BandwidthAgreements(ctx, &rba)
-		if err != nil {
-			switch sat.GetStatus() {
-			case pb.AgreementsSummary_FAIL:
-				// CASE FAILED: connection with sat couldnt be established or connection
-				// is established but lost before receiving response from satellite
-				// no updates to the bwa status is done, so it remains "UNSENT"
-				as.log.Warn("Agreementsender lost connection", zap.Error(err))
-			default:
-				// CASE REJECTED: successful connection with sat established but either failed or rejected received
-				as.log.Warn("Agreementsender had agreement explicitly rejected/failed by satellite")
-				err = as.DB.UpdateBandwidthAllocationStatus(rba.PayerAllocation.SerialNumber, psdb.AgreementStatusReject)
-				if err != nil {
-					as.log.Error("Agreementsender error", zap.Error(err))
-				}
-			}
-		} else {
-			// updates the status to "SENT"
-			err = as.DB.UpdateBandwidthAllocationStatus(rba.PayerAllocation.SerialNumber, psdb.AgreementStatusSent)
+	client, err := pb.NewBandwidthClient(conn).Settlement(ctx)
+	if err != nil {
+		as.log.Error("failed to start settlement", zap.Error(err))
+		return
+	}
+
+	var group errgroup.Group
+	group.Go(func() error {
+		for _, agreement := range agreements {
+			err := client.Send(&pb.BandwidthSettlementRequest{
+				Allocation: &agreement.Agreement,
+			})
 			if err != nil {
-				as.log.Error("Agreementsender error", zap.Error(err))
+				return err
 			}
 		}
+		return client.CloseSend()
+	})
+
+	for {
+		response, err := client.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			as.log.Error("failed to recv response", zap.Error(err))
+			break
+		}
+
+		switch response.Status {
+		case pb.AgreementsSummary_REJECTED:
+			err = as.DB.UpdateBandwidthAllocationStatus(response.SerialNumber, psdb.AgreementStatusReject)
+			if err != nil {
+				as.log.Error("error", zap.Error(err))
+			}
+		case pb.AgreementsSummary_OK:
+			err = as.DB.UpdateBandwidthAllocationStatus(response.SerialNumber, psdb.AgreementStatusSent)
+			if err != nil {
+				as.log.Error("error", zap.Error(err))
+			}
+		default:
+			as.log.Error("unexpected response", zap.Error(err))
+		}
+	}
+
+	if err := group.Wait(); err != nil {
+		as.log.Error("sending agreements returned an error", zap.Error(err))
 	}
 }
