4a19dec8f9aab32215df373a09ac272eff7c2672
Michal Niewrzal <michal@storj.io>
2019-03-19 09:54:11 +0100

    Move psserver tests

--- storagenode/piecestore/endpoint_test.go
+++ storagenode/piecestore/endpoint_test.go
@@ -4,17 +4,23 @@
 package piecestore_test
 
 import (
+	"crypto/sha256"
 	"io"
 	"math/rand"
 	"testing"
 	"time"
 
+	"github.com/golang/protobuf/ptypes"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"storj.io/storj/internal/memory"
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testplanet"
+	"storj.io/storj/internal/teststorj"
+	"storj.io/storj/pkg/auth/signing"
+	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/storj"
 	"storj.io/storj/storagenode/bandwidth"
 	"storj.io/storj/uplink/piecestore"
 )
@@ -33,7 +39,7 @@ func TestUploadAndPartialDownload(t *testing.T) {
 	_, err = rand.Read(expectedData)
 	require.NoError(t, err)
 
-	err = planet.Uplinks[0].Upload(ctx, planet.Satellites[0], "test/bucket", "test/path", expectedData)
+	err = planet.Uplinks[0].Upload(ctx, planet.Satellites[0], "testBucket", "test/path", expectedData)
 	assert.NoError(t, err)
 
 	var totalDownload int64
@@ -49,7 +55,7 @@ func TestUploadAndPartialDownload(t *testing.T) {
 		}
 		totalDownload += piecestore.DefaultConfig.InitialStep
 
-		download, err := planet.Uplinks[0].DownloadStream(ctx, planet.Satellites[0], "test/bucket", "test/path")
+		download, err := planet.Uplinks[0].DownloadStream(ctx, planet.Satellites[0], "testBucket", "test/path")
 		require.NoError(t, err)
 
 		pos, err := download.Seek(tt.offset, io.SeekStart)
@@ -73,9 +79,9 @@ func TestUploadAndPartialDownload(t *testing.T) {
 		totalBandwidthUsage.Add(usage)
 	}
 
-	err = planet.Uplinks[0].Delete(ctx, planet.Satellites[0], "test/bucket", "test/path")
+	err = planet.Uplinks[0].Delete(ctx, planet.Satellites[0], "testBucket", "test/path")
 	require.NoError(t, err)
-	_, err = planet.Uplinks[0].Download(ctx, planet.Satellites[0], "test/bucket", "test/path")
+	_, err = planet.Uplinks[0].Download(ctx, planet.Satellites[0], "testBucket", "test/path")
 	require.Error(t, err)
 
 	// check rough limits for the upload and download
@@ -84,4 +90,304 @@ func TestUploadAndPartialDownload(t *testing.T) {
 	assert.True(t, totalUpload < totalBandwidthUsage.Put && totalBandwidthUsage.Put < int64(len(planet.StorageNodes))*totalUpload)
 	t.Log(totalDownload, totalBandwidthUsage.Get, int64(len(planet.StorageNodes))*totalDownload)
 	assert.True(t, totalBandwidthUsage.Get < int64(len(planet.StorageNodes))*totalDownload)
+}
+
+func TestUpload(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.New(t, 1, 1, 1)
+	require.NoError(t, err)
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+
+	client, err := planet.Uplinks[0].DialPiecestore(ctx, planet.StorageNodes[0])
+	require.NoError(t, err)
+
+	for _, tt := range []struct {
+		pieceID storj.PieceID
+		content []byte
+		action  pb.PieceAction
+		err     string
+	}{
+		{ // should successfully store data
+			pieceID: teststorj.PieceIDFromString("99999999999999999999"),
+			content: []byte("xyzwq"),
+			action:  pb.PieceAction_PUT,
+			err:     "",
+		},
+		{ // should err with piece ID not specified
+			pieceID: teststorj.PieceIDFromBytes(make([]byte, 32)),
+			content: []byte("xyzwq"),
+			action:  pb.PieceAction_PUT,
+			err:     "missing piece id",
+		},
+		{ // should successfully store data
+			pieceID: teststorj.PieceIDFromString("99999999999999999999"),
+			content: []byte("xyzwq"),
+			action:  pb.PieceAction_GET,
+			err:     "expected put or put repair action got GET",
+		},
+	} {
+		sum := sha256.Sum256(tt.content)
+		expectedHash := sum[:]
+
+		var serialNumber storj.SerialNumber
+		_, _ = rand.Read(serialNumber[:])
+
+		orderLimit := GenerateOrderLimit(
+			t,
+			planet.Satellites[0].ID(),
+			planet.Uplinks[0].ID(),
+			planet.StorageNodes[0].ID(),
+			tt.pieceID,
+			tt.action,
+			serialNumber,
+			24*time.Hour,
+			24*time.Hour,
+			100,
+		)
+		signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+		orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+		require.NoError(t, err)
+
+		uploader, err := client.Upload(ctx, orderLimit)
+		require.NoError(t, err)
+
+		_, err = uploader.Write(tt.content)
+		require.NoError(t, err)
+
+		pieceHash, err := uploader.Commit()
+		if tt.err != "" {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.err)
+		} else {
+			require.NoError(t, err)
+
+			assert.Equal(t, expectedHash, pieceHash.Hash)
+
+			signee := signing.SignerFromFullIdentity(planet.StorageNodes[0].Identity)
+			require.NoError(t, signing.VerifyPieceHashSignature(signee, pieceHash))
+		}
+	}
+}
+
+func TestDownload(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.New(t, 1, 1, 1)
+	require.NoError(t, err)
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+
+	// upload test piece
+	client, err := planet.Uplinks[0].DialPiecestore(ctx, planet.StorageNodes[0])
+	require.NoError(t, err)
+
+	var serialNumber storj.SerialNumber
+	_, _ = rand.Read(serialNumber[:])
+
+	orderLimit := GenerateOrderLimit(
+		t,
+		planet.Satellites[0].ID(),
+		planet.Uplinks[0].ID(),
+		planet.StorageNodes[0].ID(),
+		teststorj.PieceIDFromString("existing-piece-id"),
+		pb.PieceAction_PUT,
+		serialNumber,
+		24*time.Hour,
+		24*time.Hour,
+		100,
+	)
+	signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+	orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+	require.NoError(t, err)
+
+	uploader, err := client.Upload(ctx, orderLimit)
+	require.NoError(t, err)
+
+	expectedData := []byte("for-download-test")
+	_, err = uploader.Write(expectedData)
+	require.NoError(t, err)
+
+	_, err = uploader.Commit()
+	require.NoError(t, err)
+
+	for _, tt := range []struct {
+		pieceID storj.PieceID
+		action  pb.PieceAction
+		err     string
+	}{
+		{ // should successfully download data
+			pieceID: orderLimit.PieceId,
+			action:  pb.PieceAction_GET,
+			err:     "",
+		},
+		{ // should err with piece ID not specified
+			pieceID: teststorj.PieceIDFromString("non-existing-piece"),
+			action:  pb.PieceAction_GET,
+			err:     "no such file or directory", // TODO fix returned error
+		},
+		{ // should successfully download data
+			pieceID: orderLimit.PieceId,
+			action:  pb.PieceAction_PUT,
+			err:     "expected get or get repair or audit action got PUT",
+		},
+	} {
+		var serialNumber storj.SerialNumber
+		_, _ = rand.Read(serialNumber[:])
+
+		orderLimit := GenerateOrderLimit(
+			t,
+			planet.Satellites[0].ID(),
+			planet.Uplinks[0].ID(),
+			planet.StorageNodes[0].ID(),
+			tt.pieceID,
+			tt.action,
+			serialNumber,
+			24*time.Hour,
+			24*time.Hour,
+			100,
+		)
+		signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+		orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+		require.NoError(t, err)
+
+		downloader, err := client.Download(ctx, orderLimit, 0, int64(len(expectedData)))
+		require.NoError(t, err)
+
+		buffer := make([]byte, 100)
+		n, err := downloader.Read(buffer)
+
+		if tt.err != "" {
+		} else {
+			require.NoError(t, err)
+			require.Equal(t, expectedData, buffer[:n])
+		}
+
+		err = downloader.Close()
+		if tt.err != "" {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.New(t, 1, 1, 1)
+	require.NoError(t, err)
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+
+	// upload test piece
+	client, err := planet.Uplinks[0].DialPiecestore(ctx, planet.StorageNodes[0])
+	require.NoError(t, err)
+
+	var serialNumber storj.SerialNumber
+	_, _ = rand.Read(serialNumber[:])
+
+	orderLimit := GenerateOrderLimit(
+		t,
+		planet.Satellites[0].ID(),
+		planet.Uplinks[0].ID(),
+		planet.StorageNodes[0].ID(),
+		teststorj.PieceIDFromString("existing-piece-id"),
+		pb.PieceAction_PUT,
+		serialNumber,
+		24*time.Hour,
+		24*time.Hour,
+		100,
+	)
+	signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+	orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+	require.NoError(t, err)
+
+	uploader, err := client.Upload(ctx, orderLimit)
+	require.NoError(t, err)
+
+	expectedData := []byte("for-delete-test")
+	_, err = uploader.Write(expectedData)
+	require.NoError(t, err)
+
+	_, err = uploader.Commit()
+	require.NoError(t, err)
+
+	for _, tt := range []struct {
+		pieceID storj.PieceID
+		action  pb.PieceAction
+		err     string
+	}{
+		{ // should successfully download data
+			pieceID: orderLimit.PieceId,
+			action:  pb.PieceAction_DELETE,
+			err:     "",
+		},
+		{ // should err with piece ID not specified
+			pieceID: teststorj.PieceIDFromString("non-existing-piece"),
+			action:  pb.PieceAction_DELETE,
+			err:     "", // TODO should this return error
+		},
+		{ // should successfully download data
+			pieceID: orderLimit.PieceId,
+			action:  pb.PieceAction_GET,
+			err:     "expected delete action got GET",
+		},
+	} {
+		var serialNumber storj.SerialNumber
+		_, _ = rand.Read(serialNumber[:])
+
+		orderLimit := GenerateOrderLimit(
+			t,
+			planet.Satellites[0].ID(),
+			planet.Uplinks[0].ID(),
+			planet.StorageNodes[0].ID(),
+			tt.pieceID,
+			tt.action,
+			serialNumber,
+			24*time.Hour,
+			24*time.Hour,
+			100,
+		)
+		signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+		orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+		require.NoError(t, err)
+
+		err := client.Delete(ctx, orderLimit)
+		if tt.err != "" {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}
+
+func GenerateOrderLimit(t *testing.T, satellite storj.NodeID, uplink storj.NodeID, storageNode storj.NodeID, pieceID storj.PieceID,
+	action pb.PieceAction, serialNumber storj.SerialNumber, pieceExpiration, orderExpiration time.Duration, limit int64) *pb.OrderLimit2 {
+
+	pe, err := ptypes.TimestampProto(time.Now().Add(pieceExpiration))
+	require.NoError(t, err)
+	oe, err := ptypes.TimestampProto(time.Now().Add(orderExpiration))
+	require.NoError(t, err)
+	orderLimit := &pb.OrderLimit2{
+		SatelliteId:     satellite,
+		UplinkId:        uplink,
+		StorageNodeId:   storageNode,
+		PieceId:         pieceID,
+		Action:          action,
+		SerialNumber:    serialNumber,
+		OrderExpiration: oe,
+		PieceExpiration: pe,
+		Limit:           limit,
+	}
+	return orderLimit
 }
---
+++ storagenode/piecestore/verification_test.go
@@ -0,0 +1,130 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information
+
+package piecestore_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"storj.io/storj/internal/testcontext"
+	"storj.io/storj/internal/testplanet"
+	"storj.io/storj/internal/teststorj"
+	"storj.io/storj/pkg/auth/signing"
+	"storj.io/storj/pkg/identity"
+	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/storj"
+)
+
+func TestOrderLimitValidation(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.New(t, 1, 1, 1)
+	require.NoError(t, err)
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+
+	for _, tt := range []struct {
+		satelliteID     *identity.FullIdentity
+		uplinkID        storj.NodeID
+		storageNodeID   storj.NodeID
+		pieceID         storj.PieceID
+		action          pb.PieceAction
+		serialNumber    storj.SerialNumber
+		pieceExpiration time.Duration
+		orderExpiration time.Duration
+		limit           int64
+		err             string
+	}{
+		{ // unapproved satellite id
+			satelliteID:     testplanet.MustPregeneratedIdentity(0),
+			pieceID:         teststorj.PieceIDFromString("piece-id-1"),
+			action:          pb.PieceAction_PUT,
+			serialNumber:    storj.SerialNumber([16]byte{1}),
+			pieceExpiration: time.Duration(4 * 24 * time.Hour),
+			orderExpiration: time.Duration(4 * 24 * time.Hour),
+			err:             " is untrusted",
+		},
+		{ // approved satellite id
+			pieceID:         teststorj.PieceIDFromString("piece-id-2"),
+			action:          pb.PieceAction_PUT,
+			serialNumber:    storj.SerialNumber([16]byte{2}),
+			pieceExpiration: time.Duration(4 * 24 * time.Hour),
+			orderExpiration: time.Duration(4 * 24 * time.Hour),
+		},
+		{ // wrong action type
+			pieceID:         teststorj.PieceIDFromString("piece-id-3"),
+			action:          pb.PieceAction_GET,
+			serialNumber:    storj.SerialNumber([16]byte{3}),
+			pieceExpiration: time.Duration(4 * 24 * time.Hour),
+			orderExpiration: time.Duration(4 * 24 * time.Hour),
+			err:             "expected put or put repair action got GET",
+		},
+		{ // piece expired
+			pieceID:         teststorj.PieceIDFromString("piece-id-4"),
+			action:          pb.PieceAction_PUT,
+			serialNumber:    storj.SerialNumber([16]byte{4}),
+			pieceExpiration: time.Duration(-4 * 24 * time.Hour),
+			orderExpiration: time.Duration(4 * 24 * time.Hour),
+			err:             "piece expired:",
+		},
+		{ // limit is negative
+			pieceID:         teststorj.PieceIDFromString("piece-id-5"),
+			action:          pb.PieceAction_PUT,
+			serialNumber:    storj.SerialNumber([16]byte{5}),
+			pieceExpiration: time.Duration(4 * 24 * time.Hour),
+			orderExpiration: time.Duration(4 * 24 * time.Hour),
+			limit:           -1,
+			err:             "order limit is negative",
+		},
+		{ // order limit expired
+			pieceID:         teststorj.PieceIDFromString("piece-id-6"),
+			action:          pb.PieceAction_PUT,
+			serialNumber:    storj.SerialNumber([16]byte{5}),
+			pieceExpiration: time.Duration(4 * 24 * time.Hour),
+			orderExpiration: time.Duration(-4 * 24 * time.Hour),
+			err:             "order expired:",
+		},
+	} {
+		client, err := planet.Uplinks[0].DialPiecestore(ctx, planet.StorageNodes[0])
+		require.NoError(t, err)
+
+		signer := signing.SignerFromFullIdentity(planet.Satellites[0].Identity)
+		satellite := planet.Satellites[0].Identity
+		if tt.satelliteID != nil {
+			signer = signing.SignerFromFullIdentity(tt.satelliteID)
+			satellite = tt.satelliteID
+		}
+
+		orderLimit := GenerateOrderLimit(
+			t,
+			satellite.ID,
+			planet.Uplinks[0].ID(),
+			planet.StorageNodes[0].ID(),
+			tt.pieceID,
+			tt.action,
+			tt.serialNumber,
+			tt.pieceExpiration,
+			tt.orderExpiration,
+			tt.limit,
+		)
+
+		orderLimit, err = signing.SignOrderLimit(signer, orderLimit)
+		require.NoError(t, err)
+
+		uploader, err := client.Upload(ctx, orderLimit)
+		require.NoError(t, err)
+
+		_, err = uploader.Commit()
+		if tt.err != "" {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.err)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}
