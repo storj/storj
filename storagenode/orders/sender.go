7ac3d560199bc253036e64e49664349a6edeb165
Egon Elbre <egonelbre@gmail.com>
2019-03-20 14:33:20 +0200

    implement order sending

--- storagenode/orders/sender.go
+++ storagenode/orders/sender.go
@@ -9,6 +9,7 @@ import (
 
 	"go.uber.org/zap"
 
+	"storj.io/storj/internal/sync2"
 	"storj.io/storj/pkg/identity"
 	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/pb"
@@ -37,21 +38,36 @@ type SenderConfig struct {
 
 // Sender sends every interval unsent orders to the satellite.
 type Sender struct {
-	log    *zap.Logger
-	config SenderConfig
+	log *zap.Logger
 
 	client   transport.Client
 	kademlia *kademlia.Kademlia
 	orders   DB
+
+	Loop sync2.Cycle
 }
 
 // NewSender creates an order sender.
 func NewSender(log *zap.Logger, client transport.Client, kademlia *kademlia.Kademlia, orders DB, config SenderConfig) *Sender {
 	return &Sender{
 		log:      log,
-		config:   config,
 		client:   client,
 		kademlia: kademlia,
 		orders:   orders,
+
+		Loop: *sync2.NewCycle(config.Interval),
 	}
+}
+
+// Run sends orders on every interval to the appropriate satellites.
+func (sender *Sender) Run(ctx context.Context) error {
+	return sender.Loop.Run(ctx, func(ctx context.Context) error {
+		return nil
+	})
+}
+
+// Close stops the sending service.
+func (sender *Sender) Close() error {
+	sender.Loop.Stop()
+	return nil
 }
