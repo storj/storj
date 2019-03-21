5dd789b32f049e3754c17bb80ba0b7db819afffe
Alexander Leitner <leitnersalex@gmail.com>
2019-03-21 14:08:28 -0400

    Server has basic variables for looking up object info

--- pkg/health/server.go
+++ pkg/health/server.go
@@ -6,12 +6,31 @@ package health
 import (
 	"context"
 
+	"go.uber.org/zap"
+
+	"github.com/zeebo/errs"
+
 	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/pointerdb"
+)
+
+var (
+	// Error wraps errors returned from Server struct methods
+	Error = errs.Class("HealthEndpoint error")
 )
 
 type HealthEndpoint struct {
-	// pointerdb
-	// overlay cache
+	pointerdb *pointerdb.Service
+	overlay   pb.OverlayServer
+	log       *zap.Logger
+}
+
+func NewHealthEndpoint(pdb *pointerdb.Service, os pb.OverlayServer, log *zap.Logger) (*HealthEndpoint, error) {
+	return &HealthEndpoint{
+		log:       log,
+		overlay:   os,
+		pointerdb: pdb,
+	}, nil
 }
 
 func (s *HealthEndpoint) ObjectStat(context.Context, *pb.ObjectHealthRequest) (*pb.ObjectHealthResponse, error) {
