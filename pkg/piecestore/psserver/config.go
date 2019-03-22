fdc98fda373671b8983cf0747b71d67118f0a542
Jessica Grebenschikov <Jessica.greben1+github@gmail.com>
2019-03-22 06:47:37 -0700

    add devDefault

--- pkg/piecestore/psserver/config.go
+++ pkg/piecestore/psserver/config.go
@@ -20,7 +20,7 @@ type Config struct {
 	Path string `help:"path to store data in" default:"$CONFDIR/storage"`
 
 	WhitelistedSatelliteIDs string        `help:"a comma-separated list of approved satellite node ids" default:""`
-	SatelliteIDRestriction  bool          `help:"if true, only allow data from approved satellites" default:"true"`
+	SatelliteIDRestriction  bool          `help:"if true, only allow data from approved satellites" devDefault:"false" default:"true"`
 	AllocatedDiskSpace      memory.Size   `user:"true" help:"total allocated disk space in bytes" default:"1TB"`
 	AllocatedBandwidth      memory.Size   `user:"true" help:"total allocated bandwidth in bytes" default:"500GiB"`
 	KBucketRefreshInterval  time.Duration `help:"how frequently Kademlia bucket should be refreshed with node stats" default:"1h0m0s"`
