b3142b69266b9fcf7284d7867c0ac5ed2e6fdf1a
Egon Elbre <egonelbre@gmail.com>
2019-03-22 18:11:38 +0200

    everything compiling

--- pkg/overlay/cache_test.go
+++ pkg/overlay/cache_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
-	"go.uber.org/zap/zaptest"
 
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/pkg/overlay"
