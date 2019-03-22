b915fb8f460fb450ee5f12622a4047d751bf5461
wthorp <bill3000@hotmail.com>
2019-03-22 16:14:44 -0400

    delete expired pointers during audit with tests

--- pkg/audit/cursor_test.go
+++ pkg/audit/cursor_test.go
@@ -9,10 +9,13 @@ import (
 	"math/big"
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"storj.io/storj/pkg/pointerdb"
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testplanet"
 	"storj.io/storj/internal/teststorj"
@@ -34,79 +37,8 @@ func TestAuditSegment(t *testing.T) {
 		// note: to simulate better,
 		// change limit in library to 5 in
 		// list api call, default is  0 == 1000 listing
-		tests := []struct {
-			bm   string
-			path storj.Path
-		}{
-			{
-				bm:   "success-1",
-				path: "folder1/file1",
-			},
-			{
-				bm:   "success-2",
-				path: "foodFolder1/file1/file2",
-			},
-			{
-				bm:   "success-3",
-				path: "foodFolder1/file1/file2/foodFolder2/file3",
-			},
-			{
-				bm:   "success-4",
-				path: "projectFolder/project1.txt/",
-			},
-			{
-				bm:   "success-5",
-				path: "newProjectFolder/project2.txt",
-			},
-			{
-				bm:   "success-6",
-				path: "Pictures/image1.png",
-			},
-			{
-				bm:   "success-7",
-				path: "Pictures/Nature/mountains.png",
-			},
-			{
-				bm:   "success-8",
-				path: "Pictures/City/streets.png",
-			},
-			{
-				bm:   "success-9",
-				path: "Pictures/Animals/Dogs/dogs.png",
-			},
-			{
-				bm:   "success-10",
-				path: "Nada/ビデオ/😶",
-			},
-		}
-
-		pointerdb := planet.Satellites[0].Metainfo.Service
-		allocation := planet.Satellites[0].Metainfo.Allocation
-		cache := planet.Satellites[0].Overlay.Service
-		// create a pdb client and instance of audit
-		cursor := audit.NewCursor(pointerdb, allocation, cache, planet.Satellites[0].Identity)
-
-		// put 10 paths in db
-		t.Run("putToDB", func(t *testing.T) {
-			for _, tt := range tests {
-				t.Run(tt.bm, func(t *testing.T) {
-					assert1 := assert.New(t)
-
-					// create a pointer and put in db
-					putRequest := makePutRequest(tt.path)
-
-					// put pointer into db
-					err := pointerdb.Put(tt.path, putRequest.Pointer)
-					if err != nil {
-						t.Fatalf("failed to put %v: error: %v", putRequest.Pointer, err)
-						assert1.NotNil(err)
-					}
-					if err != nil {
-						t.Error("cant instantiate the piece store client")
-					}
-				})
-			}
-		})
+		//populate pointerdb with 10 non-expired pointers of test data
+		tests, cursor, pointerdb := populateTestData(t, planet, &timestamp.Timestamp{Seconds:time.Now().Unix()+3000})
 
 		t.Run("NextStripe", func(t *testing.T) {
 			for _, tt := range tests {
@@ -183,7 +115,7 @@ func TestAuditSegment(t *testing.T) {
 	})
 }
 
-func makePutRequest(path storj.Path) pb.PutRequest {
+func makePutRequest(path storj.Path, expiration *timestamp.Timestamp) pb.PutRequest {
 	var rps []*pb.RemotePiece
 	rps = append(rps, &pb.RemotePiece{
 		PieceNum: 1,
@@ -192,6 +124,7 @@ func makePutRequest(path storj.Path) pb.PutRequest {
 	pr := pb.PutRequest{
 		Path: path,
 		Pointer: &pb.Pointer{
+			ExpirationDate: expiration,
 			Type: pb.Pointer_REMOTE,
 			Remote: &pb.RemoteSegment{
 				Redundancy: &pb.RedundancyScheme{
@@ -209,4 +142,67 @@ func makePutRequest(path storj.Path) pb.PutRequest {
 		},
 	}
 	return pr
+}
+
+func TestDeleteExpired(t *testing.T) {
+	testplanet.Run(t, testplanet.Config{
+		SatelliteCount: 1, StorageNodeCount: 4, UplinkCount: 1,
+	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		//populate pointerdb with 10 expired pointers of test data
+		tests, cursor, pointerdb := populateTestData(t, planet, &timestamp.Timestamp{})
+		//make sure it they're in there
+		list, _, err := pointerdb.List("", "", "", true, 10, meta.None)
+		require.NoError(t, err)
+		require.Len(t, list, 10)
+		// make sure its all null and no errors
+		t.Run("NextStripe", func(t *testing.T) {
+			for _, tt := range tests {
+				t.Run(tt.bm, func(t *testing.T) {
+					stripe, err := cursor.NextStripe(ctx)
+					require.NoError(t, err)
+					require.Nil(t, stripe)
+				})
+			}
+		})
+		//make sure it they're not in there anymore
+		list, _, err = pointerdb.List("", "", "", true, 10, meta.None)
+		require.NoError(t, err)
+		require.Len(t, list, 0)
+	})
+}
+
+
+type testData struct {
+	bm   string
+	path storj.Path
+}
+
+func populateTestData(t *testing.T, planet *testplanet.Planet, expiration *timestamp.Timestamp) ([]testData, *audit.Cursor, *pointerdb.Service){
+	tests := []testData{
+		{ bm:   "success-1", path: "folder1/file1" },
+		{ bm:   "success-2", path: "foodFolder1/file1/file2" },
+		{ bm:   "success-3", path: "foodFolder1/file1/file2/foodFolder2/file3" },
+		{ bm:   "success-4", path: "projectFolder/project1.txt/" },
+		{ bm:   "success-5", path: "newProjectFolder/project2.txt" },
+		{ bm:   "success-6", path: "Pictures/image1.png" },
+		{ bm:   "success-7", path: "Pictures/Nature/mountains.png" },
+		{ bm:   "success-8", path: "Pictures/City/streets.png" },
+		{ bm:   "success-9", path: "Pictures/Animals/Dogs/dogs.png" },
+		{ bm:   "success-10", path: "Nada/ビデオ/😶" },
+	}
+	pointerdb := planet.Satellites[0].Metainfo.Service
+	allocation := planet.Satellites[0].Metainfo.Allocation
+	cache := planet.Satellites[0].Overlay.Service
+	cursor := audit.NewCursor(pointerdb, allocation, cache, planet.Satellites[0].Identity)
+
+	// put 10 expired pointers in db
+	t.Run("putToDB", func(t *testing.T) {
+		for _, tt := range tests {
+			t.Run(tt.bm, func(t *testing.T) {
+				putRequest := makePutRequest(tt.path, expiration)
+				require.NoError(t, pointerdb.Put(tt.path, putRequest.Pointer))
+			})
+		}
+	})
+	return tests, cursor, pointerdb
 }
