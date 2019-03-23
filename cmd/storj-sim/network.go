b9bb986b8a24ebb9607f63f3954511cb66b47888
Egon Elbre <egonelbre@gmail.com>
2019-03-23 23:53:03 +0200

    storj-sim network env (#1560)
    * add storj-sim environment flags
    
    * don't try to connect to console

--- cmd/storj-sim/network.go
+++ cmd/storj-sim/network.go
@@ -54,6 +54,29 @@ func networkExec(flags *Flags, args []string, command string) error {
 	return errs.Combine(err, closeErr)
 }
 
+func networkEnv(flags *Flags, args []string) error {
+	flags.OnlyEnv = true
+	processes, err := newNetwork(flags)
+	if err != nil {
+		return err
+	}
+
+	// run exec before, since it will load env vars from configs
+	for _, process := range processes.List {
+		if exec := process.ExecBefore["run"]; exec != nil {
+			if err := exec(process); err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, env := range processes.Env() {
+		fmt.Println(env)
+	}
+
+	return nil
+}
+
 func networkTest(flags *Flags, command string, args []string) error {
 	processes, err := newNetwork(flags)
 	if err != nil {
@@ -255,7 +278,7 @@ func newNetwork(flags *Flags) (*Processes, error) {
 			// create example project with key and add it to the config
 			// so that gateway can have access to the satellite
 			apiKey := vip.GetString("api-key")
-			if apiKey == "" {
+			if !flags.OnlyEnv && apiKey == "" {
 				var consoleAddress string
 				satelliteConfigErr := readConfigString(&consoleAddress, satellite.Directory, "console.address")
 				if satelliteConfigErr != nil {
@@ -279,6 +302,10 @@ func newNetwork(flags *Flags) (*Processes, error) {
 				if err := vip.WriteConfig(); err != nil {
 					return err
 				}
+			}
+
+			if apiKey != "" {
+				process.Extra = append(process.Extra, "API_KEY="+apiKey)
 			}
 
 			accessKey := vip.GetString("minio.access-key")
