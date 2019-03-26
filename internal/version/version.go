b276d999ac7cc3484a9d9bccf8249fb7e6f95258
stefanbenten <mail@stefan-benten.de>
2019-03-26 18:24:19 +0100

    Fix Linting and Spelling Mistakes

--- internal/version/version.go
+++ internal/version/version.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+// Info contains all necessary values for comparing different binary version with each other.
+// It's goal is to limit the startup up outdated binaries as well as having an easier way to ensure the software
+// is on the latest version from a UI/UX perspective
 type Info struct {
 	Timestamp  string `json:"timestamp"`
 	CommitHash string `json:"hash"`
@@ -25,7 +28,8 @@ func (v Info) Marshal() (data []byte, err error) {
 	return
 }
 
-func QueryVersionFromControllServer() (ver Info, err error) {
+// QueryVersionFromControlServer handles the HTTP request to gather the allowed and latest version information
+func QueryVersionFromControlServer() (ver Info, err error) {
 	resp, err := http.Get("https://version.storj.io/")
 	if err != nil {
 		return Info{}, err
