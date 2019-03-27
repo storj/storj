330c6b683d08578b2a55c4c88f82e4a0fcdf8eb7
stefanbenten <mail@stefan-benten.de>
2019-03-27 01:09:41 +0100

    Add Configuration File Option Add Systemd Service file

--- cmd/version-control/main.go
+++ cmd/version-control/main.go
@@ -6,12 +6,19 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"storj.io/storj/internal/version"
 )
+
+type runConfig struct {
+	Listen   string `json:"listen"`
+	Versions string `json:"versions"`
+}
 
 var (
 	ver      []version.Info
@@ -38,16 +45,44 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var conf runConfig
+
 	// Flags to specify required Version
 	addr := flag.String("listen", "0.0.0.0:8080", "Defines Listen Address of Webserver")
-	versions := flag.String("version", "v0.1.0,v0.1.1", "Comma separated list of Versions")
+	fconfig := flag.String("config-file", "", "Specifies a config file to read Versions from")
+	fversions := flag.String("version", "v0.1.0,v0.1.1", "Comma separated list of Versions")
+
 	flag.Parse()
 
 	if !flag.Parsed() {
 		log.Fatal("Error while parsing flags")
 	}
 
-	subVersions := strings.Split(*versions, ",")
+	// Check for existence of Versions to use, prefer flag, fall back to config
+	if *fconfig == "" && *fversions == "" {
+		log.Fatal("No Versions specified, use either config file or flags to set them")
+	}
+
+	// If using config flag ensure to only accept it as flag
+	if *fconfig != "" {
+		file, err := os.Open(*fconfig)
+		if err != nil {
+			log.Fatalf("Could not open configuation file: %s", *fconfig)
+		}
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("Could not read configuation file: %s", *fconfig)
+		}
+		err = json.Unmarshal(bytes, &conf)
+		if err != nil {
+			log.Fatalf("Could not parse configuation file: %s %v", *fconfig, err)
+		}
+	} else {
+		conf.Listen = *addr
+		conf.Versions = *fversions
+	}
+
+	subVersions := strings.Split(conf.Versions, ",")
 	for _, subVersion := range subVersions {
 		instance := version.Info{
 			Version: subVersion,
@@ -64,8 +99,8 @@ func main() {
 
 	log.Printf("setting version info to: %v", ver)
 	http.HandleFunc("/", handleGet)
-	log.Println("starting Webserver")
+	log.Printf("starting webserver on %s", conf.Listen)
 
 	// Not pretty but works..
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	log.Fatal(http.ListenAndServe(conf.Listen, nil))
 }
