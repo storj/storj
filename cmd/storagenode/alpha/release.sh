8872267e7f65a83c3dbb7f0b4a0bdcea5c698720
Dennis Coyle <dennis@coyle.io>
2019-04-04 16:31:45 -0400

    adding docker things

--- cmd/storagenode/alpha/release.sh
+++ cmd/storagenode/alpha/release.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
- set -eu
+set -eu
 set -o pipefail
 
- echo -n "Build timestamp: "
+echo -n "Build timestamp: "
 TIMESTAMP=$(date +%s)
 echo $TIMESTAMP
 
- echo -n "Git commit: "
+echo -n "Git commit: "
 if [[ "$(git diff --stat)" != '' ]] || [[ -n "$(git status -s)" ]]; then
   COMMIT=$(git rev-parse HEAD)-dirty
   RELEASE=false
@@ -17,11 +17,11 @@ else
 fi
 echo $COMMIT
 
- echo -n "Tagged version: "
+echo -n "Tagged version: "
 VERSION=$(git describe --tags --exact-match --match "v[0-9]*.[0-9]*.[0-9]*")
 echo $VERSION
 
- echo Running "go $@"
+echo Running "go $@"
 exec go "$1" -ldflags \
 	"-X storj.io/storj/internal/version.buildTimestamp=$TIMESTAMP
          -X storj.io/storj/internal/version.buildCommitHash=$COMMIT
