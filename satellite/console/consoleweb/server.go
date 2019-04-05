d1270af7ec091391d9b5dab35484e19d2d407671
Bohdan Artemenko <artemenkobogdan@gmail.com>
2019-04-05 19:42:36 +0300

    Comments removed.

--- satellite/console/consoleweb/server.go
+++ satellite/console/consoleweb/server.go
@@ -6,6 +6,7 @@ package consoleweb
 import (
 	"context"
 	"encoding/json"
+	"html/template"
 	"net"
 	"net/http"
 	"path/filepath"
@@ -186,16 +187,15 @@ func (s *Server) passwordRecoveryHandler(w http.ResponseWriter, req *http.Reques
 			s.serveError(w, req)
 		}
 	default:
-		http.ServeFile(w, req, filepath.Join(s.config.StaticDir, "static", "resetPassword", "resetPassword.html"))
-		//t, err := template.ParseFiles(filepath.Join(s.config.StaticDir, "static", "resetPassword", "resetPassword.html"))
-		//if err != nil {
-		//	s.serveError(w, req)
-		//}
-		//
-		//err = t.Execute(w, nil)
-		//if err != nil {
-		//	s.serveError(w, req)
-		//}
+		t, err := template.ParseFiles(filepath.Join(s.config.StaticDir, "static", "resetPassword", "resetPassword.html"))
+		if err != nil {
+			s.serveError(w, req)
+		}
+
+		err = t.Execute(w, nil)
+		if err != nil {
+			s.serveError(w, req)
+		}
 	}
 }
 
