03996733a68541edef7db2bf92b2740827e9906b
Bogdan Artemenko <artemenkobogdan@gmail.com>
2019-04-10 22:16:10 +0300

    Implemented password reset on satellite console web. (#1665)
    (cherry picked from commit 6a50b187eb1abfb39c92142b9e23c072cc1bf699)

--- web/satellite/src/views/forgotPassword/ForgotPassword.vue
+++ web/satellite/src/views/forgotPassword/ForgotPassword.vue
@@ -7,17 +7,37 @@
     import { Component, Vue } from 'vue-property-decorator';
     import HeaderlessInput from '../../components/common/HeaderlessInput.vue';
     import { LOADING_CLASSES } from '@/utils/constants/classConstants';
+    import { forgotPasswordRequest } from '@/api/users';
+    import { NOTIFICATION_ACTIONS } from '@/utils/constants/actionNames';
 
     @Component(
         {
             data: function () {
                 return {
                     loadingClassName: LOADING_CLASSES.LOADING_OVERLAY,
+                    email: '',
                 };
             },
             components: {
                 HeaderlessInput,
             },
+            methods: {
+                setEmail: function (value: string): void {
+                    this.$data.email = value;
+                },
+                onSendConfigurations: async function (): Promise<any> {
+                    if (!this.$data.email) {
+                        return;
+                    }
+
+                    let passwordRecoveryResponse = await forgotPasswordRequest(this.$data.email);
+                    if (passwordRecoveryResponse.isSuccess) {
+                        this.$store.dispatch(NOTIFICATION_ACTIONS.SUCCESS, 'Please look for instructions at your email');
+                    } else {
+                        this.$store.dispatch(NOTIFICATION_ACTIONS.ERROR, passwordRecoveryResponse.errorMessage);
+                    }
+                },
+            }
         })
 
     export default class ForgotPassword extends Vue {
