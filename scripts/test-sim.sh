b99a86d3e24f2208721439f5c68970149ec18528
Egon Elbre <egonelbre@gmail.com>
2019-04-10 19:21:33 -0400

    add vars for host

--- scripts/test-sim.sh
+++ scripts/test-sim.sh
@@ -15,18 +15,21 @@ trap cleanup EXIT
 
 export STORJ_NETWORK_DIR=$TMP
 
+export HOST="127.0.0.1"
+export HOST6="::1"
+
 # setup the network
-storj-sim -x network setup
+storj-sim -x --host $HOST network setup
 
 # run aws-cli tests
-storj-sim -x network test bash "$SCRIPTDIR"/test-sim-aws.sh
-storj-sim -x network test bash "$SCRIPTDIR"/test-uplink.sh
-storj-sim -x network destroy
+storj-sim -x --host $HOST network test bash "$SCRIPTDIR"/test-sim-aws.sh
+storj-sim -x --host $HOST network test bash "$SCRIPTDIR"/test-uplink.sh
+storj-sim -x --host network destroy
 
 # setup the network with ipv6
-storj-sim -x --host "::1" network setup
+storj-sim -x --host $HOST6 network setup
 # aws-cli doesn't support gateway with ipv6 address, so change it to use localhost
 find "$STORJ_NETWORK_DIR"/gateway -type f -name config.yaml -exec sed -i 's/server.address: "\[::1\]/server.address: "127.0.0.1/' '{}' +
 # run aws-cli tests using ipv6
-storj-sim -x --host "::1" network test bash "$SCRIPTDIR"/test-sim-aws.sh
+storj-sim -x --host $HOST6 network test bash "$SCRIPTDIR"/test-sim-aws.sh
 storj-sim -x network destroy
