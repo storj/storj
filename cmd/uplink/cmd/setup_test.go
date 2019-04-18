f44a3fd9739e777d5c301458a8bbcf479c75b27a
Ivan Fraixedes <ivan@fraixed.es>
2019-04-18 16:17:11 +0200

    cmd/uplink: save root enc key into separated file
    Save the root encryption key used for encrypting the data (a.k.a
    passphrase) into a separated file instead of saving it into the
    configuration file.
    
    For avoiding to break the current API of uplink package, the uplink
    configuration file saves the file path to the file which contains the
    encryption key.

--- cmd/uplink/cmd/setup_test.go
+++ cmd/uplink/cmd/setup_test.go
@@ -4,10 +4,15 @@
 package cmd_test
 
 import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"storj.io/storj/cmd/uplink/cmd"
 )
 
@@ -114,4 +119,60 @@ func TestDefaultPortAppliedToSatelliteAddrWithPortColonButNoPort(t *testing.T) {
 
 	assert.Equal(t, "ahost:7777", setupCmd.Flags().Lookup("satellite-addr").Value.String(),
 		"satellite-addr should contain default port when no port specified")
+}
+
+func TestSaveEncriptionKey(t *testing.T) {
+	expKey := make([]byte, rand.Intn(20)+1)
+	_, err := rand.Read(expKey)
+	require.NoError(t, err)
+
+	t.Run("ok", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "storj-test-cmd-uplink")
+		require.NoError(t, err)
+		defer func() { _ = os.RemoveAll(dir) }()
+
+		fname := filepath.Join(dir, ".enc.key")
+		err = cmd.SaveEncryptionKey(expKey, fname)
+		require.NoError(t, err)
+
+		// Read the key from the file to compare that it matches with the saved one.
+		f, err := os.Open(fname)
+		require.NoError(t, err)
+		defer func() { _ = f.Close() }()
+
+		key := make([]byte, len(expKey))
+		_, err = f.Read(key)
+		require.NoError(t, err)
+		assert.Equal(t, expKey, key)
+
+		// Check that the key file has a read only file permissions for the user
+		fi, err := f.Stat()
+		require.NoError(t, err)
+		assert.Equal(t, os.FileMode(0400), fi.Mode().Perm())
+	})
+
+	t.Run("error: unexisting dir", func(t *testing.T) {
+		// Create a directory and remove it for making sure that the path doesn't
+		// exist
+		dir, err := ioutil.TempDir("", "storj-test-cmd-uplink")
+		require.NoError(t, err)
+		err = os.RemoveAll(dir)
+		require.NoError(t, err)
+
+		fname := filepath.Join(dir, "enc.key")
+		err = cmd.SaveEncryptionKey(expKey, fname)
+		assert.Errorf(t, err, "directory path doesn't exist")
+	})
+
+	t.Run("error: file already exists", func(t *testing.T) {
+		// Create an empty file
+		f, err := ioutil.TempFile("", "storj-test-cmd-uplink-key-*")
+		require.NoError(t, err)
+		err = f.Close()
+		require.NoError(t, err)
+		defer func() { _ = os.Remove(f.Name()) }()
+
+		err = cmd.SaveEncryptionKey(expKey, f.Name())
+		assert.Errorf(t, err, "file key already exists")
+	})
 }
