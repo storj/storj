cf6e1b11343e1b1fab2c4d7925bde306f654bfc9
Stefan Benten <mail@stefan-benten.de>
2019-04-18 22:22:47 +0200

    Set absolute path for debugging

--- scripts/test-sim-aws.sh
+++ scripts/test-sim-aws.sh
@@ -12,9 +12,9 @@ SRC_DIR=$TMPDIR/source
 DST_DIR=$TMPDIR/dst
 mkdir -p "$SRC_DIR" "$DST_DIR"
 
-aws configure set aws_access_key_id     "$GATEWAY_0_ACCESS_KEY"
-aws configure set aws_secret_access_key "$GATEWAY_0_SECRET_KEY"
-aws configure set default.region        us-east-1
+/usr/local/bin/aws configure set aws_access_key_id     "$GATEWAY_0_ACCESS_KEY"
+/usr/local/bin/aws configure set aws_secret_access_key "$GATEWAY_0_SECRET_KEY"
+/usr/local/bin/aws configure set default.region        us-east-1
 
 random_bytes_file () {
 	size=$1
@@ -27,26 +27,26 @@ random_bytes_file 5x1024x1024 "$SRC_DIR/big-upload-testfile"       # create 5mb 
 random_bytes_file      5x1024 "$SRC_DIR/multipart-upload-testfile" # create 5kb file of random bytes (remote)
 
 echo "Creating Bucket"
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" mb s3://bucket
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" mb s3://bucket
 
 echo "Uploading Files"
-aws configure set default.s3.multipart_threshold 1TB
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/small-upload-testfile" s3://bucket/small-testfile
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/big-upload-testfile"   s3://bucket/big-testfile
+/usr/local/bin/aws configure set default.s3.multipart_threshold 1TB
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/small-upload-testfile" s3://bucket/small-testfile
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/big-upload-testfile"   s3://bucket/big-testfile
 
 # Wait 5 seconds to trigger any error related to one of the different intervals
 sleep 5
 
 echo "Uploading Multipart File"
-aws configure set default.s3.multipart_threshold 4KB
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/multipart-upload-testfile" s3://bucket/multipart-testfile
+/usr/local/bin/aws configure set default.s3.multipart_threshold 4KB
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp "$SRC_DIR/multipart-upload-testfile" s3://bucket/multipart-testfile
 
 echo "Downloading Files"
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" ls s3://bucket
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/small-testfile     "$DST_DIR/small-download-testfile"
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/big-testfile       "$DST_DIR/big-download-testfile"
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/multipart-testfile "$DST_DIR/multipart-download-testfile"
-aws s3 --endpoint="http://$GATEWAY_0_ADDR" rb s3://bucket --force
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" ls s3://bucket
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/small-testfile     "$DST_DIR/small-download-testfile"
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/big-testfile       "$DST_DIR/big-download-testfile"
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" cp s3://bucket/multipart-testfile "$DST_DIR/multipart-download-testfile"
+/usr/local/bin/aws s3 --endpoint="http://$GATEWAY_0_ADDR" rb s3://bucket --force
 
 if cmp "$SRC_DIR/small-upload-testfile" "$DST_DIR/small-download-testfile"
 then
