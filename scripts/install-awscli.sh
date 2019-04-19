fd2e3a53ea91e904e5b74206f9ba3a996be91a95
stefanbenten <mail@stefan-benten.de>
2019-04-19 20:51:19 +0200

    Add aws to PATH

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -7,6 +7,9 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 
+# Ensure the binary is in path
+export PATH=~/bin:$PATH
+
 popd
 
 set +x
