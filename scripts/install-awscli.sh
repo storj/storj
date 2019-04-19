c1e267c6bbbdda76b1c8237b99b298cb8699bd7f
stefanbenten <mail@stefan-benten.de>
2019-04-19 21:38:36 +0200

    Debugging

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -6,6 +6,8 @@ pushd $HOME/awscli
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
+cd ~/bin
+echo pwd
 
 popd
 
