57ff3d715b86a901d8fb49dc8594735cb81dbb7c
Egon Elbre <egonelbre@gmail.com>
2019-04-23 12:11:22 +0300

    cherry pick group implementation from #1764

---
+++ internal/errs2/group.go
@@ -0,0 +1,39 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package errs2
+
+import "sync"
+
+// Group is a collection of goroutines working on subtasks that are part of
+// the same overall task.
+type Group struct {
+	wg     sync.WaitGroup
+	errors []error
+
+	mutex sync.Mutex
+}
+
+// Go calls the given function in a new goroutine.
+func (group *Group) Go(f func() error) {
+	group.wg.Add(1)
+
+	go func() {
+		defer group.wg.Done()
+
+		if err := f(); err != nil {
+			group.mutex.Lock()
+			defer group.mutex.Unlock()
+
+			group.errors = append(group.errors, err)
+		}
+	}()
+}
+
+// Wait blocks until all function calls from the Go method have returned, then
+// returns all errors (if any) from them.
+func (group *Group) Wait() []error {
+	group.wg.Wait()
+
+	return group.errors
+}
