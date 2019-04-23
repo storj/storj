b76af6c81fad5f02b0046df00148e4e2e7f321e2
wthorp <bill3000@hotmail.com>
2019-04-23 11:07:59 -0400

    new defaults

--- pkg/overlay/config.go
+++ pkg/overlay/config.go
@@ -34,11 +34,11 @@ type LookupConfig struct {
 // NodeSelectionConfig is a configuration struct to determine the minimum
 // values for nodes to select
 type NodeSelectionConfig struct {
-	UptimeRatio           float64       `help:"a node's ratio of being up/online vs. down/offline" default:"0"`
-	UptimeCount           int64         `help:"the number of times a node's uptime has been checked" default:"0"`
+	UptimeRatio           float64       `help:"a node's ratio of being up/online vs. down/offline" releaseDefault:"0.9" devDefault:"0"`
+	UptimeCount           int64         `help:"the number of times a node's uptime has been checked" releaseDefault:"500" devDefault:"0"`
 	AuditSuccessRatio     float64       `help:"a node's ratio of successful audits" releaseDefault:"0.4" devDefault:"0"`
-	AuditCount            int64         `help:"the number of times a node has been audited" releaseDefault:"500" devDefault:"0"`
-	NewNodeAuditThreshold int64         `help:"the number of audits a node must have to not be considered a New Node" default:"0"`
+	AuditCount            int64         `help:"the number of times a node has been audited" releaseDefault:"0" devDefault:"0"`
+	NewNodeAuditThreshold int64         `help:"the number of audits a node must have to not be considered a New Node" releaseDefault:"0.4" devDefault:"0"`
 	NewNodePercentage     float64       `help:"the percentage of new nodes allowed per request" default:"0.05"` // TODO: fix, this is not percentage, it's ratio
 	MinimumVersion        string        `help:"the minimum node software version for node selection queries" default:""`
 	OnlineWindow          time.Duration `help:"the amount of time without seeing a node before its considered offline" default:"1h"` //todo:  wire up instead of constants
