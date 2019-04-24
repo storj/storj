37ff0e789dd12b9b2d978982d6fcd2d0a8b9e182
Ivan Fraixedes <ivan@fraixed.es>
2019-04-24 20:48:25 +0200

    REVIEW: test fails, rename some vars & use testcontext
    As commented and discussed in the review:
    
    * make the test to fail when cleaning up the resources crated by the
      same test.
    * use testcontext package when useful.
    * Rename some var with abbreviated words when the scope is not small
      (>= 40 lines).

--- uplink/config_test.go
+++ uplink/config_test.go
@@ -17,31 +17,33 @@ import (
 
 func TestEncryptionConfig_Key(t *testing.T) {
 	saveKey := func(key []byte) (filepath string, removeFile func()) {
-		f, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
-		require.NoError(t, err)
-		defer func() { _ = f.Close() }()
+		t.Helper()
 
-		_, err = f.Write(key)
+		file, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, file.Close()) }()
+
+		_, err = file.Write(key)
 		require.NoError(t, err)
 
-		err = f.Chmod(os.FileMode(0400))
+		err = file.Chmod(os.FileMode(0400))
 		require.NoError(t, err)
 
-		return f.Name(), func() { _ = os.Remove(f.Name()) }
+		return file.Name(), func() { require.NoError(t, os.Remove(file.Name())) }
 	}
 
 	t.Run("ok: file with key length less or equal than max size", func(t *testing.T) {
 		someKey := make([]byte, rand.Intn(20)+1)
 		_, err := rand.Read(someKey)
 		require.NoError(t, err)
-		fpath, cleanup := saveKey(someKey)
+		filename, cleanup := saveKey(someKey)
 		defer cleanup()
 
 		var expKey storj.Key
 		copy(expKey[:], someKey)
 
 		encCfg := &EncryptionConfig{
-			KeyFilepath: fpath,
+			KeyFilepath: filename,
 		}
 		key, err := encCfg.Key()
 		require.NoError(t, err)
@@ -53,11 +55,11 @@ func TestEncryptionConfig_Key(t *testing.T) {
 		expKey := make([]byte, rand.Intn(10)+1+storj.KeySize)
 		_, err := rand.Read(expKey)
 		require.NoError(t, err)
-		fpath, cleanup := saveKey(expKey)
+		filename, cleanup := saveKey(expKey)
 		defer cleanup()
 
 		encCfg := &EncryptionConfig{
-			KeyFilepath: fpath,
+			KeyFilepath: filename,
 		}
 		key, err := encCfg.Key()
 		require.NoError(t, err)
@@ -66,11 +68,11 @@ func TestEncryptionConfig_Key(t *testing.T) {
 	})
 
 	t.Run("ok: empty file path", func(t *testing.T) {
-		fpath, cleanup := saveKey([]byte{})
+		filename, cleanup := saveKey([]byte{})
 		defer cleanup()
 
 		encCfg := &EncryptionConfig{
-			KeyFilepath: fpath,
+			KeyFilepath: filename,
 		}
 		key, err := encCfg.Key()
 		require.NoError(t, err)
@@ -79,41 +81,42 @@ func TestEncryptionConfig_Key(t *testing.T) {
 
 	t.Run("error: file not found", func(t *testing.T) {
 		// Create a temp file and delete it, to get a filepath which doesn't exist.
-		f, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
+		file, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
 		require.NoError(t, err)
-		err = f.Close()
+		err = file.Close()
 		require.NoError(t, err)
-		err = os.Remove(f.Name())
+		err = os.Remove(file.Name())
 		require.NoError(t, err)
 
 		encCfg := &EncryptionConfig{
-			KeyFilepath: f.Name(),
+			KeyFilepath: file.Name(),
 		}
 		_, err = encCfg.Key()
 		require.Error(t, err)
-		assert.Contains(t, err.Error(), fmt.Sprintf("not found key file %q", f.Name()))
+		assert.Contains(t, err.Error(), fmt.Sprintf("not found key file %q", file.Name()))
 		assert.True(t, Error.Has(err), "err is not of %q class", Error)
 	})
 
 	t.Run("error: permissions are too open", func(t *testing.T) {
 		// Create a key file and change its permission for not being able to read it
-		f, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
+		file, err := ioutil.TempFile("", "storj-test-uplink-keyfilepath-*")
 		require.NoError(t, err)
 		defer func() {
-			_ = f.Close()
-			_ = os.Remove(f.Name())
+			require.NoError(t, file.Close())
+			require.NoError(t, os.Remove(file.Name()))
 		}()
 
-		err = f.Chmod(0401)
+		err = file.Chmod(0401)
 		require.NoError(t, err)
 
 		encCfg := &EncryptionConfig{
-			KeyFilepath: f.Name(),
+			KeyFilepath: file.Name(),
 		}
 		_, err = encCfg.Key()
 		require.Error(t, err)
 		assert.Contains(t,
-			err.Error(), fmt.Sprintf("permissions '0401' for key file %q are too open", f.Name()),
+			err.Error(),
+			fmt.Sprintf("permissions '0401' for key file %q are too open", file.Name()),
 		)
 		assert.True(t, Error.Has(err), "err is not of %q class", Error)
 	})
