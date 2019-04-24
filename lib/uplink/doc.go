551d893fc7fccb966b45749e6b7645486637cf52
Kishore <kishore@storj.io>
2019-04-23 22:15:52 -0400

    updated code review comments

--- lib/uplink/doc.go
+++ lib/uplink/doc.go
@@ -4,23 +4,5 @@
 /*
 Package uplink provides variety of functions to access the objects using storj's
 uplink library
-
-The following functionalities are supported:
-
-Bucket functionality
-
-CreateBucket  - creates a new bucket if authorized
-DeleteBucket  - deletes a bucket if authorized
-ListBuckets   - list authorized buckets
-GetBucketInfo - returns info about the requested bucket if authorized
-OpenBucket    - returns a Bucket handle with the given EncryptionAccess
-
-Object functionality
-
-OpenObject    - returns an Object handle, if authorized
-UploadObject  - uploads a new object, if authorized
-DeleteObject  - removes an object, if authorized
-ListObjects   - lists objects a user is authorized to see
-
 */
 package uplink
