6d15d4b3dbb2f46378261a6be198eb6ac15ca22a
Alexander Leitner <leitnersalex@gmail.com>
2019-04-26 12:07:01 -0400

    Syntax

--- lib/libuplinkc_ByCStructs/structs.h
+++ lib/libuplinkc_ByCStructs/structs.h
@@ -1,23 +1,25 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
+#include <stdbool.h>
+
 struct APIKey {
     const char *key;
 };
 
 struct TLS {
-    bool SkipPeerCAWhitelist
-    char* PeerCAWhitelistPath
-}
+    bool SkipPeerCAWhitelist;
+    char* PeerCAWhitelistPath;
+};
 
 struct Volatile {
-    struct TLS tls
-    uint8_t IdentityVersion
-    uint32_t* PeerIDVersion
-    uint64_t MaxInlineSize
-    uint64_t MaxMemory
-}
+    struct TLS tls;
+    uint8_t IdentityVersion;
+    uint32_t* PeerIDVersion;
+    uint64_t MaxInlineSize;
+    uint64_t MaxMemory;
+};
 
 struct Config {
-    struct Volatile volatile
-}
+    struct Volatile volatile;
+};
