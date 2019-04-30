499600952fc26225d0a4e674ffd92626ab8f8abd
Egon Elbre <egonelbre@gmail.com>
2019-04-30 19:49:28 +0300

    dev

--- satellite/satellitedb/metainfo_objects.go
+++ satellite/satellitedb/metainfo_objects.go
@@ -27,6 +27,7 @@ type segments struct {
 func (objects *objects) Create(ctx context.Context, object *metainfo.Object) error {
 	return nil
 }
+
 func (objects *objects) Commit(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) (*metainfo.Object, error) {
 	return nil, nil
 }
@@ -34,9 +35,11 @@ func (objects *objects) Commit(ctx context.Context, bucket uuid.UUID, encryptedP
 func (objects *objects) Get(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) (*metainfo.Object, error) {
 	return nil, nil
 }
+
 func (objects *objects) List(ctx context.Context, bucket uuid.UUID, opts metainfo.ListOptions) (metainfo.ObjectList, error) {
 	return metainfo.ObjectList{}, nil
 }
+
 func (objects *objects) Delete(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) error {
 	return nil
 }
@@ -44,6 +47,7 @@ func (objects *objects) Delete(ctx context.Context, bucket uuid.UUID, encryptedP
 func (objects *objects) ListPartial(ctx context.Context, bucket uuid.UUID, opts metainfo.ListOptions) (metainfo.ObjectList, error) {
 	return metainfo.ObjectList{}, nil
 }
+
 func (objects *objects) DeletePartial(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) error {
 	return nil
 }
@@ -51,12 +55,15 @@ func (objects *objects) DeletePartial(ctx context.Context, bucket uuid.UUID, enc
 func (segments *segments) Create(ctx context.Context, segment *metainfo.Segment) error {
 	return nil
 }
+
 func (segments *segments) Commit(ctx context.Context, segment *metainfo.Segment) error {
 	return nil
 }
+
 func (segments *segments) Delete(ctx context.Context, streamID uuid.UUID, segmentIndex int64) error {
 	return nil
 }
+
 func (segments *segments) List(ctx context.Context, streamID uuid.UUID, segmentIndex int64, limit int64) ([]*metainfo.Segment, error) {
 	return nil, nil
 }
