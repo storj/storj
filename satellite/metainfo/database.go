d33664ba744f937170d983e0fcb92bddcb960482
Egon Elbre <egonelbre@gmail.com>
2019-04-30 18:07:05 +0300

    add interfaces

--- satellite/metainfo/database.go
+++ satellite/metainfo/database.go
@@ -75,3 +75,73 @@ type Buckets interface {
 	Delete(ctx context.Context, projectID uuid.UUID, name string) error
 	List(ctx context.Context, projectID uuid.UUID, opts BucketListOptions) (BucketList, error)
 }
+
+type ObjectStatus byte
+
+const (
+	Partial ObjectStatus = iota
+	Committing
+	Committed
+	Deleting
+)
+
+type Object struct {
+	BucketID      uuid.UUID
+	EncryptedPath []byte
+	Version       int64
+	Status        ObjectStatus
+
+	StreamID uuid.UUID
+
+	EncryptedMetadataNonce []byte
+	EncryptedMetadata      []byte
+
+	TotalSize  int64
+	InlineSize int64
+	RemoteSize int64
+
+	CreatedAt time.Time
+	ExpiresAt time.Time
+
+	FixedSegmentSize int64
+	Redundancy       storj.RedundancyScheme
+	Encryption       storj.EncryptionParameters
+}
+
+// Objects interface for managing objects
+type Objects interface {
+	Get(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) (*Object, error)
+	List(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) ([]*Object, error)
+	Delete(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) error
+
+	GetPartial(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) (*Object, error)
+	ListPartial(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) ([]*Object, error)
+	DeletePartial(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) error
+
+	Create(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32, object *Object) error
+	Commit(ctx context.Context, bucket uuid.UUID, encryptedPath storj.Path, version uint32) (*Object, error)
+}
+
+type Segment struct {
+	StreamID     uuid.UUID
+	SegmentIndex uint64
+
+	RootPieceID storj.PieceID
+
+	EncryptedKeyNonce storj.Nonce
+	EncryptedKey      storj.EncryptedPrivateKey
+
+	SegmentChecksum uint64
+	SegmentSize     int64
+
+	EncryptedInlineData []byte
+	Nodes               []storj.NodeID
+}
+
+// Segments interface for managing segments
+type Segments interface {
+	Create(ctx context.Context, segment *Segment) error
+	Commit(ctx context.Context, segment *Segment) error
+	Delete(ctx context.Context, streamID uuid.UUID, segmentIndex int64) error
+	List(ctx context.Context, streamID uuid.UUID, segmentIndex int64, limit int64) ([]*Segment, error)
+}
