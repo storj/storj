a4fd19d3d029a99cbdfda91744a814ae3fb5f637
Egon Elbre <egonelbre@gmail.com>
2019-05-08 11:08:38 +0300

    add package documentation

--- storagenode/collector/service.go
+++ storagenode/collector/service.go
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
+// Package collector implements expired piece deletion from storage node.
 package collector
 
 import (
