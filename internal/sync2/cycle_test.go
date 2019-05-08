c7533de090b9145e836c68299e9a83216b138b82
Egon Elbre <egonelbre@gmail.com>
2019-05-08 12:00:51 +0300

    add test for multiple stops

--- internal/sync2/cycle_test.go
+++ internal/sync2/cycle_test.go
@@ -87,3 +87,23 @@ func TestCycle_Basic(t *testing.T) {
 		})
 	}
 }
+
+func TestCycle_MultipleStops(t *testing.T) {
+	t.Parallel()
+
+	cycle := sync2.NewCycle(time.Second)
+	defer cycle.Close()
+
+	ctx := context.Background()
+
+	var group errgroup.Group
+	var count int64
+	cycle.Start(ctx, &group, func(ctx context.Context) error {
+		atomic.AddInt64(&count, 1)
+		return nil
+	})
+
+	go cycle.Stop()
+	cycle.Stop()
+	cycle.Stop()
+}
