61446ea62d749cc548b12629f0a66bbe777ef422
Bill Thorp <billt@storj.io>
2019-05-08 06:53:09 -0400

    trying different if

--- pkg/process/exec_conf.go
+++ pkg/process/exec_conf.go
@@ -104,7 +104,7 @@ func saveConfig(flagset *pflag.FlagSet, outfile string, overrides map[string]int
 			fmt.Fprintf(w, "# %s\n", f.Usage)
 		}
 		//print commented key (beginning of value assignement line)
-		if f.DefValue != value {
+		if readBoolAnnotation(f, "user") || f.Changed || overrideExist {
 			fmt.Fprintf(w, "%s: ", k)
 		} else {
 			fmt.Fprintf(w, "# %s: ", k)
