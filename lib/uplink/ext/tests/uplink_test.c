94498fc41f25903b38223e35c03178fcf804eca0
Bryan White <bryanchriswhite@gmail.com>
2019-05-10 13:34:12 +0200

    testplanet integrated into sanity test

--- lib/uplink/ext/tests/uplink_test.c
+++ lib/uplink/ext/tests/uplink_test.c
@@ -11,40 +11,45 @@
 void TestNewUplink_config(void)
 {
     uint8_t idVersionNumber = 0;
+    const struct IDVersion idVersion = {0, 8};
+    struct Config testUplinkConfig = {
+        {{true, "/whitelist.pem"},
+         idVersion,
+         "latest",
+         1,
+         2}};
     char *_err = "";
     char **err = &_err;
 
     // NB: ensure we get a valid ID version
-    IDVersion version = GetIDVersion(idVersionNumber, err);
+    struct IDVersion version = GetIDVersion(idVersionNumber, err);
     TEST_ASSERT_EQUAL_STRING("", *err);
-
-    struct Config testUplinkConfig = {
-        {{true, "/whitelist.pem"},
-         version,
-         "latest",
-         1,
-         2}};
+    TEST_ASSERT_NOT_EQUAL(0, version.GoIDVersion);
 
     testUplinkConfig.Volatile.IdentityVersion = version;
     TEST_ASSERT_EQUAL_STRING("", *err);
+    TEST_ASSERT_EQUAL_UINT8(idVersionNumber, testUplinkConfig.Volatile.IdentityVersion.Number);
 
     struct Uplink uplink = NewUplink(testUplinkConfig, err);
     TEST_ASSERT_EQUAL_STRING("", *err);
     TEST_ASSERT_NOT_EQUAL(0, uplink.GoUplink);
     TEST_ASSERT_TRUE(uplink.Config.Volatile.TLS.SkipPeerCAWhitelist);
+    TEST_ASSERT_EQUAL_UINT8(idVersionNumber, uplink.Config.Volatile.IdentityVersion.Number);
+    TEST_ASSERT_NOT_EQUAL(0, uplink.Config.Volatile.IdentityVersion.GoIDVersion);
 }
 
 struct Uplink *NewTestUplink(char **err)
 {
     uint8_t idVersionNumber = 0;
-    IDVersion version = GetIDVersion(idVersionNumber, err);
-
+    const struct IDVersion idVersion = {0, 8};
     struct Config testUplinkConfig = {
         {{true, "/whitelist.pem"},
-         version,
+         idVersion,
          "latest",
          1,
          2}};
+    struct IDVersion version = GetIDVersion(idVersionNumber, err);
+    testUplinkConfig.Volatile.IdentityVersion = version;
 
     struct Uplink *uplink = malloc(sizeof(struct Uplink));
     *uplink = NewUplink(testUplinkConfig, err);
@@ -53,39 +58,22 @@ struct Uplink *NewTestUplink(char **err)
 
 void TestOpenProject(void)
 {
-    // char *_err = "";
-    // char **err = &_err;
-    // uint8_t idVersionNumber = 0;
-    // struct IDVersion idVersion = {0, 0};
-    // struct Config uplinkConfig = {
-    //     {{true, "/whitelist.pem"},
-    //      idVersion,
-    //      "latest",
-    //      1,
-    //      2}};
-    // char *satelliteAddr = getenv("SATELLITE");
-    // APIKey apiKey = ParseAPIKey("testapikey", err);
-    // uint8_t encryptionKey[32];
-    // struct ProjectOptions opts = {
-    //     {&encryptionKey}};
+    char *_err = "";
+    char **err = &_err;
+    char *satelliteAddr = getenv("SATELLITEADDR");
+    APIKey apiKey = ParseAPIKey("testapikey", err);
+    uint8_t encryptionKey[32];
 
-    // // NB: ensure we get a valid ID version
-    // idVersion = GetIDVersion(idVersionNumber, err);
-    // TEST_ASSERT_EQUAL_STRING("", *err);
-    // TEST_ASSERT_NOT_EQUAL(0, idVersion.GoIDVersion);
+    struct ProjectOptions opts = {
+        {&encryptionKey}
+    };
 
-    // uplinkConfig.Volatile.IdentityVersion = idVersion;
-    // TEST_ASSERT_EQUAL_STRING("", *err);
-    // TEST_ASSERT_EQUAL_UINT8(idVersionNumber, uplinkConfig.Volatile.IdentityVersion.Number);
+    struct Uplink *uplink = NewTestUplink(err);
+    TEST_ASSERT_EQUAL_STRING("", *err);
+    TEST_ASSERT_NOT_NULL(uplink);
 
-    // GoCtxPtr ctx = GetContext();
-
-    // struct Uplink *uplink = NewTestUplink(ctx, err);
-    // TEST_ASSERT_EQUAL_STRING("", *err);
-    // TEST_ASSERT_NOT_NULL(uplink);
-
-    // OpenProject(*uplink, satelliteAddr, apiKey, opts, err);
-    // TEST_ASSERT_EQUAL_STRING("", *err);
+    OpenProject(*uplink, satelliteAddr, apiKey, opts, err);
+    TEST_ASSERT_EQUAL_STRING("", *err);
 }
 
 void TestCreateBucket(void)
