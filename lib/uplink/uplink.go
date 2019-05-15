3d33228c3798440425abe33272de6c1c43b748dd
JT Olio <hello@jtolio.com>
2019-05-15 11:02:21 -0600

    need to remove this
    Change-Id: I04abc13e31cbc6f3bc190faba76d3cd069e76879

--- lib/uplink/uplink.go
+++ lib/uplink/uplink.go
@@ -133,9 +133,6 @@ func NewUplink(ctx context.Context, cfg *Config) (_ *Uplink, err error) {
 
 // ProjectOptions allows configuration of various project options during opening
 type ProjectOptions struct {
-	Volatile struct {
-		EncryptionKey *storj.Key
-	}
 }
 
 // OpenProject returns a Project handle with the given APIKey
@@ -158,18 +155,11 @@ func (u *Uplink) OpenProject(ctx context.Context, satelliteAddr string, apiKey A
 		return nil, Error.New("failed to create redundancy strategy: %v", err)
 	}
 	segments := segments.NewSegmentStore(metainfo, nil, rs, maxBucketMetaSize.Int(), maxBucketMetaSize.Int64())
-	var encryptionKey *storj.Key
-	if opts != nil {
-		encryptionKey = opts.Volatile.EncryptionKey
-	}
-	if encryptionKey == nil {
-		// volatile warning: we're setting an encryption key of all zeros when one isn't provided.
-		// TODO: fix before the final alpha network wipe
-		encryptionKey = new(storj.Key)
-	}
-	streams, err := streams.NewStreamStore(segments, maxBucketMetaSize.Int64(), encryptionKey,
-		memory.KiB.Int(), storj.AESGCM, maxBucketMetaSize.Int(),
-	)
+	// volatile warning: we're setting an encryption key of all zeros when one isn't provided.
+	// TODO: fix before the final alpha network wipe
+	encryptionKey := new(storj.Key)
+	streams, err := streams.NewStreamStore(segments, maxBucketMetaSize.Int64(),
+		encryptionKey, memory.KiB.Int(), storj.AESGCM)
 	if err != nil {
 		return nil, Error.New("failed to create stream store: %v", err)
 	}
