820241eb50c0a9cbd45a2d9df9719b40b4a12a9b
Ivan Fraixedes <ivan@fraixed.es>
2019-05-16 13:15:31 +0200

    integration-test: set encryption key to uplink
    Because an encryption key is required for executing the cmd/uplink
    operations some scripts and sources which are used for the integration
    test had to be updated to pass the encryption key by parameter rather
    than loading from a file.

--- cmd/storj-sim/network.go
+++ cmd/storj-sim/network.go
@@ -321,8 +321,6 @@ func newNetwork(flags *Flags) (*Processes, error) {
 
 				"--satellite-addr", satellite.Address,
 
-				"--enc.key=TestEncryptionKey",
-
 				"--rs.min-threshold", strconv.Itoa(1 * flags.StorageNodeCount / 5),
 				"--rs.repair-threshold", strconv.Itoa(2 * flags.StorageNodeCount / 5),
 				"--rs.success-threshold", strconv.Itoa(3 * flags.StorageNodeCount / 5),
@@ -333,7 +331,9 @@ func newNetwork(flags *Flags) (*Processes, error) {
 
 				"--debug.addr", net.JoinHostPort(host, port(gatewayPeer, i, debugHTTP)),
 			},
-			"run": {},
+			"run": {
+				"--enc.encryption-key=TestEncryptionKey",
+			},
 		})
 
 		process.ExecBefore["run"] = func(process *Process) error {
