edfe8e1664d54aceb9745858b0b64369d35d4eef
Natalie Ventura Villasana <nat@storj.io>
2019-05-16 12:52:21 -0400

    adds tests for Get, IncrementPending, and Delete

--- pkg/audit/containment_test.go
+++ pkg/audit/containment_test.go
@@ -4,23 +4,43 @@
 package audit_test
 
 import (
+	"crypto/rand"
 	"testing"
 
+	"storj.io/storj/pkg/pkcrypto"
+	"storj.io/storj/pkg/storj"
+
+	"github.com/stretchr/testify/require"
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testplanet"
+	"storj.io/storj/pkg/audit"
 )
 
-func TestContainGet(t *testing.T) {
+func TestContainIncrementAndGet(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 4,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
-	})
-}
 
-func TestContainIncrementPendingFirstEntry(t *testing.T) {
-	testplanet.Run(t, testplanet.Config{
-		SatelliteCount: 1, StorageNodeCount: 4,
-	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		randBytes := make([]byte, 10)
+		rand.Read(randBytes)
+		someHash := pkcrypto.SHA256Hash(randBytes)
+
+		input := &audit.PendingAudit{
+			NodeID:            planet.StorageNodes[0].ID(),
+			PieceID:           storj.PieceID{},
+			StripeIndex:       0,
+			ShareSize:         0,
+			ExpectedShareHash: someHash,
+			ReverifyCount:     0,
+		}
+
+		err := planet.Satellites[0].DB.Containment().IncrementPending(ctx, input)
+		require.NoError(t, err)
+
+		output, err := planet.Satellites[0].DB.Containment().Get(ctx, input.NodeID)
+		require.NoError(t, err)
+
+		require.Equal(t, input, output)
 	})
 }
 
@@ -28,6 +48,48 @@ func TestContainIncrementPendingEntryExists(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 4,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+
+		randBytes := make([]byte, 10)
+		rand.Read(randBytes)
+		hash1 := pkcrypto.SHA256Hash(randBytes)
+
+		info1 := &audit.PendingAudit{
+			NodeID:            planet.StorageNodes[0].ID(),
+			PieceID:           storj.PieceID{},
+			StripeIndex:       0,
+			ShareSize:         0,
+			ExpectedShareHash: hash1,
+			ReverifyCount:     0,
+		}
+
+		err := planet.Satellites[0].DB.Containment().IncrementPending(ctx, info1)
+		require.NoError(t, err)
+
+		randBytes = make([]byte, 10)
+		rand.Read(randBytes)
+		hash2 := pkcrypto.SHA256Hash(randBytes)
+
+		info2 := &audit.PendingAudit{
+			NodeID:            planet.StorageNodes[0].ID(),
+			PieceID:           storj.PieceID{},
+			StripeIndex:       1,
+			ShareSize:         1,
+			ExpectedShareHash: hash2,
+			ReverifyCount:     0,
+		}
+
+		err = planet.Satellites[0].DB.Containment().IncrementPending(ctx, info2)
+		require.NoError(t, err)
+
+		pending, err := planet.Satellites[0].DB.Containment().Get(ctx, info1.NodeID)
+		require.NoError(t, err)
+
+		require.Equal(t, 1, pending.ReverifyCount)
+
+		// Even though info2 had different pending audit info,
+		// we only need to save the original pending audit info
+		// since the node ID is the same.
+		require.Equal(t, info1.ExpectedShareHash, pending.ExpectedShareHash)
 	})
 }
 
@@ -35,5 +97,27 @@ func TestContainDelete(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 4,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+
+		randBytes := make([]byte, 10)
+		rand.Read(randBytes)
+		hash1 := pkcrypto.SHA256Hash(randBytes)
+
+		info1 := &audit.PendingAudit{
+			NodeID:            planet.StorageNodes[0].ID(),
+			PieceID:           storj.PieceID{},
+			StripeIndex:       0,
+			ShareSize:         0,
+			ExpectedShareHash: hash1,
+			ReverifyCount:     0,
+		}
+
+		err := planet.Satellites[0].DB.Containment().IncrementPending(ctx, info1)
+		require.NoError(t, err)
+
+		err = planet.Satellites[0].DB.Containment().Delete(ctx, info1.NodeID)
+		require.NoError(t, err)
+
+		_, err = planet.Satellites[0].DB.Containment().Get(ctx, info1.NodeID)
+		require.Error(t, err, audit.ErrContainedNotFound.New(info1.NodeID.String()))
 	})
 }
