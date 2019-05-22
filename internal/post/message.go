46a298303f70458b724fb6bc1616cf69bb25a77d
Ethan <ethan@storj.io>
2019-05-22 14:41:04 -0400

    corrected scopelint issue

--- internal/post/message.go
+++ internal/post/message.go
@@ -49,6 +49,8 @@ func (msg *Message) Bytes() (data []byte, err error) {
 	fmt.Fprintf(&body, "MIME-Version: 1.0\r\n")
 	fmt.Fprintf(&body, "Subject: %v\r\n", mime.QEncoding.Encode("utf-8", msg.Subject))
 	fmt.Fprintf(&body, "From: %s\r\n", &msg.From)
+	for _, msgTo := range msg.To {
+		to := msgTo
 		fmt.Fprintf(&body, "To: %s\r\n", &to)
 	}
 	for _, recipient := range msg.ReceiptTo {
