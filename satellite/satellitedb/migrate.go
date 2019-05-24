1bd52b9f902aa06459485ec0379754d35dfde6ea
Jeff Wendling <leterip@gmail.com>
2019-05-24 16:51:27 +0000

    server side macaroons (#1945)
    What: Adds macaroon support to the server side
    
    Why: So that api keys are now macaroons

--- satellite/satellitedb/migrate.go
+++ satellite/satellitedb/migrate.go
@@ -694,6 +694,24 @@ func (db *DB) PostgresMigration() *migrate.Migration {
 					);`,
 				},
 			},
+			{
+				Description: "Drops and recreates api key table to handle macaroons and adds revocation table",
+				Version:     23,
+				Action: migrate.SQL{
+					`DROP TABLE api_keys CASCADE`,
+					`CREATE TABLE api_keys (
+						id bytea NOT NULL,
+						project_id bytea NOT NULL REFERENCES projects( id ) ON DELETE CASCADE,
+						head bytea NOT NULL,
+						name text NOT NULL,
+						secret bytea NOT NULL,
+						created_at timestamp with time zone NOT NULL,
+						PRIMARY KEY ( id ),
+						UNIQUE ( head ),
+						UNIQUE ( name, project_id )
+					);`,
+				},
+			},
 		},
 	}
 }
