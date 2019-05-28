b4574fe4abc2eaf869ddea54fce1ddaf472c346a
Alexander Leitner <leitnersalex@gmail.com>
2019-05-28 13:39:33 -0400

    Clean up tests for Project

--- lib/uplink/ext/c/tests/project_test.c
+++ lib/uplink/ext/c/tests/project_test.c
@@ -7,32 +7,50 @@
 #include "unity.h"
 #include "../../uplink-cgo.h"
 
-void create_test_bucket(ProjectRef_t ref_project, char *bucket_name, Bucket_t *bucket, BucketConfig_t *cfg, char **err)
+UplinkRef_t NewTestUplink(char **);
+
+void create_test_bucket(ProjectRef_t ref_project, char *bucket_name, Bucket_t *bucket, char **err)
 {
-    *bucket = CreateBucket(ref_project, bucket_name, cfg, err);
+    EncryptionParameters_t enc_param;
+    enc_param.cipher_suite = 1;
+    enc_param.block_size = 1024;
+
+    // NB: release defaults (maybe factor out into a lib helper)
+    RedundancyScheme_t scheme;
+    scheme.algorithm = 1;
+    scheme.share_size = 1024;
+    // TODO: we probably want to use dev defaults instead
+    scheme.required_shares = 29;
+    scheme.repair_shares = 35;
+    scheme.optimal_shares = 80;
+    scheme.total_shares = 95;
+
+    BucketConfig_t bucket_cfg;
+    bucket_cfg.path_cipher = 0;
+    bucket_cfg.encryption_parameters = &enc_param;
+
+    *bucket = CreateBucket(ref_project, bucket_name, bucket_cfg, err);
     TEST_ASSERT_EQUAL_STRING("", *err);
 
-    TEST_ASSERT_NOT_NULL(bucket.encryption_parameters);
-    TEST_ASSERT_EQUAL(enc_param.cipher_suite, bucket.encryption_parameters->cipher_suite);
-    TEST_ASSERT_EQUAL(enc_param.block_size, bucket.encryption_parameters->block_size);
+    TEST_ASSERT_NOT_NULL(bucket->encryption_parameters);
+    TEST_ASSERT_EQUAL(enc_param.cipher_suite, bucket->encryption_parameters->cipher_suite);
+    TEST_ASSERT_EQUAL(enc_param.block_size, bucket->encryption_parameters->block_size);
 
-    TEST_ASSERT_NOT_NULL(bucket.redundancy_scheme);
-    TEST_ASSERT_EQUAL(scheme.algorithm, bucket.redundancy_scheme->algorithm);
-    TEST_ASSERT_EQUAL(scheme.share_size, bucket.redundancy_scheme->share_size);
-    TEST_ASSERT_EQUAL(scheme.required_shares, bucket.redundancy_scheme->required_shares);
-    TEST_ASSERT_EQUAL(scheme.repair_shares, bucket.redundancy_scheme->repair_shares);
-    TEST_ASSERT_EQUAL(scheme.optimal_shares, bucket.redundancy_scheme->optimal_shares);
-    TEST_ASSERT_EQUAL(scheme.total_shares, bucket.redundancy_scheme->total_shares);
+    TEST_ASSERT_NOT_NULL(bucket->redundancy_scheme);
+    TEST_ASSERT_EQUAL(scheme.algorithm, bucket->redundancy_scheme->algorithm);
+    TEST_ASSERT_EQUAL(scheme.share_size, bucket->redundancy_scheme->share_size);
+    TEST_ASSERT_EQUAL(scheme.required_shares, bucket->redundancy_scheme->required_shares);
+    TEST_ASSERT_EQUAL(scheme.repair_shares, bucket->redundancy_scheme->repair_shares);
+    TEST_ASSERT_EQUAL(scheme.optimal_shares, bucket->redundancy_scheme->optimal_shares);
+    TEST_ASSERT_EQUAL(scheme.total_shares, bucket->redundancy_scheme->total_shares);
 
-    TEST_ASSERT_EQUAL_STRING(bucket_name, bucket.name);
-    TEST_ASSERT_NOT_EQUAL(0, bucket.created);
+    TEST_ASSERT_EQUAL_STRING(bucket_name, bucket->name);
+    TEST_ASSERT_NOT_EQUAL(0, bucket->created);
     // TODO: what is expected here (bucket.path_cipher is 1 when bucket_cfg.path_cipher is 0 and vice-versa)?
 //    TEST_ASSERT_EQUAL(bucket_cfg.path_cipher, bucket.path_cipher);
     // TODO: what is expected here (bucket.segment_size is 67108864)?
 //    TEST_ASSERT_EQUAL(1024, bucket.segment_size);
 }
-
-UplinkRef_t NewTestUplink(char **);
 
 ProjectRef_t OpenTestProject(char **err)
 {
@@ -52,105 +70,69 @@ void TestCloseProject(void)
     char *_err = "";
     char **err = &_err;
 
-    ProjectRef_t ref_project = OpenTestProject(err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-    TEST_ASSERT_NOT_EQUAL(0, ref_project);
+    //TODO: Fill in
 
-    CloseProject(ref_project, err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
 }
 
-void TestCreateBucket(void)
+void TestProject(void)
 {
     char *_err = "";
     char **err = &_err;
 
+    // Create Project
     ProjectRef_t ref_project = OpenTestProject(err);
     TEST_ASSERT_EQUAL_STRING("", *err);
+    if (strcmp(*err, "") != 0) {
+        goto end_project_test;
+    }
 
-    EncryptionParameters_t enc_param;
-    enc_param.cipher_suite = 1;
-    enc_param.block_size = 1024;
+    char *bucket_names[] = {"TestBucket1","bryansboringbucket"};
+    int num_of_buckets = sizeof(bucket_names) / sizeof(bucket_names[0]);
 
-    // NB: release defaults (maybe factor out into a lib helper)
-    RedundancyScheme_t scheme;
-    scheme.algorithm = 1;
-    scheme.share_size = 1024;
-    // TODO: we probably want to use dev defaults instead
-    scheme.required_shares = 29;
-    scheme.repair_shares = 35;
-    scheme.optimal_shares = 80;
-    scheme.total_shares = 95;
+    // Create buckets
+    for (int i=0; i < num_of_buckets; i++) {
+        Bucket_t *bucket = malloc(sizeof(Bucket_t));
+        create_test_bucket(ref_project, bucket_names[i], bucket, err);
+        TEST_ASSERT_EQUAL_STRING("", *err);
+        if (strcmp(*err, "") != 0) {
+            goto end_project_test;
+        }
+        free(bucket);
+    }
 
-    BucketConfig_t bucket_cfg;
-    bucket_cfg.path_cipher = 0;
-    bucket_cfg.encryption_parameters = &enc_param;
-
-    char *bucket_name = getenv("CREATE_BUCKET_NAME");
-
-    Bucket_t *bucket = malloc(sizeof(Bucket_t));
-    create_test_bucket(ref_project, bucket_name, bucket, &bucket_cfg, err);
-}
-
-void TestDeleteBucket(void)
-{
-    char *_err = "";
-    char **err = &_err;
-
-    ProjectRef_t ref_project = OpenTestProject(err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-
-    char *bucket_name = getenv("DELETE_BUCKET_NAME");
-
-    DeleteBucket(ref_project, bucket_name, err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-}
-
-void TestListBuckets(void)
-{
-    char *_err = "";
-    char **err = &_err;
-
-    ProjectRef_t ref_project = OpenTestProject(err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-
+    // List buckets
     // TODO: test BucketListOptions_t
     BucketList_t bucket_list = ListBuckets(ref_project, NULL, err);
     TEST_ASSERT_EQUAL_STRING("", *err);
     TEST_ASSERT_FALSE(bucket_list.more);
-    TEST_ASSERT_EQUAL(2, bucket_list.length);
+    TEST_ASSERT_EQUAL(num_of_buckets, bucket_list.length);
     TEST_ASSERT_NOT_NULL(bucket_list.items);
 
-    Bucket_t bucket;
-    bucket = bucket_list.items[0];
+    for (int i=0; i < num_of_buckets; i++) {
+        Bucket_t *listed_bucket;
+        listed_bucket = &bucket_list.items[i];
+        TEST_ASSERT_EQUAL_STRING(bucket_names[i], listed_bucket->name);
+        TEST_ASSERT_NOT_EQUAL(0, listed_bucket->created);
+    }
 
-    char *create_bucket_name = getenv("CREATE_BUCKET_NAME");
-    TEST_ASSERT_EQUAL_STRING(create_bucket_name, bucket.name);
-    TEST_ASSERT_NOT_EQUAL(0, bucket.created);
+    // Delete Buckets
+    for (int i=0; i < num_of_buckets; i++) {
+        DeleteBucket(ref_project, bucket_names[i], err);
+        TEST_ASSERT_EQUAL_STRING("", *err);
+        if (strcmp(*err, "") != 0) {
+            goto end_project_test;
+        }
+    }
 
-    bucket = bucket_list.items[1];
-
-    char *delete_bucket_name = getenv("DELETE_BUCKET_NAME");
-    TEST_ASSERT_EQUAL_STRING(delete_bucket_name, bucket.name);
-    TEST_ASSERT_NOT_EQUAL(0, bucket.created);
-}
-
-void TestGetBucketInfo(void)
-{
-    char *_err = "";
-    char **err = &_err;
-
-    ProjectRef_t ref_project = OpenTestProject(err);
+end_project_test:
+    // Close Project
+    CloseProject(ref_project, err);
     TEST_ASSERT_EQUAL_STRING("", *err);
-
-    char *bucket_name = getenv("INFO_BUCKET_NAME");
 }
 
 int main(int argc, char *argv[])
 {
     UNITY_BEGIN();
-    RUN_TEST(TestCreateBucket);
-    RUN_TEST(TestListBuckets);
-    RUN_TEST(TestDeleteBucket);
+    RUN_TEST(TestProject);
     return UNITY_END();
 }
--- lib/uplink/ext/project_test.go
+++ lib/uplink/ext/project_test.go
@@ -3,11 +3,7 @@ package main_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
 	"storj.io/storj/internal/testcontext"
-	"storj.io/storj/lib/uplink"
 )
 
 func TestCProjectTests(t *testing.T) {
@@ -17,46 +13,16 @@ func TestCProjectTests(t *testing.T) {
 	planet := startTestPlanet(t, ctx)
 	defer ctx.Check(planet.Shutdown)
 
-	createBucketName := "create-bucket-" + t.Name()
-	deleteBucketName := "delete-bucket-" + t.Name()
 	project := newProject(t, planet)
 	apikey := newAPIKey(t, ctx, planet, project.ID)
 	satelliteAddr := planet.Satellites[0].Addr()
 
 	envVars := []string{
-		"CREATE_BUCKET_NAME=" + createBucketName,
-		"DELETE_BUCKET_NAME=" + deleteBucketName,
 		"SATELLITE_ADDR=" + satelliteAddr,
 		"APIKEY=" + apikey,
 	}
 
 	{
-		goUplink, err := uplink.NewUplink(ctx, testConfig)
-		require.NoError(t, err)
-
-		apikey, err := uplink.ParseAPIKey(apikey)
-		require.NoError(t, err)
-
-		project, err := goUplink.OpenProject(ctx, satelliteAddr, apikey, nil)
-		require.NoError(t, err)
-
-		deleteBucket, err := project.CreateBucket(ctx, deleteBucketName, nil)
-		require.NoError(t, err)
-		require.NotNil(t, deleteBucket)
-
-		buckets, err := project.ListBuckets(ctx, nil)
-		require.NoError(t, err)
-		require.NotNil(t, buckets)
-		require.NotEmpty(t, buckets.Items)
-		assert.Len(t, buckets.Items, 1)
-
 		runCTest(t, ctx, "project_test.c", envVars...)
-
-		buckets, err = project.ListBuckets(ctx, nil)
-		require.NoError(t, err)
-		require.NotNil(t, buckets)
-		require.NotEmpty(t, buckets.Items)
-		assert.Len(t, buckets.Items, 1)
-		assert.Equal(t, createBucketName, buckets.Items[0].Name)
 	}
 }
