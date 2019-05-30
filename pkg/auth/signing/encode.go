24c81329753242c464cbda6743c29a3090458941
Jennifer Li Johnson <jennifer@storj.io>
2019-05-30 15:01:55 -0400

    rewrites encode methods for readability (#2084)

--- pkg/auth/signing/encode.go
+++ pkg/auth/signing/encode.go
@@ -9,26 +9,29 @@ import (
 	"storj.io/storj/pkg/pb"
 )
 
-// EncodeOrderLimit encodes order limit into bytes for signing.
+// EncodeOrderLimit encodes order limit into bytes for signing. Removes signature from serialized limit.
 func EncodeOrderLimit(limit *pb.OrderLimit2) ([]byte, error) {
 	signature := limit.SatelliteSignature
 	limit.SatelliteSignature = nil
-	defer func() { limit.SatelliteSignature = signature }()
-	return proto.Marshal(limit)
+	out, err := proto.Marshal(limit)
+	limit.SatelliteSignature = signature
+	return out, err
 }
 
-// EncodeOrder encodes order into bytes for signing.
+// EncodeOrder encodes order into bytes for signing. Removes signature from serialized order.
 func EncodeOrder(order *pb.Order2) ([]byte, error) {
 	signature := order.UplinkSignature
 	order.UplinkSignature = nil
-	defer func() { order.UplinkSignature = signature }()
-	return proto.Marshal(order)
+	out, err := proto.Marshal(order)
+	order.UplinkSignature = signature
+	return out, err
 }
 
-// EncodePieceHash encodes piece hash into bytes for signing.
+// EncodePieceHash encodes piece hash into bytes for signing. Removes signature from serialized hash.
 func EncodePieceHash(hash *pb.PieceHash) ([]byte, error) {
 	signature := hash.Signature
 	hash.Signature = nil
-	defer func() { hash.Signature = signature }()
-	return proto.Marshal(hash)
+	out, err := proto.Marshal(hash)
+	hash.Signature = signature
+	return out, err
 }
