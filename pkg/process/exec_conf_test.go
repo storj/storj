abf4a39071f254b34b89dc89b528fb630b21a171
Jeff Wendling <leterip@gmail.com>
2019-05-31 12:19:27 -0600

    add test for setting propagation

---
+++ pkg/process/exec_conf_test.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package process
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/spf13/cobra"
+)
+
+func setenv(key, value string) func() {
+	old := os.Getenv(key)
+	os.Setenv(key, value)
+	return func() { os.Setenv(key, old) }
+}
+
+func TestExec_PropagatesSettings(t *testing.T) {
+	// Set up a command that does nothing.
+	cmd := &cobra.Command{RunE: func(cmd *cobra.Command, args []string) error { return nil }}
+
+	// Define a config struct and some flags.
+	var config struct {
+		X int `default:"0"`
+	}
+	Bind(cmd, &config)
+	y := cmd.Flags().Int("y", 0, "y flag (command)")
+	z := flag.Int("z", 0, "z flag (stdlib)")
+
+	// Set some environment variables for viper.
+	defer setenv("STORJ_X", "1")()
+	defer setenv("STORJ_Y", "2")()
+	defer setenv("STORJ_Z", "3")()
+
+	// Run the command through the exec call.
+	Exec(cmd)
+
+	// Check that the variables are now bound.
+	require.Equal(t, 1, config.X)
+	require.Equal(t, 2, *y)
+	require.Equal(t, 3, *z)
+}
