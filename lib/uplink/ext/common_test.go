dc6349691fb3e8fb04de2da68d41cd55a06e3c36
Bryan White <bryanchriswhite@gmail.com>
2019-06-01 14:52:12 +0200

    move apikey test to their own file and test `Serialize`

--- lib/uplink/ext/common_test.go
+++ lib/uplink/ext/common_test.go
@@ -1,9 +1,9 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"storj.io/storj/lib/uplink"
 	"testing"
 
 	"storj.io/storj/internal/testcontext"
@@ -18,17 +18,4 @@ func TestCCommonTests(t *testing.T) {
 	defer ctx.Check(planet.Shutdown)
 
 	runCTest(t, ctx, "common_test.c")
-}
-
-func TestParseAPIKey(t *testing.T) {
-	var cErr Cchar
-	apikeyString := "testapikey123"
-	cAPIKeyRef := ParseAPIKey(stringToCCharPtr(apikeyString), &cErr)
-	require.Empty(t, cCharToGoString(cErr))
-
-	apikey, ok := structRefMap.Get(token(cAPIKeyRef)).(uplink.APIKey)
-	require.True(t, ok)
-	require.NotEmpty(t, apikey)
-
-	assert.Equal(t, apikeyString, apikey.Serialize())
 }
