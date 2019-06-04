14af1bb4e4125ae4c61f9b94206521704eb49ead
JT Olio <hello@jtolio.com>
2019-06-04 14:45:05 -0600

    db: set max open conns, conn max lifetime, add db stat monitoring
    this also makes these values configurable. we'll need to clean up
    the configuration so it's not in flags and in our config system
    later
    
    Change-Id: Ief557be5664e9ca255cc69a05c739ffbaef1b27e

--- internal/dbutil/defaults.go
+++ internal/dbutil/defaults.go
@@ -3,5 +3,31 @@
 
 package dbutil
 
-// DefaultMaxIdleConns default value for database connections
-const DefaultMaxIdleConns = 100
+import (
+	"database/sql"
+	"flag"
+
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+)
+
+var (
+	maxIdleConns    = flag.Int("db.max_idle_conns", 50, "default value for database connections, -1 means the stdlib default")
+	maxOpenConns    = flag.Int("db.max_open_conns", 100, "default value for database connections, -1 means the stdlib default")
+	connMaxLifetime = flag.Duration("db.conn_max_lifetime", -1, "default value for database connections, -1 means the stdlib default")
+)
+
+func Configure(db *sql.DB, mon *monkit.Scope) {
+	if *maxIdleConns >= 0 {
+		db.SetMaxIdleConns(*maxIdleConns)
+	}
+	if *maxOpenConns >= 0 {
+		db.SetMaxOpenConns(*maxOpenConns)
+	}
+	if *connMaxLifetime >= 0 {
+		db.SetConnMaxLifetime(*connMaxLifetime)
+	}
+	mon.Chain("db_stats", monkit.StatSourceFunc(
+		func(cb func(name string, val float64)) {
+			monkit.StatSourceFromStruct(db.Stats()).Stats(cb)
+		}))
+}
