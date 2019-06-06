87d9b221dd131a9e6a7898c609a3d8509add6a64
Egon Elbre <egonelbre@gmail.com>
2019-06-06 17:32:01 +0300

    remove unused consts

--- pkg/accounting/common.go
+++ pkg/accounting/common.go
@@ -11,13 +11,6 @@ import (
 
 // Constants for accounting_raw, accounting_rollup, and accounting_timestamps
 const (
-	// AtRest is the data_type representing at-rest data calculated from metainfo
-	BandwidthPut       = 0 // int(pb.BandwidthAction_PUT)
-	BandwidthGet       = 1 // int(pb.BandwidthAction_GET)
-	BandwidthGetAudit  = 2 // int(pb.BandwidthAction_GET_AUDIT)
-	BandwidthGetRepair = 3 // int(pb.BandwidthAction_GET_REPAIR)
-	BandwidthPutRepair = 4 // int(pb.BandwidthAction_PUT_REPAIR)
-	AtRest             = 5 // int(pb.BandwidthAction_PUT_REPAIR + 1)
 	// LastAtRestTally represents the accounting timestamp for the at-rest data calculation
 	LastAtRestTally = "LastAtRestTally"
 	// LastBandwidthTally represents the accounting timestamp for the bandwidth allocation query
