29576752ce6010b11c003cac8430ef06befcdc75
Kishore <kishore@storj.io>
2019-06-07 10:41:07 -0400

    add value attribution db functions

--- satellite/satellitedb/connectorkeys.go
+++ satellite/satellitedb/connectorkeys.go
@@ -14,21 +14,50 @@ import (
 
 // connectorkeys is an implementation of satellite.APIKeys
 type connectorkeys struct {
-	db dbx.Methods
+	db *dbx.DB
 }
 
 // GetByProjectID
-func (keys *connectorkeys) GetByProjectID(ctx context.Context, projectID uuid.UUID) (connectorkeyinfo *console.ConnectorKeyInfo, err error) {
+func (keys *connectorkeys) GetByProjectID(ctx context.Context, projectID uuid.UUID) (_ *console.ConnectorKeyInfo, err error) {
+	defer mon.Task()(&ctx)(&err)
 
-	return connectorkeyinfo, nil
+	dbxInfo, err := keys.db.Get_ValueAttribution_By_BucketId(ctx, dbx.ValueAttribution_BucketId(projectID[:]))
+
+	if err != nil {
+		return nil, err
+	}
+	return fromDBXConnectorKey(ctx, dbxInfo)
 }
 
 // Create implements satellite.APIKeys
-func (keys *connectorkeys) Create(ctx context.Context, head []byte, info console.APIKeyInfo) (connectorkeyinfo *console.ConnectorKeyInfo, err error) {
-	return connectorkeyinfo, nil
+func (keys *connectorkeys) Create(ctx context.Context, info console.ConnectorKeyInfo) (connectorkeyinfo *console.ConnectorKeyInfo, err error) {
+	defer mon.Task()(&ctx)(&err)
+	dbxInfo, err := keys.db.Create_ValueAttribution(ctx, dbx.ValueAttribution_BucketId(info.ProjectID[:]),
+		dbx.ValueAttribution_PartnerId(info.ID[:]), dbx.ValueAttribution_LastUpdated(info.CreatedAt))
+	return fromDBXConnectorKey(ctx, dbxInfo)
 }
 
 // Delete implements satellite.APIKeys
 func (keys *connectorkeys) Delete(ctx context.Context, id uuid.UUID) (err error) {
 	return nil
+}
+
+// fromDBXConnectorKey converts dbx.ValueAttribution to connectory key info
+func fromDBXConnectorKey(ctx context.Context, key *dbx.ValueAttribution) (_ *console.ConnectorKeyInfo, err error) {
+	defer mon.Task()(&ctx)(&err)
+	id, err := bytesToUUID(key.PartnerId)
+	if err != nil {
+		return nil, err
+	}
+
+	projectID, err := bytesToUUID(key.BucketId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &console.ConnectorKeyInfo{
+		ID:        id,
+		ProjectID: projectID,
+		CreatedAt: key.LastUpdated,
+	}, nil
 }
