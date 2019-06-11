5041b59a290a7dcc44cb7e9fb9f5d6443a4a0cfc
Egon Elbre <egonelbre@gmail.com>
2019-06-11 23:07:11 +0300

    using require

--- lib/uplinkc/testdata/project_test.c
+++ lib/uplinkc/testdata/project_test.c
@@ -57,55 +57,79 @@ int main(int argc, char *argv[])
 }
 
 void HandleProject(Project project) {
+    char *_err = "";
+    char **err = &_err;
+
     char *bucket_names[] = {"TestBucket1", "TestBucket2", "TestBucket3", "TestBucket4"};
     int num_of_buckets = sizeof(bucket_names) / sizeof(bucket_names[0]);
 
     // Create buckets
     for (int i=0; i < num_of_buckets; i++) {
-        Bucket *bucket = CreateTestBucket(ref_project, bucket_names[i], err);
-        free(bucket);
+        char *bucket_name = bucket_names[i];
+
+        BucketConfig config = {};
+
+        config.path_cipher = 0;
+
+        config.encryption_parameters.cipher_suite = 1; // TODO: make a named const
+        config.encryption_parameters.block_size = 1024;
+
+        config.redundancy_scheme.algorithm = 1; // TODO: make a named const
+        config.redundancy_scheme.share_size = 1024;
+        config.redundancy_scheme.required_shares = 4;
+        config.redundancy_scheme.repair_shares = 6;
+        config.redundancy_scheme.optimal_shares = 8;
+        config.redundancy_scheme.total_shares = 10;
+
+        BucketInfo info = CreateBucket(project, bucket_name, config, err);
+        require_noerror(*err);
+
+        require(strcmp(bucket_name, info.name) == 0, "name mismatch\n");
+        require(info.created != 0, "created was 0");
+
+        require(config.encryption_parameters.cipher_suite == info.encryption_parameters.cipher_suite, "cipher_suite mismatch\n");
+        require(config.encryption_parameters.block_size   == info.encryption_parameters.block_size, "block_size mismatch\n");
+
+        require(config.redundancy_scheme.algorithm        == info.redundancy_scheme.algorithm, "algorithm mismatch\n");
+        require(config.redundancy_scheme.share_size       == info.redundancy_scheme.share_size, "share_size mismatch\n");
+        require(config.redundancy_scheme.required_shares  == info.redundancy_scheme.required_shares, "required_shares mismatch\n");
+        require(config.redundancy_scheme.repair_shares    == info.redundancy_scheme.repair_shares, "repair_shares mismatch\n");
+        require(config.redundancy_scheme.optimal_shares   == info.redundancy_scheme.optimal_shares, "optimal_shares mismatch\n");
+        require(config.redundancy_scheme.total_shares     == info.redundancy_scheme.total_shares, "total_shares mismatch\n");
+
+        FreeBucketInfo(&bucketinfo)
+    }
+
+    // listing buckets
+    BucketList bucket_list = ListBuckets(project, NULL, err);
+    require_noerror(*err);
+    require(bucket_list.more == FALSE);
+    require(bucket_list.length == num_of_buckets);
+    require(bucket_list.items != NULL);
+
+    for(int i = 0; i < bucket_list.length; i++) {
+        BucketInfo *info = &bucket_list.items[i];
+        require(strcmp(info->name, bucket_names[0]) == 0);
+        require(info->created != 0, "created was 0");
+    }
+
+    FreeBucketList(bucket_list);
+
+    // getting bucket infos
+    for(int i = 0; i < num_of_buckets; i++) {
+        char *bucket_name = bucket_names[i];
+        BucketInfo info = GetBucketInfo(project, bucket_name, err);
+        require_noerror(err);
+        require(strcmp(info->name, bucket_names[0]) == 0);
+        require(info->created != 0, "created was 0");
+
+        FreeBucketInfo(info);
     }
 }
 
+/*
 void TestProject(void)
 {
-    char *_err = "";
-    char **err = &_err;
-
-    // Open Project
-    ProjectRef_t ref_project = OpenTestProject(err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-
-    char *bucket_names[] = {"TestBucket1","TestBucket2","TestBucket3","TestBucket4"};
-    int num_of_buckets = sizeof(bucket_names) / sizeof(bucket_names[0]);
-
-    // Create buckets
-    for (int i=0; i < num_of_buckets; i++) {
-        Bucket_t *bucket = CreateTestBucket(ref_project, bucket_names[i], err);
-        free(bucket);
-    }
-
-    // List buckets
-    // TODO: test BucketListOptions_t
-    BucketList_t bucket_list = ListBuckets(ref_project, NULL, err);
-    TEST_ASSERT_EQUAL_STRING("", *err);
-    TEST_ASSERT_FALSE(bucket_list.more);
-    TEST_ASSERT_EQUAL(num_of_buckets, bucket_list.length);
-    TEST_ASSERT_NOT_NULL(bucket_list.items);
-
-    for (int i=0; i < num_of_buckets; i++) {
-        Bucket_t *bucket = &bucket_list.items[i];
-        TEST_ASSERT_EQUAL_STRING(bucket_names[i], bucket->name);
-        TEST_ASSERT_NOT_EQUAL(0, bucket->created);
-
-        // Get bucket info
-        BucketInfo_t bucket_info = GetBucketInfo(ref_project, bucket->name, err);
-        TEST_ASSERT_EQUAL_STRING("", *err);
-        TEST_ASSERT_EQUAL_STRING(bucket->name, bucket_info.bucket.name);
-        TEST_ASSERT_NOT_EQUAL(0, bucket_info.bucket.created);
-    }
-    free(bucket_list.items);
-
     uint8_t *enc_key = "abcdefghijklmnopqrstuvwxyzABCDEF";
     EncryptionAccess_t *access = NewEncryptionAccess(enc_key, strlen((const char *)enc_key));
 
@@ -134,3 +158,4 @@ int main(int argc, char *argv[])
     RUN_TEST(TestProject);
     return UNITY_END();
 }
+*/
