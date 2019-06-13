e4ecb3299ca1d0aa00ccbffd2ac2a581fe06b4a5
Alexander Leitner <leitnersalex@gmail.com>
2019-06-13 13:53:41 -0400

    Deleted tests

--- lib/uplinkc/testdata_test.go
+++ lib/uplinkc/testdata_test.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,6 +16,28 @@ import (
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testplanet"
 )
+
+func RunPlanet(t *testing.T, run func(ctx *testcontext.Context, planet *testplanet.Planet)) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	planet, err := testplanet.NewCustom(
+		zaptest.NewLogger(t, zaptest.Level(zapcore.WarnLevel)),
+		testplanet.Config{
+			SatelliteCount:   1,
+			StorageNodeCount: 8,
+			UplinkCount:      1,
+			Reconfigure:      testplanet.DisablePeerCAWhitelist,
+		},
+	)
+	require.NoError(t, err)
+	defer ctx.Check(planet.Shutdown)
+
+	planet.Start(ctx)
+
+	run(ctx, planet)
+}
+
 
 func TestC(t *testing.T) {
 	ctx := testcontext.New(t)
