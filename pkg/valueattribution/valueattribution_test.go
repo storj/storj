09940d4e0b8ffc437546d2997f7f275d109b196f
aligeti <34487396+aligeti@users.noreply.github.com>
2019-06-18 09:06:33 -0400

    value attribution DB interface Insert & Get method support (#2200)
    * value attribution DB interface methods support

---
+++ pkg/valueattribution/valueattribution_test.go
@@ -0,0 +1,63 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package valueattribution_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"storj.io/storj/internal/testcontext"
+	"storj.io/storj/pkg/valueattribution"
+	"storj.io/storj/satellite"
+	"storj.io/storj/satellite/satellitedb/satellitedbtest"
+)
+
+func TestValueAttribution(t *testing.T) {
+	satellitedbtest.Run(t, func(t *testing.T, db satellite.DB) {
+		ctx := testcontext.New(t)
+		defer ctx.Cleanup()
+
+		valAttrdb := db.ValueAttribution()
+
+		// unique partner and bucket
+		partnerInfo := &valueattribution.PartnerInfo{
+			PartnerID:  []byte("valueattribution testcase partnerID"),
+			BucketName: []byte("valueattribution testcase bucketname"),
+		}
+
+		// same partner and dfferent bucket
+		partnerInfo1 := &valueattribution.PartnerInfo{
+			PartnerID:  []byte("valueattribution testcase partnerID"),
+			BucketName: []byte("valueattribution testcase different bucketname"),
+		}
+
+		// different partner and existing bucket
+		partnerInfo2 := &valueattribution.PartnerInfo{
+			PartnerID:  []byte("valueattribution testcase different partnerID"),
+			BucketName: []byte("valueattribution testcase different bucketname"),
+		}
+
+		{ // Insert
+			_, err := valAttrdb.Insert(ctx, partnerInfo)
+			assert.NoError(t, err)
+
+			_, err = valAttrdb.Insert(ctx, partnerInfo1)
+			assert.NoError(t, err)
+
+			_, err = valAttrdb.Insert(ctx, partnerInfo2)
+			assert.Error(t, err)
+		}
+
+		{ // Get
+			info, err := valAttrdb.Get(ctx, partnerInfo.BucketName)
+			assert.NoError(t, err)
+			assert.Equal(t, partnerInfo.PartnerID, info.PartnerID)
+
+			info, err = valAttrdb.Get(ctx, partnerInfo1.BucketName)
+			assert.NoError(t, err)
+			assert.Equal(t, partnerInfo1.PartnerID, info.PartnerID)
+		}
+	})
+}
