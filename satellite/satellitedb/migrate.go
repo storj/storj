5ca401fcbb0dcea1a4e83d6c6bece0d287c268b7
Bill Thorp <billt@storj.io>
2019-06-18 09:17:02 -0400

    migrate version number

--- satellite/satellitedb/migrate.go
+++ satellite/satellitedb/migrate.go
@@ -831,7 +831,7 @@ func (db *DB) PostgresMigration() *migrate.Migration {
 			},
 			{
 				Description: "Add alpha and beta columns for reputations",
-				Version:     32,
+				Version:     33,
 				Action: migrate.SQL{
 					`ALTER TABLE nodes ADD COLUMN audit_reputation_alpha double precision NOT NULL DEFAULT 1;`,
 					`ALTER TABLE nodes ADD COLUMN audit_reputation_beta double precision NOT NULL DEFAULT 0;`,
