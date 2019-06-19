a20efe639ee0f02ce88a43422163566ea6cb62ef
JT Olio <hello@jtolio.com>
2019-06-19 17:47:24 -0600

    scripts/tag-release.sh: libuplink release tagging
    a couple of arch review meetings ago we discussed how to make sure
    that it is much easier to get the release defaults for binaries,
    libraries, and so on. we already imperfectly solved the binary
    problem with the release.sh script, but (until now!) have not solved
    the problem of getting release defaults for people building from
    source.
    
    the solution we seemed to all prefer was to make sure our tagged
    version commits check the release state into the source code.
    
    this script aides in tagging commits with version tags and
    updating the source defaults. it still plays nicely with
    release.sh and our other build processes.
    
    after this is merged we should configure github/go modules to
    prefer people use one of our tags instead of master (which will
    keep dev defaults).
    
    Change-Id: I36c5c33a1bc90ec1685f59b05dde779090e252b6

---
+++ scripts/tag-release.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+VERSION="$1"
+shift
+
+if [[ "$VERSION" == "" ]]; then
+  echo "usage: $0 vMAJOR.MINOR.PATCH"
+  exit 1
+fi
+
+cd "$(git rev-parse --show-toplevel)"
+
+if [[ "$(git diff --stat)" != '' ]] || [[ -n "$(git status -s)" ]]; then
+  echo "git working tree unclean"
+  exit 1
+fi
+
+TIMESTAMP=$(date +%s)
+COMMIT=$(git rev-parse HEAD)
+
+cat > ./internal/version/release.go <<EOF
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+  buildTimestamp = "$TIMESTAMP"
+  buildCommitHash = "$COMMIT"
+  buildVersion = "$VERSION"
+  buildRelease = "true"
+}
+EOF
+
+go install ./internal/version
+
+git add ./internal/version/release.go >/dev/null
+git commit -m "release $VERSION" >/dev/null
+if git tag $VERSION; then
+  echo successfully created tag $VERSION
+fi
+git reset --hard $COMMIT >/dev/null
