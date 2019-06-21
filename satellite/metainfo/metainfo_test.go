441c1c94921903418b824fbc46fd1feef4e32c22
Michal Niewrzal <michal@storj.io>
2019-06-21 09:24:42 +0200

    skip test

--- satellite/metainfo/metainfo_test.go
+++ satellite/metainfo/metainfo_test.go
@@ -525,6 +525,8 @@ func createTestPointer(t *testing.T) *pb.Pointer {
 }
 
 func TestBucketNameValidation(t *testing.T) {
+	t.Skip("Skip until bucket name validation is not enabled")
+
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 6, UplinkCount: 1,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
