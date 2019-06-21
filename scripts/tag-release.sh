4b92a7015dc8c69ffe075526a55968159ef15057
JT Olio <hello@jtolio.com>
2019-06-20 23:20:52 -0600

    added some docs
    Change-Id: Ide624fab794ce849e3a3e7254fb038251bba0c71

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+## This script:
+#
+# 1) Makes sure the current git working tree is clean
+# 2) Creates a release file that changes the build defaults to include
+#    a timestamp, a commit hash, a version number, and set the release
+#    flag to true.
+# 3) commits that release file and tags it with the release version
+# 4) resets the working tree back
+#
+# This script should be used instead of 'git tag' for Storj releases,
+# so downstream users developing with Go 1.11+ style modules find code
+# with our release defaults set instead of our dev defaults set.
+#
+
 set -eu
 set -o pipefail
 
