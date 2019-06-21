3d48a533e367021b74e9021342f350bca76e3645
Jeff Wendling <leterip@gmail.com>
2019-06-21 09:31:49 -0600

    add path implementation
    This commit adds a pkg/paths package which contains two types,
    Encrypted and Unencrypted, to statically enforce what is contained
    in a path. It's part of a refactoring of the code base to be more
    clear about what is contained in a storj.Path at all the layers.
    
    Change-Id: Ifc4d4932da26a97ea99749b8356b4543496a8864

---
+++ pkg/paths/path_test.go
@@ -0,0 +1,114 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package paths
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func assertConsumed(t *testing.T, it Iterator, consumed string, ok bool) {
+	t.Helper()
+	gotConsumed, gotOk := it.Consumed()
+	assert.Equal(t, gotConsumed, consumed)
+	assert.Equal(t, gotOk, ok)
+}
+
+func TestUnencrypted(t *testing.T) {
+	it := NewUnencrypted("foo").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "foo")
+	assertConsumed(t, it, "foo", true)
+	assert.True(t, it.Done())
+
+	it = NewUnencrypted("").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.True(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "", false)
+
+	it = NewUnencrypted("foo/").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "foo")
+	assertConsumed(t, it, "foo/", true)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "foo/", true)
+	assert.True(t, it.Done())
+
+	it = Unencrypted{}.Iterator()
+	assertConsumed(t, it, "", false)
+	assert.True(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "", false)
+}
+
+func TestEncrypted(t *testing.T) {
+	it := NewEncrypted("foo").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "foo")
+	assertConsumed(t, it, "foo", true)
+	assert.True(t, it.Done())
+
+	it = NewEncrypted("").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.True(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "", false)
+
+	it = NewEncrypted("foo/").Iterator()
+	assertConsumed(t, it, "", false)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "foo")
+	assertConsumed(t, it, "foo/", true)
+	assert.False(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "foo/", true)
+	assert.True(t, it.Done())
+
+	it = Encrypted{}.Iterator()
+	assertConsumed(t, it, "", false)
+	assert.True(t, it.Done())
+	assert.Equal(t, it.Next(), "")
+	assertConsumed(t, it, "", false)
+}
+
+func TestIterator(t *testing.T) {
+	for i, tt := range []struct {
+		path  string
+		comps []string
+	}{
+		{"", []string{}},
+		{"/", []string{"", ""}},
+		{"//", []string{"", "", ""}},
+		{" ", []string{" "}},
+		{"a", []string{"a"}},
+		{"/a/", []string{"", "a", ""}},
+		{"a/b/c/d", []string{"a", "b", "c", "d"}},
+		{"///a//b////c/d///", []string{"", "", "", "a", "", "b", "", "", "", "c", "d", "", "", ""}},
+	} {
+		errTag := fmt.Sprintf("Test case #%d", i)
+		iter, got := NewIterator(tt.path), make([]string, 0, len(tt.comps))
+		for !iter.Done() {
+			got = append(got, iter.Next())
+		}
+		assert.Equal(t, tt.comps, got, errTag)
+	}
+}
+
+func TestIteratorConsumed(t *testing.T) {
+	it := NewIterator("foo")
+	consumed, ok := it.Consumed()
+	assert.Equal(t, consumed, "")
+	assert.False(t, ok)
+	assert.Equal(t, it.Next(), "foo")
+	consumed, ok = it.Consumed()
+	assert.Equal(t, consumed, "foo")
+	assert.True(t, ok)
+}
