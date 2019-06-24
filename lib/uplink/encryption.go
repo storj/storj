7dcbb634e987db7be5be7c652c5d7ce6d0af243d
Jeff Wendling <leterip@gmail.com>
2019-06-24 10:37:14 -0600

    implement panics in libuplink encryption code
    todo on cipher suite selection as well as an api concern
    
    Change-Id: Ifa39eb3cc4b3443f7d96f9304df9b2ac4ec4085d

--- lib/uplink/encryption.go
+++ lib/uplink/encryption.go
@@ -4,8 +4,11 @@
 package uplink
 
 import (
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/gogo/protobuf/proto"
 	"github.com/zeebo/errs"
-	"storj.io/storj/pkg/encryption"
+
+	"storj.io/storj/pkg/macaroon"
 	"storj.io/storj/pkg/storj"
 )
 
@@ -40,9 +43,12 @@ func (s *EncryptionCtx) SetDefaultKey(defaultKey storj.Key) {
 
 // Import merges the other encryption context into this one. In cases
 // of conflicting path decryption settings (including if both contexts have
-// a default key), the existing settings are kept.
+// a default key), the new settings are kept.
 func (s *EncryptionCtx) Import(other *EncryptionCtx) error {
-	panic("TODO")
+	if key := other.store.GetDefaultKey(); key != nil {
+		s.store.SetDefaultKey(key)
+	}
+	return other.store.Iterate(s.store.Add)
 }
 
 // EncryptionRestriction represents a scenario where some set of objects
@@ -62,15 +68,109 @@ func (s *EncryptionCtx) Restrict(apiKey APIKey, restrictions ...EncryptionRestri
 		// so we don't have to do this test?
 		return APIKey{}, nil, errs.New("at least one restriction required")
 	}
-	panic("TODO")
+
+	caveat := macaroon.Caveat{}
+	encCtx := NewEncryptionCtx()
+
+	for _, res := range restrictions {
+		unencPath := paths.NewUnencrypted(res.PathPrefix)
+		cipher := storj.AESGCM // TODO(jeff): pick the right path cipher
+
+		encPath, err := encryption.StoreEncryptPath(res.Bucket, unencPath, cipher, s.store)
+		if err != nil {
+			return APIKey{}, nil, err
+		}
+		derivedKey, err := encryption.StoreDerivePathKey(res.Bucket, unencPath, s.store)
+		if err != nil {
+			return APIKey{}, nil, err
+		}
+
+		if err := encCtx.store.Add(res.Bucket, unencPath, encPath, *derivedKey); err != nil {
+			return APIKey{}, nil, err
+		}
+		caveat.AllowedPaths = append(caveat.AllowedPaths, &macaroon.Caveat_Path{
+			Bucket:              res.Bucket,
+			EncryptedPathPrefix: []byte(encPath.Raw()),
+		})
+	}
+
+	apiKey, err := apiKey.Restrict(caveat)
+	if err != nil {
+		return APIKey{}, nil, err
+	}
+
+	return apiKey, encCtx, nil
 }
 
 // Serialize turns an EncryptionCtx into base58
 func (s *EncryptionCtx) Serialize() ([]byte, error) {
-	panic("TODO")
+	var storeEntries []*pb.EncryptionCtx_StoreEntry
+	err := s.store.Iterate(func(bucket string, unenc, enc storj.Path, key storj.Key) error {
+		storeEntries = append(storeEntries, &pb.EncryptionCtx_StoreEntry{
+			Bucket:          []byte(bucket),
+			UnencryptedPath: []byte(unenc.Raw()),
+			EncryptedPath:   []byte(enc.Raw()),
+			Key:             key[:],
+		})
+		return nil
+	})
+	var defaultKey []byte
+	if key := s.store.GetDefaultKey(); key != nil {
+		defaultKey = key[:]
+	}
+
+	data, err := proto.Marshal(&pb.Scope{
+		DefaultKey:   defaultKey,
+		StoreEntries: storeEntries,
+	})
+	if err != nil {
+		return "", errs.New("unable to marshal encryption ctx: %v", err)
+	}
+
+	return base58.CheckEncode(data, 0), nil
+
 }
 
 // ParseEncryptionCtx parses a base58 serialized encryption context into a working one.
 func ParseEncryptionCtx(data []byte) (*EncryptionCtx, error) {
-	panic("TODO")
+	// TODO(jeff): should parse operate on a string?
+	data, version, err := base58.CheckDecode(string(data))
+	if err != nil || version != 0 {
+		return nil, errs.New("invalid encryption context format")
+	}
+
+	p := new(pb.EncryptionCtx)
+	if err := proto.Unmarshal(data, p); err != nil {
+		return nil, errs.New("unable to unmarshal encryption context: %v", err)
+	}
+
+	encCtx := NewEncryptionCtx()
+
+	if len(p.DefaultKey) > 0 {
+		if len(p.DefaultKey) != len(storj.Key{}) {
+			return nil, errs.New("invalid default key in encryption context")
+		}
+		var defaultKey storj.Key
+		copy(defaultKey[:], p.DefaultKey)
+		encCtx.SetDefaultKey(defaultKey)
+	}
+
+	for _, entry := range p.StoreEntries {
+		if len(entry.Key) != len(storj.Key{}) {
+			return nil, errs.New("invalid key in encryption context entry")
+		}
+		var key storj.Key
+		copy(key[:], entry.Key)
+
+		err := encCtx.store.Add(
+			string(entry.Bucket),
+			paths.NewUnencrypted(entry.UnencryptedPath),
+			paths.NewEncrypted(entry.EncryptedPath),
+			key)
+		if err != nil {
+			return nil, errs.New("invalid encryption context entry: %v", err)
+		}
+	}
+
+	return encCtx, nil
 }
