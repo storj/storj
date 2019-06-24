0652629fd02e1ffa908bde845394a747552def03
Moby <mobyvb@gmail.com>
2019-06-24 14:24:31 -0400

    update node selection test

--- pkg/overlay/selection_test.go
+++ pkg/overlay/selection_test.go
@@ -51,14 +51,13 @@ func TestOffline(t *testing.T) {
 }
 
 func TestNodeSelection(t *testing.T) {
-	// t.Skip("flaky")
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 10, UplinkCount: 1,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 		var err error
 		satellite := planet.Satellites[0]
 
-		// This sets audit counts of 0, 1, 2, 3, etc...
+		// This sets audit counts of 0, 1, 2, 3, ... 9
 		// so that we can fine-tune how many nodes are considered new or reputable
 		// by modifying the audit count cutoff passed into FindStorageNodesWithPreferences
 		for i, node := range planet.StorageNodes {
@@ -99,61 +98,46 @@ func TestNodeSelection(t *testing.T) {
 				RequestCount:  5,
 				ExpectedCount: 5,
 			},
-			// { // all reputable nodes except one, reputable and new nodes requested
-			// 	Preferences:   testNodeSelectionConfig(1, 1, false),
-			// 	RequestCount:  5,
-			// 	ExpectedCount: 6,
-			// },
-			{ // 50-50 reputable and new nodes, reputable and new nodes requested (new node ratio 1.0)
-				Preferences:   testNodeSelectionConfig(5, 0.5, false),
-				RequestCount:  10,
-				ExpectedCount: 10,
-			},
 			{ // 50-50 reputable and new nodes, not enough reputable nodes
 				Preferences:    testNodeSelectionConfig(5, 0, false),
 				RequestCount:   10,
 				ExpectedCount:  5,
 				ShouldFailWith: &overlay.ErrNotEnoughNodes,
 			},
-			// { // 50-50 reputable and new nodes, reputable and new nodes requested (new node ratio 0.5)
-			// 	Preferences:   testNodeSelectionConfig(5, 0.5, false),
-			// 	RequestCount:  4,
-			// 	ExpectedCount: 6,
-			// },
-			// { // all new nodes except one, reputable and new nodes requested (happy path)
-			// 	Preferences:   testNodeSelectionConfig(8, 1, false),
-			// 	RequestCount:  1,
-			// 	ExpectedCount: 2,
-			// },
-			// { // all new nodes except one, reputable and new nodes requested (not happy path)
-			// 	Preferences:    testNodeSelectionConfig(9, 1, false),
-			// 	RequestCount:   2,
-			// 	ExpectedCount:  3,
-			// 	ShouldFailWith: &overlay.ErrNotEnoughNodes,
-			// },
-			{ // all new nodes, reputable and new nodes requested
-				Preferences:    testNodeSelectionConfig(50, 1, false),
-				RequestCount:   2,
-				ExpectedCount:  2,
+			{ // 50-50 reputable and new nodes, reputable and new nodes requested, not enough reputable nodes
+				Preferences:    testNodeSelectionConfig(5, 0.2, false),
+				RequestCount:   10,
+				ExpectedCount:  7,
 				ShouldFailWith: &overlay.ErrNotEnoughNodes,
+			},
+			{ // all new nodes except one, reputable and new nodes requested (happy path)
+				Preferences:   testNodeSelectionConfig(9, 0.5, false),
+				RequestCount:  2,
+				ExpectedCount: 2,
+			},
+			{ // all new nodes except one, reputable and new nodes requested (not happy path)
+				Preferences:    testNodeSelectionConfig(9, 0.5, false),
+				RequestCount:   4,
+				ExpectedCount:  3,
+				ShouldFailWith: &overlay.ErrNotEnoughNodes,
+			},
+			{ // all new nodes, reputable and new nodes requested
+				Preferences:   testNodeSelectionConfig(50, 1, false),
+				RequestCount:  2,
+				ExpectedCount: 2,
 			},
 			{ // audit threshold edge case (1)
 				Preferences:   testNodeSelectionConfig(9, 0, false),
 				RequestCount:  1,
 				ExpectedCount: 1,
 			},
-			{ // audit threshold edge case (2)
-				Preferences:   testNodeSelectionConfig(0, 1, false),
-				RequestCount:  1,
-				ExpectedCount: 1,
+			{ // excluded node ids being excluded
+				Preferences:    testNodeSelectionConfig(5, 0, false),
+				ExcludeCount:   7,
+				RequestCount:   5,
+				ExpectedCount:  3,
+				ShouldFailWith: &overlay.ErrNotEnoughNodes,
 			},
-			// { // excluded node ids being excluded
-			// 	Preferences:    testNodeSelectionConfig(5, 0, false),
-			// 	ExcludeCount:   7,
-			// 	RequestCount:   5,
-			// 	ExpectedCount:  3,
-			// 	ShouldFailWith: &overlay.ErrNotEnoughNodes,
-			// },
 		} {
 			t.Logf("#%2d. %+v", i, tt)
 			service := planet.Satellites[0].Overlay.Service
