ec07e8b53c35756782a84b58f3991205ad7a24f7
Jennifer Johnson <jennifer@storj.io>
2019-06-24 17:28:41 -0400

    more notes

--- pkg/kademlia/antechamber.go
+++ pkg/kademlia/antechamber.go
@@ -14,4 +14,12 @@ func (rt *RoutingTable) antechamberAddNode(ctx context.Context, node *pb.Node) {
 
 func (rt *RoutingTable) antechamberRemoveNode(ctx context.Context, node *pb.Node) {} //removes a node from the antechamber. Called when node moves into RT or node is outside neighborhood
 
-func (rt *RoutingTable) antechamberFindNear(ctx context.Context, target storj.NodeID, limit int) {}
+func (rt *RoutingTable) antechamberFindNear(ctx context.Context, target storj.NodeID, limit int) (_ []*pb.Node, err error) {
+	closestNodes := make([]*pb.Node, 0, limit+1)
+	return closestNodes, nil
+}
+
+//checks whether the node in question has a valid voucher. If true, continue attempt to add to Rt. If false, call antechamberAddNode
+func (rt *RoutingTable) hasAcceptableVoucher(ctx context.Context, node *pb.Node) bool {
+	return false
+}
