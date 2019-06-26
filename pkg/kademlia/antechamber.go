211b2a0213bd50770ee35109d5c3c2120564ec12
Jennifer Johnson <jennifer@storj.io>
2019-06-25 22:38:05 -0400

    implements methods, stubs tests

--- pkg/kademlia/antechamber.go
+++ pkg/kademlia/antechamber.go
@@ -6,20 +6,103 @@ package kademlia
 import (
 	"context"
 
+	"github.com/gogo/protobuf/proto"
+
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/storj"
+	"storj.io/storj/storage"
 )
 
-func (rt *RoutingTable) antechamberAddNode(ctx context.Context, node *pb.Node) {} //attempts to add a node the antechamber. Only allowed in if within neighborhood
-
-func (rt *RoutingTable) antechamberRemoveNode(ctx context.Context, node *pb.Node) {} //removes a node from the antechamber. Called when node moves into RT or node is outside neighborhood
-
-func (rt *RoutingTable) antechamberFindNear(ctx context.Context, target storj.NodeID, limit int) (_ []*pb.Node, err error) {
-	closestNodes := make([]*pb.Node, 0, limit+1)
-	return closestNodes, nil
+// Attempts to add a node the antechamber. Only allowed in if within rt neighborhood.
+func (rt *RoutingTable) antechamberAddNode(ctx context.Context, node *pb.Node) error {
+	defer mon.Task()(&ctx)(&err)
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+	inNeighborhood, err := rt.wouldBeInNearestK(ctx, node.Id)
+	if err != nil {
+		return RoutingErr.New("could not check node neighborhood: %s", err)
+	}
+	if inNeighborhood {
+		v, err := proto.Marshal(node)
+		if err != nil {
+			return RoutingErr.New("could not marshall node: %s", err)
+		}
+		err = rt.antechamber.Put(ctx, node.Id.Bytes(), v)
+		if err != nil {
+			return RoutingErr.New("could not add key value pair to antechamber: %s", err)
+		}
+	}
+	return nil
 }
 
-//checks whether the node in question has a valid voucher. If true, continue attempt to add to Rt. If false, call antechamberAddNode
+
+// Removes a node from the antechamber.
+// Called when node moves into RT, node is outside neighborhood (check when any node is added to RT), or node failed contact
+func (rt *RoutingTable) antechamberRemoveNode(ctx context.Context, node *pb.Node) error {
+	defer mon.Task()(&ctx)(&err)
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+	err = rt.antechamber.Delete(ctx, node.Id.Bytes())
+	if err != nil {
+		return RoutingErr.New("could not delete node %s", err)
+	}
+	return nil
+}
+
+// Called in conjunction with RT FindNear in some circumstances
+func (rt *RoutingTable) antechamberFindNear(ctx context.Context, target storj.NodeID, limit int) (_ []*pb.Node, err error) {
+	defer mon.Task()(&ctx)(&err)
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+	closestNodes := make([]*pb.Node, 0, limit+1)
+	err = rt.iterateAntechamber(ctx, storj.NodeID{}, func(ctx context.Context, newID storj.NodeID, protoNode []byte) error {
+		newPos := len(closestNodes)
+		for ; newPos > 0 && compareByXor(closestNodes[newPos-1].Id, newID, target) > 0; newPos-- {
+		}
+		if newPos != limit {
+			newNode := pb.Node{}
+			err := proto.Unmarshal(protoNode, &newNode)
+			if err != nil {
+				return err
+			}
+			closestNodes = append(closestNodes, &newNode)
+			if newPos != len(closestNodes) { //reorder
+				copy(closestNodes[newPos+1:], closestNodes[newPos:])
+				closestNodes[newPos] = &newNode
+				if len(closestNodes) > limit {
+					closestNodes = closestNodes[:limit]
+				}
+			}
+		}
+		return nil
+	})
+	return closestNodes, Error.Wrap(err)
+}
+
+// checks whether the node in question has a valid voucher.
 func (rt *RoutingTable) hasAcceptableVoucher(ctx context.Context, node *pb.Node) bool {
+	// TODO
+	// If true, call addNode
+	// If false, call antechamberAddNode
 	return false
+}
+
+func (rt *RoutingTable) iterateAntechamber(ctx context.Context, start storj.NodeID, f func(context.Context, storj.NodeID, []byte) error) (err error) {
+	defer mon.Task()(&ctx)(&err)
+	return rt.antechamber.Iterate(ctx, storage.IterateOptions{First: storage.Key(start.Bytes()), Recurse: true},
+		func(ctx context.Context, it storage.Iterator) error {
+			var item storage.ListItem
+			for it.Next(ctx, &item) {
+				nodeID, err := storj.NodeIDFromBytes(item.Key)
+				if err != nil {
+					return err
+				}
+				err = f(ctx, nodeID, item.Value)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	)
 }
---
+++ pkg/kademlia/antechamber_test.go
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package kademlia
+
+import "testing"
+
+func TestAntechamberAddNode(t *testing.T){}
+
+func TestAntechamberRemoveNode(t *testing.T){}
+
+func TestAntechamberFindNear(t *testing.T){}
+
+func TestHasAcceptableVoucher(t *testing.T){
+	//TODO
+}
