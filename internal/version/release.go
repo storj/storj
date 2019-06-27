ab7f561e64013aea715040879d38e83b510c1320
JT Olio <hello@jtolio.com>
2019-06-27 13:06:44 -0600

    release v0.14.4
    Change-Id: Ia2f0506162f170eda5c61691da08c628f0e43cd6

---
+++ internal/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1561662403"
+	buildCommitHash = "d333a6015c46c05251f1064fcf1c6b373fc1a2f3"
+	buildVersion = "v0.14.4"
+	buildRelease = "true"
+}
