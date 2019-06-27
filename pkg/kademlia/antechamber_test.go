0a10656726660a306caf473fdb986bda8080fb96
Jennifer Johnson <jennifer@storj.io>
2019-06-27 16:57:32 -0400

    starts tests

--- pkg/kademlia/antechamber_test.go
+++ pkg/kademlia/antechamber_test.go
@@ -3,14 +3,44 @@
 
 package kademlia
 
-import "testing"
+import (
+	"testing"
 
-func TestAntechamberAddNode(t *testing.T){}
+	"storj.io/storj/internal/testcontext"
+	"storj.io/storj/internal/teststorj"
+)
 
-func TestAntechamberRemoveNode(t *testing.T){}
+func TestAntechamberAddNode(t *testing.T){
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+	rt := createRoutingTable(ctx, teststorj.NodeIDFromString("OO"))
+	defer ctx.Check(rt.Close)
+	// 1. not in neighborhood
+	// 2. in neighborhood
+}
 
-func TestAntechamberFindNear(t *testing.T){}
+func TestAntechamberRemoveNode(t *testing.T){
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+	rt := createRoutingTable(ctx, teststorj.NodeIDFromString("OO"))
+	defer ctx.Check(rt.Close)
+	// remove node, check if gone
+}
+
+func TestAntechamberFindNear(t *testing.T){
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+	rt := createRoutingTable(ctx, teststorj.NodeIDFromString("OO"))
+	defer ctx.Check(rt.Close)
+	// add 5 nodes
+	// select 3 closest
+}
 
 func TestHasAcceptableVoucher(t *testing.T){
-	//TODO
+	// no vouchers
+	// no vouchers have a matching satellite id
+	// node id doesn't match
+	// voucher is expired
+	// signature is unverified
+	// one voucher doesn't match and another does
 }
