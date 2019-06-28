7bc255ec540b2530bfdbe9615132a69da0b6aa02
Moby <mobyvb@gmail.com>
2019-06-28 10:56:07 -0400

    fix

--- satellite/metainfo/metainfo_test.go
+++ satellite/metainfo/metainfo_test.go
@@ -234,7 +234,7 @@ func TestServiceList(t *testing.T) {
 	}
 
 	config := planet.Uplinks[0].GetConfig(planet.Satellites[0])
-	metainfo, _, err := testplanet.GetMetainfo(ctx, planet.Uplinks[0].Log.Named("ecclient"), config, planet.Uplinks[0].Identity)
+	metainfo, _, err := testplanet.GetMetainfo(ctx, planet.Uplinks[0].Log.Named("metainfo"), config, planet.Uplinks[0].Identity)
 	require.NoError(t, err)
 
 	type Test struct {
@@ -559,7 +559,7 @@ func TestSetAttribution(t *testing.T) {
 		uplink := planet.Uplinks[0]
 
 		config := uplink.GetConfig(planet.Satellites[0])
-		metainfo, _, err := testplanet.GetMetainfo(ctx, uplink.Log.Named("ecclient"), config, uplink.Identity)
+		metainfo, _, err := testplanet.GetMetainfo(ctx, uplink.Log.Named("metainfo"), config, uplink.Identity)
 		require.NoError(t, err)
 
 		_, err = metainfo.CreateBucket(ctx, "alpha", &storj.Bucket{PathCipher: config.GetEncryptionScheme().Cipher})
