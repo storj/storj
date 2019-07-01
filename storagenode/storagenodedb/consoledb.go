b59750f9a5c500409ffd64d19e0f44ba05079c4d
Yaroslav <yar.vorobiov@gmail.com>
2019-07-01 15:48:49 +0300

    extract get daily bandwith method

--- storagenode/storagenodedb/consoledb.go
+++ storagenode/storagenodedb/consoledb.go
@@ -6,6 +6,7 @@ package storagenodedb
 import (
 	"context"
 	"database/sql"
+	"strings"
 	"time"
 
 	"github.com/zeebo/errs"
@@ -62,89 +63,44 @@ func (db *consoledb) GetSatelliteIDs(ctx context.Context, from, to time.Time) (_
 // sorted in ascending order
 func (db *consoledb) GetDailyTotalBandwidthUsed(ctx context.Context, from, to time.Time) (_ []console.BandwidthUsed, err error) {
 	defer mon.Task()(&ctx)(&err)
-	defer db.locked()()
 
 	since, _ := getDateEdges(from.UTC())
 	_, before := getDateEdges(to.UTC())
 
-	rows, err := db.db.QueryContext(ctx, db.Rebind(`
-		SELECT action, SUM(amount), created_at
-		FROM bandwidth_usage
-		WHERE ? <= created_at AND created_at <= ?
-		GROUP BY DATE(created_at), action
-		ORDER BY created_at ASC`), since, before)
-	if err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		err = errs.Combine(err, rows.Close())
-	}()
-
-	var dates []time.Time
-	dailyBandwidth := make(map[time.Time]*console.BandwidthUsed, 0)
-
-	for rows.Next() {
-		var action int32
-		var amount int64
-		var createdAt time.Time
-
-		err = rows.Scan(&action, &amount, &createdAt)
-		if err != nil {
-			return nil, err
-		}
-
-		from, to := getDateEdges(createdAt)
-
-		bandwidthUsed, ok := dailyBandwidth[from]
-		if !ok {
-			bandwidthUsed = &console.BandwidthUsed{
-				From: from,
-				To:   to,
-			}
-
-			dates = append(dates, from)
-			dailyBandwidth[from] = bandwidthUsed
-		}
-
-		switch pb.PieceAction(action) {
-		case pb.PieceAction_GET:
-			bandwidthUsed.Egress.Usage = amount
-		case pb.PieceAction_GET_AUDIT:
-			bandwidthUsed.Egress.Audit = amount
-		case pb.PieceAction_GET_REPAIR:
-			bandwidthUsed.Egress.Repair = amount
-		case pb.PieceAction_PUT:
-			bandwidthUsed.Ingress.Usage = amount
-		case pb.PieceAction_PUT_REPAIR:
-			bandwidthUsed.Ingress.Repair = amount
-		}
-	}
-
-	var bandwidthUsedList []console.BandwidthUsed
-	for _, date := range dates {
-		bandwidthUsedList = append(bandwidthUsedList, *dailyBandwidth[date])
-	}
-
-	return bandwidthUsedList, nil
+	return db.getDailyBandwidthUsed(ctx,
+		"WHERE ? <= created_at AND created_at <= ?",
+		since, before)
 }
 
 // GetDailyBandwidthUsed returns slice of daily bandwidth usage for provided time range,
 // sorted in ascending order for particular satellite
 func (db *consoledb) GetDailyBandwidthUsed(ctx context.Context, satelliteID storj.NodeID, from, to time.Time) (_ []console.BandwidthUsed, err error) {
 	defer mon.Task()(&ctx)(&err)
-	defer db.locked()()
 
 	since, _ := getDateEdges(from.UTC())
 	_, before := getDateEdges(to.UTC())
 
-	rows, err := db.db.QueryContext(ctx, db.Rebind(`
-		SELECT action, SUM(amount), created_at
-		FROM bandwidth_usage
-		WHERE satellite_id = ?
-		WHERE ? <= created_at AND created_at <= ?
-		GROUP BY DATE(created_at), action
-		ORDER BY created_at ASC`), satelliteID, since, before)
+	return db.getDailyBandwidthUsed(ctx,
+		"WHERE satellite_id = ? AND ? <= created_at AND created_at <= ?",
+		satelliteID, since, before)
+}
+
+// getDailyBandwidthUsed returns slice of grouped by date bandwidth usage
+// sorted in ascending order and applied condition if any
+func (db *consoledb) getDailyBandwidthUsed(ctx context.Context, cond string, args ...interface{}) (_ []console.BandwidthUsed, err error) {
+	defer mon.Task()(&ctx)(&err)
+	defer db.locked()()
+
+	qb := strings.Builder{}
+	qb.WriteString("SELECT action, SUM(amount), created_at ")
+	qb.WriteString("FROM bandwidth_usage ")
+	if cond != "" {
+		qb.WriteString(cond + " ")
+	}
+	qb.WriteString("GROUP BY DATE(created_at), action ")
+	qb.WriteString("ORDER BY created_at ASC")
+
+	rows, err := db.db.QueryContext(ctx, db.Rebind(qb.String()), args)
 	if err != nil {
 		return nil, err
 	}
