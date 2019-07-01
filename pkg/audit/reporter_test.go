d98caab74ed633f41bf761d3685afe6463eb0fad
Kaloyan Raev <kaloyan@storj.io>
2019-07-01 16:02:07 +0300

    No extra audits var in TestReportPendingAudits

--- pkg/audit/reporter_test.go
+++ pkg/audit/reporter_test.go
@@ -21,8 +21,7 @@ func TestReportPendingAudits(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 1, UplinkCount: 0,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
-		audits := planet.Satellites[0].Audit.Service
-		err := audits.Close()
+		err := planet.Satellites[0].Audit.Service.Close()
 		require.NoError(t, err)
 
 		nodeID := planet.StorageNodes[0].ID()
