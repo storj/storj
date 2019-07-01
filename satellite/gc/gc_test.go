dac6a25519c5d3db4d2ce28265966f6ed9b3ac35
Moby <mobyvb@gmail.com>
2019-07-01 10:21:16 -0400

    simplify test

--- satellite/gc/gc_test.go
+++ satellite/gc/gc_test.go
@@ -17,14 +17,12 @@ import (
 	"storj.io/storj/pkg/storage/streams"
 	"storj.io/storj/pkg/storj"
 	"storj.io/storj/satellite"
-	"storj.io/storj/uplink"
 )
 
 // TestGarbageCollection does the following:
+// * Set up a network with one storagenode
 // * Upload two objects
-// * Put one storagenode offline
-// * Delete one object
-// * Put the storagenode back online
+// * Delete one object from the metainfo service on the satellite
 // * Trigger a bloom filter generation
 // * Check that pieces of the deleted object are deleted on the storagenode
 // * Check that pieces of the kept object are not deleted on the storagenode
@@ -32,7 +30,7 @@ func TestGarbageCollection(t *testing.T) {
 	t.Skip("Garbage collection not implemented yet.")
 
 	testplanet.Run(t, testplanet.Config{
-		SatelliteCount: 1, StorageNodeCount: 5, UplinkCount: 1,
+		SatelliteCount: 1, StorageNodeCount: 1, UplinkCount: 1,
 		Reconfigure: testplanet.Reconfigure{
 			Satellite: func(log *zap.Logger, index int, config *satellite.Config) {
 				// TODO see if we should reconfigure anything
@@ -41,12 +39,6 @@ func TestGarbageCollection(t *testing.T) {
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 		satellite := planet.Satellites[0]
 		upl := planet.Uplinks[0]
-		uplConfig := &uplink.RSConfig{
-			MinThreshold:     2,
-			RepairThreshold:  3,
-			SuccessThreshold: 5,
-			MaxThreshold:     5,
-		}
 		targetNode := planet.StorageNodes[0]
 
 		checker := satellite.Repair.Checker
@@ -56,9 +48,9 @@ func TestGarbageCollection(t *testing.T) {
 		testData1 := testrand.Bytes(8 * memory.KiB)
 		testData2 := testrand.Bytes(8 * memory.KiB)
 
-		err := upl.UploadWithConfig(ctx, satellite, uplConfig, "testbucket", "test/path/1", testData1)
+		err := upl.Upload(ctx, satellite, "testbucket", "test/path/1", testData1)
 		require.NoError(t, err)
-		pointerToDelete := getPointer(ctx, t, satellite, upl, "testbucket", "test/path/1")
+		deletedEncPath, pointerToDelete := getPointer(ctx, t, satellite, upl, "testbucket", "test/path/1")
 		var deletedPiece *pb.RemotePiece
 		for _, p := range pointerToDelete.GetRemote().GetRemotePieces() {
 			if p.NodeId == targetNode.ID() {
@@ -69,9 +61,9 @@ func TestGarbageCollection(t *testing.T) {
 		require.NotNil(t, deletedPiece)
 		deletedPieceID := pointerToDelete.GetRemote().RootPieceId.Derive(targetNode.ID())
 
-		err = upl.UploadWithConfig(ctx, satellite, uplConfig, "testbucket", "test/path/2", testData2)
+		err = upl.Upload(ctx, satellite, "testbucket", "test/path/2", testData2)
 		require.NoError(t, err)
-		pointerToKeep := getPointer(ctx, t, satellite, upl, "testbucket", "test/path/2")
+		_, pointerToKeep := getPointer(ctx, t, satellite, upl, "testbucket", "test/path/2")
 		var keptPiece *pb.RemotePiece
 		for _, p := range pointerToKeep.GetRemote().GetRemotePieces() {
 			if p.NodeId == targetNode.ID() {
@@ -82,16 +74,8 @@ func TestGarbageCollection(t *testing.T) {
 		require.NotNil(t, keptPiece)
 		keptPieceID := pointerToKeep.GetRemote().RootPieceId.Derive(targetNode.ID())
 
-		// Take storagenode offline
-		// TODO make sure we avoid potential race condition if overlay marks node as online in background before segment is deleted
-		_, err = satellite.Overlay.Service.UpdateUptime(ctx, targetNode.ID(), false)
-		require.NoError(t, err)
-
-		// Delete object
-		upl.Delete(ctx, satellite, "testbucket", "test/path/1")
-
-		// Bring storagenode back online
-		_, err = satellite.Overlay.Service.UpdateUptime(ctx, targetNode.ID(), true)
+		// Delete object from metainfo service on satellite
+		err = satellite.Metainfo.Service.Delete(ctx, deletedEncPath)
 		require.NoError(t, err)
 
 		// Check that piece of the deleted object is on the storagenode
@@ -115,7 +99,7 @@ func TestGarbageCollection(t *testing.T) {
 	})
 }
 
-func getPointer(ctx *testcontext.Context, t *testing.T, satellite *satellite.Peer, upl *testplanet.Uplink, bucket, path string) *pb.Pointer {
+func getPointer(ctx *testcontext.Context, t *testing.T, satellite *satellite.Peer, upl *testplanet.Uplink, bucket, path string) (encryptedPath string, pointer *pb.Pointer) {
 	projects, err := satellite.DB.Console().Projects().GetAll(ctx)
 	require.NoError(t, err)
 	require.Len(t, projects, 1)
@@ -128,8 +112,8 @@ func getPointer(ctx *testcontext.Context, t *testing.T, satellite *satellite.Pee
 	require.NoError(t, err)
 
 	lastSegPath := storj.JoinPaths(projects[0].ID.String(), "l", encryptedAfterBucket)
-	pointer, err := satellite.Metainfo.Service.Get(ctx, lastSegPath)
+	pointer, err = satellite.Metainfo.Service.Get(ctx, lastSegPath)
 	require.NoError(t, err)
 
-	return pointer
+	return lastSegPath, pointer
 }
