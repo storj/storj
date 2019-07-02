a2ff22096eddb1dee681bc2034ab95db5fa0ba2b
crawter <crawter@hotmail.com>
2019-07-02 15:47:19 +0300

    SNO Dashboard Live reload

--- storagenode/console/consoleserver/server.go
+++ storagenode/console/consoleserver/server.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
@@ -47,7 +48,8 @@ type Server struct {
 	service  *console.Service
 	listener net.Listener
 
-	server http.Server
+	server   http.Server
+	upgrader websocket.Upgrader
 }
 
 // NewServer creates new instance of storagenode console web server
@@ -74,6 +76,8 @@ func NewServer(logger *zap.Logger, config Config, service *console.Service, list
 	server.server = http.Server{
 		Handler: mux,
 	}
+
+	server.upgrader = websocket.Upgrader{}
 
 	return &server
 }
@@ -102,15 +106,127 @@ func (s *Server) Close() error {
 }
 
 // appHandler is web app http handler function
-func (s *Server) appHandler(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, filepath.Join(s.config.StaticDir, "dist", "index.html"))
+func (s *Server) appHandler(writer http.ResponseWriter, request *http.Request) {
+	http.ServeFile(writer, request, filepath.Join(s.config.StaticDir, "dist", "index.html"))
 }
 
 // appHandler is web app http handler function
-func (s *Server) dashboardHandler(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
+func (s *Server) dashboardHandler(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
 	defer mon.Task()(&ctx)(nil)
-	w.Header().Set(contentType, applicationJSON)
+	writer.Header().Set(contentType, applicationJSON)
+
+	var response struct {
+		Data struct {
+			Bandwidth          console.BandwidthInfo   `json:"bandwidth"`
+			DiskSpace          console.DiskSpaceInfo   `json:"diskSpace"`
+			WalletAddress      string                  `json:"walletAddress"`
+			VersionInfo        version.Info            `json:"versionInfo"`
+			IsLastVersion      bool                    `json:"isLastVersion"`
+			Uptime             time.Duration           `json:"uptime"`
+			NodeID             string                  `json:"nodeId"`
+			Satellites         storj.NodeIDList        `json:"satellites"`
+			BandwidthChartData []console.BandwidthUsed `json:"bandwidthChartData"`
+		} `json:"data"`
+		Error string `json:"error,omitempty"`
+	}
+
+	defer func() {
+		err := json.NewEncoder(writer).Encode(&response)
+		if err != nil {
+			s.log.Error(err.Error())
+		}
+	}()
+
+	if request.Method != http.MethodGet {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	satelliteIDParam := request.URL.Query().Get("satelliteId")
+	satelliteID, err := s.parseSatelliteIDParam(satelliteIDParam)
+	if err != nil {
+		s.log.Error("satellite id is not valid", zap.Error(err))
+		response.Error = "satellite id is not valid"
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	satellites, err := s.service.GetSatellites(ctx)
+	if err != nil {
+		s.log.Error("can not get satellites list", zap.Error(err))
+		response.Error = "can not get satellites list"
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// checks if current satellite id is related to current storage node
+	if satelliteID != nil {
+		if err = s.checkSatelliteID(satellites, *satelliteID); err != nil {
+			s.log.Error(err.Error())
+			response.Error = err.Error()
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	space, err := s.getStorage(ctx, satelliteID)
+	if err != nil {
+		s.log.Error("can not get disk space usage", zap.Error(err))
+		response.Error = "can not get disk space usage"
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	usage, err := s.getBandwidth(ctx, satelliteID)
+	if err != nil {
+		s.log.Error("can not get bandwidth usage", zap.Error(err))
+		response.Error = "can not get bandwidth usage"
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	walletAddress := s.service.GetWalletAddress(ctx)
+
+	versionInfo := s.service.GetVersion(ctx)
+
+	err = s.service.CheckVersion(ctx)
+	if err != nil {
+		s.log.Error("can not check latest storage node version", zap.Error(err))
+		response.Error = "can not check latest storage node version"
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	bandwidthChartData, err := s.getBandwidthChartData(ctx, satelliteID)
+	if err != nil {
+		s.log.Error("can not get bandwidth usage for chart", zap.Error(err))
+		response.Error = "can not get bandwidth usage for chart"
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	uptime := s.service.GetUptime(ctx)
+	nodeID := s.service.GetNodeID(ctx)
+
+	response.Data.DiskSpace = *space
+	response.Data.Bandwidth = *usage
+	response.Data.WalletAddress = walletAddress
+	response.Data.VersionInfo = versionInfo
+	response.Data.IsLastVersion = true
+	response.Data.Uptime = uptime
+	response.Data.NodeID = nodeID.String()
+	response.Data.Satellites = satellites
+	response.Data.BandwidthChartData = bandwidthChartData
+
+	writer.WriteHeader(http.StatusOK)
+}
+
+// appHandler is web app http handler function
+func (s *Server) liveReloadHandler(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
+	defer mon.Task()(&ctx)(nil)
+	writer.Header().Set(contentType, applicationJSON)
 
 	var response struct {
 		Data struct {
@@ -127,85 +243,36 @@ func (s *Server) dashboardHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	defer func() {
-		err := json.NewEncoder(w).Encode(&response)
+		err := json.NewEncoder(writer).Encode(&response)
 		if err != nil {
 			s.log.Error(err.Error())
 		}
 	}()
 
-	if req.Method != http.MethodGet {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+	var conn, _ = s.upgrader.Upgrade(writer, request, nil)
 
-	satelliteIDParam := req.URL.Query().Get("satelliteId")
-	satelliteID, err := s.parseSatelliteIDParam(satelliteIDParam)
-	if err != nil {
-		s.log.Error("satellite id is not valid", zap.Error(err))
-		response.Error = "satellite id is not valid"
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	satellites, err := s.service.GetSatellites(ctx)
-	if err != nil {
-		s.log.Error("can not get satellites list", zap.Error(err))
-		response.Error = "can not get satellites list"
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// checks if current satellite id is related to current storage node
-	if satelliteID != nil {
-		if err = s.checkSatelliteID(satellites, *satelliteID); err != nil {
-			s.log.Error(err.Error())
-			response.Error = err.Error()
-			w.WriteHeader(http.StatusBadRequest)
-			return
+	go func(conn *websocket.Conn) {
+		for {
+			_, _, err := conn.ReadMessage()
+			if err != nil {
+				conn.Close()
+			}
 		}
-	}
+	}(conn)
 
-	space, err := s.getStorage(ctx, satelliteID)
-	if err != nil {
-		s.log.Error("can not get disk space usage", zap.Error(err))
-		response.Error = "can not get disk space usage"
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	//go func(conn *websocket.Conn) {
+	//	ch := time.Tick(5 * time.Second)
+	//
+	//	for range ch {
+	//		conn.WriteJSON(myStruct{
+	//			Username:  "mvansickle",
+	//			FirstName: "Michael",
+	//			LastName:  "Van Sickle",
+	//		})
+	//	}
+	//}(conn)
 
-	usage, err := s.getBandwidth(ctx, satelliteID)
-	if err != nil {
-		s.log.Error("can not get bandwidth usage", zap.Error(err))
-		response.Error = "can not get bandwidth usage"
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	walletAddress := s.service.GetWalletAddress(ctx)
-
-	versionInfo := s.service.GetVersion(ctx)
-
-	err = s.service.CheckVersion(ctx)
-	if err != nil {
-		s.log.Error("can not check latest storage node version", zap.Error(err))
-		response.Error = "can not check latest storage node version"
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	uptime := s.service.GetUptime(ctx)
-	nodeID := s.service.GetNodeID(ctx)
-
-	response.Data.DiskSpace = *space
-	response.Data.Bandwidth = *usage
-	response.Data.WalletAddress = walletAddress
-	response.Data.VersionInfo = versionInfo
-	response.Data.IsLastVersion = true
-	response.Data.Uptime = uptime
-	response.Data.NodeID = nodeID.String()
-	response.Data.Satellites = satellites
-
-	w.WriteHeader(http.StatusOK)
+	writer.WriteHeader(http.StatusOK)
 }
 
 func (s *Server) getBandwidth(ctx context.Context, satelliteID *storj.NodeID) (_ *console.BandwidthInfo, err error) {
@@ -214,6 +281,14 @@ func (s *Server) getBandwidth(ctx context.Context, satelliteID *storj.NodeID) (_
 	}
 
 	return s.service.GetUsedBandwidthTotal(ctx)
+}
+
+func (s *Server) getBandwidthChartData(ctx context.Context, satelliteID *storj.NodeID) (_ []console.BandwidthUsed, err error) {
+	if satelliteID != nil {
+		return s.service.GetDailyBandwidthUsed(ctx, *satelliteID, time.Now(), time.Now())
+	}
+
+	return s.service.GetDailyTotalBandwidthUsed(ctx, time.Now(), time.Now())
 }
 
 func (s *Server) getStorage(ctx context.Context, satelliteID *storj.NodeID) (_ *console.DiskSpaceInfo, err error) {
