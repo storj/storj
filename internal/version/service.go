2bc5f0b08b756ee2e91142a467b7c2c8bdb0afae
Stefan Benten <mail@stefan-benten.de>
2019-07-07 13:21:11 +0200

    Reset to general defaults

--- internal/version/service.go
+++ internal/version/service.go
@@ -19,9 +19,9 @@ import (
 
 // Config contains the necessary Information to check the Software Version
 type Config struct {
-	ServerAddress  string        `help:"server address to check its version against" releaseDefault:"https://version.alpha.storj.io"`
-	RequestTimeout time.Duration `help:"Request timeout for version checks" releaseDefault:"0h1m0s"`
-	CheckInterval  time.Duration `help:"Interval to check the version" releaseDefault:"0h15m0s"`
+	ServerAddress  string        `help:"server address to check its version against" default:"https://version.alpha.storj.io"`
+	RequestTimeout time.Duration `help:"Request timeout for version checks" default:"0h1m0s"`
+	CheckInterval  time.Duration `help:"Interval to check the version" default:"0h15m0s"`
 }
 
 // Service contains the information and variables to ensure the Software is up to date
