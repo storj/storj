0c6a74408ef72af8da9c4a93d57744c1d3b63515
ethanadams <ethan@storj.io>
2019-07-10 13:47:22 -0400

    Move int64s to top of struct to resolve alignment issue on ARM (#2521)
    * move int64s to top of struct to resolve alignment issue on ARM
    
    (cherry picked from commit f06aec06fbb0c45a9321c0f20cab8e311dfb6c0f)

--- storagenode/storagenodedb/infodb.go
+++ storagenode/storagenodedb/infodb.go
@@ -44,8 +44,8 @@ func newInfo(path string) (*InfoDB, error) {
 	dbutil.Configure(db, mon)
 
 	infoDb := &InfoDB{db: db}
-	infoDb.pieceinfo = pieceinfo{infoDb, spaceUsed{sync.Once{}, 0}}
-	infoDb.bandwidthdb = bandwidthdb{infoDb, bandwidthUsed{sync.RWMutex{}, time.Time{}, 0}}
+	infoDb.pieceinfo = pieceinfo{InfoDB: infoDb, space: spaceUsed{used: 0, once: sync.Once{}}}
+	infoDb.bandwidthdb = bandwidthdb{InfoDB: infoDb, bandwidth: bandwidthUsed{used: 0, mu: sync.RWMutex{}, usedSince: time.Time{}}}
 
 	return infoDb, nil
 }
@@ -70,8 +70,8 @@ func NewInfoInMemory() (*InfoDB, error) {
 		}))
 
 	infoDb := &InfoDB{db: db}
-	infoDb.pieceinfo = pieceinfo{infoDb, spaceUsed{sync.Once{}, 0}}
-	infoDb.bandwidthdb = bandwidthdb{infoDb, bandwidthUsed{sync.RWMutex{}, time.Time{}, 0}}
+	infoDb.pieceinfo = pieceinfo{InfoDB: infoDb, space: spaceUsed{used: 0, once: sync.Once{}}}
+	infoDb.bandwidthdb = bandwidthdb{InfoDB: infoDb, bandwidth: bandwidthUsed{used: 0, mu: sync.RWMutex{}, usedSince: time.Time{}}}
 
 	return infoDb, nil
 }
