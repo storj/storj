02b5295049ee41ac8af1d758f880918b03dc8aa6
Moby <mobyvb@gmail.com>
2019-07-12 17:13:45 -0400

    add repair queue order test

--- satellite/satellitedb/repairqueue_test.go
+++ satellite/satellitedb/repairqueue_test.go
@@ -5,6 +5,7 @@ package satellitedb_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,6 +13,7 @@ import (
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/satellite"
+	dbx "storj.io/storj/satellite/satellitedb/dbx"
 	"storj.io/storj/satellite/satellitedb/satellitedbtest"
 	"storj.io/storj/storage"
 )
@@ -46,5 +48,51 @@ func TestRepairQueue(t *testing.T) {
 		for _, selectCount := range pathsMap {
 			assert.Equal(t, selectCount, 1)
 		}
+	})
+}
+
+func TestRepairQueueOrder(t *testing.T) {
+	satellitedbtest.Run(t, func(t *testing.T, db satellite.DB) {
+		ctx := testcontext.New(t)
+		defer ctx.Cleanup()
+
+		repairQueue := db.RepairQueue()
+
+		nullPath := "/path/null"
+		recentRepairPath := "/path/recent"
+		oldRepairPath := "/path/old"
+		olderRepairPath := "/path/older"
+
+		for _, path := range []string{oldRepairPath, recentRepairPath, nullPath, olderRepairPath} {
+			injuredSeg := &pb.InjuredSegment{Path: []byte(path)}
+			err := repairQueue.Insert(ctx, injuredSeg)
+			require.NoError(t, err)
+		}
+
+		dbAccess := db.(interface{ TestDBAccess() *dbx.DB }).TestDBAccess()
+		// set recentRepairPath attempted to now, oldRepairPath attempted to 2 hours ago, olderRepairPath to 3 hours ago
+		dbAccess.ExecContext(ctx, dbAccess.Rebind(`UPDATE injuredsegments SET attempted = datetime(?) WHERE path = ?`), time.Now(), recentRepairPath)
+		dbAccess.ExecContext(ctx, dbAccess.Rebind(`UPDATE injuredsegments SET attempted = datetime(?) WHERE path = ?`), time.Now().Add(-2*time.Hour), oldRepairPath)
+		dbAccess.ExecContext(ctx, dbAccess.Rebind(`UPDATE injuredsegments SET attempted = datetime(?) WHERE path = ?`), time.Now().Add(-3*time.Hour), olderRepairPath)
+
+		// path with attempted = null should be selected first
+		injuredSeg, err := repairQueue.Select(ctx)
+		require.NoError(t, err)
+		require.Equal(t, nullPath, string(injuredSeg.Path))
+
+		// path with attempted = 3 hours ago should be selected next
+		injuredSeg, err = repairQueue.Select(ctx)
+		require.NoError(t, err)
+		require.Equal(t, olderRepairPath, string(injuredSeg.Path))
+
+		// path with attempted = 2 hours ago should be selected next
+		injuredSeg, err = repairQueue.Select(ctx)
+		require.NoError(t, err)
+		require.Equal(t, oldRepairPath, string(injuredSeg.Path))
+
+		// queue should be considered "empty" now
+		injuredSeg, err = repairQueue.Select(ctx)
+		require.True(t, storage.ErrEmptyQueue.Has(err))
+		require.Nil(t, injuredSeg)
 	})
 }
