deb9aed3274f69a9545f2f7f2909f328a4caf60e
Jessica Grebenschikov <Jessica.greben1+github@gmail.com>
2019-07-12 10:33:44 -0400

    rm old comment

--- pkg/accounting/tally/tally.go
+++ pkg/accounting/tally/tally.go
@@ -132,8 +132,6 @@ func (t *Service) CalculateAtRestData(ctx context.Context) (latestTally time.Tim
 				// check to make sure there are at least *4* path elements. the first three
 				// are project, segment, and bucket name, but we want to make sure we're talking
 				// about an actual object, and that there's an object name specified
-
-				// handle conditions with buckets with no files
 				if len(pathElements) >= 4 {
 					project, segment, bucketName := pathElements[0], pathElements[1], pathElements[2]
 
