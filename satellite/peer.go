ae7e984e25ba25a5316bd2272aecc9c91e344329
Moby <mobyvb@gmail.com>
2019-07-15 18:44:00 -0400

    fix build error

--- satellite/peer.go
+++ satellite/peer.go
@@ -439,7 +439,7 @@ func New(log *zap.Logger, full *identity.FullIdentity, db DB, config *Config, ve
 		log.Debug("Setting up datarepair")
 		// TODO: simplify argument list somehow
 		peer.Repair.Checker = checker.NewChecker(
-			peer.Metainfo.Service,
+			peer.Metainfoloop.Service,
 			peer.DB.RepairQueue(),
 			peer.Overlay.Service, peer.DB.Irreparable(),
 			0, peer.Log.Named("checker"),
