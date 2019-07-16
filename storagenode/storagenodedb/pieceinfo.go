5596764829c3a909bda525e2c886a3669c5feb3f
Jeff Wendling <leterip@gmail.com>
2019-07-16 16:59:47 -0600

    scripts/atomicalign: check for proper alignment for 64bit atomics
    Change-Id: Ibe14fc31f54fe37794155832325248e893bb10dd

--- storagenode/storagenodedb/pieceinfo.go
+++ storagenode/storagenodedb/pieceinfo.go
@@ -19,6 +19,8 @@ import (
 )
 
 type pieceinfo struct {
+	fixMe byte
+
 	// Moved to top of struct to resolve alignment issue with atomic operations on ARM
 	usedSpace     int64
 	loadSpaceOnce sync.Once
