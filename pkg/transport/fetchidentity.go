51534688da378288a7ffd6ef000bf056a48f1e23
Egon Elbre <egonelbre@gmail.com>
2019-07-17 17:46:34 +0300

    implement fetching peer identity without endpoint

---
+++ pkg/transport/fetchidentity.go
@@ -0,0 +1,101 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package transport
+
+import (
+	"context"
+	"net"
+
+	"github.com/zeebo/errs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"storj.io/storj/pkg/identity"
+	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/peertls"
+)
+
+// TODO: check whether this would be problematic with concurrency or should we thread the authinfo through context?
+type handshakeCapture struct {
+	credentials.TransportCredentials
+	AuthInfo credentials.AuthInfo
+}
+
+// ClientHandshake does the authentication handshake specified by the corresponding
+// authentication protocol on rawConn for clients. It returns the authenticated
+// connection and the corresponding auth information about the connection.
+func (capture *handshakeCapture) ClientHandshake(ctx context.Context, s string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	conn, auth, err := capture.TransportCredentials.ClientHandshake(ctx, s, conn)
+	capture.AuthInfo = auth
+	return conn, auth, err
+}
+
+// ServerHandshake does the authentication handshake for servers. It returns
+// the authenticated connection and the corresponding auth information about
+// the connection.
+func (capture *handshakeCapture) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	conn, auth, err := capture.TransportCredentials.ServerHandshake(conn)
+	capture.AuthInfo = auth
+	return conn, auth, err
+}
+
+// FetchPeerIdentity dials the node and fetches the identity
+func (transport *Transport) FetchPeerIdentity(ctx context.Context, node *pb.Node, opts ...grpc.DialOption) (_ *identity.PeerIdentity, err error) {
+	defer mon.Task()(&ctx, "node: "+node.Id.String()[0:8])(&err)
+
+	if node.Address == nil || node.Address.Address == "" {
+		return nil, Error.New("no address")
+	}
+	tlsConfig := transport.tlsOpts.ClientTLSConfig(node.Id)
+
+	capture := &handshakeCapture{
+		TransportCredentials: credentials.NewTLS(tlsConfig),
+	}
+
+	options := append([]grpc.DialOption{
+		grpc.WithTransportCredentials(capture),
+		grpc.WithBlock(),
+		grpc.FailOnNonTempDialError(true),
+		grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+			conn, err := (&net.Dialer{}).DialContext(ctx, "tcp", addr)
+			if err != nil {
+				return nil, err
+			}
+			return &timeoutConn{conn: conn, timeout: transport.timeouts.Request}, nil
+		}),
+	}, opts...)
+
+	timedCtx, cancel := context.WithTimeout(ctx, transport.timeouts.Dial)
+	defer cancel()
+
+	conn, err := grpc.DialContext(timedCtx, node.GetAddress().Address, options...)
+	if err != nil {
+		if err == context.Canceled {
+			return nil, err
+		}
+		transport.AlertFail(timedCtx, node, err)
+		return nil, Error.Wrap(err)
+	}
+	defer func() {
+		err = errs.Combine(err, conn.Close())
+	}()
+	transport.AlertSuccess(timedCtx, node)
+
+	switch info := capture.AuthInfo.(type) {
+	case credentials.TLSInfo:
+		chain := info.State.PeerCertificates
+		if len(chain)-1 < peertls.CAIndex {
+			return nil, Error.New("invalid certificate chain")
+		}
+
+		pi, err := identity.PeerIdentityFromChain(chain)
+		if err != nil {
+			return nil, err
+		}
+
+		return pi, nil
+	default:
+		return nil, Error.New("unknown capture info %T", capture.AuthInfo)
+	}
+}
