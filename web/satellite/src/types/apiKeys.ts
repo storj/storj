6707442abb5003d2dff17346c911cd41b8660fe7
VitaliiShpital <takur12345@gmail.com>
2019-07-18 16:26:14 +0300

    copyright added

--- web/satellite/src/types/apiKeys.ts
+++ web/satellite/src/types/apiKeys.ts
@@ -1,3 +1,6 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
 // APIKeyInfo describing api key model in the database
 export class ApiKey {
     public id: string;
