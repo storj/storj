3fa6e026f7421d78915dc3f702d4820f592a9678
stefanbenten <mail@stefan-benten.de>
2019-07-25 22:14:58 +0200

    release v0.15.4

---
+++ internal/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1564085696"
+	buildCommitHash = "a9e562a030b9d37e6320ee9594fa794e99810c60"
+	buildVersion = "v0.15.4"
+	buildRelease = "true"
+}
