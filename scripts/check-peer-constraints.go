f32929945632504267b787720f6aff83b4f6b754
Egon Elbre <egonelbre@gmail.com>
2019-07-29 14:58:36 +0300

    cleaner peer constraint checking

--- scripts/check-peer-constraints.go
+++ scripts/check-peer-constraints.go
@@ -10,60 +10,82 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"sort"
 	"strings"
 
 	"golang.org/x/tools/go/packages"
 )
+
+var Libraries = []string{
+	"storj.io/storj/pkg/...",
+	"storj.io/storj/lib/...",
+	"storj.io/storj/uplink/...",
+	"storj.io/storj/mobile/...",
+	"storj.io/storj/storage/...",
+}
+
+var Peers = []string{
+	"storj.io/storj/satellite/...",
+	"storj.io/storj/storagenode/...",
+	"storj.io/storj/bootstrap/...",
+	"storj.io/storj/versioncontrol/...",
+	"storj.io/storj/linksharing/...",
+	"storj.io/storj/cmd/...",
+}
 
 var race = flag.Bool("race", false, "load with race tag")
 
 func main() {
 	flag.Parse()
 
-	var exitcode int
-
-	peers, err := load(
-		"storj.io/storj/satellite/...",
-		"storj.io/storj/storagenode/...",
-		"storj.io/storj/bootstrap/...",
-		"storj.io/storj/versioncontrol/...",
-	)
-	if err != nil {
-		fmt.Printf("failed to load peers: %v\n", err)
-		os.Exit(1)
+	var buildFlags []string
+	if *race {
+		buildFlags = append(buildFlags, "-race")
 	}
 
-	for _, source := range []string{
-		"storj.io/storj/pkg/...",
-		"storj.io/storj/lib/...",
-		"storj.io/storj/uplink/...",
-	} {
-		sources, err := load(source)
-		if err != nil {
-			fmt.Printf("failed to load %q: %v\n", source, err)
-			os.Exit(1)
-		}
+	pkgs, err := packages.Load(&packages.Config{
+		Mode:       packages.LoadImports,
+		Env:        os.Environ(),
+		BuildFlags: buildFlags,
+		Tests:      false,
+	}, "storj.io/storj/...")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load pacakges: %v\n", err)
+		os.Exit(1)
+	}
+	pkgs = flatten(pkgs)
 
-		if links(sources, peers) {
-			exitcode = 1
+	exitcode := 0
+
+	for _, library := range Libraries {
+		source := match(pkgs, library)
+		for _, peer := range Peers {
+			destination := match(pkgs, peer)
+			if links(source, destination) {
+				fmt.Fprintf(os.Stdout, "%q is importing %q\n", library, peer)
+				exitcode = 1
+			}
 		}
 	}
 
 	os.Exit(exitcode)
 }
 
-func load(names ...string) ([]*packages.Package, error) {
-	var buildFlags []string
-	if *race {
-		buildFlags = append(buildFlags, "-race")
+func match(pkgs []*packages.Package, globs ...string) []*packages.Package {
+	for i, glob := range globs {
+		globs[i] = strings.ReplaceAll(glob, "...", ".*")
+	}
+	rx := regexp.MustCompile("^(" + strings.Join(globs, "|") + ")$")
+
+	var rs []*packages.Package
+	for _, pkg := range pkgs {
+		if rx.MatchString(pkg.PkgPath) {
+			rs = append(rs, pkg)
+		}
 	}
 
-	return packages.Load(&packages.Config{
-		Mode:       packages.LoadImports,
-		Env:        os.Environ(),
-		BuildFlags: buildFlags,
-		Tests:      false,
-	}, names...)
+	return rs
 }
 
 func links(source, destination []*packages.Package) bool {
@@ -84,8 +106,7 @@ func links(source, destination []*packages.Package) bool {
 			visited[id] = true
 			if targets[id] {
 				links = true
-				fmt.Printf("import %q\n", pathstr(append(path, pkg, imp)))
-				continue
+				fmt.Printf("# import chain %q\n", pathstr(append(path, pkg, imp)))
 			}
 			visit(imp, append(path, pkg))
 		}
@@ -96,6 +117,33 @@ func links(source, destination []*packages.Package) bool {
 	}
 
 	return links
+}
+
+func flatten(pkgs []*packages.Package) []*packages.Package {
+	var all []*packages.Package
+	visited := map[string]bool{}
+	var visit func(pkg *packages.Package)
+	visit = func(pkg *packages.Package) {
+		if _, visited := visited[pkg.PkgPath]; visited {
+			return
+		}
+		visited[pkg.PkgPath] = true
+		all = append(all, pkg)
+
+		for _, imp := range pkg.Imports {
+			visit(imp)
+		}
+	}
+
+	for _, pkg := range pkgs {
+		visit(pkg)
+	}
+
+	sort.Slice(all, func(i, k int) bool {
+		return all[i].PkgPath < all[k].PkgPath
+	})
+
+	return all
 }
 
 func pathstr(path []*packages.Package) string {
