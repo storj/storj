08ed938b185f289568c39b07892388e30f36c72b
YINGRONG ZHAO <lionzhao0820@gmail.com>
2019-07-30 10:52:31 -0400

    re-organize partners map

--- satellite/rewards/partners.go
+++ satellite/rewards/partners.go
@@ -18,49 +18,48 @@ type Partners map[string]PartnerInfo
 
 // LoadPartnerInfos returns our current Open Source Partners.
 func LoadPartnerInfos() Partners {
-	//TODO: implement actual function call to get partner infos
 	return Partners{
-		"Couchbase": PartnerInfo{
+		"OSPP001": PartnerInfo{
 			Name: "Couchbase",
 			ID:   "OSPP001",
 		},
-		"MongoDB": PartnerInfo{
+		"OSPP002": PartnerInfo{
 			Name: "MongoDB",
 			ID:   "OSPP002",
 		},
-		"FileZilla": PartnerInfo{
+		"OSPP003": PartnerInfo{
 			Name: "FileZilla",
 			ID:   "OSPP003",
 		},
-		"InfluxDB": PartnerInfo{
+		"OSPP004": PartnerInfo{
 			Name: "InfluxDB",
 			ID:   "OSPP004",
 		},
-		"Kafka": PartnerInfo{
+		"OSPP005": PartnerInfo{
 			Name: "Kafka",
 			ID:   "OSPP005",
 		},
-		"Minio": PartnerInfo{
+		"OSPP006": PartnerInfo{
 			Name: "Minio",
 			ID:   "OSPP006",
 		},
-		"Nextcloud": PartnerInfo{
+		"OSPP007": PartnerInfo{
 			Name: "Nextcloud",
 			ID:   "OSPP007",
 		},
-		"MariaDB": PartnerInfo{
+		"OSPP008": PartnerInfo{
 			Name: "MariaDB",
 			ID:   "OSPP008",
 		},
-		"Plesk": PartnerInfo{
+		"OSPP009": PartnerInfo{
 			Name: "Plesk",
 			ID:   "OSPP009",
 		},
-		"Pydio": PartnerInfo{
+		"OSPP010": PartnerInfo{
 			Name: "Pydio",
 			ID:   "OSPP010",
 		},
-		"Zenko": PartnerInfo{
+		"OSPP011": PartnerInfo{
 			Name: "Zenko",
 			ID:   "OSPP011",
 		},
@@ -69,14 +68,18 @@ func LoadPartnerInfos() Partners {
 
 // GeneratePartnerLink returns base64 encoded partner referral link
 func GeneratePartnerLink(offerName string) ([]string, error) {
-	referralInfo := &referralInfo{UserID: "", PartnerID: GetPartnerIDByName(offerName)}
+	pID, err := GetPartnerID(offerName)
+	if err != nil {
+		return nil, errs.Wrap(err)
+	}
+	referralInfo := &referralInfo{UserID: "", PartnerID: pID}
 	refJSON, err := json.Marshal(referralInfo)
 	if err != nil {
-		return nil, err
+		return nil, errs.Wrap(err)
 	}
 
 	domains := getTardigradeDomains()
-	referralLinks := make([]string, len(domians))
+	referralLinks := make([]string, len(domains))
 	encoded := base64.StdEncoding.EncodeToString(refJSON)
 
 	for i, url := range domains {
@@ -86,11 +89,14 @@ func GeneratePartnerLink(offerName string) ([]string, error) {
 	return referralLinks, nil
 }
 
-// GetPartnerIDByName returns a partner id based on its name
-func GetPartnerIDByName(name string) string {
-	partner, ok := LoadPartnerInfos()[name]
-	if !ok {
-		return ""
+// GetPartnerID returns partner ID based on partner name
+func GetPartnerID(partnerName string) (partnerID string, err error) {
+	partners := LoadPartnerInfos()
+	for i := range partners {
+		if partners[i].Name == partnerName {
+			return partners[i].ID, nil
+		}
 	}
-	return partner.ID
+
+	return "", errs.New("partner id not found")
 }
