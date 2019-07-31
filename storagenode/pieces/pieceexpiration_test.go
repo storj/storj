0a682256c77eebe484cb293c4512289cd9eed5ec
paul cannon <paul@thepaul.org>
2019-07-30 20:20:19 -0500

    flesh out piece_expirations tests

--- storagenode/pieces/pieceexpiration_test.go
+++ storagenode/pieces/pieceexpiration_test.go
@@ -4,6 +4,7 @@
 package pieces_test
 
 import (
+	"storj.io/storj/storagenode/pieces"
 	"testing"
 	"time"
 
@@ -26,6 +27,11 @@ func TestPieceExpirationDB(t *testing.T) {
 
 		satelliteID := testrand.NodeID()
 		pieceID := testrand.PieceID()
+		expectedExpireInfo := pieces.ExpiredInfo{
+			SatelliteID: satelliteID,
+			PieceID:     pieceID,
+			InPieceInfo: false,
+		}
 
 		// GetExpired with no matches
 		expiredPieceIDs, err := expireDB.GetExpired(ctx, time.Now(), 1000)
@@ -41,20 +47,45 @@ func TestPieceExpirationDB(t *testing.T) {
 		err = expireDB.DeleteFailed(ctx, satelliteID, pieceID, time.Now())
 		require.NoError(t, err)
 
+		expireAt := time.Now()
+
 		// SetExpiration normal usage
-		err = expireDB.SetExpiration(ctx, satelliteID, pieceID, time.Now())
+		err = expireDB.SetExpiration(ctx, satelliteID, pieceID, expireAt)
 		require.NoError(t, err)
 
 		// SetExpiration duplicate
-		err = expireDB.SetExpiration(ctx, satelliteID, pieceID, time.Now())
+		err = expireDB.SetExpiration(ctx, satelliteID, pieceID, expireAt.Add(time.Hour))
 		require.Error(t, err)
 
 		// GetExpired normal usage
-		expiredPieceIDs, err = expireDB.GetExpired(ctx, time.Now(), 1000)
+		expiredPieceIDs, err = expireDB.GetExpired(ctx, expireAt.Add(time.Microsecond), 1000)
 		require.NoError(t, err)
 		require.Len(t, expiredPieceIDs, 1)
-		assert.Equal(t, expiredPieceIDs[0].SatelliteID, satelliteID)
-		assert.Equal(t, expiredPieceIDs[0].PieceID, pieceID)
-		assert.False(t, expiredPieceIDs[0].InPieceInfo)
+		assert.Equal(t, expiredPieceIDs[0], expectedExpireInfo)
+
+		deleteFailedAt := expireAt.Add(2 * time.Microsecond)
+
+		// DeleteFailed normal usage
+		err = expireDB.DeleteFailed(ctx, satelliteID, pieceID, deleteFailedAt)
+		require.NoError(t, err)
+
+		// GetExpired filters out rows with deletion_failed_at = t
+		expiredPieceIDs, err = expireDB.GetExpired(ctx, deleteFailedAt, 1000)
+		require.NoError(t, err)
+		require.Len(t, expiredPieceIDs, 0)
+		expiredPieceIDs, err = expireDB.GetExpired(ctx, deleteFailedAt.Add(time.Microsecond), 1000)
+		require.NoError(t, err)
+		require.Len(t, expiredPieceIDs, 1)
+		assert.Equal(t, expiredPieceIDs[0], expectedExpireInfo)
+
+		// DeleteExpiration normal usage
+		found, err = expireDB.DeleteExpiration(ctx, satelliteID, pieceID)
+		require.NoError(t, err)
+		require.True(t, found)
+
+		// Should not be there anymore
+		expiredPieceIDs, err = expireDB.GetExpired(ctx, expireAt.Add(365*24*time.Hour), 1000)
+		require.NoError(t, err)
+		require.Len(t, expiredPieceIDs, 0)
 	})
 }
