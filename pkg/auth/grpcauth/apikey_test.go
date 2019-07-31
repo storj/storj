78388b3d1d893d769209aa6b5c0d5379ee19fbde
Egon Elbre <egonelbre@gmail.com>
2019-07-31 13:27:38 +0300

    use grpc.WithRPCCredentials rather than client interceptor

---
+++ pkg/auth/grpcauth/apikey_test.go
@@ -0,0 +1,15 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package grpcauth
+
+import (
+	"testing"
+)
+
+func TestAPIKey(t *testing.T) {
+	// todo implement test
+	// {"", status.Errorf(codes.Unauthenticated, "Invalid API credential")}
+	// {"good key", nil}
+	// {"wrong key", status.Errorf(codes.Unauthenticated, "Invalid API credential")}
+}
