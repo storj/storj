99faa2ecca63293fcadf0dda92ab78e2eda17325
Bohdan Artemenko <artemenkobogdan@gmail.com>
2019-08-05 18:55:02 +0300

    Generic List component implemented.

---
+++ web/satellite/src/components/common/ListComponent.vue
@@ -0,0 +1,35 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+<template>
+    <div>
+        <div v-for="item in dataSet">
+            <component :is="itemComponent" :data="item" v-on:click.native="onItemClick(item)"></component>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component({
+        components: {},
+    })
+
+    export default class ListComponent<T> extends Vue {
+        @Prop({default: ''})
+        private readonly itemComponent: string;
+        @Prop({
+            default: () => {
+                console.error('onItemClick is not reinitialized');
+            }
+        })
+        private readonly onItemClick: Function;
+        @Prop({default: []})
+        private readonly dataSet: T[];
+
+    }
+</script>
+
+<style scoped lang="scss">
+</style>
