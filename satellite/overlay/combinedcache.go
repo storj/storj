dd0951d0326300605ca9252f2e9609facd850bd0
Egon Elbre <egonelbre@gmail.com>
2019-08-06 13:00:40 +0300

    alternate code

--- satellite/overlay/combinedcache.go
+++ satellite/overlay/combinedcache.go
@@ -11,29 +11,28 @@ import (
 	"storj.io/storj/pkg/storj"
 )
 
-type addressInfo struct {
-	address   string
-	lastIP    string
-	transport pb.NodeTransport
-}
-
 // CombinedCache is a simple caching mechanism for overlaycache updates. It
 // provdes methods to help reduce calls to UpdateAddress and UpdateTime, but can
 // be extended for other calls in the future.
 type CombinedCache struct {
 	DB
-	addressLock  sync.RWMutex
-	addressCache map[storj.NodeID]*addressInfo
 
-	keyLock *KeyLock
+	mu   sync.RWMutex
+	info map[storj.NodeID]*cachedNodeInfo
+}
+
+type cachedNodeInfo struct {
+	address   string
+	transport pb.NodeTransport
+
+	lastIP string
 }
 
 // NewCombinedCache instantiates a new CombinedCache
 func NewCombinedCache(db DB) *CombinedCache {
 	return &CombinedCache{
-		DB:           db,
-		addressCache: make(map[storj.NodeID]*addressInfo),
-		keyLock:      NewKeyLock(),
+		DB:   db,
+		info: make(map[storj.NodeID]*cachedNodeInfo),
 	}
 }
 
@@ -42,49 +41,64 @@ func NewCombinedCache(db DB) *CombinedCache {
 // to match the values held in the database; however this code does not
 // guarantee that concurrent UpdateAddress calls will be handled in any
 // particular order.
-func (c *CombinedCache) UpdateAddress(ctx context.Context, info *pb.Node, defaults NodeSelectionConfig) (err error) {
+func (cache *CombinedCache) UpdateAddress(ctx context.Context, info *pb.Node, defaults NodeSelectionConfig) (err error) {
 	// Update internal cache and check if this call requires a db call
-
 	if info == nil {
 		return ErrEmptyNode
 	}
 
-	address := info.Address
-	if address == nil {
-		address = &pb.NodeAddress{}
-	}
-
-	c.addressLock.RLock()
-	cached, ok := c.addressCache[info.Id]
-	c.addressLock.RUnlock()
-
-	if ok &&
-		address.Address == cached.address &&
-		address.Transport == cached.transport &&
-		info.LastIp == cached.lastIP {
-
+	newInfo := newCachedInfo(info)
+	if !cache.update(info.Id, newInfo) {
 		return nil
 	}
 
-	// Acquire lock for this node ID. This prevents a concurrent db update to
-	// this same node ID and guarantees the cache and database stay in sync.
-	// This solution works so long as calls to this code are occurring within a
-	// single process.
-	unlockFunc := c.keyLock.Lock(info.Id)
-	defer unlockFunc()
-
-	err = c.DB.UpdateAddress(ctx, info, defaults)
+	err = cache.DB.UpdateAddress(ctx, info, defaults)
 	if err != nil {
 		return err
 	}
 
-	c.addressLock.Lock()
-	c.addressCache[info.Id] = &addressInfo{
-		address:   address.Address,
-		lastIP:    info.LastIp,
-		transport: address.Transport,
-	}
-	c.addressLock.Unlock()
-
 	return nil
+}
+
+// update updates the cache information
+func (cache *CombinedCache) update(id storj.NodeID, info *cachedNodeInfo) bool {
+	cache.mu.RLock()
+	cached, exists := cache.info[id]
+	cache.mu.RUnlock()
+
+	changed := !exists || !cached.Equal(info)
+	if !changed {
+		return false
+	}
+
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cached, exists = cache.info[id]
+	changed = !exists || !cached.Equal(info)
+	if !changed {
+		return false
+	}
+
+	cache.info[id] = info
+	return true
+}
+
+// newCachedInfo creates cached info from the node
+func newCachedInfo(info *pb.Node) *cachedNodeInfo {
+	address := info.GetAddress()
+	if address == nil {
+		address = &pb.NodeAddress{}
+	}
+
+	return &cachedNodeInfo{
+		address:   address.Address,
+		transport: address.Transport,
+		lastIP:    info.LastIp,
+	}
+}
+
+// Equal compares it with existing value.
+func (cached *cachedNodeInfo) Equal(newInfo *cachedNodeInfo) bool {
+	return *cached == *newInfo
 }
