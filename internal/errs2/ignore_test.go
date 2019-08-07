f3453ac24aa4c40b002bd933aa4aa341ee1c4392
Bryan White <bryanchriswhite@gmail.com>
2019-08-07 14:06:19 +0200

    add grpc errors

--- internal/errs2/ignore_test.go
+++ internal/errs2/ignore_test.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/zeebo/errs"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"storj.io/storj/internal/errs2"
 )
@@ -29,25 +31,35 @@ func TestIsCanceled(t *testing.T) {
 	require.True(t, errs2.IsCanceled(parentErr))
 	require.True(t, errs2.IsCanceled(childErr))
 
+	// grpc errors
+	grpcErr := status.Error(codes.Canceled, context.Canceled.Error())
+
+	require.NotEqual(t, grpcErr, context.Canceled)
+	require.True(t, errs2.IsCanceled(grpcErr))
+
 	// nested errors
 	nestedParentErr := nestedErr.Wrap(parentErr)
 	nestedChildErr := nestedErr.Wrap(childErr)
+	nestedGRPCErr := nestedErr.Wrap(grpcErr)
 
 	require.NotEqual(t, nestedParentErr, context.Canceled)
 	require.NotEqual(t, nestedChildErr, context.Canceled)
+	require.NotEqual(t, nestedGRPCErr, context.Canceled)
 
 	require.True(t, errs2.IsCanceled(nestedParentErr))
+	require.True(t, errs2.IsCanceled(nestedChildErr))
 	require.True(t, errs2.IsCanceled(nestedChildErr))
 
 	// combined errors
 	combinedParentErr := errs.Combine(combinedErr, parentErr)
 	combinedChildErr := errs.Combine(combinedErr, childErr)
+	combinedGRPCErr := errs.Combine(combinedErr, childErr)
 
 	require.NotEqual(t, combinedParentErr, context.Canceled)
 	require.NotEqual(t, combinedChildErr, context.Canceled)
+	require.NotEqual(t, combinedGRPCErr, context.Canceled)
 
 	require.True(t, errs2.IsCanceled(combinedParentErr))
 	require.True(t, errs2.IsCanceled(combinedChildErr))
-
-	// TODO: additional error cases?
+	require.True(t, errs2.IsCanceled(combinedGRPCErr))
 }
