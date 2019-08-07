506cfeeae3ea842f53a75e6ed5a664474046ba9b
crawter <crawter@hotmail.com>
2019-08-07 15:20:26 +0300

    fixed indentation

--- satellite/satellitedb/migrate.go
+++ satellite/satellitedb/migrate.go
@@ -1089,25 +1089,29 @@ func (db *DB) PostgresMigration() *migrate.Migration {
 				Version:     51,
 				Action: migrate.SQL{
 					`ALTER TABLE projects
-					    ADD COLUMN owner_id BYTEA;`,
+					ADD COLUMN owner_id BYTEA;`,
 
 					`UPDATE projects as proj
-						SET owner_id =
-							(SELECT member_id
-								FROM project_members
-									WHERE project_id = proj.id
-										ORDER BY created_at ASC
-											LIMIT 1);`,
+					SET owner_id = (
+						SELECT member_id
+						FROM project_members
+						WHERE project_id = proj.id
+						ORDER BY created_at ASC
+						LIMIT 1
+					);`,
 
 					`DELETE FROM bucket_metainfos
-						WHERE project_id in (
-							SELECT id FROM projects WHERE owner_id is null);`,
+					WHERE project_id in (
+							SELECT id 
+							FROM projects 
+							WHERE owner_id is null
+					);`,
 
 					`DELETE FROM projects
-						WHERE owner_id is null;`,
+					WHERE owner_id is null;`,
 
 					`ALTER TABLE projects
-						ALTER COLUMN owner_id SET NOT NULL;`,
+					ALTER COLUMN owner_id SET NOT NULL;`,
 				},
 			},
 		},
