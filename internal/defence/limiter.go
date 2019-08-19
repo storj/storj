ab3299e03051187e700c6bb2f342b73a1dde8462
crawter <crawter@hotmail.com>
2019-08-19 19:08:34 +0300

    fixed comments

--- internal/defence/limiter.go
+++ internal/defence/limiter.go
@@ -8,139 +8,89 @@ import (
 	"sync"
 	"time"
 
-	"storj.io/storj/internal/sync2"
-
 	"golang.org/x/time/rate"
+
+	"storj.io/storj/internal/sync2"
 )
 
-// Attacker stores information about attacker entity
-type Attacker struct {
+// limited stores information about attacker entity
+type limited struct {
 	limiter *rate.Limiter
 	Expire  time.Time
-	// TODO: could be interface{}
-	key string
-
-	IsBanned bool
-}
-
-func (attacker *Attacker) attack(attemptTime time.Time, banDuration time.Duration) bool {
-	// is not cleared by Clear method of Limiter
-	if attacker.IsBanned && attemptTime.After(attacker.Expire) {
-		attacker.IsBanned = false
-	}
-
-	if attacker.IsBanned {
-		return true
-	}
-
-	isBanned := !attacker.limiter.AllowN(attemptTime, 1)
-
-	if isBanned {
-		attacker.IsBanned = true
-		attacker.Expire = attemptTime.Add(banDuration)
-	}
-
-	return isBanned
 }
 
 // Limiter is used to store and manage list of banned entities
 type Limiter struct {
-	attackers map[string]*Attacker
+	attackers map[string]*limited
 
 	// Attempts defines how many times attacker could perform an operation
-	Attempts int
+	attempts int
 	// AttemptsPeriod defines period in which attempts will count. For example, 5 attempts per minute.
-	AttemptsPeriod time.Duration
-	BanDuration    time.Duration
+	attemptsPeriod time.Duration
+	lockDuration   time.Duration
 
 	mu   sync.Mutex
 	loop *sync2.Cycle
 }
 
 // NewLimiter is a constructor for Limiter
-func NewLimiter(attempts int, attemptsPeriod, banDuration, clearPeriod time.Duration) *Limiter {
+func NewLimiter(attempts int, attemptsPeriod, lockDuration, clearPeriod time.Duration) *Limiter {
 	return &Limiter{
-		attackers:      map[string]*Attacker{},
-		Attempts:       attempts,
-		AttemptsPeriod: attemptsPeriod,
-		BanDuration:    banDuration,
+		attackers:      map[string]*limited{},
+		attempts:       attempts,
+		attemptsPeriod: attemptsPeriod,
+		lockDuration:   lockDuration,
 		loop:           sync2.NewCycle(clearPeriod),
 	}
 }
 
-// Attack is use to add new fail attack
-func (limiter *Limiter) Attack(key string) bool {
+// Limit is use to add new fail attack
+func (limiter *Limiter) Limit(key string) bool {
 	limiter.mu.Lock()
-
 	defer limiter.mu.Unlock()
+
 	now := time.Now()
 
-	// Try to retrieve the
-	if _, found := limiter.attackers[key]; !found {
-		limiter.attackers[key] = &Attacker{
-			key:     key,
-			limiter: rate.NewLimiter(rate.Every(limiter.BanDuration), limiter.Attempts),
-			Expire:  now.Add(limiter.BanDuration),
+	attacker, found := limiter.attackers[key]
+	if !found {
+		attacker = &limited{
+			limiter: rate.NewLimiter(rate.Every(limiter.lockDuration), limiter.attempts),
+			Expire:  now.Add(limiter.lockDuration),
 		}
+		limiter.attackers[key] = attacker
 	}
 
-	limit := limiter.attackers[key]
-
-	return limit.attack(now, limiter.BanDuration)
+	return attacker.limiter.AllowN(now, 1)
 }
 
-// Banned returns the list of banned attackers
-func (limiter *Limiter) Banned() []*Attacker {
-	limiter.mu.Lock()
-
-	defer limiter.mu.Unlock()
-
-	var attackers []*Attacker
-
-	for _, attacker := range limiter.attackers {
-		if attacker.IsBanned {
-			attackers = append(attackers, attacker)
-		}
-	}
-
-	return attackers
-}
-
-// Find can be used to find an attacker by specified key
-func (limiter *Limiter) Find(key string) (Attacker, bool) {
-	limiter.mu.Lock()
-
-	defer limiter.mu.Unlock()
-
-	attacker, ok := limiter.attackers[key]
-	return *attacker, ok
-}
-
-// CleanUp is used to clean all attackers whose ban is expired
-func (limiter *Limiter) CleanUp(ctx context.Context) error {
+// Run is used to clean all attackers whose ban is expired
+func (limiter *Limiter) Run(ctx context.Context) error {
 	return limiter.loop.Run(ctx, func(ctx context.Context) error {
-		limiter.cleanUpCallback()
-		return nil
+		return limiter.cleanUp(ctx, time.Now())
 	})
 }
 
-func (limiter *Limiter) cleanUpCallback() {
+func (limiter *Limiter) cleanUp(ctx context.Context, cleanUpTime time.Time) error {
 	limiter.mu.Lock()
-
 	defer limiter.mu.Unlock()
 
 	for key, limit := range limiter.attackers {
-		if time.Now().After(limit.Expire) {
+		select {
+		case <-ctx.Done():
+			limiter.loop.Close()
+			return ctx.Err()
+		default:
+		}
+
+		if cleanUpTime.After(limit.Expire) {
 			delete(limiter.attackers, key)
 		}
 	}
+
+	return nil
 }
 
 // Close should be used when limiter is no longer needed
 func (limiter *Limiter) Close() {
-	limiter.mu.Lock()
-
-	defer limiter.mu.Unlock()
-
-	limiter.loop.Stop()
+	limiter.loop.Close()
 }
--- internal/defence/limiter_test.go
+++ internal/defence/limiter_test.go
@@ -4,77 +4,181 @@
 package defence
 
 import (
+	"context"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
-	"storj.io/storj/internal/testcontext"
-
 	"github.com/stretchr/testify/assert"
+
+	"storj.io/storj/internal/testcontext"
+)
+
+const (
+	key1           = "email1@example.com"
+	key2           = "email2@example.com"
+	maxAttempts    = 3
+	attemptsPeriod = time.Second
+	banDuration    = time.Second
+	clearPeriod    = time.Second * 3
 )
 
 func TestLimiter(t *testing.T) {
 	ctx := testcontext.New(t)
-	key1 := "email1@example.com"
-	key2 := "email2@example.com"
-	maxAttempts := 3
-	attemptsPeriod := time.Second
-	banDuration := time.Second
-	clearPeriod := time.Second * 3
 
 	var limiter = NewLimiter(maxAttempts, attemptsPeriod, banDuration, clearPeriod)
 
 	t.Run("Testing constructor", func(t *testing.T) {
-		assert.Equal(t, limiter.Attempts, maxAttempts)
-		assert.Equal(t, limiter.AttemptsPeriod, attemptsPeriod)
-		assert.Equal(t, limiter.BanDuration, banDuration)
-		assert.NotNil(t, limiter.Attempts)
+		assert.Equal(t, limiter.attempts, maxAttempts)
+		assert.Equal(t, limiter.attemptsPeriod, attemptsPeriod)
+		assert.Equal(t, limiter.lockDuration, banDuration)
+		assert.NotNil(t, limiter.attempts)
 	})
 
 	t.Run("should not be banned when to attack < attempts during attemptsPeriod", func(t *testing.T) {
+		result := false
+
 		for i := 0; i < 2; i++ {
-			limiter.Attack(key1)
+			result = limiter.Limit(key1)
 		}
 
-		assert.Equal(t, len(limiter.Banned()), 0)
-		attacker, ok := limiter.Find(key1)
-		assert.Equal(t, ok, true)
-		assert.Equal(t, attacker.IsBanned, false)
+		assert.Equal(t, result, true)
 	})
 
 	t.Run("should be banned when attack > attempts when attemptsPeriod exceeded", func(t *testing.T) {
+		result := false
+
 		for i := 0; i <= maxAttempts; i++ {
-			limiter.Attack(key2)
+			result = limiter.Limit(key2)
 		}
 
-		assert.Equal(t, len(limiter.Banned()), 1)
-		attacker, ok := limiter.Find(key2)
-		assert.Equal(t, ok, true)
-		assert.Equal(t, attacker.IsBanned, true)
+		assert.Equal(t, result, false)
 	})
 
 	t.Run("clear works fine", func(t *testing.T) {
 		for i := 0; i <= maxAttempts; i++ {
-			limiter.Attack(key1)
-			limiter.Attack(key2)
+			limiter.Limit(key1)
+			limiter.Limit(key2)
 		}
-
-		assert.Equal(t, len(limiter.Banned()), 2)
 
 		var err error
 
 		go func() {
-			err = limiter.CleanUp(ctx)
+			err = limiter.Run(ctx)
 			assert.NoError(t, err)
 		}()
 
-		ticker := time.NewTicker(clearPeriod)
+		ticker := time.NewTicker(clearPeriod + time.Second)
 
 		for range ticker.C {
-			assert.Equal(t, len(limiter.Banned()), 0)
+			assert.Equal(t, 0, len(limiter.attackers))
 			ticker.Stop()
 			break
 		}
 
 		limiter.Close()
 	})
+}
+
+func TestLimiterConcurrent(t *testing.T) {
+	ctx, cancel := context.WithCancel(testcontext.New(t))
+	var wg sync.WaitGroup
+	limiter := NewLimiter(maxAttempts, attemptsPeriod, banDuration, clearPeriod)
+
+	go func() {
+		err := limiter.Run(ctx)
+		assert.NoError(t, err)
+	}()
+
+	wg.Add(1)
+	go processFirstAttacker(t, &wg, limiter)
+
+	wg.Add(1)
+	go processSecondAttacker(t, &wg, limiter)
+
+	wg.Wait()
+	cancel()
+}
+
+// first attacker performs 3 operation ( with 3 max attempts ) per ~4 seconds ( with attempt duration 1 sec)
+// so he should not be locked
+func processFirstAttacker(t *testing.T, wg *sync.WaitGroup, limiter *Limiter) {
+	defer wg.Done()
+
+	ticker := time.NewTicker(time.Second)
+	i := 0
+	result := false
+
+	for range ticker.C {
+		if i == maxAttempts {
+			ticker.Stop()
+			break
+		}
+
+		result = limiter.Limit(key1)
+
+		i++
+	}
+
+	assert.True(t, result)
+}
+
+// second attacker performs 4 operation ( with 3 max attempts ) per ~0.8 second ( with attempt duration 1 sec)
+// so he should be locked
+func processSecondAttacker(t *testing.T, wg *sync.WaitGroup, limiter *Limiter) {
+	defer wg.Done()
+
+	ticker := time.NewTicker(time.Second / 5)
+	i := 0
+	result := false
+
+	for range ticker.C {
+		if i == 4 {
+			ticker.Stop()
+			break
+		}
+
+		result = limiter.Limit(key1)
+
+		i++
+	}
+
+	assert.False(t, result)
+}
+
+// ExampleLimit shows how to use and close Limiter
+func ExampleLimit() {
+	ctx := context.Background()
+	limiter := NewLimiter(maxAttempts, attemptsPeriod, banDuration, clearPeriod)
+
+	go func() {
+		if err := limiter.Run(ctx); err != nil {
+			fmt.Print(err)
+		}
+	}()
+
+	if !limiter.Limit("someKey") {
+		return
+	}
+
+	limiter.Close()
+}
+
+// ExampleLimit shows how to use and close (with context) Limiter
+func ExampleLimit_second() {
+	ctx, close := context.WithCancel(context.Background())
+	limiter := NewLimiter(maxAttempts, attemptsPeriod, banDuration, clearPeriod)
+
+	go func() {
+		if err := limiter.Run(ctx); err != nil {
+			fmt.Print(err)
+		}
+	}()
+
+	if !limiter.Limit("someKey") {
+		fmt.Print("you are banned")
+	}
+
+	close()
 }
