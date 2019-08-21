243cedb6280aa7f67e62ce972fdea84f4d3bfaa6
Natalie Villasana <nat@storj.io>
2019-08-21 11:49:27 -0400

    satellite/audit: implement reservoir struct and RemoteSegment observer method (#2744)

---
+++ satellite/audit/reservoir.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package audit
+
+import (
+	"math/rand"
+
+	"storj.io/storj/pkg/storj"
+)
+
+const maxReservoirSize = 2
+
+// Reservoir holds a certain number of segments to reflect a random sample
+type Reservoir struct {
+	Paths [maxReservoirSize]storj.Path
+	size  int8
+	index int64
+}
+
+// NewReservoir instantiates a Reservoir
+func NewReservoir(size int) *Reservoir {
+	if size < 1 {
+		size = 1
+	} else if size > maxReservoirSize {
+		size = maxReservoirSize
+	}
+	return &Reservoir{
+		size:  int8(size),
+		index: 0,
+	}
+}
+
+// Sample makes sure that for every segment in metainfo from index i=size..n-1,
+// pick a random number r = rand(0..i), and if r < size, replace reservoir.Segments[r] with segment
+func (reservoir *Reservoir) Sample(r *rand.Rand, path storj.Path) {
+	reservoir.index++
+	if reservoir.index < int64(reservoir.size) {
+		reservoir.Paths[reservoir.index] = path
+	} else {
+		random := r.Int63n(reservoir.index)
+		if random < int64(reservoir.size) {
+			reservoir.Paths[random] = path
+		}
+	}
+}
