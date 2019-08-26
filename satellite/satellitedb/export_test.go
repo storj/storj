e9ff9eebcce224696deaffebce0874513a32a494
Bryan White <bryanchriswhite@gmail.com>
2019-08-26 10:39:01 +0200

    delete unused code

--- satellite/satellitedb/export_test.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2019 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-package satellitedb
-
-type Overlaycache = overlaycache
-type LockedOverlaycache = lockedOverlayCache
