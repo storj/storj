f404aad8789f7a190f17c14e8a3f2cb21efbba8a
Matt Robinson <mrobinson@storj.io>
2019-08-27 16:27:34 -0400

    Add deploy script for storagenodes (#2889)

---
+++ scripts/deploy-storagenode.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Usage: TAG=6e8c4ed-v0.19.0-go1.12.9 scripts/deploy-storagenode.sh
+set -euo pipefail
+
+: "${TAG:?Must be set to the gitish version of the release without architecture}"
+
+for v in alpha arm beta; do
+	docker manifest create storjlabs/storagenode:$v \
+	storjlabs/storagenode:${TAG}-amd64 \
+	storjlabs/storagenode:${TAG}-arm32v6
+
+	docker manifest annotate storjlabs/storagenode:$v \
+	storjlabs/storagenode:${TAG}-amd64 --os linux --arch amd64
+
+	docker manifest annotate storjlabs/storagenode:$v \
+	storjlabs/storagenode:${TAG}-arm32v6 --os linux --arch arm --variant arm32v6
+
+	docker manifest push --purge storjlabs/storagenode:$v
+done
