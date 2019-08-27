2bfd08f0e214556ea29388ac4ff4e035e3ab5dab
Yingrong Zhao <yingrong.zhao@gmail.com>
2019-08-27 14:24:57 -0400

    add a method

--- satellite/marketingweb/server.go
+++ satellite/marketingweb/server.go
@@ -132,9 +132,7 @@ func (s *Server) parseTemplates() (err error) {
 	)
 
 	s.templates.home, err = template.New("home-page").Funcs(template.FuncMap{
-		"BaseURL": func() string {
-			return s.config.BaseURL
-		},
+		"BaseURL":      s.GetBaseURL,
 		"ReferralLink": rewards.GeneratePartnerLink,
 	}).ParseFiles(homeFiles...)
 	if err != nil {
@@ -142,27 +140,21 @@ func (s *Server) parseTemplates() (err error) {
 	}
 
 	s.templates.pageNotFound, err = template.New("page-not-found").Funcs(template.FuncMap{
-		"BaseURL": func() string {
-			return s.config.BaseURL
-		},
+		"BaseURL": s.GetBaseURL,
 	}).ParseFiles(pageNotFoundFiles...)
 	if err != nil {
 		return Error.Wrap(err)
 	}
 
 	s.templates.internalError, err = template.New("internal-server-error").Funcs(template.FuncMap{
-		"BaseURL": func() string {
-			return s.config.BaseURL
-		},
+		"BaseURL": s.GetBaseURL,
 	}).ParseFiles(internalErrorFiles...)
 	if err != nil {
 		return Error.Wrap(err)
 	}
 
 	s.templates.badRequest, err = template.New("bad-request-error").Funcs(template.FuncMap{
-		"BaseURL": func() string {
-			return s.config.BaseURL
-		},
+		"BaseURL": s.GetBaseURL,
 	}).ParseFiles(badRequestFiles...)
 	if err != nil {
 		return Error.Wrap(err)
@@ -221,6 +213,10 @@ func (s *Server) StopOffer(w http.ResponseWriter, req *http.Request) {
 	}
 
 	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func (s *Server) GetBaseURL() string {
+	return s.config.BaseURL
 }
 
 // serveNotFound handles 404 errors and defaults to 500 if template parsing fails.
