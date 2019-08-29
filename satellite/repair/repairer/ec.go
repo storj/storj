faa61409e880316aa050d426e864212b41ef5e0d
Moby von Briesen <mobyvb@gmail.com>
2019-08-29 18:35:31 -0400

    add description of process in TODO comments

--- satellite/repair/repairer/ec.go
+++ satellite/repair/repairer/ec.go
@@ -29,6 +29,25 @@ func NewECRepairer() *ECRepairer {
 func (*ECRepairer) Get(ctx context.Context, limits []*pb.AddressedOrderLimit, privateKey storj.PiecePrivateKey, es eestream.ErasureScheme, size int64) (data io.ReadCloser, err error) {
 	defer mon.Task()(&ctx)(&err)
 
+	// TODO (async) start a number of workers equal to the minimum number of required pieces
+	// TODO for each worker, select an unused order limit and attempt to run downloadAndVerifyPiece
+	// TODO if there is an error, select another unused order limit and re-attempt
+	// TODO if at any point there is a failure and there are no more unused order limits, cancel all workers and return an error
+
+	// TODO wait until context canceled, timeout expired, or we have downloaded and verified the minimum number of pieces
+	// TODO decode pieces into a segment and return
+
+	return nil, nil
+}
+
+// downloadAndVerifyPiece downloads a piece from a storagenode,
+// expects the original order limit to have the correct piece public key,
+// and expects the hash of the data to match the signed hash provided by the storagenode.
+func (*ECRepairer) downloadAndVerifyPiece() (data io.ReadCloser, err error) {
+	// TODO download piece and calculate hash (stop early if ctx canceled)
+	// TODO get order limit and hash
+	// TODO verify order limit validity
+	// TODO verify hash validity
 	return nil, nil
 }
 
@@ -36,6 +55,8 @@ func (*ECRepairer) Get(ctx context.Context, limits []*pb.AddressedOrderLimit, pr
 // and uploads the pieces in need of repair to new nodes provided by order limits.
 func (ec *ECRepairer) Repair(ctx context.Context, limits []*pb.AddressedOrderLimit, privateKey storj.PiecePrivateKey, rs eestream.RedundancyStrategy, data io.Reader, expiration time.Time, timeout time.Duration, path storj.Path) (successfulNodes []*pb.Node, successfulHashes []*pb.PieceHash, err error) {
 	defer mon.Task()(&ctx)(&err)
+
+	// TODO should be almost identical to ecclient Repair (remove ecclient Repair once implemented here)
 
 	return nil, nil, nil
 }
