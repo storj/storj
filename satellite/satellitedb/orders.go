a380ebe9fde17a2b5aa437e2c47e46664779c594
ethanadams <ethan@storj.io>
2019-08-29 10:29:26 -0400

    Update satellite/satellitedb/orders.go
    Co-Authored-By: Ivan Fraixedes <ivan@fraixed.es>

--- satellite/satellitedb/orders.go
+++ satellite/satellitedb/orders.go
@@ -267,7 +267,7 @@ func (db *ordersDB) ProcessOrders(ctx context.Context, requests []*orders.Proces
 	// processed orders are rejected
 	for _, request := range requests {
 		// avoid the PG error "current transaction is aborted, commands ignored until end of transaction block" if the below insert fails due any constraint.
-		// aee https://www.postgresql.org/message-id/13131805-BCBB-42DF-953B-27EE36AAF213%40yahoo.com
+		// see https://www.postgresql.org/message-id/13131805-BCBB-42DF-953B-27EE36AAF213%40yahoo.com
 		_, err = tx.Exec("savepoint sp")
 		if err != nil {
 			return nil, err
