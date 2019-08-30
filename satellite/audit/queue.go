f87474717f3ae4e76da2dd8c0431c64f22d860f1
Moby von Briesen <mobyvb@gmail.com>
2019-08-30 10:19:17 -0400

    lock mutex before checking len

--- satellite/audit/queue.go
+++ satellite/audit/queue.go
@@ -23,19 +23,20 @@ type queue struct {
 // swap switches the backing queue slice with a new queue slice.
 func (q *queue) swap(newQueue []storj.Path) {
 	q.mu.Lock()
+	defer q.mu.Unlock()
+
 	q.queue = newQueue
-	q.mu.Unlock()
 }
 
 // next gets the next item in the queue.
 func (q *queue) next() (storj.Path, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
 	// return error if queue is empty
 	if len(q.queue) == 0 {
 		return "", ErrEmptyQueue.New("")
 	}
-
-	q.mu.Lock()
-	defer q.mu.Unlock()
 
 	next := q.queue[0]
 	q.queue = q.queue[1:]
