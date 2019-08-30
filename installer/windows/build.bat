34643fc2a9c3f45ae70288a73b798546f5bf62c6
Kaloyan Raev <kaloyan@storj.io>
2019-08-30 11:55:44 +0300

    Config dialog to collect operator wallet and email from user input

--- installer/windows/build.bat
+++ installer/windows/build.bat
@@ -1,3 +1,3 @@
-candle.exe -arch x64 StorageNode.wxs CustomInstallDir.wxs Config.wxs
+candle.exe -arch x64 StorageNode.wxs CustomInstallDir.wxs OperatorConfig.wxs
 
-light.exe -o storagenode.msi -ext WixUIExtension StorageNode.wixobj CustomInstallDir.wixobj Config.wixobj
+light.exe -o storagenode.msi -ext WixUIExtension StorageNode.wixobj CustomInstallDir.wixobj OperatorConfig.wixobj
