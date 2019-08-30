9c5efa6fdcc1af9e517ce0ef6c1304b8ae636c59
crawter <crawter@hotmail.com>
2019-08-30 10:17:20 +0300

    button spec linter fixed

--- web/satellite/tests/unit/common/Button.spec.ts
+++ web/satellite/tests/unit/common/Button.spec.ts
@@ -10,7 +10,7 @@ describe('Button.vue', () => {
     it('renders correctly', () => {
         const wrapper = shallowMount(Button, {
             propsData: {
-                onPress: () => { return },
+                onPress: () => { return; },
             },
         });
 
@@ -21,7 +21,7 @@ describe('Button.vue', () => {
         const wrapper = shallowMount(Button, {
             propsData: {
                 isWhite: true,
-                onPress: () => { return },
+                onPress: () => { return; },
             }
         });
 
@@ -32,7 +32,7 @@ describe('Button.vue', () => {
         const wrapper = shallowMount(Button, {
             propsData: {
                 isDisabled: true,
-                onPress: () => { return },
+                onPress: () => { return; },
             }
         });
 
@@ -49,7 +49,7 @@ describe('Button.vue', () => {
                 label,
                 width,
                 height,
-                onPress: () => { return },
+                onPress: () => { return; },
             },
         });
 
@@ -61,7 +61,7 @@ describe('Button.vue', () => {
     it('renders correctly with default props', () => {
         const wrapper = shallowMount(Button, {
             propsData: {
-                onPress: () => { return },
+                onPress: () => { return; },
             },
         });
 
