2fecc2121b58ce0cb9f5ddb5c481c77f2493dcdf
Kaloyan Raev <kaloyan@storj.io>
2019-08-30 18:35:06 +0300

    Dialog for selecting identity dir

--- installer/windows/build.bat
+++ installer/windows/build.bat
@@ -1,3 +1,3 @@
-candle.exe -arch x64 Product.wxs CustomInstallDir.wxs OperatorConfig.wxs
+candle.exe -arch x64 Product.wxs CustomInstallDir.wxs IdentityDir.wxs OperatorConfig.wxs
 
-light.exe -o storagenode.msi -ext WixUIExtension Product.wixobj CustomInstallDir.wixobj OperatorConfig.wixobj
+light.exe -o storagenode.msi -ext WixUIExtension Product.wixobj CustomInstallDir.wixobj IdentityDir.wixobj  OperatorConfig.wixobj
