93d6daab5a89397d30a6f334b4f0c0fc9ea59401
Natalie Ventura Villasana <nat@storj.io>
2019-09-05 10:03:15 -0400

    simplify part of TestQueue

--- satellite/audit/queue_test.go
+++ satellite/audit/queue_test.go
@@ -31,17 +31,11 @@ func TestQueue(t *testing.T) {
 	testQueue2 := []storj.Path{"0", "1", "2"}
 	q.Swap(testQueue2)
 
-	path, err = q.Next()
-	require.NoError(t, err)
-	require.EqualValues(t, testQueue2[0], path)
-
-	path, err = q.Next()
-	require.NoError(t, err)
-	require.EqualValues(t, testQueue2[1], path)
-
-	path, err = q.Next()
-	require.NoError(t, err)
-	require.EqualValues(t, testQueue2[2], path)
+	for _, expected := range testQueue2 {
+		path, err := q.Next()
+		require.NoError(t, err)
+		require.EqualValues(t, expected, path)
+	}
 
 	_, err = q.Next()
 	require.True(t, audit.ErrEmptyQueue.Has(err), "required ErrEmptyQueue error")
