f4c207ebb39b4d4be199c43ee29662773b85ba00
Natalie Ventura Villasana <nat@storj.io>
2019-09-04 20:57:23 -0400

    update Worker to function more like service 1.0

--- satellite/audit/worker.go
+++ satellite/audit/worker.go
@@ -6,28 +6,38 @@ package audit
 import (
 	"context"
 
+	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 
 	"storj.io/storj/internal/sync2"
+	"storj.io/storj/pkg/identity"
 	"storj.io/storj/pkg/storj"
+	"storj.io/storj/pkg/transport"
+	"storj.io/storj/satellite/metainfo"
+	"storj.io/storj/satellite/orders"
+	"storj.io/storj/satellite/overlay"
 )
 
 // Worker contains information for populating audit queue and processing audits.
 type Worker struct {
 	log      *zap.Logger
 	queue    *Queue
-	verifier *Verifier
+	Verifier *Verifier
+	Reporter reporter
 	Loop     sync2.Cycle
 	limiter  sync2.Limiter
 }
 
 // NewWorker instantiates Worker.
-func NewWorker(log *zap.Logger, queue *Queue, verifier Verifier, config Config) (*Worker, error) {
+func NewWorker(log *zap.Logger, queue *Queue, metainfo *metainfo.Service,
+	orders *orders.Service, transport transport.Client, overlay *overlay.Service,
+	containment Containment, identity *identity.FullIdentity, config Config) (*Worker, error) {
 	return &Worker{
 		log: log,
 
 		queue:    queue,
-		verifier: verifier,
+		Verifier: NewVerifier(log.Named("audit:verifier"), metainfo, transport, overlay, containment, orders, identity, config.MinBytesPerSecond, config.MinDownloadTimeout),
+		Reporter: NewReporter(log.Named("audit:reporter"), overlay, containment, config.MaxRetriesStatDB, int32(config.MaxReverifyCount)),
 		Loop:     *sync2.NewCycle(config.QueueInterval),
 		limiter:  *sync2.NewLimiter(config.WorkerConcurrency),
 	}, nil
@@ -80,7 +90,39 @@ func (worker *Worker) process(ctx context.Context) (err error) {
 }
 
 func (worker *Worker) work(ctx context.Context, path storj.Path) error {
-	// TODO: handle work.
-	// worker.verifier.Verify2(ctx, path, ...)
-	return nil
+	var errlist errs.Group
+	report, err := worker.Verifier.Reverify2(ctx, path)
+	if err != nil {
+		errlist.Add(err)
+	}
+
+	// TODO(moby) we need to decide if we want to do something with nodes that the reporter failed to update
+	_, err = worker.Reporter.RecordAudits(ctx, report)
+	if err != nil {
+		errlist.Add(err)
+	}
+
+	// Skip all reverified nodes in the next Verify step.
+	skip := make(map[storj.NodeID]bool)
+	if report != nil {
+		for _, nodeID := range report.Successes {
+			skip[nodeID] = true
+		}
+		for _, nodeID := range report.Offlines {
+			skip[nodeID] = true
+		}
+		for _, nodeID := range report.Fails {
+			skip[nodeID] = true
+		}
+		for _, pending := range report.PendingAudits {
+			skip[pending.NodeID] = true
+		}
+	}
+
+	report, err = worker.Verifier.Verify2(ctx, path, skip)
+	if err != nil {
+		errlist.Add(err)
+	}
+
+	return errlist.Err()
 }
