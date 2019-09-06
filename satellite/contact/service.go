2bfa68845c3e0a48b12e2de7e2a00bd180c757bd
Jennifer Johnson <jennifer@storj.io>
2019-09-05 22:21:43 -0400

    fetch info

--- satellite/contact/service.go
+++ satellite/contact/service.go
@@ -4,10 +4,14 @@
 package contact
 
 import (
+	"context"
+
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
 	"gopkg.in/spacemonkeygo/monkit.v2"
 
+	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/transport"
 	"storj.io/storj/satellite/overlay"
 )
@@ -45,4 +49,37 @@ func NewService(log *zap.Logger, self overlay.NodeDossier, overlay *overlay.Serv
 // Local returns the satellite node dossier
 func (service *Service) Local() overlay.NodeDossier {
 	return service.self
+}
+
+// FetchInfo connects to a node and returns its node info.
+func (service *Service) FetchInfo(ctx context.Context, target pb.Node) (_ *pb.InfoResponse, err error) {
+	conn, err := service.dialNode(ctx, target)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := conn.client.RequestInfo(ctx, &pb.InfoRequest{})
+
+	return resp, errs.Combine(err, conn.disconnect())
+}
+
+// dialNode dials the specified node.
+func (service *Service) dialNode(ctx context.Context, target pb.Node) (_ *Conn, err error) {
+	defer mon.Task()(&ctx)(&err)
+	grpcconn, err := service.transport.DialNode(ctx, &target)
+	return &Conn{
+		conn:   grpcconn,
+		client: pb.NewNodeClient(grpcconn),
+	}, err
+}
+
+// Conn represents a connection
+type Conn struct {
+	conn   *grpc.ClientConn
+	client pb.NodesClient
+}
+
+// disconnect disconnects this connection.
+func (conn *Conn) disconnect() error {
+	return conn.conn.Close()
 }
