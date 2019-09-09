dd7a5560a0dd2dcb3b1b4f7a949c8be5bc8a1fbf
Egon Elbre <egonelbre@gmail.com>
2019-09-09 11:03:51 +0300

    remove testing code

--- scripts/test-sim-backwards.sh
+++ scripts/test-sim-backwards.sh
@@ -50,12 +50,21 @@ PATH=$RELEASE_DIR/bin:$PATH storj-sim -x --host $STORJ_NETWORK_HOST4 network tes
 # this replaces anywhere that has "/release/" in the config file, which currently just renames the static dir paths
 sed -i -e 's#/release/#/branch/#g' `storj-sim network env SATELLITE_0_DIR`/config.yaml
 
-# keep half of the storage nodes on the old version
-ln $BRANCH_DIR/bin/panick `storj-sim network env STORAGENODE_0_DIR`/storagenode
-ln $BRANCH_DIR/bin/panick `storj-sim network env STORAGENODE_1_DIR`/storagenode
-ln $BRANCH_DIR/bin/panick `storj-sim network env STORAGENODE_2_DIR`/storagenode
-ln $BRANCH_DIR/bin/panick `storj-sim network env STORAGENODE_3_DIR`/storagenode
-ln $BRANCH_DIR/bin/panick `storj-sim network env STORAGENODE_4_DIR`/storagenode
+## Ensure that partially upgraded network works
 
+# keep half of the storage nodes on the old version
+ln $RELEASE_DIR/bin/storagenode `storj-sim network env STORAGENODE_0_DIR`/storagenode
+ln $RELEASE_DIR/bin/storagenode `storj-sim network env STORAGENODE_1_DIR`/storagenode
+ln $RELEASE_DIR/bin/storagenode `storj-sim network env STORAGENODE_2_DIR`/storagenode
+ln $RELEASE_DIR/bin/storagenode `storj-sim network env STORAGENODE_3_DIR`/storagenode
+ln $RELEASE_DIR/bin/storagenode `storj-sim network env STORAGENODE_4_DIR`/storagenode
+
+# run download part of backward compatibility tests from the current branch, using new uplink
+PATH=$BRANCH_DIR/bin:$PATH storj-sim -x --host $STORJ_NETWORK_HOST4 network test bash "$SCRIPTDIR"/test-backwards.sh download
+
+## Ensure that old uplink works
+
+# overwrite new uplink with release branch and test the download
+cp $RELEASE_BRANCH/bin/uplink cp $BRANCH_DIR/bin/uplink
 # run download part of backward compatibility tests from the current branch
 PATH=$BRANCH_DIR/bin:$PATH storj-sim -x --host $STORJ_NETWORK_HOST4 network test bash "$SCRIPTDIR"/test-backwards.sh download
