6d70b74d899331c0e4eb1a14102c699208721177
Jennifer Johnson <jennifer@storj.io>
2019-09-11 17:18:57 -0400

    removes fetchinfo on sn

--- storagenode/contact/service.go
+++ storagenode/contact/service.go
@@ -4,17 +4,13 @@
 package contact
 
 import (
-	"context"
 	"sync"
 	"time"
 
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
 	"gopkg.in/spacemonkeygo/monkit.v2"
 
-	"storj.io/storj/pkg/pb"
-	"storj.io/storj/pkg/transport"
 	"storj.io/storj/satellite/overlay"
 )
 
@@ -33,29 +29,20 @@ type Config struct {
 	MaxSleep time.Duration `help:"maximum duration to wait before pinging satellites" releaseDefault:"45m" devDefault:"0s" hidden:"true"`
 }
 
-// Conn represents a connection
-type Conn struct {
-	conn   *grpc.ClientConn
-	client pb.ContactClient
-}
-
 // Service is the contact service between storage nodes and satellites
 type Service struct {
 	log *zap.Logger
 
 	mutex *sync.Mutex
 	self  *overlay.NodeDossier
-
-	transport transport.Client
 }
 
 // NewService creates a new contact service
-func NewService(log *zap.Logger, self *overlay.NodeDossier, transport transport.Client) *Service {
+func NewService(log *zap.Logger, self *overlay.NodeDossier) *Service {
 	return &Service{
-		log:       log,
-		mutex:     &sync.Mutex{},
-		self:      self,
-		transport: transport,
+		log:   log,
+		mutex: &sync.Mutex{},
+		self:  self,
 	}
 }
 
@@ -64,31 +51,4 @@ func (service *Service) Local() overlay.NodeDossier {
 	service.mutex.Lock()
 	defer service.mutex.Unlock()
 	return *service.self
-}
-
-// FetchInfo connects to a node and returns its node info.
-func (service *Service) FetchInfo(ctx context.Context, target pb.Node) (_ *pb.InfoResponse, err error) {
-	conn, err := service.dialNode(ctx, target)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := conn.client.RequestInf(ctx, &pb.InfoRequest{})
-
-	return resp, errs.Combine(err, conn.disconnect())
-}
-
-// dialNode dials the specified node.
-func (service *Service) dialNode(ctx context.Context, target pb.Node) (_ *Conn, err error) {
-	defer mon.Task()(&ctx)(&err)
-	grpcconn, err := service.transport.DialNode(ctx, &target)
-	return &Conn{
-		conn:   grpcconn,
-		client: pb.NewContactClient(grpcconn),
-	}, err
-}
-
-// disconnect disconnects this connection.
-func (conn *Conn) disconnect() error {
-	return conn.conn.Close()
 }
