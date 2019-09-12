57f9206b627c9bfb1c3422bd020fad2764a00e51
Michal Niewrzal <michal@storj.io>
2019-09-12 15:08:15 +0200

    increase timeout

--- pkg/transport/common.go
+++ pkg/transport/common.go
@@ -18,8 +18,8 @@ var (
 
 const (
 	// defaultTransportDialTimeout is the default time to wait for a connection to be established.
-	defaultTransportDialTimeout = 20 * time.Second
+	defaultTransportDialTimeout = 60 * time.Second
 
 	// defaultTransportRequestTimeout is the default time to wait for a response.
-	defaultTransportRequestTimeout = 20 * time.Second
+	defaultTransportRequestTimeout = 60 * time.Second
 )
