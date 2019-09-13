2b5e3de264b0c7ccb9c48fe18c2482e53d9c5eb6
Cameron Ayer <camayer92@gmail.com>
2019-09-13 16:59:15 -0400

    remove kademlia flags from satellites and storagenodes in storj-sim

--- cmd/storj-sim/network.go
+++ cmd/storj-sim/network.go
@@ -282,8 +282,6 @@ func newNetwork(flags *Flags) (*Processes, error) {
 				"--server.address", process.Address,
 				"--server.private-address", net.JoinHostPort(host, port(satellitePeer, i, privateGRPC)),
 
-				"--kademlia.bootstrap-addr", bootstrap.Address,
-
 				"--server.extensions.revocation=false",
 				"--server.use-peer-ca-whitelist=false",
 
@@ -449,10 +447,6 @@ func newNetwork(flags *Flags) (*Processes, error) {
 				"--console.static-dir", filepath.Join(storjRoot, "web/storagenode/"),
 				"--server.address", process.Address,
 				"--server.private-address", net.JoinHostPort(host, port(storagenodePeer, i, privateGRPC)),
-
-				"--kademlia.bootstrap-addr", bootstrap.Address,
-				"--kademlia.operator.email", fmt.Sprintf("storage%d@mail.test", i),
-				"--kademlia.operator.wallet", "0x0123456789012345678901234567890123456789",
 
 				"--storage2.monitor.minimum-disk-space", "0",
 				"--storage2.monitor.minimum-bandwidth", "0",
