0988c62245e0fb99d365a3a73290568ccb4c1b8b
Yingrong Zhao <yingrong.zhao@gmail.com>
2019-09-16 10:40:35 -0400

    typo oops

--- satellite/repair/repair_test.go
+++ satellite/repair/repair_test.go
@@ -800,7 +800,7 @@ func stopNodeByID(t *testing.T, ctx context.Context, planet *testplanet.Planet, 
 	}
 }
 
-// corruptPieceData manipulates piece data on a storagde node.
+// corruptPieceData manipulates piece data on a storage node.
 func corruptPieceData(ctx context.Context, t *testing.T, planet *testplanet.Planet, corruptedNode *storagenode.Peer, corruptedPieceID storj.PieceID) {
 	t.Helper()
 
