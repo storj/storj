f8fdc970790c655ecfeadad53a095920c76c544c
Kaloyan Raev <kaloyan@storj.io>
2019-09-17 10:25:36 +0300

    Comment for the purpose of the windows.go file

--- cmd/storagenode/windows.go
+++ cmd/storagenode/windows.go
@@ -1,6 +1,8 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
+// Implements support for running the storage node as a Windows Service.
+
 // +build windows
 
 package main
