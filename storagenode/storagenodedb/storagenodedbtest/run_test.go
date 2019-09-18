af436f655293675787ca39f6685006a4b55c42f2
Jennifer Johnson <jennifer@storj.io>
2019-09-18 13:49:56 -0400

    kad ref

--- storagenode/storagenodedb/storagenodedbtest/run_test.go
+++ storagenode/storagenodedb/storagenodedbtest/run_test.go
@@ -57,11 +57,10 @@ func TestInMemoryConcurrency(t *testing.T) {
 
 	storageDir := ctx.Dir("storage")
 	cfg := storagenodedb.Config{
-		Pieces:   storageDir,
-		Storage:  storageDir,
-		Info:     filepath.Join(storageDir, "piecestore.db"),
-		Info2:    filepath.Join(storageDir, "info.db"),
-		Kademlia: filepath.Join(storageDir, "kademlia"),
+		Pieces:  storageDir,
+		Storage: storageDir,
+		Info:    filepath.Join(storageDir, "piecestore.db"),
+		Info2:   filepath.Join(storageDir, "info.db"),
 	}
 
 	db, err := storagenodedb.New(log, cfg)
