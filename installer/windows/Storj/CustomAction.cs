60a9ccc6ca6df3ccdc0f558e83cab73158b99448
Kaloyan Raev <kaloyan@storj.io>
2019-09-18 12:58:27 +0300

    Validation for allocated storage and bandwidth

--- installer/windows/Storj/CustomAction.cs
+++ installer/windows/Storj/CustomAction.cs
@@ -36,5 +36,60 @@ namespace Storj
             session["STORJ_WALLET_VALID"] = "1";
             return ActionResult.Success;
         }
+
+        [CustomAction]
+        public static ActionResult ValidateStorage(Session session)
+        {
+            string storageStr = session["STORJ_STORAGE"];
+
+            if (string.IsNullOrEmpty(storageStr))
+            {
+                session["STORJ_STORAGE_VALID"] = "The value cannot be empty.";
+                return ActionResult.Success;
+            }
+
+            if (!double.TryParse(storageStr, out double storage))
+            {
+                session["STORJ_STORAGE_VALID"] = string.Format("'{0}' is not a valid number.", storageStr);
+                return ActionResult.Success;
+            }
+
+            if (storage < 0.5) {
+                session["STORJ_STORAGE_VALID"] = "The allocated disk space cannot be less than 0.5 TB.";
+                return ActionResult.Success;
+            }
+
+            // Allocated Storage value is valid
+            session["STORJ_STORAGE_VALID"] = "1";
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult ValidateBandwidth(Session session)
+        {
+            string bandwidthStr = session["STORJ_BANDWIDTH"];
+
+            if (string.IsNullOrEmpty(bandwidthStr))
+            {
+                session["STORJ_BANDWIDTH_VALID"] = "The value cannot be empty.";
+                return ActionResult.Success;
+            }
+
+            if (!double.TryParse(bandwidthStr, out double bandwidth))
+            {
+                session["STORJ_BANDWIDTH_VALID"] = string.Format("'{0}' is not a valid number.", bandwidthStr);
+                return ActionResult.Success;
+            }
+
+            if (bandwidth < 2.0)
+            {
+                session["STORJ_BANDWIDTH_VALID"] = "The allocated bandwidth cannot be less than 2 TB.";
+                return ActionResult.Success;
+            }
+
+            // Allocated Bandwidth value is valid
+            session["STORJ_BANDWIDTH_VALID"] = "1";
+            return ActionResult.Success;
+        }
     }
 }
