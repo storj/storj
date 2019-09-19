936c88c0d560c9c02ed91f8514b25bd6351d2ff9
Ethan <ethan@storj.io>
2019-09-19 17:06:28 -0400

    Revert "consolidate postgres byte conversion"
    This reverts commit 18b74d9e1d8698564a6c8887d772b83e516dc4fa.

--- satellite/satellitedb/utils.go
+++ satellite/satellitedb/utils.go
@@ -28,34 +28,55 @@ type postgresNodeIDList storj.NodeIDList
 
 // Value converts a NodeIDList to a postgres array
 func (nodes postgresNodeIDList) Value() (driver.Value, error) {
+	const hextable = "0123456789abcdef"
+
 	if nodes == nil {
 		return nil, nil
 	}
-
-	values := make([][]byte, len(nodes))
-	for i, n := range nodes {
-		values[i] = n.Bytes()
+	if len(nodes) == 0 {
+		return []byte("{}"), nil
 	}
 
-	return convertToPostgres(values), nil
+	var wp, x int
+	out := make([]byte, 2+len(nodes)*(6+storj.NodeIDSize*2)-1)
+
+	x = copy(out[wp:], []byte(`{"\\x`))
+	wp += x
+
+	for i := range nodes {
+		for _, v := range nodes[i] {
+			out[wp] = hextable[v>>4]
+			out[wp+1] = hextable[v&0xf]
+			wp += 2
+		}
+
+		if i+1 < len(nodes) {
+			x = copy(out[wp:], []byte(`","\\x`))
+			wp += x
+		}
+	}
+
+	x = copy(out[wp:], `"}`)
+	wp += x
+
+	if wp != len(out) {
+		panic("unreachable")
+	}
+
+	return out, nil
 }
 
 type postgresArrayList [][]byte
 
 // Value converts an array of byte arrays to a postgres array
 func (values postgresArrayList) Value() (driver.Value, error) {
+	const hextable = "0123456789abcdef"
+
 	if values == nil {
 		return nil, nil
 	}
-
-	return convertToPostgres(values), nil
-}
-
-func convertToPostgres(values [][]byte) []byte {
-	const hextable = "0123456789abcdef"
-
 	if len(values) == 0 {
-		return []byte("{}")
+		return []byte("{}"), nil
 	}
 
 	var total int
@@ -87,7 +108,8 @@ func convertToPostgres(values [][]byte) []byte {
 	if wp != len(out) {
 		panic("unreachable")
 	}
-	return out
+
+	return out, nil
 }
 
 // uuidScan used to represent uuid scan struct
