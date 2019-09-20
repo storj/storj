a45bd7ed134d91e0007ea4b10e0f37a38123f76e
Kaloyan Raev <kaloyan@storj.io>
2019-09-20 14:26:35 +0300

    Remove build.bat

--- installer/windows/build.bat
+++
@@ -1,3 +0,0 @@
-candle.exe -arch x64 Product.wxs CustomInstallDir.wxs IdentityDir.wxs OperatorConfig.wxs
-
-light.exe -o storagenode.msi -ext WixUIExtension Product.wixobj CustomInstallDir.wixobj IdentityDir.wixobj  OperatorConfig.wixobj
