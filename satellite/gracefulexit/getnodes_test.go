da8438007ac35078452c0b99804fcb9498c7c580
Natalie Ventura Villasana <nat@storj.io>
2019-09-20 15:24:25 -0400

    add TestGetExitingNodesLoopIncomplete

--- satellite/gracefulexit/getnodes_test.go
+++ satellite/gracefulexit/getnodes_test.go
@@ -69,3 +69,63 @@ func TestGetExitingNodes(t *testing.T) {
 		require.Len(t, nodes, numExitingNodes)
 	})
 }
+
+func TestGetExitingNodesLoopIncomplete(t *testing.T) {
+	satellitedbtest.Run(t, func(t *testing.T, db satellite.DB) {
+		ctx := testcontext.New(t)
+		defer ctx.Cleanup()
+
+		var (
+			cache = db.OverlayCache()
+
+			numTotalNodes          = 5
+			numExitingNodesLoopInc = 2
+		)
+
+		for i := 0; i < numTotalNodes; i++ {
+			newID := testrand.NodeID()
+			// add nodes to cache
+			err := cache.UpdateAddress(ctx, &pb.Node{Id: newID}, overlay.NodeSelectionConfig{})
+			require.NoError(t, err)
+
+			var (
+				initiatedAt         *time.Time
+				completedAt         *time.Time
+				finishedAt          *time.Time
+				updateInitiated     = false
+				updateLoopCompleted = false
+				updateFinished      = false
+			)
+
+			// set some nodes to have an exiting status
+			if i < numExitingNodesLoopInc {
+				timestamp := time.Now().UTC()
+				initiatedAt = &timestamp
+				updateInitiated = true
+			} else {
+				ts1 := time.Now().UTC()
+				ts2 := time.Now().UTC()
+				initiatedAt = &ts1
+				updateInitiated = true
+				completedAt = &ts2
+				updateLoopCompleted = true
+			}
+
+			req := &overlay.ExitStatusRequest{
+				NodeID:              newID,
+				ExitInitiatedAt:     initiatedAt,
+				ExitLoopCompletedAt: completedAt,
+				ExitFinishedAt:      finishedAt,
+				UpdateInitiated:     updateInitiated,
+				UpdateLoopCompleted: updateLoopCompleted,
+				UpdateFinished:      updateFinished,
+			}
+			_, err = cache.UpdateExitStatus(ctx, req)
+			require.NoError(t, err)
+		}
+
+		nodes, err := cache.GetExitingNodesLoopIncomplete(ctx)
+		require.NoError(t, err)
+		require.Len(t, nodes, numExitingNodesLoopInc)
+	})
+}
