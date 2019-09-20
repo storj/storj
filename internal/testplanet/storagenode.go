d2502bb51bda867e5eb5f28e5010eea8a2092763
Jennifer Li Johnson <jennifer@storj.io>
2019-09-20 16:02:23 -0400

    Adds tests for kad replacement and restores kad operator configs (#3094)
    * test that all nodes can check in with all satellites
    
    * keep kademlia config
    
    * add untrusted satellite test
    
    * use getversion
    
    * remove kademlia config changes in test-sim-backwards.sh
    
    * add kademlia flags back to storj-sim storagenode
    
    * reset kademlia flags in storagenode entrypoint

--- internal/testplanet/storagenode.go
+++ internal/testplanet/storagenode.go
@@ -14,6 +14,7 @@ import (
 	"github.com/zeebo/errs"
 
 	"storj.io/storj/internal/memory"
+	"storj.io/storj/pkg/kademlia"
 	"storj.io/storj/pkg/peertls/extensions"
 	"storj.io/storj/pkg/peertls/tlsopts"
 	"storj.io/storj/pkg/revocation"
@@ -71,9 +72,11 @@ func (planet *Planet) newStorageNodes(count int, whitelistedSatellites storj.Nod
 					},
 				},
 			},
-			Operator: storagenode.OperatorConfig{
-				Email:  prefix + "@mail.test",
-				Wallet: "0x" + strings.Repeat("00", 20),
+			Kademlia: kademlia.Config{
+				Operator: kademlia.OperatorConfig{
+					Email:  prefix + "@mail.test",
+					Wallet: "0x" + strings.Repeat("00", 20),
+				},
 			},
 			Storage: piecestore.OldConfig{
 				Path:                   filepath.Join(storageDir, "pieces/"),
