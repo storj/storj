e75449a177154fc04357917e25a8f5e20d68b6d3
Cameron Ayer <camayer92@gmail.com>
2019-09-20 15:16:59 -0400

    add kademlia flags back to storj-sim storagenode

--- cmd/storj-sim/network.go
+++ cmd/storj-sim/network.go
@@ -448,8 +448,8 @@ func newNetwork(flags *Flags) (*Processes, error) {
 				"--server.address", process.Address,
 				"--server.private-address", net.JoinHostPort(host, port(storagenodePeer, i, privateGRPC)),
 
-				"--operator.email", fmt.Sprintf("storage%d@mail.test", i),
-				"--operator.wallet", "0x0123456789012345678901234567890123456789",
+				"--kademlia.operator.email", fmt.Sprintf("storage%d@mail.test", i),
+				"--kademlia.operator.wallet", "0x0123456789012345678901234567890123456789",
 
 				"--storage2.monitor.minimum-disk-space", "0",
 				"--storage2.monitor.minimum-bandwidth", "0",
