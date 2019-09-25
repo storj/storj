e2b3e40268f306c742e7f5c8f5652fad4d4459f6
Cameron Ayer <camayer92@gmail.com>
2019-09-25 16:40:00 -0600

    update node version columns in UpdateCheckIn

--- satellite/satellitedb/overlaycache.go
+++ satellite/satellitedb/overlaycache.go
@@ -1420,6 +1420,7 @@ func (cache *overlaycache) UpdateCheckIn(ctx context.Context, node overlay.NodeC
 				wallet=$7,
 				free_bandwidth=$8,
 				free_disk=$9,
+				major=$18, minor=$20, patch=$21, hash=$22, timestamp=$22, release=$23,
 				total_uptime_count=nodes.total_uptime_count+1,
 				uptime_reputation_alpha=$16::numeric*nodes.uptime_reputation_alpha + $17::numeric*$10::bool::int,
 				uptime_reputation_beta=$16::numeric*nodes.uptime_reputation_beta + $17::numeric*(NOT $10)::bool::int,
