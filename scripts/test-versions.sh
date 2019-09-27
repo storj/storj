0880913fc842e599f712183a76ba47c7f8244d22
Isaac Hess <isaachess@gmail.com>
2019-09-27 09:34:42 -0600

    Moby's script for test-versions.sh

--- scripts/test-versions.sh
+++ scripts/test-versions.sh
@@ -1,41 +1,34 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-#main_cfg_dir=$1
+main_cfg_dir=$1
 
-echo "Begin test-versions.sh" ${main_cfg_dir}
+echo "Begin test-versions.sh, storj-sim config directory:" ${main_cfg_dir}
 
-echo "which storj-sim"
-which storj-sim
-shasum $(which storj-sim)
+echo "which storj-sim: $(which storj-sim)"
+# shasum $(which storj-sim)
 
-#echo "Uplink version:"
-#uplink version --config-dir "${main_cfg_dir}/gateway/0/"
+echo -e "\nConfig directory for uplink:"
+echo "${main_cfg_dir}/gateway/0"
+echo "which uplink: $(which uplink)"
+echo "Shasum for uplink:"
+shasum uplink
+# uplink version --config-dir "${main_cfg_dir}/gateway/0/"
 
-#echo "Satellite config directory:"
-#storj-sim network env --config-dir "${main_cfg_dir}" SATELLITE_0_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" SATELLITE_0_DIR)/satellite
+echo -e "\nConfig directory for satellite:"
+echo "${main_cfg_dir}/satellite/0"
+# storj-sim network env --config-dir "${main_cfg_dir}" SATELLITE_0_DIR
+echo "Shasum for satellite:"
+shasum ${main_cfg_dir}/satellite/0/satellite
 
-#echo "Storagenode config directories:"
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_0_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_0_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_1_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_1_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_2_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_2_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_3_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_3_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_4_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_4_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_5_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_5_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_6_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_6_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_7_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_7_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_8_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_8_DIR)/storagenode
-#storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_9_DIR
-#shasum $(storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_9_DIR)/storagenode
+echo -e "\nStoragenode config directories:"
+for i in {0..9}
+do 
+    echo -e "\nConfig directory for sn ${i}:"
+    echo "${main_cfg_dir}/storagenode/${i}"
+    # storj-sim network env --config-dir "${main_cfg_dir}" STORAGENODE_${i}_DIR
+    echo "Shasum for sn ${i} binary:"
+    shasum ${main_cfg_dir}/storagenode/${i}/storagenode
+done
 
-#echo "!!!!!!!!!!!!"
+echo "Done with test-versions.sh"
