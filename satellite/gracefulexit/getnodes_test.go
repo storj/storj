06672d96156e6d0a8dfc4c62705739c1854e1abd
Moby von Briesen <mobyvb@gmail.com>
2019-10-01 14:39:14 -0400

    update test

--- satellite/gracefulexit/getnodes_test.go
+++ satellite/gracefulexit/getnodes_test.go
@@ -12,6 +12,7 @@ import (
 	"storj.io/storj/internal/testcontext"
 	"storj.io/storj/internal/testrand"
 	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/storj"
 	"storj.io/storj/satellite"
 	"storj.io/storj/satellite/overlay"
 	"storj.io/storj/satellite/satellitedb/satellitedbtest"
@@ -22,90 +23,62 @@ func TestGetExitingNodes(t *testing.T) {
 		ctx := testcontext.New(t)
 		defer ctx.Cleanup()
 
-		var (
-			cache = db.OverlayCache()
+		cache := db.OverlayCache()
+		exiting := make(map[storj.NodeID]bool)
+		exitingCount := 0
+		exitingLoopIncomplete := make(map[storj.NodeID]bool)
+		exitingLoopIncompleteCount := 0
 
-			numTotalNodes   = 5
-			numExitingNodes = 2
-		)
+		testData := []struct {
+			nodeID                  storj.NodeID
+			initiatedAt             time.Time
+			completedAt             time.Time
+			finishedAt              time.Time
+			isExiting               bool
+			isExitingLoopIncomplete bool
+		}{
+			{testrand.NodeID(), time.Time{}, time.Time{}, time.Time{}, false, false},
+			{testrand.NodeID(), time.Now(), time.Time{}, time.Time{}, true, true},
+			{testrand.NodeID(), time.Now(), time.Now(), time.Time{}, true, false},
+			{testrand.NodeID(), time.Now(), time.Now(), time.Now(), false, false},
+			{testrand.NodeID(), time.Now(), time.Time{}, time.Now(), false, false},
+		}
 
-		for i := 0; i < numTotalNodes; i++ {
-			newID := testrand.NodeID()
-			// add nodes to cache
-			err := cache.UpdateAddress(ctx, &pb.Node{Id: newID}, overlay.NodeSelectionConfig{})
+		for _, data := range testData {
+			err := cache.UpdateAddress(ctx, &pb.Node{Id: data.nodeID}, overlay.NodeSelectionConfig{})
 			require.NoError(t, err)
 
-			var (
-				initiatedAt time.Time
-				completedAt time.Time
-				finishedAt  time.Time
-			)
-
-			// set some nodes to have an exiting status
-			if i < numExitingNodes {
-				initiatedAt = time.Now().UTC()
-			}
-
 			req := &overlay.ExitStatusRequest{
-				NodeID:              newID,
-				ExitInitiatedAt:     initiatedAt,
-				ExitLoopCompletedAt: completedAt,
-				ExitFinishedAt:      finishedAt,
+				NodeID:              data.nodeID,
+				ExitInitiatedAt:     data.initiatedAt,
+				ExitLoopCompletedAt: data.completedAt,
+				ExitFinishedAt:      data.finishedAt,
 			}
 			_, err = cache.UpdateExitStatus(ctx, req)
 			require.NoError(t, err)
+
+			if data.isExiting {
+				exitingCount++
+				exiting[data.nodeID] = true
+			}
+			if data.isExitingLoopIncomplete {
+				exitingLoopIncompleteCount++
+				exitingLoopIncomplete[data.nodeID] = true
+			}
 		}
 
 		nodes, err := cache.GetExitingNodes(ctx)
 		require.NoError(t, err)
-		require.Len(t, nodes, numExitingNodes)
-	})
-}
-
-func TestGetExitingNodesLoopIncomplete(t *testing.T) {
-	satellitedbtest.Run(t, func(t *testing.T, db satellite.DB) {
-		ctx := testcontext.New(t)
-		defer ctx.Cleanup()
-
-		var (
-			cache = db.OverlayCache()
-
-			numTotalNodes          = 5
-			numExitingNodesLoopInc = 2
-		)
-
-		for i := 0; i < numTotalNodes; i++ {
-			newID := testrand.NodeID()
-			// add nodes to cache
-			err := cache.UpdateAddress(ctx, &pb.Node{Id: newID}, overlay.NodeSelectionConfig{})
-			require.NoError(t, err)
-
-			var (
-				initiatedAt time.Time
-				completedAt time.Time
-				finishedAt  time.Time
-			)
-
-			// set some nodes to have an exiting status
-			if i < numExitingNodesLoopInc {
-				initiatedAt = time.Now().UTC()
-			} else {
-				initiatedAt = time.Now().UTC()
-				completedAt = time.Now().UTC()
-			}
-
-			req := &overlay.ExitStatusRequest{
-				NodeID:              newID,
-				ExitInitiatedAt:     initiatedAt,
-				ExitLoopCompletedAt: completedAt,
-				ExitFinishedAt:      finishedAt,
-			}
-			_, err = cache.UpdateExitStatus(ctx, req)
-			require.NoError(t, err)
+		require.Len(t, nodes, exitingCount)
+		for _, id := range nodes {
+			require.True(t, exiting[id])
 		}
 
-		nodes, err := cache.GetExitingNodesLoopIncomplete(ctx)
+		nodes, err = cache.GetExitingNodesLoopIncomplete(ctx)
 		require.NoError(t, err)
-		require.Len(t, nodes, numExitingNodesLoopInc)
+		require.Len(t, nodes, exitingLoopIncompleteCount)
+		for _, id := range nodes {
+			require.True(t, exitingLoopIncomplete[id])
+		}
 	})
 }
