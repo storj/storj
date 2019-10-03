f1dd44314ecac87403086294f3a9a7a392b9c4e2
Michal Niewrzal <michal@storj.io>
2019-10-03 11:37:52 +0200

    mobile: build libuplink aar

--- mobile/build.sh
+++ mobile/build.sh
@@ -4,7 +4,6 @@
 # Required:
 # * ANDROID_HOME set with NDK available
 # * go
-# * gospace
 
 if [ -z "$ANDROID_HOME" ]
 then
@@ -12,42 +11,35 @@ then
       exit 1
 fi
 
-OUTPUT_DIR=${1:-$PWD}
-OUTPUT_AAR="libuplink-android.aar"
-OUTPUT_JAVA_PACKAGE="io.storj.libuplink"
+# setup tmpdir for testfiles and cleanup
+TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
+cleanup(){
+	rm -rf "$TMP"
+}
+trap cleanup EXIT
 
-STORJ_PATH=~/storj-for-android
+OUTPUT=$PWD
+
+go mod vendor
+
+# go knows where our gopath is
+export GOPATH=$TMP
 
 # set go modules to default behavior
 export GO111MODULE=auto
 
-# go knows where our gopath is
-export GOPATH=$STORJ_PATH
+mkdir $GOPATH $GOPATH/src $GOPATH/bin $GOPATH/pkg
 
-# gospace knows where our gopath is (this is to avoid accidental damage to existing GOPATH)
-# you should not use default GOPATH here
-export GOSPACE_ROOT=$STORJ_PATH
+mv $PWD/vendor/* "$GOPATH/src"
 
-# set the github repository that this GOSPACE manages
-export GOSPACE_PKG=storj.io/storj
+# symlink doesn't look to be working with gomobile
+# ln -s "$PWD/../storj" "$GOPATH/src/storj.io/storj"
+rsync -am --stats --exclude=".*" "$PWD/../storj" "$GOPATH/src/storj.io/"
 
-# set the where the repository is located
-export GOSPACE_REPO=git@github.com:storj/storj.git
-
-gospace setup
-
-export PATH=$PATH:$GOPATH/bin
-
-# step can be removed after merge to master
-cd $GOPATH/src/storj.io/storj
-git checkout -q mn/java-bindings
-
-cd $GOPATH
+cd "$GOPATH/src/storj.io/storj"
 
 go get golang.org/x/mobile/cmd/gomobile
 
-gomobile init
+$GOPATH/bin/gomobile init
 
-echo -e "\nbuilding aar"
-gomobile bind -target android -o $OUTPUT_DIR/libuplink-android.aar -javapkg $OUTPUT_JAVA_PACKAGE storj.io/storj/mobile
-echo "output aar: $OUTPUT_DIR/libuplink-android.aar"
+$GOPATH/bin/gomobile bind -v -target android -o "$OUTPUT/libuplink-android.aar" -javapkg io.storj.libuplink storj.io/storj/mobile
