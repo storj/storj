fb4b4d8d436036f28e7acc633256f9a4d87a537e
VitaliiShpital <takur12345@gmail.com>
2019-10-04 16:19:40 +0300

    comment resolved

--- web/satellite/src/store/modules/projects.ts
+++ web/satellite/src/store/modules/projects.ts
@@ -95,10 +95,10 @@ export function makeProjectsModule(api: ProjectsApi): StoreModule<ProjectsState>
                 selected.description = updateProjectModel.description;
             },
             [REMOVE](state: any, projectID: string): void {
-                state.projects = state.projects.filter(proj => proj.id !== projectID);
+                state.projects = state.projects.filter(project => project.id !== projectID);
 
                 if (state.selectedProject.id === projectID) {
-                    state.selectedProject = new Project('', '', '', '', '');
+                    state.selectedProject = new Project();
                 }
             },
             [CLEAR_PROJECTS](state: ProjectsState): void {
