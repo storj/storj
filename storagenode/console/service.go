4824ecdb8da5c62628e9fe1876e63786c1cb36e5
Yaroslav Vorobiov <yar.vorobiov@gmail.com>
2019-10-04 09:17:28 -0600

    storagenode/console: use bytes for remaining info (#3186)

--- storagenode/console/service.go
+++ storagenode/console/service.go
@@ -159,13 +159,13 @@ func (s *Service) GetDashboardData(ctx context.Context) (_ *Dashboard, err error
 	}
 
 	data.DiskSpace = DiskSpaceInfo{
-		Used:      memory.Size(spaceUsage).GB(),
-		Available: s.allocatedDiskSpace.GB(),
+		Used:      spaceUsage,
+		Available: s.allocatedDiskSpace.Int64(),
 	}
 
 	data.Bandwidth = BandwidthInfo{
-		Used:      memory.Size(bandwidthUsage.Total()).GB(),
-		Available: s.allocatedBandwidth.GB(),
+		Used:      bandwidthUsage.Total(),
+		Available: s.allocatedBandwidth.Int64(),
 	}
 
 	return data, nil
