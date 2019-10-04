8905774d990fe8233429da5406b8b5b169e7f1b7
Michal Niewrzal <michal@storj.io>
2019-10-04 10:40:32 +0200

    move all operations to tmp dir

--- mobile/build.sh
+++ mobile/build.sh
@@ -20,17 +20,10 @@ trap cleanup EXIT
 
 OUTPUT=$PWD
 
-go mod vendor
-
 # go knows where our gopath is
 export GOPATH=$TMP
 
-# set go modules to default behavior
-export GO111MODULE=off
-
 mkdir "$GOPATH/src"
-
-mv $PWD/vendor/* "$GOPATH/src"
 
 # symlink doesn't look to be working with gomobile
 # ln -s "$PWD/../storj" "$GOPATH/src/storj.io/storj"
@@ -38,8 +31,18 @@ rsync -am --stats --exclude=".*" "$PWD/../storj" "$GOPATH/src/storj.io/"
 
 cd "$GOPATH/src/storj.io/storj"
 
+go mod vendor
+
+cp -r $GOPATH/src/storj.io/storj/vendor/* "$GOPATH/src"
+
+# set go modules to default behavior
+export GO111MODULE=off
+
 go get golang.org/x/mobile/cmd/gomobile
 
 $GOPATH/bin/gomobile init
 
 $GOPATH/bin/gomobile bind -v -target android -o "$OUTPUT/libuplink-android.aar" -javapkg io.storj.libuplink storj.io/storj/mobile
+
+# cleanup pkg/mod directory
+go clean -modcache
