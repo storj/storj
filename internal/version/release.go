d709144bf0b3d4fca7caa8c6126b2d40a30d1d68
littleskunk <jens.heimbuerge@googlemail.com>
2019-10-09 16:10:32 +0200

    release v0.23.2

---
+++ internal/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1570630232"
+	buildCommitHash = "2c7cf54d811f2c02eae9ea5dc012c81e6fd28f5a"
+	buildVersion = "v0.23.2"
+	buildRelease = "true"
+}
