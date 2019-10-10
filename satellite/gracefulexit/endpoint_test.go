f9e06af45b1204992a2c30dcc38e087ee88ce127
Ethan <ethan@storj.io>
2019-10-09 20:46:03 -0400

    added test for max failures

--- satellite/gracefulexit/endpoint_test.go
+++ satellite/gracefulexit/endpoint_test.go
@@ -4,6 +4,7 @@
 package gracefulexit_test
 
 import (
+	"fmt"
 	"io"
 	"strconv"
 	"testing"
@@ -18,12 +19,119 @@ import (
 	"storj.io/storj/internal/testrand"
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/storj"
+	"storj.io/storj/storagenode"
 	"storj.io/storj/uplink"
 )
 
 const numMessages = 6
 
-func TestInitiate(t *testing.T) {
+func TestSuccess(t *testing.T) {
+	testTransfers(t, numMessages, func(ctx *testcontext.Context, satellite *testplanet.SatelliteSystem, processClient pb.SatelliteGracefulExit_ProcessClient, exitingNode *storagenode.Peer) {
+		var pieceID storj.PieceID
+		for {
+			response, err := processClient.Recv()
+			if err == io.EOF {
+				// Done
+				break
+			}
+			require.NoError(t, err)
+
+			switch m := response.GetMessage().(type) {
+			case *pb.SatelliteMessage_TransferPiece:
+				require.NotNil(t, m)
+
+				// pick the first one to fail
+				if pieceID.IsZero() {
+					pieceID = m.TransferPiece.PieceId
+				}
+				fmt.Printf("EEEE: %v\n", m.TransferPiece.PieceId.String())
+				if pieceID != m.TransferPiece.PieceId {
+					success := &pb.StorageNodeMessage{
+						Message: &pb.StorageNodeMessage_Succeeded{
+							Succeeded: &pb.TransferSucceeded{
+								OriginalPieceHash: &pb.PieceHash{PieceId: m.TransferPiece.PieceId},
+								AddressedOrderLimit: &pb.AddressedOrderLimit{
+									Limit: &pb.OrderLimit{
+										PieceId: m.TransferPiece.PieceId,
+									},
+								},
+							},
+						},
+					}
+					err = processClient.Send(success)
+					require.NoError(t, err)
+				} else {
+					failed := &pb.StorageNodeMessage{
+						Message: &pb.StorageNodeMessage_Failed{
+							Failed: &pb.TransferFailed{
+								PieceId: m.TransferPiece.PieceId,
+								Error:   pb.TransferFailed_UNKNOWN,
+							},
+						},
+					}
+					err = processClient.Send(failed)
+					require.NoError(t, err)
+				}
+			case *pb.SatelliteMessage_ExitCompleted:
+				// TODO test completed signature stuff
+				break
+			default:
+				t.FailNow()
+			}
+		}
+
+		// check that the exit has completed and we have the correct transferred/failed values
+		progress, err := satellite.DB.GracefulExit().GetProgress(ctx, exitingNode.ID())
+		require.NoError(t, err)
+
+		require.Equal(t, int64(numMessages), progress.PiecesTransferred)
+		// even though we failed 1, it eventually succeeded, so the count should be 0
+		require.Equal(t, int64(0), progress.PiecesFailed)
+	})
+}
+
+func TestFailure(t *testing.T) {
+	testTransfers(t, 1, func(ctx *testcontext.Context, satellite *testplanet.SatelliteSystem, processClient pb.SatelliteGracefulExit_ProcessClient, exitingNode *storagenode.Peer) {
+		for {
+			response, err := processClient.Recv()
+			if err == io.EOF {
+				// Done
+				break
+			}
+			require.NoError(t, err)
+
+			switch m := response.GetMessage().(type) {
+			case *pb.SatelliteMessage_TransferPiece:
+				require.NotNil(t, m)
+
+				failed := &pb.StorageNodeMessage{
+					Message: &pb.StorageNodeMessage_Failed{
+						Failed: &pb.TransferFailed{
+							PieceId: m.TransferPiece.PieceId,
+							Error:   pb.TransferFailed_UNKNOWN,
+						},
+					},
+				}
+				err = processClient.Send(failed)
+				require.NoError(t, err)
+			case *pb.SatelliteMessage_ExitCompleted:
+				// TODO test completed signature stuff
+				break
+			default:
+				t.FailNow()
+			}
+		}
+
+		// check that the exit has completed and we have the correct transferred/failed values
+		progress, err := satellite.DB.GracefulExit().GetProgress(ctx, exitingNode.ID())
+		require.NoError(t, err)
+
+		require.Equal(t, int64(0), progress.PiecesTransferred)
+		require.Equal(t, int64(1), progress.PiecesFailed)
+	})
+}
+
+func testTransfers(t *testing.T, messageCount int, verifier func(ctx *testcontext.Context, satellite *testplanet.SatelliteSystem, processClient pb.SatelliteGracefulExit_ProcessClient, exitingNode *storagenode.Peer)) {
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount:   1,
 		StorageNodeCount: 8,
@@ -42,7 +150,7 @@ func TestInitiate(t *testing.T) {
 			MaxThreshold:     8,
 		}
 
-		for i := 0; i < numMessages; i++ {
+		for i := 0; i < messageCount; i++ {
 			err := uplinkPeer.UploadWithConfig(ctx, satellite, rs, "testbucket", "test/path"+strconv.Itoa(i), testrand.Bytes(5*memory.KiB))
 			require.NoError(t, err)
 		}
@@ -87,7 +195,7 @@ func TestInitiate(t *testing.T) {
 		// make sure all the pieces are in the transfer queue
 		incompleteTransfers, err := satellite.DB.GracefulExit().GetIncomplete(ctx, exitingNode.ID(), numMessages, 0)
 		require.NoError(t, err)
-		require.Len(t, incompleteTransfers, numMessages)
+		require.Len(t, incompleteTransfers, messageCount)
 
 		// connect to satellite again to start receiving transfers
 		c, err = client.Process(ctx, grpc.EmptyCallOption{})
@@ -96,65 +204,6 @@ func TestInitiate(t *testing.T) {
 			err = errs.Combine(err, c.CloseSend())
 		}()
 
-		var pieceID storj.PieceID
-		for {
-			response, err = c.Recv()
-			if err == io.EOF {
-				// Done
-				break
-			}
-			require.NoError(t, err)
-
-			switch m := response.GetMessage().(type) {
-			case *pb.SatelliteMessage_TransferPiece:
-				require.NotNil(t, m)
-
-				// pick the first one to fail
-				if pieceID.IsZero() {
-					pieceID = m.TransferPiece.PieceId
-				}
-
-				if pieceID != m.TransferPiece.PieceId {
-					success := &pb.StorageNodeMessage{
-						Message: &pb.StorageNodeMessage_Succeeded{
-							Succeeded: &pb.TransferSucceeded{
-								OriginalPieceHash: &pb.PieceHash{PieceId: m.TransferPiece.PieceId},
-								AddressedOrderLimit: &pb.AddressedOrderLimit{
-									Limit: &pb.OrderLimit{
-										PieceId: m.TransferPiece.PieceId,
-									},
-								},
-							},
-						},
-					}
-					err = c.Send(success)
-					require.NoError(t, err)
-				} else {
-					failed := &pb.StorageNodeMessage{
-						Message: &pb.StorageNodeMessage_Failed{
-							Failed: &pb.TransferFailed{
-								PieceId: m.TransferPiece.PieceId,
-								Error:   pb.TransferFailed_UNKNOWN,
-							},
-						},
-					}
-					err = c.Send(failed)
-					require.NoError(t, err)
-				}
-			case *pb.SatelliteMessage_ExitCompleted:
-				// TODO test completed signature stuff
-				break
-			default:
-				t.FailNow()
-			}
-		}
-
-		// check that the exit has completed and we have the correct transferred/failed values
-		progress, err := satellite.DB.GracefulExit().GetProgress(ctx, exitingNode.ID())
-		require.NoError(t, err)
-
-		require.Equal(t, int64(numMessages), progress.PiecesTransferred)
-		// even though we failed 1, it eventually succeeded, so the count should be 0
-		require.Equal(t, int64(0), progress.PiecesFailed)
+		verifier(ctx, satellite, c, exitingNode)
 	})
 }
