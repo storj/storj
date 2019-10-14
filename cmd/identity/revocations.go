84e2599a92161c7740a144e3acba3e3f6190deb2
Jennifer Johnson <jennifer@storj.io>
2019-10-13 23:31:04 -0400

    lint

--- cmd/identity/revocations.go
+++ cmd/identity/revocations.go
@@ -26,7 +26,7 @@ var (
 	}
 
 	revCfg struct {
-		RevocationDBURL string `default:"bolt://$CONFDIR/revocations.db" help:"url for revocation database (e.g. bolt://some.db OR "redis://127.0.0.1:6379?db=2&password=abc123)"`
+		RevocationDBURL string `default:"bolt://$CONFDIR/revocations.db" help:"url for revocation database (e.g. bolt://some.db OR redis://127.0.0.1:6379?db=2&password=abc123)"`
 	}
 )
 
