7344cf0db8578cfbb1fbfbd2c16c5811e08d78bd
Egon Elbre <egonelbre@gmail.com>
2019-10-15 08:51:45 +0300

    add json definition

--- satellite/partners/service.go
+++ satellite/partners/service.go
@@ -1,11 +1,48 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
+// Package partners implements partners management for attributions.
 package partners
 
 import (
+	"bytes"
+	"context"
+	htmltemplate "html/template"
+	"path/filepath"
+	"sync"
+
+	"github.com/zeebo/errs"
 	"go.uber.org/zap"
+	monkit "gopkg.in/spacemonkeygo/monkit.v2"
+
+	"storj.io/storj/internal/post"
 )
+
+var (
+	// Error is the default error class for partners package.
+	Error = errs.Class("partners error class")
+
+	mon = monkit.Package()
+)
+
+// Partner contains information about a partner.
+type Partner struct {
+	Name string
+	ID   string
+}
+
+// UserAgent returns partners cano user agent.
+func (p *Partner) UserAgent() string { return p.Name }
+
+// CanonicalUserAgent returns canonicalizes the user name, which is suitable for lookups.
+func CanonicalUserAgent(useragent string) string { return strigns.ToLower(useragent)}
+
+// DB allows access to partners database.
+type DB interface {
+	ByName(ctx context.Context, name string) ([]Partner, error)
+	ByID(ctx context.Context, id string) (Partner, error)
+	ByUserAgent(ctx context.Context, agent string) (Partner, error)
+}
 
 // Service allows manipulating and accessing partner information.
 type Service struct {
---
+++ satellite/partners/static-db.go
@@ -0,0 +1,70 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+// Package partners implements partners management for attributions.
+package partners
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/zeebo/errs"
+)
+
+// List defines a json struct for defining partners.
+type List struct {
+	Partners []Partner
+}
+
+// ListFromJSONFile loads a json definition of partners.
+func ListFromJSONFile(path string) (*List, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, Error.Wrap(err)
+	}
+	defer func() {
+		err = errs.Combine(err, Error.Wrap(file.Close()))
+	}()
+
+	var list List
+	err = json.NewDecoder(file).Decode(&list)
+	return list, Error.Wrap(err)
+}
+
+// StaticDB implements partner lookup based on a static definition.
+type StaticDB struct {
+	list        *List
+	byName      map[string][]Partner
+	byID        map[string]Partner
+	byUserAgent map[string]Partner
+}
+
+// NewStaticDB creates a new StaticDB.
+func NewStaticDB(list *List) (*StaticDB, error) {
+	db := &StaticDB{
+		list:        *list,
+		byName:      map[string][]Partner{},
+		byID:        map[string]Partner{},
+		byUserAgent: map[string]Partner{},
+	}
+
+	var errg errs.Group
+	for _, p := range list.Partners {
+		db.byName[p.Name] = append(db.byName[p.Name], p)
+
+		if _, exists := db.byID[p.ID]; exists {
+			errg.Add(Error.New("id %q already exists", p.ID))
+		} else {
+			db.byID[p.ID] = p
+		}
+
+		useragent := CanonicalUserAgent(p.UserAgent())
+		if _, exists := db.byUserAgent[useragent]; exists {
+			errg.Add(Error.New("user agent %q already exists", useragent))
+		} else {
+			db.byUserAgent[useragent] = p
+		}
+	}
+
+	return db, errg.Err()
+}
