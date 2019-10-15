61ee4aed85e53d085ecc816dcdff19e5f349ed24
Michal Niewrzal <michal@storj.io>
2019-10-15 14:45:57 +0200

    fix sources file name

--- lib/uplink-gomobile/release/release.sh
+++ lib/uplink-gomobile/release/release.sh
@@ -25,7 +25,7 @@ make libuplink-gomobile
 # create versioned copies of files
 cp -v "$SCRIPTDIR/libuplink-android-gomobile.pom"   "$TMP/libuplink-android-gomobile-$VERSION.pom"
 cp -v "libuplink-android.aar"                       "$TMP/libuplink-android-gomobile-$VERSION.aar"
-cp -v "libuplink-android-sources.jar"               "$TMP/libuplink-android-gomobile-sources-$VERSION.jar"
+cp -v "libuplink-android-sources.jar"               "$TMP/libuplink-android-gomobile-$VERSION-sources.jar"
 cp -v "$SCRIPTDIR/AndroidManifest.xml"              "$TMP/AndroidManifest.xml"
 
 # set version for pom file
@@ -42,6 +42,6 @@ TARGET_URL="https://api.bintray.com/content/storj/maven/libuplink-android-gomobi
 
 curl -T "$TMP/libuplink-android-gomobile-$VERSION.pom"          -u$BINTRAY_USER:$BINTRAY_API_KEY "$TARGET_URL/libuplink-android-gomobile-$VERSION.pom"
 curl -T "$TMP/libuplink-android-gomobile-$VERSION.aar"          -u$BINTRAY_USER:$BINTRAY_API_KEY "$TARGET_URL/libuplink-android-gomobile-$VERSION.aar"
-curl -T "$TMP/libuplink-android-gomobile-sources-$VERSION.jar"  -u$BINTRAY_USER:$BINTRAY_API_KEY "$TARGET_URL/libuplink-android-gomobile-sources-$VERSION.jar"
+curl -T "$TMP/libuplink-android-gomobile-$VERSION-sources.jar"  -u$BINTRAY_USER:$BINTRAY_API_KEY "$TARGET_URL/libuplink-android-gomobile-$VERSION-sources.jar"
 # TODO enable this after verifying that upload was done correctly
 # curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY "https://api.bintray.com/content/storj/maven/libuplink-android-gomobile/$VERSION/publish"
