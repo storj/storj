34764e5c97b2ecb0d06752decc5a5e8bd9679554
Jess G <Jessica.greben1+github@gmail.com>
2019-10-16 13:34:25 -0700

    cmd/satellite: create API subcommand (#3280)
    * create sa api subcommand
    
    * add api to storj-sim
    
    * require postgres
    
    * pass live accounting cache into satellite.NewAPI

--- cmd/satellite/main.go
+++ cmd/satellite/main.go
@@ -45,6 +45,11 @@ var (
 		Short: "Run the satellite",
 		RunE:  cmdRun,
 	}
+	runAPICmd = &cobra.Command{
+		Use:   "api",
+		Short: "Run the satellite API",
+		RunE:  cmdAPIRun,
+	}
 	setupCmd = &cobra.Command{
 		Use:         "setup",
 		Short:       "Create config files",
@@ -101,12 +106,14 @@ func init() {
 	cfgstruct.SetupFlag(zap.L(), rootCmd, &identityDir, "identity-dir", defaultIdentityDir, "main directory for satellite identity credentials")
 	defaults := cfgstruct.DefaultsFlag(rootCmd)
 	rootCmd.AddCommand(runCmd)
+	runCmd.AddCommand(runAPICmd)
 	rootCmd.AddCommand(setupCmd)
 	rootCmd.AddCommand(qdiagCmd)
 	rootCmd.AddCommand(reportsCmd)
 	reportsCmd.AddCommand(nodeUsageCmd)
 	reportsCmd.AddCommand(partnerAttributionCmd)
 	process.Bind(runCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
+	process.Bind(runAPICmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(setupCmd, &setupCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir), cfgstruct.SetupMode())
 	process.Bind(qdiagCmd, &qdiagCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(nodeUsageCmd, &nodeUsageCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
@@ -175,6 +182,66 @@ func cmdRun(cmd *cobra.Command, args []string) (err error) {
 	err = db.CreateTables()
 	if err != nil {
 		return errs.New("Error creating tables for master database on satellite: %+v", err)
+	}
+
+	runError := peer.Run(ctx)
+	closeError := peer.Close()
+	return errs.Combine(runError, closeError)
+}
+
+func cmdAPIRun(cmd *cobra.Command, args []string) (err error) {
+	ctx, _ := process.Ctx(cmd)
+	log := zap.L()
+
+	identity, err := runCfg.Identity.Load()
+	if err != nil {
+		zap.S().Fatal(err)
+	}
+
+	db, err := satellitedb.New(log.Named("db"), runCfg.Database)
+	if err != nil {
+		return errs.New("Error starting master database on satellite api: %+v", err)
+	}
+	defer func() {
+		err = errs.Combine(err, db.Close())
+	}()
+
+	pointerDB, err := metainfo.NewStore(log.Named("pointerdb"), runCfg.Config.Metainfo.DatabaseURL)
+	if err != nil {
+		return errs.New("Error creating revocation database on satellite api: %+v", err)
+	}
+	defer func() {
+		err = errs.Combine(err, db.Close())
+	}()
+
+	revocationDB, err := revocation.NewDBFromCfg(runCfg.Config.Server.Config)
+	if err != nil {
+		return errs.New("Error creating revocation database on satellite api: %+v", err)
+	}
+	defer func() {
+		err = errs.Combine(err, revocationDB.Close())
+	}()
+
+	accountingCache, err := live.NewCache(log.Named("live-accounting"), runCfg.LiveAccounting)
+	if err != nil {
+		return errs.New("Error creating live accounting cache on satellite api: %+v", err)
+	}
+	defer func() {
+		err = errs.Combine(err, accountingCache.Close())
+	}()
+
+	peer, err := satellite.NewAPI(log, identity, db, pointerDB, revocationDB, accountingCache, &runCfg.Config, version.Build)
+	if err != nil {
+		return err
+	}
+
+	err = peer.Version.CheckVersion(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := process.InitMetricsWithCertPath(ctx, log, nil, runCfg.Identity.CertPath); err != nil {
+		zap.S().Warn("Failed to initialize telemetry batcher on satellite api: ", err)
 	}
 
 	runError := peer.Run(ctx)
