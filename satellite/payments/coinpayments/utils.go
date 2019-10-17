b7ca985f1de59eef94b8dcf8eab82a78a985435e
Yaroslav <yar.vorobiov@gmail.com>
2019-10-17 15:20:46 +0300

    update comment

--- satellite/payments/coinpayments/utils.go
+++ satellite/payments/coinpayments/utils.go
@@ -13,7 +13,7 @@ import (
 var ErrNoAuthorizationKey = Error.New("no authorization key")
 
 // GetTransacationKeyFromURL parses provided raw url string
-// and extracts authorization key from it. Returns error if
+// and extracts authorization key from it. Returns ErrNoAuthorizationKey if
 // there is no authorization key.
 func GetTransacationKeyFromURL(rawurl string) (string, error) {
 	u, err := url.Parse(rawurl)
