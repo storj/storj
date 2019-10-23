1db481296b5e067ae8d2885f3b92471f25e6b33b
Michal Niewrzal <michal@storj.io>
2019-10-23 18:07:20 +0200

    use psql from container

--- scripts/test-sim-dev.sh
+++ scripts/test-sim-dev.sh
@@ -2,10 +2,11 @@
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run --rm -p 5433:5432 --name storj_sim_postgres postgres &
+CONTAINER_NAME=storj_sim_postgres
+docker run -d --rm -p 5433:5432 --name $CONTAINER_NAME postgres
 
 cleanup(){
-  docker rm -f storj_sim_postgres
+  docker rm -f $CONTAINER_NAME
 }
 trap cleanup EXIT
 
@@ -13,12 +14,12 @@ STORJ_SIM_DATABASE=${STORJ_SIM_DATABASE:-"teststorj"}
 
 RETRIES=5
 
-until psql -h localhost -U postgres -d postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until docker exec $CONTAINER_NAME psql -h localhost -U postgres -d postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
 
-psql -h localhost -p 5433 -U postgres -c "create database $STORJ_SIM_DATABASE;"
+docker exec $CONTAINER_NAME psql -h localhost -U postgres -c "create database $STORJ_SIM_DATABASE;"
 
 export STORJ_SIM_POSTGRES="postgres://postgres@localhost:5433/$STORJ_SIM_DATABASE?sslmode=disable"
 
