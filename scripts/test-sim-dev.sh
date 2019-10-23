d0e433060b4bb2144714678427968e4834cf1032
Michal Niewrzal <michal@storj.io>
2019-10-23 10:43:41 +0200

    scripts: dev script to start postgres before tests

---
+++ scripts/test-sim-dev.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run --rm -p 5432:5432 --name storj_sim_postgres postgres &
+
+cleanup(){
+	docker stop storj_sim_postgres
+}
+trap cleanup EXIT
+
+STORJ_SIM_DATABASE=${STORJ_SIM_DATABASE:-"teststorj"}
+
+RETRIES=5
+
+until psql -h localhost -U postgres -d postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
+psql -h localhost -U postgres -c "create database $STORJ_SIM_DATABASE;"
+
+export STORJ_SIM_POSTGRES="postgres://postgres@localhost/$STORJ_SIM_DATABASE?sslmode=disable"
+
+$SCRIPTDIR/test-sim.sh
