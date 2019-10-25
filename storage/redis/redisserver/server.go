cda88849f91262c6c35db55684b6ef566e085e6b
Jennifer Johnson <jennifer@storj.io>
2019-10-25 19:32:04 -0400

    whitespaces

--- storage/redis/redisserver/server.go
+++ storage/redis/redisserver/server.go
@@ -85,8 +85,8 @@ func Process() (addr string, cleanup func(), err error) {
 
 	// start the process
 	cmd := exec.Command("redis-server", confpath)
-
 	processgroup.Setup(cmd)
+
 	read, write, err := os.Pipe()
 	if err != nil {
 		return "", nil, err
