c700c0dfc8814ee9ea0129c0dfaba5ef41029fb2
Bryan White <bryanchriswhite@gmail.com>
2019-10-29 13:41:35 +0100

    refactor install batch script

--- installer/windows/install.bat
+++ installer/windows/install.bat
@@ -1,9 +1,12 @@
 @echo off
-rem uninstall existing storagenode product
-msiexec /x {E97D368F-CB18-45B5-8799-1EBB10728A99}
-
-rem NB: only installs from *first* release directory
-for /d %%d in (release\*) do (
-	msiexec /i %%d\storagenode_windows_amd64.msi /passive /qb /norestart /log %%d\install.log STORJ_WALLET="0x0000000000000000000000000000000000000000" STORJ_EMAIL="user@mail.example" STORJ_PUBLIC_ADDRESS="127.0.0.1:10000"
-	goto :EOF
+if "%1"=="" (
+    echo "missing required msi path argument"
+    exit /B 1
 )
+
+rem uninstall existing storagenode product
+echo uninstalling storagenode
+msiexec /x {E97D368F-CB18-45B5-8799-1EBB10728A99} /passive /qb
+
+echo installing storagenode from %1
+msiexec /i %1 /passive /qb /norestart /log %~dp1install.log STORJ_WALLET="0x0000000000000000000000000000000000000000" STORJ_EMAIL="user@mail.example" STORJ_PUBLIC_ADDRESS="127.0.0.1:10000"
