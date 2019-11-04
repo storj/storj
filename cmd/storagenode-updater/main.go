84fea5820f38cd7cdd2b03319c71de0bd25b147f
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2019-11-04 11:22:08 +0200

    Fix error when storagenode-updater service is stopped (#3440)

--- cmd/storagenode-updater/main.go
+++ cmd/storagenode-updater/main.go
@@ -144,7 +144,7 @@ func cmdRun(cmd *cobra.Command, args []string) (err error) {
 		loop := sync2.NewCycle(runCfg.CheckInterval)
 		err = loop.Run(ctx, loopFunc)
 	}
-	if err != nil && errs2.IsCanceled(err) {
+	if err != nil && !errs2.IsCanceled(err) {
 		log.Fatal(err)
 	}
 	return nil
