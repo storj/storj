2bdea22e0faa0e514061a0789816bc78fb398142
Ivan Fraixedes <ivan@fraixed.es>
2019-11-04 12:52:36 +0100

    FIX: remove unneded err check

--- uplink/storage/streams/store.go
+++ uplink/storage/streams/store.go
@@ -136,10 +136,6 @@ func (s *streamStore) upload(ctx context.Context, path Path, pathCipher storj.Ci
 		ExpiresAt:     expiration,
 	}
 
-	if err != nil {
-		return Meta{}, 0, storj.StreamID{}, err
-	}
-
 	var (
 		streamID       storj.StreamID
 		currentSegment int64
