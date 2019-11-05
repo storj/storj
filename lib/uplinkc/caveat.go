374f055e452e513fc7cd502a04025f68530d5e38
TopperDEL <github_com@tparth.de>
2019-11-05 09:31:54 +0100

    Resolving review-comments

--- lib/uplinkc/caveat.go
+++ lib/uplinkc/caveat.go
@@ -8,17 +8,18 @@ import "C"
 
 import "storj.io/storj/pkg/macaroon"
 
-// export new_caveat
+//export new_caveat
 // new_caveat returns a Caveat with a random generated nonce.
 func new_caveat(cerr **C.char) C.Caveat {
 	caveat, err := macaroon.NewCaveat()
 	if err != nil {
 		*cerr = C.CString(err.Error())
+		return nil
 	}
 	return newCaveat(&caveat)
 }
 
-// export free_caveat
+//export free_caveat
 // free_caveat frees a caveat
 func free_caveat(caveat C.Caveat) {
 	universe.Del(caveat.allowed_paths._handle)
