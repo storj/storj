5f02a49b596a3261f0977095a4d3538a4983007c
Natalie Ventura Villasana <nat@storj.io>
2019-11-05 12:19:30 -0500

    make sure other node selection defaults are used in find nodes call

--- satellite/overlay/service.go
+++ satellite/overlay/service.go
@@ -6,7 +6,6 @@ package overlay
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net"
 	"time"
 
@@ -252,10 +251,6 @@ func (service *Service) FindStorageNodes(ctx context.Context, req FindStorageNod
 func (service *Service) FindStorageNodesWithPreferences(ctx context.Context, req FindStorageNodesRequest, preferences *NodeSelectionConfig) (nodes []*pb.Node, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	fmt.Println("excluded ips")
-	for _, ip := range req.ExcludedIPs {
-		fmt.Println(ip)
-	}
 	// TODO: add sanity limits to requested node count
 	// TODO: add sanity limits to excluded nodes
 	reputableNodeCount := req.MinimumRequiredNodes
@@ -458,7 +453,8 @@ func (service *Service) FindStorageNodesDistinctIPs(ctx context.Context, req Fin
 		return nil, Error.Wrap(err)
 	}
 	req.ExcludedIPs = nodeIPs
-	preferences := &NodeSelectionConfig{DistinctIP: true}
+	preferences := &service.config.Node
+	preferences.DistinctIP = true
 
 	newNodes, err := service.FindStorageNodesWithPreferences(ctx, req, preferences)
 	if err != nil {
