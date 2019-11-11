5e171f27a311200eeafdf58f41285665c95232b8
Egon Elbre <egonelbre@gmail.com>
2019-11-11 16:03:04 +0200

    add check to new planet

--- satellite/satellitedb/satellitedbtest/pgschema.go
+++ satellite/satellitedb/satellitedbtest/pgschema.go
@@ -16,8 +16,8 @@ import (
 
 // NewPostgres returns the default postgres satellite.DB for testing.
 func NewPostgres(log *zap.Logger, schema string) (satellite.DB, error) {
-	if *pgtest.ConnStr == "" {
-		return nil, errs.New("flag --postgres-test-db or environment variable STORJ_POSTGRES_TEST not defined for PostgreSQL test database")
+	if err := PostgresDefined(); err != nil {
+		return nil, err
 	}
 
 	db, err := satellitedb.New(log, pgutil.ConnstrWithSchema(*pgtest.ConnStr, schema))
@@ -30,6 +30,14 @@ func NewPostgres(log *zap.Logger, schema string) (satellite.DB, error) {
 		Schema:   schema,
 		AutoDrop: true,
 	}, nil
+}
+
+// PostgresDefined returns an error when the --postgres-test-db or STORJ_POSTGRES_TEST is not set for tests.
+func PostgresDefined() error {
+	if *pgtest.ConnStr == "" {
+		return errs.New("flag --postgres-test-db or environment variable STORJ_POSTGRES_TEST not defined for PostgreSQL test database")
+	}
+	return nil
 }
 
 // SchemaDB implements automatic schema handling for satellite.DB
