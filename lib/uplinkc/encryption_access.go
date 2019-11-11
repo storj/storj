3284d0e16896c8411174ac148698b59634837501
TopperDEL <github_com@tparth.de>
2019-11-11 21:35:28 +0100

    Next try...getting closer...

--- lib/uplinkc/encryption_access.go
+++ lib/uplinkc/encryption_access.go
@@ -97,8 +97,8 @@ func restrict_encryption_access(encAccessRef C.EncryptionAccessRef, apikeyHandle
 	restrictionsGo := make([]libuplink.EncryptionRestriction, 0)
 	for i := 0; i < int(len(restrictionsArray)); i++ {
 		restrictionsGo = append(restrictionsGo, libuplink.EncryptionRestriction{
-			Bucket: C.GoString(&restrictionsArray[i].bucket),
-			PathPrefix: C.GoString(&restrictionsArray[i].path_prefix),
+			Bucket: C.GoString(*&restrictionsArray[i].bucket),
+			PathPrefix: C.GoString(&restrictionsArray[i].(*path_prefix)),
 		})
 	}
 	apikeyRestricted, encAccess, err := encAccess.Restrict(apikey, restrictionsGo...)
