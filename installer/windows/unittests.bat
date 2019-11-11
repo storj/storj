7779bb8d5b9ecd7da1a8ddf583c91caf2f3ce2d9
Kaloyan Raev <kaloyan@storj.io>
2019-11-11 22:06:48 +0200

    installer/windows: unit test mocking file system

--- installer/windows/unittests.bat
+++ installer/windows/unittests.bat
@@ -1,4 +1,5 @@
 rem install NuGet packages
+nuget install installer\windows\Storj\packages.config -o installer\windows\packages
 nuget install installer\windows\StorjTests\packages.config -o installer\windows\packages
 
 rem build the test project
