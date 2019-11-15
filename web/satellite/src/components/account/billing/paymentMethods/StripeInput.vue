27f97068caba66836932690a7631220fe1774df1
Qweder93 <nikolai.siedov@gmail.com>
2019-11-15 18:31:12 +0200

    web lint

--- web/satellite/src/components/account/billing/paymentMethods/StripeInput.vue
+++ web/satellite/src/components/account/billing/paymentMethods/StripeInput.vue
@@ -14,6 +14,7 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+
 import { MetaUtils } from '@/utils/meta';
 
 // StripeInput encapsulates Stripe add card addition logic
