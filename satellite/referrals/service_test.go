ca7d0976892f688fd97ff282ed6640e5a255c255
Yingrong Zhao <yingrong.zhao@gmail.com>
2019-11-20 16:11:59 -0500

    move mock server into test planet callback

--- satellite/referrals/service_test.go
+++ satellite/referrals/service_test.go
@@ -6,17 +6,17 @@ package referrals_test
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
-	"storj.io/storj/pkg/identity"
 	"storj.io/storj/pkg/pb"
+	"storj.io/storj/pkg/peertls/extensions"
 	"storj.io/storj/pkg/peertls/tlsopts"
+	"storj.io/storj/pkg/revocation"
 	"storj.io/storj/pkg/server"
 	"storj.io/storj/pkg/storj"
-	"storj.io/storj/private/errs2"
 	"storj.io/storj/private/testcontext"
 	"storj.io/storj/private/testidentity"
 	"storj.io/storj/private/testplanet"
@@ -24,24 +24,16 @@ import (
 	"storj.io/storj/satellite"
 )
 
-func TestConcurrentConnections(t *testing.T) {
-	// set up a mock referral manager server
-	ctx := testcontext.New(t)
+func TestService(t *testing.T) {
 	endpoint := &Endpoint{}
-	server, group, id := mockReferralManager(ctx, endpoint)
-	require.NotNil(t, server)
-	require.NotNil(t, group)
-	defer func() {
-		ctx.Cleanup()
-		_ = group.Wait()
-		server.Close()
-	}()
-
+	version := storj.LatestIDVersion()
+	identity, err := testidentity.NewPregeneratedSignedIdentities(version).NewIdentity()
+	require.NoError(t, err)
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1,
 		Reconfigure: testplanet.Reconfigure{
 			Satellite: func(logger *zap.Logger, index int, config *satellite.Config) {
-				addr := id.ID.String() + "@127.0.0.1:7777"
+				addr := identity.ID.String() + "@127.0.0.1:7777"
 				config.Referrals.ReferralManagerURL, _ = storj.ParseNodeURL(addr)
 			},
 		},
@@ -49,54 +41,53 @@ func TestConcurrentConnections(t *testing.T) {
 
 		satellite := planet.Satellites[0]
 
+		// set up mock referral manager server
+		wl, err := planet.WriteWhitelist(storj.LatestIDVersion())
+		require.NoError(t, err)
+		tlscfg := tlsopts.Config{
+			RevocationDBURL:     "bolt://" + filepath.Join(ctx.Dir("fakestoragenode"), "revocation.db"),
+			UsePeerCAWhitelist:  true,
+			PeerCAWhitelistPath: wl,
+			PeerIDVersions:      "*",
+			Extensions: extensions.Config{
+				Revocation:          false,
+				WhitelistSignedLeaf: false,
+			},
+		}
+
+		revocationDB, err := revocation.NewDBFromCfg(tlscfg)
+		require.NoError(t, err)
+
+		tlsOptions, err := tlsopts.NewOptions(identity, tlscfg, revocationDB)
+		require.NoError(t, err)
+
+		server, err := server.New(satellite.Log.Named("mock-server"), tlsOptions, "127.0.0.1:7777", "127.0.0.1:7778", nil)
+		require.NoError(t, err)
+		pb.DRPCRegisterReferralManager(server.DRPC(), endpoint)
+		go func() {
+			// TODO: get goroutine under control
+			err := server.Run(ctx)
+			require.NoError(t, err)
+
+			err = revocationDB.Close()
+			require.NoError(t, err)
+		}()
+		defer func() {
+			err = server.Close()
+			require.NoError(t, err)
+		}()
+
 		userID := testrand.UUID()
-		_, err := satellite.API.Referrals.Service.GetTokens(ctx, &userID)
+		_, err = satellite.API.Referrals.Service.GetTokens(ctx, &userID)
 		require.NoError(t, err)
 	})
-}
-
-func mockReferralManager(ctx context.Context, endpoint pb.DRPCReferralManagerServer) (*server.Server, *errgroup.Group, *identity.FullIdentity) {
-	type config struct {
-		Address        string
-		PrivateAddress string
-	}
-	version := storj.LatestIDVersion()
-	identity, err := testidentity.NewPregeneratedSignedIdentities(version).NewIdentity()
-	if err != nil {
-		return nil, nil, nil
-	}
-
-	serverConfig := config{
-		Address:        "127.0.0.1:7777",
-		PrivateAddress: "127.0.0.1:7778",
-	}
-
-	tlsOptions, err := tlsopts.NewOptions(identity, tlsopts.Config{}, nil)
-	if err != nil {
-		return nil, nil, nil
-	}
-
-	referralManager, err := server.New(nil, tlsOptions, serverConfig.Address, serverConfig.PrivateAddress, nil)
-	if err != nil {
-		return nil, nil, nil
-	}
-
-	pb.DRPCRegisterReferralManager(referralManager.DRPC(), endpoint)
-
-	group, ctx := errgroup.WithContext(ctx)
-
-	group.Go(func() error {
-		return errs2.IgnoreCanceled(referralManager.Run(ctx))
-	})
-
-	return referralManager, group, identity
 }
 
 type Endpoint struct{}
 
 func (endpoint *Endpoint) GetTokens(ctx context.Context, req *pb.GetTokensRequest) (*pb.GetTokensResponse, error) {
 	fmt.Println("getTokenEndpint")
-	return nil, nil
+	return &pb.GetTokensResponse{}, nil
 }
 
 func (endpoint *Endpoint) RedeemToken(ctx context.Context, req *pb.RedeemTokenRequest) (*pb.RedeemTokenResponse, error) {
