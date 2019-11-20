1f97e865cf36b97435d338f531d2eeee99646770
Moby von Briesen <mobyvb@gmail.com>
2019-11-20 15:10:24 -0500

    finish writing pending tests

--- satellite/gracefulexit/pending_test.go
+++ satellite/gracefulexit/pending_test.go
@@ -4,19 +4,23 @@
 package gracefulexit_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 
 	"storj.io/storj/pkg/pb"
+	"storj.io/storj/private/sync2"
 	"storj.io/storj/private/testcontext"
 	"storj.io/storj/private/testrand"
 	"storj.io/storj/satellite/gracefulexit"
 )
 
-func TestPendingHappyPath(t *testing.T) {
-	testcontext.New(t)
-	defer testcontext.Cleanup()
+func TestPendingBasic(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
 
 	newWork := gracefulexit.PendingTransfer{
 		path:             []byte("testbucket/testfile"),
@@ -31,7 +35,12 @@ func TestPendingHappyPath(t *testing.T) {
 	pending := gracefulexit.NewPendingMap()
 
 	// put should work
-	pending.Put(pieceID, pending)
+	err := pending.Put(pieceID, newWork)
+	require.NoError(t, err)
+
+	// put should return an error if the item already exists
+	err := pending.Put(pieceID, newWork)
+	require.Error(t, err)
 
 	// get should work
 	w, ok := pending.Get(pieceID)
@@ -43,24 +52,132 @@ func TestPendingHappyPath(t *testing.T) {
 	require.False(t, ok)
 
 	// IsFinished: there is remaining work to be done -> return false immediately
-	finished := pending.Isfinished(ctx)
+	finished := pending.IsFinished(ctx)
+	require.False(t, finished)
+
+	// finished should work
+	err = pending.Finish()
+	require.NoError(t, err)
+
+	// finished should error if already called
+	err = pending.Finish()
+	require.NoError(t, err)
+
+	// should not be allowed to Put new work after finished called
+	err = pending.Put(testrand.PieceID(), newWork)
+	require.Error(t, err)
+
+	// IsFinished: Finish has been called and there is remaining work -> return false
+	finished = pending.IsFinished(ctx)
 	require.False(t, finished)
 
 	// delete should work
-	err := pending.Delete(pieceID)
+	err = pending.Delete(pieceID)
 	require.NoError(t, err)
 	_, ok = pending.Get(pieceID)
 	require.False(t, ok)
 
-	// delete should return an error if the item does not exist?
+	// delete should return an error if the item does not exist
 	err = pending.Delete(pieceID)
 	require.Error(t, err)
 
-	// isFinished cases:
-	// DONE there is remaining work to be done -> return false immediately
-	// there is no remaining work to be done -> block and return false when new work added
-	// there is no remaining work to be done -> block and return true when finish is called
-	// there is no remaining work to be done -> block and return when ctx is canceled
-	// finish has been called and there is remaining work -> return false
-	// finish has been called and there is no remaining work -> return true
+	// IsFinished: Finish has been called and there is no remaining work -> return true
+	finished = pending.IsFinished(ctx)
+	require.True(t, finished)
+}
+
+// TestPendingIsFinishedWorkAdded ensures that pending.IsFinished blocks if there is no work, then returns false when new work is added
+func TestPendingIsFinishedWorkAdded(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	newWork := gracefulexit.PendingTransfer{
+		path:             []byte("testbucket/testfile"),
+		pieceSize:        10,
+		satelliteMessage: &pb.SatelliteMessage{},
+		originalPointer:  &pb.Pointer{},
+		pieceNum:         1,
+	}
+	pieceID := testrand.PieceID()
+	pending := gracefulexit.NewPendingMap()
+
+	fence := sync2.Fence{}
+	var group errgroup.Group
+	group.Go(func() error {
+		// expect no work
+		size := pending.Length()
+		require.EqualValues(t, size, 0)
+
+		fence.Release()
+		finished := pending.IsFinished(ctx)
+		require.False(t, finished)
+
+		// expect new work to be added
+		size = pending.Length()
+		require.EqualValues(t, size, 1)
+		return nil
+	})
+	group.Go(func() error {
+		// wait for IsFinished call to begin before adding work
+		require.True(t, fence.Wait(ctx))
+		time.Sleep(50 * time.Millisecond)
+
+		err := pending.Put(pieceID, newWork)
+		require.NoError(t, err)
+		return nil
+	})
+
+	require.NoError(t, group.Wait())
+}
+
+// TestPendingIsFinishedFinishedCalled ensures that pending.IsFinished blocks if there is no work, then returns true when Finished is called
+func TestPendingIsFinishedFinishedCalled(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	fence := sync2.Fence{}
+	var group errgroup.Group
+	group.Go(func() error {
+		fence.Release()
+		finished := pending.IsFinished(ctx)
+		require.True(t, finished)
+		return nil
+	})
+	group.Go(func() error {
+		// wait for IsFinished call to begin before finishing
+		require.True(t, fence.Wait(ctx))
+		time.Sleep(50 * time.Millisecond)
+
+		err := pending.Finish()
+		require.NoError(t, err)
+		return nil
+	})
+
+	require.NoError(t, group.Wait())
+}
+
+// TestPendingIsFinishedCtxCanceled ensures that pending.IsFinished blocks if there is no work, then returns true when context is canceled
+func TestPendingIsFinishedCtxCanceled(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	ctx2, cancel := context.WithCancel(ctx)
+	fence := sync2.Fence{}
+	var group errgroup.Group
+	group.Go(func() error {
+		fence.Release()
+		finished := pending.IsFinished(ctx2)
+		require.True(t, finished)
+		return nil
+	})
+	group.Go(func() error {
+		// wait for IsFinished call to begin before canceling
+		require.True(t, fence.Wait(ctx))
+		time.Sleep(50 * time.Millisecond)
+
+		cancel()
+		return nil
+	})
+
+	require.NoError(t, group.Wait())
 }
