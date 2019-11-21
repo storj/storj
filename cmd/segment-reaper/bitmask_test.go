6c45e2935096c2e51b3b9adcb3dbed76002b841a
Ivan Fraixedes <ivan@fraixed.es>
2019-11-21 14:27:49 +0100

    REVIEW: Add more tests cases

--- cmd/segment-reaper/bitmask_test.go
+++ cmd/segment-reaper/bitmask_test.go
@@ -83,17 +83,75 @@ func TestBitmask(t *testing.T) {
 	})
 
 	t.Run("Set and Has", func(t *testing.T) {
-		var (
-			expectedIdx = rand.Intn(64)
-			mask        bitmask
-		)
+		t.Run("index not set", func(t *testing.T) {
+			var (
+				expectedIdx = rand.Intn(64)
+				mask        bitmask
+			)
 
-		has, err := mask.Has(expectedIdx)
-		require.NoError(t, err, "Has")
-		assert.False(t, has, "expected tracked index")
+			has, err := mask.Has(expectedIdx)
+			require.NoError(t, err, "Has")
+			assert.False(t, has, "expected tracked index")
+		})
 
-		err = mask.Set(expectedIdx)
-		require.NoError(t, err, "Set")
+		t.Run("index is set", func(t *testing.T) {
+			var (
+				expectedIdx = rand.Intn(64)
+				mask        bitmask
+			)
+
+			err := mask.Set(expectedIdx)
+			require.NoError(t, err, "Set")
+
+			has, err := mask.Has(expectedIdx)
+			require.NoError(t, err, "Has")
+			assert.True(t, has, "expected tracked index")
+		})
+
+		t.Run("same index is set more than once", func(t *testing.T) {
+			var (
+				expectedIdx = rand.Intn(63)
+				times       = rand.Intn(10) + 2
+				mask        bitmask
+			)
+
+			for i := 0; i < times; i++ {
+				err := mask.Set(expectedIdx)
+				require.NoError(t, err, "Set")
+			}
+
+			has, err := mask.Has(expectedIdx)
+			require.NoError(t, err, "Has")
+			assert.True(t, has, "expected tracked index")
+
+			// Another index isn't set
+			has, err = mask.Has(expectedIdx + 1)
+			require.NoError(t, err, "Has")
+			assert.False(t, has, "not expected tracked index")
+		})
+
+		t.Run("several indexes are set", func(t *testing.T) {
+			var (
+				numIndexes = rand.Intn(61) + 2
+				indexes    = make([]int, numIndexes)
+				mask       bitmask
+			)
+
+			for i := 0; i < numIndexes; i++ {
+				idx := rand.Intn(63)
+				indexes[i] = idx
+
+				err := mask.Set(idx)
+				require.NoError(t, err, "Set")
+			}
+
+			for _, idx := range indexes {
+				has, err := mask.Has(idx)
+				require.NoError(t, err, "Has")
+				assert.True(t, has, "expected tracked index")
+			}
+		})
+	})
 
 		has, err = mask.Has(expectedIdx)
 		require.NoError(t, err, "Has")
