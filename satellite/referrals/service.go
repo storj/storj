b00c60d46bf397ae532e985262d0bda4ac5f01d2
Yingrong Zhao <yingrong.zhao@gmail.com>
2019-11-21 16:31:49 -0500

    add referrals config

---
+++ satellite/referrals/service.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package referrals
+
+import "storj.io/storj/pkg/storj"
+
+type Config struct {
+	ReferralManagerURL storj.NodeURL
+}
