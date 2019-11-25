839935eb4ab4a9c8e5100d69628e083d21709e74
Bryan White <bryanchriswhite@gmail.com>
2019-11-25 17:59:27 +0100

    revert build.bat

--- installer/windows/build.bat
+++ installer/windows/build.bat
@@ -1,17 +1,9 @@
-@echo off
-
-rem count # of args
-set argC=0
-for %%x in (%*) do Set /A argC+=1
-
-if not "%argC%"=="3" (
-    echo usage: %~nx0 ^<storagenode.exe input path^> ^<storagenode-updater.exe input path^> ^<msi output path^>
-    exit /B 1
-)
+rem set the TAG env var from the release dir
+for /f %%i in ('dir /B release') do set TAG=%%i
 
 rem copy the storagenode binaries to the installer project
-copy %1 installer\windows\storagenode.exe
-copy %2 installer\windows\storagenode-updater.exe
+copy release\%TAG%\storagenode_windows_amd64.exe installer\windows\storagenode.exe
+copy release\%TAG%\storagenode-updater_windows_amd64.exe installer\windows\storagenode-updater.exe
 
 rem install NuGet packages
 nuget install installer\windows\StorjTests\packages.config -o installer\windows\packages
@@ -19,9 +11,5 @@ nuget install installer\windows\StorjTests\packages.config -o installer\windows\
 rem build the installer
 msbuild installer\windows\windows.sln /t:Build /p:Configuration=Release
 
-rem cleanup copied binaries
-del installer\windows\storagenode.exe
-del installer\windows\storagenode-updater.exe
-
 rem copy the MSI to the release dir
-copy installer\windows\bin\Release\storagenode.msi %3
+copy installer\windows\bin\Release\storagenode.msi release\%TAG%\storagenode_windows_amd64.msi
