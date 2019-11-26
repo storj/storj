0aa3bc18f870a06d7be761080ed98a80a297ad75
Michal Niewrzal <michal@storj.io>
2019-11-26 17:35:49 +0100

    segment-reaper: delete command logic

---
+++ cmd/segment-reaper/delete_test.go
@@ -0,0 +1,82 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+	"storj.io/storj/pkg/pb"
+	"storj.io/storj/private/testcontext"
+	"storj.io/storj/satellite/metainfo"
+	"storj.io/storj/storage"
+)
+
+func TestDeleteSegment(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	db, err := metainfo.NewStore(zaptest.NewLogger(t), "bolt://"+filepath.Join(ctx.Dir(), "pointers.db"))
+	require.NoError(t, err)
+	defer ctx.Check(db.Close)
+
+	{
+		err := makeSegment(ctx, db, "path1", time.Unix(10, 0))
+		require.NoError(t, err)
+
+		dryRun := false
+		deleteError := deleteSegment(ctx, db, "path1", time.Unix(10, 0), dryRun)
+		require.NoError(t, deleteError)
+		_, err = db.Get(ctx, storage.Key("path1"))
+		require.Error(t, err) // segment is deleted
+	}
+	{
+		err := makeSegment(ctx, db, "path2", time.Unix(10, 0))
+		require.NoError(t, err)
+
+		dryRun := true
+		deleteError := deleteSegment(ctx, db, "path2", time.Unix(10, 0), dryRun)
+		require.NoError(t, deleteError)
+		_, err = db.Get(ctx, storage.Key("path2"))
+		require.NoError(t, err) // segment is not deleted because of dryRun
+	}
+	{
+		err := makeSegment(ctx, db, "path3", time.Unix(10, 0))
+		require.NoError(t, err)
+
+		dryRun := false
+		deleteError := deleteSegment(ctx, db, "path3", time.Unix(99, 0), dryRun)
+		require.Error(t, deleteError)
+		_, err = db.Get(ctx, storage.Key("path3"))
+		require.NoError(t, err) // segment is not deleted because of time mismatch
+	}
+	{
+		dryRun := false
+		deleteError := deleteSegment(ctx, db, "not-existing-path", time.Unix(10, 0), dryRun)
+		require.Error(t, deleteError)
+	}
+}
+
+func makeSegment(ctx context.Context, db metainfo.PointerDB, path string, creationDate time.Time) error {
+	pointer := &pb.Pointer{
+		CreationDate: creationDate,
+	}
+
+	pointerBytes, err := proto.Marshal(pointer)
+	if err != nil {
+		return err
+	}
+
+	err = db.Put(ctx, storage.Key(path), storage.Value(pointerBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
