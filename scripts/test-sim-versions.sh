a45c2bc16fa6aaee03a634e4520d0705d650afd9
Yingrong Zhao <yingrong.zhao@gmail.com>
2019-12-03 16:39:15 -0500

    cleanup worktree before start adding. add a suffix for sed command: The BSD sed command takes the -i option but requires a suffix for the backup (but an empty suffix is permitted

--- scripts/test-sim-versions.sh
+++ scripts/test-sim-versions.sh
@@ -42,7 +42,7 @@ replace_in_file(){
     local src="$1"
     local dest="$2"
     local path=$3
-    sed -i "s#${src}#${dest}#g" "${path}"
+    sed -i '' "s#${src}#${dest}#g" "${path}"
 }
 
 setup_stage(){
@@ -118,6 +118,7 @@ for version in ${unique_versions}; do
 
     echo -e "\nAdding worktree for ${version} in ${dir}."
     git fetch --tags
+    git worktree prune
     git worktree add -f ${dir} ${version}
     rm ${dir}/internal/version/release.go
     echo "Installing storj-sim for ${version} in ${dir}."
