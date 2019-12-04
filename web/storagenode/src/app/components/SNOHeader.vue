fa02326cb3e6485b63cfcdce898adfa6e84d078b
NikolaiYurchenko <nickolai.yurchenko@gmail.com>
2019-12-04 17:56:54 +0200

    bell icon in header added

--- web/storagenode/src/app/components/SNOHeader.vue
+++ web/storagenode/src/app/components/SNOHeader.vue
@@ -13,9 +13,15 @@
                     <RefreshIcon alt="refresh image"/>
                 </div>
             </div>
-            <div class="header__content-holder__node-id-container">
-                <b class="header__content-holder__node-id-container__title">Node ID:</b>
-                <p class="header__content-holder__node-id-container__id">{{this.nodeId}}</p>
+            <div class="header__content-holder__right-area">
+                <div class="header__content-holder__right-area__node-id-container">
+                    <b class="header__content-holder__right-area__node-id-container__title">Node ID:</b>
+                    <p class="header__content-holder__right-area__node-id-container__id">{{this.nodeId}}</p>
+                </div>
+                <div class="header__content-holder__right-area__bell-area">
+                    <BellIcon />
+                    <span class="header__content-holder__right-area__bell-area__new-circle"></span>
+                </div>
             </div>
         </div>
     </div>
@@ -24,6 +30,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+import BellIcon from '@/../static/images/notifications/bell.svg';
 import RefreshIcon from '@/../static/images/refresh.svg';
 import StorjIcon from '@/../static/images/storjIcon.svg';
 
@@ -38,9 +45,12 @@ const {
     components: {
         StorjIcon,
         RefreshIcon,
+        BellIcon,
     },
 })
 export default class SNOHeader extends Vue {
+    public notificationsPath: string = RouteConfig.Notifications.path;
+
     public async onRefresh(): Promise<void> {
         const selectedSatellite = this.$store.state.node.selectedSatellite.id;
 
@@ -57,6 +67,7 @@ export default class SNOHeader extends Vue {
     }
 }
 </script>
+
 
 <style scoped lang="scss">
     .header {
@@ -94,24 +105,52 @@ export default class SNOHeader extends Vue {
                 }
             }
 
-            &__node-id-container {
-                color: #535f77;
-                height: 44px;
-                padding: 0 14px 0 14px;
+            &__right-area {
                 display: flex;
                 align-items: center;
-                border: 1px solid #e8e8e8;
-                border-radius: 12px;
-                font-size: 14px;
+                justify-content: flex-end;
 
-                &__title {
-                    min-width: 55px;
-                    margin-right: 5px;
-                    user-select: none;
+                &__node-id-container {
+                    color: #535f77;
+                    height: 44px;
+                    padding: 0 14px 0 14px;
+                    display: flex;
+                    align-items: center;
+                    border: 1px solid #e8e8e8;
+                    border-radius: 12px;
+                    font-size: 14px;
+                    margin-right: 30px;
+
+                    &__title {
+                        min-width: 55px;
+                        margin-right: 5px;
+                        user-select: none;
+                    }
+
+                    &__id {
+                        font-size: 11px;
+                    }
                 }
 
-                &__id {
-                    font-size: 11px;
+                &__bell-area {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    position: relative;
+                    width: 26px;
+                    height: 32px;
+                    cursor: pointer;
+
+                    &__new-circle {
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        display: inline-block;
+                        width: 6px;
+                        height: 6px;
+                        border-radius: 50%;
+                        background-color: #eb001b;
+                    }
                 }
             }
         }
