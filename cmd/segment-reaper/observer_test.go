93900ae6d44064aa724c9b894fed1d4718805cf0
Michal Niewrzal <michal@storj.io>
2019-12-04 16:50:51 +0100

    more cases

--- cmd/segment-reaper/observer_test.go
+++ cmd/segment-reaper/observer_test.go
@@ -6,6 +6,7 @@ package main
 import (
 	"context"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,6 +16,14 @@ import (
 	"storj.io/storj/private/testrand"
 )
 
+const (
+	broken    = true
+	notBroken = false
+
+	lastSegYES = true
+	lastSegNO  = false
+)
+
 func Test_observer_analyzeProject(t *testing.T) {
 	ctx := testcontext.New(t)
 	defer ctx.Cleanup()
@@ -22,13 +31,27 @@ func Test_observer_analyzeProject(t *testing.T) {
 	tests := []struct {
 		segments                 string
 		expectedNumberOfSegments byte
-		hasLastSegment           bool
 		skip                     bool
+		segmentsAfter            string
 		brokenObject             bool
 	}{
-		{"11110000", 5, true, false, false}, // #0
-		{"00111000", 0, true, false, true},  // #1
-		// TODO add more cases
+		// known number of segments
+		{"11111_l", 6, false, "11111", notBroken}, // #0
+		{"00000_l", 1, false, "00000", notBroken}, // #1
+		{"1111100", 6, false, "11111", broken},    // #2
+		{"11011_l", 6, false, "11011", broken},    // #3
+		{"11011_l", 3, false, "00011", broken},    // #4
+		{"11110_l", 6, false, "11110", broken},    // #5
+
+		// unknown number of segments
+		{"11111_l", 0, false, "00000", notBroken}, // #6
+		{"1111100", 0, false, "11111", broken},    // #7
+		{"00111_l", 0, false, "00111", broken},    // #8
+		{"10111_l", 0, false, "00111", broken},    // #9
+		{"11011_l", 0, false, "00011", broken},    // #10
+
+		// skip
+		{"1101100", 0, true, "1101100", notBroken}, // #11
 	}
 	for i, tt := range tests {
 		t.Run("case_"+strconv.Itoa(i), func(t *testing.T) {
@@ -36,17 +59,22 @@ func Test_observer_analyzeProject(t *testing.T) {
 			singleObjectMap := make(map[storj.Path]*object)
 			segments := bitmask(0)
 			for i, char := range tt.segments {
+				if char == '_' {
+					break
+				}
 				if char == '1' {
 					err := segments.Set(i)
 					require.NoError(t, err)
 				}
 			}
-			singleObjectMap["test-path"] = &object{
+
+			object := &object{
 				segments:                 segments,
-				hasLastSegment:           tt.hasLastSegment,
+				hasLastSegment:           strings.HasSuffix(tt.segments, "_l"),
 				expectedNumberOfSegments: tt.expectedNumberOfSegments,
 				skip:                     tt.skip,
 			}
+			singleObjectMap["test-path"] = object
 			bucketObjects["test-bucket"] = singleObjectMap
 
 			observer := &observer{
@@ -60,7 +88,19 @@ func Test_observer_analyzeProject(t *testing.T) {
 			})
 			require.NoError(t, err)
 
-			require.Equal(t, tt.brokenObject, brokenObject, "case %s failed", i)
+			segmentsAfter := bitmask(0)
+			for i, char := range tt.segmentsAfter {
+				if char == '1' {
+					err := segmentsAfter.Set(i)
+					require.NoError(t, err)
+				}
+			}
+
+			if object.segments != segmentsAfter {
+				t.Fatalf("segments before and after comparison faild, case %d ", i)
+			}
+
+			require.Equal(t, tt.brokenObject, brokenObject, "case %d failed", i)
 		})
 	}
 }
