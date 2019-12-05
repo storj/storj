acf1b9a258bd84adcd2c00130faa58f4e9ae14d6
littleskunk <jens.heimbuerge@googlemail.com>
2019-12-05 20:16:06 +0100

    release v0.27.1-rc-crdb

---
+++ private/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1575573365"
+	buildCommitHash = "edba49d75bf0bebac303789fcfabed99760ee757"
+	buildVersion = "v0.27.1-rc-crdb"
+	buildRelease = "true"
+}
