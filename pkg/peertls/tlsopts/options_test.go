9ed9d3516fd98d048a296a9ea7b028c7e0267c99
Egon Elbre <egonelbre@gmail.com>
2019-12-18 17:08:54 +0200

    pkg/peertls: move tests requiring redis or bolt
    Change-Id: Ib9de8d5ac1123d109b0209de4174fb4da7c67078

--- pkg/peertls/tlsopts/options_test.go
+++ pkg/peertls/tlsopts/options_test.go
@@ -4,120 +4,16 @@
 package tlsopts_test
 
 import (
-	"io/ioutil"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"storj.io/storj/pkg/identity"
-	"storj.io/storj/pkg/peertls"
-	"storj.io/storj/pkg/peertls/extensions"
 	"storj.io/storj/pkg/peertls/tlsopts"
-	"storj.io/storj/pkg/revocation"
 	"storj.io/storj/pkg/storj"
-	"storj.io/storj/private/testcontext"
 	"storj.io/storj/private/testidentity"
 )
-
-func TestNewOptions(t *testing.T) {
-	// TODO: this is not a great test...
-	ctx := testcontext.New(t)
-	defer ctx.Cleanup()
-
-	fi, err := testidentity.PregeneratedIdentity(0, storj.LatestIDVersion())
-	require.NoError(t, err)
-
-	whitelistPath := ctx.File("whitelist.pem")
-
-	chainData, err := peertls.ChainBytes(fi.CA)
-	assert.NoError(t, err)
-
-	err = ioutil.WriteFile(whitelistPath, chainData, 0644)
-	assert.NoError(t, err)
-
-	cases := []struct {
-		testID                     string
-		config                     tlsopts.Config
-		clientVerificationFuncsLen int
-		serverVerificationFuncsLen int
-	}{
-		{
-			"default",
-			tlsopts.Config{},
-			1, 1,
-		}, {
-			"revocation processing",
-			tlsopts.Config{
-				RevocationDBURL: "bolt://" + ctx.File("revocation1.db"),
-				Extensions: extensions.Config{
-					Revocation: true,
-				},
-			},
-			1, 1,
-		}, {
-			"ca whitelist verification",
-			tlsopts.Config{
-				PeerCAWhitelistPath: whitelistPath,
-				UsePeerCAWhitelist:  true,
-			},
-			2, 1,
-		}, {
-			"ca whitelist verification and whitelist signed leaf verification",
-			tlsopts.Config{
-				// NB: file doesn't actually exist
-				PeerCAWhitelistPath: whitelistPath,
-				UsePeerCAWhitelist:  true,
-				Extensions: extensions.Config{
-					WhitelistSignedLeaf: true,
-				},
-			},
-			2, 1,
-		}, {
-			"revocation processing and whitelist verification",
-			tlsopts.Config{
-				// NB: file doesn't actually exist
-				PeerCAWhitelistPath: whitelistPath,
-				UsePeerCAWhitelist:  true,
-				RevocationDBURL:     "bolt://" + ctx.File("revocation2.db"),
-				Extensions: extensions.Config{
-					Revocation: true,
-				},
-			},
-			2, 1,
-		}, {
-			"revocation processing, whitelist, and signed leaf verification",
-			tlsopts.Config{
-				// NB: file doesn't actually exist
-				PeerCAWhitelistPath: whitelistPath,
-				UsePeerCAWhitelist:  true,
-				RevocationDBURL:     "bolt://" + ctx.File("revocation3.db"),
-				Extensions: extensions.Config{
-					Revocation:          true,
-					WhitelistSignedLeaf: true,
-				},
-			},
-			2, 1,
-		},
-	}
-
-	for _, c := range cases {
-		t.Log(c.testID)
-
-		revocationDB, err := revocation.NewDBFromCfg(c.config)
-		require.NoError(t, err)
-
-		tlsOptions, err := tlsopts.NewOptions(fi, c.config, revocationDB)
-		assert.NoError(t, err)
-		assert.True(t, reflect.DeepEqual(fi, tlsOptions.Ident))
-		assert.Equal(t, c.config, tlsOptions.Config)
-		assert.Len(t, tlsOptions.VerificationFuncs.Client(), c.clientVerificationFuncsLen)
-		assert.Len(t, tlsOptions.VerificationFuncs.Server(), c.serverVerificationFuncsLen)
-
-		require.NoError(t, revocationDB.Close())
-	}
-}
 
 func TestOptions_DialOption_error_on_empty_ID(t *testing.T) {
 	testidentity.CompleteIdentityVersionsTest(t, func(t *testing.T, version storj.IDVersion, ident *identity.FullIdentity) {
