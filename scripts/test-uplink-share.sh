2b3afad6fbdd593b34a3c27d16ab9cdb460f0271
igaidaienko <igorgaydaenko@gmail.com>
2020-01-15 15:51:22 +0200

    Replace "scope" to "access"

--- scripts/test-uplink-share.sh
+++ scripts/test-uplink-share.sh
@@ -4,11 +4,11 @@ TMPDIR=$(mktemp -d -t tmp.XXXXXXXXXX)
 
 cleanup(){
     rm -rf "$TMPDIR"
-    uplink rm "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/testfile"
-    uplink rm "sj://$BUCKET_WITH_ACCESS/another-testfile"
-    uplink rm "sj://$BUCKET_WITHOUT_ACCESS/another-testfile"
-    uplink rb "sj://$BUCKET_WITHOUT_ACCESS"
-    uplink rb "sj://$BUCKET_WITH_ACCESS"
+    uplink --access "$GATEWAY_0_ACCESS" rm "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/testfile"
+    uplink --access "$GATEWAY_0_ACCESS" rm "sj://$BUCKET_WITH_ACCESS/another-testfile"
+    uplink --access "$GATEWAY_0_ACCESS" rm "sj://$BUCKET_WITHOUT_ACCESS/another-testfile"
+    uplink --access "$GATEWAY_0_ACCESS" rb "sj://$BUCKET_WITHOUT_ACCESS"
+    uplink --access "$GATEWAY_0_ACCESS" rb "sj://$BUCKET_WITH_ACCESS"
     echo "cleaned up test successfully"
 }
 trap cleanup EXIT
@@ -31,15 +31,15 @@ random_bytes_file () {
 
 random_bytes_file "2048"       "$SRC_DIR/another-testfile"  # create 2kb file of random bytes (inline)
 random_bytes_file "5120"       "$SRC_DIR/testfile"          # create 5kb file of random bytes (inline)
-uplink mb "sj://$BUCKET_WITHOUT_ACCESS/"
-uplink mb "sj://$BUCKET_WITH_ACCESS/"
+uplink --access "$GATEWAY_0_ACCESS" mb "sj://$BUCKET_WITHOUT_ACCESS/"
+uplink --access "$GATEWAY_0_ACCESS" mb "sj://$BUCKET_WITH_ACCESS/"
 
-uplink cp --progress=false "$SRC_DIR/testfile" "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/"
-uplink cp --progress=false "$SRC_DIR/another-testfile" "sj://$BUCKET_WITH_ACCESS/"
-uplink cp --progress=false "$SRC_DIR/another-testfile" "sj://$BUCKET_WITHOUT_ACCESS/"
+uplink --access "$GATEWAY_0_ACCESS" cp "$SRC_DIR/testfile" "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/"
+uplink --access "$GATEWAY_0_ACCESS" cp "$SRC_DIR/another-testfile" "sj://$BUCKET_WITH_ACCESS/"
+uplink --access "$GATEWAY_0_ACCESS" cp "$SRC_DIR/another-testfile" "sj://$BUCKET_WITHOUT_ACCESS/"
 
-# Make scope with readonly rights
-scope=$(uplink share --allowed-path-prefix sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/ --readonly | grep scope | cut -d: -f2)
+# Make access with readonly rights
+access=$(uplink --access "$GATEWAY_0_ACCESS" share --allowed-path-prefix sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/ --readonly | grep Access | cut -d: -f2)
 
 check_exit_code(){
 	if [ $1 -eq 0 ]; then
@@ -50,19 +50,19 @@ check_exit_code(){
 	fi
 }
 
-uplink cp --progress=false "$SRC_DIR/another-testfile" "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/" --scope $scope
+uplink --access "$GATEWAY_0_ACCESS" cp "$SRC_DIR/another-testfile" "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/" --access $access
 retVal=$?
 check_exit_code $retVal
 
-uplink cp --progress=false "$SRC_DIR/testfile" "sj://$BUCKET_WITHOUT_ACCESS/" --scope $scope
+uplink --access "$GATEWAY_0_ACCESS" cp "$SRC_DIR/testfile" "sj://$BUCKET_WITHOUT_ACCESS/" --access $access
 retVal=$?
 check_exit_code $retVal
 
-uplink cp --progress=false "sj://$BUCKET_WITHOUT_ACCESS/another-testfile" "$SRC_DIR/" --scope $scope
+uplink --access "$GATEWAY_0_ACCESS" cp "sj://$BUCKET_WITHOUT_ACCESS/another-testfile" "$SRC_DIR/" --access $access
 retVal=$?
 check_exit_code $retVal
 
-number_of_buckets=$(uplink ls --scope $scope | wc -l)
+number_of_buckets=$(uplink --access "$GATEWAY_0_ACCESS" ls --access $access | wc -l)
 
 # We share one bucket, so we expect to see only one bucket in the output of ls command
 if [ $number_of_buckets -eq 1 ]; then
@@ -72,7 +72,7 @@ else
 	exit 1
 fi
 
-uplink cp --progress=false "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/testfile" "$DST_DIR" --scope $scope
+uplink --access "$GATEWAY_0_ACCESS" cp "sj://$BUCKET_WITH_ACCESS/$FOLDER_TO_SHARE_FILE/testfile" "$DST_DIR" --access $access
 
 if cmp "$SRC_DIR/testfile" "$DST_DIR/testfile"; then
     echo "Testfile matches uploaded file: PASSED"
