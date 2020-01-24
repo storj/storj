8ee9fa15e9c10e8c7f1165a6cd79bc097e94665b
Matt Robinson <mrobinson@storj.io>
2020-01-24 15:07:12 -0500

    release v0.30.7
    Change-Id: I3f5c9fb50cd4bea0da52f5c4b1d8bf799ec42142

---
+++ private/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1579896426"
+	buildCommitHash = "91a09652f0d21c5cc83fab068b8238c41179bbe6"
+	buildVersion = "v0.30.7"
+	buildRelease = "true"
+}
