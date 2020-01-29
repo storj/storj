28b6ea591a586efc99f8fcd3ffc6fd275d35d077
littleskunk <jens.heimbuerge@googlemail.com>
2020-01-29 04:01:09 +0100

    satellite/projectusage: reduce usage limit from 25GB to 5GB
    Change-Id: I2819012b520fd687ab8058000aa38d76b8208158
    (cherry picked from commit a0c9f7f3b05d23df607339c3e8d813ee4a7e41d1)

--- satellite/accounting/rollup/rollup.go
+++ satellite/accounting/rollup/rollup.go
@@ -19,7 +19,7 @@ import (
 // Config contains configurable values for rollup
 type Config struct {
 	Interval      time.Duration `help:"how frequently rollup should run" releaseDefault:"24h" devDefault:"120s"`
-	MaxAlphaUsage memory.Size   `help:"the bandwidth and storage usage limit for the alpha release" default:"25GB"`
+	MaxAlphaUsage memory.Size   `help:"the bandwidth and storage usage limit for the alpha release" default:"5GB"`
 	DeleteTallies bool          `help:"option for deleting tallies after they are rolled up" default:"true"`
 }
 
