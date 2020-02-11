ba4babcbc730095ede70aaf92eeed0e8ccbb95fa
littleskunk <jens.heimbuerge@googlemail.com>
2020-02-11 23:34:52 +0100

    release v0.33.1-rc

---
+++ private/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1581460492"
+	buildCommitHash = "71cd88a8388477976e37a3a46f29b7bfe23540b4"
+	buildVersion = "v0.33.1-rc"
+	buildRelease = "true"
+}
