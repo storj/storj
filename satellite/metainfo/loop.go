5f9e88e5fd156a6cbda1c448fb471fc02e8ab933
JT Olio <hello@jtolio.com>
2020-02-13 04:01:39 -0700

    satellite/gc: skip first gc run
    rationale: if GC kills the satellite, it would be nice to make
    it through a repair checker sweep first
    
    Change-Id: Id56171dc8e13940cfb6481e36a910bad077a01ed
    (cherry picked from commit 2ae99783048d246072b9ca57ceb6745d828434b1)

--- satellite/metainfo/loop.go
+++ satellite/metainfo/loop.go
@@ -33,6 +33,25 @@ type Observer interface {
 	InlineSegment(context.Context, ScopedPath, *pb.Pointer) error
 }
 
+// NullObserver is an observer that does nothing. This is useful for joining
+// and ensuring the metainfo loop runs once before you use a real observer
+type NullObserver struct{}
+
+// Object implements the Observer interface
+func (NullObserver) Object(context.Context, ScopedPath, *pb.Pointer) error {
+	return nil
+}
+
+// RemoteSegment implements the Observer interface
+func (NullObserver) RemoteSegment(context.Context, ScopedPath, *pb.Pointer) error {
+	return nil
+}
+
+// InlineSegment implements the Observer interface
+func (NullObserver) InlineSegment(context.Context, ScopedPath, *pb.Pointer) error {
+	return nil
+}
+
 // ScopedPath contains full expanded information about the path.
 type ScopedPath struct {
 	ProjectID           uuid.UUID
