3d6518081ad3ab0506771bf26d37b791386a86a8
Egon Elbre <egonelbre@gmail.com>
2020-03-11 20:20:23 +0200

    satellite/metainfo/piecedeletion: add Combiner
    To handle concurrent deletion requests we need to combine them into a
    single request.
    
    To implement this we introduces few concurrency ideas:
    
    * Combiner, which takes a node id and a Job and handles combining
      multiple requests to a single batch.
    
    * Job, which represents deleting of multiple piece ids with a
      notification mechanism to the caller.
    
    * Queue, which provides communication from Combiner to Handler.
      It can limit the number of requests per work queue.
    
    * Handler, which takes an active Queue and processes it until it has
      consumed all the jobs.
      It can provide limits to handling concurrency.
    
    Change-Id: I3299325534abad4bae66969ffa16c6ed95d5574f

---
+++ satellite/metainfo/piecedeletion/doc.go
@@ -0,0 +1,5 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+// Package piecedeletion implements service for deleting pieces that combines concurrent requests.
+package piecedeletion
