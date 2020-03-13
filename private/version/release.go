c61d2b22b8887d05cb782aab1492062a8178ea56
Stefan Benten <mail@stefan-benten.de>
2020-03-13 17:16:38 +0100

    release v0.34.10
    Change-Id: If8d34717d6e5c1994d78e52cd864ff27b44e62eb

---
+++ private/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1584116166"
+	buildCommitHash = "2ff553f3fd9a4778217581d95ea366e595c74274"
+	buildVersion = "v0.34.10"
+	buildRelease = "true"
+}
