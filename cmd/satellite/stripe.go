40181f5f1718c9b3d567d1cebd7577cf41b5d219
stefanbenten <mail@stefan-benten.de>
2020-03-16 15:01:57 +0100

    Add Snippet to ensure we have a customer on stripe for every satellite user
    Change-Id: I1daf5299b5a43bdb076ae6ed221b6e65a3a1020a

---
+++ cmd/satellite/stripe.go
@@ -0,0 +1,103 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"context"
+
+	"github.com/prometheus/common/log"
+	"github.com/skyrings/skyring-common/tools/uuid"
+	"github.com/zeebo/errs"
+	"go.uber.org/zap"
+
+	"storj.io/storj/private/dbutil"
+	"storj.io/storj/satellite"
+	"storj.io/storj/satellite/payments"
+	"storj.io/storj/satellite/payments/stripecoinpayments"
+	"storj.io/storj/satellite/satellitedb"
+	"storj.io/storj/satellite/satellitedb/dbx"
+)
+
+type Payments struct {
+	Accounts payments.Accounts
+}
+
+type UserData struct {
+	ID    uuid.UUID
+	Email string
+}
+
+// generateStripeCustomers creates missing stripe-customers for users in our database
+func generateStripeCustomers(ctx context.Context) error {
+	db, err := satellitedb.New(zap.L().Named("db"), runCfg.Database, satellitedb.Options{})
+	if err != nil {
+		return errs.New("error connecting to master database on satellite: %+v", err)
+	}
+	defer func() {
+		err = errs.Combine(err, db.Close())
+	}()
+
+	driver, source, implementation, err := dbutil.SplitConnStr(runCfg.Database)
+	if err != nil {
+		return err
+	}
+	if implementation != dbutil.Postgres && implementation != dbutil.Cockroach {
+		return errs.New("unsupported driver %q", driver)
+	}
+
+	dbxDB, err := dbx.Open(driver, source)
+	if err != nil {
+		return errs.New("failed opening database via DBX at %q: %v",
+			source, err)
+	}
+	log.Debug("Connected to:", zap.String("db source", source))
+
+	handler, err := setupPayments(zap.L().Named("payments"), db)
+	if err != nil {
+		return err
+	}
+
+	rows, err := dbxDB.Query(ctx, "SELECT id, email FROM users WHERE email NOT IN (SELECT email from stripe_coinpayments")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user UserData
+		err := rows.Scan(&user)
+		if err != nil {
+			return err
+		}
+
+		err = handler.Accounts.Setup(ctx, user.ID, user.Email)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func setupPayments(log *zap.Logger, db satellite.DB) (handler *Payments, err error) {
+	pc := runCfg.Payments
+	service, err := stripecoinpayments.NewService(
+		log.Named("payments.stripe:service"),
+		pc.StripeCoinPayments,
+		db.StripeCoinPayments(),
+		db.Console().Projects(),
+		db.ProjectAccounting(),
+		pc.StorageTBPrice,
+		pc.EgressTBPrice,
+		pc.ObjectPrice,
+		pc.BonusRate)
+
+	if err != nil {
+		return nil, err
+	}
+
+	handler = &Payments{}
+	handler.Accounts = service.Accounts()
+
+	return handler, err
+}
