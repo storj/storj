d3d65429e836eebb1f9fc2e9e6ec220bd5b3519b
igor gaidaienko <igor@storj.io>
2020-03-17 18:41:04 +0200

    release v0.35.1-rc

---
+++ private/version/release.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package version
+
+func init() {
+	buildTimestamp = "1584463263"
+	buildCommitHash = "e41f9c97b159b385769c0686fc698253a98eb6da"
+	buildVersion = "v0.35.1-rc"
+	buildRelease = "true"
+}
