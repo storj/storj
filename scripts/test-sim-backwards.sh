3b66ba6f02008a0b177b4a56dd041b6e87affcbc
JT Olio <hello@jtolio.com>
2020-03-19 15:37:00 -0600

    scripts/tests: uplink no longer respects --client.segment-size
    this is going to make all the tests slower but it is what it is
    
    test-sim-aws.sh is removed because it was moved to storj/gateway repo.
    
    Change-Id: I10727e747a4c3740b1c9054ce7d17313b4fa310b

--- scripts/test-sim-backwards.sh
+++ scripts/test-sim-backwards.sh
@@ -50,9 +50,6 @@ PATH=$RELEASE_DIR/bin:$PATH storj-sim -x --host $STORJ_NETWORK_HOST4 network --p
 # run upload part of backward compatibility tests from the lastest release branch
 PATH=$RELEASE_DIR/bin:$PATH storj-sim -x --host $STORJ_NETWORK_HOST4 network test bash "$SCRIPTDIR"/test-backwards.sh upload
 
-# set the segment size lower to make test run faster
-echo client.segment-size: 6 MiB >> `storj-sim network env GATEWAY_0_DIR`/config.yaml
-
 SATELLITE_CONFIG=$(storj-sim network env SATELLITE_0_DIR)/config.yaml
 
 # this replaces anywhere that has "/release/" in the config file, which currently just renames the static dir paths
