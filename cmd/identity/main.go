218cd638af6201c62f857ca8b41905b68dafab0f
sixcorners <sixcorners@gmail.com>
2020-03-21 05:49:52 -0500

    Improve CA key message

--- cmd/identity/main.go
+++ cmd/identity/main.go
@@ -145,7 +145,7 @@ func cmdNewService(cmd *cobra.Command, args []string) error {
 	}
 
 	fmt.Printf("Unsigned identity is located in %q\n", serviceDir)
-	fmt.Println("Please *move* CA key to secure storage - it is only needed for identity management!")
+	fmt.Println("Please *move* CA key to secure storage - it is only needed for identity management and isn't needed to run a storage node!")
 	fmt.Printf("\t%s\n", caConfig.KeyPath)
 	return nil
 }
