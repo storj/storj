0200290f19f405334408f6d328e45cf5ae9b0f7e
paul cannon <thepaul@storj.io>
2020-04-04 18:36:11 -0500

    installer/windows: don't require ca.key file
    As noted by @sixcorners at
    https://github.com/storj/storj/pull/3819#issuecomment-609025487, the
    Windows installer code incorrectly fails if the ca.key file is not
    present.
    
    The ca.key file should not be necessary for regular storage node usage,
    and in fact we recommend that users move the ca.key file to secure
    storage rather than keeping it on the node.
    
    I'm not sure how to run the tests, so I'll get a nod from the SNO Growth
    team before merging.
    
    Change-Id: Ib2fe236de5c75165644e880caa827a2a1a034c87

--- installer/windows/StorjTests/ValidateIdentityDirTests.cs
+++ installer/windows/StorjTests/ValidateIdentityDirTests.cs
@@ -44,19 +44,6 @@ namespace StorjTests
         }
 
         [TestMethod]
-        [ExpectedExceptionWithMessage(typeof(ArgumentException), "File 'ca.key' not found in the selected folder.")]
-        public void MissingCAKeyFile()
-        {
-            var fs = new MockFileSystem(new Dictionary<string, MockFileData>
-            {
-                { @"X:\\Some\\Identity\\Folder\\ca.cert", new MockFileData("") },
-                { @"X:\\Some\\Identity\\Folder\\identity.cert", new MockFileData("") },
-                { @"X:\\Some\\Identity\\Folder\\identity.key", new MockFileData("") }
-            });
-            new CustomActionRunner(fs).ValidateIdentityDir("X:\\Some\\Identity\\Folder");
-        }
-
-        [TestMethod]
         [ExpectedExceptionWithMessage(typeof(ArgumentException), "File 'identity.cert' not found in the selected folder.")]
         public void MissingIdentityCertFile()
         {
