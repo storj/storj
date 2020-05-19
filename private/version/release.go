7743c677e1eb37547d8217b8639a4deb3e6d3d98
Jessica Grebenschikov <Jessica.greben1+github@gmail.com>
2020-05-19 06:13:50 -0700

    release v1.4.5
    Change-Id: Iff3de40c4aeb7d28749a696147d244ae164e2a99

--- private/version/release.go
+++ private/version/release.go
@@ -6,16 +6,16 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string
+var buildTimestamp string = "1589894030"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string
+var buildCommitHash string = "c916d4e69ffb70fc1c242b488d06652b49236984"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string
+var buildVersion string = "v1.4.5"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
-var buildRelease string
+var buildRelease string = "true"
 
 // ensure that linter understands that the variables are being used.
 func init() { use(buildTimestamp, buildCommitHash, buildVersion, buildRelease) }
