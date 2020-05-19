6fac1ed130d300c080b60821aaa5037e3de2db8b
Jess G <Jessica.greben1+github@gmail.com>
2020-05-19 08:09:00 -0700

    satellite/contact: rm unhelpful verbose logging (#3894)
    * unhelpful rm verbose logging
    
    Change-Id: I74dc29f18969250fa6838fed9bec8ba7ea160b77
    
    * change to debug
    
    Change-Id: I4ba880e68b40bc3e0f29d9b1aa9154e63124c904

--- satellite/contact/service.go
+++ satellite/contact/service.go
@@ -85,8 +85,12 @@ func (service *Service) PingBack(ctx context.Context, address string, peerID sto
 		// node contact info and do not want to terminate execution by returning an err
 		mon.Event("failed dial")
 		pingNodeSuccess = false
-		pingErrorMessage = fmt.Sprintf("failed to dial storage node (ID: %s) at address %s: %q", peerID, address, err)
-		service.log.Info("pingBack failed to dial storage node", zap.Stringer("Node ID", peerID), zap.String("node address", address), zap.String("pingErrorMessage", pingErrorMessage), zap.Error(err))
+		pingErrorMessage = fmt.Sprintf("failed to dial storage node (ID: %s) at address %s: %q",
+			peerID, address, err,
+		)
+		service.log.Debug("pingBack failed to dial storage node",
+			zap.String("pingErrorMessage", pingErrorMessage),
+		)
 		return pingNodeSuccess, pingErrorMessage, nil
 	}
 	defer func() { err = errs.Combine(err, client.Close()) }()
@@ -96,7 +100,10 @@ func (service *Service) PingBack(ctx context.Context, address string, peerID sto
 		mon.Event("failed ping node")
 		pingNodeSuccess = false
 		pingErrorMessage = fmt.Sprintf("failed to ping storage node, your node indicated error code: %d, %q", rpcstatus.Code(err), err)
-		service.log.Info("pingBack pingNode error", zap.Stringer("Node ID", peerID), zap.String("pingErrorMessage", pingErrorMessage), zap.Error(err))
+		service.log.Debug("pingBack pingNode error",
+			zap.Stringer("Node ID", peerID),
+			zap.String("pingErrorMessage", pingErrorMessage),
+		)
 	}
 
 	return pingNodeSuccess, pingErrorMessage, nil
