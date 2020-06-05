95a7403802ecfcde9b8cacb506cb33ff0a2f6b5f
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2020-06-05 19:31:10 +0300

    web/storagenode: uptime section removed
    Change-Id: I0918d2fdf71e40e70d0b7ed0045de3067195e843

--- web/storagenode/src/app/components/SNOContentTitle.vue
+++ web/storagenode/src/app/components/SNOContentTitle.vue
@@ -16,11 +16,6 @@
             </div>
             <div class="title-area-divider"></div>
             <div class="title-area__info-container__info-item">
-                <p class="title-area__info-container__info-item__title">UPTIME</p>
-                <p class="title-area__info-container__info-item__content">{{ uptime }}</p>
-            </div>
-            <div class="title-area-divider"></div>
-            <div class="title-area__info-container__info-item">
                 <p class="title-area__info-container__info-item__title">LAST CONTACT</p>
                 <p class="title-area__info-container__info-item__content">{{ lastPinged }} ago</p>
             </div>
@@ -118,10 +113,6 @@ export default class SNOContentTitle extends Vue {
 
     public get lastPinged(): string {
         return this.timePassed(this.$store.state.node.info.lastPinged);
-    }
-
-    public get uptime(): string {
-        return this.timePassed(this.$store.state.node.info.startedAt);
     }
 
     public get currentMonth(): string {
