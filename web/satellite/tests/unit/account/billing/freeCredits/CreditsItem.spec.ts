d4bde7848eaa8b79d98f3d340ee8d75203e654f4
VitaliiShpital <takur12345@gmail.com>
2020-07-08 13:33:26 +0300

    web/satellite: coupon status replaced with expiration date
    WHAT:
    coupon status view replaced with month and year of coupon expiration date
    
    WHY:
    better User experience
    
    Change-Id: I67abe14418aeb876a524de5e3f3c027407343100

--- web/satellite/tests/unit/account/billing/freeCredits/CreditsItem.spec.ts
+++ web/satellite/tests/unit/account/billing/freeCredits/CreditsItem.spec.ts
@@ -7,42 +7,18 @@ import { PaymentsHistoryItem, PaymentsHistoryItemType } from '@/types/payments';
 import { createLocalVue, mount } from '@vue/test-utils';
 
 const localVue = createLocalVue();
-const couponActive = new PaymentsHistoryItem('testId', 'desc', 275, 0, 'Active', '', new Date(1), new Date(1), PaymentsHistoryItemType.Coupon, 275);
-const couponExpired = new PaymentsHistoryItem('testId', 'desc', 275, 0, 'Expired', '', new Date(1), new Date(1), PaymentsHistoryItemType.Coupon, 0);
-const couponUsed = new PaymentsHistoryItem('testId', 'desc', 500, 0, 'Used', '', new Date(1), new Date(1), PaymentsHistoryItemType.Coupon, 0);
+const coupon = new PaymentsHistoryItem('testId', 'desc', 275, 0, 'Active', '', new Date(1), new Date(1), PaymentsHistoryItemType.Coupon, 275);
 
 localVue.filter('centsToDollars', (cents: number): string => {
     return `$${(cents / 100).toFixed(2)}`;
 });
 
 describe('CreditsItem', (): void => {
-    it('renders correctly if not expired', (): void => {
+    it('renders correctly', (): void => {
         const wrapper = mount(CreditsItem, {
             localVue,
             propsData: {
-                creditsItem: couponActive,
-            },
-        });
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it('renders correctly if expired', (): void => {
-        const wrapper = mount(CreditsItem, {
-            localVue,
-            propsData: {
-                creditsItem: couponExpired,
-            },
-        });
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it('renders correctly if used', (): void => {
-        const wrapper = mount(CreditsItem, {
-            localVue,
-            propsData: {
-                creditsItem: couponUsed,
+                creditsItem: coupon,
             },
         });
 
