4633dec9e1c91d4f0afd823cd28f168c5ff0e302
Michal Niewrzal <michal@storj.io>
2020-08-26 16:48:48 +0200

    perf optimisations

--- cmd/metainfo-migration/migrator.go
+++ cmd/metainfo-migration/migrator.go
@@ -12,6 +12,7 @@ import (
 	"storj.io/storj/storage"
 )
 
+const batchSize = 500
 const objectsArgs = 10
 const segmentsArgs = 8
 
@@ -24,8 +25,11 @@ type Migrator struct {
 
 	BatchSize int
 
-	Objects  []interface{}
-	Segments []interface{}
+	ObjectsSQL string
+	Objects    []interface{}
+
+	SegmentsSQL string
+	Segments    []interface{}
 }
 
 func NewMigrator(db metainfo.PointerDB, metabase *Metabase, projectID uuid.UUID, bucketName []byte) *Migrator {
@@ -36,10 +40,13 @@ func NewMigrator(db metainfo.PointerDB, metabase *Metabase, projectID uuid.UUID,
 		ProjectID:  projectID,
 		BucketName: bucketName,
 
-		BatchSize: 500,
+		BatchSize: batchSize,
 
-		Objects:  make([]interface{}, 0, 500*objectsArgs),
-		Segments: make([]interface{}, 0, 500*segmentsArgs),
+		ObjectsSQL: preparObjectsSQL(batchSize),
+		Objects:    make([]interface{}, 0, batchSize*objectsArgs),
+
+		SegmentsSQL: preparSegmentsSQL(batchSize),
+		Segments:    make([]interface{}, 0, batchSize*segmentsArgs),
 	}
 }
 
@@ -65,28 +72,14 @@ func (m *Migrator) MigrateBucket(ctx context.Context) error {
 				return err
 			}
 
-			encodedPath := item.Key.String()
+			encodedPath := item.Key
 			if encodedPath[0] == '/' {
 				encodedPath = encodedPath[1:]
 			}
 
-			err = m.insertObject(ctx, []byte(encodedPath), pointer)
+			err = m.insertObject(ctx, encodedPath, pointer)
 			if err != nil {
 				return err
-			}
-
-			if len(m.Objects)/objectsArgs >= m.BatchSize {
-				err = m.sendObjects(ctx)
-				if err != nil {
-					return err
-				}
-			}
-
-			if len(m.Segments)/segmentsArgs >= m.BatchSize {
-				err = m.sendSegments(ctx)
-				if err != nil {
-					return err
-				}
 			}
 
 			lastKey = item.Key
@@ -97,14 +90,20 @@ func (m *Migrator) MigrateBucket(ctx context.Context) error {
 		}
 	}
 
-	err = m.sendObjects(ctx)
-	if err != nil {
-		return err
+	if len(m.Objects) != 0 {
+		sql := preparObjectsSQL(len(m.Objects) / objectsArgs)
+		err := m.Metabase.Exec(ctx, sql, m.Objects...)
+		if err != nil {
+			return err
+		}
 	}
 
-	err = m.sendSegments(ctx)
-	if err != nil {
-		return err
+	if len(m.Segments) != 0 {
+		sql := preparSegmentsSQL(len(m.Segments) / segmentsArgs)
+		err := m.Metabase.Exec(ctx, sql, m.Segments...)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -132,7 +131,14 @@ func (m *Migrator) insertObject(ctx context.Context, encryptedPath []byte, point
 		Committed, segmentsCount,
 		pointer.Metadata)
 
-	err = m.insertSegment(ctx, streamID, segmentsCount-1, pointer)
+	if len(m.Objects)/objectsArgs >= m.BatchSize {
+		err = m.sendObjects(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = m.insertSegment(ctx, streamID, segmentsCount-1, pointer, streamMeta)
 	if err != nil {
 		return err
 	}
@@ -155,7 +161,7 @@ func (m *Migrator) insertObject(ctx context.Context, encryptedPath []byte, point
 			return err
 		}
 
-		err = m.insertSegment(ctx, streamID, i, segmentPointer)
+		err = m.insertSegment(ctx, streamID, i, segmentPointer, nil)
 		if err != nil {
 			return err
 		}
@@ -164,7 +170,7 @@ func (m *Migrator) insertObject(ctx context.Context, encryptedPath []byte, point
 	return nil
 }
 
-func (m *Migrator) insertSegment(ctx context.Context, streamID UUID, segmentIndex int64, pointer *pb.Pointer) error {
+func (m *Migrator) insertSegment(ctx context.Context, streamID UUID, segmentIndex int64, pointer *pb.Pointer, streamMeta *pb.StreamMeta) error {
 	segmentPosition := SegmentPosition{
 		Part:    0,
 		Segment: uint32(segmentIndex),
@@ -175,10 +181,12 @@ func (m *Migrator) insertSegment(ctx context.Context, streamID UUID, segmentInde
 		rootPieceID = pointer.Remote.RootPieceId.Bytes()
 	}
 
-	streamMeta := &pb.StreamMeta{}
-	err := pb.Unmarshal(pointer.Metadata, streamMeta)
-	if err != nil {
-		return err
+	if streamMeta == nil {
+		streamMeta = &pb.StreamMeta{}
+		err := pb.Unmarshal(pointer.Metadata, streamMeta)
+		if err != nil {
+			return err
+		}
 	}
 
 	encryptedKey := []byte{}
@@ -193,6 +201,13 @@ func (m *Migrator) insertSegment(ctx context.Context, streamID UUID, segmentInde
 		int32(pointer.SegmentSize), pointer.InlineSegment,
 		NodeAliases{1}.Encode())
 
+	if len(m.Segments)/segmentsArgs >= m.BatchSize {
+		err := m.sendSegments(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -201,25 +216,7 @@ func (m *Migrator) sendObjects(ctx context.Context) error {
 		return nil
 	}
 
-	sql := `
-		INSERT INTO objects (
-				project_id, bucket_name, encrypted_path, version, stream_id,
-				created_at, expires_at,
-				status, segment_count,
-				encrypted_metadata_nonce
-		) VALUES 
-	`
-	i := 1
-	for i < len(m.Objects) {
-		// TODO make it cleaner
-		sql += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d),", i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9)
-		i += objectsArgs
-	}
-	sql = strings.TrimSuffix(sql, ",")
-
-	// fmt.Println(sql)
-
-	err := m.Metabase.Exec(ctx, sql, m.Objects...)
+	err := m.Metabase.Exec(ctx, m.ObjectsSQL, m.Objects...)
 	if err != nil {
 		return err
 	}
@@ -234,6 +231,36 @@ func (m *Migrator) sendSegments(ctx context.Context) error {
 		return nil
 	}
 
+	err := m.Metabase.Exec(ctx, m.SegmentsSQL, m.Segments...)
+	if err != nil {
+		return err
+	}
+
+	m.Segments = m.Segments[:0]
+
+	return nil
+}
+
+func preparObjectsSQL(batchSize int) string {
+	sql := `
+		INSERT INTO objects (
+				project_id, bucket_name, encrypted_path, version, stream_id,
+				created_at, expires_at,
+				status, segment_count,
+				encrypted_metadata_nonce
+		) VALUES 
+	`
+	i := 1
+	for i < batchSize*objectsArgs {
+		// TODO make it cleaner
+		sql += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d),",
+			i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9)
+		i += objectsArgs
+	}
+	return strings.TrimSuffix(sql, ",")
+}
+
+func preparSegmentsSQL(batchSize int) string {
 	sql := `INSERT INTO segments (
 		stream_id, segment_position, root_piece_id,
 		encrypted_key, encrypted_key_nonce,
@@ -242,20 +269,12 @@ func (m *Migrator) sendSegments(ctx context.Context) error {
 	) VALUES 
 	`
 	i := 1
-	for i < len(m.Segments) {
-		sql += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d),", i, i+1, i+2, i+3, i+4, i+5, i+6, i+7)
+	for i < batchSize*segmentsArgs {
+		sql += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d),",
+			i, i+1, i+2, i+3, i+4, i+5, i+6, i+7)
 		i += segmentsArgs
 	}
-	sql = strings.TrimSuffix(sql, ",")
 
 	// fmt.Println(sql)
-
-	err := m.Metabase.Exec(ctx, sql, m.Segments...)
-	if err != nil {
-		return err
-	}
-
-	m.Segments = m.Segments[:0]
-
-	return nil
+	return strings.TrimSuffix(sql, ",")
 }
