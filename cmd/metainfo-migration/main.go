ac7d8e4d281fc92ef312d4a17f7d231084ea868c
Michal Niewrzal <michal@storj.io>
2020-08-28 11:45:38 +0200

    add arguments support

--- cmd/metainfo-migration/main.go
+++ cmd/metainfo-migration/main.go
@@ -6,20 +6,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"go.uber.org/zap"
 
 	"storj.io/common/uuid"
 	"storj.io/storj/satellite/metainfo"
-	"storj.io/storj/satellite/satellitedb"
 )
 
 func main() {
+	if len(os.Args) != 5 {
+		fmt.Println("usage: metainfo-migration pointerdb-conn-url metabase-conn-url project-id bucket-name")
+		os.Exit(1)
+	}
+
+	pointerDBUrl := os.Args[1]
+	metabaseDBUrl := os.Args[2]
+	projectIDString := os.Args[3]
+	bucketName := os.Args[4]
+
 	ctx := context.Background()
 
-	// metabase, err := DialMetainfo(ctx, "postgres://postgres:abc@localhost/metabase?sslmode=disable")
-	metabase, err := DialMetainfo(ctx, "postgresql://root@localhost:26257/metabase?sslmode=disable")
+	// postgres://postgres:abc@localhost/metabase?sslmode=disable
+	// postgresql://root@localhost:26257/metabase?sslmode=disable
+	// postgres://postgres:abc@localhost/test_storj_sim?sslmode=disable&options=--search_path%3D%22satellite%2F0%2Fmeta%22
+	// 8e1c62d9-5a0c-410f-a33e-817689520f34
+
+	metabase, err := DialMetainfo(ctx, metabaseDBUrl)
 	check(err)
 	defer func() { check(metabase.Close(ctx)) }()
 
@@ -27,20 +41,15 @@ func main() {
 	check(metabase.Migrate(ctx))
 
 	log := zap.L()
-	satellitedb, err := satellitedb.New(log.Named("db"), "postgres://postgres:abc@localhost/test_storj_sim?sslmode=disable&options=--search_path%3D%22satellite%2F0%22",
-		satellitedb.Options{})
-	check(err)
-	defer func() { check(satellitedb.Close()) }()
-
-	pointerdb, err := metainfo.NewStore(log.Named("pointerdb"), "postgres://postgres:abc@localhost/test_storj_sim?sslmode=disable&options=--search_path%3D%22satellite%2F0%2Fmeta%22")
+	pointerdb, err := metainfo.NewStore(log.Named("pointerdb"), pointerDBUrl)
 	check(err)
 	defer func() { check(pointerdb.Close()) }()
 
-	projectID, err := uuid.FromString("8e1c62d9-5a0c-410f-a33e-817689520f34")
+	projectID, err := uuid.FromString(projectIDString)
 	check(err)
 
 	start := time.Now()
-	migrator := NewMigrator(pointerdb, metabase, projectID, []byte("metabase"))
+	migrator := NewMigrator(pointerdb, metabase, projectID, []byte(bucketName))
 	err = migrator.MigrateBucket(ctx)
 	check(err)
 
