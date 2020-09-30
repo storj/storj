f27cee91b0f63657433aa04004ac159394d52597
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2020-10-01 00:13:32 +0300

    web/storagenode: 404 page rework
    WHAT: new desist and test for 404 error page
    
    WHY: 404 error page was hidden behind infinite loading screen
    also, there were no dark theme deging for it
    
    Change-Id: If9a1f5dd2321d9fc4a294637b8d5c041947cc9d1

--- web/storagenode/src/app/components/errors/Page404.vue
+++ web/storagenode/src/app/components/errors/Page404.vue
@@ -6,17 +6,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-import MainIcon from '@/../static/images/errors/404.svg';
-import StorjIcon from '@/../static/images/storjIcon.svg';
+import { APPSTATE_ACTIONS } from '@/app/store/modules/appState';
 
-@Component({
-    components: {
-        StorjIcon,
-        MainIcon,
-    },
-})
+@Component
 // 404 component on page not found error
-export default class Page404 extends Vue {}
+export default class Page404 extends Vue {
+    public beforeMount(): void {
+        this.$store.dispatch(APPSTATE_ACTIONS.SET_LOADING, false);
+    }
+}
 </script>
 
 <style scoped lang="scss">
@@ -27,7 +25,8 @@ export default class Page404 extends Vue {}
         z-index: 1000;
         width: 100%;
         height: 100vh;
-        background-color: #fff;
+        background-color: var(--error-page-background-color);
+        color: var(--error-page-text-color);
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -37,33 +36,67 @@ export default class Page404 extends Vue {}
         -moz-user-select: none;
         -ms-user-select: none;
         user-select: none;
+        text-align: center;
 
-        &__header {
-            width: 100%;
-            height: 89px;
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-            &__content {
-                width: 822px;
-                display: flex;
-                align-items: flex-start;
-            }
+        &__main-image {
+            width: 320px;
+            height: 311px;
+            background: var(--error-page-image-path);
+            background-size: contain;
         }
 
         &__title {
-            font-family: 'font_medium', sans-serif;
-            margin-bottom: 60px;
-            font-size: 32px;
+            margin-block-start: 0;
+            margin-block-end: 0;
+            font-family: 'font_bold', sans-serif;
+            margin-top: 46px;
+            font-size: 42px;
+            line-height: 42px;
+        }
+
+        &__text {
+            margin-block-start: 0;
+            margin-block-end: 0;
+            font-family: 'font_regular', sans-serif;
+            margin: 25px 0 36px 0;
+            font-size: 16px;
+            line-height: 16px;
+        }
+
+        &__button {
+            padding: 16px 24px;
+            background: var(--error-page-button-background);
+            border-radius: 6px;
+            color: white;
+            font-family: 'font_bold', sans-serif;
+            font-size: 14px;
+            line-height: 14px;
+            text-decoration: none;
         }
     }
 
-    .text {
-        margin-left: 15px;
+    @media screen and (max-width: 630px) {
+
+        .error-container {
+            padding: 0 27px;
+            width: calc(100% - 54px);
+
+            &__main-image {
+                margin-top: 89px;
+                width: 184px;
+                height: 179px;
+            }
+
+            &__title {
+                margin-top: 27px;
+                font-size: 32px;
+                line-height: 44px;
+            }
+
+            &__text {
+                margin: 12px 0 20px 0;
+                line-height: 24px;
+            }
+        }
     }
 </style>
