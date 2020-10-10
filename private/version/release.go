e3b8c02b58c90962c3baad410f50a44b64684cb3
Moby von Briesen <mobyvb@gmail.com>
2020-10-10 17:41:34 -0400

    release v1.14.7
    Change-Id: I1579a01178cc49ea9235230f2156f03e22e540f4

--- private/version/release.go
+++ private/version/release.go
@@ -6,13 +6,13 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string = "1602183948"
+var buildTimestamp string = "1602366094"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string = "5d8c0fe96a22f69f660acd5c517a16565788ac84"
+var buildCommitHash string = "6961f5774bc5c5000f772323849fed69ba218b24"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string = "v1.14.4"
+var buildVersion string = "v1.14.7"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
 var buildRelease string = "true"
