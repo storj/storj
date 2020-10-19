fe4cbab116fcee6b8b5fbccd805c3127f919f70e
littleskunk <jens.heimbuerge@googlemail.com>
2020-10-19 15:28:09 +0200

    release v1.14.8

--- private/version/release.go
+++ private/version/release.go
@@ -6,13 +6,13 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string = "1602183948"
+var buildTimestamp string = "1603114087"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string = "5d8c0fe96a22f69f660acd5c517a16565788ac84"
+var buildCommitHash string = "6a4a288c067b277eb21146bfc626079778f36a65"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string = "v1.14.4"
+var buildVersion string = "v1.14.8"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
 var buildRelease string = "true"
