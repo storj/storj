1f386db5666a381c598f316067da298e794fe5e0
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2020-10-22 13:33:09 +0300

    cmd/satellite: remove metainfo commands (#3955)

--- cmd/satellite/main.go
+++ cmd/satellite/main.go
@@ -219,22 +219,6 @@ var (
 		Long:  "Ensures that we have a stripe customer for every satellite user.",
 		RunE:  cmdStripeCustomer,
 	}
-	metainfoCmd = &cobra.Command{
-		Use:   "metainfo",
-		Short: "Metainfo commands",
-	}
-	fixOldStyleObjectsCmd = &cobra.Command{
-		Use:   "fix-old-style-objects",
-		Short: "Fixes old-style objects",
-		Long:  "Fixes the old-style objects by adding the number of segments to the metadata.",
-		RunE:  cmdFixOldStyleObjects,
-	}
-	verifyPieceHashesCmd = &cobra.Command{
-		Use:   "verify-piece-hashes",
-		Short: "Verifies piece hashes for unverified segments",
-		Long:  "Verifies piece hashes for all segments with PieceHashesVerifeid = false in their pointer.",
-		RunE:  cmdVerifyPieceHashes,
-	}
 
 	runCfg   Satellite
 	setupCfg Satellite
@@ -270,10 +254,6 @@ var (
 	}
 	verifyGracefulExitReceiptCfg struct {
 	}
-	dryRunCfg struct {
-		Satellite
-		DryRun bool `help:"only prints logs for the changes to be made without apply them" default:"true"`
-	}
 	confDir     string
 	identityDir string
 )
@@ -295,7 +275,6 @@ func init() {
 	rootCmd.AddCommand(reportsCmd)
 	rootCmd.AddCommand(compensationCmd)
 	rootCmd.AddCommand(billingCmd)
-	rootCmd.AddCommand(metainfoCmd)
 	reportsCmd.AddCommand(nodeUsageCmd)
 	reportsCmd.AddCommand(partnerAttributionCmd)
 	reportsCmd.AddCommand(gracefulExitCmd)
@@ -309,8 +288,6 @@ func init() {
 	billingCmd.AddCommand(createCustomerInvoicesCmd)
 	billingCmd.AddCommand(finalizeCustomerInvoicesCmd)
 	billingCmd.AddCommand(stripeCustomerCmd)
-	metainfoCmd.AddCommand(fixOldStyleObjectsCmd)
-	metainfoCmd.AddCommand(verifyPieceHashesCmd)
 	process.Bind(runCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(runMigrationCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(runAPICmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
@@ -332,8 +309,6 @@ func init() {
 	process.Bind(createCustomerInvoicesCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(finalizeCustomerInvoicesCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 	process.Bind(stripeCustomerCmd, &runCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
-	process.Bind(fixOldStyleObjectsCmd, &dryRunCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
-	process.Bind(verifyPieceHashesCmd, &dryRunCfg, defaults, cfgstruct.ConfDir(confDir), cfgstruct.IdentityDir(identityDir))
 }
 
 func cmdRun(cmd *cobra.Command, args []string) (err error) {
@@ -718,18 +693,6 @@ func cmdStripeCustomer(cmd *cobra.Command, args []string) (err error) {
 	ctx, _ := process.Ctx(cmd)
 
 	return generateStripeCustomers(ctx)
-}
-
-func cmdFixOldStyleObjects(cmd *cobra.Command, args []string) (err error) {
-	ctx, _ := process.Ctx(cmd)
-
-	return fixOldStyleObjects(ctx)
-}
-
-func cmdVerifyPieceHashes(cmd *cobra.Command, args []string) (err error) {
-	ctx, _ := process.Ctx(cmd)
-
-	return verifyPieceHashes(ctx)
 }
 
 func main() {
