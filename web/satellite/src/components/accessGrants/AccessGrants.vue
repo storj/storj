c21d3d2b86c0dd77153ad3d4d59a1a3ea076aad0
Malcolm Bouzi <mrbouzi@gmail.com>
2020-11-11 15:15:20 -0500

    empty state modal set up

--- web/satellite/src/components/accessGrants/AccessGrants.vue
+++ web/satellite/src/components/accessGrants/AccessGrants.vue
@@ -1,14 +1,38 @@
 // Copyright (C) 2020 Storj Labs, Inc.
 // See LICENSE for copying information.
 <template>
-	<h2>Access Grants</h2>
+	<div class="access-grants">
+		<h2 class="access-grants__title">Access Grants</h2>
+		<EmptyState />
+	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component
+import EmptyState from './EmptyState.vue';
+
+@Component({
+    components: {
+        EmptyState,
+    },
+})
 
 export default class AccessGrants extends Vue {
 }
 </script>
+<style scoped lang="scss">
+    .access-grants {
+        position: relative;
+        padding: 40px 30px 55px 30px;
+        font-family: 'font_regular', sans-serif;
+
+        &__title {
+            font-family: 'font_bold', sans-serif;
+            font-size: 32px;
+            line-height: 39px;
+            color: #263549;
+            margin: 0;
+        }
+    }
+</style>
---
+++ web/satellite/src/components/accessGrants/EmptyState.vue
@@ -0,0 +1,70 @@
+
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+<template>
+	<div class="empty-state">
+		<div class="empty-state__modal">
+			<Key />
+			<h4 class="empty-state__modal__heading">Create Your First Access Grant</h4>
+			<p class="empty-state__modal__subheading">Get started by creating an Access to interact with your Buckets</p>
+			<VButton
+				label="Create Access Grant +"
+				width="199px"
+				height="44px"
+				v-bind:style="{ margin: `0 auto`, marginTop: `25px`}"
+			/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import VButton from '@/components/common/VButton.vue';
+
+import Key from '@/../static/images/accessGrants/key.svg';
+
+// import Background from '@/../static/images/accessGrants/access-grants-bg.svg';
+
+@Component({
+    components: {
+        Key,
+        VButton,
+    },
+})
+
+export default class EmptyState extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+.empty-state {
+    background-image: url('../../../static/images/accessGrants/access-grants-bg.png');
+
+    &__modal {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 660px;
+        text-align: center;
+        background: #fff;
+        padding: 100px 30px;
+        margin-top: 110px;
+
+        &__heading {
+            font-family: 'font_bold', sans-serif;
+            font-size: 28px;
+            font-weight: 700;
+            line-height: 16px;
+            margin-bottom: 30px;
+        }
+
+        &__subheading {
+            font-family: 'font_regular', sans-serif;
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 21px;
+        }
+    }
+}
+</style>
