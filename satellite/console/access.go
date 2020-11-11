226e13e616842004859e46240e76e21b5c272085
Jessica Grebenschikov <Jessica.greben1+github@gmail.com>
2020-11-11 05:45:03 -0800

    satellite/cosole: add tests for wasm access code
    Change-Id: I78f71b2f0bef03b6e87cd7d79ccaef5f45393b55

---
+++ satellite/console/access.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package console
+
+import (
+	"crypto/sha256"
+
+	"storj.io/common/encryption"
+	"storj.io/common/macaroon"
+	"storj.io/common/storj"
+	"storj.io/common/uuid"
+	"storj.io/uplink/private/access2"
+)
+
+// GenAccessGrant creates a new access grant and returns it serialized form.
+func GenAccessGrant(satelliteNodeURL, apiKey, encryptionPassphrase, projectID string) (string, error) {
+	parsedAPIKey, err := macaroon.ParseAPIKey(apiKey)
+	if err != nil {
+		return "", err
+	}
+
+	id, err := uuid.FromString(projectID)
+	if err != nil {
+		return "", err
+	}
+
+	const concurrency = 8
+	salt := sha256.Sum256(id[:])
+
+	key, err := encryption.DeriveRootKey([]byte(encryptionPassphrase), salt[:], "", concurrency)
+	if err != nil {
+		return "", err
+	}
+
+	encAccess := access2.NewEncryptionAccessWithDefaultKey(key)
+	encAccess.SetDefaultPathCipher(storj.EncAESGCM)
+	a := &access2.Access{
+		SatelliteAddress: satelliteNodeURL,
+		APIKey:           parsedAPIKey,
+		EncAccess:        encAccess,
+	}
+	accessString, err := a.Serialize()
+	if err != nil {
+		return "", err
+	}
+	return accessString, nil
+}
---
+++ satellite/console/access_test.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package console_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"storj.io/common/testcontext"
+	"storj.io/storj/private/testplanet"
+	"storj.io/storj/satellite/console"
+)
+
+// TestGenerateAccessGrant confirms that the access grant produced by the wasm access code
+// is the same as the code the uplink cli uses to create access grants.
+func TestGenerateAccessGrant(t *testing.T) {
+	testplanet.Run(t, testplanet.Config{
+		SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 1,
+	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		satellitePeer := planet.Satellites[0]
+		satelliteNodeURL := satellitePeer.NodeURL().String()
+
+		uplinkPeer := planet.Uplinks[0]
+		apiKeyString := uplinkPeer.Projects[0].APIKey
+		projectID := uplinkPeer.Projects[0].ID.String()
+
+		passphrase := "supersecretpassphrase"
+
+		wasmAccessString, err := console.GenAccessGrant(satelliteNodeURL, apiKeyString, passphrase, projectID)
+		require.NoError(t, err)
+
+		uplinkCliAccess, err := uplinkPeer.Config.RequestAccessWithPassphrase(ctx, satelliteNodeURL, apiKeyString, passphrase)
+		require.NoError(t, err)
+		uplinkCliAccessString, err := uplinkCliAccess.Serialize()
+		require.NoError(t, err)
+		require.Equal(t, wasmAccessString, uplinkCliAccessString)
+	})
+}
