fcf96de22c679b7a80bc0691f00a85611b6d6d82
Malcolm Bouzi <mrbouzi@gmail.com>
2020-11-16 14:11:44 -0500

    add date buttons

--- web/satellite/src/components/accessGrants/AccessGrants.vue
+++ web/satellite/src/components/accessGrants/AccessGrants.vue
@@ -3,6 +3,7 @@
 <template>
 	<div class="access-grants">
 		<h2 class="access-grants__title">Access Grants</h2>
+		<DatePicker />
 		<EmptyState />
 	</div>
 </template>
@@ -10,29 +11,32 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+import DatePicker from './DatePicker.vue'
 import EmptyState from './EmptyState.vue';
 
+
 @Component({
-    components: {
-        EmptyState,
-    },
+	components: {
+		EmptyState,
+		DatePicker
+	},
 })
 
 export default class AccessGrants extends Vue {
 }
 </script>
 <style scoped lang="scss">
-    .access-grants {
-        position: relative;
-        padding: 40px 30px 55px 30px;
-        font-family: 'font_regular', sans-serif;
+	.access-grants {
+		position: relative;
+	    padding: 40px 30px 55px 30px;
+	    font-family: 'font_regular', sans-serif;
 
-        &__title {
-            font-family: 'font_bold', sans-serif;
-            font-size: 32px;
-            line-height: 39px;
-            color: #263549;
-            margin: 0;
-        }
-    }
+	    &__title {
+	        font-family: 'font_bold', sans-serif;
+	        font-size: 32px;
+	        line-height: 39px;
+	        color: #263549;
+	        margin: 0;
+	    }
+	}
 </style>
