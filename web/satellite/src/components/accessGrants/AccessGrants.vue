dacecd35e1d86a865d0744399cf8e8aa4bffd9a8
Malcolm Bouzi <mrbouzi@gmail.com>
2020-11-17 14:01:34 -0500

    remove date picker from page

--- web/satellite/src/components/accessGrants/AccessGrants.vue
+++ web/satellite/src/components/accessGrants/AccessGrants.vue
@@ -3,7 +3,6 @@
 <template>
 	<div class="access-grants">
 		<h2 class="access-grants__title">Access Grants</h2>
-		<DurationPicker />
 		<EmptyState />
 	</div>
 </template>
@@ -11,31 +10,29 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-import DurationPicker from './DurationPicker.vue';
 import EmptyState from './EmptyState.vue';
 
 @Component({
-    components: {
-        EmptyState,
-        DurationPicker,
-    },
+	components: {
+		EmptyState,
+	},
 })
 
 export default class AccessGrants extends Vue {
 }
 </script>
 <style scoped lang="scss">
-    .access-grants {
-        position: relative;
-        padding: 40px 30px 55px 30px;
-        font-family: 'font_regular', sans-serif;
+	.access-grants {
+		position: relative;
+	    padding: 40px 30px 55px 30px;
+	    font-family: 'font_regular', sans-serif;
 
-        &__title {
-            font-family: 'font_bold', sans-serif;
-            font-size: 32px;
-            line-height: 39px;
-            color: #263549;
-            margin: 0;
-        }
-    }
+	    &__title {
+	        font-family: 'font_bold', sans-serif;
+	        font-size: 32px;
+	        line-height: 39px;
+	        color: #263549;
+	        margin: 0;
+	    }
+	}
 </style>
