b3307245e268195a21442f08f28611ae7516fa2a
Ethan <ethan@storj.io>
2020-11-24 11:44:08 -0500

    release v1.17.5-rc
    Change-Id: I72ff9c66c73cc2f8612cf5b9923b3edfe2250c5d

--- private/version/release.go
+++ private/version/release.go
@@ -6,13 +6,13 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string = "1605834837"
+var buildTimestamp string = "1606236248"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string = "63d14a3506161051ba2935275789ea9bca74f0fd"
+var buildCommitHash string = "6a64c24f55995023ab570c54f45342849f7fa281"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string = "v1.17.4"
+var buildVersion string = "v1.17.5-rc"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
 var buildRelease string = "true"
