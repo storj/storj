c6626748d025537408cf6e21862bcc4ab0caaf3b
Malcolm Bouzi <mrbouzi@gmail.com>
2020-11-24 12:18:02 -0500

    web/satellite: create acces grant: duration selection logic (#3978)

--- web/satellite/src/components/accessGrants/permissions/DurationSelection.vue
+++ web/satellite/src/components/accessGrants/permissions/DurationSelection.vue
@@ -5,60 +5,78 @@
     <div class="duration-selection">
         <div
             class="duration-selection__toggle-container"
-            @click="toggleDurationPicker"
+            @click="togglePicker"
         >
-        <h1 class="duration-selection__toggle-container__name">Forever</h1>
-        <ExpandIcon
-            class="duration-selection__toggle-container__expand-icon"
-            alt="Arrow down (expand)"
+            <h1 class="duration-selection__toggle-container__name">Forever</h1>
+            <ExpandIcon
+                class="duration-selection__toggle-container__expand-icon"
+                alt="Arrow down (expand)"
+            />
+        </div>
+        <DurationPicker
+            v-if="isDurationPickerVisible"
         />
     </div>
-</div>
 
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+import DurationPicker from '@/components/accessGrants/permissions/DurationPicker.vue';
+
 import ExpandIcon from '@/../static/images/common/BlackArrowExpand.svg';
 
 @Component({
     components: {
         ExpandIcon,
+        DurationPicker,
     },
 })
 
 export default class DurationSelection extends Vue {
-
+    public isDurationPickerVisible: boolean = false;
+    /**
+     * Toggles duration picker.
+     */
+    public togglePicker(): void {
+        this.isDurationPickerVisible = !this.isDurationPickerVisible;
+    }
+    /**
+     * Closes duration picker.
+     */
+    public closePicker(): void {
+        this.isDurationPickerVisible = false;
+    }
 }
 </script>
 
 <style scoped lang="scss">
-.duration-selection {
-    background-color: #fff;
-    cursor: pointer;
-    margin-left: 15px;
-    border-radius: 6px;
-    border: 1px solid rgba(56, 75, 101, 0.4);
-    font-family: 'font_regular', sans-serif;
-    width: 235px;
-
-    &__toggle-container {
+    .duration-selection {
+        background-color: #fff;
+        cursor: pointer;
+        margin-left: 15px;
+        border-radius: 6px;
+        border: 1px solid rgba(56, 75, 101, 0.4);
+        font-family: 'font_regular', sans-serif;
+        width: 235px;
         position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 15px 20px;
-        width: calc(100% - 40px);
 
-        &__name {
-            font-style: normal;
-            font-weight: normal;
-            font-size: 16px;
-            line-height: 21px;
-            color: #384b65;
-            margin: 0;
+        &__toggle-container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 15px 20px;
+            width: calc(100% - 40px);
+
+            &__name {
+                font-style: normal;
+                font-weight: normal;
+                font-size: 16px;
+                line-height: 21px;
+                color: #384b65;
+                margin: 0;
+            }
         }
     }
-}
 </style>
