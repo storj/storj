c630037f34c5fbdd1bb8771c8fa3969b4bbc5411
Qweder93 <nikolai.siedov@gmail.com>
2020-12-09 05:13:24 +0200

    multinodepb: diskspace, reputation and status added
    Change-Id: I470fa8b59ce7f00f2fbedbd0c0878fb5fff0590c

---
+++ multinodepb/doc.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+// Package multinodepb contains protobuf definitions for Storj peers.
+package multinodepb
+
+//go:generate go run gen.go
