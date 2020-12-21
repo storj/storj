3632034477ff5f0d5f9de5d9453f1dff003ff614
Stefan Benten <mail@stefan-benten.de>
2020-12-21 14:50:57 +0100

    Makefile,scripts: move from aarch64 to arm64v8 (#4008)
    It turns out that alpine dropped support/updates for the aarch64 image.
    Instead they have been using the arm64v8 notation for quite a while,
    which resulted in breaking our recent aarch64 builds due to missing
    dependencies/updates.
    
    Both arches are exactly the same, aarch64 was created originally by Apple
    and arm64 by GNU. The backends have been merged by now and the arm64 became
    the de facto standard.

--- scripts/deploy-storagenode.sh
+++ scripts/deploy-storagenode.sh
@@ -9,7 +9,7 @@ for v in alpha arm beta latest; do
 	docker manifest create --amend storjlabs/storagenode:$v \
 	storjlabs/storagenode:${TAG}-amd64 \
 	storjlabs/storagenode:${TAG}-arm32v6 \
-	storjlabs/storagenode:${TAG}-aarch64
+	storjlabs/storagenode:${TAG}-arm64v8
 
 	docker manifest annotate storjlabs/storagenode:$v \
 	storjlabs/storagenode:${TAG}-amd64 --os linux --arch amd64
@@ -18,7 +18,7 @@ for v in alpha arm beta latest; do
 	storjlabs/storagenode:${TAG}-arm32v6 --os linux --arch arm --variant v6
 
 	docker manifest annotate storjlabs/storagenode:$v \
-	storjlabs/storagenode:${TAG}-aarch64 --os linux --arch arm64
+	storjlabs/storagenode:${TAG}-arm64v8 --os linux --arch arm64 --variant v8
 
 	docker manifest push --purge storjlabs/storagenode:$v
 done
