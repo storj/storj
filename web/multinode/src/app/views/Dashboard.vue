857155f5acaac20bc76bb7ae7ab6cd46b66d08ce
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2020-12-29 19:59:57 +0200

    web/multinode: navigation panel added
    Change-Id: Ia8d56cfce14476e1f4b59f4d13f239dda4a87b4f

--- web/multinode/src/app/views/Dashboard.vue
+++ web/multinode/src/app/views/Dashboard.vue
@@ -2,15 +2,29 @@
 // See LICENSE for copying information.
 
 <template>
-  <div>
-
+  <div class="dashboard-area">
+      <div class="dashboard-area__navigation-area">
+          <navigation-area />
+      </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component
+import NavigationArea from '@/app/components/navigation/NavigationArea.vue';
+@Component({
+    components: {NavigationArea},
+})
 export default class Dashboard extends Vue {}
 </script>
 
+<style lang="scss" scoped>
+    .dashboard-area {
+        display: flex;
+
+        &__navigation-area {
+            width: 280px;
+        }
+    }
+</style>
