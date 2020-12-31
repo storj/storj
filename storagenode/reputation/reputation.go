710b86849ceaafb83b60d8270e09d1b6bff002c6
Moby von Briesen <mobyvb@gmail.com>
2020-12-31 11:34:16 -0500

    storagenode/{console, reputation}: Add audit history to storagenode API
    Change-Id: Id680b07feea380824e56496d545f46a94fdcc5d5

--- storagenode/reputation/reputation.go
+++ storagenode/reputation/reputation.go
@@ -53,3 +53,33 @@ type Metric struct {
 	Score        float64 `json:"score"`
 	UnknownScore float64 `json:"unknownScore"`
 }
+
+// AuditHistory encapsulates storagenode audit history.
+type AuditHistory struct {
+	Score   float64              `json:"score"`
+	Windows []AuditHistoryWindow `json:"windows"`
+}
+
+// AuditHistoryWindow encapsulates storagenode audit history window.
+type AuditHistoryWindow struct {
+	WindowStart time.Time `json:"windowStart"`
+	TotalCount  int32     `json:"totalCount"`
+	OnlineCount int32     `json:"onlineCount"`
+}
+
+// GetAuditHistoryFromPB creates the AuditHistory json struct from a protobuf.
+func GetAuditHistoryFromPB(auditHistoryPB *pb.AuditHistory) AuditHistory {
+	ah := AuditHistory{}
+	if auditHistoryPB == nil {
+		return ah
+	}
+	ah.Score = auditHistoryPB.Score
+	for _, window := range auditHistoryPB.Windows {
+		ah.Windows = append(ah.Windows, AuditHistoryWindow{
+			WindowStart: window.WindowStart,
+			TotalCount:  window.TotalCount,
+			OnlineCount: window.OnlineCount,
+		})
+	}
+	return ah
+}
