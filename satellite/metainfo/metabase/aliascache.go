d0a7ab60ec77919bf044209c7040c66a385329c5
Egon Elbre <egonelbre@gmail.com>
2021-02-05 15:43:42 +0200

    satellite/metainfo/metabase: add NodeAliasCache
    This implements a node alias cache that automatically refreshes
    and adds new nodes when needed.
    
    Change-Id: I4af121f7307b7b7ac4f87ad465f02d4eeb17d42f

--- satellite/metainfo/metabase/aliascache.go
+++ satellite/metainfo/metabase/aliascache.go
@@ -4,8 +4,127 @@
 package metabase
 
 import (
+	"context"
+	"sync"
+
 	"storj.io/common/storj"
 )
+
+// NodeAliasDB is an interface for looking up node alises.
+type NodeAliasDB interface {
+	EnsureNodeAliases(ctx context.Context, opts EnsureNodeAliases) error
+	ListNodeAliases(ctx context.Context) (_ []NodeAliasEntry, err error)
+}
+
+// NodeAliasCache is a write-through cache for looking up node ID and alias mapping.
+type NodeAliasCache struct {
+	mu     sync.Mutex
+	latest *NodeAliasMap
+	db     NodeAliasDB
+}
+
+// NewNodeAliasCache creates a new cache using the specified database.
+func NewNodeAliasCache(db NodeAliasDB) *NodeAliasCache {
+	return &NodeAliasCache{
+		db:     db,
+		latest: NewNodeAliasMap(nil),
+	}
+}
+
+// Nodes returns node ID-s corresponding to the aliases,
+// refreshing the cache once when an alias is missing.
+// This results in an error when the alias is not in the database.
+func (cache *NodeAliasCache) Nodes(ctx context.Context, aliases []NodeAlias) ([]storj.NodeID, error) {
+	cache.mu.Lock()
+	latest := cache.latest
+	cache.mu.Unlock()
+
+	nodes, missing := latest.Nodes(aliases)
+	if len(missing) == 0 {
+		return nodes, nil
+	}
+
+	if len(missing) > 0 {
+		var err error
+		latest, err = cache.refresh(ctx)
+		if err != nil {
+			return nil, Error.New("failed to refresh node alias db: %w", err)
+		}
+	}
+
+	nodes, missing = latest.Nodes(aliases)
+	if len(missing) == 0 {
+		return nodes, nil
+	}
+
+	return nil, Error.New("aliases missing in database: %v", missing)
+}
+
+// Aliases returns node aliases corresponding to the node ID-s,
+// adding missing node ID-s to the database when needed.
+func (cache *NodeAliasCache) Aliases(ctx context.Context, nodes []storj.NodeID) ([]NodeAlias, error) {
+	cache.mu.Lock()
+	latest := cache.latest
+	cache.mu.Unlock()
+
+	aliases, missing := latest.Aliases(nodes)
+	if len(missing) == 0 {
+		return aliases, nil
+	}
+
+	if len(missing) > 0 {
+		var err error
+		latest, err = cache.ensure(ctx, missing...)
+		if err != nil {
+			return nil, Error.Wrap(err)
+		}
+	}
+
+	aliases, missing = latest.Aliases(nodes)
+	if len(missing) == 0 {
+		return aliases, nil
+	}
+
+	return nil, Error.New("nodes still missing after ensuring: %v", missing)
+}
+
+// ensure tries to ensure that the specified missing node ID-s are assigned a alias.
+func (cache *NodeAliasCache) ensure(ctx context.Context, missing ...storj.NodeID) (_ *NodeAliasMap, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	if err := cache.db.EnsureNodeAliases(ctx, EnsureNodeAliases{
+		Nodes: missing,
+	}); err != nil {
+		return nil, Error.New("failed to update node alias db: %w", err)
+	}
+	return cache.refresh(ctx)
+}
+
+// refresh refreshses the state of the cache.
+func (cache *NodeAliasCache) refresh(ctx context.Context) (_ *NodeAliasMap, err error) {
+	defer mon.Task()(&ctx)(&err)
+	// TODO: allow only one inflight request
+
+	entries, err := cache.db.ListNodeAliases(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	xs := NewNodeAliasMap(entries)
+
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	// Since we never remove node aliases we can assume that the alias map that contains more
+	// entries is the latest one.
+	//
+	// Note: we merge the maps here rather than directly replacing.
+	// This is not ideal from performance side, however it should reduce possible consistency issues.
+	xs.Merge(cache.latest)
+	cache.latest = xs
+
+	return cache.latest, nil
+}
 
 // NodeAliasMap contains bidirectional mapping between node ID and a NodeAlias.
 type NodeAliasMap struct {
@@ -24,6 +143,16 @@ func NewNodeAliasMap(entries []NodeAliasEntry) *NodeAliasMap {
 		m.alias[e.ID] = e.Alias
 	}
 	return m
+}
+
+// Merge merges the other map into m.
+func (m *NodeAliasMap) Merge(other *NodeAliasMap) {
+	for k, v := range other.node {
+		m.node[k] = v
+	}
+	for k, v := range other.alias {
+		m.alias[k] = v
+	}
 }
 
 // Nodes returns NodeID-s for the given aliases and aliases that are not in this map.
--- satellite/metainfo/metabase/aliascache_test.go
+++ satellite/metainfo/metabase/aliascache_test.go
@@ -4,6 +4,9 @@
 package metabase_test
 
 import (
+	"context"
+	"errors"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,6 +16,86 @@ import (
 	"storj.io/common/testrand"
 	"storj.io/storj/satellite/metainfo/metabase"
 )
+
+func TestNodeAliasCache(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	t.Run("missing aliases", func(t *testing.T) {
+		cache := metabase.NewNodeAliasCache(&NodeAliasDB{})
+		nodes, err := cache.Nodes(ctx, []metabase.NodeAlias{1, 2, 3})
+		require.EqualError(t, err, "metabase: aliases missing in database: [1 2 3]")
+		require.Empty(t, nodes)
+	})
+
+	t.Run("auto add nodes", func(t *testing.T) {
+		cache := metabase.NewNodeAliasCache(&NodeAliasDB{})
+
+		n1, n2 := testrand.NodeID(), testrand.NodeID()
+
+		aliases, err := cache.Aliases(ctx, []storj.NodeID{n1, n2})
+		require.NoError(t, err)
+		require.Equal(t, []metabase.NodeAlias{1, 2}, aliases)
+
+		nx1 := testrand.NodeID()
+		aliases, err = cache.Aliases(ctx, []storj.NodeID{nx1, n1, n2})
+		require.NoError(t, err)
+		require.Equal(t, []metabase.NodeAlias{3, 1, 2}, aliases)
+
+		nodes, err := cache.Nodes(ctx, aliases)
+		require.NoError(t, err)
+		require.Equal(t, []storj.NodeID{nx1, n1, n2}, nodes)
+
+		nodes, err = cache.Nodes(ctx, []metabase.NodeAlias{3, 4, 1, 2})
+		require.EqualError(t, err, "metabase: aliases missing in database: [4]")
+		require.Empty(t, nodes)
+	})
+
+	t.Run("db error", func(t *testing.T) {
+		aliasDB := &NodeAliasDB{}
+		aliasDB.SetFail(errors.New("io.EOF"))
+		cache := metabase.NewNodeAliasCache(aliasDB)
+
+		n1, n2 := testrand.NodeID(), testrand.NodeID()
+
+		aliases, err := cache.Aliases(ctx, []storj.NodeID{n1, n2})
+		require.EqualError(t, err, "metabase: failed to update node alias db: io.EOF")
+		require.Empty(t, aliases)
+
+		nodes, err := cache.Nodes(ctx, []metabase.NodeAlias{1, 2})
+		require.EqualError(t, err, "metabase: failed to refresh node alias db: io.EOF")
+		require.Empty(t, nodes)
+	})
+}
+
+func TestNodeAliasCache_DB(t *testing.T) {
+	All(t, func(ctx *testcontext.Context, t *testing.T, db *metabase.DB) {
+		t.Run("missing aliases", func(t *testing.T) {
+			defer DeleteAll{}.Check(ctx, t, db)
+
+			cache := metabase.NewNodeAliasCache(db)
+			nodes, err := cache.Nodes(ctx, []metabase.NodeAlias{1, 2, 3})
+			require.EqualError(t, err, "metabase: aliases missing in database: [1 2 3]")
+			require.Empty(t, nodes)
+		})
+
+		t.Run("auto add nodes", func(t *testing.T) {
+			defer DeleteAll{}.Check(ctx, t, db)
+
+			cache := metabase.NewNodeAliasCache(db)
+
+			n1, n2 := testrand.NodeID(), testrand.NodeID()
+
+			aliases, err := cache.Aliases(ctx, []storj.NodeID{n1, n2})
+			require.NoError(t, err)
+			require.Equal(t, []metabase.NodeAlias{1, 2}, aliases)
+
+			nodes, err := cache.Nodes(ctx, aliases)
+			require.NoError(t, err)
+			require.Equal(t, []storj.NodeID{n1, n2}, nodes)
+		})
+	})
+}
 
 func TestNodeAliasMap(t *testing.T) {
 	defer testcontext.New(t).Cleanup()
@@ -111,4 +194,65 @@ func TestNodeAliasMap(t *testing.T) {
 		require.EqualValues(t, test.out, out)
 		require.EqualValues(t, test.missing, missing)
 	}
+}
+
+var _ metabase.NodeAliasDB = (*NodeAliasDB)(nil)
+
+// NodeAliasDB is an inmemory alias database for testing.
+type NodeAliasDB struct {
+	mu      sync.Mutex
+	fail    error
+	last    metabase.NodeAlias
+	entries []metabase.NodeAliasEntry
+}
+
+func (db *NodeAliasDB) SetFail(err error) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	db.fail = err
+}
+
+func (db *NodeAliasDB) ShouldFail() error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	return db.fail
+}
+
+func (db *NodeAliasDB) Ensure(id storj.NodeID) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	for _, e := range db.entries {
+		if e.ID == id {
+			return
+		}
+	}
+
+	db.last++
+	db.entries = append(db.entries, metabase.NodeAliasEntry{
+		ID:    id,
+		Alias: db.last,
+	})
+}
+
+func (db *NodeAliasDB) EnsureNodeAliases(ctx context.Context, opts metabase.EnsureNodeAliases) error {
+	if err := db.ShouldFail(); err != nil {
+		return err
+	}
+	for _, id := range opts.Nodes {
+		db.Ensure(id)
+	}
+	return nil
+}
+
+func (db *NodeAliasDB) ListNodeAliases(ctx context.Context) (_ []metabase.NodeAliasEntry, err error) {
+	if err := db.ShouldFail(); err != nil {
+		return nil, err
+	}
+
+	db.mu.Lock()
+	xs := append([]metabase.NodeAliasEntry{}, db.entries...)
+	db.mu.Unlock()
+
+	return xs, nil
 }
