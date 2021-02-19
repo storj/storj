79ecd80c0a3b8c3e7f7f243dec6ed72bff0fd17f
Ethan <ethan@storj.io>
2021-02-19 10:48:33 -0500

    satellite/metabase: Remove subquery on delete objects for CRDB
    The subquery for DELETE FROM obects returns a stream_id field for filtering. Unfortunately stream_id is not indexed. This change removed the subquery from the CockroachDB delete bucket query.
    
    Change-Id: If1abe21668c593e6d4bdc3ba8cdbad26c09d234e

--- satellite/metainfo/metabase/db.go
+++ satellite/metainfo/metabase/db.go
@@ -28,8 +28,9 @@ var (
 
 // DB implements a database for storing objects and segments.
 type DB struct {
-	log *zap.Logger
-	db  tagsql.DB
+	log            *zap.Logger
+	db             tagsql.DB
+	implementation dbutil.Implementation
 
 	aliasCache *NodeAliasCache
 }
@@ -44,6 +45,12 @@ func Open(ctx context.Context, log *zap.Logger, driverName, connstr string) (*DB
 
 	db := &DB{log: log, db: postgresRebind{rawdb}}
 	db.aliasCache = NewNodeAliasCache(db)
+
+	_, _, db.implementation, err = dbutil.SplitConnStr(connstr)
+	if err != nil {
+		return nil, Error.Wrap(err)
+	}
+
 	return db, nil
 }
 
