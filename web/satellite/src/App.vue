4af4568b21246bfbbb57c6abc5a52505ff988d7d
Malcolm Bouzi <mrbouzi@gmail.com>
2021-02-24 17:35:24 -0500

    web/satellite: ui changes to registration and billing pages to enable user to add promo codes to their account - add Credit History table to billing acount page and set up ui for a user adding promo codes - implement promo codes ui in registration form - add feature flag to handle if coupon code ui should be rendered
    Change-Id: I9fdeef7cffc7901958d3f9be335e1115b2471a2e

--- web/satellite/src/App.vue
+++ web/satellite/src/App.vue
@@ -31,6 +31,7 @@ export default class App extends Vue {
         const satelliteName = MetaUtils.getMetaContent('satellite-name');
         const isBetaSatellite = MetaUtils.getMetaContent('is-beta-satellite');
         const segmentioId = MetaUtils.getMetaContent('segment-io');
+        const couponCodeUIEnabled = MetaUtils.getMetaContent('coupon-code-ui-enabled');
 
         if (satelliteName) {
             this.$store.dispatch(APP_STATE_ACTIONS.SET_SATELLITE_NAME, satelliteName);
@@ -38,6 +39,10 @@ export default class App extends Vue {
 
         if (isBetaSatellite) {
             this.$store.dispatch(APP_STATE_ACTIONS.SET_SATELLITE_STATUS, isBetaSatellite === 'true');
+        }
+
+        if (couponCodeUIEnabled) {
+            this.$store.dispatch(APP_STATE_ACTIONS.SET_COUPON_CODE_UI_STATUS, couponCodeUIEnabled === 'true');
         }
 
         if (segmentioId) {
