9d52451464d3082c5adda5aed412ef364458439a
Michał Niewrzał <michal@storj.io>
2021-03-22 13:18:04 +0100

    satellite/metainfo/expireddeletion: add config value to set list limit
    We want to control how many expired objects we will query at once.
    
    Change-Id: Iba7c79a6236c6b720b53bb8f078d55514690d8e5

--- satellite/metainfo/expireddeletion/chore.go
+++ satellite/metainfo/expireddeletion/chore.go
@@ -24,8 +24,9 @@ var (
 
 // Config contains configurable values for expired segment cleanup.
 type Config struct {
-	Interval time.Duration `help:"the time between each attempt to go through the db and clean up expired segments" releaseDefault:"120h" devDefault:"10s"`
-	Enabled  bool          `help:"set if expired segment cleanup is enabled or not" releaseDefault:"true" devDefault:"true"`
+	Interval  time.Duration `help:"the time between each attempt to go through the db and clean up expired segments" releaseDefault:"120h" devDefault:"10s"`
+	Enabled   bool          `help:"set if expired segment cleanup is enabled or not" releaseDefault:"true" devDefault:"true"`
+	ListLimit int           `help:"how many expired objects to query in a batch" default:"100"`
 }
 
 // Chore implements the expired segment cleanup chore.
@@ -82,6 +83,7 @@ func (chore *Chore) deleteExpiredObjects(ctx context.Context) (err error) {
 	// that queries for deleting expired objects are stable
 	err = chore.metabase.DeleteExpiredObjects(ctx, metabase.DeleteExpiredObjects{
 		ExpiredBefore: chore.nowFn(),
+		BatchSize:     chore.config.ListLimit,
 	})
 	if err != nil {
 		chore.log.Error("deleting expired objects failed", zap.Error(err))
