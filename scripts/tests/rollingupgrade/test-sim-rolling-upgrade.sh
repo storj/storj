9bc2a9a4b711725c4f6b0a092924b13799cd2969
Michał Niewrzał <michal@storj.io>
2021-03-26 13:09:32 +0100

    rollingupgrade: change how metainfo-migration tool is installed
    Change-Id: Iad1285a770f76c9cf09a872a4bf1cd39969ee117

--- scripts/tests/rollingupgrade/test-sim-rolling-upgrade.sh
+++ scripts/tests/rollingupgrade/test-sim-rolling-upgrade.sh
@@ -142,9 +142,9 @@ setup_stage(){
     then
         mv $dest_sat_cfg_dir/satellite $dest_sat_cfg_dir/old_satellite
 
-        go install storj.io/storj/cmd/metainfo-migration
+        go build -v -o $test_dir/bin storj.io/storj/cmd/metainfo-migration >/dev/null 2>&1
         STORJ_MIGRATION_DB=${STORJ_MIGRATION_DB:-$STORJ_SIM_POSTGRES}
-        metainfo-migration --pointerdb "${STORJ_MIGRATION_DB}" --metabasedb "${STORJ_MIGRATION_DB}"
+        $test_dir/bin/metainfo-migration --pointerdb "${STORJ_MIGRATION_DB}" --metabasedb "${STORJ_MIGRATION_DB}"
     fi
 
     # ln binary and copy config.yaml for desired version
