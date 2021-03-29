74b243d1ca2ae9fc9b7b3aeccd0565b3afb9b37d
Egon Elbre <egonelbre@gmail.com>
2021-03-29 14:45:27 +0300

    certificate: move protobuf to storj/storj repository
    Change-Id: I4689318304b54121a65e28860430d39d94ac5bd6

--- certificate/peer_test.go
+++ certificate/peer_test.go
@@ -16,7 +16,6 @@ import (
 
 	"storj.io/common/identity"
 	"storj.io/common/identity/testidentity"
-	"storj.io/common/pb"
 	"storj.io/common/peertls/tlsopts"
 	"storj.io/common/pkcrypto"
 	"storj.io/common/rpc"
@@ -26,6 +25,7 @@ import (
 	"storj.io/storj/certificate"
 	"storj.io/storj/certificate/authorization"
 	"storj.io/storj/certificate/certificateclient"
+	"storj.io/storj/certificate/certificatepb"
 	"storj.io/storj/pkg/server"
 )
 
@@ -172,7 +172,7 @@ func TestCertificateSigner_Sign(t *testing.T) {
 			peerCtx := rpcpeer.NewContext(ctx, peer)
 
 			certSigner := certificate.NewEndpoint(zaptest.NewLogger(t), ca, authDB, 0)
-			req := pb.SigningRequest{
+			req := certificatepb.SigningRequest{
 				Timestamp: time.Now().Unix(),
 				AuthToken: auths[0].Token.String(),
 			}
