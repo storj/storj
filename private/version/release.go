857c0011b2a2a50e91e8f47da82c59ec95446890
Ethan <ethan@storj.io>
2021-04-02 16:04:25 -0400

    release v1.25.6-rc-multipart
    Change-Id: I324cf14dbcf8bd36f1c8bd2c97f91e7cc277385b

--- private/version/release.go
+++ private/version/release.go
@@ -6,16 +6,16 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string
+var buildTimestamp string = "1617393865"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string
+var buildCommitHash string = "d3813800daa2bec7dad51867e72afb75efa2b166"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string
+var buildVersion string = "v1.25.6-rc-multipart"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
-var buildRelease string
+var buildRelease string = "true"
 
 // ensure that linter understands that the variables are being used.
 func init() { use(buildTimestamp, buildCommitHash, buildVersion, buildRelease) }
