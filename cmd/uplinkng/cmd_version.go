e460dc51f7ea418a38d0b3f2cab8ab257728530d
Jeff Wendling <jeff@storj.io>
2021-04-05 23:40:04 -0400

    cmd/uplinkng: implement object listing
    Change-Id: Ib5f6964a0c42718913a680529bb66c6f475aeac9

--- cmd/uplinkng/cmd_version.go
+++ cmd/uplinkng/cmd_version.go
@@ -4,11 +4,9 @@
 package main
 
 import (
-	"fmt"
 	"runtime/debug"
 	"strconv"
 	"strings"
-	"text/tabwriter"
 
 	"github.com/zeebo/clingy"
 	"github.com/zeebo/errs"
@@ -31,13 +29,13 @@ func (c *cmdVersion) Execute(ctx clingy.Context) error {
 		return errs.New("unable to read build info")
 	}
 
-	tw := tabwriter.NewWriter(ctx.Stdout(), 4, 4, 4, ' ', 0)
-	defer func() { _ = tw.Flush() }()
+	tw := newTabbedWriter(ctx.Stdout(), "PATH", "VERSION")
+	defer tw.Done()
 
-	fmt.Fprintf(tw, "%s\t%s\n", bi.Main.Path, bi.Main.Version)
+	tw.WriteLine(bi.Main.Path, bi.Main.Version)
 	for _, mod := range bi.Deps {
 		if c.verbose || strings.HasPrefix(mod.Path, "storj.io/") {
-			fmt.Fprintf(tw, "  %s\t%s\n", mod.Path, mod.Version)
+			tw.WriteLine(mod.Path, mod.Version)
 		}
 	}
 
