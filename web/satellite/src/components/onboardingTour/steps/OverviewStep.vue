3088d14116eff0c5e4e307f7666745fab280d254
Malcolm Bouzi <mrbouzi@gmail.com>
2021-04-07 18:32:40 -0400

    Overview refactor (#4076)

--- web/satellite/src/components/onboardingTour/steps/OverviewStep.vue
+++ web/satellite/src/components/onboardingTour/steps/OverviewStep.vue
@@ -8,6 +8,9 @@ import { Component, Vue } from 'vue-property-decorator';
 
 import VButton from '@/components/common/VButton.vue';
 
+import DuplicatiIcon from '@/../static/images/onboardingTour/duplicati.svg';
+import GatewayIcon from '@/../static/images/onboardingTour/s3-gateway.svg';
+
 import { RouteConfig } from '@/router';
 import { ACCESS_GRANTS_ACTIONS } from '@/store/modules/accessGrants';
 import { BUCKET_ACTIONS } from '@/store/modules/buckets';
@@ -20,6 +23,8 @@ import { SegmentEvent } from '@/utils/constants/analyticsEventNames';
 @Component({
     components: {
         VButton,
+        DuplicatiIcon,
+        GatewayIcon,
     },
 })
 export default class OverviewStep extends Vue {
@@ -43,14 +48,6 @@ export default class OverviewStep extends Vue {
 
     /**
      * Holds button click logic.
-     * Redirects to next step (adding payment method).
-     */
-    public onAddPaymentClick(): void {
-        this.$router.push(RouteConfig.OnboardingTour.with(RouteConfig.PaymentStep).path);
-    }
-
-    /**
-     * Holds button click logic.
      * Creates untitled project and redirects to next step (creating access grant).
      */
     public async onCreateGrantClick(): Promise<void> {
@@ -59,30 +56,7 @@ export default class OverviewStep extends Vue {
         this.isLoading = true;
 
         try {
-            const FIRST_PAGE = 1;
-            const UNTITLED_PROJECT_NAME = 'Untitled Project';
-            const UNTITLED_PROJECT_DESCRIPTION = '___';
-            const project = new ProjectFields(
-                UNTITLED_PROJECT_NAME,
-                UNTITLED_PROJECT_DESCRIPTION,
-                this.$store.getters.user.id,
-            );
-            const createdProject = await this.$store.dispatch(PROJECTS_ACTIONS.CREATE, project);
-            const createdProjectId = createdProject.id;
-
-            this.$segment.track(SegmentEvent.PROJECT_CREATED, {
-                project_id: createdProjectId,
-            });
-
-            await this.$store.dispatch(PROJECTS_ACTIONS.SELECT, createdProjectId);
-            await this.$store.dispatch(PM_ACTIONS.CLEAR);
-            await this.$store.dispatch(PM_ACTIONS.FETCH, FIRST_PAGE);
-            await this.$store.dispatch(PAYMENTS_ACTIONS.GET_PAYMENTS_HISTORY);
-            await this.$store.dispatch(PAYMENTS_ACTIONS.GET_BALANCE);
-            await this.$store.dispatch(PAYMENTS_ACTIONS.GET_PROJECT_USAGE_AND_CHARGES_CURRENT_ROLLUP);
-            await this.$store.dispatch(PROJECTS_ACTIONS.GET_LIMITS, createdProjectId);
-            await this.$store.dispatch(ACCESS_GRANTS_ACTIONS.CLEAR);
-            await this.$store.dispatch(BUCKET_ACTIONS.CLEAR);
+            await this.createUntitledProject();
 
             this.isLoading = false;
 
@@ -94,10 +68,53 @@ export default class OverviewStep extends Vue {
     }
 
     /**
-     * Indicates if paywall is enabled.
+     * Creates untitled project and redirects to objects page.
      */
-    public get isPaywallEnabled(): boolean {
-        return this.$store.state.paymentsModule.isPaywallEnabled;
+    public async onContinueInBrowserClick(): Promise<void> {
+        if (this.isLoading) return;
+
+        this.isLoading = true;
+
+        try {
+            await this.createUntitledProject();
+
+            this.isLoading = false;
+
+            await this.$router.push(RouteConfig.Objects.with(RouteConfig.CreatePassphrase).path);
+        } catch (error) {
+            await this.$notify.error(error.message);
+            this.isLoading = false;
+        }
+    }
+
+    /**
+     * Creates untitled project in a background.
+     */
+    private async createUntitledProject(): Promise<void> {
+        const FIRST_PAGE = 1;
+        const UNTITLED_PROJECT_NAME = 'Untitled Project';
+        const UNTITLED_PROJECT_DESCRIPTION = '___';
+        const project = new ProjectFields(
+            UNTITLED_PROJECT_NAME,
+            UNTITLED_PROJECT_DESCRIPTION,
+            this.$store.getters.user.id,
+        );
+        const createdProject = await this.$store.dispatch(PROJECTS_ACTIONS.CREATE, project);
+        const createdProjectId = createdProject.id;
+
+        this.$segment.track(SegmentEvent.PROJECT_CREATED, {
+            project_id: createdProjectId,
+        });
+
+        await this.$store.dispatch(PROJECTS_ACTIONS.SELECT, createdProjectId);
+        await this.$store.dispatch(PM_ACTIONS.CLEAR);
+        await this.$store.dispatch(PM_ACTIONS.FETCH, FIRST_PAGE);
+        await this.$store.dispatch(PAYMENTS_ACTIONS.GET_PAYMENTS_HISTORY);
+        await this.$store.dispatch(PAYMENTS_ACTIONS.GET_BALANCE);
+        await this.$store.dispatch(PAYMENTS_ACTIONS.GET_PROJECT_USAGE_AND_CHARGES_CURRENT_ROLLUP);
+        await this.$store.dispatch(PROJECTS_ACTIONS.GET_LIMITS, createdProjectId);
+        await this.$store.dispatch(ACCESS_GRANTS_ACTIONS.CLEAR);
+        await this.$store.dispatch(BUCKET_ACTIONS.CLEAR);
     }
 
     /**
