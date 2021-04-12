054084d64808f087e969cb3f844cce3cacdac84b
prerna-parashar <78053555+prerna-parashar@users.noreply.github.com>
2021-04-12 09:58:36 -0700

    satellite/analytics: Added analytics for "passphrase created", "account verified" and "external_link_clicked" (#4078)

--- web/satellite/src/components/header/resourcesDropdown/ResourcesDropdown.vue
+++ web/satellite/src/components/header/resourcesDropdown/ResourcesDropdown.vue
@@ -12,6 +12,9 @@ import SupportIcon from '@/../static/images/header/support.svg';
 
 import { RouteConfig } from '@/router';
 
+import { AnalyticsEvent } from '@/utils/constants/analyticsEventNames';
+import { AnalyticsHttpApi } from '@/api/analytics';
+
 @Component({
     components: {
         DocsIcon,
@@ -20,11 +23,25 @@ import { RouteConfig } from '@/router';
     },
 })
 export default class ResourcesDropdown extends Vue {
+    
+    private readonly analytics: AnalyticsHttpApi = new AnalyticsHttpApi();
     /**
      * Indicates if current route is onboarding tour.
      */
     public get isOnboardingTour(): boolean {
         return this.$route.path.includes(RouteConfig.OnboardingTour.path);
+    }
+
+    public onDocsIconClick(): void {
+        this.analytics.linkEventTriggered(AnalyticsEvent.EXTERNAL_LINK_CLICKED, "https://documentation.storj.io");
+    }
+
+    public onCommunityIconClick(): void {
+        this.analytics.linkEventTriggered(AnalyticsEvent.EXTERNAL_LINK_CLICKED, "https://storj.io/community/");
+    }
+
+    public onSupportIconClick(): void {
+        this.analytics.linkEventTriggered(AnalyticsEvent.EXTERNAL_LINK_CLICKED, "mailto:support@storj.io");
     }
 }
 </script>
