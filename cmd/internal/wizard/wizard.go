46da995c4a946f421bcc58ecf41d731a6d1470b1
JT Olio <hello@jtolio.com>
2021-04-13 21:41:47 -0600

    storj/storj: more domain changes
    Change-Id: I91ae3bc36be38c27269f9e6e678fa3616b8e4d53

--- cmd/internal/wizard/wizard.go
+++ cmd/internal/wizard/wizard.go
@@ -45,9 +45,9 @@ func PromptForAccessName() (string, error) {
 // PromptForSatellite handles user input for a satellite address to be used with wizards.
 func PromptForSatellite(cmd *cobra.Command) (string, error) {
 	satellites := []string{
-		"12EayRS2V1kEsWESU9QMRseFhdxYxKicsiFmxrsLZHeLUtdps3S@us-central-1.tardigrade.io:7777",
-		"12L9ZFwhzVpuEKMUNUqkaTLGzwY9G24tbiigLiXpmZWKwmcNDDs@europe-west-1.tardigrade.io:7777",
-		"121RTSDpyNZVcEU84Ticf2L1ntiuUimbWgfATz21tuvgk3vzoA6@asia-east-1.tardigrade.io:7777",
+		"12EayRS2V1kEsWESU9QMRseFhdxYxKicsiFmxrsLZHeLUtdps3S@us1.storj.io:7777",
+		"12L9ZFwhzVpuEKMUNUqkaTLGzwY9G24tbiigLiXpmZWKwmcNDDs@eu1.storj.io:7777",
+		"121RTSDpyNZVcEU84Ticf2L1ntiuUimbWgfATz21tuvgk3vzoA6@ap1.storj.io:7777",
 	}
 
 	_, err := fmt.Print("Select your satellite:\n")
@@ -179,7 +179,7 @@ Enter your encryption passphrase: `)
 // PromptForTracing handles user input for consent to turn on tracing to be used with wizards.
 func PromptForTracing() (bool, error) {
 	_, err := fmt.Printf(`
-With your permission, Tardigrade can automatically collect analytics information from your uplink CLI and send it to Storj Labs (makers of Tardigrade) to help improve the quality and performance of our products. This information is sent only with your consent and is submitted anonymously to Storj Labs: (y/n)
+With your permission, Storj can automatically collect analytics information from your uplink CLI to help improve the quality and performance of our products. This information is sent only with your consent and is submitted anonymously to Storj Labs: (y/n)
 `)
 	if err != nil {
 		return false, err
