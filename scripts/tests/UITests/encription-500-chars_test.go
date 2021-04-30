87f12806fcd4064065a7133150ff194bbf25b55d
Andrii Kotko <andriikotko@gmail.com>
2021-04-30 13:55:40 +0300

    test with warning about downsides of server-side encription added

--- scripts/tests/UITests/encription-500-chars_test.go
+++ scripts/tests/UITests/encription-500-chars_test.go
@@ -7,6 +7,23 @@ import (
 	var passphrase500chars = "1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop1234567890!@#$%^&*()?><,./~`:zQWERTYUIOPqwertyuiop"
 	var bucketName = "111111"
 
+	func Test_warning_server_side_encription (t *testing.T){
+		page, browser := login_to_account()
+		defer browser.MustClose()
+		page.MustElementX("(//*[@class=\"navigation-area__item-container\"])[1]").MustClick()
+		// check texts on warning page
+		warningHeader:= page.MustElement("h1.warning-view__container__title").MustText()
+		assert.Equal(t, "Object Browser", warningHeader)
+		warningSubTitle:= page.MustElement("h2.warning-view__container__message-container__sub-title").MustText()
+		assert.Equal(t, "The object browser uses server side encryption.",warningSubTitle)
+		warningMessage:= page.MustElement(".warning-view__container__message-container__message").MustText()
+		assert.Equal(t, "If you want to use our product with only end-to-end encryption, you may want to skip this feature.", warningMessage)
+		returnToDashboard:= page.MustElement(".warning-view__container__buttons-area > div:nth-child(1)")
+		continueButton:= page.MustElement(".warning-view__container__buttons-area > div:nth-child(2)")
+		assert.Equal(t, "Return to dashboard",returnToDashboard.MustText())
+		assert.Equal(t, "Continue",continueButton.MustText())
+	}
+
 	func Test_create_500_chars (t *testing.T){
 	page, browser := login_to_account()
 	defer browser.MustClose()
@@ -23,5 +40,4 @@ import (
 	page.MustElement("div.container").MustClick().MustWaitInvisible()
 	nameFromUI:= page.MustElement("p.bucket-item__name__value").MustText()
 	assert.Equal(t, bucketName,nameFromUI)
-
 }
