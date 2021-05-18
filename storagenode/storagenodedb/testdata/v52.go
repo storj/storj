a266391967d38bf880c06faad1c0326a8bc69f39
Moby von Briesen <mobyvb@gmail.com>
2021-05-18 11:06:55 -0400

    add data to dbstate
    Change-Id: Ifd973a64eaadcf117469d6bb9faa2af15bf45acf

--- storagenode/storagenodedb/testdata/v52.go
+++ storagenode/storagenodedb/testdata/v52.go
@@ -21,6 +21,14 @@ var v52 = MultiDBState{
 		storagenodedb.NotificationsDBName:   v47.DBStates[storagenodedb.NotificationsDBName],
 		storagenodedb.HeldAmountDBName: &DBState{
 			SQL: v51.DBStates[storagenodedb.HeldAmountDBName].SQL,
+			OldData: `
+			-- distributed has been updated for the periods < 2020-12.
+			INSERT INTO paystubs (period,    satellite_id, created_at,                    codes, usage_at_rest, usage_get, usage_put, usage_get_repair, usage_put_repair, usage_get_audit, comp_at_rest, comp_get, comp_put, comp_get_repair, comp_put_repair, comp_get_audit, surge_percent, held, owed, disposed, paid, distributed) VALUES
+			                     ('2020-10', 'foo',        '2020-04-07T00:00:00.000000Z', 'X',   100,           200,       300,       400,              500,              600,             700,          800,      900,      1000,            1100,            1200,           1300,          1400, 1500, 1600,     1700, 1700),
+			                     ('2020-11', 'foo',        '2020-04-07T00:00:00.000000Z', 'X',   101,           201,       301,       401,              501,              601,             701,          801,      901,      1010,            1101,            1201,           1301,          1401, 1501, 1601,     1701, 1701),
+			                     ('2020-12', 'foo',        '2020-04-07T00:00:00.000000Z', 'X',   102,           202,       302,       402,              502,              602,             702,          802,      902,      1020,            1102,            1202,           1302,          1402, 1502, 1602,     1702, 0),
+			                     ('2021-01', 'foo',        '2020-04-07T00:00:00.000000Z', 'X',   103,           203,       303,       403,              503,              603,             703,          803,      903,      1030,            1103,            1203,           1303,          1403, 1503, 1603,     1703, 0)
+			`,
 		},
 		storagenodedb.PricingDBName: v47.DBStates[storagenodedb.PricingDBName],
 		storagenodedb.APIKeysDBName: v47.DBStates[storagenodedb.APIKeysDBName],
