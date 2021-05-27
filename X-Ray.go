6c0952ca55e26570680c67b55d89b56a6e816f4c
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-27 03:37:27 -0300

    Create X-Ray.go

---
+++ X-Ray.go
@@ -0,0 +1,15 @@
+Pass or Error, X- Ray.
+
+Nº occurrence (https://dedic.pcivil.rj.gov.br/)
+
+rdm.chl('anoonimon/:128965518', {
+  acs (bce, rtl, mns) {
+     ctx.params.id === pss or revoke.userId
+  }
+})
+
+Sponsors: 
+
+-  [Paypal](https://www.paypal.com/donate?business=RBHMVN4AQGQE2&item_name=Donation&currency_code=BRL)
+
+-  Dogecoin Wallet : D7RMk96j7BLzTcy1hVqF5fAtpA4khmsyvA
