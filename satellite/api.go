cb18dc77fc4ace1d01c3b5d386c3ceb9a63b75ab
JT Olio <hello@jtolio.com>
2021-06-28 22:18:48 -0600

    satellite/stripecoinpayments: version is the wrong name
    this service exists to do currency conversions, which is
    the best I can assume that this was meant to be named.
    
    Change-Id: Ia2416f5475749e8bfe8d05bf491649576f6d77bf

--- satellite/api.go
+++ satellite/api.go
@@ -124,10 +124,10 @@ type API struct {
 	}
 
 	Payments struct {
-		Accounts payments.Accounts
-		Version  *stripecoinpayments.VersionService
-		Service  *stripecoinpayments.Service
-		Stripe   stripecoinpayments.StripeClient
+		Accounts   payments.Accounts
+		Conversion *stripecoinpayments.ConversionService
+		Service    *stripecoinpayments.Service
+		Stripe     stripecoinpayments.StripeClient
 	}
 
 	Console struct {
@@ -533,15 +533,15 @@ func NewAPI(log *zap.Logger, full *identity.FullIdentity, db DB,
 
 		peer.Payments.Stripe = stripeClient
 		peer.Payments.Accounts = peer.Payments.Service.Accounts()
-		peer.Payments.Version = stripecoinpayments.NewVersionService(
+		peer.Payments.Conversion = stripecoinpayments.NewConversionService(
 			peer.Log.Named("payments.stripe:version"),
 			peer.Payments.Service,
 			pc.StripeCoinPayments.ConversionRatesCycleInterval)
 
 		peer.Services.Add(lifecycle.Item{
 			Name:  "payments.stripe:version",
-			Run:   peer.Payments.Version.Run,
-			Close: peer.Payments.Version.Close,
+			Run:   peer.Payments.Conversion.Run,
+			Close: peer.Payments.Conversion.Close,
 		})
 	}
 
