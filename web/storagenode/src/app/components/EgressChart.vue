ca479f4ec1ecedec585adc5dce5774abd15144f6
StobR <87031790+StobR@users.noreply.github.com>
2021-07-06 17:31:20 +0100

    Update EgressChart.vue
    Fixes z-index issue for mouseover tooltips

--- web/storagenode/src/app/components/EgressChart.vue
+++ web/storagenode/src/app/components/EgressChart.vue
@@ -146,6 +146,7 @@ export default class EgressChart extends BaseChart {
         min-width: 50px;
         min-height: 30px;
         pointer-events: none;
+        z-index: 9999;
     }
 
     .egress-tooltip-body {
@@ -161,6 +162,7 @@ export default class EgressChart extends BaseChart {
             margin-bottom: 14px;
             position: relative;
             color: var(--egress-font-color);
+            z-index: 9999;
         }
     }
 
