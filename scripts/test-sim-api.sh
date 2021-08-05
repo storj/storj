e3a3c98553827ac9ed9ac8b916896a8970143f87
storj-antonio <antonio@storj.io>
2021-08-05 12:43:29 -0400

    Getting error 2 when make calls test-sim-api.sh. This indicates a misuse of bash builtins. removed bash buitins so I can get some visability.

--- scripts/test-sim-api.sh
+++ scripts/test-sim-api.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ueo pipefail
+#set -ueo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPOROOT="$( cd "../$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
