086069b59f3e4968159272fca72a1a2de93cbbe1
storj-antonio <antonio@storj.io>
2021-08-06 01:28:13 -0400

    resolved syntax error in test_graphql.sh for reporoot env variable by removing it as its logic was invalid.

--- web/satellite/tests/graphql/test_graphql.sh
+++ web/satellite/tests/graphql/test_graphql.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-REPOROOT="${REPOROOT}:-${PWD}/../web/satellite/tests/graphql/"
-
 go run "$REPOROOT"/web/satellite/tests/graphql/main.go
 # docker pull postman/newman:alpine
 # docker run --network="host" -v ${PWD}/web/satellite/tests/graphql/:/etc/newman -t postman/newman:alpine run GraphQL.postman_collection.json -e GraphQLEndoints.postman_environment.json;
