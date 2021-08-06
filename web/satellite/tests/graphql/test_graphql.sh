83a9e04dbc61736dd155a879bd57d41fa8e85bfd
storj-antonio <antonio@storj.io>
2021-08-06 02:10:28 -0400

    removed some debug data as to not flood the logs and so that the introspection outut can be easily observed. added logic to the test_graphql.sh introspection test to pass the exit code back to the test-sim-api.sh script.

--- web/satellite/tests/graphql/test_graphql.sh
+++ web/satellite/tests/graphql/test_graphql.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
+function onExit {
+    if [ "$?" != "0" ]; then
+        echo "Tests failed";
+        # build failed, don't deploy
+        exit 1;
+    else
+        echo "Tests passed";
+        # deploy build
+		exit 0;
+    fi
+}
+
+trap onExit EXIT
 go run "$REPOROOT"/web/satellite/tests/graphql/main.go
-# docker pull postman/newman:alpine
-# docker run --network="host" -v ${PWD}/web/satellite/tests/graphql/:/etc/newman -t postman/newman:alpine run GraphQL.postman_collection.json -e GraphQLEndoints.postman_environment.json;
+
+# docker run --network="host" -v "$REPOROOT"/web/satellite/tests/graphql/:/etc/newman -t postman/newman:alpine run GraphQL.postman_collection.json -e GraphQLEndoints.postman_environment.json;
