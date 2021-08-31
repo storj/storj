e02d39091fce1dda34f17b8b0ae3a71492b0607b
Ivan Fraixedes <ivan@fraixed.es>
2021-08-31 18:40:29 +0200

    WIP: add the missing endpoints to api.ts
    Change-Id: I31d8349a527546cd4ecadfb350ad113100dd5545

--- satellite/admin/ui/src/api.ts
+++ satellite/admin/ui/src/api.ts
@@ -11,6 +11,104 @@ export interface API {
 
 export class Admin {
   readonly operations = {
+    coupons: [
+      {
+        name: "create",
+        desc: "Add a new coupon to a user's account",
+        params: [
+          ["user ID", new InputText("text", true)],
+          ["duration (months)", new InputText("number", true)],
+          ["amount (in USD cents)", new InputText("number", true)],
+          ["description", new Textarea(true)],
+        ],
+        func: async (
+          userId: string,
+          duration: number,
+          amount: number,
+          description: string
+        ): Promise<object> => {
+          return this.fetch("POST", "coupons", null, {
+            userId,
+            duration,
+            amount,
+            description,
+          });
+        },
+      },
+      {
+        name: "delete",
+        desc: "Delete a specific coupon",
+        params: [["Coupon ID", new InputText("text", true)]],
+        func: async (couponId: string): Promise<null> => {
+          return this.fetch("DELETE", `coupons/${couponId}`) as Promise<null>;
+        },
+      },
+      {
+        name: "get",
+        desc: "Get the information of a specific coupon",
+        params: [["Coupon ID", new InputText("text", true)]],
+        func: async (couponId: string): Promise<object> => {
+          return this.fetch("GET", `coupons/${couponId}`);
+        },
+      },
+    ],
+    project: [
+      {
+        name: "create",
+        desc: "Add a new project to a specific user",
+        params: [
+          ["Owner ID (user ID)", new InputText("text", true)],
+          ["Project Name", new InputText("text", true)],
+        ],
+        func: async (ownerId: string, projectName: string): Promise<object> => {
+          return this.fetch("POST", "projects", null, { ownerId, projectName });
+        },
+      },
+      {
+        name: "delete",
+        desc: "Delete a specific project",
+        params: [["Project ID", new InputText("text", true)]],
+        func: async (projectId: string): Promise<null> => {
+          return this.fetch("DELETE", `projects/${projectId}`) as Promise<null>;
+        },
+      },
+      {
+        name: "get",
+        desc: "Get the information of a specific project",
+        params: [["Project ID", new InputText("text", true)]],
+        func: async (projectId: string): Promise<object> => {
+          return this.fetch("GET", `projects/${projectId}`);
+        },
+      },
+      {
+        name: "update",
+        desc: "Update the information of a specific project",
+        params: [
+          ["Project ID", new InputText("text", true)],
+          ["Project Name", new InputText("text", true)],
+          ["Description", new InputText("text", false)],
+        ],
+        func: async (
+          projectId: string,
+          projectName: string,
+          description: string
+        ): Promise<null> => {
+          return this.fetch("POST", `projects/${projectId}`, null, {
+            projectName,
+            description,
+          }) as Promise<null>;
+        },
+      },
+      {
+        name: "get API keys",
+        desc: "Get the API keys of a specific project",
+        params: [["Project ID", new InputText("text", true)]],
+        func: async (projectId: string): Promise<object> => {
+          return this.fetch("GET", `projects/${projectId}/apiKeys`);
+        },
+      },
+      // TODO:continue with the POST /api/projects/{project}/apiKeys endpoint.
+    ],
     user: [
       {
         name: "example", // TODO: Delete this endpoint.
@@ -58,6 +156,71 @@ export class Admin {
               }
             }, 2000);
           });
+        },
+      },
+      {
+        name: "create",
+        desc: "Create a new user account",
+        params: [
+          ["email", new InputText("email", true)],
+          ["full name", new InputText("text", false)],
+          ["password", new InputText("password", true)],
+        ],
+        func: async (
+          email: string,
+          fullName: string,
+          password: string
+        ): Promise<object> => {
+          return this.fetch("POST", "users", null, {
+            email,
+            fullName,
+            password,
+          });
+        },
+      },
+      {
+        name: "delete",
+        desc: "Delete a user's account",
+        params: [["email", new InputText("email", true)]],
+        func: async (email: string): Promise<null> => {
+          return this.fetch("DELETE", `users/${email}`) as Promise<null>;
+        },
+      },
+      {
+        name: "get",
+        desc: "Get the information of a user's account",
+        params: [["email", new InputText("email", true)]],
+        func: async (email: string): Promise<object> => {
+          return this.fetch("GET", `users/${email}`);
+        },
+      },
+      {
+        name: "update",
+        desc: `Update the information of a user's account.
+Blank fields will not be updated.`,
+        params: [
+          ["current user's email", new InputText("email", true)],
+          ["new email", new InputText("email", false)],
+          ["full name", new InputText("text", false)],
+          ["short name", new InputText("text", false)],
+          ["partner ID", new InputText("text", false)],
+          ["password hash", new InputText("text", false)],
+        ],
+        func: async (
+          currentEmail: string,
+          email?: string,
+          fullName?: string,
+          shortName?: string,
+          partnerID?: string,
+          passwordHash?: string
+        ): Promise<null> => {
+          return this.fetch("PUT", `users/${currentEmail}`, null, {
+            email,
+            fullName,
+            shortName,
+            partnerID,
+            passwordHash,
+          }) as Promise<null>;
         },
       },
     ],
