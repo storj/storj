f2f8a9ca1ec7e2417e302a1da65f57a1986100c9
Michał Niewrzał <michal@storj.io>
2021-09-28 13:00:08 +0200

    satellite/metabase: send metric about zombie segment deletion
    We would like to verify if zombie object/segment works fine.
    We need some metric for that. Figuring out number of deleted
    objects is harder so let's for that later.
    
    Change-Id: Ic99e2ce93256130b7c51f514824fddc009655075

--- satellite/metabase/delete_objects.go
+++ satellite/metabase/delete_objects.go
@@ -255,11 +255,20 @@ func (db *DB) deleteInactiveObjectsAndSegments(ctx context.Context, objects []Ob
 		results := conn.SendBatch(ctx, &batch)
 		defer func() { err = errs.Combine(err, results.Close()) }()
 
+		var segmentsDeleted int64
 		var errlist errs.Group
 		for i := 0; i < batch.Len(); i++ {
-			_, err := results.Exec()
+			result, err := results.Exec()
 			errlist.Add(err)
+
+			if err == nil {
+				segmentsDeleted += result.RowsAffected()
+			}
 		}
+
+		// TODO calculate deleted objects
+		mon.Meter("zombie_segment_delete").Mark64(segmentsDeleted)
+		mon.Meter("segment_delete").Mark64(segmentsDeleted)
 
 		return errlist.Err()
 	})
