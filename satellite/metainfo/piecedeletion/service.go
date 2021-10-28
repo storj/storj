edb8d656de0ae1e45b9dc74b99e3c7371f28abb3
Egon Elbre <egonelbre@gmail.com>
2021-10-28 13:37:01 +0300

    satellite/metainfo: adjust piecedeletion timeouts
    Currently slower storagenodes can slow down deletion queue.
    To make piece deletion faster reduce the maximum time spent in
    either dialing or piece deletion requests.
    
    With this change:
    * dial timeout is 3s
    * request timeout is 15s
    * fail threshold is set to 10min
    
    Similarly, we'll mark storage node as failed when the timeout occurs.
    The timeout usually indicates that the storagenode is overwhelmed.
    Garbage collection will ensure that the pieces get deleted eventually.
    
    Change-Id: Iec5de699f5917905f5807140e2c3252088c6399b

--- satellite/metainfo/piecedeletion/service.go
+++ satellite/metainfo/piecedeletion/service.go
@@ -25,9 +25,9 @@ type Config struct {
 	MaxPiecesPerBatch   int `help:"maximum number of pieces per batch" default:"5000" testDefault:"4000"`
 	MaxPiecesPerRequest int `help:"maximum number pieces per single request" default:"1000" testDefault:"2000"`
 
-	DialTimeout    time.Duration `help:"timeout for dialing nodes (0 means satellite default)" default:"0" testDefault:"2s"`
-	FailThreshold  time.Duration `help:"threshold for retrying a failed node" releaseDefault:"5m" devDefault:"2s"`
-	RequestTimeout time.Duration `help:"timeout for a single delete request" releaseDefault:"1m" devDefault:"2s"`
+	DialTimeout    time.Duration `help:"timeout for dialing nodes (0 means satellite default)" default:"3s" testDefault:"2s"`
+	FailThreshold  time.Duration `help:"threshold for retrying a failed node" releaseDefault:"10m" devDefault:"2s"`
+	RequestTimeout time.Duration `help:"timeout for a single delete request" releaseDefault:"15s" devDefault:"2s"`
 }
 
 const (
