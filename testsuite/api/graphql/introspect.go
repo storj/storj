e914ca80d2bf0f00ff2fcb76d940b76a7900e33f
Antonio Franco <antonio@storj.io>
2021-11-03 09:35:38 -0400

    testsuite/api/graphql:Implements Introspection Test
    Rework of introspection tests to use embeded structs for Client/Server
    implementation of test. This improves the code and helps to remove
    panics on errors. Logging was added with zap and errs libraries.
    endpoints.go was removed as we no longer need a main funciton to call
    these tests, instead we are using the _test.go/packagename_test
    nomenclature to be consistent with other tests in the repo.

--- testsuite/api/graphql/introspect.go
+++ testsuite/api/graphql/introspect.go
@@ -10,24 +10,44 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+
+	"github.com/zeebo/errs"
+	"go.uber.org/zap"
 )
 
 var (
 	// graphql introspection query.
 	graphql = []byte(`{"query":"query  IntrospectionQuery {  __schema {  queryType { name }  mutationType { name }  subscriptionType { name }  types { ...FullType }  directives {  name  description  args { ...InputValue }  }  } }  fragment FullType on __Type {  kind  name  description  fields(includeDeprecated: true) {  name  description  args { ...InputValue }  type { ...TypeRef }  isDeprecated  deprecationReason  }  inputFields { ...InputValue }  interfaces { ...TypeRef }  enumValues(includeDeprecated: true) {  name  description  isDeprecated  deprecationReason  }  possibleTypes { ...TypeRef } }  fragment InputValue on __InputValue {  name  description  type { ...TypeRef }  defaultValue }  fragment TypeRef on __Type {  kind  name  ofType {  kind  name  ofType {  kind  name  ofType {  kind  name  ofType {  kind  name  ofType {  kind  name  ofType {  kind  name  ofType {  kind  name  }  }  }  }  }  }  } }  "}`)
+	err     = errs.Class("graphql")
 )
 
-func check(e error) { // easy error handler.
-	if e != nil {
-		panic(e)
+type Server struct {
+	fullpath  string
+	sataddr   string
+	satendpnt string // "/api/v0/graphql"
+}
+
+type Endpoint struct {
+	Log      *zap.Logger
+	request  []byte
+	server   *Server
+	response []byte
+}
+
+func New(log *zap.Logger, r []byte) (endpoint *Endpoint, err error) {
+	return &Endpoint{
+		Log:     log,
+		request: graphql,
+		server: Server{
+			satendpnt: "/api/v0/graphql",
+		},
 	}
 }
 
-// introspect takes a url and returns a byte slice of endpoints.
-func introspect(src string) (endpoints []byte, err error) { // pass in the src url and return []byte.
-
-	u, err := url.Parse(src) // parse the url to remove https://  and /path/tofile.
-	if err != nil {          // this is needed to set the host property of the header.
+// introspect takes a url and returns a byte slice of endpoints and an error message.
+func Introspect(src string) (endpoints []byte, err error) { // pass in the src url and return []byte slice and error || nil.
+	url, err := url.Parse(src) // parse the url to remove https://  and /path/tofile.
+	if err != nil {            // this is needed to set the host property of the header.
 		fmt.Println("Oh noes!!")
 	}
 	ctx := context.Background()
@@ -37,7 +57,7 @@ func introspect(src string) (endpoints []byte, err error) { // pass in the src u
 	req.Header.Set("Pragma", "no-cache")
 	req.Header.Set("Cache-Control", "no-cache")
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Host", u.Host)
+	req.Header.Set("Host", url.Host)
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Content-Type", "application/json")
 
