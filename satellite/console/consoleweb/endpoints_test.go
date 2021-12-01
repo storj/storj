2d4d6ba7e76111a8c346e55e722b8ca47b25549f
storj-antonio <antonio@storj.io>
2021-12-01 03:49:36 -0500

    Removhing brute force test from this unit test and including it into the functional tests contained in intros[ection_test.go.

--- satellite/console/consoleweb/endpoints_test.go
+++ satellite/console/consoleweb/endpoints_test.go
@@ -94,22 +94,6 @@ func TestAuth(t *testing.T) {
 			require.Contains(test.t, body, "error")
 			require.Equal(test.t, http.StatusUnauthorized, resp.StatusCode)
 		}
-
-		{ // repeated login attempts should end in too many requests
-			hitRateLimiter := false
-			for i := 0; i < 30; i++ {
-				resp, _ := test.request(
-					http.MethodPost, "/auth/token",
-					strings.NewReader(`{"email":"wrong@invalid.test","password":"wrong"}`))
-				require.Nil(t, findCookie(resp, "_tokenKey"))
-				if resp.StatusCode != http.StatusUnauthorized {
-					require.Equal(t, http.StatusTooManyRequests, resp.StatusCode)
-					hitRateLimiter = true
-					break
-				}
-			}
-			require.True(t, hitRateLimiter, "did not hit rate limiter")
-		}
 	})
 }
 
