6f67687311c4add46161ad0c78ff1c072cafe844
nadimhq <82845192+nadimhq@users.noreply.github.com>
2021-12-23 08:49:05 -0500

    testsuite/ui/satellite: email verification UI tests
    Added UI tests surrounding email verification

---
+++ testsuite/ui/satellite/login_unverified_nonexistent_account_test.go
@@ -0,0 +1,34 @@
+// Copyright (C) 2021 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package satellite
+
+import (
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	"github.com/stretchr/testify/require"
+	"storj.io/common/testcontext"
+	"storj.io/storj/private/testplanet"
+	"storj.io/storj/testsuite/ui/uitest"
+	"testing"
+)
+
+func TestLoginUnverifiedNonexistentAccount(t *testing.T) {
+	uitest.Run(t, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet, browser *rod.Browser) {
+		loginPageURL := planet.Satellites[0].ConsoleURL() + "/login"
+		emailAddress := "unverified@andnonexistent.test"
+		password := "qazwsx123"
+		page := browser.MustPage(loginPageURL)
+		page.MustSetViewport(1350, 600, 1, false)
+
+		// login with unverified/nonexistent email
+		page.MustElement("div.login-area__input-wrapper:nth-child(2)").MustClick().MustInput(emailAddress)
+		page.MustElement("div.login-area__input-wrapper:nth-child(3)").MustClick().MustInput(password)
+		page.Keyboard.MustPress(input.Enter)
+
+		//check for error message for unverified/nonexistent
+		invalidEmailPasswordMessage := page.MustElement(".notification-wrap__text-area__message").MustText()
+		require.Contains(t, invalidEmailPasswordMessage, "Your email or password was incorrect, please try again")
+
+	})
+}
