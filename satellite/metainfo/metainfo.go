bfad42f29a7d48bf6c43214a2b25e4cab66d2fd8
Michał Niewrzał <michal@storj.io>
2022-01-21 15:52:12 +0100

    satellite/metainfo: split metainfo endpoint into smaller files
    This is part of change to split metainfo endpoint into smaller
    files. It will be grouped by bucket/object/segment/other requests.
    
    Tests will be split as a separate set of changes.
    
    Change-Id: I5128c84e06c82777fe71460bf5f9a6e26e52a243

--- satellite/metainfo/metainfo.go
+++ satellite/metainfo/metainfo.go
@@ -6,171 +6,33 @@ package metainfo
 import (
 	"bytes"
 	"context"
-	"crypto/sha256"
 	"fmt"
 	"time"
 
-	"github.com/spacemonkeygo/monkit/v3"
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 
 	"storj.io/common/context2"
 	"storj.io/common/encryption"
 	"storj.io/common/errs2"
-	"storj.io/common/lrucache"
 	"storj.io/common/macaroon"
 	"storj.io/common/memory"
 	"storj.io/common/pb"
 	"storj.io/common/rpc/rpcstatus"
-	"storj.io/common/signing"
 	"storj.io/common/storj"
 	"storj.io/common/uuid"
-	"storj.io/storj/satellite/accounting"
-	"storj.io/storj/satellite/attribution"
 	"storj.io/storj/satellite/buckets"
-	"storj.io/storj/satellite/console"
 	"storj.io/storj/satellite/internalpb"
 	"storj.io/storj/satellite/metabase"
 	"storj.io/storj/satellite/metainfo/piecedeletion"
-	"storj.io/storj/satellite/metainfo/pointerverification"
 	"storj.io/storj/satellite/orders"
 	"storj.io/storj/satellite/overlay"
-	"storj.io/storj/satellite/revocation"
-	"storj.io/storj/satellite/rewards"
 	"storj.io/uplink/private/eestream"
 )
-
-const (
-	satIDExpiration = 48 * time.Hour
-
-	deleteObjectPiecesSuccessThreshold = 0.75
-)
-
-var (
-	mon = monkit.Package()
-	// Error general metainfo error.
-	Error = errs.Class("metainfo")
-	// ErrNodeAlreadyExists pointer already has a piece for a node err.
-	ErrNodeAlreadyExists = errs.Class("metainfo: node already exists")
-	// ErrBucketNotEmpty is returned when bucket is required to be empty for an operation.
-	ErrBucketNotEmpty = errs.Class("bucket not empty")
-)
-
-// APIKeys is api keys store methods used by endpoint.
-//
-// architecture: Database
-type APIKeys interface {
-	GetByHead(ctx context.Context, head []byte) (*console.APIKeyInfo, error)
-}
-
-// Endpoint metainfo endpoint.
-//
-// architecture: Endpoint
-type Endpoint struct {
-	pb.DRPCMetainfoUnimplementedServer
-
-	log                  *zap.Logger
-	buckets              *buckets.Service
-	metabase             *metabase.DB
-	deletePieces         *piecedeletion.Service
-	orders               *orders.Service
-	overlay              *overlay.Service
-	attributions         attribution.DB
-	partners             *rewards.PartnersService
-	pointerVerification  *pointerverification.Service
-	projectUsage         *accounting.Service
-	projects             console.Projects
-	apiKeys              APIKeys
-	satellite            signing.Signer
-	limiterCache         *lrucache.ExpiringLRU
-	encInlineSegmentSize int64 // max inline segment size + encryption overhead
-	revocations          revocation.DB
-	defaultRS            *pb.RedundancyScheme
-	config               Config
-	versionCollector     *versionCollector
-}
-
-// NewEndpoint creates new metainfo endpoint instance.
-func NewEndpoint(log *zap.Logger, buckets *buckets.Service, metabaseDB *metabase.DB,
-	deletePieces *piecedeletion.Service, orders *orders.Service, cache *overlay.Service,
-	attributions attribution.DB, partners *rewards.PartnersService, peerIdentities overlay.PeerIdentities,
-	apiKeys APIKeys, projectUsage *accounting.Service, projects console.Projects,
-	satellite signing.Signer, revocations revocation.DB, config Config) (*Endpoint, error) {
-	// TODO do something with too many params
-
-	encInlineSegmentSize, err := encryption.CalcEncryptedSize(config.MaxInlineSegmentSize.Int64(), storj.EncryptionParameters{
-		CipherSuite: storj.EncAESGCM,
-		BlockSize:   128, // intentionally low block size to allow maximum possible encryption overhead
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	defaultRSScheme := &pb.RedundancyScheme{
-		Type:             pb.RedundancyScheme_RS,
-		MinReq:           int32(config.RS.Min),
-		RepairThreshold:  int32(config.RS.Repair),
-		SuccessThreshold: int32(config.RS.Success),
-		Total:            int32(config.RS.Total),
-		ErasureShareSize: config.RS.ErasureShareSize.Int32(),
-	}
-
-	return &Endpoint{
-		log:                 log,
-		buckets:             buckets,
-		metabase:            metabaseDB,
-		deletePieces:        deletePieces,
-		orders:              orders,
-		overlay:             cache,
-		attributions:        attributions,
-		partners:            partners,
-		pointerVerification: pointerverification.NewService(peerIdentities),
-		apiKeys:             apiKeys,
-		projectUsage:        projectUsage,
-		projects:            projects,
-		satellite:           satellite,
-		limiterCache: lrucache.New(lrucache.Options{
-			Capacity:   config.RateLimiter.CacheCapacity,
-			Expiration: config.RateLimiter.CacheExpiration,
-		}),
-		encInlineSegmentSize: encInlineSegmentSize,
-		revocations:          revocations,
-		defaultRS:            defaultRSScheme,
-		config:               config,
-		versionCollector:     newVersionCollector(log),
-	}, nil
-}
-
-// Close closes resources.
-func (endpoint *Endpoint) Close() error { return nil }
 
 func calculateSpaceUsed(segmentSize int64, numberOfPieces int, rs storj.RedundancyScheme) (totalStored int64) {
 	pieceSize := segmentSize / int64(rs.RequiredShares)
 	return pieceSize * int64(numberOfPieces)
-}
-
-// ProjectInfo returns allowed ProjectInfo for the provided API key.
-func (endpoint *Endpoint) ProjectInfo(ctx context.Context, req *pb.ProjectInfoRequest) (_ *pb.ProjectInfoResponse, err error) {
-	defer mon.Task()(&ctx)(&err)
-
-	err = endpoint.versionCollector.collect(req.Header.UserAgent, mon.Func().ShortName())
-	if err != nil {
-		endpoint.log.Warn("unable to collect uplink version", zap.Error(err))
-	}
-
-	keyInfo, err := endpoint.validateAuth(ctx, req.Header, macaroon.Action{
-		Op:   macaroon.ActionProjectInfo,
-		Time: time.Now(),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	salt := sha256.Sum256(keyInfo.ProjectID[:])
-
-	return &pb.ProjectInfoResponse{
-		ProjectSalt: salt[:],
-	}, nil
 }
 
 // GetBucket returns a bucket.
@@ -2684,33 +2546,6 @@ func groupPiecesByNodeID(segments []metabase.DeletedSegmentInfo) map[storj.NodeI
 	}
 
 	return piecesToDelete
-}
-
-// RevokeAPIKey handles requests to revoke an api key.
-func (endpoint *Endpoint) RevokeAPIKey(ctx context.Context, req *pb.RevokeAPIKeyRequest) (resp *pb.RevokeAPIKeyResponse, err error) {
-	defer mon.Task()(&ctx)(&err)
-
-	err = endpoint.versionCollector.collect(req.Header.UserAgent, mon.Func().ShortName())
-	if err != nil {
-		endpoint.log.Warn("unable to collect uplink version", zap.Error(err))
-	}
-
-	macToRevoke, err := macaroon.ParseMacaroon(req.GetApiKey())
-	if err != nil {
-		return nil, rpcstatus.Error(rpcstatus.InvalidArgument, "API key to revoke is not a macaroon")
-	}
-	keyInfo, err := endpoint.validateRevoke(ctx, req.Header, macToRevoke)
-	if err != nil {
-		return nil, err
-	}
-
-	err = endpoint.revocations.Revoke(ctx, macToRevoke.Tail(), keyInfo.ID[:])
-	if err != nil {
-		endpoint.log.Error("Failed to revoke API key", zap.Error(err))
-		return nil, rpcstatus.Error(rpcstatus.Internal, "Failed to revoke API key")
-	}
-
-	return &pb.RevokeAPIKeyResponse{}, nil
 }
 
 // Server side move.
