87bf3dcc3e55780cd1c7359a8bdb276b011efc47
cl-mitch <mitch.george@compozelabs.com>
2022-05-09 13:40:08 -0500

    updated snapshots

--- web/satellite/jest.config.js
+++ web/satellite/jest.config.js
@@ -1,6 +1,8 @@
 // Copyright (C) 2022 Storj Labs, Inc.
 // See LICENSE for copying information.
 
+process.env.TZ = 'UTC';
+
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest/presets/typescript',
     setupFiles: ["./jest.setup.ts"],
