960354de2555ed207a08af4d86ef1c896317aff9
Andrew Hove <andrew.hove@concordusa.com>
2022-06-15 12:25:33 -0500

    latest stripe integration changes

--- web/satellite/src/components/account/billing/billingTabs/PaymentMethods.vue
+++ web/satellite/src/components/account/billing/billingTabs/PaymentMethods.vue
@@ -39,29 +39,65 @@
                             is-transparent="true"
                             font-size="13px"
                             class=""
-                        >
+                        />
                         <v-button
                             label='Add funds'
                             font-size="13px"
                             width="auto"
                             height="30px"
                             class=""
-                        >
+                        />
                     </div>
                 </div>
-                <div
-                    class="payments-area__container__cards"
-                >
-                    
-                </div>
-                <div 
-                    class="payments-area__container__new-payments"
-                    @click="toggleCreateModal"
-                >
-                    <div class="payments-area__container__new-payments__text-area">
+                <div class="payments-area__container__cards" />
+                <div class="payments-area__container__new-payments">
+                    <div v-if="!isAddingPayment" class="payments-area__container__new-payments__text-area">
                         <span class="payments-area__container__new-payments__text-area__plus-icon">+&nbsp;</span>
-                        <span class="payments-area__container__new-payments__text-area__text">Add New Payment Method</span>
+                        <span 
+                            class="payments-area__container__new-payments__text-area__text"
+                            @click="addPaymentMethodHandler"
+                        >Add New Payment Method</span>
                     </div>
+                    <div v-if="isAddingPayment">
+                        <div class="payments-area__create-header">Credit Card</div>
+                        <div class="payments-area__create-subheader">Add Card Info</div>
+                        <StripeCardInput
+                            ref="stripeCardInput"
+                            class="add-card-area__stripe stripe_input"
+                            :on-stripe-response-callback="addCard"
+                        />
+                        <div
+                            v-if="!isAddCardClicked"
+                            class="add-card-button"
+                            @click="addCard"
+                        >
+                            <img
+                                v-if="isLoading"
+                                class="payment-loading-image"
+                                src="@/../static/images/account/billing/loading.gif"
+                                alt="loading gif"
+                            >
+                            <SuccessImage
+                                v-if="isLoaded"
+                                class="payment-loaded-image"
+                            />
+                            <span class="add_card_button_text">Add Credit Card</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Edit Credit Card Modal -->
+            <div v-if="isEditPaymentMethodsModalOpen" class="add_payment_method">
+                <div class="add_payment_method__container">
+                    <CreditCard  class="card-icon" />
+                    <div class="add_payment_method__header">Add Credit Card</div>
+                    <div class="add_payment_method__header-subtext">This is not your default payment card.</div>
+                    <div class="add_payment_method__container__close-cross-container" @click="onCloseClick">
+                        <CloseCrossIcon />
+                    </div>
+                    <form>
+                        <label>Card Number</label>
+                    </form>
                 </div>
             </div>
             <Addpayments2 
@@ -77,11 +113,26 @@ import { Component, Vue } from 'vue-property-decorator';
 
 import VLoader from '@/components/common/VLoader.vue';
 import VButton from '@/components/common/VButton.vue';
+import CloseCrossIcon from '@/../static/images/common/closeCross.svg';
+import StripeCardInput from '@/components/account/billing/paymentMethods/StripeCardInput.vue';
+import SuccessImage from '@/../static/images/account/billing/success.svg';
 
 import StorjSmall from '@/../static/images/billing/storj-icon-small.svg';
 import StorjLarge from '@/../static/images/billing/storj-icon-large.svg';
+import CreditCard from '@/../static/images/billing/credit-card.svg';
+import { PAYMENTS_ACTIONS } from '@/store/modules/payments';
+import { USER_ACTIONS } from '@/store/modules/users';
 
 import { RouteConfig } from '@/router';
+
+
+interface StripeForm {
+    onSubmit(): Promise<void>;
+}
+const {
+    ADD_CREDIT_CARD,
+    GET_CREDIT_CARDS,
+} = PAYMENTS_ACTIONS;
 
 // @vue/component
 @Component({
@@ -90,27 +141,219 @@ import { RouteConfig } from '@/router';
         StorjSmall,
         StorjLarge,
         VButton,
+        CloseCrossIcon,
+        CreditCard,
+        StripeCardInput,
+        SuccessImage
     },
 })
 export default class paymentsArea extends Vue {
-    public depositStatus: string = 'Confirmed';
-    public balanceAmount: number = 0.00;
-
-    public testData = [{},{},{}]
-
+    public isAddingPayment = false;
+    public isEditPaymentMethodsModalOpen = false;
+    public depositStatus = 'Confirmed';
+    public balanceAmount = 0.00; 
+    public testData = [{},{},{}];
+    public isAddCardClicked = false;
+    public $refs!: {
+        stripeCardInput: StripeCardInput & StripeForm;
+    };
     /**
      * Lifecycle hook after initial render.
      * Fetches payments.
      */
-    public async mounted(): Promise<void> {
+    public async mounted() {
+    }
+
+    
+
+    public async addCard(token: string): Promise<void> {
+        this.$emit('toggleIsLoading');
+        console.log('working');
+        console.log(token, 'token');
+        try {
+        console.log('catch block firing');
+            await this.$store.dispatch(ADD_CREDIT_CARD, token);
+
+            // We fetch User one more time to update their Paid Tier status.
+            await this.$store.dispatch(USER_ACTIONS.GET);
+        } catch (error) {
+            console.log('catching');
+            await this.$notify.error(error.message);
+
+            this.$emit('toggleIsLoading');
+
+            return;
+        }
+
+        await this.$notify.success('Card successfully added');
+        try {
+            await this.$store.dispatch(GET_CREDIT_CARDS);
+        } catch (error) {
+            await this.$notify.error(error.message);
+            this.$emit('toggleIsLoading');
+        }
+
+        this.$emit('toggleIsLoading');
+        this.$emit('toggleIsLoaded');
+
+        setTimeout(() => {
+            this.$emit('cancel');
+            this.$emit('toggleIsLoaded');
+
+            setTimeout(() => {
+                if (!this.userHasOwnProject) {
+                    this.$router.push(RouteConfig.CreateProject.path);
+                }
+            }, 500);
+        }, 2000);
+    }
+    private get userHasOwnProject(): boolean {
+        return this.$store.getters.projectsCount > 0;
+    }
+
+    public async onConfirmAddStripe(): Promise<void> {
+        await this.$refs.stripeCardInput.onSubmit();
+    }
+
+    public addPaymentMethodHandler() {
+        this.isAddingPayment = true;
+    }
+
+    public editPaymentMethodHandler() {
+        this.isEditPaymentMethodsModalOpen = true;
+    }
+
+    public onCloseClick() {
+        this.isEditPaymentMethodsModalOpen = false;
     }
 }
 </script>
 
 <style scoped lang="scss">
+
+    .payments-area__container__new-payments {
+        display: grid !important;
+        grid-template-columns:  6fr;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
+    }
+
+    .card-icon {
+        position: absolute;
+        left: 45.78%;
+        top: 14.00%;
+        bottom: 67.38%;
+    }
+
+     .add_payment_method {
+        // width: 546px;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 100;
+        background: rgb(27 37 51 / 75%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &__header {
+            position: absolute;
+            left: 36%;
+            top: 24%;
+            font-family: sans-serif;
+            font-style: normal;
+            font-weight: 800;
+            font-size: 24px;
+            line-height: 31px;
+            /* or 129% */
+
+            text-align: center;
+            letter-spacing: -0.02em;
+
+            color: #1B2533;
+        }
+        &__header-subtext {
+            position: absolute;
+            left: 30%;
+            top: 29%;
+            font-family: sans-serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 20px;
+            text-align: center;
+            color: #56606D;
+        }
+
+        &__container {
+            width: 546px;
+            height: 550px;
+
+            background: #f5f6fa;
+            border-radius: 6px;
+            display: flex;
+            align-items: flex-start;
+            position: relative;
+
+            &__close-cross-container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                right: 30px;
+                top: 30px;
+                height: 24px;
+                width: 24px;
+                cursor: pointer;
+
+                &:hover .close-cross-svg-path {
+                    fill: #2683ff;
+                }
+            }
+        }
+    }
+
+    .add-card-button {
+        grid-row: 4;
+        grid-column: 1;
+        width: 115px;
+        height: 30px;
+        margin-top: 16px;
+        
+        cursor: pointer;
+        border-radius: 6px;
+        background-color: #0149FF;
+        font-family: 'font_medium', sans-serif;
+        font-size: 16px;
+        line-height: 23px;
+        color: #fff;
+        user-select: none;
+        transition: top 0.5s ease-in-out;
+
+        &:hover {
+            background-color: #0059d0;
+        }
+    }
+    
     .active-discount {
         background: #dffff7;
         color: #00ac26;
+    }
+
+    .add_card_button_text {
+        margin-left: 9px;
+        font-family: font-medium, sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 13px;
+        line-height: 29px;
+        /* identical to box height, or 154% */
+
+        display: flex;
+        align-items: center;
+        letter-spacing: -0.02em;
+        
     }
 
     .inactive-discount {
@@ -126,7 +369,37 @@ export default class paymentsArea extends Vue {
         background: #ac1a00;
     }
 
+    .stripe_input {
+        grid-row: 3;
+        grid-column: 1;
+        width: 260px;
+        margin-top: 10px;
+    }
+
     .payments-area {
+
+        &__create-header {
+            grid-row: 1;
+            grid-column: 1;
+            font-family: sans-serif;
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 27px;
+        }
+
+        &__create-subheader {
+            grid-row: 2;
+            grid-column: 1;
+            
+            font-family: sans-serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 20px;
+            color: #56606D;
+        }
+
 
         &__title {
             font-family: sans-serif;
