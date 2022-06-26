70510199de08223b0d29b2b2dfbd45df066ff976
JT Olio <hello@jtolio.com>
2022-06-25 21:29:23 -0400

    release v1.57.6
    Change-Id: I2cd058c50c67acdaf06782e2c6f1ccf77bba5d82

--- private/version/release.go
+++ private/version/release.go
@@ -6,13 +6,13 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string = "1656032275"
+var buildTimestamp string = "1656206962"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string = "169e9570a71a4949a472b68e3d1aba6f6f6acff4"
+var buildCommitHash string = "864287fba49b17def9cd34bcf92b24e2c4fb774d"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string = "v1.57.5"
+var buildVersion string = "v1.57.6"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
 var buildRelease string = "true"
