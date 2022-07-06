6f4dc963b1084d5902508a3b87a6e65268670ce2
hovex023 <97616907+hovex023@users.noreply.github.com>
2022-07-06 11:42:16 -0500

    web/satellite: Billing history tab
    * tabs and routing implemented
    * added feature flag
    * added billing history buildout
    * Update to use real data for table population
    
    Story: https://github.com/storj/storj/issues/4633
    
    Co-authored-by: cl-mitch <mitch.george@compozelabs.com>

--- web/satellite/src/components/account/billing/billingTabs/BillingHistory.vue
+++ web/satellite/src/components/account/billing/billingTabs/BillingHistory.vue
@@ -1,22 +1,52 @@
-// Copyright (C) 2019 Storj Labs, Inc.
+// Copyright (C) 2022 Storj Labs, Inc.
 // See LICENSE for copying information.
 
 <template>
     <div>
-        Billing History
+        <BillingHistoryHeader />
+        <VList 
+            :data-set="historyItems"
+            :item-component="billingHistoryStructure"
+        />
         <router-view />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import VList from '@/components/common/VList.vue';
+import BillingHistoryHeader from '@/components/account/billing/billingTabs/BillingHistoryHeader.vue';
+import BillingHistoryShape from '@/components/account/billing/billingTabs/BillingHistoryShape.vue';
+import { PaymentsHistoryItem } from '@/types/payments';
 
 // @vue/component
 @Component({
     components: {
+        VList,
+        BillingHistoryHeader,
     },
 })
-export default class BillingArea extends Vue {
 
+export default class BillingArea extends Vue {
+    public get historyItems(): PaymentsHistoryItem[] {
+        return this.$store.state.paymentsModule.paymentsHistory;
+    }
+
+    public get billingHistoryStructure() {
+        return BillingHistoryShape;
+    }
 }
 </script>
+
+<style scoped lang="scss">
+    .billing_history2 {
+        position: relative;
+
+        &__content {
+            background-color: #fff;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+        }
+    }
+</style>
