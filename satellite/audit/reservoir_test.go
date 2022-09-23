e37435602f732bc56aa2f325b10aeadf2459ad34
Michal Niewrzal <michal@storj.io>
2022-09-23 16:00:56 +0200

    satellite/audit: optimize loop observer
    Two things were done to optimize audit observer:
    * monik call was removed as we have different way to track it
    * no new allocation for audit.Segment struct inside observer
    
    Benchmark against 'main':
    name                                         old time/op    new time/op    delta
    RemoteSegment/Cockroach/multiple_segments-8    5.85µs ± 1%    0.74µs ± 4%   -87.28%  (p=0.008 n=5+5)
    
    name                                         old alloc/op   new alloc/op   delta
    RemoteSegment/Cockroach/multiple_segments-8    2.72kB ± 0%    0.00kB           ~     (p=0.079 n=4+5)
    
    name                                         old allocs/op  new allocs/op  delta
    RemoteSegment/Cockroach/multiple_segments-8      50.0 ± 0%       0.0       -100.00%  (p=0.008 n=5+5)
    
    Change-Id: Ib973e48782bad4346eee1cd5aee77f0a50f69258

--- satellite/audit/reservoir_test.go
+++ satellite/audit/reservoir_test.go
@@ -13,20 +13,21 @@ import (
 	"storj.io/common/testrand"
 	"storj.io/common/uuid"
 	"storj.io/storj/satellite/metabase"
+	"storj.io/storj/satellite/metabase/segmentloop"
 )
 
 func TestReservoir(t *testing.T) {
 	rng := rand.New(rand.NewSource(0))
 	r := NewReservoir(3)
 
-	seg := func(n byte) Segment { return Segment{StreamID: uuid.UUID{0: n}} }
+	seg := func(n byte) *segmentloop.Segment { return &segmentloop.Segment{StreamID: uuid.UUID{0: n}} }
 
 	// if we sample 3 segments, we should record all 3
 	r.Sample(rng, seg(1))
 	r.Sample(rng, seg(2))
 	r.Sample(rng, seg(3))
 
-	require.Equal(t, r.Segments[:], []Segment{seg(1), seg(2), seg(3)})
+	require.Equal(t, r.Segments[:], []segmentloop.Segment{*seg(1), *seg(2), *seg(3)})
 }
 
 func TestReservoirBias(t *testing.T) {
@@ -41,7 +42,7 @@ func TestReservoirBias(t *testing.T) {
 		weight1StreamID:  0,
 	}
 
-	segments := []Segment{
+	segments := []*segmentloop.Segment{
 		{
 			StreamID:      weight10StreamID,
 			Position:      metabase.SegmentPosition{},
