c63a40139998ed82dc9ebd87b6b3f6bb20d9d857
Michal Niewrzal <michal@storj.io>
2022-10-05 16:23:24 +0200

    satellite/accounting/tally: bring back monkit call
    We removed monkit call from "object" method because it was using
    too much cpu and was visible on cpu profile but we should first try
    optimized version of this call.
    
    Change-Id: Ib76d8a2968a704ce47235c6dac6edad4e40bde48

--- satellite/accounting/tally/tally.go
+++ satellite/accounting/tally/tally.go
@@ -235,6 +235,8 @@ func (service *Service) Tally(ctx context.Context) (err error) {
 	return errAtRest
 }
 
+var objectFunc = mon.Task()
+
 // BucketTallyCollector collects and adds up tallies for buckets.
 type BucketTallyCollector struct {
 	Now    time.Time
@@ -297,7 +299,9 @@ func (observer *BucketTallyCollector) ensureBucket(location metabase.ObjectLocat
 }
 
 // Object is called for each object once.
-func (observer *BucketTallyCollector) object(ctx context.Context, object metabase.LoopObjectEntry) (err error) {
+func (observer *BucketTallyCollector) object(ctx context.Context, object metabase.LoopObjectEntry) error {
+	defer objectFunc(&ctx)(nil)
+
 	if object.Expired(observer.Now) {
 		return nil
 	}
