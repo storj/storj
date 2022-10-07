d0488b4c589c6a762c30e80337dd76d06d057adb
Malcolm Bouzi <mrbouzi@gmail.com>
2022-10-07 11:36:15 -0400

    web/satellite: update buckets page table to match designs.
    Change-Id: I58de009f743196509ae39612a4201d36b7225e96

--- web/satellite/src/components/common/TableItem.vue
+++ web/satellite/src/components/common/TableItem.vue
@@ -3,6 +3,7 @@
 
 <template>
     <tr
+        class="item-container"
         :class="{ 'selected': selected }"
         @click="onClick"
     >
@@ -10,10 +11,15 @@
             <v-table-checkbox :disabled="selectDisabled" :value="selected" @checkChange="onChange" />
         </th>
         <th v-for="(val, key, index) in item" :key="index" class="align-left data">
+            <BucketGuide v-if="showBucketGuide(index)" :hide-guide="hideGuide" />
             <div v-if="Array.isArray(val)" class="few-items">
                 <p v-for="str in val" :key="str" class="array-val">{{ str }}</p>
             </div>
-            <p v-else>{{ val }}</p>
+            <div v-else class="table-item">
+                <BucketIcon v-if="(tableType.toLowerCase() === 'bucket') && (index == 0)" class="item-icon" />
+                <p :class="{primary: index === 0}">{{ val }}</p>
+                <div v-if="showBucketGuide(index)" class="animation"><span><span /></span></div>
+            </div>
         </th>
         <slot name="options" />
     </tr>
@@ -23,10 +29,17 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import VTableCheckbox from '@/components/common/VTableCheckbox.vue';
+import BucketGuide from '@/components/objects/BucketGuide.vue';
+
+import BucketIcon from '@/../static/images/objects/bucketIcon.svg';
 
 // @vue/component
 @Component({
-    components: { VTableCheckbox },
+    components: { 
+        VTableCheckbox,
+        BucketGuide,
+        BucketIcon,
+    },
 })
 export default class TableItem extends Vue {
     @Prop({ default: false })
@@ -35,44 +48,122 @@ export default class TableItem extends Vue {
     public readonly selected: boolean;
     @Prop({ default: false })
     public readonly selectable: boolean;
+    @Prop({ default: false })
+    public readonly showGuide: boolean;
+    @Prop({ default: 'none' })
+    private readonly tableType: string;
     @Prop({ default: () => {} })
     public readonly item: object;
     @Prop({ default: null })
     public readonly onClick: (data?: unknown) => void;
+    @Prop({ default: null })
+    public readonly hideGuide: () => void;
 
     public onChange(value: boolean): void {
         this.$emit('selectChange', value);
+    }
+
+    public showBucketGuide(index: number): boolean {
+        return (this.tableType.toLowerCase() === 'bucket') && (index === 0) && this.showGuide;
     }
 }
 </script>
 
 <style scoped lang="scss">
-    tr {
+@mixin keyframes() {
+    @keyframes pulse {
 
-        &:hover {
-            background-color: #e6e9ef;
+        0% {
+            opacity: 0.75;
+            transform: scale(1);
         }
 
-        &.selected {
-            background: #f0f3f8;
+        25% {
+            opacity: 0.75;
+            transform: scale(1);
+        }
+
+        100% {
+            opacity: 0;
+            transform: scale(2.5);
+        }
+    }
+}
+
+@include keyframes;
+
+.animation {
+    border-radius: 50%;
+    height: 8px;
+    width: 8px;
+    margin-left: 23px;
+    margin-top: 5px;
+    background-color: #0149ff;
+
+    > span {
+        animation: pulse 1s linear infinite;
+        border-radius: 50%;
+        display: block;
+        height: 8px;
+        width: 8px;
+
+        > span {
+            animation: pulse 1s linear infinite;
+            border-radius: 50%;
+            display: block;
+            height: 8px;
+            width: 8px;
         }
     }
 
-    .few-items {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
+    span {
+        background-color: rgb(1 73 255 / 2000%);
 
-    .array-val {
-        font-family: 'font_regular', sans-serif;
-        font-size: 0.75rem;
-        line-height: 1.25rem;
-
-        &:first-of-type {
-            font-family: 'font_bold', sans-serif;
-            font-size: 0.875rem;
-            margin-bottom: 3px;
+        &:after {
+            background-color: rgb(1 73 255 / 2000%);
         }
     }
+}
+
+tr {
+    cursor: pointer;
+
+    &:hover .table-item .primary {
+        color: #0149ff;
+    }
+
+    &.selected {
+        background: #f0f3f8;
+    }
+}
+
+.few-items {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.array-val {
+    font-family: 'font_regular', sans-serif;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
+
+    &:first-of-type {
+        font-family: 'font_bold', sans-serif;
+        font-size: 0.875rem;
+        margin-bottom: 3px;
+    }
+}
+
+.table-item {
+    display: flex;
+}
+
+.item-container {
+    position: relative;
+}
+
+.item-icon {
+    margin-right: 12px;
+}
 </style>
--- web/satellite/src/components/objects/BucketItem.vue
+++ web/satellite/src/components/objects/BucketItem.vue
@@ -5,6 +5,9 @@
     <table-item
         :item="itemToRender"
         :on-click="onClick"
+        :show-guide="shouldShowGuide"
+        :hide-guide="hideGuidePermanently"
+        table-type="bucket"
     >
         <th slot="options" v-click-outside="closeDropdown" class="bucket-item__functional options overflow-visible" @click.stop="openDropdown(dropdownKey)">
             <dots-icon />
@@ -16,15 +19,6 @@
                 <div class="bucket-item__functional__dropdown__item" @click.stop="onDeleteClick">
                     <delete-icon />
                     <p class="bucket-item__functional__dropdown__item__label">Delete Bucket</p>
-                </div>
-            </div>
-            <div v-if="shouldShowGuide" class="bucket-item__functional__message" @click.stop>
-                <p class="bucket-item__functional__message__title">Upload</p>
-                <p class="bucket-item__functional__message__content">To upload files, open an existing bucket or create a new one.</p>
-                <div class="bucket-item__functional__message__actions">
-                    <div class="bucket-item__functional__message__actions__primary" @click.stop="hideGuidePermanently">
-                        <p class="bucket-item__functional__message__actions__primary__label">OK</p>
-                    </div>
                 </div>
             </div>
         </th>
@@ -138,8 +132,8 @@ export default class BucketItem extends Resizable {
 
         &__functional {
             padding: 0 10px;
-            position: relative;
             cursor: pointer;
+            pointer-events: auto;
 
             &__dropdown {
                 position: absolute;
@@ -169,85 +163,6 @@ export default class BucketItem extends Resizable {
                         svg :deep(path) {
                             fill: #0068dc;
                             stroke: #0068dc;
-                        }
-                    }
-                }
-            }
-
-            &__message {
-                position: absolute;
-                top: 80%;
-                width: 25rem;
-                display: flex;
-                flex-direction: column;
-                align-items: start;
-                transform: translateX(-100%);
-                background-color: #0149ff;
-                text-align: center;
-                border-radius: 8px;
-                box-sizing: border-box;
-                padding: 20px;
-                z-index: 1001;
-
-                @media screen and (max-width: 320px) {
-                    transform: translateX(-80%);
-                }
-
-                @media screen and (max-width: 375px) and (min-width: 350px) {
-                    transform: translateX(-88%);
-                }
-
-                &:after {
-                    content: '';
-                    position: absolute;
-                    bottom: 100%;
-                    left: 10%;
-                    border-width: 5px;
-                    border-style: solid;
-                    border-color: #0149ff transparent transparent;
-                    transform: rotate(180deg);
-
-                    @media screen and (max-width: 550px) {
-                        left: 45%;
-                    }
-                }
-
-                &__title {
-                    font-weight: 400;
-                    font-size: 12px;
-                    line-height: 18px;
-                    color: white;
-                    opacity: 0.5;
-                    margin-bottom: 4px;
-                }
-
-                &__content {
-                    color: white;
-                    font-weight: 500;
-                    font-size: 15px;
-                    line-height: 24px;
-                    margin-bottom: 8px;
-                    white-space: initial;
-                    text-align: start;
-                }
-
-                &__actions {
-                    display: flex;
-                    justify-content: end;
-                    align-items: center;
-                    width: 100%;
-
-                    &__primary {
-                        padding: 6px 12px;
-                        border-radius: 8px;
-                        background-color: white;
-                        margin-left: 10px;
-
-                        &__label {
-                            font-weight: 700;
-                            font-size: 13px;
-                            line-height: 20px;
-                            color: #0149ff;
                         }
                     }
                 }
