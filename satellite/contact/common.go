97679a39ff13e64dcde7e98f693e342d7ab891bc
Márton Elek <elek@apache.org>
2022-11-03 12:42:07 +0100

    satellite/contact: emit evenkit events in case of node checkin
    Change-Id: I2da4b3055b410e476d63cc6addf982a130dba611

--- satellite/contact/common.go
+++ satellite/contact/common.go
@@ -4,11 +4,16 @@
 package contact
 
 import (
+	"github.com/jtolio/eventkit"
 	"github.com/spacemonkeygo/monkit/v3"
 	"github.com/zeebo/errs"
 )
 
-// Error is the default error class for contact package.
-var Error = errs.Class("contact")
+var (
+	// Error is the default error class for contact package.
+	Error = errs.Class("contact")
 
-var mon = monkit.Package()
+	mon = monkit.Package()
+
+	ek = eventkit.Package()
+)
