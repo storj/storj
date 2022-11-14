4a3c2cb7d0ee9bd6e6211179a51e560a0782e358
Cameron <cameron@storj.io>
2022-11-14 17:15:29 -0500

    satellite/nodeevents: implement customer.io nodeevents.Notifier
    The CustomerioNotifier sends node event data to customer.io.
    
    Change-Id: I40a942d6666ae5723c6acb84b11638497f4423f8

--- satellite/nodeevents/chore.go
+++ satellite/nodeevents/chore.go
@@ -25,6 +25,9 @@ var (
 type Config struct {
 	Interval            time.Duration `help:"how long to wait before checking the node events DB again if there is nothing to work on" default:"5m"`
 	SelectionWaitPeriod time.Duration `help:"how long the earliest instance of an event for a particular email should exist in the DB before it is selected" default:"5m"`
+	Notifier            string        `help:"which notification provider to use" default:""`
+
+	Customerio CustomerioConfig
 }
 
 // Notifier notifies node operators about node events.
