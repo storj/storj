378b8915c4ce0d33dfa7c3cb0eaada26d982b9a0
paul cannon <thepaul@storj.io>
2022-11-22 10:59:04 -0600

    satellite/{satellitedb,audit}: add NewContainment
    NewContainment will replace Containment later in this commit chain, but
    for now it is not yet being used.
    
    NewContainment will allow a node to be contained for multiple pending
    reverify jobs at a time. It is implemented by way of the reverify queue.
    
    Refs: https://github.com/storj/storj/issues/5231
    Change-Id: I126eda0b3dfc4710a88fe4a5f41780618ec19101

--- satellite/satellitedb/containment.go
+++ satellite/satellitedb/containment.go
@@ -23,6 +23,10 @@ type containment struct {
 	db *satelliteDB
 }
 
+type newContainment struct {
+	reverifyQueue audit.ReverifyQueue
+}
+
 // Get gets the pending audit by node id.
 func (containment *containment) Get(ctx context.Context, id pb.NodeID) (_ *audit.PendingAudit, err error) {
 	defer mon.Task()(&ctx)(&err)
@@ -135,4 +139,45 @@ func convertDBPending(ctx context.Context, info *dbx.SegmentPendingAudits) (_ *a
 		Position:          position,
 	}
 	return pending, nil
+}
+
+// Get gets a pending reverification audit by node id. If there are
+// multiple pending reverification audits, an arbitrary one is returned.
+// If there are none, an error wrapped by audit.ErrContainedNotFound is
+// returned.
+func (containment *newContainment) Get(ctx context.Context, id pb.NodeID) (_ *audit.ReverificationJob, err error) {
+	defer mon.Task()(&ctx)(&err)
+	if id.IsZero() {
+		return nil, audit.ContainError.New("node ID empty")
+	}
+
+	return containment.reverifyQueue.GetByNodeID(ctx, id)
+}
+
+// Insert creates a new pending audit entry.
+func (containment *newContainment) Insert(ctx context.Context, pendingJob *audit.PieceLocator) (err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	return containment.reverifyQueue.Insert(ctx, pendingJob)
+}
+
+// Delete removes a job from the reverification queue, whether because the job
+// was successful or because the job is no longer necessary. The wasDeleted
+// return value indicates whether the indicated job was actually deleted (if
+// not, there was no such job in the queue).
+func (containment *newContainment) Delete(ctx context.Context, pendingJob *audit.PieceLocator) (isDeleted, nodeStillContained bool, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	isDeleted, err = containment.reverifyQueue.Remove(ctx, pendingJob)
+	if err != nil {
+		return false, false, audit.ContainError.Wrap(err)
+	}
+
+	nodeStillContained = true
+	_, err = containment.reverifyQueue.GetByNodeID(ctx, pendingJob.NodeID)
+	if audit.ErrContainedNotFound.Has(err) {
+		nodeStillContained = false
+		err = nil
+	}
+	return isDeleted, nodeStillContained, audit.ContainError.Wrap(err)
 }
