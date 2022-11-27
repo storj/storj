25320eae34863c281e976d7678f1308a8bc008d7
Erik van Velzen <erik@evanv.nl>
2022-11-27 01:28:20 +0100

    satellite/metabase/rangedloop: measure observer duration
    Track duration of all segment loop observers. Factor out functions to
    reduce size.
    
    Still need to send the measurements out via monkit.
    
    Part of https://github.com/storj/storj/issues/5223
    
    Change-Id: Iae0260e250f8ea33affed95c6592a1f42df384eb

--- satellite/metabase/rangedloop/service_test.go
+++ satellite/metabase/rangedloop/service_test.go
@@ -4,26 +4,28 @@
 package rangedloop_test
 
 import (
-	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 
+	"storj.io/common/testcontext"
+	"storj.io/common/uuid"
 	"storj.io/storj/satellite/metabase/rangedloop"
 	"storj.io/storj/satellite/metabase/rangedloop/rangedlooptest"
 	"storj.io/storj/satellite/metabase/segmentloop"
 )
 
-func TestLoop(t *testing.T) {
+func TestLoopCount(t *testing.T) {
 	for _, parallelism := range []int{1, 2, 3} {
 		for _, nSegments := range []int{0, 1, 2, 11} {
 			for _, nObservers := range []int{0, 1, 2} {
 				t.Run(
 					fmt.Sprintf("par%d_seg%d_obs%d", parallelism, nSegments, nObservers),
 					func(t *testing.T) {
-						RunTest(t, parallelism, nSegments, nObservers)
+						runCountTest(t, parallelism, nSegments, nObservers)
 					},
 				)
 			}
@@ -31,9 +33,9 @@ func TestLoop(t *testing.T) {
 	}
 }
 
-func RunTest(t *testing.T, parallelism int, nSegments int, nObservers int) {
+func runCountTest(t *testing.T, parallelism int, nSegments int, nObservers int) {
 	batchSize := 2
-	ctx := context.Background()
+	ctx := testcontext.New(t)
 
 	observers := []rangedloop.Observer{}
 	for i := 0; i < nObservers; i++ {
@@ -43,9 +45,8 @@ func RunTest(t *testing.T, parallelism int, nSegments int, nObservers int) {
 	loopService := rangedloop.NewService(
 		zaptest.NewLogger(t),
 		rangedloop.Config{
-			BatchSize:          batchSize,
-			Parallelism:        parallelism,
-			AsOfSystemInterval: 0,
+			BatchSize:   batchSize,
+			Parallelism: parallelism,
 		},
 		&rangedlooptest.RangeSplitter{
 			Segments: make([]segmentloop.Segment, nSegments),
@@ -53,11 +54,66 @@ func RunTest(t *testing.T, parallelism int, nSegments int, nObservers int) {
 		observers,
 	)
 
-	err := loopService.RunOnce(ctx)
+	observerDurations, err := loopService.RunOnce(ctx)
 	require.NoError(t, err)
+	require.Len(t, observerDurations, nObservers)
 
 	for _, observer := range observers {
 		countObserver := observer.(*rangedlooptest.CountObserver)
 		require.Equal(t, nSegments, countObserver.NumSegments)
+	}
+}
+
+func TestLoopDuration(t *testing.T) {
+	t.Skip("Flaky test because it validates concurrency by measuring time")
+
+	nSegments := 8
+	nObservers := 2
+	parallelism := 4
+	batchSize := 2
+	sleepIncrement := time.Millisecond * 10
+
+	ctx := testcontext.New(t)
+
+	observers := []rangedloop.Observer{}
+	for i := 0; i < nObservers; i++ {
+		observers = append(observers, &rangedlooptest.SleepObserver{
+			Duration: sleepIncrement,
+		})
+	}
+
+	segments := []segmentloop.Segment{}
+	for i := 0; i < nSegments; i++ {
+		streamId, err := uuid.FromBytes([]byte{byte(i), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+		require.NoError(t, err)
+		segments = append(segments, segmentloop.Segment{
+			StreamID: streamId,
+		})
+	}
+
+	loopService := rangedloop.NewService(
+		zaptest.NewLogger(t),
+		rangedloop.Config{
+			BatchSize:   batchSize,
+			Parallelism: parallelism,
+		},
+		&rangedlooptest.RangeSplitter{
+			Segments: segments,
+		},
+		observers,
+	)
+
+	start := time.Now()
+	observerDurations, err := loopService.RunOnce(ctx)
+	require.NoError(t, err)
+
+	duration := time.Since(start)
+	expectedDuration := time.Duration(int64(nSegments) * int64(sleepIncrement) * int64(nObservers) / int64(parallelism))
+	require.Equal(t, expectedDuration, duration.Truncate(sleepIncrement))
+
+	require.Len(t, observerDurations, nObservers)
+	for _, observerDuration := range observerDurations {
+		expectedSleep := time.Duration(int64(nSegments) * int64(sleepIncrement))
+		require.Equal(t, expectedSleep, observerDuration.Duration.Round(sleepIncrement))
 	}
 }
