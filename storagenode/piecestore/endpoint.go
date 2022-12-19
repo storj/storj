1e3f6e524196497d6da950e021f0dc2cc68dc8b6
Michal Niewrzal <michal@storj.io>
2022-12-19 08:53:10 +0100

    storagenode/piecestore: small cleanup
    * normalize ExistsCheckWorkers flag to avoid setting incorrect values
    * wait for limiter to finish if context was canceled
    
    Change-Id: I688a395bd958cd09233605fc264d43f91ec45ed1

--- storagenode/piecestore/endpoint.go
+++ storagenode/piecestore/endpoint.go
@@ -211,6 +211,10 @@ func (endpoint *Endpoint) Exists(
 		return &pb.ExistsResponse{}, nil
 	}
 
+	if endpoint.config.ExistsCheckWorkers < 1 {
+		endpoint.config.ExistsCheckWorkers = 1
+	}
+
 	limiter := sync2.NewLimiter(endpoint.config.ExistsCheckWorkers)
 	var mu sync.Mutex
 
@@ -238,7 +242,8 @@ func (endpoint *Endpoint) Exists(
 			}
 		})
 		if !ok {
-			return &pb.ExistsResponse{}, rpcstatus.Wrap(rpcstatus.Internal, ctx.Err())
+			limiter.Wait()
+			return nil, rpcstatus.Wrap(rpcstatus.Canceled, ctx.Err())
 		}
 	}
 
@@ -246,7 +251,7 @@ func (endpoint *Endpoint) Exists(
 
 	return &pb.ExistsResponse{
 		Missing: missing,
-	}, rpcstatus.Wrap(rpcstatus.Internal, err)
+	}, nil
 }
 
 // Upload handles uploading a piece on piece store.
