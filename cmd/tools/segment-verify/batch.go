0b790070a32f181c7d8d3d094045e92d2c10cd80
paul cannon <thepaul@storj.io>
2022-12-22 12:09:20 -0600

    cmd/tools/segment-verify: pass over bad segments
    Change-Id: I1b4dd9da755c6a2028760723e15219f5821f702f

--- cmd/tools/segment-verify/batch.go
+++ cmd/tools/segment-verify/batch.go
@@ -7,6 +7,8 @@ import (
 	"context"
 	"sort"
 
+	"go.uber.org/zap"
+
 	"storj.io/storj/satellite/metabase"
 )
 
@@ -38,7 +40,17 @@ func (service *Service) CreateBatches(ctx context.Context, segments []*Segment) 
 	// We assume that segment.AliasPieces is randomly ordered in terms of nodes.
 	for _, segment := range segments {
 		if len(segment.AliasPieces) < int(segment.Status.Retry) {
-			panic("segment contains too few pieces")
+			if service.config.Check == 0 {
+				// some pieces were removed in selectOnlinePieces. adjust the expected count.
+				segment.Status.Retry = int32(len(segment.AliasPieces))
+			} else {
+				service.log.Error("segment contains too few pieces. skipping segment",
+					zap.Int("num-pieces", len(segment.AliasPieces)),
+					zap.Int32("expected", segment.Status.Retry),
+					zap.Stringer("stream-id", segment.StreamID),
+					zap.Uint64("position", segment.Position.Encode()))
+				continue
+			}
 		}
 		for _, piece := range segment.AliasPieces[:segment.Status.Retry] {
 			enqueue(piece.Alias, segment)
