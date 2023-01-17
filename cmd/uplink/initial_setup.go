eab595397fd5a51038ebba857b92408f290d9ed2
Erik van Velzen <erik@evanv.nl>
2023-01-17 03:16:10 +0100

    cmd/uplink: add --analytics flag
    Add a flag to enable/disable analytics so uplink can be run
    non-interactively. Also when run non-interactively for the first time
    it will not error any more but instead default to disable analytics.
    
    Part of https://github.com/storj/storj/issues/5126
    
    Change-Id: I07ac8a040664334efcb4e2536f26c330c1751a6f

--- cmd/uplink/initial_setup.go
+++ cmd/uplink/initial_setup.go
@@ -12,23 +12,34 @@ import (
 	"storj.io/storj/cmd/uplink/ulext"
 )
 
-func saveInitialConfig(ctx context.Context, ex ulext.External) error {
-	answer, err := ex.PromptInput(ctx, `With your permission, Storj can `+
-		`automatically collect analytics information from your uplink CLI to `+
-		`help improve the quality and performance of our products. This `+
-		`information is sent only with your consent and is submitted `+
-		`anonymously to Storj Labs: (y/n)`)
-	if err != nil {
-		return errs.Wrap(err)
+func saveInitialConfig(ctx context.Context, ex ulext.External, interactiveFlag bool, analyticsFlag *bool) error {
+	var analyticsEnabled bool
+	if analyticsFlag != nil {
+		analyticsEnabled = *analyticsFlag
+	} else {
+		if interactiveFlag {
+			answer, err := ex.PromptInput(ctx, `With your permission, Storj can `+
+				`automatically collect analytics information from your uplink CLI to `+
+				`help improve the quality and performance of our products. This `+
+				`information is sent only with your consent and is submitted `+
+				`anonymously to Storj Labs: (y/n)`)
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			answer = strings.ToLower(answer)
+			analyticsEnabled = answer != "y" && answer != "yes"
+		} else {
+			analyticsEnabled = false
+		}
 	}
-	answer = strings.ToLower(answer)
 
 	values := make(map[string]string)
-	if answer != "y" && answer != "yes" {
+
+	if analyticsEnabled {
+		values["analytics.enabled"] = "true"
+	} else {
 		values["metrics.addr"] = ""
 		values["analytics.enabled"] = "false"
-	} else {
-		values["analytics.enabled"] = "true"
 	}
 
 	return ex.SaveConfig(values)
