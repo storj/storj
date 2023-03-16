41bfbbe772219259c90491fb818927eb39cd78fd
Wilfred Asomani <devwilfredagyei@gmail.com>
2023-03-16 11:42:01 +0000

    satellite/console: add endpoint to get user settings
    This change adds an endpoint that gets a user's settings. It will
    create a new settings entry if no settings exists. There's also a new
    endpoint to change a user's onboarding status.
    
    Issue: https://github.com/storj/storj/issues/5661
    
    Change-Id: I9941bb9d61994af46244003f3ef4fcfe7d36918e

--- satellite/console/service.go
+++ satellite/console/service.go
@@ -3239,3 +3239,47 @@ func (s *Service) VerifyForgotPasswordCaptcha(ctx context.Context, responseToken
 	}
 	return true, nil
 }
+
+// GetUserSettings fetches a user's settings. It creates default settings if none exists.
+func (s *Service) GetUserSettings(ctx context.Context) (settings *UserSettings, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	user, err := s.getUserAndAuditLog(ctx, "get user settings")
+	if err != nil {
+		return nil, Error.Wrap(err)
+	}
+
+	settings, err = s.store.Users().GetSettings(ctx, user.ID)
+	if err != nil {
+		if !errs.Is(err, sql.ErrNoRows) {
+			return nil, Error.Wrap(err)
+		}
+		err = s.store.Users().UpsertSettings(ctx, user.ID, UpsertUserSettingsRequest{})
+		if err != nil {
+			return nil, Error.Wrap(err)
+		}
+		settings, err = s.store.Users().GetSettings(ctx, user.ID)
+		if err != nil {
+			return nil, Error.Wrap(err)
+		}
+	}
+
+	return settings, nil
+}
+
+// SetUserSettings updates a user's settings.
+func (s *Service) SetUserSettings(ctx context.Context, request UpsertUserSettingsRequest) (err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	user, err := s.getUserAndAuditLog(ctx, "get user settings")
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+	err = s.store.Users().UpsertSettings(ctx, user.ID, request)
+	if err != nil {
+		return Error.Wrap(err)
+	}
+
+	return nil
+}
