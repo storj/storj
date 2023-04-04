ecf3b89292c96c87303721ea31939d50c551b9c3
Vitalii <takur12345@gmail.com>
2023-04-04 16:16:52 +0300

    web/satellite: migrate EditProjectDetails component to use SFC composition api
    Change-Id: If799efb9cbbdbfe5f269df0c555e47b3d767052a

--- web/satellite/tests/unit/ignore/project/CreateProject.spec.ts
+++ web/satellite/tests/unit/ignore/project/CreateProject.spec.ts
@@ -4,8 +4,7 @@
 import Vuex from 'vuex';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 
-import { ProjectsApiMock } from '../mock/api/projects';
-
+import { ProjectsApiMock } from '@/../tests/unit/mock/api/projects';
 import { makeProjectsModule } from '@/store/modules/projects';
 import { NotificatorPlugin } from '@/utils/plugins/notificator';
 
