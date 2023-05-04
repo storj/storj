e3573f44eaf35526fe95ebd90daec054ab4b91f4
Wilfred Asomani <devwilfredagyei@gmail.com>
2023-05-04 13:33:40 +0000

    web/satellite: update project member invite modal
    The project member invite modal has been updated to match the new figma
    design.
    
    Issue: https://github.com/storj/storj/issues/5744
    
    Change-Id: I9ed7ad4ede0fb6f7f42f56b2d8386e12a5a5b6c1

--- web/satellite/src/components/modals/AddTeamMemberModal.vue
+++ web/satellite/src/components/modals/AddTeamMemberModal.vue
@@ -4,75 +4,58 @@
 <template>
     <VModal :on-close="closeModal">
         <template #content>
-            <div class="add-user">
-                <div class="add-user__main">
-                    <div class="add-user__info-panel-container">
-                        <h2 class="add-user__info-panel-container__title">Add Team Member</h2>
-                        <img src="@/../static/images/team/addMember.jpg" alt="add team member image">
-                    </div>
-                    <div class="add-user__form-container">
-                        <h2 class="add-user__form-container__title">Add Team Member</h2>
-                        <p v-if="!formError" class="add-user__form-container__common-label">Email Address</p>
-                        <div v-if="formError" class="add-user__form-container__label">
-                            <ErrorIcon alt="Red error icon" />
-                            <p class="add-user__form-container__label__error">{{ formError }}</p>
-                        </div>
-                        <div class="add-user__form-container__inputs-group">
-                            <div
-                                v-for="(input, index) in inputs"
-                                :key="index"
-                                class="add-user__form-container__inputs-group__item"
-                            >
-                                <input
-                                    v-model="input.value"
-                                    placeholder="email@example.com"
-                                    class="no-error-input"
-                                    :class="{ 'error-input': input.error }"
-                                    @keyup="resetFormErrors(index)"
-                                >
-                                <DeleteFieldIcon
-                                    class="add-user__form-container__inputs-group__item__image"
-                                    @click="deleteInput(index)"
-                                />
-                            </div>
-                        </div>
-                        <div class="add-user-row">
-                            <div id="add-user-button" class="add-user-row__item" @click="addInput">
-                                <div :class="{ 'inactive-image': isMaxInputsCount }">
-                                    <AddFieldIcon class="add-user-row__item__image" />
-                                </div>
-                                <p class="add-user-row__item__label" :class="{ 'inactive-label': isMaxInputsCount }">Add More</p>
-                            </div>
-                        </div>
-                        <div class="add-user__form-container__button-container">
-                            <VButton
-                                label="Cancel"
-                                width="100%"
-                                height="48px"
-                                :on-press="closeModal"
-                                :is-transparent="true"
-                            />
-                            <VButton
-                                label="Add Team Members"
-                                width="100%"
-                                height="48px"
-                                :on-press="onAddUsersClick"
-                                :is-disabled="!isButtonActive"
-                            />
-                        </div>
+            <div class="modal">
+                <div class="modal__header">
+                    <TeamMembersIcon />
+                    <h1 class="modal__header__title">Invite team members</h1>
+                </div>
+
+                <p class="modal__info">
+                    Add team members to contribute to this project.
+                </p>
+
+                <div class="modal__input-group">
+                    <VInput
+                        v-for="(_, index) in inputs"
+                        :key="index"
+                        class="modal__input-group__item"
+                        label="Email"
+                        height="38px"
+                        placeholder="email@email.com"
+                        role-description="email"
+                        :error="formError"
+                        @setData="(str) => setInput(index, str)"
+                    />
+                </div>
+
+                <div class="modal__more">
+                    <div
+                        tabindex="0"
+                        class="modal__more__button"
+                        @click.stop="addInput"
+                    >
+                        <AddCircleIcon class="modal__more__button__icon" :class="{ inactive: isMaxInputsCount }" />
+                        <span class="modal__more__button__label" :class="{ inactive: isMaxInputsCount }">Add more</span>
                     </div>
                 </div>
-                <div class="notification-wrap">
-                    <AddMemberNotificationIcon class="notification-wrap__image" />
-                    <div class="notification-wrap__text-area">
-                        <p class="notification-wrap__text-area__text">
-                            If the team member you want to invite to join the project is still not on this Satellite, please
-                            share this link to the signup page and ask them to register here:
-                            <router-link target="_blank" rel="noopener noreferrer" exact to="/signup">
-                                {{ registerPath }}
-                            </router-link>
-                        </p>
-                    </div>
+
+                <div class="modal__buttons">
+                    <VButton
+                        label="Cancel"
+                        height="48px"
+                        font-size="14px"
+                        border-radius="10px"
+                        :is-transparent="true"
+                        :on-press="closeModal"
+                    />
+                    <VButton
+                        label="Invite"
+                        height="48px"
+                        font-size="14px"
+                        border-radius="10px"
+                        :on-press="onAddUsersClick"
+                        :is-disabled="!isButtonActive"
+                    />
                 </div>
             </div>
         </template>
@@ -96,11 +79,10 @@ import { useProjectsStore } from '@/store/modules/projectsStore';
 
 import VButton from '@/components/common/VButton.vue';
 import VModal from '@/components/common/VModal.vue';
+import VInput from '@/components/common/VInput.vue';
 
-import ErrorIcon from '@/../static/images/register/ErrorInfo.svg';
-import AddFieldIcon from '@/../static/images/team/addField.svg';
-import AddMemberNotificationIcon from '@/../static/images/team/addMemberNotification.svg';
-import DeleteFieldIcon from '@/../static/images/team/deleteField.svg';
+import TeamMembersIcon from '@/../static/images/team/teamMembers.svg';
+import AddCircleIcon from '@/../static/images/common/addCircle.svg';
 
 const appStore = useAppStore();
 const pmStore = useProjectMembersStore();
@@ -111,7 +93,7 @@ const notify = useNotify();
 const FIRST_PAGE = 1;
 const analytics: AnalyticsHttpApi = new AnalyticsHttpApi();
 
-const inputs = ref<EmailInput[]>([new EmailInput(), new EmailInput(), new EmailInput()]);
+const inputs = ref<EmailInput[]>([new EmailInput()]);
 const formError = ref<string>('');
 const isLoading = ref<boolean>(false);
 
@@ -151,6 +133,11 @@ const registerPath = computed((): string => {
     return location.host + RouteConfig.Register.path;
 });
 
+function setInput(index: number, str: string) {
+    resetFormErrors(index);
+    inputs.value[index].value = str;
+}
+
 /**
  * Tries to add users related to entered emails list to current project.
  */
@@ -183,7 +170,7 @@ async function onAddUsersClick(): Promise<void> {
         newInputsArray.unshift(element);
         areAllEmailsValid = false;
 
-        formError.value = 'Field is required. Please enter a valid email address';
+        formError.value = 'Please enter a valid email address';
     }
 
     inputs.value = [...newInputsArray];
@@ -220,7 +207,7 @@ async function onAddUsersClick(): Promise<void> {
     }
 
     analytics.eventTriggered(AnalyticsEvent.PROJECT_MEMBERS_INVITE_SENT);
-    await notify.notify(`The user(s) you've invited to your project will receive your invitation if they have an account on this satellite.`);
+    await notify.notify('Invites sent!');
     pmStore.setSearchQuery('');
 
     try {
@@ -275,297 +262,127 @@ function resetFormErrors(index: number): void {
 </script>
 
 <style scoped lang='scss'>
-    .add-user {
-        width: 100%;
-        max-width: 1200px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        font-family: 'font_regular', sans-serif;
+    .modal {
+        width: 346px;
+        padding: 32px;
 
-        &__main {
-            border-radius: 6px 6px 0 0;
-            display: flex;
-            align-items: flex-start;
-            justify-content: center;
-            background-color: #fff;
-            padding: 80px 24px;
-            width: calc(100% - 48px);
-
-            @media screen and (max-width: 950px) {
-                padding: 48px 24px;
-            }
+        @media screen and (max-width: 460px) {
+            width: 280px;
+            padding: 16px;
         }
 
-        &__info-panel-container {
+        &__header {
             display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
             align-items: center;
-            margin-right: 150px;
+            padding-bottom: 16px;
+            margin-bottom: 16px;
+            border-bottom: 1px solid var(--c-grey-2);
 
-            @media screen and (max-width: 950px) {
-                display: none;
+            @media screen and (max-width: 460px) {
+                flex-direction: column;
+                align-items: flex-start;
             }
 
             &__title {
+                margin-left: 16px;
                 font-family: 'font_bold', sans-serif;
-                font-size: 32px;
-                line-height: 29px;
-                color: #384b65;
-                margin: 0 0 90px;
-                width: 130%;
-                text-align: end;
-            }
-        }
-
-        &__attention-text {
-            font-size: 10px !important;
-            line-height: 15px !important;
-        }
-
-        &__form-container {
-            width: 100%;
-            max-width: 600px;
-
-            @media screen and (max-width: 950px) {
-                max-width: unset;
-            }
-
-            &__title {
-                display: none;
-                font-family: 'font_bold', sans-serif;
-                font-size: 28px;
-                color: #384b65;
-                margin-bottom: 20px;
+                font-size: 24px;
+                line-height: 31px;
+                letter-spacing: -0.02em;
+                color: var(--c-black);
                 text-align: left;
 
-                @media screen and (max-width: 950px) {
-                    display: block;
-                }
-            }
-
-            &__label {
-                display: flex;
-                padding-left: 50px;
-                margin-bottom: 15px;
-
-                @media screen and (max-width: 950px) {
-                    padding: 0;
-                }
-
-                @media screen and (max-width: 550px) {
-
-                    svg {
-                        display: none;
-                    }
-                }
-
-                &__error {
-                    margin-left: 10px;
-                    color: #eb5757;
-                    text-align: left;
-
-                    @media screen and (max-width: 550px) {
-                        margin: 0;
-                    }
-                }
-            }
-
-            &__inputs-group {
-                padding: 3px 50px 0;
-
-                @media screen and (max-width: 950px) {
-                    padding: 0;
-                }
-
-                &__item {
-                    display: flex;
-                    align-items: center;
-
-                    .no-error-input {
-                        font-size: 16px;
-                        line-height: 21px;
-                        resize: none;
-                        margin-bottom: 18px;
-                        height: 48px;
-                        width: 100%;
-                        text-indent: 20px;
-                        border-color: rgb(56 75 101 / 40%);
-                        border-radius: 6px;
-
-                        &:last-child {
-                            margin-bottom: 0;
-                        }
-                    }
-
-                    &__image {
-                        margin-bottom: 18px;
-                        margin-left: 20px;
-                        cursor: pointer;
-
-                        &:hover .delete-input-svg-path {
-                            fill: #2683ff;
-                        }
-                    }
-                }
-            }
-
-            .full-input {
-                margin-bottom: 18px;
-
-                &:last-child {
-                    margin-bottom: 0;
-                }
-            }
-
-            &__common-label {
-                margin: 0 0 10px;
-                font-family: 'font_medium', sans-serif;
-                font-size: 16px;
-                line-height: 25px;
-                padding-left: 50px;
-                text-align: left;
-
-                @media screen and (max-width: 950px) {
-                    padding-left: unset;
-                }
-            }
-
-            &__button-container {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                margin-top: 30px;
-                padding: 0 80px 0 50px;
-                column-gap: 20px;
-
-                @media screen and (max-width: 950px) {
-                    padding: 0;
-                }
-
-                @media screen and (max-width: 420px) {
-                    flex-direction: column-reverse;
-                    row-gap: 10px;
-                    column-gap: unset;
+                @media screen and (max-width: 460px) {
+                    margin: 10px 0 0;
                 }
             }
         }
-    }
 
-    .add-user-row {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 80px 0 50px;
-
-        @media screen and (max-width: 950px) {
-            padding: 0;
+        &__info {
+            font-family: 'font_regular', sans-serif;
+            font-size: 14px;
+            line-height: 19px;
+            color: var(--c-black);
+            border-bottom: 1px solid var(--c-grey-2);
+            text-align: left;
+            padding-bottom: 16px;
+            margin-bottom: 16px;
         }
 
-        &__item {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
+        &__input-group {
 
-            &__image {
-                margin-right: 20px;
+            &__item {
+                border-bottom: 1px solid var(--c-grey-2);
+                padding-bottom: 16px;
+                margin-bottom: 16px;
             }
+        }
 
-            &__label {
-                font-family: 'font_medium', sans-serif;
-                font-size: 16px;
-                margin-left: 0;
-                padding-left: 0;
-                margin-block-start: 0;
-                margin-block-end: 0;
-            }
+        &__more {
+            border-bottom: 1px solid var(--c-grey-2);
+            padding-bottom: 16px;
+            margin-bottom: 16px;
 
-            &:first-child {
+            &__button {
+                width: fit-content;
+                display: flex;
+                column-gap: 5px;
+                align-items: flex-end;
                 cursor: pointer;
+
+                &__icon {
+                    width: 18px;
+                    height: 18px;
+
+                    &.inactive {
+
+                        :deep(path) {
+                            fill: var(--c-grey-5);
+                        }
+                    }
+
+                    :deep(path) {
+                        fill: var(--c-blue-3);
+                    }
+                }
+
+                &__label {
+                    font-family: 'font_regular', sans-serif;
+                    font-size: 16px;
+                    text-decoration: underline;
+                    text-align: center;
+                    color: var(--c-blue-3);
+
+                    &.inactive {
+                        color: var(--c-grey-5);
+                    }
+                }
             }
         }
-    }
 
-    .inactive-label {
-        cursor: default;
-        color: #dadde5;
-    }
-
-    .error-input {
-        border: 1px solid red !important;
-    }
-
-    .inactive-image {
-        cursor: default;
-
-        .add-user-row__item__image {
-
-            &__rect {
-                fill: #dadde5;
-            }
-
-            &__path {
-                fill: #acb0bc;
-            }
-        }
-    }
-
-    .input-container.full-input {
-        width: 100%;
-    }
-
-    .red {
-        background-color: #eb5757;
-    }
-
-    .notification-wrap {
-        background-color: rgb(194 214 241 / 100%);
-        height: 98px;
-        display: flex;
-        justify-content: flex-start;
-        padding: 0 50px;
-        align-items: center;
-        border-radius: 0 0 6px 6px;
-
-        @media screen and (max-width: 950px) {
-            height: unset;
-            padding: 24px;
-        }
-
-        &__image {
-            margin-right: 40px;
-            min-width: 40px;
+        &__buttons {
+            display: flex;
+            column-gap: 10px;
+            margin-top: 10px;
+            width: 100%;
 
             @media screen and (max-width: 500px) {
-                display: none;
-            }
-        }
-
-        &__text-area {
-            display: flex;
-            align-items: center;
-
-            &__text {
-                font-family: 'font_medium', sans-serif;
-                font-size: 16px;
-                text-align: left;
-
-                @media screen and (max-width: 500px) {
-                    font-size: 14px;
-                }
+                flex-direction: column-reverse;
+                column-gap: unset;
+                row-gap: 10px;
             }
         }
     }
 
-    :deep(.container) {
-        padding: 0 10px;
+    :deep(.label-container__main__label) {
+        font-size: 14px;
     }
 
-    @media screen and (max-width: 500px) {
+    :deep(.label-container__main__error) {
+        font-size: 14px;
+    }
 
-        :deep(.container) {
-            padding: 0;
-        }
+    :deep(.input-container) {
+        margin-top: 0;
     }
 </style>
