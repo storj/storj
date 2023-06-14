b66fc6dcdbb73efff7fea7eb7ff342b8cfc431c9
Moby von Briesen <mobyvb@gmail.com>
2023-06-14 12:08:54 -0400

    web/satellite: Update build script to build vuetify app
    Also copy the necessary vuetify dist directory in the `Earthfile` so
    that this app can be easily deployed to staging environments.
    
    Change-Id: I8d91c52bb8f7c31fc3764efe60a071e21b399b46

--- web/satellite/build.sh
+++ web/satellite/build.sh
@@ -8,6 +8,7 @@ set -euxo pipefail
 npm install --prefer-offline --no-audit --logleve verbose
 echo "module stub" > ./node_modules/go.mod # prevent Go from scanning this dir
 npm run build
+npm run build-vuetify
 
 npm run lint-ci
 npm audit || true
