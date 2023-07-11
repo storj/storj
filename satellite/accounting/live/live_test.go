73a279235a394a91e2069c51f2a27f5514b4a372
Michal Niewrzal <michal@storj.io>
2023-07-11 14:06:36 +0200

    satellite/accounting/live: get project totals in batches
    After infrastructure changes redis instance is not neccessay close
    to core instance (where tally is calculated) and round trips to get
    data from redis can be very costly. From less than hour calculation can take few hours for larger satellite.
    
    This change combines 'segment' and 'storage' usage requests into
    batches to reduce latency impact on tally calculation.
    
    https://github.com/storj/storj/issues/5800
    
    Change-Id: I87e57ec09e88fd167060a4ed51dc8b0274a095c5

--- satellite/accounting/live/live_test.go
+++ satellite/accounting/live/live_test.go
@@ -6,6 +6,7 @@ package live_test
 import (
 	"context"
 	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 
@@ -136,19 +137,28 @@ func TestGetAllProjectTotals(t *testing.T) {
 				require.NoError(t, err)
 			}
 
-			usage, err := cache.GetAllProjectTotals(ctx)
-			require.NoError(t, err)
-			require.Len(t, usage, len(projectIDs))
+			for _, batchSize := range []int{1, 2, 3, 10, 13, 10000} {
+				t.Run("batch-size-"+strconv.Itoa(batchSize), func(t *testing.T) {
+					config.BatchSize = batchSize
+					testCache, err := live.OpenCache(ctx, zaptest.NewLogger(t).Named("live-accounting"), config)
+					require.NoError(t, err)
+					defer ctx.Check(testCache.Close)
 
-			// make sure each project ID and total was received
-			for _, projID := range projectIDs {
-				totalStorage, err := cache.GetProjectStorageUsage(ctx, projID)
-				require.NoError(t, err)
-				assert.Equal(t, totalStorage, usage[projID].Storage)
+					usage, err := testCache.GetAllProjectTotals(ctx)
+					require.NoError(t, err)
+					require.Len(t, usage, len(projectIDs))
 
-				totalSegments, err := cache.GetProjectSegmentUsage(ctx, projID)
-				require.NoError(t, err)
-				assert.Equal(t, totalSegments, usage[projID].Segments)
+					// make sure each project ID and total was received
+					for _, projID := range projectIDs {
+						totalStorage, err := testCache.GetProjectStorageUsage(ctx, projID)
+						require.NoError(t, err)
+						assert.Equal(t, totalStorage, usage[projID].Storage)
+
+						totalSegments, err := testCache.GetProjectSegmentUsage(ctx, projID)
+						require.NoError(t, err)
+						assert.Equal(t, totalSegments, usage[projID].Segments)
+					}
+				})
 			}
 		})
 	}
