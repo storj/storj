465e5cc6b3697f3359a5604867a0d533aa70f523
Antonio Franco <antonio@storj.io>
2023-07-17 13:23:14 -0400

    testsuite/playwright-ui: port configuration update

--- testsuite/playwright-ui/playwright.config.ts
+++ testsuite/playwright-ui/playwright.config.ts
@@ -65,7 +65,7 @@ const config: PlaywrightTestConfig = {
     use: {                                              /* Shared settings for all the projects below. */
         actionTimeout: 0,                                 /* Maximum time each action can take. */
         // baseURL: 'http://nightly.storj.rodeo/',     /* Base URL to use in actions like `await page.goto('/')`. */
-         baseURL: 'http://127.0.0.1:10001',
+         baseURL: 'http://127.0.0.1:10000',
         // headless: process.env.CI ? false : true,       /* Starts the UI tests in headed mode, so we can watch execution in development */
         ignoreHTTPSErrors: true,                          /* suppress the errors relative to serving web data   */
         trace: 'on-first-retry',                          /* Collect trace when retrying the failed test. */
