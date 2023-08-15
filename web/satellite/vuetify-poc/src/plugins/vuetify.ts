storj/storj

commit 37a027af676588a43df72aebb3fd2998fba5f0f7

web/satellite/vuetify-poc: hide input message areas with no content
The message area beneath input components has been hidden by default so
that it doesn't occupy space when it isn't needed.

References #6170

Change-Id: I16b078cddb07708169b96e53db283aedb290f9f4

web/satellite/vuetify-poc/src/plugins/vuetify.ts
@@ -77,6 +77,7 @@ export default createVuetify({
     defaults: {
         global: {
             // ripple: false,
+            hideDetails: true,
         },
         VDataTable: {
             fixedHeader: true,

commit baef654197f408a48eada11fe70fc9c1da94eeba

web/satellite/vuetify-poc: fix visibility of input details
This change resolves an issue that 37a027a introduced wherein each
input component's details area was indiscriminately hidden. Now,
details are only hidden when there is no content to be displayed.

References #6170

Change-Id: I8ccbef3f64a54c400a2a2e9222618eb5015c8dd8

web/satellite/vuetify-poc/src/plugins/vuetify.ts
@@ -74,7 +74,7 @@ export default createVuetify({
     defaults: {
         global: {
             // ripple: false,
-            hideDetails: true,
+            hideDetails: 'auto',
         },
         VDataTable: {
             fixedHeader: true,