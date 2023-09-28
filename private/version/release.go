2ea2f11fa71a06f1a51d1f786b6963125853a4fe
Moby von Briesen <mobyvb@gmail.com>
2023-09-28 12:13:07 -0400

    release v1.89.1-rc-ui
    Change-Id: I5ba410ff8279d91ed4ed853a80662358488f49d2

--- private/version/release.go
+++ private/version/release.go
@@ -6,16 +6,16 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/private/version.buildTimestamp
-var buildTimestamp string
+var buildTimestamp string = "1695917587"
 
 //go:linkname buildCommitHash storj.io/private/version.buildCommitHash
-var buildCommitHash string
+var buildCommitHash string = "ece771f8af04c6d39786397db81ae621ff316ee3"
 
 //go:linkname buildVersion storj.io/private/version.buildVersion
-var buildVersion string
+var buildVersion string = "v1.89.1-rc-ui"
 
 //go:linkname buildRelease storj.io/private/version.buildRelease
-var buildRelease string
+var buildRelease string = "true"
 
 // ensure that linter understands that the variables are being used.
 func init() { use(buildTimestamp, buildCommitHash, buildVersion, buildRelease) }
