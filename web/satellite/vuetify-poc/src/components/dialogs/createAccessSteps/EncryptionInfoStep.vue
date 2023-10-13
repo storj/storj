902cf5898f810896c2288a15e8eafe1938fb8fc2
Vitalii <takur12345@gmail.com>
2023-10-13 15:55:24 +0300

    web/satellite/vuetify-poc: remove required rule on S3 credentials info step
    Remove required rule on S3 credentials info step when creating new one.
    
    Issue:
    https://github.com/storj/storj/issues/6400
    
    Change-Id: Ie59267bb881e6dfa769ae0cdda83ff369ec920cd

--- web/satellite/vuetify-poc/src/components/dialogs/createAccessSteps/EncryptionInfoStep.vue
+++ web/satellite/vuetify-poc/src/components/dialogs/createAccessSteps/EncryptionInfoStep.vue
@@ -2,41 +2,31 @@
 // See LICENSE for copying information.
 
 <template>
-    <v-form ref="form" class="pa-8">
-        <v-row>
-            <v-col cols="12">
-                By generating S3 credentials, you are opting in to
-                <a class="link" href="https://docs.storj.io/dcs/concepts/encryption-key/design-decision-server-side-encryption/">
-                    server-side encryption.
-                </a>
-            </v-col>
-            <v-col cols="12">
-                <v-checkbox
-                    density="compact"
-                    label="I understand, don't show this again."
-                    :hide-details="false"
-                    :rules="[ RequiredRule ]"
-                    @update:model-value="value => LocalData.setServerSideEncryptionModalHidden(value)"
-                />
-            </v-col>
-        </v-row>
-    </v-form>
+    <v-row class="pa-4 ma-0">
+        <v-col cols="12">
+            By generating S3 credentials, you are opting in to
+            <a class="link" href="https://docs.storj.io/dcs/concepts/encryption-key/design-decision-server-side-encryption/">
+                server-side encryption.
+            </a>
+        </v-col>
+        <v-col cols="12">
+            <v-checkbox
+                density="compact"
+                label="I understand, don't show this again."
+                hide-details
+                @update:model-value="value => LocalData.setServerSideEncryptionModalHidden(value)"
+            />
+        </v-col>
+    </v-row>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { VForm, VRow, VCol, VCheckbox } from 'vuetify/components';
+import { VRow, VCol, VCheckbox } from 'vuetify/components';
 
 import { LocalData } from '@/utils/localData';
-import { RequiredRule, DialogStepComponent } from '@poc/types/common';
-
-const form = ref<VForm | null>(null);
+import { DialogStepComponent } from '@poc/types/common';
 
 defineExpose<DialogStepComponent>({
     title: 'Encryption Information',
-    validate: () => {
-        form.value?.validate();
-        return !!form.value?.isValid;
-    },
 });
 </script>
