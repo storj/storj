98234e76b72ee8d05e74f6d72a884c7c5fac1cc8
Wilfred Asomani <devwilfredagyei@gmail.com>
2023-11-09 10:16:39 +0000

    satellite/{payment,console} add legal hold user status
    This change introduces a new user status, LegalHold to be used to pause
    an account while it is under legal review. It also modifies the
    condition whether to generate invoices for a user to apply to this new
    status.
    
    Issue: storj/storj-private#492
    
    Change-Id: Idc9a6166cc96178a95e45cf7c0f2e982631ca021

--- satellite/payments/stripe/service.go
+++ satellite/payments/stripe/service.go
@@ -1294,13 +1294,13 @@ func (service *Service) payInvoicesWithTokenBalance(ctx context.Context, cusID s
 	return errGrp.Err()
 }
 
-// isUserInactive checks whether a user has a status of console.Deleted or console.PendingDeletion.
+// isUserInactive checks whether a user does not have a status of console.Active.
 func (service *Service) isUserInactive(ctx context.Context, userID uuid.UUID) (bool, error) {
 	user, err := service.usersDB.Get(ctx, userID)
 	if err != nil {
 		return false, err
 	}
-	return user.Status == console.Deleted || user.Status == console.PendingDeletion, nil
+	return user.Status != console.Active, nil
 }
 
 // projectUsagePrice represents pricing for project usage.
