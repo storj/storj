00608ec495f3fa27e84dfc01e9096bacf87a98b3
Cameron <cameron@storj.io>
2023-12-06 12:19:31 -0500

    satellite/console: reenable wasm tests in jenkins
    Wasm tests in jenkins were disabled for a while because they stopped
    working. This change updates the tests so they pass and enables them in
    jenkins.
    
    * Update Jest version
    * Use base64 salt as input to generateNewAccessGrant instead of uuid
    * Check default permissions result is not null.
    * Remove unnecessary lines from test-wasm.sh
    * Stop returning exit code 0 even on test failure
    
    issue: https://github.com/storj/storj/issues/6518
    
    Change-Id: I4b4be0bd66d5a371cde847c4c882edff4c0d7f5d

--- scripts/test-wasm.sh
+++ scripts/test-wasm.sh
@@ -4,15 +4,12 @@
 set -x
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-: ${TAG:=dev}
-
 mkdir -p .build
 rm -rf .build/wasm || true
-rm -rf release/$TAG/wasm || true
 
-./scripts/build-wasm.sh
 cp -r satellite/console/wasm/tests/ .build/
 cd .build/tests/
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+
 npm install
 npm run test
