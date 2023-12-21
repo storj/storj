5cd440ff059e628daea7b9b4707565e47382b65c
Vitalii <takur12345@gmail.com>
2023-12-21 15:26:49 +0200

    web/satellite/vuetify-poc: add bundle compression
    Enabled compression plugin to decrease bundle size using brotli.
    
    Issue:
    https://github.com/storj/storj/issues/6617
    
    Change-Id: I21b885d4eefc3f3c237dba7857b2a40c6d23ce2f

--- web/satellite/vite.config-vuetify.js
+++ web/satellite/vite.config-vuetify.js
@@ -6,57 +6,72 @@ import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
 import vuetifyThemeCSS from './vitePlugins/vuetifyThemeCSS';
 import papaParseWorker from './vitePlugins/papaParseWorker';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/static/dist_vuetify_poc',
-    plugins: [
-        vue({
-            template: { transformAssetUrls },
-        }),
-        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-        vuetify({
-            autoImport: true,
-            styles: {
-                configFile: 'vuetify-poc/src/styles/settings.scss',
-            },
-        }),
-        vuetifyThemeCSS(),
-        papaParseWorker(),
-    ],
-    define: {
-        'process.env': {},
-        __UI_TYPE__: JSON.stringify('vuetify'),
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@poc': fileURLToPath(new URL('./vuetify-poc/src', import.meta.url)),
-            'stream': 'stream-browserify', // Passphrase mnemonic generation will not work without this
+const productionBrotliExtensions = ['js', 'css', 'ttf', 'woff', 'woff2'];
+
+const plugins = [
+    vue({
+        template: { transformAssetUrls },
+    }),
+    vuetify({
+        autoImport: true,
+        styles: {
+            configFile: 'vuetify-poc/src/styles/settings.scss',
         },
-        extensions: [
-            '.js',
-            '.json',
-            '.jsx',
-            '.mjs',
-            '.ts',
-            '.tsx',
-            '.vue',
-        ],
-    },
-    build: {
-        outDir: fileURLToPath(new URL('dist_vuetify_poc', import.meta.url)),
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                'vuetify-poc': fileURLToPath(new URL('./index-vuetify.html', import.meta.url)),
+    }),
+    vuetifyThemeCSS(),
+    papaParseWorker(),
+];
+
+export default defineConfig(({ mode }) => {
+    const isProd = mode === 'production';
+
+    // compress chunks only for production mode builds.
+    if (isProd) {
+        plugins.push(viteCompression({
+            algorithm: 'brotliCompress',
+            threshold: 1024,
+            ext: '.br',
+            filter: new RegExp('\\.(' + productionBrotliExtensions.join('|') + ')$'),
+        }));
+    }
+
+    return {
+        base: '/static/dist_vuetify_poc',
+        plugins,
+        define: {
+            'process.env': {},
+            __UI_TYPE__: JSON.stringify('vuetify'),
+        },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@poc': fileURLToPath(new URL('./vuetify-poc/src', import.meta.url)),
+                'stream': 'stream-browserify', // Passphrase mnemonic generation will not work without this
+            },
+            extensions: [
+                '.js',
+                '.json',
+                '.jsx',
+                '.mjs',
+                '.ts',
+                '.tsx',
+                '.vue',
+            ],
+        },
+        build: {
+            outDir: fileURLToPath(new URL('dist_vuetify_poc', import.meta.url)),
+            emptyOutDir: true,
+            reportCompressedSize: isProd,
+            rollupOptions: {
+                input: {
+                    'vuetify-poc': fileURLToPath(new URL('./index-vuetify.html', import.meta.url)),
+                },
             },
         },
-    },
-    server: {
-        port: 3000,
-    },
+    };
 });
