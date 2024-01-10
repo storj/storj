52bfc875553b77fb470c4a0ef5bb65136419fffe
Egon Elbre <egonelbre@gmail.com>
2024-01-10 13:03:26 +0200

    satellite/metabase: separate pending listing/iteration
    We need to have different behavior for listing pending objects
    so separate them out.
    
    It seems that eventually we'll move towards the ListObjects approach,
    then move the code directly into `list_pending.go` rather than
    `iterate_pending.go` to avoid moves down the line.
    
    Updates https://github.com/storj/storj/issues/6679
    
    Change-Id: I300937ab7a8b9f6145cc5369ade0c218fff4e7dd

--- satellite/metabase/list.go
+++ satellite/metabase/list.go
@@ -72,13 +72,6 @@ type StreamIDCursor struct {
 	StreamID uuid.UUID
 }
 
-// IteratePendingObjectsByKey contains arguments necessary for listing pending objects by ObjectKey.
-type IteratePendingObjectsByKey struct {
-	ObjectLocation
-	BatchSize int
-	Cursor    StreamIDCursor
-}
-
 // IterateObjectsWithStatus contains arguments necessary for listing objects in a bucket.
 type IterateObjectsWithStatus struct {
 	ProjectID             uuid.UUID
@@ -103,80 +96,6 @@ func (db *DB) IterateObjectsAllVersionsWithStatus(ctx context.Context, opts Iter
 
 // Verify verifies get object request fields.
 func (opts *IterateObjectsWithStatus) Verify() error {
-	switch {
-	case opts.ProjectID.IsZero():
-		return ErrInvalidRequest.New("ProjectID missing")
-	case opts.BucketName == "":
-		return ErrInvalidRequest.New("BucketName missing")
-	case opts.BatchSize < 0:
-		return ErrInvalidRequest.New("BatchSize is negative")
-	}
-	return nil
-}
-
-// IteratePendingObjectsByKey iterates through all streams of pending objects with the same ObjectKey.
-func (db *DB) IteratePendingObjectsByKey(ctx context.Context, opts IteratePendingObjectsByKey, fn func(context.Context, ObjectsIterator) error) (err error) {
-	defer mon.Task()(&ctx)(&err)
-
-	if err := opts.Verify(); err != nil {
-		return err
-	}
-	return iteratePendingObjectsByKey(ctx, db, opts, fn)
-}
-
-// Verify verifies get object request fields.
-func (opts *IteratePendingObjectsByKey) Verify() error {
-	if err := opts.ObjectLocation.Verify(); err != nil {
-		return err
-	}
-	if opts.BatchSize < 0 {
-		return ErrInvalidRequest.New("BatchSize is negative")
-	}
-	return nil
-}
-
-// PendingObjectEntry contains information about an pending object item in a bucket.
-type PendingObjectEntry struct {
-	IsPrefix bool
-
-	ObjectKey ObjectKey
-	StreamID  uuid.UUID
-
-	CreatedAt time.Time
-	ExpiresAt *time.Time
-
-	EncryptedMetadataNonce        []byte
-	EncryptedMetadata             []byte
-	EncryptedMetadataEncryptedKey []byte
-
-	Encryption storj.EncryptionParameters
-}
-
-// PendingObjectsIterator iterates over a sequence of PendingObjectEntry items.
-type PendingObjectsIterator interface {
-	Next(ctx context.Context, item *PendingObjectEntry) bool
-}
-
-// PendingObjectsCursor cursor for iterating over pending objects.
-type PendingObjectsCursor struct {
-	Key      ObjectKey
-	StreamID uuid.UUID
-}
-
-// IteratePendingObjects contains arguments necessary for listing pending objects in a bucket.
-type IteratePendingObjects struct {
-	ProjectID             uuid.UUID
-	BucketName            string
-	Recursive             bool
-	BatchSize             int
-	Prefix                ObjectKey
-	Cursor                PendingObjectsCursor
-	IncludeCustomMetadata bool
-	IncludeSystemMetadata bool
-}
-
-// Verify verifies request fields.
-func (opts *IteratePendingObjects) Verify() error {
 	switch {
 	case opts.ProjectID.IsZero():
 		return ErrInvalidRequest.New("ProjectID missing")
