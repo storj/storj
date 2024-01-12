H3c-t0r/storsjs

satellite/admin/back-office: Add settings endpoint
Define and implement a new endpoint to expose services' settings.

At this initial point the endpoint only exposes Satellite Admin features
flags.

Closes https://github.com/storj/storj/issues/6644

Change-Id: I419f9173e9f53f7f9c267904d210141856ddfb5a

satellite/admin/back-office/settings.go
@@ -1,0 +1,139 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package admin
+
+import (
+	"context"
+
+	"storj.io/storj/private/api"
+)
+
+// Settings contains relevant settings for the consumers of this service. It may contain settings
+// of:
+//
+// - this service.
+//
+// - the server that exposes the service.
+//
+// - related Storj services (e.g. Satellite).
+type Settings struct {
+	Admin SettingsAdmin `json:"admin"`
+}
+
+// SettingsAdmin are the settings of this service and the server that exposes it.
+type SettingsAdmin struct {
+	Features FeatureFlags `json:"features"`
+}
+
+// FeatureFlags indicates what Admin service features are enabled or disabled. The features are
+// usually disabled when they are not fully implemented.
+type FeatureFlags struct {
+	Account         AccountFlags `json:"account"`
+	Project         ProjectFlags `json:"project"`
+	Bucket          BucketFlags  `json:"bucket"`
+	Dashboard       bool         `json:"dashboard"`
+	Operator        bool         `json:"operator"` // This is the information about the logged operator
+	SignOut         bool         `json:"singOut"`
+	SwitchSatellite bool         `json:"switchSatellite"`
+}
+
+// AccountFlags are the feature flags related to user's accounts.
+type AccountFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	Projects               bool `json:"projects"`
+	Suspend                bool `json:"suspend"`
+	Unsuspend              bool `json:"unsusped"`
+	ResetMFA               bool `json:"resetMFA"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdateLimits           bool `json:"updateLimits"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateStatus           bool `json:"updateStatus"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+}
+
+// ProjectFlags are the feature flags related to projects.
+type ProjectFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdateLimits           bool `json:"updateLimits"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+	MemberList             bool `json:"memberList"`
+	MemberAdd              bool `json:"memberAdd"`
+	MemberRemove           bool `json:"memberRemove"`
+}
+
+// BucketFlags are the feature flags related to buckets.
+type BucketFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+}
+
+// GetSettings returns the service settings.
+func (s *Service) GetSettings(ctx context.Context) (*Settings, api.HTTPError) {
+	return &Settings{
+		Admin: SettingsAdmin{
+			Features: FeatureFlags{
+				Account: AccountFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					Projects:               true,
+					Suspend:                false,
+					Unsuspend:              false,
+					ResetMFA:               false,
+					UpdateInfo:             false,
+					UpdateLimits:           false,
+					UpdatePlacement:        false,
+					UpdateStatus:           false,
+					UpdateValueAttribution: false,
+					View:                   true,
+				},
+				Project: ProjectFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					UpdateInfo:             false,
+					UpdateLimits:           false,
+					UpdatePlacement:        false,
+					UpdateValueAttribution: false,
+					View:                   false,
+					MemberList:             false,
+					MemberAdd:              false,
+					MemberRemove:           false,
+				},
+				Bucket: BucketFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					UpdateInfo:             false,
+					UpdatePlacement:        false,
+					UpdateValueAttribution: false,
+					View:                   false,
+				},
+				Dashboard:       false,
+				Operator:        false,
+				SignOut:         false,
+				SwitchSatellite: false,
+			},
+		},
+	}, api.HTTPError{}
+}

satellite/admin/back-office/ui/src/api/client.gen.ts
@@ -4,9 +4,53 @@
 import { HttpClient } from '@/utils/httpClient';
 import { Time, UUID } from '@/types/common';
 
+export class AccountFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    projects: boolean;
+    suspend: boolean;
+    unsusped: boolean;
+    resetMFA: boolean;
+    update: boolean;
+    view: boolean;
+}
+
+export class BucketFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    update: boolean;
+    view: boolean;
+}
+
+export class FeatureFlags {
+    account: AccountFlags;
+    project: ProjectFlags;
+    bucket: BucketFlags;
+    dashboard: boolean;
+    operator: boolean;
+    singOut: boolean;
+    switchSatellite: boolean;
+}
+
 export class PlacementInfo {
     id: number;
     location: string;
+}
+
+export class ProjectFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    update: boolean;
+    view: boolean;
+    memberList: boolean;
+    memberAdd: boolean;
+    memberRemove: boolean;
 }
 
 export class ProjectUsageLimits {
@@ -18,6 +62,14 @@ export class ProjectUsageLimits {
     bandwidthUsed: number;
     segmentLimit: number;
     segmentUsed: number | null;
+}
+
+export class Settings {
+    admin: SettingsAdmin;
+}
+
+export class SettingsAdmin {
+    features: FeatureFlags;
 }
 
 export class User {
@@ -38,6 +90,21 @@ class APIError extends Error {
         public readonly responseStatusCode?: number,
     ) {
         super(msg);
+    }
+}
+
+export class SettingsHttpApiV1 {
+    private readonly http: HttpClient = new HttpClient();
+    private readonly ROOT_PATH: string = '/back-office/api/v1/settings';
+
+    public async get(): Promise<Settings> {
+        const fullPath = `${this.ROOT_PATH}/`;
+        const response = await this.http.get(fullPath);
+        if (response.ok) {
+            return response.json().then((body) => body as Settings);
+        }
+        const err = await response.json();
+        throw new APIError(err.error, response.status);
     }
 }
 

storj/storj

satellite/admin/back-office/settings.go
@@ -1,0 +1,139 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package admin
+
+import (
+	"context"
+
+	"storj.io/storj/private/api"
+)
+
+// Settings contains relevant settings for the consumers of this service. It may contain settings
+// of:
+//
+// - this service.
+//
+// - the server that exposes the service.
+//
+// - related Storj services (e.g. Satellite).
+type Settings struct {
+	Admin SettingsAdmin `json:"admin"`
+}
+
+// SettingsAdmin are the settings of this service and the server that exposes it.
+type SettingsAdmin struct {
+	Features FeatureFlags `json:"features"`
+}
+
+// FeatureFlags indicates what Admin service features are enabled or disabled. The features are
+// usually disabled when they are not fully implemented.
+type FeatureFlags struct {
+	Account         AccountFlags `json:"account"`
+	Project         ProjectFlags `json:"project"`
+	Bucket          BucketFlags  `json:"bucket"`
+	Dashboard       bool         `json:"dashboard"`
+	Operator        bool         `json:"operator"` // This is the information about the logged operator
+	SignOut         bool         `json:"signOut"`
+	SwitchSatellite bool         `json:"switchSatellite"`
+}
+
+// AccountFlags are the feature flags related to user's accounts.
+type AccountFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	Projects               bool `json:"projects"`
+	Suspend                bool `json:"suspend"`
+	Unsuspend              bool `json:"unsuspend"`
+	ResetMFA               bool `json:"resetMFA"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdateLimits           bool `json:"updateLimits"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateStatus           bool `json:"updateStatus"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+}
+
+// ProjectFlags are the feature flags related to projects.
+type ProjectFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdateLimits           bool `json:"updateLimits"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+	MemberList             bool `json:"memberList"`
+	MemberAdd              bool `json:"memberAdd"`
+	MemberRemove           bool `json:"memberRemove"`
+}
+
+// BucketFlags are the feature flags related to buckets.
+type BucketFlags struct {
+	Create                 bool `json:"create"`
+	Delete                 bool `json:"delete"`
+	History                bool `json:"history"`
+	List                   bool `json:"list"`
+	UpdateInfo             bool `json:"updateInfo"`
+	UpdatePlacement        bool `json:"updatePlacement"`
+	UpdateValueAttribution bool `json:"updateValueAttribution"`
+	View                   bool `json:"view"`
+}
+
+// GetSettings returns the service settings.
+func (s *Service) GetSettings(ctx context.Context) (*Settings, api.HTTPError) {
+	return &Settings{
+		Admin: SettingsAdmin{
+			Features: FeatureFlags{
+				Account: AccountFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					Projects:               true,
+					Suspend:                false,
+					Unsuspend:              false,
+					ResetMFA:               false,
+					UpdateInfo:             false,
+					UpdateLimits:           false,
+					UpdatePlacement:        false,
+					UpdateStatus:           false,
+					UpdateValueAttribution: false,
+					View:                   true,
+				},
+				Project: ProjectFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					UpdateInfo:             false,
+					UpdateLimits:           false,
+					UpdatePlacement:        false,
+					UpdateValueAttribution: false,
+					View:                   false,
+					MemberList:             false,
+					MemberAdd:              false,
+					MemberRemove:           false,
+				},
+				Bucket: BucketFlags{
+					Create:                 false,
+					Delete:                 false,
+					History:                false,
+					List:                   false,
+					UpdateInfo:             false,
+					UpdatePlacement:        false,
+					UpdateValueAttribution: false,
+					View:                   false,
+				},
+				Dashboard:       false,
+				Operator:        false,
+				SignOut:         false,
+				SwitchSatellite: false,
+			},
+		},
+	}, api.HTTPError{}
+}

satellite/admin/back-office/ui/src/api/client.gen.ts
@@ -4,6 +4,44 @@
 import { HttpClient } from '@/utils/httpClient';
 import { Time, UUID } from '@/types/common';
 
+export class AccountFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    projects: boolean;
+    suspend: boolean;
+    unsuspend: boolean;
+    resetMFA: boolean;
+    updateInfo: boolean;
+    updateLimits: boolean;
+    updatePlacement: boolean;
+    updateStatus: boolean;
+    updateValueAttribution: boolean;
+    view: boolean;
+}
+
+export class BucketFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    updateInfo: boolean;
+    updatePlacement: boolean;
+    updateValueAttribution: boolean;
+    view: boolean;
+}
+
+export class FeatureFlags {
+    account: AccountFlags;
+    project: ProjectFlags;
+    bucket: BucketFlags;
+    dashboard: boolean;
+    operator: boolean;
+    signOut: boolean;
+    switchSatellite: boolean;
+}
+
 export class PlacementInfo {
     id: number;
     location: string;
@@ -28,6 +66,21 @@ export class Project {
     segmentUsed: number | null;
 }
 
+export class ProjectFlags {
+    create: boolean;
+    delete: boolean;
+    history: boolean;
+    list: boolean;
+    updateInfo: boolean;
+    updateLimits: boolean;
+    updatePlacement: boolean;
+    updateValueAttribution: boolean;
+    view: boolean;
+    memberList: boolean;
+    memberAdd: boolean;
+    memberRemove: boolean;
+}
+
 export class ProjectLimitsUpdate {
     maxBuckets: number;
     storageLimit: number;
@@ -35,6 +88,14 @@ export class ProjectLimitsUpdate {
     segmentLimit: number;
     rateLimit: number;
     burstLimit: number;
+}
+
+export class Settings {
+    admin: SettingsAdmin;
+}
+
+export class SettingsAdmin {
+    features: FeatureFlags;
 }
 
 export class User {
@@ -72,6 +133,21 @@ class APIError extends Error {
         public readonly responseStatusCode?: number,
     ) {
         super(msg);
+    }
+}
+
+export class SettingsHttpApiV1 {
+    private readonly http: HttpClient = new HttpClient();
+    private readonly ROOT_PATH: string = '/back-office/api/v1/settings';
+
+    public async get(): Promise<Settings> {
+        const fullPath = `${this.ROOT_PATH}/`;
+        const response = await this.http.get(fullPath);
+        if (response.ok) {
+            return response.json().then((body) => body as Settings);
+        }
+        const err = await response.json();
+        throw new APIError(err.error, response.status);
     }
 }
 