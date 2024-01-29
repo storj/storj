b5c3362077407d8bec6d1c3e10725fc738f02f41
Vitalii <takur12345@gmail.com>
2024-01-29 14:19:39 +0200

    web/satellite/v2: disable swipes for multistep dialog flows
    Disallow mobile swipes on multistep dialog flows e.g. create AG, enable MFA etc.
    
    Issue:
    https://github.com/storj/storj/issues/6729
    
    Change-Id: I452115c3e02810eece61feee1ac4ba4769f66a75

--- web/satellite/vuetify-poc/src/main.ts
+++ web/satellite/vuetify-poc/src/main.ts
@@ -1,23 +1,18 @@
 // Copyright (C) 2023 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App
- */
-// Components
 import { createApp } from 'vue';
 import Papa from 'papaparse';
 import PAPA_PARSE_WORKER_URL from 'virtual:papa-parse-worker';
 
 import App from './App.vue';
 
-// Plugins
 import { registerPlugins } from '@poc/plugins';
+import { registerMixins } from '@poc/mixins';
 
 const app = createApp(App);
 
+registerMixins(app);
 registerPlugins(app).then(() => {
     const loader = document.getElementById('pre-app-loader');
     loader?.remove();
