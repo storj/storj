d1682e71620b228a9b0f67a3a3427d11221ce953
Egon Elbre <egonelbre@gmail.com>
2024-03-08 23:59:55 +0200

    testsuite/: fix finding previous versions in non-latest branches
    Change-Id: I044504b816c9e2d8cacb08b481ec4b1df8b0330c

--- testsuite/find-previous-major-release-tag.sh
+++ testsuite/find-previous-major-release-tag.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-# This script finds the previous major tag from the current HEAD position.
-
-closestHeadVersion="$(git describe --tags)"
-
-sortedTags="$(git tag --list --sort -version:refname)"
-
 verlte() {
     # sort and get the first result
     [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
@@ -15,10 +9,32 @@ verlt() {
     [ "$1" = "$2" ] && return 1 || verlte $1 $2
 }
 
-closestZeroVersion="$(echo $closestHeadVersion | cut -d '.' -f 1-2).0"
+# This script finds the previous major tag from the current HEAD position.
+
+closest_head_version="$(git describe --tags)"
+
+sorted_tags="$(git tag --list --sort -version:refname)"
+
+# this is ugly, but we use this approach to detect whether we are
+# on a main branch, if that's the case, then we'll output the
+# latest release version.
+if [[ $closest_head_version =~ ^v1\.91\.0\-alpha ]]; then
+    for tag in $sorted_tags
+    do
+        if [[ "$tag" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            echo $tag
+            exit 0
+        fi
+    done
+
+    echo "did not find an appropriate release tag from main branch"
+    exit 1
+fi
+
+closest_zero_version="$(echo $closest_head_version | cut -d '.' -f 1-2).0"
 
 IFS=$'\n'
-for tag in $sortedTags
+for tag in $sorted_tags
 do
     if [[ ! "$tag" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         # it's not a proper version so ignore
@@ -26,7 +42,7 @@ do
     fi
 
     # the first less than our closest head should be our latest version
-    if verlt $tag $closestZeroVersion; then
+    if verlt $tag $closest_zero_version; then
         echo $tag
         exit 0
     fi
