2483fd38d29cd5984f7a66dd862bc34810d85b18
JT Olio <jt@olio.lol>
2024-05-24 10:20:01 -0400

    satellite/metainfo: make power of two choice load balancing the default
    Change-Id: Ib9e4526a898a27a75f2ccb676da555022aa5854a

--- satellite/metainfo/config.go
+++ satellite/metainfo/config.go
@@ -141,7 +141,7 @@ type Config struct {
 	RateLimiter                  RateLimiterConfig   `help:"rate limiter configuration"`
 	UploadLimiter                UploadLimiterConfig `help:"object upload limiter configuration"`
 	ProjectLimits                ProjectLimitConfig  `help:"project limit configuration"`
-	SuccessTrackerEnabled        bool                `default:"false" devDefault:"true" help:"enable success tracker based node selection"`
+	SuccessTrackerEnabled        bool                `default:"true" devDefault:"true" help:"enable success tracker based node selection"`
 	SuccessTrackerTickDuration   time.Duration       `default:"10m" help:"how often to bump the generation in the node success tracker"`
 	SuccessTrackerTrustedUplinks []string            `help:"list of trusted uplinks for success tracker"`
 
