30f80af8cb49f3f37b5e38569176b1218df7fb27
Clement Sam <clementsam75@gmail.com>
2024-06-17 04:06:05 +0000

    storagenode/storagenodedb: buffer up GC Filewalker progress storage
    Previously, the GC filewalker stores the progress after completing
    a full scan of each prefix. This consequently could lead to the
    database lock issues, if there are multiple gc filewalkers running
    as reported on the forum.
    
    This patch makes sure the db is only updated after the
    filewalker completes scanning a specified number of
    prefixes (currently 5; hardcoded).
    
    https://forum.storj.io/t/error-lazyfilewalker-gc-filewalker-subprocess-failed-to-save-progress-in-the-database/26407
    
    Change-Id: I26a0cbb778b74095f2b1c83221896d856f1052dc

--- storagenode/storagenodedb/gcprogress.go
+++ storagenode/storagenodedb/gcprogress.go
@@ -5,6 +5,8 @@ package storagenodedb
 
 import (
 	"context"
+	"strings"
+	"sync"
 
 	"github.com/zeebo/errs"
 
@@ -12,29 +14,76 @@ import (
 	"storj.io/storj/storagenode/pieces"
 )
 
-// ErrGCProgress represents errors from the filewalker database.
-var ErrGCProgress = errs.Class("gc_filewalker_progress_db")
+var (
+	// ErrGCProgress represents errors from the filewalker database.
+	ErrGCProgress = errs.Class("gc_filewalker_progress_db")
+
+	maxPrefixesScannedToFlush = 5
+)
 
 // GCFilewalkerProgressDBName represents the database name.
 const GCFilewalkerProgressDBName = "garbage_collection_filewalker_progress"
 
 type gcFilewalkerProgressDB struct {
 	dbContainerImpl
+
+	mu  sync.Mutex
+	buf map[storj.NodeID]*progressBuffer
+}
+
+type progressBuffer struct {
+	prefixesScanned int
+	pieces.GCFilewalkerProgress
 }
 
 func (db *gcFilewalkerProgressDB) Store(ctx context.Context, progress pieces.GCFilewalkerProgress) (err error) {
 	defer mon.Task()(&ctx)(&err)
 
+	db.mu.Lock()
+	if db.buf == nil {
+		db.buf = make(map[storj.NodeID]*progressBuffer)
+	}
+	sat := db.buf[progress.SatelliteID]
+
+	if sat == nil {
+		sat = &progressBuffer{}
+		db.buf[progress.SatelliteID] = sat
+	}
+	sat.prefixesScanned++
+	sat.GCFilewalkerProgress = progress
+	if sat.prefixesScanned < maxPrefixesScannedToFlush {
+		db.mu.Unlock()
+		return nil
+	}
+	// if any of the satellites has scanned more than maxPrefixesScannedToFlush prefixes, flush the buffer
+	batch := db.buf
+	db.buf = make(map[storj.NodeID]*progressBuffer)
+	db.mu.Unlock()
+
+	var args []any
+	for id, buf := range batch {
+		args = append(args, id, buf.BloomfilterCreatedBefore.UTC(), buf.Prefix)
+	}
+
+	values := strings.TrimRight(strings.Repeat("(?,?,?),", len(args)/3), ",")
 	_, err = db.ExecContext(ctx, `
 		INSERT OR REPLACE INTO progress(satellite_id, bloomfilter_created_before, last_checked_prefix)
-		VALUES(?,?,?)
-	`, progress.SatelliteID, progress.BloomfilterCreatedBefore.UTC(), progress.Prefix)
+	`+values, args...)
 
 	return ErrGCProgress.Wrap(err)
 }
 
 func (db *gcFilewalkerProgressDB) Get(ctx context.Context, satelliteID storj.NodeID) (progress pieces.GCFilewalkerProgress, err error) {
 	defer mon.Task()(&ctx)(&err)
+
+	db.mu.Lock()
+	if db.buf != nil {
+		if buf, ok := db.buf[satelliteID]; ok {
+			defer db.mu.Unlock()
+			return buf.GCFilewalkerProgress, nil
+		}
+	}
+	db.mu.Unlock()
 
 	err = db.QueryRowContext(ctx, `
 		SELECT last_checked_prefix, bloomfilter_created_before
@@ -49,6 +98,10 @@ func (db *gcFilewalkerProgressDB) Get(ctx context.Context, satelliteID storj.Nod
 
 func (db *gcFilewalkerProgressDB) Reset(ctx context.Context, satelliteID storj.NodeID) (err error) {
 	defer mon.Task()(&ctx)(&err)
+
+	db.mu.Lock()
+	delete(db.buf, satelliteID)
+	db.mu.Unlock()
 
 	_, err = db.ExecContext(ctx, `
 		DELETE FROM progress
