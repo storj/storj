351f8d67876b0e96c0f334ea3f6549972e7630b4
paul cannon <thepaul@storj.io>
2024-07-01 14:58:28 -0500

    storagenode/collector: test used space updating
    We want to make sure that the used space cache is updated when expired
    pieces are deleted.
    
    Refs: https://github.com/storj/storj/issues/7025
    Change-Id: I85eac606157d730832090cd7a79240e8854c3f5b

--- storagenode/collector/service_test.go
+++ storagenode/collector/service_test.go
@@ -43,11 +43,11 @@ func TestCollector(t *testing.T) {
 
 		// imagine we are 30 minutes in the future
 		for _, storageNode := range planet.StorageNodes {
-			pieceStore := storageNode.DB.Pieces()
+			blobCache := storageNode.Storage2.BlobsCache
 			usedSerials := storageNode.UsedSerials
 
 			// verify that we actually have some data on storage nodes
-			used, err := pieceStore.SpaceUsedForBlobs(ctx)
+			used, _, err := blobCache.SpaceUsedForPieces(ctx)
 			require.NoError(t, err)
 			if used == 0 {
 				// this storage node didn't get picked for storing data
@@ -87,7 +87,7 @@ func TestCollector(t *testing.T) {
 
 		// imagine we are 10 days in the future
 		for _, storageNode := range planet.StorageNodes {
-			pieceStore := storageNode.DB.Pieces()
+			blobCache := storageNode.Storage2.BlobsCache
 			usedSerials := storageNode.UsedSerials
 
 			// collect all the data
@@ -95,7 +95,7 @@ func TestCollector(t *testing.T) {
 			require.NoError(t, err)
 
 			// verify that we deleted everything
-			used, err := pieceStore.SpaceUsedForBlobs(ctx)
+			used, _, err := blobCache.SpaceUsedForPieces(ctx)
 			require.NoError(t, err)
 			require.Equal(t, int64(0), used)
 
