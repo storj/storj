6e98aa0df77da4234661746fdac3142955acacce
Wilfred Asomani <devwilfredagyei@gmail.com>
2024-07-26 14:49:00 +0000

    web/satellite: add final step to create bucket dialog
    This change adds a last step to the create bucket dialog which gives
    users the option to either stay on the current page or be taken to the
    new bucket's file browser.
    
    Issue: https://github.com/storj/storj/issues/7066
    
    Change-Id: Idaf763a2aeac3412191721d28e287b6c7a12059a

--- testsuite/playwright-ui/pageFactory/objectRepository/BucketsPageObjects.ts
+++ testsuite/playwright-ui/pageFactory/objectRepository/BucketsPageObjects.ts
@@ -4,11 +4,12 @@
 export class BucketsPageObjects {
     static NEW_BUCKET_BUTTON_XPATH = `//button[span[text()=' New Bucket ']]`;
     static BUCKET_NAME_INPUT_FIELD_XPATH = `//input[@id='Bucket Name']`;
+    static CLOSE_BUTTON_CREATE_BUCKET_FLOW_XPATH = `//button[span[text()='Close']]`;
     static CONFIRM_BUTTON_CREATE_BUCKET_FLOW_XPATH = `//button[span[text()='Create Bucket']]`;
     static BUCKET_ROW_MORE_BUTTON_XPATH = `//button[@title='Bucket Actions']`;
     static VIEW_BUCKET_DETAILS_BUTTON_XPATH = `//div[div[div[text()=' Bucket Details ']]]`;
     static SHARE_BUCKET_BUTTON_XPATH = `//div[div[div[text()=' Share Bucket ']]]`;
     static DELETE_BUCKET_BUTTON_XPATH = `//div[div[div[text()=' Delete Bucket ']]]`;
     static CONFIRM_BUTTON_DELETE_BUCKET_FLOW_XPATH = `//button[span[text()=' Delete ']]`;
-    static CLOSE_DETAILS_MODAL_BUTTON_XPATH = `//button[@id='close-bucket-details']`
+    static CLOSE_DETAILS_MODAL_BUTTON_XPATH = `//button[@id='close-bucket-details']`;
 }
