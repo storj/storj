5b7a536aec5beb9312222bfd211b80f7369d8fd9
Wilfred Asomani <devwilfredagyei@gmail.com>
2024-08-27 17:31:02 +0000

    web/satellite: fix pricing plan step success on account setup
    This change prevents the pricing plan step's success UI from showing on
    the account setup dialog.
    
    Issue: https://github.com/storj/storj/issues/7107
    
    Change-Id: Ia4f7f379514951258cd489f0e13c1d6a744a4fcb

--- web/satellite/src/components/dialogs/AccountSetupDialog.vue
+++ web/satellite/src/components/dialogs/AccountSetupDialog.vue
@@ -89,7 +89,9 @@
                             <v-row justify="center" align="center">
                                 <v-col cols="12" sm="8" md="6" lg="4">
                                     <PricingPlanStep
+                                        v-model:loading="isLoading"
                                         :plan="plan"
+                                        is-account-setup
                                         @back="toPrevStep"
                                         @success="toNextStep"
                                     />
