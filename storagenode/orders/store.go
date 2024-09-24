9e5a1eace9b9ffc4ff8d4480a7b9c10df4dab0bf
Ivan Fraixedes <ivan@fraixed.es>
2024-09-24 13:52:12 +0200

    storagenode/store: Fix data race
    Fix a data race caused by accessing to the unsent orders files map
    without acquiring the mutex.
    
    In order to avoid deadlocks, @egonelbre realized that when we need to
    acquire the unsent mutext after the other mutexes to avoid deadlocks.
    For example:
    
    ```
    store.activeMu.Lock()
    defer store.activeMu.UnLock()
    store.unsentMu.Lock()
    defer store.unsentMu.UnLock()
    ```
    
    ```
    store.archiveMu.Lock()
    defer store.archiveMu.UnLock()
    store.unsentMu.Lock()
    defer store.unsentMu.UnLock()
    ```
    
    This commit also drops the unsent orders files map mutex acquisition of
    a method that doesn't access it.
    
    See:
    https://forum.storj.io/t/fatal-error-concurrent-map-read-and-map-write/27688/11
    
    Change-Id: Ic7fcf900a7c96e0cf7c14ab8b87b72fe028da80b

--- storagenode/orders/store.go
+++ storagenode/orders/store.go
@@ -37,9 +37,26 @@ type FileStore struct {
 	unsentDir  string
 	archiveDir string
 
-	// always acquire the activeMu after the unsentMu to avoid deadlocks. if someone acquires
-	// activeMu before unsentMu, then you can be in a situation where two goroutines are
-	// waiting for each other forever.
+	// NOTE: When two mutexes have to be acquired, always acquire and release them with the same
+	// order because there may be situations where two goroutines are waiting for each other forever,
+	// a.k.a deadlock.
+	// - First activeMu, then unsentMu
+	// - First archiveMu, then unsentMu
+	//
+	// Examples:
+	// ```
+	// store.activeMu.Lock()
+	// defer store.activeMu.Unlock()
+	// store.unsentMu.Lock()
+	// defer store.unsentMu.Unlock()
+	// ```
+	//
+	// ```
+	// store.archiveMu.Lock()
+	// defer store.archiveMu.Unlock()
+	// store.unsentMu.Lock()
+	// defer store.unsentMu.Unlock()
+	// ```
 
 	// mutex for the active map
 	activeMu sync.Mutex
@@ -77,10 +94,10 @@ func NewFileStore(log *zap.Logger, ordersDir string, orderLimitGracePeriod time.
 
 // Close closes the file store.
 func (store *FileStore) Close() (err error) {
-	store.unsentMu.Lock()
-	defer store.unsentMu.Unlock()
 	store.activeMu.Lock()
 	defer store.activeMu.Unlock()
+	store.unsentMu.Lock()
+	defer store.unsentMu.Unlock()
 
 	for fileName, file := range store.unsentOrdersFiles {
 		err = errs.Combine(err, OrderError.Wrap(file.Close()))
@@ -93,8 +110,6 @@ func (store *FileStore) Close() (err error) {
 // BeginEnqueue returns a function that can be called to enqueue the passed in Info. If the Info
 // is too old to be enqueued, then an error is returned.
 func (store *FileStore) BeginEnqueue(satelliteID storj.NodeID, createdAt time.Time) (commit func(*ordersfile.Info) error, err error) {
-	store.unsentMu.Lock()
-	defer store.unsentMu.Unlock()
 	store.activeMu.Lock()
 	defer store.activeMu.Unlock()
 
@@ -109,11 +124,10 @@ func (store *FileStore) BeginEnqueue(satelliteID storj.NodeID, createdAt time.Ti
 	store.enqueueStartedLocked(satelliteID, createdAt)
 
 	return func(info *ordersfile.Info) error {
-		// always acquire the activeMu after the unsentMu to avoid deadlocks
-		store.unsentMu.Lock()
-		defer store.unsentMu.Unlock()
 		store.activeMu.Lock()
 		defer store.activeMu.Unlock()
+		store.unsentMu.Lock()
+		defer store.unsentMu.Unlock()
 
 		// always remove the in flight operation
 		defer store.enqueueFinishedLocked(satelliteID, createdAt)
@@ -219,6 +233,8 @@ func (store *FileStore) ListUnsentBySatellite(ctx context.Context, now time.Time
 	// shouldn't be necessary, but acquire archiveMu to ensure we do not attempt to archive files during list
 	store.archiveMu.Lock()
 	defer store.archiveMu.Unlock()
+	store.unsentMu.Lock()
+	defer store.unsentMu.Unlock()
 
 	var errList errs.Group
 	infoMap = make(map[storj.NodeID]UnsentInfo)
@@ -310,10 +326,10 @@ func (store *FileStore) getUnsentInfoFromUnsentFile(dir, fileName string, fileIn
 
 // Archive moves a file from "unsent" to "archive".
 func (store *FileStore) Archive(satelliteID storj.NodeID, unsentInfo UnsentInfo, archivedAt time.Time, status pb.SettlementWithWindowResponse_Status) error {
-	store.unsentMu.Lock()
-	defer store.unsentMu.Unlock()
 	store.archiveMu.Lock()
 	defer store.archiveMu.Unlock()
+	store.unsentMu.Lock()
+	defer store.unsentMu.Unlock()
 
 	return OrderError.Wrap(ordersfile.MoveUnsent(
 		store.unsentDir,
