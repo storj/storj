61c97a6d8d9f6e0c7e38d67fb06c7087e926e721
Michal Niewrzal <michal@storj.io>
2024-09-24 17:40:35 +0200

    satellite/metabase/rangedloop: support stale spanner stale reads
    We would like to use stale reads to have fixed view on DB for
    GC execution. This change implements supports performaing ranged loop
    queries at the given time in the past.
    
    Change-Id: I90f809542ace317c2e078e355c2575a627af4fd0

--- satellite/repair/checker/observer_test.go
+++ satellite/repair/checker/observer_test.go
@@ -453,7 +453,7 @@ func TestRepairObserver(t *testing.T) {
 			service := rangedloop.NewService(planet.Log(), rangedloop.Config{
 				Parallelism: tc.Parallelism,
 				BatchSize:   tc.BatchSize,
-			}, rangedloop.NewMetabaseRangeSplitter(planet.Satellites[0].Metabase.DB, config.RangedLoop.AsOfSystemInterval, config.RangedLoop.BatchSize), []rangedloop.Observer{observer})
+			}, rangedloop.NewMetabaseRangeSplitter(planet.Satellites[0].Metabase.DB, config.RangedLoop.AsOfSystemInterval, config.RangedLoop.SpannerStaleInterval, config.RangedLoop.BatchSize), []rangedloop.Observer{observer})
 
 			_, err = service.RunOnce(ctx)
 			require.NoError(t, err)
