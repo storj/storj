868618adfc0400de5fbf9faebb03b737ddfc1d19
Egon Elbre <egonelbre@gmail.com>
2024-10-01 14:36:22 +0300

    satellite/satellitedb: workaround for spanner emulator bug
    Spanner emulator 1.5.24 has a bug where when concurrent databases with
    sequences with the same name end up colliding with each other and
    resetting. Use unique names for the sequences to avoid this issue until
    the bug gets fixed upstream.
    
    Change-Id: I95c40ac35adc0c5c7f265d110791f9e5e65abe26

--- satellite/payments/billing/chore_test.go
+++ satellite/payments/billing/chore_test.go
@@ -145,7 +145,7 @@ func TestChore(t *testing.T) {
 
 	t.Run("without StorjScan bonus", func(t *testing.T) {
 		testplanet.Run(t, testplanet.Config{
-			SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, // TODO(spanner): EnableSpanner, currently flaky
+			SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, EnableSpanner: true,
 		}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 			sat := planet.Satellites[0]
 			db := sat.DB
@@ -169,7 +169,7 @@ func TestChore(t *testing.T) {
 
 	t.Run("with StorjScan bonus", func(t *testing.T) {
 		testplanet.Run(t, testplanet.Config{
-			SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, // TODO(spanner): EnableSpanner, currently flaky
+			SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, EnableSpanner: true,
 		}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 			sat := planet.Satellites[0]
 			db := sat.DB
@@ -194,7 +194,7 @@ func TestChore(t *testing.T) {
 
 func TestChore_UpgradeUserObserver(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
-		SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, // TODO(spanner): EnableSpanner, currently flaky
+		SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, EnableSpanner: true,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 		sat := planet.Satellites[0]
 		db := sat.DB
@@ -344,7 +344,7 @@ func TestChore_UpgradeUserObserver(t *testing.T) {
 
 func TestChore_PayInvoiceObserver(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
-		SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, // TODO(spanner): EnableSpanner, currently flaky
+		SatelliteCount: 1, StorageNodeCount: 0, UplinkCount: 0, EnableSpanner: true,
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 		sat := planet.Satellites[0]
 		db := sat.DB
