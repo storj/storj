f931bbb656338fe8909fe94a28281cb0aba09a32
Márton Elek <elek@apache.org>
2024-10-14 13:47:56 +0200

    storagenode/run: new lightweight main entrypoint for modular storagenode
    Change-Id: Ifa9997df3b7e042a3c2bebbb89e679ba2885b68b

---
+++ storagenode/run/main.go
@@ -0,0 +1,18 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package root
+
+import (
+	_ "storj.io/storj/private/version" // This attaches version information during release builds.
+	_ "storj.io/storj/web/storagenode" // This embeds storagenode assets.
+)
+
+func Main() {
+	rootCmd, _ := newRootCmd()
+
+	err := rootCmd.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
