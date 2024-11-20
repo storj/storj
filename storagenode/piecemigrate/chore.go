b3670398f2c597d959491d8a1a8895cc23e46526
Artur M. Wolff <artur@storj.io>
2024-11-20 18:24:04 +0100

    storagenode/piecemigrate: actually implement it
    This commit implements the piece migration chore backed by a queue.
    
    Key details:
    - Implements the Migrator interface, allowing pieces to be enqueued for
      migration
    - Runs at a specified interval, checking if global active migration is
      enabled and if specific satellites are configured for migration. If
      so, it retrieves pieces from storage for these satellites and enqueues
      them for migration
    - Processes the migration queue sequentially. If the queue capacity is
      exceeded, items are dropped. These pieces can later be picked up for
      migration if active migration is enabled
    - Deletes pieces from the source backend once they are successfully
      migrated
    
    Updates #7258
    
    Change-Id: If165744f31113ffd0bd8f47171ec928562ee0d7b

--- storagenode/piecemigrate/chore.go
+++ storagenode/piecemigrate/chore.go
@@ -5,25 +5,43 @@ package piecemigrate
 
 import (
 	"context"
-	"fmt"
+	"io/fs"
 	"sync"
 	"time"
 
 	"github.com/spacemonkeygo/monkit/v3"
+	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 	"golang.org/x/exp/maps"
 
+	"storj.io/common/errs2"
+	"storj.io/common/pb"
 	"storj.io/common/storj"
 	"storj.io/common/sync2"
+	"storj.io/storj/storagenode/pieces"
 	"storj.io/storj/storagenode/piecestore"
 )
 
 var mon = monkit.Package()
 
+// Backend is the minimal interface that the old piece backend needs to
+// implement for the migration to work.
+//
+// TODO(artur): make at least OldPieceBackend implement this interface,
+// or give up and just put the pieces' type for the old backend.
+type Backend interface {
+	Writer(context.Context, storj.NodeID, storj.PieceID, pb.PieceHashAlgorithm) (*pieces.Writer, error)
+	Reader(context.Context, storj.NodeID, storj.PieceID) (*pieces.Reader, error)
+	WalkSatellitePieces(context.Context, storj.NodeID, func(pieces.StoredPieceAccess) error) error
+	Delete(context.Context, storj.NodeID, storj.PieceID) error
+}
+
 // Config defines the configuration for the chore.
 type Config struct {
-	BatchSize int           `help:"how many pieces to migrate at once" default:"10000"`
-	Interval  time.Duration `help:"how long to wait between the batches" default:"10m"`
+	BatchSize       int           `help:"how many pieces to migrate at once" default:"10000"`
+	Interval        time.Duration `help:"how long to wait between the batches" default:"10m"`
+	MigrateInactive bool          `help:"whether to also migrate pieces for satellites outside actively migrated" default:"false"`
+	ActiveMigration bool          `help:"whether to perform an active migration while processing queued pieces" default:"false"`
 }
 
 // Chore migrates pieces.
@@ -33,12 +51,14 @@ type Chore struct {
 	log  *zap.Logger
 	Loop *sync2.Cycle
 
-	config   Config
-	old, new piecestore.PieceBackend
+	config Config
+	old    Backend
+	new    piecestore.PieceBackend
 
-	migrationQueue chan migrationItem
-	mu             sync.Mutex
-	active         map[storj.NodeID]struct{}
+	migrationQueue   chan migrationItem
+	baselineDataRate *monkit.FloatVal
+	mu               sync.Mutex
+	active           map[storj.NodeID]struct{}
 }
 
 type migrationItem struct {
@@ -47,7 +67,7 @@ type migrationItem struct {
 }
 
 // NewChore initializes and returns a new Chore instance.
-func NewChore(log *zap.Logger, config Config, old, new piecestore.PieceBackend) *Chore {
+func NewChore(log *zap.Logger, config Config, old Backend, new piecestore.PieceBackend) *Chore {
 	return &Chore{
 		log:  log,
 		Loop: sync2.NewCycle(config.Interval),
@@ -56,8 +76,9 @@ func NewChore(log *zap.Logger, config Config, old, new piecestore.PieceBackend) 
 		old:    old,
 		new:    new,
 
-		migrationQueue: make(chan migrationItem, config.BatchSize),
-		active:         make(map[storj.NodeID]struct{}),
+		migrationQueue:   make(chan migrationItem, config.BatchSize),
+		baselineDataRate: mon.FloatVal("migration_chore"),
+		active:           make(map[storj.NodeID]struct{}),
 	}
 }
 
@@ -84,6 +105,14 @@ func (chore *Chore) SetMigrate(sat storj.NodeID, migrate bool) {
 	}
 }
 
+func (chore *Chore) getMigrate(sat storj.NodeID) bool {
+	chore.mu.Lock()
+	defer chore.mu.Unlock()
+
+	_, ok := chore.active[sat]
+	return ok
+}
+
 // Run starts the chore loop to migrate pieces based on the
 // configuration.
 func (chore *Chore) Run(ctx context.Context) (err error) {
@@ -101,17 +130,164 @@ func (chore *Chore) RunOnce(ctx context.Context) (err error) {
 	chore.mu.Unlock()
 
 	for _, sat := range sats {
-		_ = chore.runSatellite(ctx, sat)
+		n, size, err := chore.runSatellite(ctx, sat)
+
+		chore.log.Info("batch processed",
+			zap.Error(err),
+			zap.Stringer("sat", sat),
+			zap.Int("successes", n),
+			zap.Int64("total", size))
+
+		if err != nil {
+			return err
+		}
 	}
 
+	// NOTE(artur): we will keep going if we have inflight pieces in the
+	// queue, and that's probably fine. however, we will sleep for
+	// chore.config.Interval after a processed batch.
 	return nil
 }
 
-func (chore *Chore) runSatellite(ctx context.Context, sat storj.NodeID) (err error) {
+func (chore *Chore) runSatellite(ctx context.Context, sat storj.NodeID) (n int, total int64, err error) {
 	defer mon.Task()(&ctx)(&err)
 
-	fmt.Println("LOL MIGRATE", sat.String())
-	return nil
+	if chore.config.ActiveMigration {
+		walkCtx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if err := chore.old.WalkSatellitePieces(walkCtx, sat, func(spa pieces.StoredPieceAccess) error {
+			select {
+			case chore.migrationQueue <- migrationItem{satellite: sat, piece: spa.PieceID()}:
+			default:
+				cancel()
+			}
+			return nil
+		}); err != nil && !errs2.IsCanceled(err) {
+			chore.log.Info("couldn't list new pieces to migrate",
+				zap.Error(err),
+				zap.Stringer("sat", sat))
+			// even if we couldn't list new pieces, we might have new
+			// pieces from other sources in the queue now. let's keep
+			// going and process them
+		}
+	}
+
+	for {
+		select {
+		case m := <-chore.migrationQueue:
+			if !chore.config.MigrateInactive && !chore.getMigrate(m.satellite) {
+				chore.log.Debug("skipping a piece that's not part of the active migration",
+					zap.Stringer("active", sat),
+					zap.Stringer("sat", m.satellite),
+					zap.Stringer("id", m.piece))
+				n++
+				continue
+			}
+
+			start := time.Now()
+			if size, err := chore.migrateOne(ctx, m.satellite, m.piece); err != nil {
+				chore.log.Info("couldn't migrate",
+					zap.Error(err),
+					zap.Stringer("sat", m.satellite),
+					zap.Stringer("id", m.piece))
+			} else {
+				d := time.Since(start)
+				chore.log.Debug("migrated a piece",
+					zap.Stringer("sat", m.satellite),
+					zap.Stringer("id", m.piece),
+					zap.Int64("size", size),
+					zap.Duration("took", d))
+				n++
+				total += size
+				// TODO(artur): use chore.baselineDataRate to determine
+				// if we should be going slower
+				chore.baselineDataRate.Observe(float64(size) / d.Seconds())
+			}
+		default:
+			return n, total, nil
+		}
+	}
+}
+
+// migrateOne migrates a piece returning the size of the migrated piece
+// and any error encountered.
+//
+// NOTE/TODO?(artur): there might be a situation where there are two
+// identical items in the queue due to different callers adding to the
+// migration queue. that's okay, but should we proactively check for
+// whether it's already in the new backend to skip the rest of the
+// method? maybe this wouldn't be too frequent, so maybe that's fine.
+func (chore *Chore) migrateOne(ctx context.Context, sat storj.NodeID, piece storj.PieceID) (size int64, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	src, err := chore.old.Reader(ctx, sat, piece)
+	if err != nil {
+		if errs.Is(err, fs.ErrNotExist) {
+			chore.log.Debug("not in the old backend (we might have already processed it)",
+				zap.Stringer("sat", sat),
+				zap.Stringer("id", piece))
+			return 0, nil // not in the old one, so nothing to migrate
+		}
+		return 0, errs.New("opening the old reader: %w", err)
+	}
+	defer func() {
+		// we don't want upstream to think that the piece hasn't been
+		// migrated if we just couldn't close the reader; log it
+		// instead.
+		if errClose := src.Close(); errClose != nil {
+			chore.log.Debug("couldn't close the reader",
+				zap.Error(errClose),
+				zap.Stringer("sat", sat),
+				zap.Stringer("piece", piece))
+		}
+	}()
+
+	hdr, err := src.GetPieceHeader()
+	if err != nil {
+		return 0, errs.New("getting the piece header: %w", err)
+	}
+
+	dst, err := chore.new.Writer(ctx, sat, piece, hdr.HashAlgorithm, hdr.OrderLimit.PieceExpiration)
+	if err != nil {
+		return 0, errs.New("opening the new writer: %w", err)
+	}
+	defer func() {
+		// if it's necessary to cancel the write, it likely means that
+		// committing it was unsuccessful. it's not a big deal if we
+		// cannot cancel it afterward, but just to be aware of it
+		// happening, we're going to log the error, if any.
+		if errCancel := dst.Cancel(ctx); errCancel != nil {
+			chore.log.Debug("couldn't close the writer",
+				zap.Error(errCancel),
+				zap.Stringer("sat", sat),
+				zap.Stringer("piece", piece))
+		}
+	}()
+
+	size, err = sync2.Copy(ctx, dst, src)
+	if err != nil {
+		return 0, errs.New("while copying the piece: %w", err)
+	}
+
+	if sizeSrc, sizeDst := src.Size(), dst.Size(); !allEqual(sizeSrc, size, sizeDst) {
+		return 0, errs.New("size mismatch: source=%d,written=%d,destination=%d", sizeSrc, size, sizeDst)
+	}
+
+	if err = dst.Commit(ctx, hdr); err != nil {
+		return 0, errs.New("committing: %w", err)
+	}
+
+	// after committing, the piece has been successfully migrated; we
+	// can now delete it from the old backend.
+	if err = chore.old.Delete(ctx, sat, piece); err != nil {
+		return 0, errs.New("deleting: %w", err)
+	}
+
+	return size, nil
+}
+
+func allEqual(a, b, c int64) bool {
+	return a == b && b == c
 }
 
 // Close shuts down the chore's loop and releases associated resources.
---
+++ storagenode/piecemigrate/chore_test.go
@@ -0,0 +1,424 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package piecemigrate
+
+import (
+	"bytes"
+	"context"
+	"io/fs"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zeebo/errs"
+	"go.uber.org/zap/zaptest"
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/common/memory"
+	"storj.io/common/pb"
+	"storj.io/common/storj"
+	"storj.io/common/sync2"
+	"storj.io/common/testcontext"
+	"storj.io/common/testrand"
+	"storj.io/storj/storagenode/blobstore/filestore"
+	"storj.io/storj/storagenode/pieces"
+	"storj.io/storj/storagenode/piecestore"
+	"storj.io/storj/storagenode/retain"
+)
+
+func TestChoreWithPassiveMigrationOnly(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	log := zaptest.NewLogger(t)
+	defer ctx.Check(log.Sync)
+
+	dir, err := filestore.NewDir(log, t.TempDir())
+	require.NoError(t, err)
+
+	blobs := filestore.New(log, dir, filestore.DefaultConfig)
+	defer ctx.Check(blobs.Close)
+
+	fw := pieces.NewFileWalker(log, blobs, nil, nil, nil)
+
+	bfm, err := retain.NewBloomFilterManager(t.TempDir(), 0)
+	require.NoError(t, err)
+
+	rtm := retain.NewRestoreTimeManager(t.TempDir())
+
+	old := pieces.NewStore(log, fw, nil, blobs, nil, nil, pieces.DefaultConfig)
+	new := piecestore.NewHashStoreBackend(t.TempDir(), bfm, rtm, log)
+
+	satellites1 := randomSatsPieces(2, 100)
+	writeSatsPieces(ctx, t, old, satellites1)
+	satellites2 := randomSatsPieces(2, 100)
+	writeSatsPieces(ctx, t, old, satellites2)
+	satellites3 := randomSatsPieces(2, 100)
+	writeSatsPieces(ctx, t, old, satellites3)
+
+	config := Config{
+		BatchSize:       1000,
+		Interval:        100 * time.Millisecond,
+		MigrateInactive: true,
+		ActiveMigration: false,
+	}
+
+	chore := NewChore(log, config, old, new)
+	group := errgroup.Group{}
+	group.Go(func() error { return chore.Run(ctx) })
+	defer ctx.Check(group.Wait)
+	defer ctx.Check(chore.Close)
+
+	for sat := range satellites1 {
+		chore.SetMigrate(sat, true)
+	}
+	for sat := range satellites2 {
+		chore.SetMigrate(sat, false) // explicitly excluded
+	}
+
+	for sat, pieces := range satellites2 {
+		for _, p := range pieces {
+			chore.TryMigrateOne(sat, p.id)
+		}
+	}
+	for sat, pieces := range satellites3 {
+		for _, p := range pieces {
+			chore.TryMigrateOne(sat, p.id)
+		}
+	}
+
+	waitUntilMigrationFinished(ctx, t, old, satellites2)
+	waitUntilMigrationFinished(ctx, t, old, satellites3)
+
+	// migration complete! let's check if the new backend contains what
+	// we migrated to it:
+	for sat, pieces := range satellites2 {
+		for _, p := range pieces {
+			readFromBackend(ctx, t, new, sat, p)
+		}
+	}
+	for sat, pieces := range satellites3 {
+		for _, p := range pieces {
+			readFromBackend(ctx, t, new, sat, p)
+		}
+	}
+	for sat, pieces := range satellites2 {
+		for _, p := range pieces {
+			require.False(t, existsInStore(ctx, t, old, sat, p.id))
+		}
+	}
+	for sat, pieces := range satellites3 {
+		for _, p := range pieces {
+			require.False(t, existsInStore(ctx, t, old, sat, p.id))
+		}
+	}
+	// check that what we didn't want to migrate is still in place:
+	for sat, pieces := range satellites1 {
+		for _, p := range pieces {
+			require.False(t, existsInBackend(ctx, t, new, sat, p.id))
+		}
+	}
+	for sat, pieces := range satellites1 {
+		for _, p := range pieces {
+			readFromStore(ctx, t, old, sat, p)
+		}
+	}
+}
+
+func TestChoreActiveWithPassiveMigration(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	log := zaptest.NewLogger(t)
+	defer ctx.Check(log.Sync)
+
+	dir, err := filestore.NewDir(log, t.TempDir())
+	require.NoError(t, err)
+
+	blobs := filestore.New(log, dir, filestore.DefaultConfig)
+	defer ctx.Check(blobs.Close)
+
+	fw := pieces.NewFileWalker(log, blobs, nil, nil, nil)
+
+	bfm, err := retain.NewBloomFilterManager(t.TempDir(), 0)
+	require.NoError(t, err)
+
+	rtm := retain.NewRestoreTimeManager(t.TempDir())
+
+	old := pieces.NewStore(log, fw, nil, blobs, nil, nil, pieces.DefaultConfig)
+	new := piecestore.NewHashStoreBackend(t.TempDir(), bfm, rtm, log)
+
+	migratedSatellites := randomSatsPieces(3, 1000)
+	migratedSatellitesMu := sync.Mutex{}
+	writeSatsPieces(ctx, t, old, migratedSatellites)
+
+	excludedSatellites1 := randomSatsPieces(1, 1000)
+	writeSatsPieces(ctx, t, old, excludedSatellites1)
+	excludedSatellites2 := randomSatsPieces(1, 1000)
+	writeSatsPieces(ctx, t, old, excludedSatellites2)
+	excludedSatellites3 := randomSatsPieces(1, 1000)
+	writeSatsPieces(ctx, t, old, excludedSatellites3)
+
+	config := Config{
+		BatchSize:       100,
+		Interval:        100 * time.Millisecond,
+		MigrateInactive: false,
+		ActiveMigration: true,
+	}
+
+	chore := NewChore(log, config, old, new)
+	group := errgroup.Group{}
+	group.Go(func() error { return chore.Run(ctx) })
+	defer ctx.Check(group.Wait)
+	defer ctx.Check(chore.Close)
+
+	passiveCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	group.Go(func() error { // mimick passive migration
+		for {
+			select {
+			case <-passiveCtx.Done():
+				return nil
+			default:
+				for sat, pieces := range excludedSatellites1 {
+					max := testrand.Intn(3)
+					for i, p := range pieces {
+						if i == max {
+							break
+						}
+						chore.TryMigrateOne(sat, p.id)
+						time.Sleep(time.Duration(testrand.Int63n(10)) * time.Millisecond)
+					}
+				}
+				for sat, pieces := range excludedSatellites2 {
+					max := testrand.Intn(5)
+					for i, p := range pieces {
+						if i == max {
+							break
+						}
+						chore.TryMigrateOne(sat, p.id)
+						time.Sleep(time.Duration(testrand.Int63n(20)) * time.Millisecond)
+					}
+				}
+				migratedSatellitesMu.Lock()
+				for sat, pieces := range migratedSatellites {
+					max := testrand.Intn(8)
+					for i, p := range pieces {
+						if i == max {
+							break
+						}
+						chore.TryMigrateOne(sat, p.id)
+						time.Sleep(time.Duration(testrand.Int63n(30)) * time.Millisecond)
+					}
+				}
+				migratedSatellitesMu.Unlock()
+				for sat, pieces := range excludedSatellites3 {
+					max := testrand.Intn(13)
+					for i, p := range pieces {
+						if i == max {
+							break
+						}
+						chore.TryMigrateOne(sat, p.id)
+						time.Sleep(time.Duration(testrand.Int63n(40)) * time.Millisecond)
+					}
+				}
+			}
+		}
+	})
+
+	for sat := range migratedSatellites {
+		chore.SetMigrate(sat, true)
+	}
+	for sat := range excludedSatellites1 { // explicitly excluded
+		chore.SetMigrate(sat, false)
+	}
+
+	waitUntilMigrationFinished(ctx, t, old, migratedSatellites)
+
+	// excludedSatellites3 are no longer excluded:
+	for sat, pieces := range excludedSatellites3 {
+		chore.SetMigrate(sat, true)
+		migratedSatellitesMu.Lock()
+		migratedSatellites[sat] = pieces
+		migratedSatellitesMu.Unlock()
+	}
+
+	waitUntilMigrationFinished(ctx, t, old, migratedSatellites)
+
+	// migration complete! let's check if the new backend contains what
+	// we migrated to it:
+	for sat, pieces := range migratedSatellites {
+		for _, p := range pieces {
+			readFromBackend(ctx, t, new, sat, p)
+		}
+	}
+	for sat, pieces := range migratedSatellites {
+		for _, p := range pieces {
+			require.False(t, existsInStore(ctx, t, old, sat, p.id))
+		}
+	}
+	// check that what we didn't want to migrate is still in place:
+	for sat, pieces := range excludedSatellites1 {
+		for _, p := range pieces {
+			require.False(t, existsInBackend(ctx, t, new, sat, p.id))
+		}
+	}
+	for sat, pieces := range excludedSatellites1 {
+		for _, p := range pieces {
+			readFromStore(ctx, t, old, sat, p)
+		}
+	}
+	for sat, pieces := range excludedSatellites2 {
+		for _, p := range pieces {
+			require.False(t, existsInBackend(ctx, t, new, sat, p.id))
+		}
+	}
+	for sat, pieces := range excludedSatellites2 {
+		for _, p := range pieces {
+			readFromStore(ctx, t, old, sat, p)
+		}
+	}
+}
+
+// TODO(artur): there's a lot of duplication among the helper functions.
+// Making sure that OldPieceBackend implements PieceMigrateBackend would
+// allow getting rid of that.
+
+type pieceToCheck struct {
+	sat      storj.NodeID
+	id       storj.PieceID
+	content  []byte
+	hashAlgo pb.PieceHashAlgorithm
+	hash     []byte
+}
+
+func randomSatsPieces(n, nPieces int) map[storj.NodeID][]*pieceToCheck {
+	ret := make(map[storj.NodeID][]*pieceToCheck)
+
+	for i := 0; i < n; i++ {
+		id := testrand.NodeID()
+
+		var pieces []*pieceToCheck
+		for j := 0; j < nPieces; j++ {
+			pieces = append(pieces, &pieceToCheck{
+				sat:      id,
+				id:       testrand.PieceID(),
+				content:  testrand.Bytes(memory.Size(testrand.Intn(10)) * memory.KB),
+				hashAlgo: pb.PieceHashAlgorithm_SHA256,
+			})
+		}
+
+		ret[id] = pieces
+	}
+
+	return ret
+}
+
+func writeSatsPieces(ctx context.Context, t *testing.T, store *pieces.Store, satsPieces map[storj.NodeID][]*pieceToCheck) {
+	for sat, pieces := range satsPieces {
+		for _, p := range pieces {
+			writeToStore(ctx, t, store, sat, p)
+		}
+	}
+}
+
+func writeToStore(ctx context.Context, t *testing.T, store *pieces.Store, sat storj.NodeID, piece *pieceToCheck) {
+	w, err := store.Writer(ctx, sat, piece.id, piece.hashAlgo)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, w.Cancel(ctx)) }()
+
+	n, err := sync2.Copy(ctx, w, bytes.NewReader(piece.content))
+	require.NoError(t, err)
+
+	require.Equal(t, len(piece.content), int(n))
+	require.Equal(t, len(piece.content), int(w.Size()))
+
+	piece.hash = w.Hash()
+
+	require.NoError(t, w.Commit(ctx, &pb.PieceHeader{Hash: w.Hash()}))
+}
+
+func readFromStore(ctx context.Context, t *testing.T, store *pieces.Store, sat storj.NodeID, piece *pieceToCheck) {
+	r, err := store.Reader(ctx, sat, piece.id)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, r.Close()) }()
+
+	hdr, err := r.GetPieceHeader()
+	require.NoError(t, err)
+	require.Equal(t, piece.hashAlgo, hdr.HashAlgorithm)
+	require.Equal(t, piece.hash, hdr.Hash)
+
+	b := bytes.NewBuffer(nil)
+
+	n, err := sync2.Copy(ctx, b, r)
+	require.NoError(t, err)
+
+	require.Equal(t, len(piece.content), int(n))
+	require.Equal(t, len(piece.content), int(r.Size()))
+
+	require.Equal(t, piece.content, b.Bytes())
+}
+
+func readFromBackend(ctx context.Context, t *testing.T, backend piecestore.PieceBackend, sat storj.NodeID, piece *pieceToCheck) {
+	r, err := backend.Reader(ctx, sat, piece.id)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, r.Close()) }()
+
+	hdr, err := r.GetPieceHeader()
+	require.NoError(t, err)
+	require.Equal(t, piece.hashAlgo, hdr.HashAlgorithm)
+	require.Equal(t, piece.hash, hdr.Hash)
+
+	b := bytes.NewBuffer(nil)
+
+	n, err := sync2.Copy(ctx, b, r)
+	require.NoError(t, err)
+
+	require.Equal(t, len(piece.content), int(n))
+	require.Equal(t, len(piece.content), int(r.Size()))
+
+	require.Equal(t, piece.content, b.Bytes())
+}
+
+func existsInStore(ctx context.Context, t *testing.T, store *pieces.Store, sat storj.NodeID, piece storj.PieceID) bool {
+	r, err := store.Reader(ctx, sat, piece)
+	if err != nil {
+		if errs.Is(err, fs.ErrNotExist) {
+			return false
+		}
+		require.NoError(t, err)
+	}
+	defer func() { require.NoError(t, r.Close()) }()
+	return true
+}
+
+func existsInBackend(ctx context.Context, t *testing.T, backend piecestore.PieceBackend, sat storj.NodeID, piece storj.PieceID) bool {
+	r, err := backend.Reader(ctx, sat, piece)
+	if err != nil {
+		if errs.Is(err, fs.ErrNotExist) {
+			return false
+		}
+		require.NoError(t, err)
+	}
+	defer func() { require.NoError(t, r.Close()) }()
+	return true
+}
+
+func waitUntilMigrationFinished(ctx context.Context, t *testing.T, store *pieces.Store, satsPieces map[storj.NodeID][]*pieceToCheck) {
+	for done := false; !done; {
+		var count int
+		for sat := range satsPieces {
+			var c int
+			require.NoError(t, store.WalkSatellitePieces(ctx, sat, func(spa pieces.StoredPieceAccess) error {
+				c++
+				return nil
+			}))
+			t.Logf("%d left to migrate for %s", c, sat)
+			count += c
+		}
+		done = count == 0
+		time.Sleep(time.Second)
+	}
+}
