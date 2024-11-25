f4cfbd44243e291f26d1c70b1a525d439b5aad29
Márton Elek <elek@apache.org>
2024-11-25 12:31:36 +0100

    satellite/run: modular executor for satellite
    Change-Id: I4f8bd7b369b0b698b5ebbf0b5eaead392759de95

---
+++ satellite/run/mud.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package root
+
+import (
+	"storj.io/storj/private/mud"
+	"storj.io/storj/satellite"
+	"storj.io/storj/satellite/satellitedb"
+	"storj.io/storj/shared/modular"
+	"storj.io/storj/shared/modular/logger"
+)
+
+// Module registers all the possible components for the satellite instance.
+func Module(ball *mud.Ball) {
+	logger.Module(ball)
+	modular.IdentityModule(ball)
+	satellitedb.Module(ball)
+	satellite.Module(ball)
+}
