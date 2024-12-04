7c2b527bdd9f9ec955f1988770fa148bd3b21d11
Márton Elek <elek@apache.org>
2024-12-04 09:13:56 +0100

    satellite/audit: support full piece files with the dedicated audit executor
    Change-Id: I62e44ce181b9d8d2c3c7b3668c0f90a688dac003

--- satellite/audit/verifier.go
+++ satellite/audit/verifier.go
@@ -107,11 +107,6 @@ func (verifier *Verifier) Verify(ctx context.Context, segment Segment, skip map[
 		recordStats(report, len(segmentInfo.Pieces), err)
 	}()
 
-	if segment.Expired(verifier.nowFn()) {
-		verifier.log.Debug("segment expired before Verify")
-		return Report{}, nil
-	}
-
 	segmentInfo, err = verifier.metabase.GetSegmentByPosition(ctx, metabase.GetSegmentByPosition{
 		StreamID: segment.StreamID,
 		Position: segment.Position,
@@ -122,6 +117,10 @@ func (verifier *Verifier) Verify(ctx context.Context, segment Segment, skip map[
 			return Report{}, nil
 		}
 		return Report{}, err
+	}
+	if segmentInfo.Expired(verifier.nowFn()) {
+		verifier.log.Debug("segment expired before Verify")
+		return Report{}, nil
 	}
 
 	randomIndex, err := GetRandomStripe(ctx, segmentInfo)
