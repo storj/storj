cb515d48b0312e6fcc0f20bcd4bbe2b30a267a08
Paul Willoughby <pwilloughby@storj.io>
2025-03-14 17:04:01 -0600

    satellite/metainfo/bloomrate: convert to token bucket design
    Change-Id: I34a0325a218a78d090361687e38fcf96dcc576ea

--- satellite/metainfo/bloomrate/rate.go
+++ satellite/metainfo/bloomrate/rate.go
@@ -10,24 +10,18 @@ import (
 	"golang.org/x/time/rate"
 )
 
-// Rate is a rate limiter that uses a single uint64 word. The 10 least
-// significant bits represent a counter that allows for burst behavior. As long
-// as the current limiter hasn't expired, the counter is allowed to climb to
-// the burst limit.
+// Rate is a token bucket rate limiter that uses a single uint64 word. The
+// 10 least significant bits represent tokens that allow for burst behavior.
 // The remaining 54 significant bits are a timestamp (treated as positive
 // nanoseconds from Unix epoch, with the 9 least significant bits masked off,
 // for a loss of granularity of about half a microsecond). This rate limiter
 // cannot be configured to allow rates higher than 1,956,947 events/second.
-// This timestamp is when the current counter expires. The expiry is bumped
-// every time the counter is bumped.
+// This timestamp is when the current tokens were modified. The expiry is bumped
+// every time the tokens are changed.
 // The zero value is valid and works as if no traffic has yet arrived.
 // Note that the burst logic is not exact - there might be a slightly higher
 // amount of requests let through than the provided burst limit, but not in
 // the long term.
-// Also note that this is not a token bucket design - the burst amount is
-// allowed per time window, and the limit remains hit until the time window
-// expires, at which point the burst limit is allowed through again. For this
-// reason, smaller burst amounts are better than larger burst amounts.
 type Rate atomic.Uint64
 
 const (
@@ -40,45 +34,51 @@ const (
 // that limit and burst are consistent and don't change across calls to Allow,
 // but are stored elsewhere (see BloomRate).
 func (r *Rate) Allow(now time.Time, limit rate.Limit, burst int) bool {
-	// how much should the expiry get bumped if we bump the counter?
+	// how much should the expiry get bumped for each token?
 	timePerOperation := time.Duration(float64(time.Second) / float64(limit))
 
 	// parse the value
 	val := (*atomic.Uint64)(r).Load()
 	expiry := time.Unix(0, int64((val&timeMask)>>1))
+	tokens := int(val & burstMask)
 
-	// has the counter expired?
-	if now.Before(expiry) {
-		// no. is the count under the burst limit?
-		if int(val&burstMask) < burst {
-			// success. bump the expiry and the counter and allow this through.
-			(*atomic.Uint64)(r).Add(1 | ((uint64(timePerOperation) << 1) & timeMask))
-			// N.B. - we bumped the expiry and the counter. perhaps we could improve
-			// this rate limiting algorithm if we checked the new return value to see
-			// if anyone else raced with us.
-			return true
+	// fill token bucket
+	if now.Sub(expiry) > timePerOperation {
+		tokens = min(tokens+int(now.Sub(expiry)/timePerOperation), burst)
+
+		// calculate new expiry in timePerOperation steps to reduce incremental error
+		if tokens == burst {
+			expiry = now
+		} else {
+			expiry = expiry.Add(time.Duration(timePerOperation.Nanoseconds() * (now.Sub(expiry) / timePerOperation).Nanoseconds()))
 		}
-		// no, we've hit the counter limit.
+	}
+	if tokens == 0 {
 		return false
 	}
 
-	// the counter is expired. let's store a brand new expiry and count of 1.
 	if (*atomic.Uint64)(r).CompareAndSwap(val,
-		1|((uint64(now.Add(timePerOperation).UnixNano())<<1)&timeMask)) {
+		uint64(tokens-1)|((uint64(expiry.UnixNano())<<1)&timeMask)) {
 		// no one raced us. it's done!
 		return true
 	}
 
-	// someone raced us. this means there's a new timestamp in there. do bursts
-	// allow for more than 1 at a time? it's too bad CompareAndSwap doesn't
+	// someone raced us. this means there's a new timestamp in there. is there
+	// more than 1 token? it's too bad CompareAndSwap doesn't
 	// return the value you raced with.
-	if burst <= 1 {
-		// no, we only do one event per expiry.
+	if tokens <= 1 {
+		// no, someone else used our token
 		return false
 	}
 
-	// we do more than one event per expiry. like before, we are going to just
-	// bump the expiry and the counter indiscriminately and hope for the best.
-	(*atomic.Uint64)(r).Add(1 | ((uint64(timePerOperation) << 1) & timeMask))
+	// we are going to just store the expiry and the tokens indiscriminately and hope for the best.
+	(*atomic.Uint64)(r).Store(uint64(tokens-2) | ((uint64(expiry.UnixNano()) << 1) & timeMask))
 	return true
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
--- satellite/metainfo/bloomrate/rate_test.go
+++ satellite/metainfo/bloomrate/rate_test.go
@@ -30,8 +30,10 @@ func TestRate(t *testing.T) {
 	assert(t, !r.Allow(now.Add(3*time.Millisecond), rate.Every(time.Second), 3))
 	assert(t, !r.Allow(now.Add(4*time.Millisecond), rate.Every(time.Second), 3))
 	assert(t, !r.Allow(now.Add(5*time.Millisecond), rate.Every(time.Second), 3))
-	assert(t, !r.Allow(now.Add(time.Second), rate.Every(time.Second), 3))
-	assert(t, !r.Allow(now.Add(2*time.Second), rate.Every(time.Second), 3))
+	// should have filled after a second
+	assert(t, r.Allow(now.Add(time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(2*time.Second), rate.Every(time.Second), 3))
+	// shy a microsecond, should get rejected
 	assert(t, !r.Allow(now.Add(3*time.Second-time.Microsecond), rate.Every(time.Second), 3))
 
 	// the counter should have expired by 3 seconds. make sure many requests can
@@ -41,21 +43,24 @@ func TestRate(t *testing.T) {
 	assert(t, r.Allow(now.Add(5*time.Second), rate.Every(time.Second), 3))
 	assert(t, r.Allow(now.Add(6*time.Second), rate.Every(time.Second), 3))
 
-	// increase the rate and make sure we fail after 3
-	assert(t, r.Allow(now.Add(7*time.Second), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(7*time.Second+time.Second/2), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(8*time.Second), rate.Every(time.Second), 3))
-	assert(t, !r.Allow(now.Add(8*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	// allow to fill then increase the rate and make sure we fail
+	assert(t, r.Allow(now.Add(9*time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(9*time.Second+time.Second/3), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(9*time.Second+2*time.Second/3), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(10*time.Second), rate.Every(time.Second), 3))
+	assert(t, !r.Allow(now.Add(10*time.Second+time.Second/3), rate.Every(time.Second), 3))
 
 	// try a rate that is bursty but overall under the limit
-	assert(t, r.Allow(now.Add(10*time.Second), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(10*time.Second+time.Second/2), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(12*time.Second), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(12*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(13*time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(13*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(15*time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(15*time.Second+time.Second/2), rate.Every(time.Second), 3))
 
 	// make sure it caps us if we go over.
-	assert(t, r.Allow(now.Add(14*time.Second), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(14*time.Second+time.Second/2), rate.Every(time.Second), 3))
-	assert(t, r.Allow(now.Add(15*time.Second), rate.Every(time.Second), 3))
-	assert(t, !r.Allow(now.Add(15*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(18*time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(18*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(19*time.Second), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(19*time.Second+time.Second/2), rate.Every(time.Second), 3))
+	assert(t, r.Allow(now.Add(20*time.Second), rate.Every(time.Second), 3))
+	assert(t, !r.Allow(now.Add(20*time.Second+time.Second/2), rate.Every(time.Second), 3))
 }
