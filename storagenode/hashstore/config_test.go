6a8be1ae3ad9c898e097f238fc92774fe637b315
Márton Elek <elek@apache.org>
2025-03-28 10:25:59 +0100

    storagenode/hashstore: make ConfigTest os agnostic
    As Egon reported, the test is failing on Windows due to different separator:
    
    https://review.dev.storj.tools/c/storj/storj/+/16334?tab=comments
    
    > This test is failing on windows with:
    
    ```
    
    Change-Id: Iaa84207c2d95f024a9ecb34432439d9f45d1a590
    --- FAIL: TestConfig (0.00s)
        config_test.go:18:
                    Error Trace:    s:/core/storj/storagenode/hashstore/config_test.go:18
                    Error:          Not equal:
                                    expected: "/path/to/storage/logs"
                                    actual  : "\\path\\to\\storage\\logs"
    
                                    Diff:
                                    --- Expected
                                    +++ Actual
                                    @@ -1 +1 @@
                                    -/path/to/storage/logs
                                    +\path\to\storage\logs
                    Test:           TestConfig
    FAIL
    exit status 1
    FAIL    storj.io/storj/storagenode/hashstore    1.160s
    ```
    
    Change-Id: I0495cd27c0d6c5479a70a309b37d1edc5bcb9ade

--- storagenode/hashstore/config_test.go
+++ storagenode/hashstore/config_test.go
@@ -4,25 +4,44 @@
 package hashstore
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestConfig(t *testing.T) {
-	c := Config{
-		LogsPath:  "logs",
-		TablePath: "tables",
-	}
-	logs, table := c.Directories("/path/to/storage")
-	require.Equal(t, "/path/to/storage/logs", logs)
-	require.Equal(t, "/path/to/storage/tables", table)
 
-	c = Config{
-		LogsPath:  "/logs",
-		TablePath: "/tables",
-	}
-	logs, table = c.Directories("/path/to/storage")
-	require.Equal(t, "/logs", logs)
-	require.Equal(t, "/tables", table)
+	baseDir := filepath.FromSlash("/path/to/storage")
+
+	t.Run("relative dirs", func(t *testing.T) {
+		c := Config{
+			LogsPath:  "logs",
+			TablePath: "tables",
+		}
+		logs, table := c.Directories(baseDir)
+		require.Equal(t, filepath.Join(baseDir, "logs"), logs)
+		require.Equal(t, filepath.Join(baseDir, "tables"), table)
+
+	})
+
+	t.Run("absolute logs path", func(t *testing.T) {
+		pwd, err := os.Getwd()
+		require.NoError(t, err)
+
+		absLogs, err := filepath.Abs(filepath.Join(pwd, "logs"))
+		require.NoError(t, err)
+
+		absTables, err := filepath.Abs(filepath.Join(pwd, "tables"))
+		require.NoError(t, err)
+
+		c := Config{
+			LogsPath:  absLogs,
+			TablePath: absTables,
+		}
+		logs, table := c.Directories(baseDir)
+		require.Equal(t, absLogs, logs)
+		require.Equal(t, absTables, table)
+	})
 }
