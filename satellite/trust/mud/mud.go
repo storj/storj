e7d7c087c6cd8b8e8cb94c1fa53934abea6fdaca
Márton Elek <elek@apache.org>
2025-04-30 10:29:31 +0200

    satellite/api: revert back from using dedicated type for node ID list
    Looks like the values are not injected properly to NodeIDList.
    
    Not clear why. The (pflag.Value).Set is called and the valus are set to FlagSet. But somehow it's not connected to the struct value.
    
    This reverts commit d327c47f4ff842e02f0844d5ba8b832a67336bc0.
    
    Change-Id: I9bb7545951d33ea6dd5d1cf085ebed2976a60eb9

--- satellite/trust/mud/mud.go
+++ satellite/trust/mud/mud.go
@@ -6,8 +6,10 @@ package trust_mud
 // TODO: this package is separated as we have circular dependencies between due to the usage of metainfo.Config.
 
 import (
+	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 
+	"storj.io/common/storj"
 	"storj.io/storj/satellite/metainfo"
 	"storj.io/storj/satellite/trust"
 	"storj.io/storj/shared/mud"
@@ -16,7 +18,15 @@ import (
 // Module is a mud Module definition.
 func Module(ball *mud.Ball) {
 	mud.Provide[*trust.TrustedPeersList](ball, func(logger *zap.Logger, config metainfo.Config) (*trust.TrustedPeersList, error) {
-		return trust.NewTrustedPeerList(config.SuccessTrackerTrustedUplinks), nil
+		var uplinks []storj.NodeID
+		for _, u := range config.SuccessTrackerTrustedUplinks {
+			nodeID, err := storj.NodeIDFromString(u)
+			if err != nil {
+				return nil, errs.Wrap(err)
+			}
+			uplinks = append(uplinks, nodeID)
+		}
+		return trust.NewTrustedPeerList(uplinks), nil
 	})
 
 }
