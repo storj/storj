3dbb910639515f4589adef4adee84c5a8cbb3a9d
Ivan Fraixedes <ivan@fraixed.es>
2025-06-02 12:44:57 +0200

    satellite/metabase: Add error message for context
    Add an error message to the each of the 2 queries that we execute in
    Spanner for deleting bucket with objects to distinguish what query
    returned the error.
    
    Change-Id: I8924953b0e5f742c52981913a0c9b397804cb473

--- satellite/metabase/delete_bucket.go
+++ satellite/metabase/delete_bucket.go
@@ -165,7 +165,7 @@ func (s *SpannerAdapter) DeleteAllBucketObjects(ctx context.Context, opts Delete
 			return nil
 		})
 		if err != nil {
-			return Error.Wrap(err)
+			return Error.New("delete objects query error: %w", err)
 		}
 		if len(deleteSegments) == 0 {
 			return nil
@@ -173,7 +173,7 @@ func (s *SpannerAdapter) DeleteAllBucketObjects(ctx context.Context, opts Delete
 
 		err = tx.BufferWrite(deleteSegments)
 		if err != nil {
-			return err
+			return Error.New("delete segments query error: %w", err)
 		}
 		return nil
 	}, spanner.TransactionOptions{
