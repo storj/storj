b91994ae5e08a248623c4c7c356d3b0be2e891cb
Vitalii <takur12345@gmail.com>
2025-06-10 21:49:38 +0300

    satellite/console: set a minimum of 10 USD for adding funds
    Issue: https://github.com/storj/storj/issues/7490
    
    Change-Id: I8aca7cec1af04efbf5917ec9119dedb7b21f6662

--- satellite/console/config.go
+++ satellite/console/config.go
@@ -29,7 +29,7 @@ type Config struct {
 	ZkSyncContractAddress             string                    `help:"the STORJ zkSync Era contract address" default:"0xA0806DA7835a4E63dB2CE44A2b622eF8b73B5DB5"`
 	BillingFeaturesEnabled            bool                      `help:"indicates if billing features should be enabled" default:"true"`
 	MaxAddFundsAmount                 int                       `help:"maximum amount (in cents) allowed to be added to an account balance." default:"10000"`
-	MinAddFundsAmount                 int                       `help:"minimum amount (in cents) allowed to be added to an account balance." default:"100"`
+	MinAddFundsAmount                 int                       `help:"minimum amount (in cents) allowed to be added to an account balance." default:"1000"`
 	SignupActivationCodeEnabled       bool                      `help:"indicates whether the whether account activation is done using activation code" default:"true" testDefault:"false" devDefault:"false"`
 	FreeTrialDuration                 time.Duration             `help:"duration for which users can access the system free of charge, 0 = unlimited time trial" default:"0"`
 	VarPartners                       []string                  `help:"list of partners whose users will not see billing UI." default:""`
