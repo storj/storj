5cb44f053215daaf4dd08fb31f28b83c1b710c8f
Egon Elbre <egonelbre@gmail.com>
2025-06-13 21:58:54 +0300

    cmd/uplink: add `rb --bypass-governance-retention --force`
    Change-Id: Icf2969e5e9be1aefd60b1b29dfe2c07eb5081e7b

--- cmd/uplink/cmd_rb.go
+++ cmd/uplink/cmd_rb.go
@@ -13,13 +13,15 @@ import (
 
 	"storj.io/storj/cmd/uplink/ulext"
 	"storj.io/storj/cmd/uplink/ulloc"
+	"storj.io/uplink/private/bucket"
 )
 
 type cmdRb struct {
 	ex ulext.External
 
-	access string
-	force  bool
+	access           string
+	force            bool
+	bypassGovernance bool
 
 	loc ulloc.Location
 }
@@ -31,6 +33,9 @@ func newCmdRb(ex ulext.External) *cmdRb {
 func (c *cmdRb) Setup(params clingy.Parameters) {
 	c.access = params.Flag("access", "Access name or value to use", "").(string)
 	c.force = params.Flag("force", "Deletes any objects in bucket first", false,
+		clingy.Transform(strconv.ParseBool), clingy.Boolean,
+	).(bool)
+	c.bypassGovernance = params.Flag("bypass-governance-retention", "Bypass Object Lock governance mode restrictions", false,
 		clingy.Transform(strconv.ParseBool), clingy.Boolean,
 	).(bool)
 
@@ -48,7 +53,7 @@ func (c *cmdRb) Execute(ctx context.Context) (err error) {
 	}
 	defer func() { _ = project.Close() }()
 
-	bucket, key, ok := c.loc.RemoteParts()
+	bucketName, key, ok := c.loc.RemoteParts()
 	if !ok {
 		return errs.New("location must be remote")
 	}
@@ -56,15 +61,17 @@ func (c *cmdRb) Execute(ctx context.Context) (err error) {
 		return errs.New("key must not be specified: %q", key)
 	}
 
-	if c.force {
-		_, err = project.DeleteBucketWithObjects(ctx, bucket)
+	if c.force && c.bypassGovernance {
+		_, err = bucket.DeleteBucketWithObjectsBypassGovernanceRetention(ctx, project, bucketName)
+	} else if c.force {
+		_, err = project.DeleteBucketWithObjects(ctx, bucketName)
 	} else {
-		_, err = project.DeleteBucket(ctx, bucket)
+		_, err = project.DeleteBucket(ctx, bucketName)
 	}
 	if err != nil {
 		return err
 	}
 
-	_, _ = fmt.Fprintf(clingy.Stdout(ctx), "Bucket %q has been deleted.\n", bucket)
+	_, _ = fmt.Fprintf(clingy.Stdout(ctx), "Bucket %q has been deleted.\n", bucketName)
 	return nil
 }
