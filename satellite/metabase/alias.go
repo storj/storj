aebb5b23b5e2045dd4ec6e7c9346c09d74063a48
Michał Niewrzał <michal@storj.io>
2025-08-21 12:46:35 +0200

    satellite/metabase: use Spanner Read API for ListNodeAliases
    Change-Id: I2fe57161cda6ce295ede2d03c66ccb8389649303

--- satellite/metabase/alias.go
+++ satellite/metabase/alias.go
@@ -168,10 +168,9 @@ func (s *SpannerAdapter) ListNodeAliases(ctx context.Context) (aliases []NodeAli
 	defer mon.Task()(&ctx)(&err)
 
 	return spannerutil.CollectRows(
-		s.client.Single().QueryWithOptions(ctx,
-			spanner.Statement{SQL: `
-				SELECT node_id, node_alias FROM node_aliases
-			`}, spanner.QueryOptions{RequestTag: "list-node-aliases"}),
+		s.client.Single().ReadWithOptions(ctx, "node_aliases", spanner.AllKeys(), []string{"node_id", "node_alias"}, &spanner.ReadOptions{
+			RequestTag: "list-node-aliases",
+		}),
 		func(row *spanner.Row, item *NodeAliasEntry) error {
 			return Error.Wrap(row.Columns(&item.ID, spannerutil.Int(&item.Alias)))
 		})
