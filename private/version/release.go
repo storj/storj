a77d09d38b8047cbeedace338c69d52f4701f08e
Andrii Kotko <andriikotko@gmail.com>
2025-09-03 10:45:52 +0300

    release v1.137.0-rc

--- private/version/release.go
+++ private/version/release.go
@@ -6,16 +6,16 @@ package version
 import _ "unsafe" // needed for go:linkname
 
 //go:linkname buildTimestamp storj.io/common/version.buildTimestamp
-var buildTimestamp string
+var buildTimestamp string = "1756885550"
 
 //go:linkname buildCommitHash storj.io/common/version.buildCommitHash
-var buildCommitHash string
+var buildCommitHash string = "47f862b4eef5dd23fed242539f4225f32dc6e305"
 
 //go:linkname buildVersion storj.io/common/version.buildVersion
-var buildVersion string
+var buildVersion string = "v1.137.0-rc"
 
 //go:linkname buildRelease storj.io/common/version.buildRelease
-var buildRelease string
+var buildRelease string = "true"
 
 // ensure that linter understands that the variables are being used.
 func init() { use(buildTimestamp, buildCommitHash, buildVersion, buildRelease) }
