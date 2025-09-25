796fc9f3dba53fd810c9a8e036264d5fc4da25e4
Márton Elek <elek@apache.org>
2025-09-25 10:54:09 +0200

    cmd/storagenode: remove the experimental exec subcommand
    Earlier this was the way to start the modular storagenode.
    
    Today it's more reliable to use the dedicated binary (./storagenode/storagenode), as it can do proper configuration loading which is properly separated (without loading global variables)
    
    Change-Id: If8d07d2d67382a9eff7ffcab7925dad71e6abf8a

--- cmd/storagenode/root.go
+++ cmd/storagenode/root.go
@@ -55,7 +55,6 @@ func newRootCmd(setDefaults bool) (*cobra.Command, *Factory) {
 		newDashboardCmd(factory),
 		newDiagCmd(factory),
 		newRunCmd(factory),
-		newExecCmd(factory),
 		newNodeInfoCmd(factory),
 		newIssueAPIKeyCmd(factory),
 		newGracefulExitInitCmd(factory),
