0d02c63101bf7e3d9ac3dde15c5a89290d3d3441
Márton Elek <elek@apache.org>
2025-10-06 10:46:32 +0200

    satellite/eventing: more robust changestream processor
     1. topic merges are supported (deduplication)
     2. all possible changestreams are watched parallel
    
    Change-Id: I9536d34f1b6d6285c2b867b9fa96907e8087876e

---
+++ satellite/eventing/changestream/processor.go
@@ -0,0 +1,135 @@
+// Copyright (C) 2025 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package changestream
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/storj/satellite/metabase"
+)
+
+// Processor processes change stream records in batches (parallel). This contains the logic to follow child partitions.
+func Processor(ctx context.Context, adapter metabase.ChangeStreamAdapter, feedName string, startTime time.Time, fn func(record metabase.DataChangeRecord) error) error {
+	tracker := &Tracker{
+		todo:    make(map[string]Todo),
+		status:  make(map[string]TodoStatus),
+		receive: make(chan Todo),
+	}
+
+	eg, childCtx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case todoItem := <-tracker.receive:
+				eg.Go(func() error {
+					partitions, err := adapter.ChangeStream(childCtx, feedName, todoItem.Token, todoItem.StarTimestamp, func(record metabase.DataChangeRecord) error {
+						return fn(record)
+					})
+					if err != nil {
+						tracker.Failed(todoItem.Token)
+						//nolint
+						return nil
+					}
+					for _, partition := range partitions {
+						for _, children := range partition.ChildPartitions {
+							tracker.Add(children.Token, children.ParentPartitionTokens, partition.StartTimestamp, partition.RecordSequence)
+						}
+					}
+					tracker.Finish(todoItem.Token)
+					return nil
+				})
+			}
+		}
+	})
+	tracker.Add("", nil, startTime, "")
+	return eg.Wait()
+}
+
+// Todo represents a partition to be processed.
+type Todo struct {
+	Token          string
+	ParentTokens   []string
+	StarTimestamp  time.Time
+	RecordSequence string
+}
+
+// TodoStatus represents the processing status of a partition.
+type TodoStatus int
+
+const (
+	statusReceived TodoStatus = iota
+	statusRunning
+	statusFinished
+)
+
+// Tracker tracks the processing status of partitions.
+type Tracker struct {
+	mu   sync.Mutex
+	todo map[string]Todo
+	// TODO: this one kept in memory forever. We should have a way to clean it up.
+	status  map[string]TodoStatus
+	receive chan Todo
+}
+
+// Add adds a new token to be tracked, and notify the listener, if new partitions are ready to be processed.
+func (t *Tracker) Add(token string, parentTokens []string, start time.Time, recordSequence string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if _, found := t.status[token]; found {
+		return
+	}
+	t.todo[token] = Todo{
+		Token:          token,
+		ParentTokens:   parentTokens,
+		StarTimestamp:  start,
+		RecordSequence: recordSequence,
+	}
+	t.status[token] = statusReceived
+	t.notifyReady()
+}
+
+// notifyReady checks for partitions that are ready to be processed and sends them to the receive channel.
+func (t *Tracker) notifyReady() {
+	for _, todo := range t.todo {
+		if t.status[todo.Token] != statusReceived {
+			continue
+		}
+		if !allFinished(t.status, todo.ParentTokens) {
+			continue
+		}
+		t.status[todo.Token] = statusRunning
+		t.receive <- todo
+	}
+}
+
+func allFinished(status map[string]TodoStatus, tokens []string) bool {
+	for _, token := range tokens {
+		if status[token] != statusFinished {
+			return false
+		}
+	}
+	return true
+}
+
+// Finish marks a token as finished.
+func (t *Tracker) Finish(token string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.status[token] = statusFinished
+	delete(t.todo, token)
+}
+
+// Failed marks a token as failed, so it can be retried.
+func (t *Tracker) Failed(token string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.status[token] = statusReceived
+}
--- satellite/eventing/changestream/service.go
+++ satellite/eventing/changestream/service.go
@@ -5,13 +5,11 @@ package changestream
 
 import (
 	"context"
-	"slices"
 	"sync"
 	"time"
 
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 
 	"storj.io/storj/satellite/eventing"
 	"storj.io/storj/satellite/metabase"
@@ -180,59 +178,4 @@ func (s *Service) Close() error {
 		err = errs.Combine(err, publisher.Close())
 	}
 	return err
-}
-
-// Processor processes change stream records in batches (parallel). This contains the logic to follow child partitions.
-func Processor(ctx context.Context, adapter metabase.ChangeStreamAdapter, feedName string, startTime time.Time, fn func(record metabase.DataChangeRecord) error) error {
-	var mu sync.Mutex
-	// TODO: from the spanner documentation it's not clear the graph structure returned by changestream. Might be enough to use []ChildPartitionsRecord.
-	//  or we can execute all potential ready to listen partitions parallel not just one level.
-	var todo [][]metabase.ChildPartitionsRecord
-	for {
-		if ctx.Err() != nil {
-			return ctx.Err()
-		}
-		if len(todo) == 0 {
-			partitions, err := adapter.ChangeStream(ctx, feedName, "", startTime, func(record metabase.DataChangeRecord) error {
-				return fn(record)
-			})
-			if err != nil {
-				return errs.Wrap(err)
-			}
-			todo = append(todo, partitions)
-			continue
-		}
-
-		// TODO: make sure parents are already processed
-		slices.SortFunc(todo, func(a, b []metabase.ChildPartitionsRecord) int {
-			return a[0].StartTimestamp.Compare(b[0].StartTimestamp)
-		})
-
-		eg, childCtx := errgroup.WithContext(ctx)
-		nextItem := todo[0]
-		for _, p := range nextItem {
-			for _, child := range p.ChildPartitions {
-				eg.Go(func() error {
-					child := child
-					partitions, err := adapter.ChangeStream(childCtx, feedName, child.Token, p.StartTimestamp, func(record metabase.DataChangeRecord) error {
-						return fn(record)
-					})
-					if err == nil {
-						mu.Lock()
-						if len(partitions) > 0 {
-							todo = append(todo, partitions)
-						}
-						mu.Unlock()
-					}
-					return err
-				})
-			}
-		}
-		err := eg.Wait()
-		if err != nil {
-			return errs.Wrap(err)
-		}
-
-		todo = todo[1:]
-	}
 }
