f2c085643595b29ceb42416e330579f3c3e66adc
Márton Elek <elek@apache.org>
2025-11-24 14:15:22 +0100

    scripts: tag-release.sh doesn't create dedicated commits
    This simplifies the development process, as we don't need to build the commit 2x (once on the  branch, once for the tagged commit).
    
    Also: `git describe --tags` will work.
    
    Change-Id: I9ad07cff4cd7c6a76027c203c2a91ac62430b561

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -3,11 +3,7 @@
 ## This script:
 #
 # 1) Makes sure the current git working tree is clean
-# 2) Creates a release file that changes the build defaults to include
-#    a timestamp, a commit hash, a version number, and set the release
-#    flag to true.
-# 3) commits that release file and tags it with the release version
-# 4) resets the working tree back
+# 2) Creates the git tag
 #
 # This script should be used instead of 'git tag' for Storj releases,
 # so downstream users developing with Go 1.11+ style modules find code
@@ -30,42 +26,6 @@ if [[ "$(git diff --stat)" != '' ]] || [[ -n "$(git status -s)" ]]; then
   echo "git working tree unclean"
   exit 1
 fi
-
-TIMESTAMP=$(date +%s)
-COMMIT=$(git rev-parse HEAD)
-
-cat > ./private/version/release.go <<EOF
-// Copyright (C) 2020 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-package version
-
-import _ "unsafe" // needed for go:linkname
-
-//go:linkname buildTimestamp storj.io/common/version.buildTimestamp
-var buildTimestamp string = "$TIMESTAMP"
-
-//go:linkname buildCommitHash storj.io/common/version.buildCommitHash
-var buildCommitHash string = "$COMMIT"
-
-//go:linkname buildVersion storj.io/common/version.buildVersion
-var buildVersion string = "$VERSION"
-
-//go:linkname buildRelease storj.io/common/version.buildRelease
-var buildRelease string = "true"
-
-// ensure that linter understands that the variables are being used.
-func init() { use(buildTimestamp, buildCommitHash, buildVersion, buildRelease) }
-
-func use(...interface{}) {}
-EOF
-
-gofmt -w -s ./private/version/release.go
-go install ./private/version
-
-git add ./private/version/release.go >/dev/null
-git commit -m "release $VERSION" >/dev/null
 if git tag $VERSION; then
   echo successfully created tag $VERSION
 fi
-git reset --hard $COMMIT >/dev/null
