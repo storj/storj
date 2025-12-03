4bb49f13afd328840a943367f2e780c831c4e909
Vitalii <takur12345@gmail.com>
2025-12-03 15:23:37 +0200

    web/multinode: add pinia modules
    Issue: https://github.com/storj/storj/issues/7688
    
    Change-Id: Ib37421678ea29c2a5bb2d58fd809545388692518

---
+++ web/multinode/src/app/store/pinia/notificationsStore.ts
@@ -0,0 +1,86 @@
+// Copyright (C) 2025 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+import { defineStore } from 'pinia';
+import { reactive } from 'vue';
+
+import { DelayedNotification, NotificationPayload, NotificationType } from '@/app/types/delayedNotification';
+
+class NotificationsState {
+    public notificationQueue: DelayedNotification[] = [];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const useNotificationsStore = defineStore('notifications', () => {
+    const state = reactive<NotificationsState>(new NotificationsState());
+
+    function deleteNotification(id: string): void {
+        if (state.notificationQueue.length < 1) {
+            return;
+        }
+
+        const selectedNotification = state.notificationQueue.find(n => n.id === id);
+        if (selectedNotification) {
+            selectedNotification.pause();
+            state.notificationQueue.splice(state.notificationQueue.indexOf(selectedNotification), 1);
+        }
+    }
+
+    function notifySuccess(payload: NotificationPayload): void {
+        const notification: DelayedNotification = new DelayedNotification(
+            () => deleteNotification(notification.id),
+            NotificationType.Success,
+            payload.message,
+            payload.title,
+        );
+
+        state.notificationQueue.push(notification);
+    }
+
+    function notifyInfo(payload: NotificationPayload): void {
+        const notification: DelayedNotification = new DelayedNotification(
+            () => deleteNotification(notification.id),
+            NotificationType.Info,
+            payload.message,
+            payload.title,
+        );
+
+        state.notificationQueue.push(notification);
+    }
+
+    function notifyWarning(payload: NotificationPayload): void {
+        const notification: DelayedNotification = new DelayedNotification(
+            () => deleteNotification(notification.id),
+            NotificationType.Warning,
+            payload.message,
+            payload.title,
+        );
+
+        state.notificationQueue.push(notification);
+    }
+
+    function notifyError(payload: NotificationPayload): void {
+        const notification: DelayedNotification = new DelayedNotification(
+            () => deleteNotification(notification.id),
+            NotificationType.Error,
+            payload.message,
+            payload.title,
+        );
+
+        state.notificationQueue.push(notification);
+    }
+
+    function clear(): void {
+        state.notificationQueue.forEach(notification => notification.pause());
+        state.notificationQueue = [];
+    }
+
+    return {
+        state,
+        notifySuccess,
+        notifyInfo,
+        notifyWarning,
+        notifyError,
+        clear,
+    };
+});
