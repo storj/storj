7da1f04ae2d0db4c5a75e6273b15dde11778afa5
Ivan Fraixedes <ivan@fraixed.es>
2026-01-13 17:24:39 +0100

    storagenode/hashstore: Add build flags for synctest
    Move synctest to a separated file to be able to add the build flags to
    use synctest for supported Go versions.
    
    It has to be in different file to apply the build flags to only the
    synctest tests.
    
    For some reason, this never was spotted by our CI until now.
    
    Change-Id: I62278743d5a40a0566e1af57a529a5ed57bdb9d3

---
+++ storagenode/hashstore/db_synctest_test.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2026 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+//go:build goexperiment.synctest && go1.24
+
+package hashstore
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
+func TestDB_BackgroundCompactionLoop(t *testing.T) {
+	forAllTables(t, testDB_BackgroundCompactionLoop)
+}
+
+func testDB_BackgroundCompactionLoop(t *testing.T, cfg Config) {
+	synctest.Test(t, func(t *testing.T) {
+		db := newTestDB(t, cfg)
+		defer db.Close()
+
+		for func() bool {
+			db, s0, s1 := db.Stats()
+			return db.Compactions < 10 || s0.Compactions < 5 || s1.Compactions < 5
+		}() {
+			time.Sleep(time.Hour)
+		}
+	})
+}
