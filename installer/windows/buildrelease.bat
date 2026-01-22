47588ec7e9a2f0dc9d63e4d829d8d6fa02d64449
Egon Elbre <egonelbre@gmail.com>
2026-01-22 16:30:36 +0200

    installer/windows: fix path escaping
    In windows `copy a + b x` means concatenating paths,
    however the version numbers we are using contains `+`,
    so we need to properly escape them.
    
    Change-Id: I1c3749fec856146c401761e1c5ee38dc0835dc40

--- installer/windows/buildrelease.bat
+++ installer/windows/buildrelease.bat
@@ -2,5 +2,5 @@
 
 rem build msi installer for each release directory
 for /d %%d in (release\*) do (
-    call %~dp0build.bat %%d\windows_amd64\storagenode.exe %%d\windows_amd64\storagenode-updater.exe %%d\windows_amd64\storagenode.msi
+    call %~dp0build.bat "%%d\windows_amd64\storagenode.exe" "%%d\windows_amd64\storagenode-updater.exe" "%%d\windows_amd64\storagenode.msi"
 )
