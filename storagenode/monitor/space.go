3f773e8173420dcc8fa163dd06b99799d09f11a5
Márton Elek <elek@apache.org>
2026-04-17 15:32:33 +0200

    storagenode/monitor: Fix allocated_space for hashstore only storagenodes
     1. Looks like PreFlight call was missing, it was added to both implementation
     2. if it's hashstore only (select) node, the free disk space is calculated (if picestore is available, it's coming from there)
    
    Change-Id: Ibe6a108d54f91f0d16fb65ebf412d152b8fadd19

--- storagenode/monitor/space.go
+++ storagenode/monitor/space.go
@@ -7,10 +7,6 @@ import "context"
 
 // SpaceReport is an interface for reporting disk usage.
 type SpaceReport interface {
-
-	// PreFlightCheck checks if the disk is ready to use.
-	PreFlightCheck(ctx context.Context) error
-
 	// DiskSpace returns consolidated disk space state info.
 	// Used by reporting only.
 	DiskSpace(ctx context.Context) (_ DiskSpace, err error)
