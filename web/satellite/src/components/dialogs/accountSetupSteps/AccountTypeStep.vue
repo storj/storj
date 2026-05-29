da400b94505ba7ce7e479f96135d46f91d5c22a6
Vitalii <takur12345@gmail.com>
2026-05-29 12:51:42 +0300

    web/satellite: added more info about the new pricing
    Added a link to Pricing Details on the opt-in popup.
    Added a link to pricing page on onboarding plan selection step.
    Updated Upgrade flow indicating that the minimum fee is going to be $50 starting July 1.
    
    Change-Id: Ia163f9ebbceb9d1fc6e76058fba1f35d7025c5ec

--- web/satellite/src/components/dialogs/accountSetupSteps/AccountTypeStep.vue
+++ web/satellite/src/components/dialogs/accountSetupSteps/AccountTypeStep.vue
@@ -11,16 +11,22 @@
                     Account Type
                 </p>
                 <h2>Choose your account type</h2>
+                <v-row v-if="optInPopupEnabled" justify="center">
+                    <v-col cols="12" sm="10" md="10" lg="6">
+                        <v-alert variant="outlined" color="warning" class="mt-4">
+                            <strong>Important!</strong> New pricing going into effect July 1, 2026.
+                            <a href="https://www.storj.io/pricing" target="_blank" rel="noopener noreferrer">Learn more</a>.
+                        </v-alert>
+                    </v-col>
+                </v-row>
             </v-col>
         </v-row>
 
-        <v-row justify="center">
-            <PricingPlans
-                @free-click="emit('freeClick')"
-                @pro-click="emit('proClick')"
-                @pkg-click="emit('pkgClick')"
-            />
-        </v-row>
+        <PricingPlans
+            @free-click="emit('freeClick')"
+            @pro-click="emit('proClick')"
+            @pkg-click="emit('pkgClick')"
+        />
 
         <v-row justify="center" class="mt-8">
             <v-col cols="6" sm="4" md="3" lg="2">
@@ -31,7 +37,7 @@
 </template>
 
 <script setup lang="ts">
-import { VBtn, VCol, VContainer, VImg, VRow } from 'vuetify/components';
+import { VBtn, VCol, VContainer, VImg, VRow, VAlert } from 'vuetify/components';
 import { ChevronLeft } from 'lucide-vue-next';
 import { computed } from 'vue';
 import { useTheme } from 'vuetify';
@@ -44,6 +50,8 @@ import PricingPlans from '@/components/dialogs/upgradeAccountFlow/PricingPlans.v
 const configStore = useConfigStore();
 
 const theme = useTheme();
+
+const optInPopupEnabled = computed<boolean>(() => configStore.state.config.optInPopupEnabled);
 
 const logoSrc = computed<string>(() => {
     if (theme.global.current.value.dark) {
