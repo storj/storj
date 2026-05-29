603d9475183f4ec7c818b1d7e28fff197da5d8ab
Vitalii <takur12345@gmail.com>
2026-05-29 12:51:42 +0300

    web/satellite: added more info about the new pricing
    Added a link to Pricing Details on the opt-in popup.
    Added a link to pricing page on onboarding plan selection step.
    Updated Upgrade flow indicating that the minimum fee is going to be $50 starting July 1.
    
    Change-Id: Ia163f9ebbceb9d1fc6e76058fba1f35d7025c5ec

--- web/satellite/src/components/dialogs/PricingOptInDialog.vue
+++ web/satellite/src/components/dialogs/PricingOptInDialog.vue
@@ -61,6 +61,7 @@
 
                 <!-- Footer text -->
                 <p class="text-body-medium text-medium-emphasis mb-6">
+                    For a full breakdown of pricing details and fees, visit the <a class="link" href="https://storj.dev/dcs/pricing" target="_blank" rel="noopener noreferrer">Pricing Details</a>.<br>
                     Clicking Accept &amp; Continue confirms you've reviewed these changes.<br>
                     Have questions? Read the
                     <a href="https://storj.io/pricing" target="_blank" rel="noopener noreferrer" class="text-primary">pricing FAQ</a>
